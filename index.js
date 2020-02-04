07d449c2df910e42b96f78f5ba0d4735f800c953
David Carboni <david@carboni.io>
2020-02-04 21:38:27 +0000

    Added redirect and static directory

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.redirect('/home.html'))
+app.use(express.static('static'))
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
