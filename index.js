3f6426ba399d3a0cd98a1e6ebc9545c638187bfc
David Carboni <david@carboni.io>
2020-02-04 12:15:17 +0000

    Added Express.

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
 const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 5000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}/`);
-});
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
