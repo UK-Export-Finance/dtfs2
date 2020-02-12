fa8ce9c878e9f2c3b3dc2da36c13d8162530005f
ttbarnes <tony@tonybarnes.me>
2020-02-12 16:52:34 +0000

    start now page

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 const app = express()
 
 const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 5000
 
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  noCache: true,
+  watch: true
+});
+
 app.get('/', (req, res) => res.redirect('/home.html'))
+app.get('/start-now', (req, res) => res.render('start-now.njk'))
+
 app.use(express.static('static'))
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
