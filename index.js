4e3da12bb4c825db5a4322a529c42c10cda4c700
ttbarnes <tony@tonybarnes.me>
2020-02-13 15:34:00 +0000

    header/footer partials, rename `views` to `templates`

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 5000
 
-nunjucks.configure('views', {
+nunjucks.configure('templates', {
   autoescape: true,
   express: app,
   noCache: true,
