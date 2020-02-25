3dcd7d57b5a678d3afef0933d308a431651a9371
ttbarnes <tony@tonybarnes.me>
2020-02-25 17:23:50 +0000

    eligibility page template and mock data structure

--- mocks/index.js
+++ mocks/index.js
@@ -1,3 +1,38 @@
+const ELIGIBILITY_CRITERIA = [
+  {
+    id: 11,
+    description: 'The Supplier has confirmed in its Supplier Declaration that the Supply Contract does not involve agents and the Bank is not aware that any of the information contained within it is inaccurate.'
+  },
+  {
+    id: 12,
+    description: 'The cover period for each Transaction does not exceed 5 years, or such other period approved by UKEF (that has not lapsed or been withdrawn) in relation to bonds and/or loans for this Obligor.'
+  },
+  {
+    id: 13,
+    description: 'The total UKEF exposure, across all short-term schemes (including bond support and export working capital transactions), for this Obligor (including this Transaction) does not exceed £2 million, or such other limit approved by UKEF (that has not lapsed or been withdrawn).'
+  },
+  {
+    id: 14,
+    description: 'For a bond Transaction, the bond has not yet been issued or, where the bond has been issued, this was done no more than 3 months prior to the submission of this Inclusion Notice. For a loan Transaction, the loan has not yet been advance.'
+  },
+  {
+    id: 15,
+    description: 'The Requested Cover Start Date is no more than three months from the date of submission.'
+  },
+  {
+    id: 16,
+    description: 'The Supplier has confirmed in its Supplier Declaration that the Supply Contract does not involve any of the following Controlled Sectors: sharp arms defence, nuclear, radiological, biological, human cloning, pornography, tobacco or gambling, and the Bank is not aware that any of the information contained within it is inaccurate.'
+  },
+  {
+    id: 17,
+    description: 'The Bank has completed its Bank Due Diligence to its satisfaction in accordance with its policies and procedures without having to escalate to any Relevant Person.'
+  },
+  {
+    id: 18,
+    description: 'The fees and/or interest apply to the whole Cover Period, and have been set in accordance with the Bank’s normal pricing policies and, if any, minimum or overall pricing requirements set by UKEF.'
+  }
+];
+
 const MOCKS = {
   CONTRACTS: [
     {
@@ -17,8 +52,14 @@ const MOCKS = {
       aboutSupplyContract: {
         status: 'Complete'
       },
-      confirmEligibility: {
-        submissionType: 'Automatic Inclusion Notice'
+      eligibility: {
+        status: 'Incomplete',
+        submissionType: 'Automatic Inclusion Notice',
+        criteria: ELIGIBILITY_CRITERIA,
+        completedStatus: {
+          eligibilityCriteria: true,
+          supportingDocumentation: false
+        }
       },
       bondTransactions: {},
       loanTransactions: {},
@@ -80,9 +121,14 @@ const MOCKS = {
       aboutSupplyContract: {
         status: 'Incomplete'
       },
-      confirmEligibility: {
+      eligibility: {
         status: 'Complete',
-        submissionType: 'Manual Inclusion Notice'
+        submissionType: 'Manual Inclusion Notice',
+        criteria: ELIGIBILITY_CRITERIA,
+        completedStatus: {
+          eligibilityCriteria: true,
+          supportingDocumentation: true
+        }
       },
       bondTransactions: {
         items: [
@@ -226,48 +272,12 @@ const MOCKS = {
           corporateStructure: '',
           security: ''
         },
-        eligibilityCriteria: [
-          {
-            heading: 'Eligibility criterion 11',
-            description: 'The Supplier has confirmed in its Supplier Declaration that the Supply Contract does not involve agents and the Bank is not aware that any of the information contained within it is inaccurate.',
-            answer: true
-          },
-          {
-            heading: 'Eligibility criterion 12',
-            description: 'The cover period for each Transaction does not exceed 5 years, or such other period approved by UKEF (that has not lapsed or been withdrawn) in relation to bonds and/or loans for this Obligor.',
-            answer: false
-          },
-          {
-            heading: 'Eligibility criterion 13',
-            description: 'The total UKEF exposure, across all short-term schemes (including bond support and export working capital transactions), for this Obligor (including this Transaction) does not exceed £2 million, or such other limit approved by UKEF (that has not lapsed or been withdrawn).',
-            answer: true
-          },
-          {
-            heading: 'Eligibility criterion 14',
-            description: 'For a bond Transaction, the bond has not yet been issued or, where the bond has been issued, this was done no more than 3 months prior to the submission of this Inclusion Notice. For a loan Transaction, the loan has not yet been advance.',
-            answer: true
-          },
-          {
-            heading: 'Eligibility criterion 15',
-            description: 'The Requested Cover Start Date is no more than three months from the date of submission.',
-            answer: true
-          },
-          {
-            heading: 'Eligibility criterion 16',
-            description: 'The Supplier has confirmed in its Supplier Declaration that the Supply Contract does not involve any of the following Controlled Sectors: sharp arms defence, nuclear, radiological, biological, human cloning, pornography, tobacco or gambling, and the Bank is not aware that any of the information contained within it is inaccurate.',
-            answer: true
-          },
-          {
-            heading: 'Eligibility criterion 17',
-            description: 'The Bank has completed its Bank Due Diligence to its satisfaction in accordance with its policies and procedures without having to escalate to any Relevant Person.',
-            answer: true
-          },
-          {
-            heading: 'Eligibility criterion 18',
-            description: 'The fees and/or interest apply to the whole Cover Period, and have been set in accordance with the Bank’s normal pricing policies and, if any, minimum or overall pricing requirements set by UKEF.',
-            answer: true
+        eligibilityCriteria: ELIGIBILITY_CRITERIA.map(c => {
+          return {
+            ...c,
+            answer: (c.id === 12 || c.id === 14) ? false : true
           }
-        ]
+        })
       }
     },
   ],
