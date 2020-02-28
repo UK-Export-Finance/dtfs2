4418a8be754d1d45cbfef29ec49c176ac6d1daab
ttbarnes <tony@tonybarnes.me>
2020-02-28 10:33:24 +0000

    nunjucks with webpack

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ import MOCKS from './mocks'
 
 const app = express()
 
+const router = express.Router()
+
 const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 5000
-
-app.use(express.static('dist'))
 
 nunjucks.configure('templates', {
   autoescape: true,
@@ -20,60 +20,66 @@ nunjucks.configure('templates', {
 const getMockContractById = id =>
   MOCKS.CONTRACTS.find(c => c.id === id) || MOCKS.CONTRACTS[0];
 
-app.get('/', (req, res) => res.render('login.njk'))
+router.get('/', (req, res) => res.redirect('/login'))
 
-app.get('/start-now', (req, res) => res.render('start-now.njk'))
+router.get('/login', (req, res) => res.render('login.njk'))
 
-app.get('/before-you-start', (req, res) =>
+router.get('/start-now', (req, res) => res.render('start-now.njk'))
+
+router.get('/before-you-start', (req, res) =>
   res.render('before-you-start.njk', { mandatoryCriteria: MOCKS.MANDATORY_CRITERIA })
 )
 
-app.get('/before-you-start/bank-deal', (req, res) => res.render('before-you-start-bank-deal.njk'))
+router.get('/before-you-start/bank-deal', (req, res) => res.render('before-you-start-bank-deal.njk'))
 
-app.get('/unable-to-proceed', (req, res) => res.render('unable-to-proceed.njk'))
+router.get('/unable-to-proceed', (req, res) => res.render('unable-to-proceed.njk'))
 
-app.get('/dashboard', (req, res) =>
+router.get('/dashboard', (req, res) =>
   res.render('deals.njk', { contracts: MOCKS.CONTRACTS }))
 
-app.get('/dashboard/transactions', (req, res) =>
+router.get('/dashboard/transactions', (req, res) =>
   res.render('transactions.njk', { transactions: MOCKS.TRANSACTIONS }))
 
-app.get('/contract/:id', (req, res) =>
+router.get('/contract/:id', (req, res) =>
   res.render('contract-view.njk', getMockContractById(req.params.id))
 )
 
-app.get('/contract/:id/comments', (req, res) => 
+router.get('/contract/:id/comments', (req, res) => 
   res.render('contract-view-comments.njk', getMockContractById(req.params.id))
 )
 
-app.get('/contract/:id/submission-details', (req, res) => 
+router.get('/contract/:id/submission-details', (req, res) => 
   res.render('contract-submission-details.njk', {
     contract: getMockContractById(req.params.id),
     mandatoryCriteria: MOCKS.MANDATORY_CRITERIA
   })
 )
 
-app.get('/contract/:id/delete', (req, res) =>
+router.get('/contract/:id/delete', (req, res) =>
   res.render('contract-delete.njk', getMockContractById(req.params.id))
 )
 
-app.get('/contract/:id/eligibility/criteria', (req, res) =>
+router.get('/contract/:id/eligibility/criteria', (req, res) =>
   res.render('confirm-eligibility-criteria.njk', getMockContractById(req.params.id))
 )
 
-app.get('/contract/:id/eligibility/supporting-documentation', (req, res) =>
+router.get('/contract/:id/eligibility/supporting-documentation', (req, res) =>
   res.render('confirm-eligibility-supporting-documentation.njk', getMockContractById(req.params.id))
 )
 
-app.get('/contract/:id/eligibility/preview', (req, res) =>
+router.get('/contract/:id/eligibility/preview', (req, res) =>
   res.render('confirm-eligibility-preview.njk', {
     contract: getMockContractById(req.params.id),
     mandatoryCriteria: MOCKS.MANDATORY_CRITERIA
   })
 )
 
-app.get('/feedback', (req, res) => res.render('feedback.njk'))
+router.get('/feedback', (req, res) => res.render('feedback.njk'))
 
-app.get('/contact-us', (req, res) => res.render('contact.njk'))
+router.get('/contact-us', (req, res) => res.render('contact.njk'))
+
+app.use('/', router);
+
+app.use(express.static('dist'))
 
 app.listen(PORT, () => console.log(`DTFS2 app listening on port ${PORT}!`))
