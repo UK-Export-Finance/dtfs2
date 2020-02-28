9bf37ed21b6fa02b4671738b12eb5c390ee453ea
ttbarnes <tony@tonybarnes.me>
2020-02-28 10:33:24 +0000

    nunjucks with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ const path = require('path')
 const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const globEntries = require('webpack-glob-entries')
+
+// Webpack doesn't support glob paths. For the nunjucks-html-loader,
+// we need each path to be specified for it to work (even subdirectories)
+function returnEntries(globPath) {
+  let entries = globEntries(globPath, true);
+  let folderList = new Array();
+  for (let folder in entries) {
+    folderList.push(path.join(__dirname, entries[folder]));
+  }
+  return folderList;
+}
 
 module.exports = {
   entry: {
@@ -22,24 +34,24 @@ module.exports = {
         loader: "babel-loader",
       },
       {
-        // Loads the javacript into html template provided.
-        // Entry point is set below in HtmlWebPackPlugin in Plugins 
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            //options: { minimize: true }
+        test: /\.html$|njk|nunjucks/,
+        use: ['html-loader', {
+          loader: 'nunjucks-html-loader',
+          options: {
+            // base directory in which webpack is going to find any .njk files
+            searchPaths: [...returnEntries('./templates/**/')],
+            // minimize: true
           }
-        ]
+        }]
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
+          // creates `style` nodes from JS strings
           'style-loader',
-          // Translates CSS into CommonJS
+          // translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
+          // compiles Sass to CSS
           'sass-loader',
         ]
       }
@@ -47,8 +59,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './index.html',
-      filename: './index.html'
+      filename: 'index.html',
+      inject: 'body',
+      // tell webpack to pass index.njk through the nunjucks-html-loader
+      template: 'nunjucks-html-loader!./templates/index.njk',
     })
   ]
 }
