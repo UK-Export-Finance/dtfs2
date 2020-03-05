3f07b69e6f672b007ea9ebed9ebe76633187b149
Dan <dan.watkinson@gmail.com>
2020-03-05 10:55:03 +0000

    moved old example xmls, change translate code to use later format

--- k2reader/translate.tests.js
+++ k2reader/translate.tests.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 
 describe('translate', () => {
 
-  it('maps ./example-data/type-a-xml/329_NEWDEAL.xml -> ./example-data/contract-json/329_NEWDEAL.json', async () => {
-    const xml = await xmlReader('./example-data/type-a-xml/329_NEWDEAL.xml');
-    const expectedJson = fs.readFileSync('./example-data/contract-json/329_NEWDEAL.json', 'utf-8');
+  it('maps ./example-data/type-a-xml/10002_NEWDEAL.xml -> ./example-data/contract-json/10002_NEWDEAL.json', async () => {
+    const xml = await xmlReader('./example-data/type-a-xml/10002_NEWDEAL.xml');
+    const expectedJson = fs.readFileSync('./example-data/contract-json/10002_NEWDEAL.json', 'utf-8');
     const prettyExpectedJson = JSON.stringify( JSON.parse(expectedJson),null,2 );
 
     const actualJson = await translate(xml);
