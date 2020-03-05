a06c5bd42a92f383ce965ec2e8805c4f81c03036
Dan <dan.watkinson@gmail.com>
2020-03-05 11:29:47 +0000

    candidate folder structure?

--- k2reader/src/translate.tests.js
+++ k2reader/src/translate.tests.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 
 describe('translate', () => {
 
-  it('maps ./example-data/type-a-xml/10002_NEWDEAL.xml -> ./example-data/contract-json/10002_NEWDEAL.json', async () => {
-    const xml = await xmlReader('./example-data/type-a-xml/10002_NEWDEAL.xml');
-    const expectedJson = fs.readFileSync('./example-data/contract-json/10002_NEWDEAL.json', 'utf-8');
+  it('maps test-data/type-a-xml/10002_NEWDEAL.xml -> test-data/contract-json/10002_NEWDEAL.json', async () => {
+    const xml = await xmlReader('test-data/type-a-xml/10002_NEWDEAL.xml');
+    const expectedJson = fs.readFileSync('test-data/contract-json/10002_NEWDEAL.json', 'utf-8');
     const prettyExpectedJson = JSON.stringify( JSON.parse(expectedJson),null,2 );
 
     const actualJson = await translate(xml);
