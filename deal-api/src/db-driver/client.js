d1ecfe26f49887979c46ee7e40e55d207b12a446
Andrew Price <andrew@lemoncog.com>
2020-03-10 20:16:46 +0000

    Added deal controller and db driver

---
+++ deal-api/src/db-driver/client.js
@@ -0,0 +1,33 @@
+
+const { MongoClient } = require('mongodb');
+
+const { dbName, url, option } = require('../config/database.config');
+
+let client;
+
+let connection = null;
+
+
+const dbConnect = async () => {
+  client = await MongoClient.connect(url, { useNewUrlParser: true });
+  connection = client.db(dbName);
+  return connection;
+};
+
+const getConnection = async () => {
+  if (!connection) {
+    connection = await dbConnect();
+  }
+
+  return connection;
+};
+
+module.exports.get = getConnection;
+
+module.exports.getCollection = async (collectionName) => {
+  if (!connection) {
+    await getConnection();
+  }
+
+  return connection.collection(collectionName);
+};
