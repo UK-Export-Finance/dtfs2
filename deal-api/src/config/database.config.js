1805e00282426ca7e46b0dd30f53ebe386b99bc0
Andrew Price <andrew@lemoncog.com>
2020-03-10 20:16:46 +0000

    Added deal controller and db driver

--- deal-api/src/config/database.config.js
+++ deal-api/src/config/database.config.js
@@ -1,8 +1,13 @@
 // Connection URL
-const user = encodeURIComponent("<todo>");
-const password = encodeURIComponent("<todo>");
-const authMechanism = "DEFAULT";
+const user = encodeURIComponent('<todo>');
+const password = encodeURIComponent('<todo>');
+const authMechanism = 'DEFAULT';
+const dbName = '<todo>';
+const dbHost = 'deal-api-data';
+const dbPort = '27017';
 
 module.exports = {
-  url: `mongodb://${user}:${password}@localhost:27017/?authMechanism=${authMechanism}`
+  dbName,
+  url: `mongodb://${user}:${password}@${dbHost}:${dbPort}?authMechanism=${authMechanism}`,
+  option: {},
 };
