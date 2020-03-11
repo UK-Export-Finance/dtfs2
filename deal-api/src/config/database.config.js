7996d1e8dd8137239a71bc721ecd6b5cd41d8db1
Dan <dan.watkinson@gmail.com>
2020-03-11 18:38:41 +0000

    created separate .env files for running-everything-under-docker-compose vs running-a-services-dependencies-via-docker-compose-but-run-the-actual-service-locally.. the second being to support api-testing.. splitting the config into 2 files essentially covers the different host-name we need to use depending on whether we are connecting from inside or outside of the docker-network.

--- deal-api/src/config/database.config.js
+++ deal-api/src/config/database.config.js
@@ -1,11 +1,12 @@
-// Connection URL
-const user = encodeURIComponent('<todo>');
-const password = encodeURIComponent('<todo>');
-const authMechanism = 'DEFAULT';
-const dbName = '<todo>';
-// const dbHost = 'deal-api-data';
-const dbHost = 'localhost';
-const dbPort = '27017';
+const dotenv = require('dotenv');
+dotenv.config();
+
+const user =          process.env.MONGO_INITDB_ROOT_USERNAME;
+const password =      process.env.MONGO_INITDB_ROOT_PASSWORD;
+const authMechanism = process.env.MONGO_CLIENT_AUTH_MECHANISM;
+const dbName =        process.env.MONGO_INITDB_DATABASE;
+const dbHost =        process.env.MONGO_HOST;
+const dbPort =        process.env.MONGO_PORT;
 
 module.exports = {
   dbName,
