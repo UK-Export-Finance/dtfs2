7d1157114fcf10e593512aeaba355f79df50f580
David Carboni <david@carboni.io>
2020-03-11 09:52:08 +0000

    Updated branch name.

--- update-heroku.sh
+++ update-heroku.sh
@@ -1,4 +1,4 @@
 git checkout master && git pull --rebase
-git checkout heroku-dtfs2-portal && git pull --rebase && git merge --no-edit --commit master && git push
-git checkout heroku-dtfs2-api    && git pull --rebase && git merge --no-edit --commit master && git push
+git checkout heroku-dtfs2-portal   && git pull --rebase && git merge --no-edit --commit master && git push
+git checkout heroku-dtfs2-deal-api && git pull --rebase && git merge --no-edit --commit master && git push
 git checkout master
