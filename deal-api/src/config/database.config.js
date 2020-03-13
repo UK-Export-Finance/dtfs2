67b3d5a862e98adf8bdfa744a9e749989c8661c3
Dan <dan.watkinson@gmail.com>
2020-03-13 12:00:17 +0000

    heroku-style uri working locally

--- deal-api/src/config/database.config.js
+++ deal-api/src/config/database.config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const database = process.env.MONGO_INITDB_DATABASE;
-const connectionString = process.env.MONGO_CONNECTION_STRING
+const connectionString = process.env.MONGODB_URI
 
 module.exports = {
   database,
