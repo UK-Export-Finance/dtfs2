da1d4b3ebfa7f24c312ac7f96445f1f8515dc329
Dan <dan.watkinson@gmail.com>
2020-03-13 11:06:34 +0000

    moved mongo configuration to match herokus expectation

--- deal-api/src/config/database.config.js
+++ deal-api/src/config/database.config.js
@@ -1,15 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const user =          process.env.MONGO_INITDB_ROOT_USERNAME;
-const password =      process.env.MONGO_INITDB_ROOT_PASSWORD;
-const authMechanism = process.env.MONGO_CLIENT_AUTH_MECHANISM;
-const dbName =        process.env.MONGO_INITDB_DATABASE;
-const dbHost =        process.env.MONGO_HOST;
-const dbPort =        process.env.MONGO_PORT;
+const database = process.env.MONGO_INITDB_DATABASE;
+const connectionString = process.env.MONGO_CONNECTION_STRING
 
 module.exports = {
-  dbName,
-  url: `mongodb://${user}:${password}@${dbHost}:${dbPort}?authMechanism=${authMechanism}`,
+  database,
+  url: connectionString,
   option: {},
 };
