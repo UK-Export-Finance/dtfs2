99f7782fccd1547d1fd828ad2b6595a4f9384391
Dan <dan.watkinson@gmail.com>
2020-03-13 14:10:10 +0000

    industry sector mocks moved into mongodb

--- deal-api/src/controllers/banks.controller.js
+++ deal-api/src/controllers/banks.controller.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 
 const db = require('../db-driver/client');
 
-const findDeals = async (callback) => {
-  const collection = await db.getCollection('deals');
+const findBanks = async (callback) => {
+  const collection = await db.getCollection('banks');
 
   collection.find({}).toArray((err, result) => {
     assert.equal(err, null);
@@ -11,8 +11,8 @@ const findDeals = async (callback) => {
   });
 };
 
-const findOneDeal = async (id, callback) => {
-  const collection = await db.getCollection('deals');
+const findOneBank = async (id, callback) => {
+  const collection = await db.getCollection('banks');
 
   collection.findOne({ id }, (err, result) => {
     assert.equal(err, null);
@@ -21,18 +21,18 @@ const findOneDeal = async (id, callback) => {
 };
 
 exports.create = async (req, res) => {
-  const collection = await db.getCollection('deals');
+  const collection = await db.getCollection('banks');
   const deal = await collection.insertOne(req.body);
 
   res.status(200).send(deal);
 };
 
 exports.findAll = (req, res) => (
-  findDeals((deals) => res.status(200).send(deals))
+  findBanks((banks) => res.status(200).send(banks))
 );
 
 exports.findOne = (req, res) => (
-  findOneDeal(req.params.id, (deal) => res.status(200).send(deal))
+  findOneBank(req.params.id, (deal) => res.status(200).send(deal))
 );
 
 exports.update = (req, res) => {};
