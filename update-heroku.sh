efcff3bb7839698a1cae14475d7165253c740ccc
ttbarnes <tony@tonybarnes.me>
2020-03-16 09:44:57 +0000

    delete local heroku branches in update-heroku script

--- update-heroku.sh
+++ update-heroku.sh
@@ -1,6 +1,7 @@
 echo Running update-heroku script
 
 git checkout master && git pull --rebase
-git checkout heroku-dtfs2-portal  && git pull --rebase && git merge --no-edit --commit master && git push && git branch -d heroku-dtfs2-portal
-git checkout heroku-dtfs2-deal-api && git pull --rebase && git merge --no-edit --commit master && git push && git branch -d heroku-dtfs2-deal-api
+git checkout heroku-dtfs2-portal  && git pull --rebase && git merge --no-edit --commit master && git push
+git checkout heroku-dtfs2-deal-api && git pull --rebase && git merge --no-edit --commit master && git push
 git checkout master
+git branch -d heroku-dtfs2-portal && git branch -d heroku-dtfs2-deal-api
