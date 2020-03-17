d1be7414995d301e348b888cd2511e7f199a0a96
Dan <dan.watkinson@gmail.com>
2020-03-17 11:17:21 +0000

    mock data util now cleans old data before inserting

---
+++ utils/mock-data-loader/clean-all-tables.js
@@ -0,0 +1,55 @@
+const api = require('./api');
+
+const cleanBanks = async () => {
+  for (bank of await api.listBanks()) {
+    api.deleteBank(bank);
+  }
+}
+
+const cleanBondCurrencies = async () => {
+  for (bondCurrency of await api.listBondCurrencies()) {
+    api.deleteBondCurrency(bondCurrency);
+  }
+}
+
+const cleanCountries = async () => {
+  for (country of await api.listCountries()) {
+    api.deleteCountry(country);
+  }
+}
+
+const cleanDeals = async () => {
+  for (deal of await api.listDeals()) {
+    api.deleteDeal(deal);
+  }
+}
+
+const cleanIndustrySectors = async () => {
+  for (industrySector of await api.listIndustrySectors()) {
+    api.deleteIndustrySector(industrySector);
+  }
+}
+
+const cleanMandatoryCriteria = async () => {
+  for (mandatoryCriteria of await api.listMandatoryCriteria()) {
+    api.deleteMandatoryCriteria(mandatoryCriteria);
+  }
+}
+
+const cleanTransactions = async () => {
+  for (transaction of await api.listTransactions()) {
+    api.deleteTransaction(transaction);
+  }
+}
+
+const cleanAllTables = async () => {
+  await cleanBanks();
+  await cleanBondCurrencies();
+  await cleanCountries();
+  await cleanDeals();
+  await cleanIndustrySectors();
+  await cleanMandatoryCriteria();
+  await cleanTransactions();
+}
+
+module.exports = cleanAllTables;
