8994fb5e814e8f6eb18016c1db1864c25ae95516
Dan <dan.watkinson@gmail.com>
2020-03-20 13:45:11 +0000

    ability to restrict user access based on roles

--- users/service/src/models/user.js
+++ users/service/src/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     username: String,
     hash: String,
-    salt: String
+    salt: String,
+    roles: [String]
+
 });
 mongoose.model('User', UserSchema);
