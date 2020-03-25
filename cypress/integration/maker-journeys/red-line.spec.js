53944cb840abc06cc3352538548094b7ce970447
Dan <dan.watkinson@gmail.com>
2020-03-25 14:43:59 +0000

    first cypress journey

--- cypress/integration/maker-journeys/red-line.spec.js
+++ cypress/integration/maker-journeys/red-line.spec.js
@@ -1,22 +1,54 @@
 context('Red Line eligibility checking', () => {
 
-  xit('A deal that fails red-line checks is rejected.', () => {
-    // log in as maker
-    // create a deal
-    // enter data that will fail red-line checking
-      //TODO need list of reasons that red-line checking would fail
-      //assume it's "answering no to any of the questions"
-    // submit data that is going to fail red-line checking
-    // watch the failure occur and prove the journey is 'nice'
+  beforeEach(() => {
+
+    //[dw] at time of writing, the portal was throwing exceptions; this stops cypress caring
+    cy.on('uncaught:exception', (err, runnable) => {
+      console.log(err.stack)
+      return false
+    })
+
   });
 
-  xit('A deal that passes red-line checks can progress to enter supply details.', () => {
-    //+ log in as maker
-    //+ create a deal
-    // enter data that will pass red-line checking
-      //TODO need example of 'good' data
-    // submit data that is going to pass red-line checking
-    // prove that we land on the 'supply contract details' page
+  it('A deal that fails red-line checks is rejected.', () => {
+
+// go to the homepage
+    cy.visit('http://localhost:5000/');
+
+// log in
+    cy.get('#email').type('testing');
+    cy.get('#password').type('testing');
+    cy.get('#LogIn').click();
+    // confirm that we're on '/start-now'
+
+// click 'Create new Submission'
+    cy.contains('Create new submission').click();
+    // confirm that we're on '/before-you-start'
+
+    cy.get('#criteria-met-2').click();
+    cy.get('#submit-red-line').click();
+
+    // confirm we're in the fail case
+  });
+
+  it('A deal that passes red-line checks can progress to enter supply details.', () => {
+    // go to the homepage
+        cy.visit('http://localhost:5000/');
+
+    // log in
+        cy.get('#email').type('testing');
+        cy.get('#password').type('testing');
+        cy.get('#LogIn').click();
+        // confirm that we're on '/start-now'
+
+    // click 'Create new Submission'
+        cy.contains('Create new submission').click();
+        // confirm that we're on '/before-you-start'
+
+        cy.get('#criteria-met').click();
+        cy.get('#submit-red-line').click();
+
+        // confirm we're in the success case
   });
 
 })
