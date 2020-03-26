18d038015b8255a09a4e80215dddb16f28c6ebe9
ttbarnes <tony@tonybarnes.me>
2020-03-26 15:56:28 +0000

    portal `about deal` routes/form prep for integration

--- portal/server/routes/misc.js
+++ portal/server/routes/misc.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/feedback', (req, res) => res.render('feedback.njk'));
 
-router.post('/feedback', (req, res) => res.redirect('./feedback'));
+router.post('/feedback', (req, res) => res.redirect('/feedback'));
 
 router.get('/contact-us', (req, res) => res.render('contact.njk'));
 
