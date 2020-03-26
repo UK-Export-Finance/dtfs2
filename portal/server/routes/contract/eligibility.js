ec147243503afd1332bae49f710d779008d83636
ttbarnes <tony@tonybarnes.me>
2020-03-26 17:29:00 +0000

    portal `eligibility`routes/form prep for integration

--- portal/server/routes/contract/eligibility.js
+++ portal/server/routes/contract/eligibility.js
@@ -5,7 +5,29 @@ const router = express.Router();
 
 router.get('/contract/:id/eligibility/criteria', async (req, res) => res.render('eligibility/eligibility-criteria.njk', await api.contract(req.params.id, req.session.userToken)));
 
+router.post('/contract/:id/eligibility/criteria', (req, res) => {
+  const redirectUrl = `/contract/${req.params.id}/eligibility/criteria`;
+  return res.redirect(redirectUrl);
+});
+
+router.post('/contract/:id/eligibility/criteria/save-go-back', (req, res) => {
+  // TODO: check if the 'go back' action takes you back to this page (currently staging is down so can't check)
+  const redirectUrl = `/contract/${req.params.id}`;
+  return res.redirect(redirectUrl);
+});
+
 router.get('/contract/:id/eligibility/supporting-documentation', async (req, res) => res.render('eligibility/eligibility-supporting-documentation.njk', await api.contract(req.params.id, req.session.userToken)));
+
+router.post('/contract/:id/eligibility/supporting-documentation', (req, res) => {
+  const redirectUrl = `/contract/${req.params.id}/eligibility/supporting-documentation`;
+  return res.redirect(redirectUrl);
+});
+
+router.post('/contract/:id/eligibility/supporting-documentation/save-go-back', (req, res) => {
+  // TODO: check if the 'go back' action takes you back to this page (currently staging is down so can't check)
+  const redirectUrl = `/contract/${req.params.id}`;
+  return res.redirect(redirectUrl);
+});
 
 router.get('/contract/:id/eligibility/preview', async (req, res) => res.render('eligibility/eligibility-preview.njk', {
   contract: await api.contract(req.params.id, req.session.userToken),
