11f32e6e8ed66f96931e1992e58fe0f985dc10b6
ttbarnes <tony@tonybarnes.me>
2020-03-26 15:56:28 +0000

    portal `about deal` routes/form prep for integration

--- portal/server/routes/contract.js
+++ portal/server/routes/contract.js
@@ -14,25 +14,62 @@ router.get('/contract/:id/submission-details', async (req, res) => res.render('c
 
 router.get('/contract/:id/delete', async (req, res) => res.render('contract/contract-delete.njk', await api.contract(req.params.id, req.session.userToken)));
 
+
+// ABOUT ROUTES START
 router.get('/contract/:id/about/supplier', async (req, res) => res.render('about/about-supplier.njk', {
   contract: await api.contract(req.params.id, req.session.userToken),
   countries: await api.countries(req.session.userToken),
   industrySectors: await api.industrySectors(req.session.userToken),
 }));
 
-router.get('/contract/:id/about/financial', async (req, res) => res.render('about/about-supply-financial.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-  currencies: await api.bondCurrencies(req.session.userToken),
-}));
+router.post('/contract/:id/about/supplier', (req, res) => {
+  const redirectUrl = `/contract/${req.params.id}/about/supplier`;
+  return res.redirect(redirectUrl);
+});
+
+router.post('/contract/:id/about/supplier/save-go-back', (req, res) => {
+  // TODO: check if the 'go back' action takes you back to this page (currently staging is down so can't check)
+  const redirectUrl = `/contract/${req.params.id}`;
+  return res.redirect(redirectUrl);
+});
 
 router.get('/contract/:id/about/buyer', async (req, res) => res.render('about/about-supply-buyer.njk', {
   contract: await api.contract(req.params.id, req.session.userToken),
   countries: await api.countries(req.session.userToken),
 }));
 
+router.post('/contract/:id/about/buyer', (req, res) => {
+  const redirectUrl = `/contract/${req.params.id}/about/buyer`;
+  return res.redirect(redirectUrl);
+});
+
+router.post('/contract/:id/about/buyer/save-go-back', (req, res) => {
+  // TODO: check if the 'go back' action takes you back to this page (currently staging is down so can't check)
+  const redirectUrl = `/contract/${req.params.id}`;
+  return res.redirect(redirectUrl);
+});
+
+router.get('/contract/:id/about/financial', async (req, res) => res.render('about/about-supply-financial.njk', {
+  contract: await api.contract(req.params.id, req.session.userToken),
+  currencies: await api.bondCurrencies(req.session.userToken),
+}));
+
+router.post('/contract/:id/about/financial', (req, res) => {
+  const redirectUrl = `/contract/${req.params.id}/about/financial`;
+  return res.redirect(redirectUrl);
+});
+
+router.post('/contract/:id/about/financial/save-go-back', (req, res) => {
+  // TODO: check if the 'go back' action takes you back to this page (currently staging is down so can't check)
+  const redirectUrl = `/contract/${req.params.id}`;
+  return res.redirect(redirectUrl);
+});
+
 router.get('/contract/:id/about/preview', async (req, res) => res.render('about/about-supply-preview.njk', {
   contract: await api.contract(req.params.id, req.session.userToken),
 }));
+
+// ABOUT ROUTES END
 
 router.get('/contract/:id/eligibility/criteria', async (req, res) => res.render('eligibility/eligibility-criteria.njk', await api.contract(req.params.id, req.session.userToken)));
 
