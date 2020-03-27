3c9347865edfb2f1c85cec32decb52a217006de8
ttbarnes <tony@tonybarnes.me>
2020-03-27 17:17:42 +0000

    portal api query wrapper working with contract routes, pass _id to templates

--- portal/server/routes/contract.js
+++ portal/server/routes/contract.js
@@ -12,81 +12,222 @@ const makeApiCall = async (query) => {
   }
 };
 
-/* eslint-disable */
 const getApiData = (query, res) => new Promise((resolve) =>
-  makeApiCall(query).then((data) => resolve(data)) // eslint-disable-line implicit-arrow-linebreak
-    .catch(() => { // eslint-disable-line arrow-body-style
-      // redirect to login (currently assuming all errors are Auth errors)
+  makeApiCall(query).then((data) => resolve(data))
+    .catch(() => { // eslint-disable-line
+      // redirect to login (currently assuming all errors are auth errors)
       return res.redirect('/');
     }));
-/* eslint-enable */
+
+const getDealIdAndToken = (req) => {
+  const { _id, bondId } = req.params;
+  const { userToken } = req.session;
+  return { _id, bondId, userToken };
+};
 
 router.get('/contract/:_id', async (req, res) => {
-  // eslint-disable-next-line no-underscore-dangle
-  const contract = await getApiData(
-    api.contract(req.params._id, req.session.userToken),
-    res,
-  );
-  return res.render('contract/contract-view.njk', contract);
+  const { _id, userToken } = getDealIdAndToken(req);
+
+  return res.render('contract/contract-view.njk',
+    await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ));
 });
 
-router.get('/contract/:id/comments', async (req, res) => res.render('contract/contract-view-comments.njk', await api.contract(req.params.id, req.session.userToken)));
+router.get('/contract/:_id/comments', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
 
-router.get('/contract/:id/submission-details', async (req, res) => res.render('contract/contract-submission-details.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-  mandatoryCriteria: await api.mandatoryCriteria(req.session.userToken),
-}));
+  return res.render('contract/contract-view-comments.njk',
+    await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ));
+});
 
-router.get('/contract/:id/delete', async (req, res) => res.render('contract/contract-delete.njk', await api.contract(req.params.id, req.session.userToken)));
+router.get('/contract/:_id/submission-details', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
 
-router.get('/contract/:id/about/supplier', async (req, res) => res.render('about/about-supplier.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-  countries: await api.countries(req.session.userToken),
-  industrySectors: await api.industrySectors(req.session.userToken),
-}));
+  return res.render('contract/contract-submission-details.njk', {
+    contract: await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ),
+    mandatoryCriteria: await getApiData(
+      api.mandatoryCriteria(userToken),
+      res,
+    ),
+  });
+});
 
-router.get('/contract/:id/about/financial', async (req, res) => res.render('about/about-supply-financial.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-  currencies: await api.bondCurrencies(req.session.userToken),
-}));
+router.get('/contract/:_id/delete', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
 
-router.get('/contract/:id/about/buyer', async (req, res) => res.render('about/about-supply-buyer.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-  countries: await api.countries(req.session.userToken),
-}));
+  return res.render('contract/contract-delete.njk',
+    await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ));
+});
 
-router.get('/contract/:id/about/preview', async (req, res) => res.render('about/about-supply-preview.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-}));
+router.get('/contract/:_id/about/supplier', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
 
-router.get('/contract/:id/eligibility/criteria', async (req, res) => res.render('eligibility/eligibility-criteria.njk', await api.contract(req.params.id, req.session.userToken)));
+  return res.render('about/about-supplier.njk', {
+    contract: await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ),
+    countries: await getApiData(
+      api.countries(userToken),
+      res,
+    ),
+    industrySectors: await getApiData(
+      api.industrySectors(userToken),
+      res,
+    ),
+  });
+});
 
-router.get('/contract/:id/eligibility/supporting-documentation', async (req, res) => res.render('eligibility/eligibility-supporting-documentation.njk', await api.contract(req.params.id, req.session.userToken)));
+router.get('/contract/:_id/about/financial', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
 
-router.get('/contract/:id/eligibility/preview', async (req, res) => res.render('eligibility/eligibility-preview.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-  mandatoryCriteria: await api.mandatoryCriteria(req.session.userToken),
-}));
+  res.render('about/about-supply-financial.njk', {
+    contract: await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ),
+    currencies: await getApiData(
+      api.bondCurrencies(userToken),
+      res,
+    ),
+  });
+});
 
-router.get('/contract/:id/bond/:bondId/details', async (req, res) => res.render('bond/bond-details.njk',
-  await api.contractBond(req.params.id, req.params.bondId, req.session.userToken)));
+router.get('/contract/:_id/about/buyer', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
 
-router.get('/contract/:id/bond/:bondId/financial-details', async (req, res) => res.render('bond/bond-financial-details.njk', {
-  ...await api.contractBond(req.params.id, req.params.bondId, req.session.userToken),
-  currencies: await api.bondCurrencies(req.session.userToken),
-}));
+  return res.render('about/about-supply-buyer.njk', {
+    contract: await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ),
+    countries: await getApiData(
+      api.countries(userToken),
+      res,
+    ),
+  });
+});
 
-router.get('/contract/:id/bond/:bondId/fee-details', async (req, res) => res.render('bond/bond-fee-details.njk',
-  await api.contractBond(req.params.id, req.params.bondId, req.session.userToken)));
+router.get('/contract/:_id/about/preview', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
 
-router.get('/contract/:id/bond/:bondId/preview', async (req, res) => res.render('bond/bond-preview.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-  bond: api.contractBond(req.params.id, req.params.bondId, req.session.userToken),
-}));
+  return res.render('about/about-supply-preview.njk',
+    await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ));
+});
 
-router.get('/contract/:id/bond/:bondId/delete', async (req, res) => res.render('bond/bond-delete.njk', {
-  contract: await api.contract(req.params.id, req.session.userToken),
-  bond: api.contractBond(req.params.id, req.params.bondId, req.session.userToken),
-}));
+router.get('/contract/:_id/eligibility/criteria', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
+
+  return res.render('eligibility/eligibility-criteria.njk',
+    await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ));
+});
+
+router.get('/contract/:_id/eligibility/supporting-documentation', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
+
+  return res.render('eligibility/eligibility-supporting-documentation.njk',
+    await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ));
+});
+
+router.get('/contract/:_id/eligibility/preview', async (req, res) => {
+  const { _id, userToken } = getDealIdAndToken(req);
+
+  return res.render('eligibility/eligibility-preview.njk', {
+    contract: await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ),
+    mandatoryCriteria: await getApiData(
+      api.mandatoryCriteria(userToken),
+      res,
+    ),
+  });
+});
+
+router.get('/contract/:_id/bond/:bondId/details', async (req, res) => {
+  const { _id, bondId, userToken } = getDealIdAndToken(req);
+
+  return res.render('bond/bond-details.njk',
+    await getApiData(
+      api.contractBond(_id, bondId, userToken),
+      res,
+    ));
+});
+
+router.get('/contract/:_id/bond/:bondId/financial-details', async (req, res) => {
+  const { _id, bondId, userToken } = getDealIdAndToken(req);
+
+  return res.render('bond/bond-financial-details.njk', {
+    ...await getApiData(
+      api.contractBond(_id, bondId, userToken),
+      res,
+    ),
+    currencies: await getApiData(
+      api.bondCurrencies(userToken),
+      res,
+    ),
+  });
+});
+
+router.get('/contract/:_id/bond/:bondId/fee-details', async (req, res) => {
+  const { _id, bondId, userToken } = getDealIdAndToken(req);
+
+  return res.render('bond/bond-fee-details.njk', {
+    ...await getApiData(
+      api.contractBond(_id, bondId, userToken),
+      res,
+    ),
+  });
+});
+
+router.get('/contract/:_id/bond/:bondId/preview', async (req, res) => {
+  const { _id, bondId, userToken } = getDealIdAndToken(req);
+
+  return res.render('bond/bond-preview.njk', {
+    contract: await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ),
+    ...await getApiData(
+      api.contractBond(_id, bondId, userToken),
+      res,
+    ),
+  });
+});
+
+router.get('/contract/:_id/bond/:bondId/delete', async (req, res) => {
+  const { _id, bondId, userToken } = getDealIdAndToken(req);
+
+  return res.render('bond/bond-delete.njk', {
+    contract: await getApiData(
+      api.contract(_id, userToken),
+      res,
+    ),
+    ...await getApiData(
+      api.contractBond(_id, bondId, userToken),
+      res,
+    ),
+  });
+});
 
 export default router;
