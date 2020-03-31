8bf121aec07ef8cf6938fb523c0b905b349b5920
Dan <dan.watkinson@gmail.com>
2020-03-31 13:20:30 +0100

    bulk insert of deals for e2e-tests

--- cypress/integration/journeys/maker/contract.spec.js
+++ cypress/integration/journeys/maker/contract.spec.js
@@ -1,20 +1,25 @@
 const {createADeal} = require('../../missions');
-const {contract} = require('../../pages');
+const {deleteAllDeals} = require('../../missions/deal-api');
+
+const {contract, dashboard} = require('../../pages');
 const relative = require('../../relativeURL');
 
+const user = {username: 'MAKER', password: 'MAKER'};
+
 context('View a deal', () => {
-  beforeEach(() => {
+  beforeEach( async() => {
     // [dw] at time of writing, the portal was throwing exceptions; this stops cypress caring
     cy.on('uncaught:exception', (err, runnable) => {
       console.log(err.stack);
       return false;
     });
+
+    await deleteAllDeals(user);
   });
 
   it('The deal page contains the data entered in /before-you-start/bank-details', () => {
     createADeal({
-      username:'MAKER',
-      password:'MAKER',
+      ... user,
       bankDealId: 'someDealId',
       bankDealName: 'someDealName'
     });
@@ -28,4 +33,18 @@ context('View a deal', () => {
     });
 
   });
+
+  it('A created deal appears on the dashboard', () => {
+    createADeal({
+      ... user,
+      bankDealId: 'abc/1/def',
+      bankDealName: 'Tibettan submarine acquisition scheme'
+    });
+
+    dashboard.visit();
+    dashboard.deal('abc/1/def').click();
+
+    cy.url().should('include', '/contract/');
+  });
+
 });
--- cypress/integration/journeys/maker/dashboard.spec.js
+++ cypress/integration/journeys/maker/dashboard.spec.js
@@ -1,10 +1,16 @@
-const {deleteAllDeals, createADeal, createManyDeals} = require('../../missions');
+const {createADeal, login} = require('../../missions');
+const {deleteAllDeals, createManyDeals} = require('../../missions/deal-api');
 const {dashboard} = require('../../pages');
 const relative = require('../../relativeURL');
 
 const user = {username: 'MAKER', password: 'MAKER'};
 
-context('Pick a deal from the dashboard', async () => {
+// test data we want to set up + work with..
+const twentyOneDeals = require('./twentyOneDeals');
+const twentyDeals = twentyOneDeals.map(deal=>deal.details.bankDealId).slice(0,20);
+const oneDeal = [twentyOneDeals[20].details.bankDealId];
+
+context('Pick a deal from the dashboard', () => {
   beforeEach( async() => {
     // [dw] at time of writing, the portal was throwing exceptions; this stops cypress caring
     cy.on('uncaught:exception', (err, runnable) => {
@@ -13,190 +19,33 @@ context('Pick a deal from the dashboard', async () => {
     });
 
     await deleteAllDeals(user);
+    await createManyDeals(twentyOneDeals, { ...user });
   });
 
-  it('A created deal appears on the dashboard', () => {
-    const deal = {
-      bankDealId: 'abc/1/def',
-      bankDealName: 'Tibettan submarine acquisition scheme'
-    }
-
-    createADeal({
-      ...user,
-      ...deal
-    });
-
-    // log out and log back in? feels un-neccesary so not doing it...
-    dashboard.visit();
-    dashboard.deal('abc/1/def').click();
-
-    cy.url().should('include', '/contract/');
-
-  });
-
-
-  it('A created deal appears on the dashboard', () => {
-    const deals = [{
-        bankDealId: 'abc/1/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/2/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/3/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/4/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/5/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/6/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/7/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/8/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/9/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/10/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/11/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/12/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/13/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/14/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/15/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/16/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/17/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/18/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/19/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/20/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }, {
-        bankDealId: 'abc/21/def',
-        bankDealName: 'Tibettan submarine acquisition scheme'
-      }];
-
-
-    createManyDeals({
-      ...user,
-      deals
-    });
-
-    // log out and log back in? feels un-neccesary so not doing it...
+  it('The dashboard displays paginated data, pagesize=20', () => {
+    login({...user});
     dashboard.visit();
 
-    dashboard.confirmDealsPresent([
-      'abc/1/def',
-      'abc/2/def',
-      'abc/3/def',
-      'abc/4/def',
-      'abc/5/def',
-      'abc/6/def',
-      'abc/7/def',
-      'abc/8/def',
-      'abc/9/def',
-      'abc/10/def',
-      'abc/11/def',
-      'abc/12/def',
-      'abc/13/def',
-      'abc/14/def',
-      'abc/15/def',
-      'abc/16/def',
-      'abc/17/def',
-      'abc/18/def',
-      'abc/19/def',
-      'abc/20/def',
-    ]);
-
+    //test ininital dashboard page
+    dashboard.confirmDealsPresent(twentyDeals);
     dashboard.totalItems().invoke('text').then((text) => {
       expect(text.trim()).equal('(21 items)');
     });
 
+    //prove the Next button
     dashboard.next().click();
+    dashboard.confirmDealsPresent(oneDeal);
 
-    dashboard.confirmDealsPresent([
-      'abc/21/def',
-    ]);
-
+    //prove the Previous button
     dashboard.previous().click();
+    dashboard.confirmDealsPresent(twentyDeals);
 
-    dashboard.confirmDealsPresent([
-      'abc/1/def',
-      'abc/2/def',
-      'abc/3/def',
-      'abc/4/def',
-      'abc/5/def',
-      'abc/6/def',
-      'abc/7/def',
-      'abc/8/def',
-      'abc/9/def',
-      'abc/10/def',
-      'abc/11/def',
-      'abc/12/def',
-      'abc/13/def',
-      'abc/14/def',
-      'abc/15/def',
-      'abc/16/def',
-      'abc/17/def',
-      'abc/18/def',
-      'abc/19/def',
-      'abc/20/def',
-    ]);
-
+    //prove the Last button
     dashboard.last().click();
+    dashboard.confirmDealsPresent(oneDeal);
 
-    dashboard.confirmDealsPresent([
-      'abc/21/def',
-    ]);
-
+    //prove the First button
     dashboard.first().click();
-
-    dashboard.confirmDealsPresent([
-      'abc/1/def',
-      'abc/2/def',
-      'abc/3/def',
-      'abc/4/def',
-      'abc/5/def',
-      'abc/6/def',
-      'abc/7/def',
-      'abc/8/def',
-      'abc/9/def',
-      'abc/10/def',
-      'abc/11/def',
-      'abc/12/def',
-      'abc/13/def',
-      'abc/14/def',
-      'abc/15/def',
-      'abc/16/def',
-      'abc/17/def',
-      'abc/18/def',
-      'abc/19/def',
-      'abc/20/def',
-    ]);
+    dashboard.confirmDealsPresent(twentyDeals);
   });
 });
