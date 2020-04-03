72b256bdc38d5ea803aa1ad4b124e46f08111238
Dan <dan.watkinson@gmail.com>
2020-04-03 09:58:07 +0100

    re-do dashboard page tests to use data-cy tagging

--- cypress/integration/pages/dashboard.js
+++ cypress/integration/pages/dashboard.js
@@ -1,20 +1,20 @@
 const page = {
   visit: () => cy.visit('/dashboard'),
-  next: () => cy.contains('Next'),
-  previous: () => cy.contains('Previous'),
-  first: () => cy.contains('First'),
-  last: () => cy.contains('Last'),
-  totalItems: () => cy.get('#totalItems'),
+  first: () => cy.get('[data-cy="First"]'),
+  previous: () => cy.get('[data-cy="Previous"]'),
+  next: () => cy.get('[data-cy="Next"]'),
+  last: () => cy.get('[data-cy="Last"]'),
+  totalItems: () => cy.get('[data-cy="totalItems"]'),
   row: (deal) => {
     return {
-      bank: () => cy.get(`#deal_${deal._id}`).get('._bank'),
-      bankDealId: () => cy.get(`#deal_${deal._id}`).get('._bankDealId'),
-      ukefDealId: () => cy.get(`#deal_${deal._id}`).get('._ukefDealId'),
-      status: () => cy.get(`#deal_${deal._id}`).get('._status'),
-      submissionType: () => cy.get(`#deal_${deal._id}`).get('._submissionType'),
-      maker: () => cy.get(`#deal_${deal._id}`).get('._maker'),
-      checker: () => cy.get(`#deal_${deal._id}`).get('._checker'),
-      updated: () => cy.get(`#deal_${deal._id}`).get('._updated'),
+      bank: () => cy.get('[data-cy="bank"]'),
+      bankSupplyContractID: () => cy.get('[data-cy="bankSupplyContractID"]'),
+      ukefDealId: () => cy.get('[data-cy="ukefDealId"]'),
+      status: () => cy.get('[data-cy="status"]'),
+      submissionType: () => cy.get('[data-cy="submissionType"]'),
+      maker: () => cy.get('[data-cy="maker"]'),
+      checker: () => cy.get('[data-cy="checker"]'),
+      updated: () => cy.get('[data-cy="updated"]'),
     }
   },
   confirmDealsPresent: (deals) => {
