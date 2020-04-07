a2b919c0617c4a5857d2c66f81cd3c13b7216f5f
Dan <dan.watkinson@gmail.com>
2020-04-07 15:07:57 +0100

    added mocks to help test dashboard filtering

--- utils/mock-data-loader/mocks/contracts/index.js
+++ utils/mock-data-loader/mocks/contracts/index.js
@@ -1,4 +1,6 @@
 module.exports = [
-  require('./1'),
-  require('./2'),
+  require('./1-automatic-inclusion-notice'),
+  require('./2-automatic-inclusion-notice'),
+  require('./3-manual-inclusion-notice'),
+  require('./4-manual-inclusion-application'),
 ];
