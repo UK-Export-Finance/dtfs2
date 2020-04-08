91c8929cd0e625f2f37a826bf5fbb84e080dcc58
Dan <dan.watkinson@gmail.com>
2020-04-08 20:50:09 +0100

    Dashboard: when checker views dashboard it defaults to filtering status=readyForApproval

---
+++ cypress/integration/journeys/checker/dashboard.spec.js
@@ -0,0 +1,39 @@
+const {createADeal, login} = require('../../missions');
+const {deleteAllDeals, createManyDeals} = require('../../missions/deal-api');
+const {dashboard} = require('../../pages');
+const relative = require('../../relativeURL');
+
+const maker1 = {username: 'MAKER', password: 'MAKER'};
+const checker = {username: 'CHECKER', password: 'CHECKER'};
+
+// test data we want to set up + work with..
+const twentyOneDeals = require('../maker/dashboard/twentyOneDeals');
+
+context('Dashboard Deals viewed by a user with role=checker', () => {
+
+  let dealsFromMaker1 = twentyOneDeals;
+
+  beforeEach( () => {
+    // [dw] at time of writing, the portal was throwing exceptions; this stops cypress caring
+    cy.on('uncaught:exception', (err, runnable) => {
+      console.log(err.stack);
+      return false;
+    });
+  });
+
+  before( async() => {
+    // clean down anything our test-users have created
+    await deleteAllDeals(maker1);
+    // insert deals as each user
+    dealsFromMaker1 = await createManyDeals(dealsFromMaker1, { ...maker1 });
+  });
+
+  it('Dashboard defaults to showing status=readyForApproval', () => {
+    login({...checker});
+    dashboard.visit();
+
+    dashboard.filterByStatus().should('be.visible');
+    dashboard.filterByStatus().should('have.value', 'readyForApproval');
+  });
+
+});
