b4d05b1aee70760168c49824c6be8410f3ebfb44
ttbarnes <tony@tonybarnes.me>
2020-04-09 14:17:53 +0100

    match bank details input names/ids with clone deal

--- cypress/integration/pages/bankDetails.js
+++ cypress/integration/pages/bankDetails.js
@@ -1,7 +1,7 @@
 const page = {
   visit: () => cy.visit('/before-you-start/bank-deal'),
-  bankDealId: () => cy.get('[data-cy="bankDealId"]'),
-  bankDealName: () => cy.get('[data-cy="bankDealName"]'),
+  bankDealId: () => cy.get('[data-cy="bankSupplyContractID"]'),
+  bankDealName: () => cy.get('[data-cy="bankSupplyContractName"]'),
   submit: () => cy.get('button'),
   cancel: () => cy.get('[data-cy="Cancel"]'),
   bondSupportScheme: () => '//TODO',
