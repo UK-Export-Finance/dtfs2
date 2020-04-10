0dcf3ee190c4e2ab2a3aad9fcafc483d231c87d9
ttbarnes <tony@tonybarnes.me>
2020-04-10 18:53:00 +0100

    create/update/bond-details WIP

--- deal-api/src/v1/controllers/bonds.controller.js
+++ deal-api/src/v1/controllers/bonds.controller.js
@@ -1,40 +1,55 @@
 const { ObjectId } = require('mongodb');
+const assert = require('assert');
+const db = require('../../db-driver/client');
 const { findOneDeal, update: updateDeal } = require('./deal.controller');
 const { userHasAccessTo } = require('../users/checks');
 
 exports.create = async (req, res) => {
-  await findOneDeal(req.params.id, (deal) => {
-    if (!deal) {
-      res.status(404).send();
-    }
+  // await findOneDeal(req.params.id, async (deal) => {
+  //   if (!deal) {
+  //     res.status(404).send();
+  //   }
 
-    if (deal) {
-      if (!userHasAccessTo(req.user, deal)) {
-        res.status(401).send();
-      }
+  //   if (deal) {
+  //     if (!userHasAccessTo(req.user, deal)) {
+  //       res.status(401).send();
+  //     }
 
-      const updatedBondTransactions = {
-        ...deal.bondTransactions,
-        items: [
-          ...deal.bondTransactions.items,
-          {
-            _id: new ObjectId(),
-          },
-        ],
-      };
 
-      const updatedDeal = {
-        ...deal,
-        bondTransactions: updatedBondTransactions,
-      };
+  const collection = await db.getCollection('deals');
 
-      const newReq = {
-        params: req.params,
-        body: updatedDeal,
-        user: req.user,
-      };
+  const newBondObj = { _id: new ObjectId() };
 
-      updateDeal(newReq, res);
-    }
-  });
+  // TODO: allow for existing bonds in the deal.
+  const result = await collection.findOneAndUpdate(
+    { _id: ObjectId(req.params.id) },
+    { $push: { 'bondTransactions.items': { ...newBondObj } } },
+    { upsert: false },
+  );
+  return res.status(200).send(result.value);
+};
+
+// ISSUE: this is not updating items ARRAY, making it an object
+exports.updateBond = async (req, res) => {
+  const {
+    id: dealId,
+    bondId,
+  } = req.params;
+
+  const updatedBond = {
+    _id: ObjectId(bondId),
+    ...req.body,
+  };
+
+  const collection = await db.getCollection('deals');
+  const result = await collection.findOneAndUpdate(
+    {
+      _id: ObjectId(dealId),
+      'bondTransactions.items._id': ObjectId(bondId),
+    }, {
+      $set: { 'bondTransactions.items': { ...updatedBond } },
+    },
+    { upsert: false },
+  );
+  return res.status(200).send(result.value);
 };
