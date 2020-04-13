89c7ccc079f46d15ec9837922256d07b9ca8fd05
ttbarnes <tony@tonybarnes.me>
2020-04-13 09:10:17 +0100

    create/update/bond-details

--- portal/server/routes/contract/bond.js
+++ portal/server/routes/contract/bond.js
@@ -12,14 +12,9 @@ const router = express.Router();
 router.get('/contract/:_id/bond/create', async (req, res) => {
   const { _id: dealId, userToken } = requestParams(req);
 
-  const updatedDeal = await api.createBond(dealId, userToken);
+  const { _id, bondId } = await api.createBond(dealId, userToken); // eslint-disable-line no-underscore-dangle
 
-  // TODO: this is assuming the _last_ bond in the array is the created one.
-  // somehow get the created bond ID back from API.
-  const { items: bonds } = updatedDeal.bondTransactions;
-  const createdBondId = bonds[bonds.length - 1]._id; // eslint-disable-line no-underscore-dangle
-
-  return res.redirect(`/contract/${dealId}/bond/${createdBondId}/details`); // eslint-disable-line no-underscore-dangle
+  return res.redirect(`/contract/${_id}/bond/${bondId}/details`); // eslint-disable-line no-underscore-dangle
 });
 
 // TODO: if some details have been submitted
@@ -36,22 +31,14 @@ router.get('/contract/:_id/bond/:bondId/details', async (req, res) => {
 });
 
 router.post('/contract/:_id/bond/:bondId/details', async (req, res) => {
-  const { _id, bondId, userToken } = requestParams(req);
+  const { _id: dealId, bondId, userToken } = requestParams(req);
 
-  // console.log('*** bond details POST req body \n', req.body);
-
-  const apiResponse = await postToApi(
-    api.updateBond(_id, bondId, req.body, userToken),
+  await postToApi(
+    api.updateBond(dealId, bondId, req.body, userToken),
     errorHref,
   );
 
-  // if (validationErrors) {
-  //   return res.render('bond/bond-details.njk', {
-  //     validationErrors,
-  //   });
-  // }
-
-  const redirectUrl = `/contract/${req.params._id}/bond/${req.params.bondId}/financial-details`;
+  const redirectUrl = `/contract/${dealId}/bond/${bondId}/financial-details`;
   return res.redirect(redirectUrl);
 });
 
