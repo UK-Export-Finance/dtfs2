b817136902417cbde2b6d2030e71a1b0023a7ec9
ttbarnes <tony@tonybarnes.me>
2020-04-23 13:54:43 +0100

    mock bond pages

--- portal/server/routes/contract/bond.js
+++ portal/server/routes/contract/bond.js
@@ -118,6 +118,10 @@ router.post('/contract/:_id/bond/:bondId/save-go-back', async (req, res) => {
   return res.redirect(redirectUrl);
 });
 
+router.get('/contract/:_id/bond/:_bondId/issue-facility', async (req, res) => res.render('bond/bond-issue-facility.njk'));
+
+router.get('/contract/:_id/bond/:_bondId/confirm-requested-cover-start-date', async (req, res) => res.render('bond/bond-confirm-requested-cover-start-date.njk'));
+
 router.get('/contract/:_id/bond/:bondId/delete', async (req, res) => {
   const { _id, bondId, userToken } = requestParams(req);
 
