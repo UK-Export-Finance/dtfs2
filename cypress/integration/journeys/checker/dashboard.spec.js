470f10bc123a2b08f6b85dd40a06848c3d93163c
Dan <dan.watkinson@gmail.com>
2020-04-24 15:57:40 +0100

    tidying, removing un-needed steps from e2e test

--- cypress/integration/journeys/checker/dashboard.spec.js
+++ cypress/integration/journeys/checker/dashboard.spec.js
@@ -1,15 +1,9 @@
 const {dashboard} = require('../../pages');
 const relative = require('../../relativeURL');
 
-const maker1 = {username: 'MAKER', password: 'MAKER'};
 const checker = {username: 'CHECKER', password: 'CHECKER'};
 
-// test data we want to set up + work with..
-const twentyOneDeals = require('../maker/dashboard/twentyOneDeals');
-
 context('Dashboard Deals viewed by a user with role=checker', () => {
-
-  let dealsFromMaker1 = twentyOneDeals;
 
   beforeEach( () => {
     // [dw] at time of writing, the portal was throwing exceptions; this stops cypress caring
@@ -17,11 +11,6 @@ context('Dashboard Deals viewed by a user with role=checker', () => {
       console.log(err.stack);
       return false;
     });
-  });
-
-  before( () => {
-    cy.deleteDeals(maker1);
-    cy.insertManyDeals(dealsFromMaker1, { ...maker1 });
   });
 
   it('Dashboard defaults to showing status=readyForApproval', () => {
