c1aa7eaa92737e0b88bfd4a069a244e3503d88b8
Dan <dan.watkinson@gmail.com>
2020-05-07 11:44:06 +0100

    house-keeping, bumping jest version to clear minimist vulnerability warnings; removing k2reader as no longer releveant; the learnings have been taken into deal-api

--- utils/k2-reader/src/processFile.js
+++
@@ -1,7 +0,0 @@
-const readK2Xml = require('./read-k2-type-a-xml');
-const translate = require('./translate');
-
-module.exports = async (file) => {
-  const xmlContent = await readK2Xml(file);
-  return translate(xmlContent);;
-}
