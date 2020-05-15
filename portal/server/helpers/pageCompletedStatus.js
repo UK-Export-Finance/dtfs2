752872b551ad7a1da21e6e8dad0ade50a9f3c781
ttbarnes <tony@tonybarnes.me>
2020-05-15 14:34:19 +0100

    portal completedStatus unit tests

---
+++ portal/server/helpers/pageCompletedStatus.js
@@ -0,0 +1,27 @@
+import {
+  requiredFieldsArray,
+  filterErrorList,
+} from './pageFields';
+
+// TODO: should it be something like pageValidationErrorsCount / field...
+export const validationErrorsCount = (validationErrors, fields) => {
+  if (validationErrors.errorList) {
+    const allRequiredFields = requiredFieldsArray(fields);
+    const requiredFieldErrors = filterErrorList(validationErrors.errorList, allRequiredFields);
+
+    return Object.keys(requiredFieldErrors).length;
+  }
+  return 0;
+};
+
+export const isCompleted = (validationErrors, fields) => {
+  const errorsCount = validationErrorsCount(validationErrors, fields);
+
+  if (errorsCount === 0) {
+    return true;
+  }
+  return false;
+};
+
+
+export default isCompleted;
