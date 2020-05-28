00925303bafdc23de244a49ca60c3ec55e49ae10
ttbarnes <tony@tonybarnes.me>
2020-05-28 17:19:11 +0100

    DTFS2-1045 - e2e test for default bond cover start date

--- cypress/integration/journeys/maker/submit-a-deal-for-review.spec.js
+++ cypress/integration/journeys/maker/submit-a-deal-for-review.spec.js
@@ -1,5 +1,6 @@
+const moment = require('moment');
 const { contract, contractReadyForReview, defaults } = require('../../pages');
-const { errorSummary, successMessage } = require('../../partials');
+const { successMessage } = require('../../partials');
 const relative = require('../../relativeURL');
 
 const maker1 = { username: 'MAKER', password: 'MAKER' };
@@ -92,6 +93,84 @@ context('A maker selects to submit a contract for review from the view-contract 
     });
   });
 
+  describe('when a deal has Completed Eligibility and a Bond with `Issued` bond stage without a Cover Start Date', () => {
+    const date = moment().add(1, 'month');
+
+    let dealWithIssuedBondAndNoCoverStartDate = {
+      details: {
+        bankSupplyContractID: 'abc/1/def',
+        bankSupplyContractName: 'Tibettan submarine acquisition scheme',
+        submissionType: 'Automatic Inclusion Notice',
+        status: 'Draft',
+      },
+      eligibility: {
+        status: 'Completed',
+        criteria: [
+          { id: 11, answer: true },
+          { id: 12, answer: true },
+          { id: 13, answer: true },
+          { id: 14, answer: true },
+          { id: 15, answer: true },
+          { id: 16, answer: true },
+          { id: 17, answer: true },
+          { id: 18, answer: true },
+        ],
+      },
+      bondTransactions: {
+        items: [
+          {
+            _id: '1234567891',
+            bondIssuer: 'issuer',
+            bondType: 'bond type',
+            bondStage: 'Issued',
+            'coverEndDate-day': moment(date).format('DD'),
+            'coverEndDate-month': moment(date).format('MM'),
+            'coverEndDate-year': moment(date).format('YYYY'),
+            uniqueIdentificationNumber: '1234',
+            bondBeneficiary: 'test',
+            bondValue: '123',
+            transactionCurrencySameAsSupplyContractCurrency: 'true',
+            riskMarginFee: '1',
+            coveredPercentage: '2',
+            feeType: 'test',
+            feeFrequency: 'test',
+            dayCountBasis: 'test',
+          },
+        ],
+      },
+    };
+
+    beforeEach(() => {
+      cy.insertOneDeal(dealWithIssuedBondAndNoCoverStartDate, { ...maker1 })
+        .then((insertedDeal) => dealWithIssuedBondAndNoCoverStartDate = insertedDeal);
+    });
+
+    it('should use todays date for the Bond in Deal page', () => {
+      cy.login({ ...maker1 });
+      contract.visit(dealWithIssuedBondAndNoCoverStartDate);
+
+      contract.proceedToReview().should('not.be.disabled');
+      contract.proceedToReview().click();
+      cy.url().should('eq', relative(`/contract/${dealWithIssuedBondAndNoCoverStartDate._id}/ready-for-review`));
+
+      contractReadyForReview.comments().type('a mandatory comment');
+      contractReadyForReview.readyForCheckersApproval().click();
+
+      cy.url().should('include', '/dashboard');
+
+      successMessage.successMessageLink().click();
+
+      cy.url().should('eq', relative(`/contract/${dealWithIssuedBondAndNoCoverStartDate._id}`));
+
+      const bondId = dealWithIssuedBondAndNoCoverStartDate.bondTransactions.items[0]._id;
+      const row = contract.bondTransactionsTable.row(bondId);
+
+      const expectedDate = moment().format('DD/MM/YYYY')
+      row.requestedCoverStartDate().invoke('text').then((text) => {
+        expect(text.trim()).equal(expectedDate);
+      });
+    });
+  });
 
   it('The cancel button returns the user to the view-contract page.', () => {
     // log in, visit a deal, select abandon
