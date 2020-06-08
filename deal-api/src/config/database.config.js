bac18ac70479a60a1294ebb96528c8e296e990c4
Dan <dan.watkinson@gmail.com>
2020-06-08 16:06:09 +0100

    temp; logging env variables so we can diagnose some of what is happening deploying to azure..

--- deal-api/src/config/database.config.js
+++ deal-api/src/config/database.config.js
@@ -5,7 +5,8 @@ dotenv.config();
 const database = process.env.MONGO_INITDB_DATABASE;
 const connectionString = process.env.MONGODB_URI;
 
-console.log(`database.config.js providing config from environmnet variables  - ${process.env.BOB}`);
+console.log(`all environment variables that we have access to at startup: ${Object.keys(process.env)}`)
+console.log(`database.config.js providing config from environmnet variables  - BOB  == ${process.env.BOB}`);
 
 module.exports = {
   database,
