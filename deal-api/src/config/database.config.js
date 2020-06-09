8027055e067bb58063614688c47deed9170d22a6
Dan <dan.watkinson@gmail.com>
2020-06-09 09:04:33 +0100

    tidying output in deal-api once azure is deploying correctly; leaving some log output as it seems likely to be helpful in future devops work..

--- deal-api/src/config/database.config.js
+++ deal-api/src/config/database.config.js
@@ -1,12 +1,11 @@
 const dotenv = require('dotenv');
 
 dotenv.config();
+console.log(`deal-api getting database config, available environment keys: ${Object.keys(process.env)}`)
 
 const database = process.env.MONGO_INITDB_DATABASE || process.env.CUSTOMCONNSTR_MONGO_INITDB_DATABASE;
 const connectionString = process.env.MONGODB_URI || process.env.CUSTOMCONNSTR_MONGODB_URI;
 
-console.log(`all environment variables that we have access to at startup: ${Object.keys(process.env)}`)
-console.log(`database  == ${database}`);
 
 module.exports = {
   database,
