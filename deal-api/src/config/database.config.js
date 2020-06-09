d146274a2bc80ffcd7a82287b2231bbe97c2108b
ttbarnes <tony@tonybarnes.me>
2020-06-09 09:38:20 +0100

    linting

--- deal-api/src/config/database.config.js
+++ deal-api/src/config/database.config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 
 dotenv.config();
-console.log(`deal-api getting database config, available environment keys: ${Object.keys(process.env)}`)
+console.log(`deal-api getting database config, available environment keys: ${Object.keys(process.env)}`);
 
 const database = process.env.MONGO_INITDB_DATABASE || process.env.CUSTOMCONNSTR_MONGO_INITDB_DATABASE;
 const connectionString = process.env.MONGODB_URI || process.env.CUSTOMCONNSTR_MONGODB_URI;
