1d18fcd4743b06840714ebee1e9a64e2ddc8bbac
ttbarnes <tony@tonybarnes.me>
2020-06-11 15:46:31 +0100

    DTFS2-1046 - loan guarantee details - e2e tests

--- cypress/integration/pages/loanGuaranteeDetails.js
+++ cypress/integration/pages/loanGuaranteeDetails.js
@@ -1,16 +1,16 @@
 const page = {
+  // Facility stage = Conditional specifics
   facilityStageConditionalInput: () => cy.get('[data-cy="facility-stage-conditional"]'),
   facilityStageUnconditionalInput: () => cy.get('[data-cy="facility-stage-unconditional"]'),
   facilityStageErrorMessage: () => cy.get('[data-cy="facility-stage-error-message"]'),
 
+  conditionalBankReferenceNumberInput: () => cy.get('[data-cy="facilityStageConditional-bankReferenceNumber"]'),
   ukefGuaranteeInMonthsInput: () => cy.get('[data-cy="ukef-guarantee-in-months"]'),
   ukefGuaranteeInMonthsErrorMessage: () => cy.get('[data-cy="ukef-guarantee-in-months-error-message"]'),
 
-  // Facility stage = any
-  bankReferenceNumberInput: () => cy.get('[data-cy="bank-reference-number"]'),
-
   // Facility stage = Unconditional specifics
-  bankReferenceNumberErrorMessage: () => cy.get('[data-cy="bank-reference-number-error-message"]'),
+  unconditionalBankReferenceNumberInput: () => cy.get('[data-cy="facilityStageUnconditional-bankReferenceNumber"]'),
+  unconditionalBankReferenceNumberErrorMessage: () => cy.get('[data-cy="facilityStageUnconditional-bankReferenceNumber-error-message"]'),
   requestedCoverStartDateDayInput: () => cy.get('[data-cy="requestedCoverStartDate-day"]'),
   requestedCoverStartDateMonthInput: () => cy.get('[data-cy="requestedCoverStartDate-month"]'),
   requestedCoverStartDateYearInput: () => cy.get('[data-cy="requestedCoverStartDate-year"]'),
@@ -20,7 +20,6 @@ const page = {
   coverEndDateMonthInput: () => cy.get('[data-cy="coverEndDate-month"]'),
   coverEndDateYearInput: () => cy.get('[data-cy="coverEndDate-year"]'),
   coverEndDateErrorMessage: () => cy.get('[data-cy="coverEndDate-error-message"]'),
-
 
   submit: () => cy.get('[data-cy="submit-button"]'),
   saveGoBackButton: () => cy.get('[data-cy="save-go-back-button"]'),
