7a7876fb7bd9c4aea4f5a70244a8c4f1e0cd979a
ttbarnes <tony@tonybarnes.me>
2020-06-12 09:39:05 +0100

    DTFS2-1091 - only submit deal for review if all bonds completed

--- portal/server/helpers/dealFormsCompleted.js
+++ portal/server/helpers/dealFormsCompleted.js
@@ -1,8 +1,16 @@
 // TODO - perhaps each of these sections should have their own rules for this that we call on here?
 
-const hasACompletedBond = (deal) => deal.bondTransactions
-        && deal.bondTransactions.items
-        && deal.bondTransactions.items.filter((b) => b.status === 'Completed').length >= 1;
+const hasCompletedBonds = (deal) => {
+  const allBonds = deal.bondTransactions && deal.bondTransactions.items;
+  const totalBonds = allBonds.length;
+  const completed = allBonds.filter((b) => b.status === 'Completed');
+
+  if (totalBonds >= 1 && totalBonds === completed.length) {
+    return true;
+  }
+
+  return false;
+};
 
 const submissionDetailsComplete = (deal) => deal.submissionDetails && deal.submissionDetails.status === 'Completed';
 
@@ -10,7 +18,7 @@ const eligibilityComplete = (deal) => deal.eligibility && deal.eligibility.statu
 
 const dealFormsCompleted = (deal) =>
   eligibilityComplete(deal)
-  && hasACompletedBond(deal)
+  && hasCompletedBonds(deal)
   && submissionDetailsComplete(deal);
 
 export default dealFormsCompleted;
