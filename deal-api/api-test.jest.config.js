b3004c2dabf89f4528c6a3c5ef0d766517bf13a5
ttbarnes <tony@tonybarnes.me>
2020-06-18 17:02:43 +0100

    DTFS2-1088 - improve loan api tests

--- deal-api/api-test.jest.config.js
+++ deal-api/api-test.jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{js,}'],
   coverageDirectory: 'reports/coverage/api-test',
-  testMatch: ['**/*.api-test.js'],
+  testMatch: ['**/loan.api-test.js'],
 };
