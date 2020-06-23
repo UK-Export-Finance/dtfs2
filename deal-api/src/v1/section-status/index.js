a454a3bd7ea0bc7de863b08302e586529b599b94
ttbarnes <tony@tonybarnes.me>
2020-06-23 16:35:23 +0100

    DTFS2-1089 - return loan status in api get response

--- deal-api/src/v1/section-status/index.js
+++ deal-api/src/v1/section-status/index.js
@@ -1,9 +1,11 @@
 const { multipleBondStatus } = require('./bond');
+const { multipleLoanStatus } = require('./loan');
 const { aboutSupplyContractStatus } = require('./about');
 
 exports.dealSectionStatuses = (deal) => ({
   ...deal,
   bondTransactions: multipleBondStatus(deal.bondTransactions),
+  loanTransactions: multipleLoanStatus(deal.loanTransactions),
   submissionDetails: {
     ...deal.submissionDetails,
     status: aboutSupplyContractStatus(deal.submissionDetails),
