c219daadb4074d40c87ed7a2f93c8da8bd3f678d
ttbarnes <tony@tonybarnes.me>
2020-06-25 14:44:12 +0100

    DTFS2-1089 - loan dates/repayments e2e tests

--- cypress/integration/pages/loanDatesRepayments.js
+++ cypress/integration/pages/loanDatesRepayments.js
@@ -1,9 +1,11 @@
 const page = {
   premiumTypeInAdvanceInput: () => cy.get('[data-cy="premium-type-in-advance"]'),
+  premiumTypeInArrearInput: () => cy.get('[data-cy="premium-type-in-arrear"]'),
   premiumTypeAtMaturityInput: () => cy.get('[data-cy="premium-type-at-maturity"]'),
   premiumTypeInputErrorMessage: () => cy.get('[data-cy="premium-type-error-message"]'),
 
-  premiumFrequencyAnnuallyInput: () => cy.get('[data-cy="premium-frequency-annually"]'),
+  premiumFrequencyAnnuallyInput: () => cy.get('[data-cy="in-advance-premium-frequency-radios"] [data-cy="premium-frequency-annually"]'),
+  premiumFrequencyQuarterlyInput: () => cy.get('[data-cy="in-arrear-premium-frequency-radios"] [data-cy="premium-frequency-quarterly"]'),
   premiumFrequencyInputErrorMessage: () => cy.get('[data-cy="premium-frequency-error-message"]'),
 
   dayCountBasis365Input: () => cy.get('[data-cy="day-count-basis-365"]'),
