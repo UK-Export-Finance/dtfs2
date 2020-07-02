307162a6ef2c366e50bbae6e9c55d1217be68012
ttbarnes <tony@tonybarnes.me>
2020-07-02 09:14:49 +0100

    DTFS2-1225 - supply contract summary function cleanup

--- deal-api/src/v1/deal-summary/index.js
+++ deal-api/src/v1/deal-summary/index.js
@@ -3,65 +3,60 @@ const {
   roundNumber,
 } = require('../../utils/number');
 
+const calculateTotalValue = (supplyContractConversionRateToGbp, bonds, loans) => {
+  let bondCurrency = 0;
+  let loanCurrency = 0;
+
+  if (bonds.length > 0) {
+    bonds.forEach((bond) => {
+      bondCurrency += (Number(bond.facilityValue) / Number(bond.conversionRate));
+    });
+  }
+
+  if (loans.length > 0) {
+    loans.forEach((loan) => {
+      loanCurrency += (Number(loan.facilityValue) / Number(loan.conversionRate));
+    });
+  }
+
+  const dealCurrency = bondCurrency + loanCurrency;
+  const bondInGbp = (bondCurrency / supplyContractConversionRateToGbp);
+  const loanInGbp = (loanCurrency / supplyContractConversionRateToGbp);
+  const dealInGbp = (bondInGbp + loanInGbp);
+
+  const formattedNumber = (numb) => roundNumber(numb, 2).toLocaleString('en', { minimumFractionDigits: 2 });
+
+  return {
+    dealCurrency: formattedNumber(dealCurrency),
+    dealInGbp: formattedNumber(dealInGbp),
+    bondCurrency: formattedNumber(bondCurrency),
+    bondInGbp: formattedNumber(bondInGbp),
+    loanCurrency: formattedNumber(loanCurrency),
+    loanInGbp: formattedNumber(loanInGbp),
+  };
+};
+
 const calculateDealSummary = (deal) => {
+  const { supplyContractConversionRateToGBP } = deal.submissionDetails;
   const bonds = deal.bondTransactions.items;
   const loans = deal.loanTransactions.items;
 
-  const hasBonds = bonds.length > 0;
-  const hasLoans = loans.length > 0;
+  const completedBonds = bonds.filter((b) => b.status === 'Completed');
+  const completedLoans = loans.filter((l) => l.status === 'Completed');
+  const hasCompletedBonds = completedBonds.length > 0;
+  const hasCompletedLoans = completedLoans.length > 0;
 
-  const hasBondsOrLoans = (hasBonds || hasLoans);
+  const hasSupplyContractConversionRateToGBP = isNumeric(Number(supplyContractConversionRateToGBP));
+  const canCalculate = (hasSupplyContractConversionRateToGBP && (hasCompletedBonds || hasCompletedLoans));
 
-  // can only do this calculation if the deal:
-  // has supplyContractConversionRateToGBP
-  // has at least one bond or loan completed
-
-  const { supplyContractConversionRateToGBP } = deal.submissionDetails;
-
-  if (isNumeric(supplyContractConversionRateToGBP) && hasBondsOrLoans) {
-    const supplyContractConversionRateToGbp = Number(supplyContractConversionRateToGBP);
-
-    const completedBonds = bonds.filter((b) => b.status === 'Completed');
-    const completedLoans = loans.filter((l) => l.status === 'Completed');
-
-    if (completedBonds.length < 1 && completedLoans.length < 1) {
-      return null;
-    }
-
-    let bondCurrency = 0;
-    let loanCurrency = 0;
-
-    if (completedBonds.length > 0) {
-      completedBonds.forEach((bond) => {
-        bondCurrency += (Number(bond.facilityValue) / Number(bond.conversionRate));
-      });
-    }
-
-    if (completedLoans.length > 0) {
-      completedLoans.forEach((loan) => {
-        loanCurrency += (Number(loan.facilityValue) / Number(loan.conversionRate));
-      });
-    }
-
-    const dealCurrency = bondCurrency + loanCurrency;
-    const bondInGbp = (bondCurrency / supplyContractConversionRateToGbp);
-
-    const loanInGbp = (loanCurrency / supplyContractConversionRateToGbp);
-    const dealInGbp = (bondInGbp + loanInGbp);
-
-    const formattedNumber = (numb) => roundNumber(numb, 2).toLocaleString('en', { minimumFractionDigits: 2 });
-
+  if (canCalculate) {
     return {
-      totalValue: {
-        dealCurrency: formattedNumber(dealCurrency),
-        dealInGbp: formattedNumber(dealInGbp),
-        bondCurrency: formattedNumber(bondCurrency),
-        bondInGbp: formattedNumber(bondInGbp),
-        loanCurrency: formattedNumber(loanCurrency),
-        loanInGbp: formattedNumber(loanInGbp),
-      },
-      // TODO
-      // ukefExposure: {},
+      totalValue: calculateTotalValue(
+        Number(supplyContractConversionRateToGBP),
+        completedBonds,
+        completedLoans,
+      ),
+      // totalUkefExposure: calculateTotalUkefExposure,
     };
   }
   return {};
