4dc101f1702f0dbbcea3df0076a2c8ea11f711d0
ttbarnes <tony@tonybarnes.me>
2020-07-03 16:12:24 +0100

    DTFS2-1225 - contract/deal summary table stats

---
+++ portal/component-tests/contract/components/summary-table.component-test.js
@@ -0,0 +1,32 @@
+const componentRenderer = require('../../componentRenderer');
+const deal = require('../../fixtures/deal-fully-completed');
+
+const component = 'contract/components/summary-table.njk';
+const render = componentRenderer(component);
+
+describe(component, () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = render(deal);
+  });
+
+  it('should render deal currency', () => {
+    wrapper.expectText('[data-cy="deal-currency"]').toRead(deal.submissionDetails.supplyContractCurrency.id);
+  });
+
+  it('should render number of bonds', () => {
+    const expected = String(deal.bondTransactions.items.length);
+    wrapper.expectText('[data-cy="number-of-bonds"]').toRead(expected);
+  });
+
+  it('should render number of loans', () => {
+    const expected = String(deal.loanTransactions.items.length);
+    wrapper.expectText('[data-cy="number-of-loans"]').toRead(expected);
+  });
+
+  it('should render total number of transactions (bonds and loans)', () => {
+    const expected = String(deal.bondTransactions.items.length + deal.loanTransactions.items.length);
+    wrapper.expectText('[data-cy="number-of-transactions"]').toRead(expected);
+  });
+});
