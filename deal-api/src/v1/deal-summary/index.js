c3a17552dae421d5af91cec9099719abdca65ba5
ttbarnes <tony@tonybarnes.me>
2020-07-06 14:08:25 +0100

    DTFS2-1239 - contract summary table - ukefExposure poc

--- deal-api/src/v1/deal-summary/index.js
+++ deal-api/src/v1/deal-summary/index.js
@@ -2,12 +2,16 @@ const {
   isNumeric,
   roundNumber,
   formattedNumber,
+  sanitizeCurrency,
 } = require('../../utils/number');
 const { hasValue } = require('../../utils/string');
 
-const calculateTotalValue = (supplyContractConversionRateToGbp, bonds, loans) => {
+const calculateTotalValues = (supplyContractConversionRateToGbp, bonds, loans) => {
   const hasBonds = bonds.length > 0;
   const hasLoans = loans.length > 0;
+
+  // TODO: bondS<<
+  // TODO: loanS<<
   let bondInDealCurrency = 0;
   let loanInDealCurrency = 0;
 
@@ -75,6 +79,87 @@ const calculateTotalValue = (supplyContractConversionRateToGbp, bonds, loans) =>
   };
 };
 
+
+const calculateTotalUkefExposure = (supplyContractConversionRateToGbp, bonds, loans) => {
+  const hasBonds = bonds.length > 0;
+  const hasLoans = loans.length > 0;
+
+  // TODO: bondS<<
+  // TODO: loanS<<
+  let bondInDealCurrency = 0;
+  let loanInDealCurrency = 0;
+
+  if (hasBonds) {
+    bonds.forEach((bond) => {
+      const { ukefExposure, conversionRate } = bond;
+
+      // TODO: ukefExposure should be sanitising when added to DB.
+      const sanitizedUkefExposure = sanitizeCurrency(ukefExposure).sanitizedValue;
+
+      if (hasValue(conversionRate)) {
+        bondInDealCurrency += (Number(sanitizedUkefExposure) / Number(conversionRate));
+      } else {
+        bondInDealCurrency += Number(sanitizedUkefExposure);
+      }
+    });
+  }
+
+  if (hasLoans) {
+    loans.forEach((loan) => {
+      const { ukefExposure, conversionRate } = loan;
+
+      // TODO: ukefExposure should be sanitising when added to DB.
+      const sanitizedUkefExposure = sanitizeCurrency(ukefExposure).sanitizedValue;
+
+      if (hasValue(conversionRate)) {
+        loanInDealCurrency += (Number(sanitizedUkefExposure) / Number(conversionRate));
+      } else {
+        loanInDealCurrency += Number(sanitizedUkefExposure);
+      }
+    });
+  }
+
+  const dealInDealCurrency = bondInDealCurrency + loanInDealCurrency;
+
+  const bondInGbp = () => {
+    if (Number(supplyContractConversionRateToGbp) > 0) {
+      return (bondInDealCurrency / supplyContractConversionRateToGbp);
+    }
+    return bondInDealCurrency;
+  };
+
+  const loanInGbp = () => {
+    if (Number(supplyContractConversionRateToGbp) > 0) {
+      return (loanInDealCurrency / supplyContractConversionRateToGbp);
+    }
+    return loanInDealCurrency;
+  };
+
+  const dealInGbp = () => {
+    let result = 0;
+
+    if (hasBonds) {
+      result = bondInGbp();
+    }
+
+    if (hasLoans) {
+      result += loanInGbp();
+    }
+
+    return result;
+  };
+
+  return {
+    dealInDealCurrency: formattedNumber(roundNumber(dealInDealCurrency, 2)),
+    dealInGbp: formattedNumber(roundNumber(dealInGbp(), 2)),
+    bondInDealCurrency: formattedNumber(roundNumber(bondInDealCurrency, 2)),
+    bondInGbp: formattedNumber(roundNumber(bondInGbp(), 2)),
+    loanInDealCurrency: formattedNumber(roundNumber(loanInDealCurrency, 2)),
+    loanInGbp: formattedNumber(roundNumber(loanInGbp(), 2)),
+  };
+};
+
+
 const calculateDealSummary = (deal) => {
   const {
     supplyContractCurrency,
@@ -102,7 +187,12 @@ const calculateDealSummary = (deal) => {
 
   if (canCalculate) {
     return {
-      totalValue: calculateTotalValue(
+      totalValue: calculateTotalValues(
+        Number(supplyContractConversionRateToGBP),
+        completedBonds,
+        completedLoans,
+      ),
+      totalUkefExposure: calculateTotalUkefExposure(
         Number(supplyContractConversionRateToGBP),
         completedBonds,
         completedLoans,
