3edcd266bd8acbe788f3c2b5e0f71da7983b2c13
David Carboni <david@carboni.io>
2020-07-10 09:51:34 +0100

    Merged into set_jwt_keypair.sh

--- secrets/generate_jwt_keypair.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -exuo pipefail
-
-openssl genrsa -out ./private.pem 4096
-openssl rsa -in ./private.pem -outform PEM -pubout -out public.pem
-
-base64 -w 0 private.pem > private.b64
-base64 -w 0 public.pem > public.b64
