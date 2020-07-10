a78b69fca9d902f31b08fdff8b65f6dc582ca742
ttbarnes <tony@tonybarnes.me>
2020-07-10 09:20:56 +0100

    DTFS2-1232 - contract submission details component tests

--- portal/component-tests/contract/contract-submission-details.component-test.js
+++ portal/component-tests/contract/contract-submission-details.component-test.js
@@ -5,6 +5,9 @@ const page = 'contract/contract-submission-details.njk';
 
 const render = pageRenderer(page);
 
+// TODO: alternative for checking for 'at least one value'
+// wrap the render in conditional for the data. if the component is then rendered it's ok.
+
 describe(page, () => {
   let wrapper;
 
@@ -31,21 +34,50 @@ describe(page, () => {
     });
   });
 
-  // TODO: assert table is rendered
 
-  // TODO
-  // About Supply Contract
-  // edit link
-  // Supplier and counter-indemnifier/guarantor
-  // Buyer
-  // Financial information
+  it('should render contract overview table with a least one table cell value', () => {
+    wrapper.expectElement('[data-cy="contract-overview-table"]').toExist();
 
+    // no need to assert all table cells - these are covered in other component tests
+    wrapper.expectText('[data-cy="bankSupplyContractID"]').toRead(deal.details.bankSupplyContractID);
+  });
 
-  // Eligibility
-  // edit link
-  // Mandatory criteria
-  // Eligibility criteria
-  // Supporting documentation
+  describe('General deal information / About Supply Contract', () => {
+    it('should render a heading with edit link', () => {
+      wrapper.expectText('[data-cy="general-deal-info-heading"]').toRead('General deal information');
+
+      const expectedLink = `/contract/${deal._id}/about/supplier`;
+      wrapper.expectLink('[data-cy="edit-about-link"]').toLinkTo(expectedLink, 'edit');
+    });
+
+    it('should render submission details component with at at least one submitted value', () => {
+      wrapper.expectElement('[data-cy="contract-about-submission-details"]').toExist();
+
+      // no need to assert all values - these are covered in other component tests
+      wrapper.expectText('[data-cy="supplier-type"]').toRead(deal.submissionDetails['supplier-type']);
+    });
+  });
+
+  describe('Eligibility', () => {
+    it('should render a heading with edit link', () => {
+      wrapper.expectText('[data-cy="confirm-eligibility-heading"]').toRead('Confirm eligibility');
+
+      const expectedLink = `/contract/${deal._id}/eligibility/criteria`;
+      wrapper.expectLink('[data-cy="edit-eligibility-link"]').toLinkTo(expectedLink, 'edit');
+    });
+
+    it('should render Mandatory criteria component', () => {
+      wrapper.expectElement('[data-cy="mandatory-criteria-box"]').toExist();
+    });
+
+    it('should render Eligibility Criteria Answers component', () => {
+      wrapper.expectElement('[data-cy="eligibility-criteria-answers"]').toExist();
+    });
+
+    it('should render Eligibility Documentation component', () => {
+      wrapper.expectElement('[data-cy="eligibility-documentation"]').toExist();
+    });
+  });
 
   describe('Bonds', () => {
     it('should render a heading, edit link and at least one submitted value for each bond', () => {
@@ -58,8 +90,7 @@ describe(page, () => {
         const expectedLink = `/contract/${deal._id}/bond/${bond._id}/details`;
         wrapper.expectLink(editBondLinkSelector).toLinkTo(expectedLink, 'edit');
 
-        // no need to assert all submitted values
-        // these are covered in bond submission details component test
+        // no need to assert all submitted values - these are covered in other component tests
         wrapper.expectText(`${bondSelector} [data-cy="bond-issuer"]`).toRead(bond.bondIssuer);
       }
     });
@@ -76,13 +107,15 @@ describe(page, () => {
         const expectedLink = `/contract/${deal._id}/loan/${loan._id}/guarantee-details`;
         wrapper.expectLink(editLoanLinkSelector).toLinkTo(expectedLink, 'edit');
 
-        // no need to assert all submitted values
-        // these are covered in loan submission details component test
+        // no need to assert all submitted values - these are covered in other component tests
         wrapper.expectText(`${loanSelector} [data-cy="bank-reference-number"]`).toRead(loan.bankReferenceNumber);
       }
     });
   });
 
-  // back to supply contract link
+  it('should render a go back link', () => {
+    wrapper.expectLink('[data-cy="go-back-link"]').toLinkTo(`/contract/${deal._id}`, 'Back to Supply Contract page');
+  });
+
   // TODO: add go back button test to loan/bond submission tests
 });
