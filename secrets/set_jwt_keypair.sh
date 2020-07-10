8933b9bbf18ffae72b447c2daf37f498f6ddbab1
David Carboni <david@carboni.io>
2020-07-10 09:46:56 +0100

    Made the script able to run without being in the secrets directory, and merged generation into this one.

--- secrets/set_jwt_keypair.sh
+++ secrets/set_jwt_keypair.sh
@@ -1,4 +1,22 @@
 # API Keys - save file in ./secrets folder
 
-export JWT_SIGNING_KEY=$(cat private.b64)
-export JWT_VALIDATING_KEY=$(cat public.b64)
+# Directory locations
+current_dir=$PWD
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+secrets_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $secrets_dir
+pwd
+
+# Generate the key if needed
+if [ ! -f "jwt_key/private.pem" ]; then
+    mkdir -p jwt_key
+    openssl genrsa -out jwt_key/private.pem 4096
+    openssl rsa -in jwt_key/private.pem -outform PEM -pubout -out jwt_key/public.pem
+fi
+
+# The keys are stored as Base 64 to avoid any issues with line-wrapping
+export JWT_SIGNING_KEY=$(base64 -w 0 jwt_key/private.pem)
+export JWT_VALIDATING_KEY=$(base64 -w 0 jwt_key/public.pem)
+
+cd $current_dir
