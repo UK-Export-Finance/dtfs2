af21ad8d440334247f3d70f5b112255dc632f1c7
ttbarnes <tony@tonybarnes.me>
2020-07-10 09:55:23 +0100

    DTFS2-1232 - rename submission-details.njk to about-submission-details.njk

---
+++ portal/component-tests/contract/about/about-submission-details.component-test.js
@@ -0,0 +1,18 @@
+const pageRenderer = require('../../pageRenderer');
+const deal = require('../../fixtures/deal-fully-completed');
+
+const page = 'contract/about/components/about-submission-details.njk';
+
+const render = pageRenderer(page);
+
+describe(page, () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = render({ deal });
+  });
+
+  it('should...', () => {
+    
+  });
+});
