49037c6f4ff595438b1a183381c22d8fd75d1a45
Andrew Price <andrew@madebymoon.com>
2020-07-14 14:21:58 +0100

    Added workflow calculation of facility exposure period

---
+++ deal-api/api-tests/v1/integration/helpers/calculate-exposure-period.api-test.js
@@ -0,0 +1,107 @@
+const { calculateExposurePeriod } = require('../../../../src/v1/controllers/integration/helpers');
+
+describe('calculate exposure period', () => {
+  const notEndMonthFacilityDayLessThan = {
+    'requestedCoverStartDate-day': '21',
+    'requestedCoverStartDate-month': '08',
+    'requestedCoverStartDate-year': '2020',
+    'coverEndDate-day': '20',
+    'coverEndDate-month': '09',
+    'coverEndDate-year': '2021',
+  };
+
+  const notEndMonthFacilityDayGreaterThan = {
+    'requestedCoverStartDate-day': '02',
+    'requestedCoverStartDate-month': '08',
+    'requestedCoverStartDate-year': '2020',
+    'coverEndDate-day': '20',
+    'coverEndDate-month': '09',
+    'coverEndDate-year': '2021',
+  };
+
+  const notEndMonthFacilityDaySame = {
+    'requestedCoverStartDate-day': '20',
+    'requestedCoverStartDate-month': '08',
+    'requestedCoverStartDate-year': '2020',
+    'coverEndDate-day': '20',
+    'coverEndDate-month': '09',
+    'coverEndDate-year': '2021',
+  };
+
+  const commenceEndOfMonthFacility = {
+    'requestedCoverStartDate-day': '31',
+    'requestedCoverStartDate-month': '08',
+    'requestedCoverStartDate-year': '2020',
+    'coverEndDate-day': '20',
+    'coverEndDate-month': '09',
+    'coverEndDate-year': '2021',
+  };
+
+  const expireEndOfMonthFacility = {
+    'requestedCoverStartDate-day': '01',
+    'requestedCoverStartDate-month': '08',
+    'requestedCoverStartDate-year': '2020',
+    'coverEndDate-day': '30',
+    'coverEndDate-month': '09',
+    'coverEndDate-year': '2021',
+  };
+
+  const commenceAndExpireEndOfMonthFacility = {
+    'requestedCoverStartDate-day': '31',
+    'requestedCoverStartDate-month': '08',
+    'requestedCoverStartDate-year': '2020',
+    'coverEndDate-day': '30',
+    'coverEndDate-month': '09',
+    'coverEndDate-year': '2021',
+  };
+
+  const unissuedFacility = {
+    ukefGuaranteeInMonths: 9,
+  };
+
+  it('doesn\'t alter value if dates are not valid for unissued facility correctly', () => {
+    const unissued = calculateExposurePeriod(unissuedFacility, 'BSS');
+    expect(unissued).toEqual(unissuedFacility.ukefGuaranteeInMonths);
+  });
+
+
+  it('calculates BSS totals correctly', () => {
+    const notEndOfMonthDayLessThan = calculateExposurePeriod(notEndMonthFacilityDayLessThan, 'BSS');
+    expect(notEndOfMonthDayLessThan).toEqual(12);
+
+    const notEndOfMonthDaySame = calculateExposurePeriod(notEndMonthFacilityDaySame, 'BSS');
+    expect(notEndOfMonthDaySame).toEqual(13);
+
+    const notEndOfMonthDayGreaterThan = calculateExposurePeriod(notEndMonthFacilityDayGreaterThan, 'BSS');
+    expect(notEndOfMonthDayGreaterThan).toEqual(13);
+
+    const commenceEndOfMonth = calculateExposurePeriod(commenceEndOfMonthFacility, 'BSS');
+    expect(commenceEndOfMonth).toEqual(12);
+
+    const expireEndOfMonth = calculateExposurePeriod(expireEndOfMonthFacility, 'BSS');
+    expect(expireEndOfMonth).toEqual(13);
+
+    const commenceAndExpireEndOfMonth = calculateExposurePeriod(commenceAndExpireEndOfMonthFacility, 'BSS');
+    expect(commenceAndExpireEndOfMonth).toEqual(13);
+  });
+
+  it('calculates EWCS totals correctly', () => {
+    const notEndOfMonthDayLessThan = calculateExposurePeriod(notEndMonthFacilityDayLessThan, 'EWCS');
+    expect(notEndOfMonthDayLessThan).toEqual(12);
+
+    const notEndOfMonthDaySame = calculateExposurePeriod(notEndMonthFacilityDaySame, 'EWCS');
+    expect(notEndOfMonthDaySame).toEqual(12);
+
+    const notEndOfMonthDayGreaterThan = calculateExposurePeriod(notEndMonthFacilityDayGreaterThan, 'EWCS');
+    expect(notEndOfMonthDayGreaterThan).toEqual(13);
+
+    const commenceEndOfMonth = calculateExposurePeriod(commenceEndOfMonthFacility, 'EWCS');
+    expect(commenceEndOfMonth).toEqual(12);
+
+    const expireEndOfMonth = calculateExposurePeriod(expireEndOfMonthFacility, 'EWCS');
+    expect(expireEndOfMonth).toEqual(13);
+
+    const commenceAndExpireEndOfMonth = calculateExposurePeriod(commenceAndExpireEndOfMonthFacility, 'EWCS');
+    expect(commenceAndExpireEndOfMonth).toEqual(12);
+  });
+});
