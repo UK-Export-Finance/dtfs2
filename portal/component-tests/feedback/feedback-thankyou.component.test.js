ef758bc0f7949d2c0d23ab0e3dac83fdf1bc3224
ttbarnes <tony@tonybarnes.me>
2020-07-15 12:22:49 +0100

    DTFS2-1290 - feedback component tests

---
+++ portal/component-tests/feedback/feedback-thankyou.component.test.js
@@ -0,0 +1,20 @@
+const pageRenderer = require('../pageRenderer');
+
+const page = 'feedback/feedback-thankyou.njk';
+const render = pageRenderer(page);
+
+describe(page, () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = render();
+  });
+
+  it('should render page heading', () => {
+    wrapper.expectText('[data-cy="heading"]').toRead('Feedback');
+  });
+
+  it('should render thank you copy', () => {
+    wrapper.expectText('[data-cy="thankyou-copy"]').toRead('Thank you for your feedback. We will use your feedback for future improvement. Do let us know if there is anything else we need to know to improve this area of concern.');
+  });
+});
