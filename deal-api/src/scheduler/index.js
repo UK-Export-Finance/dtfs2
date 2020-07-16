8249a4bdaa11c6c1d8a4c447534c24831e2f7a25
Andrew Price <andrew@madebymoon.com>
2020-07-16 11:44:27 +0100

    Scheduler only running if env variable present

--- deal-api/src/scheduler/index.js
+++ deal-api/src/scheduler/index.js
@@ -2,11 +2,15 @@ const cron = require('node-cron');
 const jobs = require('./jobs');
 
 const initScheduler = () => {
-  Object.values(jobs).forEach((job) => {
-    const { schedule, task, message } = job.init();
-    console.log(`Added schedule: ${message}`);
-    cron.schedule(schedule, task);
-  });
+  if (process.env.DTFS_PORTAL_SCHEDULER) {
+    Object.values(jobs).forEach((job) => {
+      const { schedule, task, message } = job.init();
+      console.log(`Added schedule: ${message}`);
+      cron.schedule(schedule, task);
+    });
+  } else {
+    console.log('Portal scheduler not running');
+  }
 };
 
 module.exports = initScheduler;
