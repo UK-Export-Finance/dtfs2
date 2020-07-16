d2f678858a6d8643b45a864e10f08d282dcf6265
David Carboni <david@carboni.io>
2020-07-16 16:39:15 +0100

    Trying out Lighthouse in e2e testing.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,10 +12,21 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+// See: https://github.com/mfrachet/cypress-audit
+const { lighthouse, pa11y, prepareAudit } = require("cypress-audit");
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+  });
+
+  on("task", {
+    lighthouse: lighthouse(), // calling the function is important
+    pa11y: pa11y(), // calling the function is important
+  });
 }
