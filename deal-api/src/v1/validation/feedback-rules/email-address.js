b5b7463fc94dbbbca2e2662fef60861fe1488469
ttbarnes <tony@tonybarnes.me>
2020-07-17 09:18:17 +0100

    DTFS2-1290 - feedback email validation

--- deal-api/src/v1/validation/feedback-rules/email-address.js
+++ deal-api/src/v1/validation/feedback-rules/email-address.js
@@ -2,10 +2,21 @@ const { hasValue } = require('../../../utils/string');
 const { isNumeric } = require('../../../utils/number');
 const { orderNumber } = require('../../../utils/error-list-order-number');
 
+const isValidEmail = (email) => {
+  const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email)
+};
+
 module.exports = (feedback, errorList) => {
   const newErrorList = { ...errorList };
 
-  // TODO: 'is this an email' rule.
+  if (hasValue(feedback.emailAddress) && !isValidEmail(feedback.emailAddress)) {
+    newErrorList.emailAddress = {
+      text: 'Enter an email address in the correct format, like name@example.com',
+      order: orderNumber(newErrorList),
+    };
+  }
  
   return newErrorList;
 };
+
