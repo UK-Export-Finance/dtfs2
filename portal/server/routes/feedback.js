50853bb42024a737a0979095ae3f36f67dc4e1b1
ttbarnes <tony@tonybarnes.me>
2020-07-20 15:06:01 +0100

    DTFS2-1290 - feedback displaying logged in header

--- portal/server/routes/feedback.js
+++ portal/server/routes/feedback.js
@@ -9,15 +9,21 @@ const router = express.Router();
 
 const errorHref = (id) => `#${id}`;
 
-router.get('/feedback', (req, res) => res.render('feedback/feedback-form.njk'));
+router.get('/feedback', (req, res) =>
+  res.render('feedback/feedback-form.njk', {
+    user: req.session.user,
+  }));
 
 router.post('/feedback', async (req, res) => {
   const { userToken } = requestParams(req);
 
+
   try {
     const response = await api.createFeedback(req.body, userToken);
     if (response) {
-      return res.render('feedback/feedback-thankyou.njk');
+      return res.render('feedback/feedback-thankyou.njk', {
+        user: req.session.user,
+      });
     }
   } catch (catchErr) {
     const { data } = catchErr.response;
@@ -30,11 +36,13 @@ router.post('/feedback', async (req, res) => {
     return res.render('feedback/feedback-form.njk', {
       feedback: data.feedback,
       validationErrors,
+      user: req.session.user,
     });
   }
 
   return res.render('feedback/feedback-form.njk', {
     feedack: req.body,
+    user: req.session.user,
   });
 });
 
