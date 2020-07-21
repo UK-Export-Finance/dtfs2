b5658b6f957545b77649ad49f436db9375132018
ttbarnes <tony@tonybarnes.me>
2020-07-21 15:42:11 +0100

    DTFS2-1336 - contract `action` buttons conditions for different user roles.

--- portal/component-tests/contract/components/contract-actions/return-to-maker-button.component-test.js
+++ portal/component-tests/contract/components/contract-actions/return-to-maker-button.component-test.js
@@ -39,11 +39,10 @@ describe(component, () => {
       const user = {roles: ['checker']};
       const deals = [
         {_id: 1, details:{status:"Draft"}},
-        {_id: 2, details:{status:"Further Maker's input required"}},
-        {_id: 3, details:{status:"Abandoned Deal"}},
-        {_id: 4, details:{status:"Acknowledged by UKEF"}},
-        {_id: 5, details:{status:"Accepted by UKEF (without conditions)"}},
-        {_id: 6, details:{status:"Accepted by UKEF (with conditions)"}},
+        {_id: 2, details:{status:"Abandoned Deal"}},
+        {_id: 3, details:{status:"Acknowledged by UKEF"}},
+        {_id: 4, details:{status:"Accepted by UKEF (without conditions)"}},
+        {_id: 5, details:{status:"Accepted by UKEF (with conditions)"}},
       ];
       const userCanSubmit = true;
 
@@ -51,6 +50,23 @@ describe(component, () => {
         const wrapper = render({ user, deal, userCanSubmit});
         wrapper.expectSecondaryButton('[data-cy="ReturnToMaker"]')
           .toBeDisabled();
+      }
+    });
+  });
+
+  describe('when viewed by a user with checker AND maker roles, with userCanSubmit param set to true', () => {
+    it('should be enabled regardless of deal status', () => {
+      const user = { roles: ['maker', 'checker'] };
+      const deals = [
+        { _id: 1, details: { status: 'Submitted' } },
+        { _id: 2, details: { status: 'Rejected by UKEF' } },
+      ];
+      const userCanSubmit = true;
+
+      for (const deal of deals) {
+        const wrapper = render({ user, deal, userCanSubmit });
+        wrapper.expectSecondaryButton('[data-cy="ReturnToMaker"]')
+          .toLinkTo(`/contract/${deal._id}/return-to-maker`, 'Return to Maker');
       }
     });
   });
