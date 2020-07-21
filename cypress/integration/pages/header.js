3ae3253c8017ec1c768ba688625f68b9934e59cb
David Carboni <david@carboni.io>
2020-07-21 15:18:10 +0100

    Fixed login e2e tests.

--- cypress/integration/pages/header.js
+++ cypress/integration/pages/header.js
@@ -1,6 +1,6 @@
 const page = {
-  home: () => cy.get('[data-cy="home"]'),
-  serviceName: () => cy.get('[data-cy="service-name"]'),
+  home: () => cy.get('[data-cy="header"]').find('.govuk-header__link--homepage'),
+  serviceName: () => cy.get('[data-cy="header"]').find('.govuk-header__link--service-name'),
   dashboard: () => cy.get('[data-cy="dashboard"]'),
   manageUsers: () => cy.get('[data-cy="manageUsers"]'),
 };
