6046313049f5e4a025eb010e86bf2f4e6b6399c9
David Carboni <david@carboni.io>
2020-07-23 14:22:37 +0100

    Commented out deal-api as that reloads changed code. This now functions as a rebuild of routing (which doesn't currently reload).

--- run.sh
+++ run.sh
@@ -15,9 +15,9 @@ cd portal
 docker build --tag dtfs/portal .
 cd $home
 
-cd deal-api
-docker build --tag deal-api .
-cd $home
+#cd deal-api
+#docker build --tag deal-api .
+#cd $home
 
 docker rm -f $(docker ps -aq)
 docker-compose up
