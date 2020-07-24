9dc8ac3e29409a5604c8bb46a64c7ef435e42c56
David Carboni <david@carboni.io>
2020-07-24 12:10:10 +0100

    Debugging env vars on Azure.

--- deal-api/src/v1/users/passport.js
+++ deal-api/src/v1/users/passport.js
@@ -2,12 +2,12 @@ const dotenv = require('dotenv');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
 
+const util = require('util');
 const { findByUsername } = require('./controller');
 
-const util = require('util')
-console.log("Before config: " + util.inspect(process.env));
+console.log(`Before config: ${util.inspect(process.env)}`);
 dotenv.config();
-console.log("After config: " + util.inspect(process.env));
+console.log(`After config: ${util.inspect(process.env)}`);
 
 const PUB_KEY = Buffer.from(process.env.JWT_VALIDATING_KEY, 'base64').toString('ascii');
 
