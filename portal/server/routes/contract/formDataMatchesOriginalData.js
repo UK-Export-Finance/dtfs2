c0b1ff15858dad090be88ccc3ca18cc0b53880d1
ttbarnes <tony@tonybarnes.me>
2020-07-26 14:47:12 +0100

    DTFS2-1336 - portal submitted/api data checks for about - financial details

--- portal/server/routes/contract/formDataMatchesOriginalData.js
+++ portal/server/routes/contract/formDataMatchesOriginalData.js
@@ -46,6 +46,9 @@ const formDataMatchesOriginalData = (formData, originalData) => {
   const originalDataEmptyValues = getFieldsWithEmptyValues(strippedOriginalData);
   const cleanFormData = stripEmptyValuesFromObject(formData, originalDataEmptyValues);
 
+  console.log('----------- strippedOriginalData \n', strippedOriginalData);
+  console.log('----------- cleanFormData \n', cleanFormData);
+
   if (isEqual(strippedOriginalData, cleanFormData)) {
     return true;
   }
