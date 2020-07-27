b56326c7dd8680003f10136339712c0d2bff313d
David Carboni <david@carboni.io>
2020-07-27 20:45:32 +0100

    Facility status fro audit transactions report.

---
+++ portal/server/constants/facility-status.js
@@ -0,0 +1,8 @@
+const FACILITY_STATUS = {
+  issued: 'Issued',
+  unissued: 'Unissued',
+  conditional: 'Conditional',
+  unconditional: 'Unconditional',
+};
+
+export default FACILITY_STATUS;
--- portal/server/constants/index.js
+++ portal/server/constants/index.js
@@ -1,10 +1,12 @@
 import STATUS from './status';
+import FACILITY_STATUS from './facility-status';
 import SUBMISSION_TYPE from './submission-type';
 import TRANSACTION_STAGE from './transaction-stage';
 import TRANSACTION_TYPE from './transaction-type';
 
 export default {
   STATUS,
+  FACILITY_STATUS,
   SUBMISSION_TYPE,
   TRANSACTION_STAGE,
   TRANSACTION_TYPE,
