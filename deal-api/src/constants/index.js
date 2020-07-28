fbfb665ffb99fd7dca1fc5fc586dc76e6909c51d
Dan <dan.watkinson@gmail.com>
2020-07-28 13:48:19 +0100

    DTFS2-1386 - now with added notification email of unblockedness

--- deal-api/src/constants/index.js
+++ deal-api/src/constants/index.js
@@ -1,9 +1,11 @@
 const FACILITIES = require('./facilities');
 const DEAL = require('./deal');
 const LOGINRESULTS = require('./login-results');
+const USER = require('./user');
 
 module.exports = {
   FACILITIES,
   DEAL,
   LOGINRESULTS,
+  USER,
 };
