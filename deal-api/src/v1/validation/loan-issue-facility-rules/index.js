2a35dfd01f5b456922b0049c3cc747dc8d588974
ttbarnes <tony@tonybarnes.me>
2020-07-29 20:41:08 +0100

    DTFS2-1693 - move loan validation rules into generic loan-rules directory

--- deal-api/src/v1/validation/loan-issue-facility-rules/index.js
+++ deal-api/src/v1/validation/loan-issue-facility-rules/index.js
@@ -1,17 +1,9 @@
 const issuedDateRules = require('../fields/issued-date');
-// const requestedCoverStartDate = require('../fields/requested-cover-start-date');
 const requestedCoverStartDateRules = require('./requested-cover-start-date');
 const coverEndDateRules = require('../fields/cover-end-date');
-const disbursementAmountRules = require('../loan-rules/facility-stage-unconditional/disbursement-amount');
+const disbursementAmountRules = require('../loan-rules/disbursement-amount');
 const bankReferenceNumberRules = require('../loan-rules/bank-reference-number');
 const coverDatesRules = require('../fields/cover-dates');
-
-// TODO move bankReferenceNumber to fields directory
-// TODO move disbursementAmount to fields directory
-
-// TODO figure out conditional/unconditional things
-// ^ isn't in this case, disbursementAmount for a conditional loan?
-// so why is it in unconditional rules directory?
 
 module.exports = (submittedValues, dealSubmissionDate, issuedDate, requestedCoverStartDate) => {
   let errorList = {};
