870639ab4d3fb513cee948a9e211d7179d3d91c2
ttbarnes <tony@tonybarnes.me>
2020-07-31 08:19:15 +0100

    DTFS2-1693 - rename loan.facilityIssued to issueFacilityDetailsProvided

--- portal/server/routes/contract/dealHasIssuedFacilities.test.js
+++ portal/server/routes/contract/dealHasIssuedFacilities.test.js
@@ -1,11 +1,11 @@
 import dealHasIssuedFacilities from './dealHasIssuedFacilities';
 
 describe('dealHasIssuedFacilities', () => {
-  describe('when any single bond has bond.facilityIssued as true', () => {
+  describe('when any single bond has bond.issueFacilityDetailsProvided as true', () => {
     it('should return true', () => {
       const mockDeal = {
         bondTransactions: {
-          items: [{ facilityIssued: true }],
+          items: [{ issueFacilityDetailsProvided: true }],
         },
         loanTransactions: {
           items: [],
@@ -15,21 +15,21 @@ describe('dealHasIssuedFacilities', () => {
     });
   });
 
-  describe('when any single loan has loan.facilityIssued as true', () => {
+  describe('when any single loan has loan.issueFacilityDetailsProvided as true', () => {
     it('should return true', () => {
       const mockDeal = {
         bondTransactions: {
           items: [],
         },
         loanTransactions: {
-          items: [{ facilityIssued: true }],
+          items: [{ issueFacilityDetailsProvided: true }],
         },
       };
       expect(dealHasIssuedFacilities(mockDeal)).toEqual(true);
     });
   });
 
-  describe('when no bond or loan has facility.facilityIssued as true', () => {
+  describe('when no bond or loan has facility.issueFacilityDetailsProvided as true', () => {
     it('should return false', () => {
       const mockDeal = {
         bondTransactions: {
