4ec14700f31efc41f5723a0718b4b4927b5d98dc
ttbarnes <tony@tonybarnes.me>
2020-08-02 13:52:41 +0100

    DTFS2-1691 - bond issuance validation

--- deal-api/src/v1/validation/bond-issue-facility-rules/index.js
+++ deal-api/src/v1/validation/bond-issue-facility-rules/index.js
@@ -1,9 +1,8 @@
 const issuedDateRules = require('../fields/issued-date');
 const requestedCoverStartDateRules = require('../fields/issue-facility/requested-cover-start-date');
 const coverEndDateRules = require('../fields/cover-end-date');
-const disbursementAmountRules = require('../loan-rules/disbursement-amount');
-const bankReferenceNumberRules = require('./bank-reference-number');
 const coverDatesRules = require('../fields/cover-dates');
+const uniqueIdentificationNumberRules = require('../bond-rules/bond-stage-issued-rules/unique-identification-number');
 
 module.exports = (submittedValues, dealSubmissionDate) => {
   let errorList = {};
@@ -12,8 +11,7 @@ module.exports = (submittedValues, dealSubmissionDate) => {
   errorList = requestedCoverStartDateRules(submittedValues, errorList, dealSubmissionDate);
   errorList = coverEndDateRules(submittedValues, errorList);
   errorList = coverDatesRules(submittedValues, errorList);
-  errorList = disbursementAmountRules(submittedValues, errorList);
-  errorList = bankReferenceNumberRules(submittedValues, errorList);
+  errorList = uniqueIdentificationNumberRules(submittedValues, errorList);
 
   return errorList;
 };
