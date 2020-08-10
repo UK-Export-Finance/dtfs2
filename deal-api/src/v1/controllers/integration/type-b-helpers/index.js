2bcfcc870bd1f7a63acd8fdecb36691966397f26
ttbarnes <tony@tonybarnes.me>
2020-08-10 15:04:50 +0100

    DTFS2-1848 - update facility statuses from type-b

--- deal-api/src/v1/controllers/integration/type-b-helpers/index.js
+++ deal-api/src/v1/controllers/integration/type-b-helpers/index.js
@@ -1,7 +1,10 @@
 const generateStatus = require('./generate-status');
 const updateComments = require('./update-comments');
+const { updateBonds, updateLoans } = require('./update-facilities');
 
 module.exports = {
   generateStatus,
   updateComments,
+  updateBonds,
+  updateLoans,
 };
