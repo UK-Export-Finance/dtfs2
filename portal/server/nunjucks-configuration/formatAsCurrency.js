ebf685de3a17b5edb59d3ca32db9a3a02931efbc
Martin Coulthurst <mc@lightness.co.uk>
2020-08-12 16:33:13 +0100

    Add nunjucks format for currency

---
+++ portal/server/nunjucks-configuration/formatAsCurrency.js
@@ -0,0 +1,12 @@
+
+const formatAsCurrency = (str) => {
+  const isEmpty = !(str && str.trim().length > 0);
+  let amount = 0;
+  if (!isEmpty) {
+    amount = parseInt(str, 10);
+    amount = amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
+  }
+  return isEmpty ? '-' : amount;
+};
+
+export default formatAsCurrency;
