9a3781124e2f7e1ff816578ecbd539730d4bd959
ttbarnes <tony@tonybarnes.me>
2020-08-13 17:26:27 +0100

    DTFS2-1691 - e2e test for issued facilities defaulting to requestedCoverStartDate

--- cypress/integration/journeys/maker/submit-issued-facilities-for-review/fillAndSubmitIssueBondFacilityForm.js
+++ cypress/integration/journeys/maker/submit-issued-facilities-for-review/fillAndSubmitIssueBondFacilityForm.js
@@ -1,11 +1,27 @@
 const moment = require('moment');
 const pages = require('../../../pages');
 
+export const ISSUED_BOND_DATE_VALUE = moment().add(1, 'day');
+
+export const fillAndSubmitIssueBondFacilityFormWithoutRequestedCoverStartDate = () => {
+  pages.bondIssueFacility.issuedDateDayInput().type(ISSUED_BOND_DATE_VALUE.format('DD'));
+  pages.bondIssueFacility.issuedDateMonthInput().type(ISSUED_BOND_DATE_VALUE.format('MM'));
+  pages.bondIssueFacility.issuedDateYearInput().type(ISSUED_BOND_DATE_VALUE.format('YYYY'));
+
+  const coverEndDate = moment().add(1, 'month');
+  pages.bondIssueFacility.coverEndDateDayInput().type(coverEndDate.format('DD'));
+  pages.bondIssueFacility.coverEndDateMonthInput().type(coverEndDate.format('MM'));
+  pages.bondIssueFacility.coverEndDateYearInput().type(coverEndDate.format('YYYY'));
+
+  pages.bondIssueFacility.uniqueIdentificationNumber().type('1234');
+
+  pages.bondIssueFacility.submit().click();
+};
+
 const fillAndSubmitIssueBondFacilityForm = () => {
-  const issuedDate = moment().add(1, 'day');
-  pages.bondIssueFacility.issuedDateDayInput().type(issuedDate.format('DD'));
-  pages.bondIssueFacility.issuedDateMonthInput().type(issuedDate.format('MM'));
-  pages.bondIssueFacility.issuedDateYearInput().type(issuedDate.format('YYYY'));
+  pages.bondIssueFacility.issuedDateDayInput().type(ISSUED_BOND_DATE_VALUE.format('DD'));
+  pages.bondIssueFacility.issuedDateMonthInput().type(ISSUED_BOND_DATE_VALUE.format('MM'));
+  pages.bondIssueFacility.issuedDateYearInput().type(ISSUED_BOND_DATE_VALUE.format('YYYY'));
 
   const requestedCoverStartDate = moment().add(2, 'day');
   pages.bondIssueFacility.requestedCoverStartDateDayInput().type(requestedCoverStartDate.format('DD'));
