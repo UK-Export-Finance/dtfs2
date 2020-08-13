236b6c81a9c5f6214c80c2a7fb79420c28c90078
Andrew Price <andrew@madebymoon.com>
2020-08-13 18:05:52 +0100

    dtfs2-1139 calculated facility issued_date and cover end date for typeA xml

--- deal-api/api-tests/v1/deals/deals-status.api-test.js
+++ deal-api/api-tests/v1/deals/deals-status.api-test.js
@@ -626,50 +626,6 @@ describe('/v1/deals/:id/status', () => {
 
           issuedLoansThatShouldBeUpdated.forEach((loan) => {
             expect(loan.status).toEqual('Ready for check');
-            expect(loan.facilityStage).toEqual('Unconditional');
-            expect(loan.previousFacilityStage).toEqual('Conditional');
-          });
-        });
-      });
-
-      describe('any issued bonds that have details provided, but not yet been submitted', () => {
-        it('defaults requestedCoverStartDate to the issuedDate if no requestedCoverStartDate', async () => {
-          expect(updatedDeal.status).toEqual(200);
-          expect(updatedDeal.body).toBeDefined();
-
-          const { body } = await as(aSuperuser).get(`/v1/deals/${createdDeal._id}`);
-
-          const issuedBondsThatShouldBeUpdated = createdDeal.bondTransactions.items.filter((b) =>
-            isUnsubmittedIssuedFacility(b) && !b.requestedCoverStartDate);
-
-          // make sure we have some bonds to test against
-          expect(issuedBondsThatShouldBeUpdated.length > 0).toEqual(true);
-
-          issuedBondsThatShouldBeUpdated.forEach((bond) => {
-            const updatedBond = body.deal.bondTransactions.items.find((b) => b._id === bond._id);
-            expect(updatedBond.requestedCoverStartDate).toEqual(bond.issuedDate);
-          });
-        });
-      });
-
-      describe('any issued loans that have details provided, but not yet been submitted', () => {
-        it('defaults requestedCoverStartDate to the issuedDate if no requestedCoverStartDate', async () => {
-          expect(updatedDeal.status).toEqual(200);
-          expect(updatedDeal.body).toBeDefined();
-
-          const { body } = await as(aSuperuser).get(`/v1/deals/${createdDeal._id}`);
-
-          const issuedLoansThatShouldBeUpdated = createdDeal.loanTransactions.items.filter((l) =>
-            isUnsubmittedIssuedFacility(l)
-            && !l.requestedCoverStartDate
-          );
-
-          // make sure we have some loans to test against
-          expect(issuedLoansThatShouldBeUpdated.length > 0).toEqual(true);
-
-          issuedLoansThatShouldBeUpdated.forEach((loan) => {
-            const updatedLoan = body.deal.loanTransactions.items.find((l) => l._id === loan._id);
-            expect(updatedLoan.requestedCoverStartDate).toEqual(loan.issuedDate);
           });
         });
       });
@@ -759,6 +715,46 @@ describe('/v1/deals/:id/status', () => {
         return null;
       };
 
+      describe('any issued loans (facilityStage=`Conditional`, `issueFacilityDetailsProvided`) that have not yet been submitted', () => {
+        it('should add `Submitted` status and `issueFacilityDetailsSubmitted` property', async () => {
+          expect(updatedDeal.status).toEqual(200);
+          expect(updatedDeal.body).toBeDefined();
+
+          const { body } = await as(aSuperuser).get(`/v1/deals/${createdDeal._id}`);
+
+          const issuedLoansThatShouldBeUpdated = createdDeal.loanTransactions.items.filter((l) =>
+            isUnsubmittedIssuedFacility(l));
+
+          // make sure we have some loans to test against
+          expect(issuedLoansThatShouldBeUpdated.length > 0).toEqual(true);
+
+          issuedLoansThatShouldBeUpdated.forEach((loan) => {
+            const updatedLoan = body.deal.loanTransactions.items.find((l) => l._id === loan._id);
+            expect(updatedLoan.issueFacilityDetailsSubmitted).toEqual(true);
+            expect(updatedLoan.status).toEqual('Submitted');
+          });
+        });
+
+        it('defaults requestedCoverStartDate to the issuedDate if no requestedCoverStartDate', async () => {
+          expect(updatedDeal.status).toEqual(200);
+          expect(updatedDeal.body).toBeDefined();
+
+          const { body } = await as(aSuperuser).get(`/v1/deals/${createdDeal._id}`);
+
+          const issuedLoansThatShouldBeUpdated = createdDeal.loanTransactions.items.filter((l) =>
+            isUnsubmittedIssuedFacility(l)
+            && !l.requestedCoverStartDate);
+
+          // make sure we have some loans to test against
+          expect(issuedLoansThatShouldBeUpdated.length > 0).toEqual(true);
+
+          issuedLoansThatShouldBeUpdated.forEach((loan) => {
+            const updatedLoan = body.deal.loanTransactions.items.find((l) => l._id === loan._id);
+            expect(updatedLoan.requestedCoverStartDate).toEqual(loan.issuedDate);
+          });
+        });
+      });
+
       describe('any bonds loans that have details provided, but not yet been submitted', () => {
         it('should add `Submitted` status, `issueFacilityDetailsSubmitted` property', async () => {
           expect(updatedDeal.status).toEqual(200);
@@ -767,8 +763,7 @@ describe('/v1/deals/:id/status', () => {
           const { body } = await as(aSuperuser).get(`/v1/deals/${createdDeal._id}`);
 
           const issuedBondsThatShouldBeUpdated = createdDeal.bondTransactions.items.filter((b) =>
-            isUnsubmittedIssuedFacilityWithFacilityStageChange(b)
-          );
+            isUnsubmittedIssuedFacility(b));
 
           // make sure we have some bonds to test against
           expect(issuedBondsThatShouldBeUpdated.length > 0).toEqual(true);
@@ -777,6 +772,25 @@ describe('/v1/deals/:id/status', () => {
             const updatedBond = body.deal.bondTransactions.items.find((b) => b._id === bond._id);
             expect(updatedBond.issueFacilityDetailsSubmitted).toEqual(true);
             expect(updatedBond.status).toEqual('Submitted');
+          });
+        });
+
+        it('defaults requestedCoverStartDate to the issuedDate if no requestedCoverStartDate', async () => {
+          expect(updatedDeal.status).toEqual(200);
+          expect(updatedDeal.body).toBeDefined();
+
+          const { body } = await as(aSuperuser).get(`/v1/deals/${createdDeal._id}`);
+
+          const issuedBondsThatShouldBeUpdated = createdDeal.bondTransactions.items.filter((b) =>
+            isUnsubmittedIssuedFacility(b)
+            && !b.requestedCoverStartDate);
+
+          // make sure we have some bonds to test against
+          expect(issuedBondsThatShouldBeUpdated.length > 0).toEqual(true);
+
+          issuedBondsThatShouldBeUpdated.forEach((bond) => {
+            const updatedBond = body.deal.bondTransactions.items.find((b) => b._id === bond._id);
+            expect(updatedBond.requestedCoverStartDate).toEqual(bond.issuedDate);
           });
         });
       });
