068fff3b10ef36759e9ac9bcf4b5a989660c384d
ttbarnes <tony@tonybarnes.me>
2020-08-17 10:50:37 +0100

    DTFS2-1847 - display success message when deleting facilities

--- cypress/integration/journeys/maker/loan/delete-a-loan.spec.js
+++ cypress/integration/journeys/maker/loan/delete-a-loan.spec.js
@@ -1,4 +1,5 @@
 const pages = require('../../../pages');
+const partials = require('../../../partials');
 const relative = require('../../../relativeURL');
 
 const mockUsers = require('../../../../fixtures/mockUsers');
@@ -42,7 +43,7 @@ context('Delete a Loan', () => {
       });
   });
 
-  it('Deleting a loan via the Deal page should remove the loan and redirect back to the Deal page', () => {
+  it('Deleting a loan via the Deal page should remove the loan and redirect back to the Deal page with a success message', () => {
     cy.login({ ...MAKER_LOGIN });
     pages.contract.visit(deal);
 
@@ -57,6 +58,10 @@ context('Delete a Loan', () => {
     pages.loanDelete.submit().click();
 
     cy.url().should('eq', relative(`/contract/${dealId}`));
+
+    partials.successMessage.successMessageListItem().invoke('text').then((text) => {
+      expect(text.trim()).to.equal(`Loan #${loanToDeleteId} has been deleted`);
+    });
 
     pages.contract.loansTransactionsTableRows().should('have.length', 2);
     pages.contract.loansTransactionsTable.row(loanToDeleteId).row.should('not.exist');
