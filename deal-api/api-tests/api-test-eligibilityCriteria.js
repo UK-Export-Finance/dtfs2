1984f919560496efacacb3378960c2c3756419d4
Dan <dan.watkinson@gmail.com>
2020-08-17 18:43:58 +0100

    DTFS2-1389 - eligibility criteria fully wired in to use mongo collection instead of static default values

---
+++ deal-api/api-tests/api-test-eligibilityCriteria.js
@@ -0,0 +1,28 @@
+const api = require('./api');
+
+const ECs = require('./fixtures/eligibilityCriteria');
+
+let notYetInitialised = true;
+
+module.exports.initialise = async (app, user) => {
+  if (notYetInitialised) {
+    const {
+      get, post, put, remove,
+    } = api(app).as(user);
+
+    const currentECResponse = await get('/v1/eligibility-criteria');
+    const existingECs = currentECResponse.body.eligibilityCriteria;
+
+    for (const existingEC of existingECs) {
+      await remove(`/v1/users/${existingEC._id}`);
+    }
+
+    for (const ec of ECs) {
+      await post(ec).to('/v1/eligibility-criteria/');
+    }
+
+    notYetInitialised = false;
+  }
+
+  return;
+};
