5a5cef52a03d418a89befcd29fdc481ca18367b3
ttbarnes <tony@tonybarnes.me>
2020-08-17 14:09:51 +0100

    DTFS2-1848 - fix validation/dynamic status issue with issued facilities. Fixes `Completed` status not appearing when issue facility form is completed but deal not submitted

--- deal-api/src/v1/section-status/bonds.js
+++ deal-api/src/v1/section-status/bonds.js
@@ -44,7 +44,7 @@ const bondHasIncompleteIssueFacilityDetails = (dealStatus, previousDealStatus, d
   return false;
 };
 
-const addAccurateStatusesToBonds = (dealStatus, previousDealStatus, dealSubmissionType, bondTransactions) => {
+const addAccurateStatusesToBonds = (dealStatus, previousDealStatus, dealSubmissionType, dealSubmissionDate, bondTransactions) => {
   if (bondTransactions.items.length) {
     bondTransactions.items.forEach((b) => {
       const bond = b;
@@ -52,7 +52,7 @@ const addAccurateStatusesToBonds = (dealStatus, previousDealStatus, dealSubmissi
       let issueFacilityValidationErrors;
 
       if (bondHasIncompleteIssueFacilityDetails(dealStatus, previousDealStatus, dealSubmissionType, bond)) {
-        issueFacilityValidationErrors = bondIssueFacilityValidationErrors(bond);
+        issueFacilityValidationErrors = bondIssueFacilityValidationErrors(bond, dealSubmissionDate);
       }
 
       bond.status = bondStatus(bond, validationErrors, issueFacilityValidationErrors);
