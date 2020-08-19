5b640f4e6f6cde6c08911464f475d40099a5557d
Dan <dan.watkinson@gmail.com>
2020-08-19 09:40:20 +0100

    DTFS2-1972 - clone should copy existing ECs from source deal, not repopulate the ECs from the latest EC-questions in the system..

--- deal-api/src/v1/controllers/deal-clone.controller.js
+++ deal-api/src/v1/controllers/deal-clone.controller.js
@@ -3,7 +3,6 @@ const { findMandatoryCriteria } = require('./mandatoryCriteria.controller');
 const { findOneDeal, create: createDeal } = require('./deal.controller');
 const { generateDealId } = require('../../utils/generateIds');
 const { getCloneDealErrors } = require('../validation/clone-deal');
-const { findEligibilityCriteria } = require('./eligibilityCriteria.controller');
 
 const now = require('../../now');
 
@@ -63,8 +62,6 @@ const getCurrentMandatoryCriteria = () => new Promise((resolve) => {
 
 exports.clone = async (req, res) => {
   await findOneDeal(req.params.id, async (existingDeal) => {
-    const eligibilityCriteria = await findEligibilityCriteria();
-
     if (!existingDeal) {
       return res.status(404).send();
     }
@@ -91,10 +88,6 @@ exports.clone = async (req, res) => {
       },
       editedBy: [],
       mandatoryCriteria: await getCurrentMandatoryCriteria(),
-      eligibility: {
-        status: 'Incomplete',
-        criteria: eligibilityCriteria,
-      },
     };
 
     if (cloneTransactions === 'false') {
