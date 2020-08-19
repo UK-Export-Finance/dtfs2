2c56a9239483c2acc5c47ae0463b1189469dcf12
ttbarnes <tony@tonybarnes.me>
2020-08-19 13:19:22 +0100

    DTFS2-1848 - e2e test for unissued/conditional loans status changing to `not started` when checker submits MIA deal

--- cypress/integration/journeys/checker/issued-specs-new/MIN-submitted-deal-receiving-type-b-acknowledged-updates-unissued-facility-statuses/MIN-submitted-deal-receiving-type-b-acknowledged-updates-unissued-facility-statuses.spec.js
+++ cypress/integration/journeys/checker/issued-specs-new/MIN-submitted-deal-receiving-type-b-acknowledged-updates-unissued-facility-statuses/MIN-submitted-deal-receiving-type-b-acknowledged-updates-unissued-facility-statuses.spec.js
@@ -5,16 +5,8 @@ const mockUsers = require('../../../../../fixtures/mockUsers');
 const CHECKER_LOGIN = mockUsers.find(user => (user.roles.includes('checker') && user.bank.name === 'Barclays Bank'));
 const MAKER_LOGIN = mockUsers.find(user => (user.roles.includes('maker') && user.bank.name === 'Barclays Bank'));
 
-// TODO should be:
-// submit MIA deal
-// maker sends MIA deal to checker
-// checker submits MIA deal again
-// submitted deal becomes MIN
-// THEN facility statuses are updated.
-
-context('Checker submits an MIN deal with `Unissued` bonds and `Unconditional` loans; workflow responds', () => {
+context('Checker submits an MIA deal with `Unissued` bonds and `Conditional` loans; workflow responds, becomes an MIN deal, maker re-submits, checker re-submits, workflow responds', () => {
   let deal;
-  let dealId;
 
   beforeEach(() => {
     cy.on('uncaught:exception', (err, runnable) => {
@@ -28,14 +20,20 @@ context('Checker submits an MIN deal with `Unissued` bonds and `Unconditional` l
     cy.insertOneDeal(MIADealWithUnissuedFacilities, { ...MAKER_LOGIN })
       .then((insertedDeal) => {
         deal = insertedDeal;
-        dealId = deal._id; // eslint-disable-line no-underscore-dangle
       });
   });
 
-  describe('Checker submits an MIA deal with `Unissued` bonds and `Unconditional` loans; maker sends MIA deal to checker, checker submits deal again, deal becomes MIN; workflow responds', () => {
-    it('Updates the statuses of `Unissued` bonds and `Unconditional` loans from `Completed` to `Not started`', () => {
+  describe('Checker re-submits the MIN deal with `Unissued` bonds and `Conditional` loans; workflow responds', () => {
+    it('Updates the statuses of `Unissued` bonds and `Conditional` loans from `Completed` to `Not started`', () => {
+      //---------------------------------------------------------------
+      // checker submits deal to UKEF
+      //---------------------------------------------------------------
       cy.login({ ...CHECKER_LOGIN });
       pages.contract.visit(deal);
+
+      pages.contract.eligibilitySubmissionType().invoke('text').then((text) => {
+        expect(text.trim()).to.contain('Manual Inclusion Application');
+      });
 
       const bondId = deal.bondTransactions.items[0]._id; // eslint-disable-line no-underscore-dangle
       const bondRow = pages.contract.bondTransactionsTable.row(bondId);
@@ -53,7 +51,7 @@ context('Checker submits an MIN deal with `Unissued` bonds and `Unconditional` l
       });
 
       loanRow.facilityStage().invoke('text').then((text) => {
-        expect(text.trim()).to.equal('Unconditional');
+        expect(text.trim()).to.equal('Conditional');
       });
 
       loanRow.loanStatus().invoke('text').then((text) => {
@@ -65,12 +63,46 @@ context('Checker submits an MIN deal with `Unissued` bonds and `Unconditional` l
       pages.contractConfirmSubmission.confirmSubmit().check();
       pages.contractConfirmSubmission.acceptAndSubmit().click();
 
+      //---------------------------------------------------------------
+      // receive typeB XML with `Submission Acknowledged` deal status
+      //---------------------------------------------------------------
+      cy.sendTypeB({
+        header: {
+          portal_deal_id: deal._id,
+          bank_deal_id: deal.details.bankSupplyContractID,
+          Message_Type: 'B',
+          Action_Code: '006',
+        },
+        deal: {
+          UKEF_deal_id: deal._id,
+          Deal_status: 'submission_acknowledged',
+        },
+        bonds: [
+          {
+            BSS_portal_facility_id: deal.bondTransactions.items[0]._id,
+            BSS_ukef_facility_id: '12345',
+            BSS_status: '""',
+          },
+        ],
+        loans: [
+          {
+            EWCS_portal_facility_id: deal.loanTransactions.items[0]._id,
+            EWCS_ukef_facility_id: '56789',
+            EWCS_status: '""',
+          },
+        ],
+      });
+
+      //---------------------------------------------------------------
+      // receive typeB XML with `Approved` deal status
+      //---------------------------------------------------------------
       cy.sendTypeB({
         header: {
           portal_deal_id: deal._id,
           bank_deal_id: deal.details.bankSupplyContractID,
           Message_Type: 'B',
           Action_Code: '016',
+          // Action_Code: '006',
         },
         deal: {
           UKEF_deal_id: deal._id,
@@ -92,8 +124,19 @@ context('Checker submits an MIN deal with `Unissued` bonds and `Unconditional` l
         ],
       });
 
+
+      //---------------------------------------------------------------
+      // maker checks the deal and re-submits the deal to checker
+      //---------------------------------------------------------------
+      cy.login({ ...MAKER_LOGIN });
       pages.contract.visit(deal);
-      // statuses should remain beause the deal is still MIA, not MIN.
+
+      // submission type should not be changed
+      pages.contract.eligibilitySubmissionType().invoke('text').then((text) => {
+        expect(text.trim()).to.contain('Manual Inclusion Application');
+      });
+
+      // facility statuses should not be changed
       bondRow.bondStatus().invoke('text').then((text) => {
         expect(text.trim()).to.equal('Completed');
       });
@@ -102,15 +145,87 @@ context('Checker submits an MIN deal with `Unissued` bonds and `Unconditional` l
         expect(text.trim()).to.equal('Completed');
       });
 
+      pages.contract.proceedToReview().click();
+      pages.contractReadyForReview.comments().type('LGTM');
+      pages.contractReadyForReview.readyForCheckersApproval().click();
 
 
-      // bondRow.bondStatus().invoke('text').then((text) => {
-      //   expect(text.trim()).to.equal('Not started');
-      // });
+      //---------------------------------------------------------------
+      // checker reviews the deal and re-submits the deal to UKEF
+      //---------------------------------------------------------------
+      cy.login({ ...CHECKER_LOGIN });
+      pages.contract.visit(deal);
 
-      // loanRow.loanStatus().invoke('text').then((text) => {
-      //   expect(text.trim()).to.equal('Not started');
-      // });
+      // submission type should not be changed
+      pages.contract.eligibilitySubmissionType().invoke('text').then((text) => {
+        expect(text.trim()).to.contain('Manual Inclusion Application');
+      });
+
+      // facility statuses should not be changed
+      bondRow.bondStatus().invoke('text').then((text) => {
+        expect(text.trim()).to.equal('Completed');
+      });
+
+      loanRow.loanStatus().invoke('text').then((text) => {
+        expect(text.trim()).to.equal('Completed');
+      });
+
+      pages.contract.proceedToSubmit().click();
+      pages.contractConfirmSubmission.confirmSubmit().check();
+      pages.contractConfirmSubmission.acceptAndSubmit().click();
+
+      //---------------------------------------------------------------
+      // receive typeB XML with `Submission Acknowledged` deal status
+      //---------------------------------------------------------------
+      cy.sendTypeB({
+        header: {
+          portal_deal_id: deal._id,
+          bank_deal_id: deal.details.bankSupplyContractID,
+          Message_Type: 'B',
+          Action_Code: '006',
+        },
+        deal: {
+          UKEF_deal_id: deal._id,
+          Deal_status: 'submission_acknowledged',
+        },
+        bonds: [
+          {
+            BSS_portal_facility_id: deal.bondTransactions.items[0]._id,
+            BSS_ukef_facility_id: '12345',
+            BSS_status: '""',
+          },
+        ],
+        loans: [
+          {
+            EWCS_portal_facility_id: deal.loanTransactions.items[0]._id,
+            EWCS_ukef_facility_id: '56789',
+            EWCS_status: '""',
+          },
+        ],
+      });
+
+
+      //---------------------------------------------------------------
+      // go back into the deal and assert:
+      // - submissionType has changed from MIA to MIN
+      // - facility statuses have changed from `Completed` to `Not started`
+      //---------------------------------------------------------------
+      pages.contract.visit(deal);
+
+      pages.contract.eligibilitySubmissionType().invoke('text').then((text) => {
+        expect(text.trim()).to.contain('Manual Inclusion Notice');
+      });
+
+      bondRow.bondStatus().invoke('text').then((text) => {
+        expect(text.trim()).to.equal('Not started');
+      });
+
+      // TODO: this should only be Not started for CONDITIONAL loans, not Conditional
+      // this should not pass right now - need to update test data
+      // assert that loan status changed from `Completed` to `Not started`
+      loanRow.loanStatus().invoke('text').then((text) => {
+        expect(text.trim()).to.equal('Not started');
+      });
     });
   });
 });
