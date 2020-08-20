efdf4403712656402175b966a087acae6621282e
David Carboni <david@carboni.io>
2020-08-20 21:00:02 +0100

    Typo.

--- cypress/integration/journeys/checker/submit-a-deal-with-issued-facilities/submit-a-deal-with-issued-facilities.spec.js
+++ cypress/integration/journeys/checker/submit-a-deal-with-issued-facilities/submit-a-deal-with-issued-facilities.spec.js
@@ -6,7 +6,7 @@ const mockUsers = require('../../../../fixtures/mockUsers');
 const MAKER_LOGIN = mockUsers.find((user) => (user.roles.includes('maker') && user.bank.name === 'Barclays Bank'));
 const CHECKER_LOGIN = mockUsers.find((user) => (user.roles.includes('checker') && user.bank.name === 'Barclays Bank'));
 
-context('A checker submit a deal with issued loan/bond facilities', () => {
+context('A checker submits a deal with issued loan/bond facilities', () => {
   let deal;
   let dealId;
 
