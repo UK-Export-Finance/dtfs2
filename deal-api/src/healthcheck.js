8a35bfd03e76f84d6ce9e9f70c408d3c295c99fa
Dan <dan.watkinson@gmail.com>
2020-08-20 11:43:24 +0100

    saw-sharpening: added /healthcheck endpoint to portal+deal-api so we can better understand what is deployed where..

--- deal-api/src/healthcheck.js
+++ deal-api/src/healthcheck.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-const GITGUB_SHA = process.env.GITGUB_SHA || 'undefined';
+const GITHUB_SHA = process.env.GITHUB_SHA || 'undefined';
 
 router.get('/healthcheck', (req, res) => {
   res.status(200).json({
-    GITGUB_SHA,
+    GITHUB_SHA,
   });
 });
 
