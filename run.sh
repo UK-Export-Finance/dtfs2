e53b1e2383b01916ebdbe4b9b888302a5b25e296
David Carboni <david@carboni.io>
2020-08-20 17:06:23 +0100

    Tweaked local container version identifiers.

--- run.sh
+++ run.sh
@@ -12,11 +12,11 @@ done
 npm run lint:fix
 
 cd portal
-docker build --tag dtfs/portal --build-arg GITHUB_SHA=development .
+docker build --tag dtfs/portal --build-arg GITHUB_SHA=portal-ui-local .
 cd $home
 
 #cd deal-api
-#docker build --tag deal-api --build-arg GITHUB_SHA=development .
+#docker build --tag deal-api --build-arg GITHUB_SHA=portal-api-local .
 #cd $home
 
 if [ ! -z "$(docker ps -aq)"; then
