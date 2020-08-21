0f72953be4fc9220d4f27d3a5bbf217cf7b35c45
ttbarnes <tony@tonybarnes.me>
2020-08-21 15:17:33 +0100

    DTFS2-1692 e2e test for issue-facility and delete links appearing/not appearing depending on deal status

--- cypress/integration/journeys/maker/submit-a-deal-for-review-with-issued-and-unissued-facilities-and-resubmit-after-checker-returns/submit-a-deal-for-review-with-multiple-facility-types-and-resubmit-after-checker-returns.spec.js
+++ cypress/integration/journeys/maker/submit-a-deal-for-review-with-issued-and-unissued-facilities-and-resubmit-after-checker-returns/submit-a-deal-for-review-with-multiple-facility-types-and-resubmit-after-checker-returns.spec.js
@@ -2,11 +2,12 @@ const pages = require('../../../pages');
 const mockUsers = require('../../../../fixtures/mockUsers');
 const dealReadyToSubmitToChecker = require('./dealReadyToSubmitToChecker');
 
-const CHECKER_LOGIN = mockUsers.find(user => (user.roles.includes('checker') && user.bank.name === 'Barclays Bank'));
-const MAKER_LOGIN = mockUsers.find(user => (user.roles.includes('maker') && user.bank.name === 'Barclays Bank'));
+const CHECKER_LOGIN = mockUsers.find((user) => (user.roles.includes('checker') && user.bank.name === 'Barclays Bank'));
+const MAKER_LOGIN = mockUsers.find((user) => (user.roles.includes('maker') && user.bank.name === 'Barclays Bank'));
 
 context('A maker and checker can submit and re-submit a deal to each other multiple times', () => {
   let deal;
+  let dealId;
 
   beforeEach(() => {
     cy.on('uncaught:exception', (err, runnable) => {
@@ -19,10 +20,11 @@ context('A maker and checker can submit and re-submit a deal to each other mult
     cy.insertOneDeal(dealReadyToSubmitToChecker, { ...MAKER_LOGIN })
       .then((insertedDeal) => {
         deal = insertedDeal;
+        dealId = deal._id; // eslint-disable-line no-underscore-dangle
       });
   });
 
-  const assertFacilityStatuseAreTheSame = () => {
+  const assertFacilityTableValuesWithDealInDraftStatus = () => {
     deal.bondTransactions.items.forEach((bond) => {
       const bondId = bond._id; // eslint-disable-line no-underscore-dangle
       const bondRow = pages.contract.bondTransactionsTable.row(bondId);
@@ -34,6 +36,8 @@ context('A maker and checker can submit and re-submit a deal to each other mult
       bondRow.bondStage().invoke('text').then((text) => {
         expect(text.trim()).to.equal(bond.bondStage);
       });
+      bondRow.deleteLink().should('be.visible');
+      bondRow.issueFacilityLink().should('not.be.visible');
     });
 
     deal.loanTransactions.items.forEach((loan) => {
@@ -47,10 +51,91 @@ context('A maker and checker can submit and re-submit a deal to each other mult
       loanRow.facilityStage().invoke('text').then((text) => {
         expect(text.trim()).to.equal(loan.facilityStage);
       });
+
+      loanRow.deleteLink().should('be.visible');
+      loanRow.issueFacilityLink().should('not.be.visible');
     });
   };
 
-  it('should retain loan and bond facility statuses as they were in Draft (`Completed`) during all stages of submits/re-submits, regardless of Issued/Unissued/Conditional/Unconditional', () => {
+  const assertFacilityTableValues = () => {
+    const unissuedBonds = deal.bondTransactions.items.filter((b) => b.bondStage === 'Unissued');
+    const issuedBonds = deal.bondTransactions.items.filter((b) => b.bondStage === 'Issued');
+
+    const conditionalLoans = deal.loanTransactions.items.filter((l) => l.facilityStage === 'Conditional');
+    const unconditionalLoans = deal.loanTransactions.items.filter((l) => l.facilityStage === 'Unconditional');
+
+    unissuedBonds.forEach((bond) => {
+      const bondId = bond._id; // eslint-disable-line no-underscore-dangle
+      const bondRow = pages.contract.bondTransactionsTable.row(bondId);
+
+      bondRow.bondStatus().invoke('text').then((text) => {
+        expect(text.trim()).to.equal('Completed');
+      });
+
+      bondRow.bondStage().invoke('text').then((text) => {
+        expect(text.trim()).to.equal(bond.bondStage);
+      });
+
+      bondRow.issueFacilityLink().should('be.visible');
+      bondRow.issueFacilityLink().invoke('attr', 'href').then((href) => {
+        expect(href).to.equal(`/contract/${dealId}/bond/${bondId}/issue-facility`);
+      });
+      bondRow.deleteLink().should('not.be.visible');
+    });
+
+    issuedBonds.forEach((bond) => {
+      const bondId = bond._id; // eslint-disable-line no-underscore-dangle
+      const bondRow = pages.contract.bondTransactionsTable.row(bondId);
+
+      bondRow.bondStatus().invoke('text').then((text) => {
+        expect(text.trim()).to.equal('Completed');
+      });
+
+      bondRow.bondStage().invoke('text').then((text) => {
+        expect(text.trim()).to.equal(bond.bondStage);
+      });
+
+      bondRow.issueFacilityLink().should('not.be.visible');
+      bondRow.deleteLink().should('not.be.visible');
+    });
+
+    conditionalLoans.forEach((loan) => {
+      const loanId = loan._id; // eslint-disable-line no-underscore-dangle
+      const loanRow = pages.contract.loansTransactionsTable.row(loanId);
+
+      loanRow.loanStatus().invoke('text').then((text) => {
+        expect(text.trim()).to.equal('Completed');
+      });
+
+      loanRow.facilityStage().invoke('text').then((text) => {
+        expect(text.trim()).to.equal(loan.facilityStage);
+      });
+
+      loanRow.issueFacilityLink().should('be.visible');
+      loanRow.issueFacilityLink().invoke('attr', 'href').then((href) => {
+        expect(href).to.equal(`/contract/${dealId}/loan/${loanId}/issue-facility`);
+      });
+      loanRow.deleteLink().should('not.be.visible');
+    });
+
+    unconditionalLoans.forEach((loan) => {
+      const loanId = loan._id; // eslint-disable-line no-underscore-dangle
+      const loanRow = pages.contract.loansTransactionsTable.row(loanId);
+
+      loanRow.loanStatus().invoke('text').then((text) => {
+        expect(text.trim()).to.equal('Completed');
+      });
+
+      loanRow.facilityStage().invoke('text').then((text) => {
+        expect(text.trim()).to.equal(loan.facilityStage);
+      });
+
+      loanRow.issueFacilityLink().should('not.be.visible');
+      loanRow.deleteLink().should('not.be.visible');
+    });
+  };
+
+  it('Unchanged/unissued Facility statuses should be retained as they were in Draft (`Completed`) during all stages of submits/re-submits; `Delete` or `Issue Facility` links should appear for correct facility types.', () => {
     //---------------------------------------------------------------
     // maker submits deal to checker
     //---------------------------------------------------------------
@@ -58,7 +143,7 @@ context('A maker and checker can submit and re-submit a deal to each other mult
     cy.login({ ...MAKER_LOGIN });
     pages.contract.visit(deal);
 
-    assertFacilityStatuseAreTheSame();
+    assertFacilityTableValuesWithDealInDraftStatus();
 
     pages.contract.proceedToReview().click();
     pages.contractReadyForReview.comments().type('Ready for review');
@@ -82,7 +167,7 @@ context('A maker and checker can submit and re-submit a deal to each other mult
     cy.login({ ...MAKER_LOGIN });
     pages.contract.visit(deal);
 
-    assertFacilityStatuseAreTheSame();
+    assertFacilityTableValues();
 
     //---------------------------------------------------------------
     // maker re-submits deal with no changes
@@ -94,6 +179,6 @@ context('A maker and checker can submit and re-submit a deal to each other mult
 
     pages.contract.visit(deal);
 
-    assertFacilityStatuseAreTheSame();
+    assertFacilityTableValues();
   });
 });
