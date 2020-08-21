ea2c0083413b9ae7404fa1d9d0b8f3536c891aeb
Andrew Price <andrew@madebymoon.com>
2020-08-21 15:40:50 +0100

    dtfs2-1425 added confirmation os start dates to ready to submit page on approved deal

--- portal/component-tests/contract/components/contract-actions/proceed-to-review-button.component-test.js
+++ portal/component-tests/contract/components/contract-actions/proceed-to-review-button.component-test.js
@@ -1,16 +1,15 @@
 const componentRenderer = require('../../../componentRenderer');
+
 const component = 'contract/components/contract-actions/proceed-to-review-button.njk';
 const render = componentRenderer(component);
 
 describe(component, () => {
-
-  describe("when viewed by a maker", () => {
-
-    it("should be enabled for deals in status=Draft and status=Further Maker's input required and when dealFormsCompleted flag is true", () =>{
-      const user = {roles: ['maker']};
+  describe('when viewed by a maker', () => {
+    it("should be enabled for deals in status=Draft and status=Further Maker's input required and when dealFormsCompleted flag is true", () => {
+      const user = { roles: ['maker'] };
       const deals = [
-        {_id: 1, details:{status:"Draft"}},
-        {_id: 2, details:{status:"Further Maker's input required"}},
+        { _id: 1, details: { status: 'Draft' } },
+        { _id: 2, details: { status: "Further Maker's input required" } },
       ];
 
       const dealFormsCompleted = true;
@@ -32,31 +31,33 @@ describe(component, () => {
       const dealHasIssuedFacilitiesToSubmit = true;
 
       for (const deal of deals) {
-        const wrapper = render({ user, deal, dealFormsCompleted, dealHasIssuedFacilitiesToSubmit });
+        const wrapper = render({
+          user, deal, dealFormsCompleted, dealHasIssuedFacilitiesToSubmit,
+        });
         wrapper.expectPrimaryButton('[data-cy="ProceedToReview"]')
           .toLinkTo(`/contract/${deal._id}/ready-for-review`, 'Proceed to review');
       }
     });
 
-    it('should be enabled for deals in status=`Acknowledged by UKEF`,`Accepted by UKEF (with conditions)`, `Accepted by UKEF (without conditions)`, `Ready for Checker\'s approval`, `Further Maker\'s input required` when dealHasIssuedFacilitiesToSubmit flag set to true and dealFormsCompleted flag set to false', () => {
+    it('should be enabled for deals in status=`Acknowledged by UKEF`, `Ready for Checker\'s approval`, `Further Maker\'s input required` when dealHasIssuedFacilitiesToSubmit flag set to true and dealFormsCompleted flag set to false', () => {
       const user = { roles: ['maker'] };
       const deals = [
         { _id: 1, details: { status: 'Acknowledged by UKEF' } },
-        { _id: 2, details: { status: 'Accepted by UKEF (with conditions)' } },
-        { _id: 3, details: { status: 'Accepted by UKEF (without conditions)' } },
         { _id: 4, details: { status: 'Ready for Checker\'s approval' } },
       ];
 
       const dealHasIssuedFacilitiesToSubmit = true;
 
       for (const deal of deals) {
-        const wrapper = render({ user, deal, dealHasIssuedFacilitiesToSubmit });
+        const wrapper = render({
+          user, deal, dealHasIssuedFacilitiesToSubmit,
+        });
         wrapper.expectPrimaryButton('[data-cy="ProceedToReview"]')
           .toLinkTo(`/contract/${deal._id}/ready-for-review`, 'Proceed to review');
       }
     });
 
-    it('should be enabled for deals in status=`Accepted by UKEF (with conditions)`, `Accepted by UKEF (without conditions)`, regardless of incomplete/completed facilities and dealForms', () => {
+    it('should not be enabled for deals in status=`Accepted by UKEF (with conditions)`, `Accepted by UKEF (without conditions)`, until facility start dates confirmed', () => {
       const user = { roles: ['maker'] };
       const deals = [
         { _id: 1, details: { status: 'Accepted by UKEF (with conditions)' } },
@@ -64,47 +65,66 @@ describe(component, () => {
       ];
 
       for (const deal of deals) {
-        const wrapper = render({ user, deal });
+        const wrapper = render({
+          user, deal, dealFormsCompleted: true, allRequestedCoverStartDatesConfirmed: false,
+        });
+        wrapper.expectPrimaryButton('[data-cy="ProceedToReview"]').toBeDisabled();
+      }
+    });
+
+    it('should be enabled for deals in status=`Accepted by UKEF (with conditions)`, `Accepted by UKEF (without conditions)`,and all facility start dates confirmed', () => {
+      const user = { roles: ['maker'] };
+      const deals = [
+        { _id: 1, details: { status: 'Accepted by UKEF (with conditions)' } },
+        { _id: 2, details: { status: 'Accepted by UKEF (without conditions)' } },
+      ];
+
+      for (const deal of deals) {
+        const wrapper = render({
+          user, deal, dealFormsCompleted: true, allRequestedCoverStartDatesConfirmed: true,
+        });
         wrapper.expectPrimaryButton('[data-cy="ProceedToReview"]')
           .toLinkTo(`/contract/${deal._id}/ready-for-review`, 'Proceed to review');
       }
     });
 
-    it("should not render at all for deals in status=Submitted and status=Rejected by UKEF", () =>{
-      const user = {roles: ['maker']};
+    it('should not render at all for deals in status=Submitted and status=Rejected by UKEF', () => {
+      const user = { roles: ['maker'] };
       const deals = [
-        {_id: 1, details:{status:"Submitted"}},
-        {_id: 2, details:{status:"Rejected by UKEF"}},
+        { _id: 1, details: { status: 'Submitted' } },
+        { _id: 2, details: { status: 'Rejected by UKEF' } },
       ];
 
       for (const deal of deals) {
-        const wrapper = render({user, deal});
+        const wrapper = render({ user, deal });
         wrapper.expectPrimaryButton('[data-cy="ProceedToReview"]')
           .notToExist();
       }
     });
 
-    it("should be disabled when dealFormsCompleted and dealHasIssuedFacilitiesToSubmit flags are false", () =>{
-      const user = {roles: ['maker']};
+    it('should be disabled when dealFormsCompleted and dealHasIssuedFacilitiesToSubmit flags are false', () => {
+      const user = { roles: ['maker'] };
       const deals = [
-        { _id: 1, details: { status: "Draft" } },
+        { _id: 1, details: { status: 'Draft' } },
         { _id: 2, details: { status: "Further Maker's input required" } },
-        { _id: 3, details: { status: "Abandoned Deal" } },
-        { _id: 4, details: { status: "Acknowledged by UKEF" } },
+        { _id: 3, details: { status: 'Abandoned Deal' } },
+        { _id: 4, details: { status: 'Acknowledged by UKEF' } },
       ];
 
       const dealFormsCompleted = false;
       const dealHasIssuedFacilitiesToSubmit = false;
 
       for (const deal of deals) {
-        const wrapper = render({ user, deal, dealFormsCompleted, dealHasIssuedFacilitiesToSubmit});
+        const wrapper = render({
+          user, deal, dealFormsCompleted, dealHasIssuedFacilitiesToSubmit,
+        });
         wrapper.expectPrimaryButton('[data-cy="ProceedToReview"]')
           .toBeDisabled();
       }
     });
 
-    it("should be disabled for deals in all other states", () =>{
-      const user = {roles: ['maker']};
+    it('should be disabled for deals in all other states', () => {
+      const user = { roles: ['maker'] };
       const deals = [
         { _id: 1, details: { status: 'Draft' } },
         { _id: 2, details: { status: 'Further Maker\'s input required' } },
@@ -113,31 +133,30 @@ describe(component, () => {
       ];
 
       for (const deal of deals) {
-        const wrapper = render({user, deal});
+        const wrapper = render({ user, deal });
         wrapper.expectPrimaryButton('[data-cy="ProceedToReview"]')
           .toBeDisabled();
       }
     });
-
   });
 
-  describe("when viewed by a checker", () => {
-    it("should not render at all", () =>{
-      const user = {roles: ['checker']};
+  describe('when viewed by a checker', () => {
+    it('should not render at all', () => {
+      const user = { roles: ['checker'] };
       const deals = [
-        {_id: 1, details:{status:"Draft"}},
-        {_id: 2, details:{status:"Further Maker's input required"}},
-        {_id: 3, details:{status:"Submitted"}},
-        {_id: 4, details:{status:"Rejected by UKEF"}},
-        {_id: 5, details:{status:"Abandoned Deal"}},
-        {_id: 6, details:{status:"Acknowledged by UKEF"}},
-        {_id: 7, details:{status:"Accepted by UKEF (without conditions)"}},
-        {_id: 8, details:{status:"Accepted by UKEF (with conditions)"}},
-        {_id: 9, details:{status:"Ready for Checker's approval"}},
+        { _id: 1, details: { status: 'Draft' } },
+        { _id: 2, details: { status: "Further Maker's input required" } },
+        { _id: 3, details: { status: 'Submitted' } },
+        { _id: 4, details: { status: 'Rejected by UKEF' } },
+        { _id: 5, details: { status: 'Abandoned Deal' } },
+        { _id: 6, details: { status: 'Acknowledged by UKEF' } },
+        { _id: 7, details: { status: 'Accepted by UKEF (without conditions)' } },
+        { _id: 8, details: { status: 'Accepted by UKEF (with conditions)' } },
+        { _id: 9, details: { status: "Ready for Checker's approval" } },
       ];
 
       for (const deal of deals) {
-        const wrapper = render({user, deal});
+        const wrapper = render({ user, deal });
         wrapper.expectPrimaryButton('[data-cy="ProceedToReview"]')
           .notToExist();
       }
--- portal/component-tests/contract/contract-view.component-test.js
+++ portal/component-tests/contract/contract-view.component-test.js
@@ -1,170 +1,149 @@
+import STATUS from '../../server/constants';
+
 const pageRenderer = require('../pageRenderer');
+
 const page = 'contract/contract-view.njk';
 const render = pageRenderer(page);
 const deal = require('../fixtures/deal-fully-completed');
-import STATUS from '../../server/constants';
 
-const aDealInStatus = (status) => {
-  return {
-    ...deal,
-    details: {
-      ...deal.details,
-      status: status,
-    }
-  };
+const aDealInStatus = (status) => ({
+  ...deal,
+  details: {
+    ...deal.details,
+    status,
+  },
+});
+
+const oneDealInEachStatus = () => [
+  aDealInStatus(STATUS.draft),
+  aDealInStatus(STATUS.readyForApproval),
+  aDealInStatus(STATUS.inputRequired),
+  aDealInStatus(STATUS.abandoned),
+  aDealInStatus(STATUS.submitted),
+  aDealInStatus(STATUS.submissionAcknowledged),
+  aDealInStatus(STATUS.approved),
+  aDealInStatus(STATUS.approvedWithConditions),
+  aDealInStatus(STATUS.refused),
+];
+
+const roles = ['maker', 'checker'];
+
+const confirmedRequestStartDateParams = {
+  confirmedRequestedCoverStartDates: [],
+  allRequestedCoverStartDatesConfirmed: true,
 };
-
-const oneDealInEachStatus = () => {
-  return [
-    aDealInStatus(STATUS.draft),
-    aDealInStatus(STATUS.readyForApproval),
-    aDealInStatus(STATUS.inputRequired),
-    aDealInStatus(STATUS.abandoned),
-    aDealInStatus(STATUS.submitted),
-    aDealInStatus(STATUS.submissionAcknowledged),
-    aDealInStatus(STATUS.approved),
-    aDealInStatus(STATUS.approvedWithConditions),
-    aDealInStatus(STATUS.refused),
-  ];
-};
-
-const roles = ['maker','checker'];
 
 describe(page, () => {
   describe('always', () => {
     const wrappers = [];
 
-    beforeAll( ()=>{
-      for(const role of roles) {
-        const user = { roles:[role], timezone:'Europe/London' };
-        for(const deal of oneDealInEachStatus()) {
-          wrappers.push( render({user, deal}) )
+    beforeAll(() => {
+      for (const role of roles) {
+        const user = { roles: [role], timezone: 'Europe/London' };
+        for (const deal of oneDealInEachStatus()) {
+          wrappers.push(render({
+            user, deal, ...confirmedRequestStartDateParams,
+          }));
         }
       }
     });
 
-    it('displays bankSupplyContractName', () => {
-      return wrappers.forEach(wrapper => wrapper.expectText('[data-cy="bankSupplyContractName"]')
-                                          .toRead(deal.details.bankSupplyContractName));
-    });
+    it('displays bankSupplyContractName', () => wrappers.forEach((wrapper) => wrapper.expectText('[data-cy="bankSupplyContractName"]')
+      .toRead(deal.details.bankSupplyContractName)));
 
-    it('should render contract overview table', () => {
-      return wrappers.forEach((wrapper) =>
-        wrapper.expectElement('[data-cy="contract-overview-table"]').toExist());
-    });
+    it('should render contract overview table', () => wrappers.forEach((wrapper) =>
+      wrapper.expectElement('[data-cy="contract-overview-table"]').toExist()));
 
-    it('should render Summary Table component', () => {
-      return wrappers.forEach((wrapper) =>
-        wrapper.expectElement('[data-cy="summary-table"]').toExist());
-    });
+    it('should render Summary Table component', () => wrappers.forEach((wrapper) =>
+      wrapper.expectElement('[data-cy="summary-table"]').toExist()));
 
-    it('should render Forms Incomplete text component', () => {
-      return wrappers.forEach((wrapper) =>
-        wrapper.expectElement('[data-cy="forms-incomplete-text"]').toExist());
-    });
+    it('should render Forms Incomplete text component', () => wrappers.forEach((wrapper) =>
+      wrapper.expectElement('[data-cy="forms-incomplete-text"]').toExist()));
 
-    it('should render Totals Table component', () => {
-      return wrappers.forEach((wrapper) =>
-        wrapper.expectElement('[data-cy="totals-table"]').toExist());
-    });
-
+    it('should render Totals Table component', () => wrappers.forEach((wrapper) =>
+      wrapper.expectElement('[data-cy="totals-table"]').toExist()));
   });
 
   describe('when viewed with editable=true', () => {
     const wrappers = [];
-    beforeAll( ()=>{
-      for(const role of roles) {
-        const user = { roles:[role], timezone:'Europe/London' };
-        for(const deal of oneDealInEachStatus()) {
-          wrappers.push( render({user, deal, editable:true}) )
+    beforeAll(() => {
+      for (const role of roles) {
+        const user = { roles: [role], timezone: 'Europe/London' };
+        for (const deal of oneDealInEachStatus()) {
+          wrappers.push(render({
+            user, deal, editable: true, ...confirmedRequestStartDateParams,
+          }));
         }
       }
     });
 
-    it('links to the about supply contract section', () => {
-      return wrappers.forEach(wrapper => wrapper.expectLink('[data-cy="ViewAboutSupplierDetails"]')
-                                          .toLinkTo(`/contract/${deal._id}/about/supplier`, 'View details'));
-    });
+    it('links to the about supply contract section', () => wrappers.forEach((wrapper) => wrapper.expectLink('[data-cy="ViewAboutSupplierDetails"]')
+      .toLinkTo(`/contract/${deal._id}/about/supplier`, 'View details')));
 
-    it('links to the eligibility criteria section', () => {
-      return wrappers.forEach(wrapper => wrapper.expectLink('[data-cy="ViewDetails"]')
-                                          .toLinkTo(`/contract/${deal._id}/eligibility/criteria`, 'View details'));
-    });
+    it('links to the eligibility criteria section', () => wrappers.forEach((wrapper) => wrapper.expectLink('[data-cy="ViewDetails"]')
+      .toLinkTo(`/contract/${deal._id}/eligibility/criteria`, 'View details')));
 
-    it('allows the user to add a bond', () => {
-      return wrappers.forEach(wrapper => wrapper.expectLink('[data-cy="link-add-bond"]')
-                                          .toLinkTo(`/contract/${deal._id}/bond/create`, 'Add a Bond'));
-    });
+    it('allows the user to add a bond', () => wrappers.forEach((wrapper) => wrapper.expectLink('[data-cy="link-add-bond"]')
+      .toLinkTo(`/contract/${deal._id}/bond/create`, 'Add a Bond')));
 
     it('provides a link to the bond', () => {
       const dealId = deal._id;
       const bondId = deal.bondTransactions.items[0]._id;
 
-      return wrappers.forEach(wrapper => wrapper.expectLink(`[data-cy="unique-number-${bondId}"]`)
-                        .toLinkTo(`/contract/${dealId}/bond/${bondId}/details`, deal.bondTransactions.items[0].uniqueIdentificationNumber));
-
-
+      return wrappers.forEach((wrapper) => wrapper.expectLink(`[data-cy="unique-number-${bondId}"]`)
+        .toLinkTo(`/contract/${dealId}/bond/${bondId}/details`, deal.bondTransactions.items[0].uniqueIdentificationNumber));
     });
 
-    it('allows the user to add a loan', () => {
-      return wrappers.forEach(wrapper => wrapper.expectLink('[data-cy="link-add-loan"]')
-                        .toLinkTo(`/contract/${deal._id}/loan/create`, 'Add a Loan'));
-    });
+    it('allows the user to add a loan', () => wrappers.forEach((wrapper) => wrapper.expectLink('[data-cy="link-add-loan"]')
+      .toLinkTo(`/contract/${deal._id}/loan/create`, 'Add a Loan')));
 
     it('provides a link to the loan', () => {
       const dealId = deal._id;
       const loanId = deal.loanTransactions.items[0]._id;
 
-      return wrappers.forEach(wrapper => wrapper.expectLink(`[data-cy="loan-bank-reference-number-${loanId}"]`)
-                .toLinkTo(`/contract/${dealId}/loan/${loanId}/guarantee-details`, deal.loanTransactions.items[0].bankReferenceNumber));
+      return wrappers.forEach((wrapper) => wrapper.expectLink(`[data-cy="loan-bank-reference-number-${loanId}"]`)
+        .toLinkTo(`/contract/${dealId}/loan/${loanId}/guarantee-details`, deal.loanTransactions.items[0].bankReferenceNumber));
     });
-
   });
 
 
   describe('when viewed with editable=false', () => {
     const wrappers = [];
 
-    beforeAll( ()=>{
-      for(const role of roles) {
-        const user = { roles:[role], timezone:'Europe/London' };
-        for(const deal of oneDealInEachStatus()) {
-          wrappers.push( render({user, deal, editable:false}) )
+    beforeAll(() => {
+      for (const role of roles) {
+        const user = { roles: [role], timezone: 'Europe/London' };
+        for (const deal of oneDealInEachStatus()) {
+          wrappers.push(render({
+            user, deal, editable: false, ...confirmedRequestStartDateParams,
+          }));
         }
       }
     });
 
-    it('hides the link to the about supply contract section', () => {
-      return wrappers.forEach(wrapper => wrapper.expectLink('[data-cy="ViewAboutSupplierDetails"]').notToExist());
-    });
+    it('hides the link to the about supply contract section', () => wrappers.forEach((wrapper) => wrapper.expectLink('[data-cy="ViewAboutSupplierDetails"]').notToExist()));
 
-    it('hides the link to the eligibility criteria section', () => {
-      return wrappers.forEach(wrapper => wrapper.expectLink('[data-cy="ViewDetails"]').notToExist());
-    });
+    it('hides the link to the eligibility criteria section', () => wrappers.forEach((wrapper) => wrapper.expectLink('[data-cy="ViewDetails"]').notToExist()));
 
-    it('hides the link to add a bond', () => {
-      return wrappers.forEach(wrapper => wrapper.expectLink('[data-cy="link-add-bond"]').notToExist());
-    });
+    it('hides the link to add a bond', () => wrappers.forEach((wrapper) => wrapper.expectLink('[data-cy="link-add-bond"]').notToExist()));
 
     it('hides the link to delete a bond', () => {
       const bondId = deal.bondTransactions.items[0]._id;
-      return wrappers.forEach(wrapper => wrapper.expectLink(`[data-cy="delete-bond-${bondId}"]`).notToExist());
+      return wrappers.forEach((wrapper) => wrapper.expectLink(`[data-cy="delete-bond-${bondId}"]`).notToExist());
     });
 
     it('hides the link to the bond', () => {
       const bondId = deal.bondTransactions.items[0]._id;
-      return wrappers.forEach(wrapper => wrapper.expectLink(`[data-cy="unique-number-${bondId}"]`).notToExist());
+      return wrappers.forEach((wrapper) => wrapper.expectLink(`[data-cy="unique-number-${bondId}"]`).notToExist());
     });
 
-    it('hides the link to add a loan', () => {
-      return wrappers.forEach(wrapper => wrapper.expectLink('[data-cy="link-add-loan"]').notToExist());
-    });
+    it('hides the link to add a loan', () => wrappers.forEach((wrapper) => wrapper.expectLink('[data-cy="link-add-loan"]').notToExist()));
 
     it('hides the link to the loan', () => {
       const dealId = deal._id;
       const loanId = deal.loanTransactions.items[0]._id;
 
-      return wrappers.forEach(wrapper => wrapper.expectLink(`[data-cy="loan-bank-reference-number-${loanId}"]`).notToExist());
+      return wrappers.forEach((wrapper) => wrapper.expectLink(`[data-cy="loan-bank-reference-number-${loanId}"]`).notToExist());
     });
   });
 
@@ -172,33 +151,32 @@ describe(page, () => {
   describe('when viewed with editable=true and any status except Acknowledged by UKEF and Ready for Checker\'s approval', () => {
     const wrappers = [];
 
-    const dealsWithStatusThatAllowBondDeletion = () => {
-      return [ 
-        aDealInStatus(STATUS.draft),
-        aDealInStatus(STATUS.inputRequired),
-        aDealInStatus(STATUS.abandoned),
-        aDealInStatus(STATUS.submitted),
-        aDealInStatus(STATUS.approved),
-        aDealInStatus(STATUS.approvedWithConditions),
-        aDealInStatus(STATUS.refused),
-      ];
-    };
+    const dealsWithStatusThatAllowBondDeletion = () => [
+      aDealInStatus(STATUS.draft),
+      aDealInStatus(STATUS.inputRequired),
+      aDealInStatus(STATUS.abandoned),
+      aDealInStatus(STATUS.submitted),
+      aDealInStatus(STATUS.approved),
+      aDealInStatus(STATUS.approvedWithConditions),
+      aDealInStatus(STATUS.refused),
+    ];
 
-    beforeAll( ()=>{
-      for(const role of roles) {
-        const user = { roles:[role], timezone:'Europe/London' };
+    beforeAll(() => {
+      for (const role of roles) {
+        const user = { roles: [role], timezone: 'Europe/London' };
         for (const deal of dealsWithStatusThatAllowBondDeletion()) {
-          wrappers.push( render({user, deal, editable:true}) )
+          wrappers.push(render({
+            user, deal, editable: true, ...confirmedRequestStartDateParams,
+          }));
         }
       }
     });
 
     it('allows the user to delete a bond', () => {
-
       const dealId = deal._id;
       const bondId = deal.bondTransactions.items[0]._id;
 
-      return wrappers.forEach(wrapper => wrapper.expectLink(`[data-cy="bond-delete-${bondId}"]`)
+      return wrappers.forEach((wrapper) => wrapper.expectLink(`[data-cy="bond-delete-${bondId}"]`)
         .toLinkTo(`/contract/${dealId}/bond/${bondId}/delete`, 'Delete'));
     });
   });
