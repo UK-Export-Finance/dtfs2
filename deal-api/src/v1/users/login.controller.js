33fd321a95a366a032d89956356f6f865c21a0d5
David Carboni <david@carboni.io>
2020-08-24 20:19:20 +0100

    Lint

--- deal-api/src/v1/users/login.controller.js
+++ deal-api/src/v1/users/login.controller.js
@@ -11,18 +11,18 @@ module.exports = (username, password) => new Promise((resolve) => {
     }
 
     if (user === null) {
-      console.log(`No user returned`);
+      console.log('No user returned');
       return resolve({ err: userNotFound });
     }
 
     const passwordIncorrect = !utils.validPassword(password, user.hash, user.salt);
     if (passwordIncorrect) {
-      console.log(`Incorrect password`);
+      console.log('Incorrect password');
       await incrementFailedLoginCount(user);
       return resolve({ err: incorrectPassword });
     }
     if (user['user-status'] === 'blocked') {
-      console.log(`User is blocked`);
+      console.log('User is blocked');
       return resolve({ err: userIsBlocked });
     }
     const tokenObject = utils.issueJWT(user);
