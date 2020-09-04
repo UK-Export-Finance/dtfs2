d8bd6edb2f8d0c16df84920cc99d8aa2d813714a
ttbarnes <tony@tonybarnes.me>
2020-09-04 09:48:45 +0100

    DTFS2-1868 - ensure that if a facility status is `Maker input required`, contract `proceed to review` button is enabled

--- portal/server/routes/contract/dealHasIssuedFacilitiesToSubmit.js
+++ portal/server/routes/contract/dealHasIssuedFacilitiesToSubmit.js
@@ -1,23 +1,23 @@
 const dealHasIssuedFacilitiesToSubmit = (deal) => {
   const bonds = deal.bondTransactions.items;
   const loans = deal.loanTransactions.items;
+  const allFacilities = [...bonds, ...loans];
 
-  const unsubmittedLoansWithIssuedFacilities = loans.filter((loan) =>
-    (loan.issueFacilityDetailsProvided === true
-    && !loan.issueFacilityDetailsSubmitted
-    && loan.status !== 'Ready for check'
-    && loan.status !== 'Submitted'));
+  const facilitiesToSubmit = allFacilities.filter((facility) => {
+    if (facility.issueFacilityDetailsProvided === true
+      && !facility.issueFacilityDetailsSubmitted
+      && facility.status !== 'Ready for check'
+      && facility.status !== 'Submitted') {
+      return facility;
+    }
 
-  const unsubmittedBondsWithIssuedFacilities = bonds.filter((bond) =>
-    (bond.issueFacilityDetailsProvided === true
-    && !bond.issueFacilityDetailsSubmitted
-    && bond.status !== 'Ready for check'
-    && bond.status !== 'Submitted'));
+    if (facility.issueFacilityDetailsSubmitted
+        && facility.status === 'Maker\'s input required') {
+      return facility;
+    }
+  });
 
-  const hasIssuedFacilities = (unsubmittedBondsWithIssuedFacilities.length > 0
-                              || unsubmittedLoansWithIssuedFacilities.length > 0);
-
-  if (hasIssuedFacilities) {
+  if (facilitiesToSubmit.length > 0) {
     return true;
   }
 
