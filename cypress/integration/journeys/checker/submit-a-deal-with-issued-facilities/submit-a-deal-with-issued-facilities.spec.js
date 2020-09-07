3030a8bc2e1be3c73b30789bab8e189c8959c7b2
ttbarnes <tony@tonybarnes.me>
2020-09-07 11:14:52 +0100

    DTFS2-1868 - update e2e test for facility statuses updating when deal is submitted

--- cypress/integration/journeys/checker/submit-a-deal-with-issued-facilities/submit-a-deal-with-issued-facilities.spec.js
+++ cypress/integration/journeys/checker/submit-a-deal-with-issued-facilities/submit-a-deal-with-issued-facilities.spec.js
@@ -1,7 +1,14 @@
 const pages = require('../../../pages');
 const relative = require('../../../relativeURL');
-const dealWithIssuedFacilitiesReadyForReview = require('./dealWithIssuedFacilitiesReadyForReview');
+const dealWithUnIssuedFacilities = require('./dealWithUnIssuedFacilities');
 const mockUsers = require('../../../../fixtures/mockUsers');
+const {
+  fillAndSubmitIssueBondFacilityForm,
+} = require('../../maker/submit-issued-facilities-for-review/fillAndSubmitIssueBondFacilityForm');
+const {
+  fillAndSubmitIssueLoanFacilityForm,
+} = require('../../maker/submit-issued-facilities-for-review/fillAndSubmitIssueLoanFacilityForm');
+
 
 const MAKER_LOGIN = mockUsers.find((user) => (user.roles.includes('maker') && user.bank.name === 'Barclays Bank'));
 const CHECKER_LOGIN = mockUsers.find((user) => (user.roles.includes('checker') && user.bank.name === 'Barclays Bank'));
@@ -16,7 +23,7 @@ context('A checker submits a deal with issued loan/bond facilities', () => {
       console.log(err.stack);
       return false;
     });
-    cy.insertOneDeal(dealWithIssuedFacilitiesReadyForReview, MAKER_LOGIN)
+    cy.insertOneDeal(dealWithUnIssuedFacilities, MAKER_LOGIN)
       .then((insertedDeal) => {
         deal = insertedDeal;
         dealId = deal._id; // eslint-disable-line no-underscore-dangle
@@ -24,26 +31,178 @@ context('A checker submits a deal with issued loan/bond facilities', () => {
   });
 
   it('Checker can view (but not edit) Issued facilities and after re-submitting the deal, the Deal/facilities should be updated', () => {
+    //---------------------------------------------------------------
+    // maker submits deal to checker
+    //---------------------------------------------------------------
+    cy.login(MAKER_LOGIN);
+    pages.contract.visit(deal);
+
+    pages.contract.proceedToReview().click();
+    pages.contractReadyForReview.comments().type('Issued facilities');
+    pages.contractReadyForReview.readyForCheckersApproval().click();
+
+    //---------------------------------------------------------------
+    // checker submits deal to ukef
+    //---------------------------------------------------------------
+    cy.login({ ...CHECKER_LOGIN });
+    pages.contract.visit(deal);
+    pages.contract.proceedToSubmit().click();
+
+    pages.contractConfirmSubmission.confirmSubmit().check();
+    pages.contractConfirmSubmission.acceptAndSubmit().click();
+
+    //---------------------------------------------------------------
+    // receive typeB XML with `Approved` deal status
+    //---------------------------------------------------------------
+    cy.sendTypeB({
+      header: {
+        portal_deal_id: dealId,
+        bank_deal_id: deal.details.bankSupplyContractID,
+        Message_Type: 'B',
+        Action_Code: '016',
+      },
+      deal: {
+        UKEF_deal_id: dealId,
+        Deal_status: 'approved',
+      },
+      bonds: [
+        {
+          BSS_portal_facility_id: deal.bondTransactions.items[0]._id,
+          BSS_ukef_facility_id: '12345',
+          BSS_status: '""',
+        },
+      ],
+      loans: [
+        {
+          EWCS_portal_facility_id: deal.loanTransactions.items[0]._id,
+          EWCS_ukef_facility_id: '56789',
+          EWCS_status: '""',
+        },
+      ],
+    });
+
+    //---------------------------------------------------------------
+    // maker adds Issued Facilities and submits deal for review by checker
+    //---------------------------------------------------------------
+    cy.login(MAKER_LOGIN);
+    pages.contract.visit(deal);
+
+    const bondId = deal.bondTransactions.items[0]._id; // eslint-disable-line no-underscore-dangle
+    const bondRow = pages.contract.bondTransactionsTable.row(bondId);
+
+    const loanId = deal.loanTransactions.items[0]._id; // eslint-disable-line no-underscore-dangle
+    const loanRow = pages.contract.loansTransactionsTable.row(loanId);
+
+    bondRow.bondStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Not started');
+    });
+
+    bondRow.deleteLink().should('not.exist');
+
+    loanRow.loanStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Not started');
+    });
+
+    loanRow.deleteLink().should('not.exist');
+
+    bondRow.issueFacilityLink().click();
+    fillAndSubmitIssueBondFacilityForm();
+
+    loanRow.issueFacilityLink().click();
+    fillAndSubmitIssueLoanFacilityForm();
+
+    pages.contract.proceedToReview().click();
+    pages.contractReadyForReview.comments().type('Issued facilities');
+    pages.contractReadyForReview.readyForCheckersApproval().click();
+
+    //---------------------------------------------------------------
+    // checker can navigate to and view bond/loan details on the Deal submissions page
+    //---------------------------------------------------------------
     cy.login(CHECKER_LOGIN);
     pages.contract.visit(deal);
 
-    // assert that the Checker can navigate to and view bond details on the Deal submissions page
-    const bondId = deal.bondTransactions.items[0]._id; // eslint-disable-line no-underscore-dangle
-    const bondRow = pages.contract.bondTransactionsTable.row(bondId);
+    bondRow.bondStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Ready for check');
+    });
+
+    loanRow.loanStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Ready for check');
+    });
+
     bondRow.issueFacilityLink().click();
     cy.url().should('eq', relative(`/contract/${dealId}/submission-details#bond-${bondId}`));
 
-    // go back to deal page
     pages.contractSubmissionDetails.goBackLink().click();
 
-    // assert that the Checker can navigate to and view loan details on the Deal submissions page
-    const loanId = deal.loanTransactions.items[0]._id; // eslint-disable-line no-underscore-dangle
-    const loanRow = pages.contract.loansTransactionsTable.row(loanId);
     loanRow.issueFacilityLink().click();
     cy.url().should('eq', relative(`/contract/${dealId}/submission-details#loan-${loanId}`));
 
-    // go back to deal page
     pages.contractSubmissionDetails.goBackLink().click();
+
+    //---------------------------------------------------------------
+    // checker returns to maker
+    //---------------------------------------------------------------
+    pages.contract.returnToMaker().click();
+    pages.contractReturnToMaker.comments().type('Nope!');
+    pages.contractReturnToMaker.returnToMaker().click();
+
+    //---------------------------------------------------------------
+    // maker submits back to checker
+    //---------------------------------------------------------------
+    cy.login(MAKER_LOGIN);
+    pages.contract.visit(deal);
+
+    bondRow.bondStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Maker\'s input required');
+    });
+
+    loanRow.loanStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Maker\'s input required');
+    });
+
+    pages.contract.proceedToReview().click();
+    pages.contractReadyForReview.comments().type('test');
+    pages.contractReadyForReview.readyForCheckersApproval().click();
+
+    //---------------------------------------------------------------
+    // checker submits deal to ukef
+    //---------------------------------------------------------------
+    cy.login(CHECKER_LOGIN);
+    pages.contract.visit(deal);
+
+    bondRow.bondStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Ready for check');
+    });
+
+    loanRow.loanStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Ready for check');
+    });
+
+    pages.contract.proceedToSubmit().click();
+
+    pages.contractConfirmSubmission.confirmSubmit().check();
+    pages.contractConfirmSubmission.acceptAndSubmit().click();
+
+    pages.contract.visit(deal);
+
+    bondRow.bondStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Submitted');
+    });
+
+    bondRow.deleteLink().should('not.exist');
+    bondRow.issueFacilityLink().should('not.exist');
+
+    loanRow.loanStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Submitted');
+    });
+
+    loanRow.deleteLink().should('not.exist');
+    loanRow.issueFacilityLink().should('not.exist');
+
+    /*
+    //---------------------------------------------------------------
+    // TBC ............... checker logins in and....
+    //---------------------------------------------------------------
 
     // submit the deal
     pages.contract.proceedToSubmit().click();
@@ -100,5 +259,6 @@ context('A checker submits a deal with issued loan/bond facilities', () => {
 
     pages.contract.proceedToSubmit().should('not.exist');
     pages.contract.returnToMaker().should('not.exist');
+    */
   });
 });
