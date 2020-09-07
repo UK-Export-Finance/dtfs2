c42e18e8a2c645f9f5f020cf472d41b464562223
SimonTheDeveloper <mail@simon-neumann.com>
2020-09-07 11:10:47 +0100

    countdown indicator report changes

--- portal/server/routes/reports.js
+++ portal/server/routes/reports.js
@@ -821,9 +821,17 @@ router.get('/reports/mia-to-be-submitted/with-conditions/:page', async (req, res
   const fromDays = req.query.fromDays || 0;
   const toDays = req.query.toDays || maxDays;
 
+
   const submissionFilters = {
     filterBySubmissionType: 'manualInclusionApplication',
   };
+
+  const banks = await getApiData(
+    api.banks(userToken),
+    res,
+  );
+  // const banks = [{ id: 1, name: 'HSBC' }, { id: 2, name: 'NatWest' }];
+
 
   const MIAfilters = buildReportFilters(submissionFilters, req.session.user);
   const applications = await getApiData(
@@ -876,6 +884,92 @@ router.get('/reports/mia-to-be-submitted/with-conditions/:page', async (req, res
     pages,
     conditions: 'with',
     deals,
+    banks,
+    sortOrder,
+    primaryNav,
+    subNav: 'countdown-indicator',
+    user: req.session.user,
+  });
+});
+
+
+router.post('/reports/mia-to-be-submitted/with-conditions/:page', async (req, res) => {
+  // TODO wire up getMIAData function
+  const filterByDealStatus = 'Accepted by UKEF (with conditions)';
+  const { userToken } = requestParams(req);
+  let maxDays = 10;
+  let workingDays = 14;
+  if (filterByDealStatus === 'Accepted by UKEF (with conditions)') {
+    workingDays = 28;
+    maxDays = 20;
+  }
+  const fromDays = req.query.fromDays || 0;
+  const toDays = req.query.toDays || maxDays;
+
+  const submissionFilters = req.body;
+  if (submissionFilters.bank === 'any') {
+    submissionFilters.bank = '';
+  }
+  submissionFilters.filterBySubmissionType = 'manualInclusionApplication';
+
+  const banks = await getApiData(
+    api.banks(userToken),
+    res,
+  );
+  // const banks = [{ id: 1, name: 'HSBC' }, { id: 2, name: 'NatWest' }];
+
+  const MIAfilters = buildReportFilters(submissionFilters, req.session.user);
+  const applications = await getApiData(
+    api.contracts(0, 0, MIAfilters, userToken),
+    res,
+  );
+
+  let miaWithConditions = [];
+  let tempDeals = [];
+  let deals = [];
+  let count = 0;
+  if (applications.deals) {
+    miaWithConditions = applications.deals.filter((deal) => (deal.details.status === filterByDealStatus));
+    tempDeals = getExpiryDates(miaWithConditions, workingDays, true);
+    // once we have the deals and expiry dates, filter the display
+    if (fromDays > 0) {
+      deals = tempDeals.filter(
+        (deal) => deal.remainingDays >= fromDays && deal.remainingDays <= toDays,
+      );
+    } else {
+      deals = tempDeals.filter(
+        (deal) => deal.remainingDays <= toDays,
+      );
+    }
+    count = deals.length;
+  }
+
+  const pages = {
+    totalPages: Math.ceil(count / PAGESIZE),
+    currentPage: parseInt(req.params.page, 10),
+    totalItems: count,
+  };
+
+  const sortOrder = {
+    queryString: `${req.params.page}?fromDays=${fromDays}&toDays=${toDays}&sort=desc`,
+    order: 'ascending',
+    image: 'twistie-up',
+  };
+
+  // default order from getExpiryDates is asc
+  if (req.query && req.query.sort && req.query.sort === 'desc') {
+    if (deals) {
+      deals.sort((a, b) => parseFloat(b.remainingDays) - parseFloat(a.remainingDays));
+    }
+    sortOrder.queryString = `${req.params.page}?fromDays=${fromDays}&toDays=${toDays}`;
+    sortOrder.order = 'descending';
+    sortOrder.image = 'twistie-down';
+  }
+  return res.render('reports/MIA-to-be-submitted-report.njk', {
+    pages,
+    conditions: 'with',
+    deals,
+    banks,
     sortOrder,
     primaryNav,
     subNav: 'countdown-indicator',
@@ -932,6 +1026,11 @@ router.get('/reports/mia-to-be-submitted/without-conditions/:page', async (req, 
     totalItems: count,
   };
 
+  const banks = await getApiData(
+    api.banks(userToken),
+    res,
+  );
+  // const banks = [{ id: 1, name: 'HSBC' }, { id: 2, name: 'NatWest' }];
   const sortOrder = {
     queryString: `${req.params.page}?fromDays=${fromDays}&toDays=${toDays}&sort=desc`,
     order: 'ascending',
@@ -952,6 +1051,96 @@ router.get('/reports/mia-to-be-submitted/without-conditions/:page', async (req, 
     pages,
     conditions: 'without',
     deals,
+    banks,
+    sortOrder,
+    primaryNav,
+    subNav: 'countdown-indicator',
+    user: req.session.user,
+  });
+});
+
+router.post('/reports/mia-to-be-submitted/without-conditions/:page', async (req, res) => {
+  // TODO wire up getMIAData function
+  const filterByDealStatus = 'Accepted by UKEF (without conditions)';
+  const { userToken } = requestParams(req);
+  let maxDays = 10;
+  let workingDays = 14;
+  if (filterByDealStatus === 'Accepted by UKEF (with conditions)') {
+    workingDays = 28;
+    maxDays = 20;
+  }
+  const fromDays = req.query.fromDays || 0;
+  const toDays = req.query.toDays || maxDays;
+
+  if (!await api.validateToken(userToken)) {
+    res.redirect('/');
+  }
+
+  const submissionFilters = req.body;
+  if (submissionFilters.bank === 'any') {
+    submissionFilters.bank = '';
+  }
+  submissionFilters.filterBySubmissionType = 'manualInclusionApplication';
+
+
+  const MIAfilters = buildReportFilters(submissionFilters, req.session.user);
+  const applications = await getApiData(
+    api.contracts(0, 0, MIAfilters, userToken),
+    res,
+  );
+
+  let miaWithConditions = [];
+  let tempDeals = [];
+  let deals = [];
+  let count = 0;
+  if (applications.deals) {
+    miaWithConditions = applications.deals.filter((deal) => (deal.details.status === filterByDealStatus));
+    tempDeals = getExpiryDates(miaWithConditions, workingDays, true);
+    // once we have the deals and expiry dates, filter the display
+    if (fromDays > 0) {
+      deals = tempDeals.filter(
+        (deal) => deal.remainingDays >= fromDays && deal.remainingDays <= toDays,
+      );
+    } else {
+      deals = tempDeals.filter(
+        (deal) => deal.remainingDays <= toDays,
+      );
+    }
+    count = deals.length;
+  }
+
+  const pages = {
+    totalPages: Math.ceil(count / PAGESIZE),
+    currentPage: parseInt(req.params.page, 10),
+    totalItems: count,
+  };
+
+  const banks = await getApiData(
+    api.banks(userToken),
+    res,
+  );
+  // const banks = [{ id: 1, name: 'HSBC' }, { id: 2, name: 'NatWest' }];
+  const sortOrder = {
+    queryString: `${req.params.page}?fromDays=${fromDays}&toDays=${toDays}&sort=desc`,
+    order: 'ascending',
+    image: 'twistie-up',
+  };
+
+  // default order from getExpiryDates is asc
+  if (req.query && req.query.sort && req.query.sort === 'desc') {
+    if (deals) {
+      deals.sort((a, b) => parseFloat(b.remainingDays) - parseFloat(a.remainingDays));
+    }
+    sortOrder.queryString = `${req.params.page}?fromDays=${fromDays}&toDays=${toDays}`;
+    sortOrder.order = 'descending';
+    sortOrder.image = 'twistie-down';
+  }
+
+  return res.render('reports/MIA-to-be-submitted-report.njk', {
+    pages,
+    conditions: 'without',
+    deals,
+    banks,
     sortOrder,
     primaryNav,
     subNav: 'countdown-indicator',
