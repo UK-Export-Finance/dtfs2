e6ad413f5b8d7199ef4b60b90d0dbcd079efb53f
Andrew Price <andrew@madebymoon.com>
2020-09-15 12:34:14 +0100

    dtfs2-2088 Added MIN maker & checker

--- deal-api/api-tests/v1/deals/deals-status.api-test.js
+++ deal-api/api-tests/v1/deals/deals-status.api-test.js
@@ -502,5 +502,56 @@ describe('/v1/deals/:id/status', () => {
         expect(updatedDeal.body.errorCount).toEqual(updatedDeal.body.errorList.length);
       });
     });
+
+    describe('on approved MIA', () => {
+      describe('when the status changes to `Ready for Checker\'s approval` on approved MIA', () => {
+        it('should add the makers details as MIN maker', async () => {
+          const dealCreatedByMaker = {
+            ...completedDeal,
+            details: {
+              ...completedDeal.details,
+              submissionType: 'Manual Inclusion Application',
+              previousWorkflowStatus: 'approved',
+            },
+          };
+
+          const postResult = await as(aBarclaysMaker).post(dealCreatedByMaker).to('/v1/deals');
+          const createdDeal = postResult.body;
+          const statusUpdate = {
+            status: 'Ready for Checker\'s approval',
+            comments: 'Yay!',
+          };
+
+          const updatedDeal = await as(aBarclaysMaker).put(statusUpdate).to(`/v1/deals/${createdDeal._id}/status`);
+
+          expect(updatedDeal.body.details.makerMIN.username).toEqual(aBarclaysMaker.username);
+        });
+      });
+
+      describe('when the status changes to `Submitted` on approved MIA', () => {
+        it('should add the checkers details as MIN checker', async () => {
+          const dealCreatedByMaker = {
+            ...completedDeal,
+            details: {
+              ...completedDeal.details,
+              maker: aBarclaysMakerChecker,
+              submissionType: 'Manual Inclusion Application',
+              previousWorkflowStatus: 'approved',
+            },
+          };
+
+          const postResult = await as(aBarclaysMaker).post(dealCreatedByMaker).to('/v1/deals');
+          const createdDeal = postResult.body;
+          const statusUpdate = {
+            status: 'Submitted',
+            confirmSubmit: true,
+          };
+
+          const updatedDeal = await as(aBarclaysChecker).put(statusUpdate).to(`/v1/deals/${createdDeal._id}/status`);
+
+          expect(updatedDeal.body.details.checkerMIN.username).toEqual(aBarclaysChecker.username);
+        });
+      });
+    });
   });
 });
