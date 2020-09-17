5f0ba332d4c5aa40c86914c3bd24255184c8e024
Dan <dan.watkinson@gmail.com>
2020-09-17 13:19:25 +0100

    DTFS2-1298 - free text search on deal dashboard; separating out my test data as other unrelated tests were broken by my tinkering with the existing test data..

---
+++ cypress/fixtures/aDealForTestingFreeTextSearch.js
@@ -0,0 +1,28 @@
+module.exports = {
+  details: {
+    bankSupplyContractID: 'abc-1-def',
+    bankSupplyContractName: 'Tibettan submarine acquisition scheme',
+    submissionType: 'Automatic Inclusion Notice',
+    status: 'Draft',
+    owningBank: {
+      id: '956',
+      name: 'Barclays Bank',
+      emails: [
+        'maker4@ukexportfinance.gov.uk',
+        'checker4@ukexportfinance.gov.uk',
+      ],
+    },
+    ukefDealId: 'A-deal-id-FROM_UKEF',
+  },
+  submissionDetails: {
+    status: 'Incomplete',
+    'supplier-name': 'Supplier name 1',
+    'supplier-companies-house-registration-number': '1234567',
+    'indemnifier-name': 'Indemnifier 1 name',
+    'indemnifier-companies-house-registration-number': '7654321',
+    'buyer-name': 'Buyer Name 1',
+  },
+  eligibility: {
+    status: 'Incomplete',
+  },
+}
--- cypress/fixtures/deal-dashboard-data.js
+++ cypress/fixtures/deal-dashboard-data.js
@@ -19,15 +19,9 @@ module.exports = [
           'checker4@ukexportfinance.gov.uk',
         ],
       },
-      ukefDealId: 'A-deal-id-FROM_UKEF',
     },
     submissionDetails: {
       status: 'Incomplete',
-      'supplier-name': 'Supplier name 1',
-      'supplier-companies-house-registration-number': '1234567',
-      'indemnifier-name': 'Indemnifier 1 name',
-      'indemnifier-companies-house-registration-number': '7654321',
-      'buyer-name': 'Buyer Name 1',
     },
     eligibility: {
       status: 'Incomplete',
