eddf7530ccec6fa6452cdfce1a0952e0a3f92f45
Dan <dan.watkinson@gmail.com>
2020-09-21 15:17:37 +0100

    DTFS2-2197 - Destination of Goods and Services should be mandatory.

--- deal-api/src/v1/validation/submission-details-rules/buyer-destination-of-goods-and-services-is-required.js
+++ deal-api/src/v1/validation/submission-details-rules/buyer-destination-of-goods-and-services-is-required.js
@@ -4,8 +4,9 @@ const { hasValue } = require('../../../utils/string');
 module.exports = (submissionDetails, errorList) => {
   const newErrorList = { ...errorList };
 
-  if (!submissionDetails['destinationOfGoodsAndServices'] || !hasValue(submissionDetails['destinationOfGoodsAndServices'].id)) {
-    newErrorList['destinationOfGoodsAndServices'] = {
+  if (!submissionDetails.destinationOfGoodsAndServices
+      || !hasValue(submissionDetails.destinationOfGoodsAndServices.code)) {
+    newErrorList.destinationOfGoodsAndServices = {
       order: orderNumber(newErrorList),
       text: 'Destination of Goods and Services is required',
     };
