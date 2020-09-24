4378a4d8964f815447a6a5c6c3cbf91c02bcf054
ttbarnes <tony@tonybarnes.me>
2020-09-24 12:56:33 +0100

    DTFS2-1425 - if maker `confirms facility start date` by selecting `no, keep existing date`, update the session

--- cypress/integration/journeys/maker/facilities/MIA-accepted-deal-status-maker-cannot-resubmit-until-confirmed-all-issued-facilities-cover-start-date/MIA-deal-with-accepted-status-issued-facilities.js
+++ cypress/integration/journeys/maker/facilities/MIA-accepted-deal-status-maker-cannot-resubmit-until-confirmed-all-issued-facilities-cover-start-date/MIA-deal-with-accepted-status-issued-facilities.js
@@ -267,12 +267,50 @@ const deal = {
           "id": "GBP"
         }
       },
+      {
+        "_id": "1000064",
+        "createdDate": moment().utc().valueOf(),
+        "bondIssuer": "",
+        "bondType": "Performance bond",
+        "bondStage": "Issued",
+        "ukefGuaranteeInMonths": "12",
+        "bondBeneficiary": "",
+        "guaranteeFeePayableByBank": "18.0000",
+        "lastEdited": moment().utc().valueOf(),
+        "facilityValue": "1234.00",
+        "currencySameAsSupplyContractCurrency": "true",
+        "riskMarginFee": "20",
+        "coveredPercentage": "40",
+        "minimumRiskMarginFee": "",
+        "ukefExposure": "493.60",
+        "feeType": "At maturity",
+        "dayCountBasis": "365",
+        "currency": {
+          "text": "GBP - UK Sterling",
+          "id": "GBP"
+        },
+        "issuedDate-day": "25",
+        "issuedDate-month": "08",
+        "issuedDate-year": "2020",
+        "coverEndDate-day": "24",
+        "coverEndDate-month": "09",
+        "coverEndDate-year": "2020",
+        "uniqueIdentificationNumber": "1234",
+        "issueFacilityDetailsStarted": true,
+        "uniqueIdentificationNumberRequiredForIssuance": true,
+        "requestedCoverStartDate": moment().utc().valueOf(),
+        "issuedDate": moment().utc().valueOf(),
+        "issueFacilityDetailsProvided": true,
+        "status": "Submitted",
+        "previousFacilityStage": "Unissued",
+        "issueFacilityDetailsSubmitted": true
+      },
     ]
   },
   "loanTransactions": {
     "items": [
       {
-        "_id": "1000063",
+        "_id": "1000073",
         "createdDate": moment().utc().valueOf(),
         "facilityStage": "Unconditional",
         "ukefGuaranteeInMonths": "12",
@@ -304,8 +342,7 @@ const deal = {
         "issueFacilityDetailsSubmitted": true
       },
       {
-        "newOne": true,
-        "_id": "1000064",
+        "_id": "1000074",
         "createdDate": moment().utc().valueOf(),
         "facilityStage": "Unconditional",
         "ukefGuaranteeInMonths": "12",
@@ -330,7 +367,7 @@ const deal = {
         "requestedCoverStartDate": moment().utc().valueOf(),
       },
       {
-        "_id": "1000065",
+        "_id": "1000075",
         "createdDate": moment().utc().valueOf(),
         "facilityStage": "Conditional",
         "ukefGuaranteeInMonths": "12",
@@ -349,6 +386,38 @@ const deal = {
           "text": "GBP - UK Sterling",
           "id": "GBP"
         }
+      },
+      {
+        "_id": "1000076",
+        "createdDate": moment().utc().valueOf(),
+        "facilityStage": "Unconditional",
+        "ukefGuaranteeInMonths": "12",
+        "bankReferenceNumber": "5678",
+        "guaranteeFeePayableByBank": "27.0000",
+        "lastEdited": moment().utc().valueOf(),
+        "facilityValue": "1234.00",
+        "currencySameAsSupplyContractCurrency": "true",
+        "interestMarginFee": "30",
+        "coveredPercentage": "20",
+        "minimumQuarterlyFee": "",
+        "ukefExposure": "246.80",
+        "premiumType": "At maturity",
+        "dayCountBasis": "365",
+        "issuedDate-day": "25",
+        "issuedDate-month": "08",
+        "issuedDate-year": "2020",
+        "coverEndDate-day": "24",
+        "coverEndDate-month": "09",
+        "coverEndDate-year": "2020",
+        "disbursementAmount": "1,234.00",
+        "issueFacilityDetailsStarted": true,
+        "bankReferenceNumberRequiredForIssuance": true,
+        "requestedCoverStartDate": moment().utc().valueOf(),
+        "issuedDate": moment().utc().valueOf(),
+        "issueFacilityDetailsProvided": true,
+        "status": "Submitted",
+        "previousFacilityStage": "Conditional",
+        "issueFacilityDetailsSubmitted": true
       },
     ]
   },
