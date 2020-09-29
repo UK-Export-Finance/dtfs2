3306e669fd8e9dec5de71b94f969e66109e64e60
ttbarnes <tony@tonybarnes.me>
2020-09-29 10:06:00 +0100

    DTFS2-2064 - `skip to main content` e2e test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,9 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-require('cypress-xpath')
+require('cypress-xpath');
+require('cypress-plugin-tab');
