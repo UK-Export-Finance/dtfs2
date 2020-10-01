0f4b570779294c519b6b7f4b9ecce35304763908
ttbarnes <tony@tonybarnes.me>
2020-10-01 15:48:41 +0100

    DTFS2-2070 - About Supply Contract - Preview validation errors linking to other pages

--- portal/server/routes/contract/about/pageSpecificValidationErrors/index.js
+++ portal/server/routes/contract/about/pageSpecificValidationErrors/index.js
@@ -9,3 +9,31 @@ export const buyerValidationErrors = (validationErrors, submittedValues) =>
 
 export const financialPageValidationErrors = (validationErrors, submittedValues) =>
   pageSpecificValidationErrors(validationErrors, FIELDS.FINANCIAL, submittedValues);
+
+// TODO: make reusable
+// preview pages display all required field validation errors.
+// because each field/validation error originates from  a different page,
+// for each validation error, we need to add a hrefRoot to the page in question.
+export const aboutSupplyContractPreviewValidationErrors = (validationErrors, dealId) => {
+  const mappedValidationErrors = validationErrors;
+
+  if (mappedValidationErrors && mappedValidationErrors.errorList) {
+    Object.keys(mappedValidationErrors.errorList).forEach((fieldName) => {
+      if (FIELDS.SUPPLIER.REQUIRED_FIELDS.includes(fieldName)
+        || FIELDS.SUPPLIER.CONDITIONALLY_REQUIRED_FIELDS.includes(fieldName)) {
+        mappedValidationErrors.errorList[fieldName].hrefRoot = `/contract/${dealId}/about/supplier`;
+      }
+
+      if (FIELDS.BUYER.REQUIRED_FIELDS.includes(fieldName)
+        || FIELDS.BUYER.CONDITIONALLY_REQUIRED_FIELDS.includes(fieldName)) {
+        mappedValidationErrors.errorList[fieldName].hrefRoot = `/contract/${dealId}/about/buyer`;
+      }
+
+      if (FIELDS.FINANCIAL.REQUIRED_FIELDS.includes(fieldName)) {
+        mappedValidationErrors.errorList[fieldName].hrefRoot = `/contract/${dealId}/about/financial`;
+      }
+    });
+  }
+
+  return mappedValidationErrors;
+};
