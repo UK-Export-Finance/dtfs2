30ce12885b91aaa8aa0fff771d63833e93ae22bc
ttbarnes <tony@tonybarnes.me>
2020-10-01 15:01:46 +0100

    linting

--- portal/server/routes/contract/about/pageSpecificValidationErrors/index.js
+++ portal/server/routes/contract/about/pageSpecificValidationErrors/index.js
@@ -1,7 +1,7 @@
 import { pageSpecificValidationErrors } from '../../../../helpers';
 import FIELDS from './pageFields';
 
-export const supplierValidationErrors = (validationErrors, submittedValues) => 
+export const supplierValidationErrors = (validationErrors, submittedValues) =>
   pageSpecificValidationErrors(validationErrors, FIELDS.SUPPLIER, submittedValues);
 
 export const buyerValidationErrors = (validationErrors, submittedValues) =>
