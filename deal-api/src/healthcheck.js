ef8205cbceffcf5f658a0617338dbceac765995e
David Carboni <david@carboni.io>
2020-10-08 18:53:39 +0100

    Added a Notify ping to healthcheck.

--- deal-api/src/healthcheck.js
+++ deal-api/src/healthcheck.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const { NotifyClient } = require('notifications-node-client')
 const util = require('util');
 
 const router = express.Router();
 const GITHUB_SHA = process.env.GITHUB_SHA || 'undefined';
-const { MONGODB_URI, REDIS_URI, MONGO_INITDB_DATABASE } = process.env;
+const { MONGODB_URI } = process.env;
+const MONGO_INITDB_DATABASE = process.env.MONGO_INITDB_DATABASE || 'test';
 const STORAGE_ACCOUNT = process.env.AZURE_PORTAL_STORAGE_ACCOUNT;
 // const STORAGE_ACCESS_KEY = process.env.AZURE_PORTAL_STORAGE_ACCESS_KEY;
 
@@ -16,36 +18,37 @@ async function pingMongo() {
   let client;
   try {
     client = await MongoClient.connect(MONGODB_URI);
-    console.log('Querying mongo');
     return await client.db(MONGO_INITDB_DATABASE).listCollections().toArray();
   } catch (error) {
-    console.log('Errrrrr');
     return util.inspect(error);
   } finally {
     if (client) {
-      console.log('Closing client');
       client.close();
     }
   }
-}
-
-async function pingRedis() {
-  return REDIS_URI;
 }
 
 async function pingStorage() {
   return STORAGE_ACCOUNT;
 }
 
+async function pingNotify() {
+  const notifyClient = new NotifyClient(process.env.GOV_NOTIFY_API_KEY)
+  return await notifyClient
+    .getAllTemplates('email')
+    .then((response) => { return response.statusCode })
+    .catch((err) => { return util.inspect(error); })
+}
+
 router.get('/healthcheck', (req, res) => {
   const mongo = pingMongo();
-  const redis = pingRedis();
+  const notify = pingNotify();
   const storage = pingStorage();
-  Promise.all([mongo, redis, storage]).then((values) => {
+  Promise.all([mongo, notify, storage]).then((values) => {
     res.status(200).json({
       commit_hash: GITHUB_SHA,
       mongo: values[0],
-      redis: values[1],
+      notify: values[1],
       storage: values[2],
     });
   });
