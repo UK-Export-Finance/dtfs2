17bc3c306fe8f5cf0a56bc50800e40171038d3d0
Andrew Price <andrew@madebymoon.com>
2020-10-09 11:05:08 +0100

    Added items to ui healthcheck

--- portal/server/healthcheck.js
+++ portal/server/healthcheck.js
@@ -1,11 +1,37 @@
+import companiesHouseAPI from './companies-house-api';
+
 const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
 
 const healthcheck = express.Router();
 const GITHUB_SHA = process.env.GITHUB_SHA || 'undefined';
+const dealApiUrl = process.env.DEAL_API_URL;
 
-healthcheck.get('/healthcheck', (req, res) => {
+const apiHealthCheck = async () => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `${dealApiUrl}/healthcheck`,
+    });
+    return response.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+const companiesHouseHealthCheck = async () => {
+  const chCheck = await companiesHouseAPI.getByRegistrationNumber('00014259');
+  return chCheck ? 'ok' : 'not ok';
+};
+
+healthcheck.get('/healthcheck', async (req, res) => {
   res.status(200).json({
-    GITHUB_SHA,
+    ui: {
+      commit_hash: GITHUB_SHA,
+      companies_house: await companiesHouseHealthCheck(),
+    },
+    api: await apiHealthCheck(),
   });
 });
 
