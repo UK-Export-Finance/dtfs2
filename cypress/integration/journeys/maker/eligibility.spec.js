20da997eb0e9317bbf6ebe621a538e049680ff8b
ttbarnes <tony@tonybarnes.me>
2020-10-19 10:45:44 +0100

    DTFS2-2200 - eligibility `Completed` status e2e test

--- cypress/integration/journeys/maker/eligibility.spec.js
+++ cypress/integration/journeys/maker/eligibility.spec.js
@@ -2,10 +2,12 @@ const {
   contract,
   eligibilityCriteria,
   eligibilityDocumentation,
+  eligibilityPreview,
 } = require('../../pages');
-
+const partials = require('../../partials');
 const mockUsers = require('../../../fixtures/mockUsers');
-const MAKER_LOGIN = mockUsers.find( user=> (user.roles.includes('maker')) );
+
+const MAKER_LOGIN = mockUsers.find((user) => (user.roles.includes('maker')));
 
 context('Eligibility', () => {
   beforeEach(() => {
@@ -20,20 +22,92 @@ context('Eligibility', () => {
       bankDealId: 'someDealId',
       bankDealName: 'someDealName',
     });
-
-    contract.eligibilityCriteriaLink().click();
   });
 
   describe('when all fields provided', () => {
-    it('should redirect to the deal page and display `Completed` status', () => {
+    it('should update task list header items to `Completed` statuses, redirect to the deal page and display `Completed` status', () => {
+      //---------------------------------------------------------------
+      // complete and submit first eligibility form/page - Criteria
+      //---------------------------------------------------------------
+      contract.eligibilityCriteriaLink().click();
       eligibilityCriteria.eligibilityCriteriaItemsRadioButtons.trueInput().click({ multiple: true });
       eligibilityCriteria.nextPageButton().click();
-      eligibilityDocumentation.questionnaireFileInput().attachFile('questionnaire.pdf');
 
-      eligibilityDocumentation.saveGoBackButton().click();
+      cy.url().should('include', '/supporting-documentation');
+
+      //---------------------------------------------------------------
+      // check that first eligibility form/page - Criteria has `completed` status
+      //---------------------------------------------------------------
+      partials.taskListHeader.itemStatus('eligibility-criteria').invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+      //---------------------------------------------------------------
+      // complete and submit second eligibility form/page - Documentation
+      //---------------------------------------------------------------
+      eligibilityDocumentation.questionnaireFileInput().attachFile('questionnaire.pdf');
+      eligibilityDocumentation.saveButton().click();
+
+      cy.url().should('include', '/check-your-answers');
+
+      //---------------------------------------------------------------
+      // check that all eligibility form/pages have `completed` status
+      //---------------------------------------------------------------
+      partials.taskListHeader.itemStatus('supporting-documentation').invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+      partials.taskListHeader.itemStatus('eligibility-criteria').invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+
+      //---------------------------------------------------------------
+      // go back to deal page, check Eligibility status is `completed`
+      //---------------------------------------------------------------
+      eligibilityPreview.saveGoBackButton().click();
+
       cy.url().should('not.include', '/eligibility');
 
       contract.eligibilityStatus().invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+      //---------------------------------------------------------------
+      // go back to first Eligibility form/page
+      // navigate through the forms via task list header
+      // check that all `completed` statuses are retained
+      //---------------------------------------------------------------
+
+      // go back to Eligibility pages, check completed statuses
+      contract.eligibilityCriteriaLink().click();
+      cy.url().should('include', '/criteria');
+
+      partials.taskListHeader.itemStatus('supporting-documentation').invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+      partials.taskListHeader.itemStatus('eligibility-criteria').invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+      partials.taskListHeader.itemLink('supporting-documentation').click();
+
+      partials.taskListHeader.itemStatus('supporting-documentation').invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+      partials.taskListHeader.itemStatus('eligibility-criteria').invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+      partials.taskListHeader.checkYourAnswersLink().click();
+
+      partials.taskListHeader.itemStatus('supporting-documentation').invoke('text').then((text) => {
+        expect(text.trim()).equal('Completed');
+      });
+
+      partials.taskListHeader.itemStatus('eligibility-criteria').invoke('text').then((text) => {
         expect(text.trim()).equal('Completed');
       });
     });
