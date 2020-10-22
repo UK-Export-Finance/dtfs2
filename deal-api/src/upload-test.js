c5943f7b4441e4a230a9fd572f6a503de3fd7ddc
Andrew Price <andrew@madebymoon.com>
2020-10-22 16:43:08 +0100

    Removed DNS test from upload-test

--- deal-api/src/upload-test.js
+++ deal-api/src/upload-test.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { Resolver } = require('dns').promises;
 
 const router = express.Router();
 
@@ -7,8 +6,6 @@ const fileshare = require('./drivers/fileshare');
 
 const config = fileshare.getConfig('workflow');
 let workflowFolder;
-const resolver = new Resolver();
-
 
 const doUpload = async () => {
   workflowFolder = `v2-upload-test/${Date.now()}`; // eslint-disable-line no-underscore-dangle
@@ -33,25 +30,6 @@ router.get('/upload-test', (req, res) => {
       config,
       upload: values[0],
     });
-  });
-});
-
-const lookup = async (hostname) => {
-  const ns = await resolver.resolveNs(hostname).catch((err) => ({ err }));
-  const ip = await resolver.resolve(hostname).catch((err) => ({ err }));
-  return {
-    ip,
-    ns,
-  };
-};
-
-router.get('/dns-test', async (req, res) => {
-  res.status(200).json({
-    dnsServers: resolver.getServers(),
-    bbc: await lookup('bbc.co.uk'),
-    rrstagingmedia: await lookup('rrstagingmedia.file.core.windows.net'),
-    tfsandrew: await lookup('tfsandrew.file.core.windows.net'),
-    tfsandrew_private: await lookup('tfsandrew.privatelink.file.core.windows.net'),
   });
 });
 
