0ab9321ed729d48443cda22935a9f853b3647c4d
Andrew Price <andrew@madebymoon.com>
2020-10-23 17:02:04 +0100

    dtfs2-2522 Removed facility currency info from preview if same as deal

--- cypress/integration/pages/contractSubmissionDetails.js
+++ cypress/integration/pages/contractSubmissionDetails.js
@@ -4,6 +4,12 @@ const page = {
   editLinkConfirmEligibility: (dealId) => cy.get(`[data-cy="edit-eligibility-link-/contract/${dealId}/eligibility/criteria"]`),
   editLinkBond: (dealId, bondId) => cy.get(`[data-cy="edit-bond-${bondId}-link-/contract/${dealId}/bond/${bondId}/details"]`),
   editLinkLoan: (dealId, loanId) => cy.get(`[data-cy="edit-loan-${loanId}-link-/contract/${dealId}/loan/${loanId}/guarantee-details"]`),
+  currencyInfoFacility: (type, facilityId) => ({
+    sameAsDealCurrency: () => cy.get(`[data-cy="${type}-${facilityId}"]`).find('[data-cy="currency-same-as-supply-contract-currency"]'),
+    currency: () => cy.get(`[data-cy="${type}-${facilityId}"]`).find('[data-cy="currency"]'),
+    conversionRate: () => cy.get(`[data-cy="${type}-${facilityId}"]`).find('[data-cy="conversion-rate"]'),
+  }
+  ),
 };
 
 module.exports = page;
