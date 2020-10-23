77f8f916a0577767eaa265af18bf0cbdc3c634da
Andrew Price <andrew@madebymoon.com>
2020-10-23 15:28:39 +0100

    dtfs2-1991 Data migration - modified MC change date

--- utils/data-migration/maps/map-mandatory-criteria.js
+++ utils/data-migration/maps/map-mandatory-criteria.js
@@ -2,7 +2,7 @@ const MANDATORY_CRITERIA = require('../../mock-data-loader/mocks/mandatoryCriter
 
 const mapMandatoryCriteria = (v1Deal) => {
   const submissionDate = v1Deal.Deal_information.Extra_fields.Submission_date_AIN_and_MIA;
-  if (submissionDate && submissionDate > '2020-01-31') {
+  if (submissionDate && submissionDate > '2020-02') {
     return MANDATORY_CRITERIA;
   }
   return [];
