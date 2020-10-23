32642962c3a1327f978f30ab3cf0021c204398d9
ttbarnes <tony@tonybarnes.me>
2020-10-23 15:18:53 +0100

    bump webpack

--- portal/webpack.prod.config.js
+++ portal/webpack.prod.config.js
@@ -34,7 +34,7 @@ module.exports = {
     minimize: true,
     minimizer: [
       new TerserPlugin(),
-      new OptimizeCSSAssetsPlugin({})
+      new OptimizeCSSAssetsPlugin({}),
     ],
   },
   module: {
