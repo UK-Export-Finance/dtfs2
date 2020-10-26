ce3bd801de8b0a9fc96692ff7a097336116ce12d
Andrew Price <andrew@madebymoon.com>
2020-10-26 18:36:25 +0000

    dtfs2-2538 Data migration - disbale validation for deals that have been migrated.

--- deal-api/src/v1/validation/is-validation-required.js
+++ deal-api/src/v1/validation/is-validation-required.js
@@ -1,9 +1,1 @@
-const CONSTANTS = require('../../constants');
-
-const dealStatusToValidateOn = [
-  CONSTANTS.DEAL.STATUS.DRAFT,
-  CONSTANTS.DEAL.STATUS.INPUT_REQUIRED,
-  CONSTANTS.DEAL.STATUS.READY_FOR_APPROVAL,
-];
-
-module.exports = (deal) => dealStatusToValidateOn.includes(deal.details.status);
+module.exports = (deal) => !deal.dataMigrationInfo;
