572b453c9a0ea235807ba5bc24806e8349fb6271
Andrew Price <andrew@madebymoon.com>
2020-10-27 14:16:58 +0000

    Mocked calls to Notify service

---
+++ deal-api/__mocks__/notifications-node-client.js
@@ -0,0 +1,9 @@
+
+function NotifyClient() {
+  console.log('MOCKING notifications-node-client::NotifyClient');
+  return {
+    sendEmail: () => Promise.resolve(true),
+  };
+}
+
+module.exports.NotifyClient = NotifyClient;
