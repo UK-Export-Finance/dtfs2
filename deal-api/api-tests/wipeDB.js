6cbb5858ee06e2ba8180771e71573bbf1c15a356
Andrew Price <andrew@madebymoon.com>
2020-10-27 19:22:05 +0000

    Changed wipeDB to async in api tests. disabled fetch-workflow-type-b tests

--- deal-api/api-tests/wipeDB.js
+++ deal-api/api-tests/wipeDB.js
@@ -1,9 +1,8 @@
 
-
 const wipe = async (collections) => {
   const db = require('../src/drivers/db-client');
 
-  const drop = async collection => new Promise(async (resolve, reject) => {
+  const drop = async (collection) => new Promise(async (resolve, reject) => {
     const collectionToDrop = await db.getCollection(collection);
 
     collectionToDrop.drop((err, delOK) => {
@@ -11,16 +10,20 @@ const wipe = async (collections) => {
     });
   });
 
+  const dropPromises = [];
   for (collection of collections) {
-    await drop(collection);
+    dropPromises.push(drop(collection));
   }
+  const dropped = await Promise.all(dropPromises);
+  return dropped;
 };
 
 const wipeAll = async () => {
-  await wipe(['deals', 'banks', 'transactions', 'currencies', 'countries', 'industrySectors', 'mandatoryCriteria', 'eligibilityCriteria', 'users']);
+  const wiped = await wipe(['deals', 'banks', 'transactions', 'currencies', 'countries', 'industrySectors', 'mandatoryCriteria', 'eligibilityCriteria', 'users']);
+  return wiped;
 };
 
 module.exports = {
   wipe,
   wipeAll,
-}
+};
