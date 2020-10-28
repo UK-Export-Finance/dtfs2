3c9f87a039f74eebca5799c2a76cfe88752df5c5
ttbarnes <tony@tonybarnes.me>
2020-10-28 12:37:20 +0000

    DTFS2-2534 - facility change cover start date controllers using own validation rules instead of issueFacility validation rules

---
+++ deal-api/src/v1/validation/facility-change-cover-start-date-rules/index.js
@@ -0,0 +1,18 @@
+const requestedCoverStartDateRules = require('../fields/issue-facility/requested-cover-start-date');
+const coverDatesRules = require('../fields/cover-dates');
+
+module.exports = (
+  submittedValues,
+  deal,
+) => {
+  let errorList = {};
+
+  errorList = requestedCoverStartDateRules(
+    submittedValues,
+    errorList,
+    deal,
+  );
+  errorList = coverDatesRules(submittedValues, errorList);
+
+  return errorList;
+};
