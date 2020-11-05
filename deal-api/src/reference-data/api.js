01f3cb5f8b271269d517056d503f11b8daeb6c84
Andrew Price <andrew@madebymoon.com>
2020-11-05 17:35:43 +0000

    dtfs2-2483 integrate reference-data-proxy with deal-api

---
+++ deal-api/src/reference-data/api.js
@@ -0,0 +1,33 @@
+
+const axios = require('axios');
+
+const referenceProxyUrl = process.env.REFERENCE_DATA_PROXY_URL;
+
+const getCountries = async () => {
+  const response = await axios({
+    method: 'get',
+    url: `${referenceProxyUrl}/countries`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return response.data && response.data.countries;
+};
+
+const getCountry = async (code) => {
+  const response = await axios({
+    method: 'get',
+    url: `${referenceProxyUrl}/countries/${code}`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).catch((err) => err);
+
+  return response.data;
+};
+
+module.exports = {
+  getCountries,
+  getCountry,
+};
