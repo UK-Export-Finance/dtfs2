ea679a234f08eff2dafd393e8bdddf202130fb88
Andrew Price <andrew@madebymoon.com>
2020-11-06 16:15:16 +0000

    dtfs2-2239 Dont validate submitted deals

--- deal-api/src/v1/validation/is-validation-required.js
+++ deal-api/src/v1/validation/is-validation-required.js
@@ -1,1 +1,15 @@
-module.exports = (deal) => !deal.dataMigrationInfo;
+const CONSTANTS = require('../../constants');
+
+const statusToValidate = [
+  CONSTANTS.DEAL.STATUS.DRAFT,
+  CONSTANTS.DEAL.STATUS.INPUT_REQUIRED,
+  CONSTANTS.DEAL.STATUS.READY_FOR_APPROVAL,
+  CONSTANTS.DEAL.STATUS.ABANDONED,
+];
+
+module.exports = (deal) => {
+  if (deal.dataMigrationInfo) {
+    return false;
+  }
+  return statusToValidate.includes(deal.details.status);
+};
