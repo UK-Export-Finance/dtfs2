a01b80d2f8f885f3781b0c2cc3c191cc41782e16
Andrew Price <andrew@madebymoon.com>
2020-11-09 12:54:14 +0000

    db driver option change to limit numberOfTries=0 to align with azure db setttings

--- deal-api/src/drivers/db-client.js
+++ deal-api/src/drivers/db-client.js
@@ -13,6 +13,7 @@ const dbConnect = async () => {
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      numberOfRetries: 0, // this can change if a mongo replica-set is setup
     },
   );
   connection = await client.db(dbName);
