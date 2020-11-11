45db9e1c2b1a1e614dad7377f76f6aa13e2c8941
Andrew Price <andrew@madebymoon.com>
2020-11-11 12:10:45 +0000

    dtfs2-2494 Added industry-sectors to reference-data-proxy

---
+++ reference-data-proxy/src/v1/controllers/industry-sectors.controller.js
@@ -0,0 +1,22 @@
+const utils = require('../../utils/array');
+const INDUSTRY_SECTORS = require('../../reference-data/industry-sectors');
+
+const sortIndustrySectors = (industrySectors) => utils.sortArrayAlphabetically(industrySectors, 'name').map((sector) => ({
+  ...sector,
+  classes: utils.sortArrayAlphabetically(sector.classes, 'name'),
+}));
+
+const findOneIndustrySector = (findCode) => INDUSTRY_SECTORS.find(({ code }) => code === findCode);
+
+exports.findAll = (req, res) => (
+  res.status(200).send({
+    count: INDUSTRY_SECTORS.length,
+    industrySectors: sortIndustrySectors(INDUSTRY_SECTORS),
+  })
+);
+
+exports.findOne = (req, res) => {
+  const industrySector = findOneIndustrySector(req.params.code);
+  const status = industrySector ? '200' : '401';
+  res.status(status).send(industrySector);
+};
