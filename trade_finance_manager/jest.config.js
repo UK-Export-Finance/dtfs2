63eb32415cbbfc8cef4a8c628f8c3c3b4a53d08e
SimonTheDeveloper <mail@simon-neumann.com>
2020-11-11 11:08:04 +0000

    Docker tfm

---
+++ trade_finance_manager/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  collectCoverageFrom: [
+    'server/routes/**/*.{js,}',
+    'server/helpers/**/*.{js,}',
+    'scripts/**/*.{js,}',
+  ],
+  coverageDirectory: 'generated_reports/coverage/unit',
+  testMatch: ['**/*.test.js', '**/*.component-test.js'],
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss)$': 'babel-jest',
+  },
+};
