3e18104d166546b8c9c17c385f30dcd255750442
Andrew Price <andrew@madebymoon.com>
2020-11-13 12:47:40 +0000

    dtfs2-2493 debug for reference-data-proxy microservice api call

--- deal-api/src/reference-data/currencies.js
+++ deal-api/src/reference-data/currencies.js
@@ -4,18 +4,22 @@ const axios = require('axios');
 const referenceProxyUrl = process.env.REFERENCE_DATA_PROXY_URL;
 
 const getCurrencies = async () => {
+  console.log(`getCurrencies: ${referenceProxyUrl}/currencies`);
   const response = await axios({
     method: 'get',
     url: `${referenceProxyUrl}/currencies`,
     headers: {
       'Content-Type': 'application/json',
     },
-  });
+  }).catch((err) => err);
+
+  console.log('getCurrenciesResponse', response);
 
   return response.data && response.data.currencies;
 };
 
 const getCurrency = async (id) => {
+  console.log(`getCurrency: ${referenceProxyUrl}/currencies/${id}`);
   const response = await axios({
     method: 'get',
     url: `${referenceProxyUrl}/currencies/${id}`,
@@ -23,6 +27,7 @@ const getCurrency = async (id) => {
       'Content-Type': 'application/json',
     },
   }).catch((err) => err);
+  console.log(`getCurrency ${id}`, response);
 
   return response.data;
 };
