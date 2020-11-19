c04dee1bcd2a71e4bc28c85b3c39bbbc63ec6880
ttbarnes <tony@tonybarnes.me>
2020-11-19 11:40:49 +0000

    DTFS2-2519 - tfm api init graphql, deals query

---
+++ trade-finance-manager-api/src/graphql/schemas/index.js
@@ -0,0 +1,46 @@
+const typeDefs = `
+
+type OwningBank {
+  name: String
+}
+
+type Maker {
+  username: String
+  firstname: String
+  surname: String
+}
+
+type Checker {
+  username: String
+  firstname: String
+  surname: String
+}
+
+type DealDetails {
+  status: String
+  bankSupplyContractID: String
+  bankSupplyContractName: String
+  ukefDealId: String
+  submissionType: String
+  maker: Maker
+  checker: Checker
+  checkerMIN: Checker
+  dateOfLastAction: String
+  submissionDate: String
+  approvalDate: String
+  created: String
+  workflowStatus: String
+  owningBank: OwningBank
+}
+
+type Deal {
+  _id: String!
+  details: DealDetails
+}
+
+type Query {
+  deal: Deal
+}
+`;
+
+module.exports = typeDefs;
