d2dab5ebd4e47444038db7d70bef36f4fae2610c
ttbarnes <tony@tonybarnes.me>
2020-11-20 17:00:23 +0000

    tfm linting

--- trade-finance-manager/server/api.js
+++ trade-finance-manager/server/api.js
@@ -1,20 +1,13 @@
-import axios from 'axios';
-import FormData from 'form-data';
 import apollo from './graphql/apollo';
 
-import { dealQuery } from './graphql/queries';
-
-require('dotenv').config();
-
-const urlRoot = process.env.TRADE_FINANCE_MANAGER_API_URL;
+import dealQuery from './graphql/queries';
 
 const getDeal = async (id) => {
-  console.log('GET DEAL ID ', id);
   const response = await apollo('GET', dealQuery, { id });
 
   return response.data.deal;
 };
 
 export default {
-  getDeal
+  getDeal,
 };
--- trade-finance-manager/server/routes/login.js
+++ trade-finance-manager/server/routes/login.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get('/', (req, res) => res.render('login.njk'));
 
 router.post('/', (req, res) =>
-  res.redirect('/case/deal/1000581'));
+  res.redirect('/case/deal/1000123'));
 
 export default router;
