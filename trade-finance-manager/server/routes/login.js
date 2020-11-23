2ab6cd7a8d780895ccbecd57dddc36e7cbcef4cb
ttbarnes <tony@tonybarnes.me>
2020-11-23 15:51:32 +0000

    DTFS2-2519 - tfm - remove hardcoded token, use temp deal id from mocks

--- trade-finance-manager/server/routes/login.js
+++ trade-finance-manager/server/routes/login.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get('/', (req, res) => res.render('login.njk'));
 
 router.post('/', (req, res) =>
-  res.redirect('/case/deal/1000123'));
+  res.redirect('/case/deal/1000677'));
 
 export default router;
