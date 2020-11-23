6086f62fc040dac10baa4ac656675ad50d21c53d
ttbarnes <tony@tonybarnes.me>
2020-11-23 15:56:18 +0000

    fix tfm rebase issue

--- trade-finance-manager/server/controllers/login/index.js
+++ trade-finance-manager/server/controllers/login/index.js
@@ -1,6 +1,6 @@
 const getLogin = (req, res) => res.render('login.njk');
 
-const postLogin = (req, res) => res.redirect('/case/deal/1000123');
+const postLogin = (req, res) => res.redirect('/case/deal/1000677');
 
 export default {
   getLogin,
