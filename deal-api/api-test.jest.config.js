19b307d977bc8fa719c4ee9d393413faa710925b
ttbarnes <tony@tonybarnes.me>
2020-11-24 16:21:02 +0000

    DTFS2-2669 - bond issue facility api route - init unit tests

--- deal-api/api-test.jest.config.js
+++ deal-api/api-test.jest.config.js
@@ -3,6 +3,7 @@ const commonSettings = require('./api-test-common.jest.config');
 module.exports = {
   collectCoverageFrom: ['src/**/*.{js,}'],
   coverageDirectory: 'generated_reports/coverage/api-test',
-  testMatch: ['**/*.api-test.js'],
+  // testMatch: ['**/*.api-test.js'],
+  testMatch: ['**/bond-issue-facility.api-test.js'],
   ...commonSettings,
 };
