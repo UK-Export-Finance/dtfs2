bdacf641e969f5505bb02014a76c43d3b2b85e4a
Andrew Price <andrew@madebymoon.com>
2020-11-26 15:51:39 +0000

    DTFS2-2491 Added TFM-API api test config

---
+++ trade-finance-manager-api/api-test.jest.config.js
@@ -0,0 +1,8 @@
+const commonSettings = require('./api-test-common.jest.config');
+
+module.exports = {
+  collectCoverageFrom: ['src/**/*.{js,}'],
+  coverageDirectory: 'generated_reports/coverage/api-test',
+  testMatch: ['**/*.api-test.js'],
+  ...commonSettings,
+};
