8c4ffbc0bedfcde7c031ba230d2f8ac937c90227
SimonTheDeveloper <mail@simon-neumann.com>
2020-12-06 19:06:11 +0000

    deal page

--- trade-finance-manager-ui/templates/case/_macros/case-summary.component-test.js
+++ trade-finance-manager-ui/templates/case/_macros/case-summary.component-test.js
@@ -9,10 +9,18 @@ describe(component, () => {
     _id: '12345678',
     details: {
       submissionType: 'Automatic Inclusion Notice',
+      submissionDate: '1597067095109',
+      owningBank: {
+        name: 'Lloyds'
+      }
     },
     submissionDetails: {
       supplierName: 'The Supplier name',
       buyerName: 'The Buyer name',
+      destinationCountry: 'USA',
+      supplyContractDescription: 'Lore ipsum...',
+      supplyContractCurrency: 'USD',
+      supplyContractValue: '5,000,000'
     },
   };
 
@@ -28,19 +36,31 @@ describe(component, () => {
     wrapper.expectText('[data-cy="buyer-name"]').toRead(params.submissionDetails.buyerName);
   });
 
+  it('should render destination country', () => {
+    wrapper.expectText('[data-cy="destination-country"]').toRead(params.submissionDetails.destinationCountry);
+  });
+
+  it('should render export description', () => {
+    wrapper.expectText('[data-cy="export-description"]').toRead(params.submissionDetails.supplyContractDescription);
+  });
+
+  it('should render contract value', () => {
+    wrapper.expectText('[data-cy="contract-value"]').toRead(params.submissionDetails.supplyContractCurrency + ' ' + params.submissionDetails.supplyContractValue);
+  });
+
+
   it('should render submission type', () => {
     wrapper.expectText('[data-cy="submission-type"]').toRead(params.details.submissionType);
   });
 
-  it('should render correct exporter name', () => {
-    pages.caseDealPage.exporterName().should('have.text', 'Supplier name');
+  it('should render submission date', () => {
+    wrapper.expectText('[data-cy="submission-date"]').toRead(params.details.submissionDate);
   });
 
-  it('should render correct buyer name', () => {
-    pages.caseDealPage.buyerName().should('have.text', 'Buyer name');
+  it('should render  bank name', () => {
+    wrapper.expectText('[data-cy="bank-name"]').toRead(params.details.owningBank.name);
   });
 
-  it('should render correct bank name', () => {
-    pages.caseDealPage.bank().should('have.text', 'HSBC');
-  });
+
+  
 });
---
+++ trade-finance-manager-ui/templates/case/deal/deal.component-test.js
@@ -0,0 +1,53 @@
+const pageRenderer = require('../../../component-tests/pageRenderer');
+const page = '../templates/case/deal/deal.njk';
+const render = pageRenderer(page);
+
+describe(page, () => {
+  let wrapper;
+  let params = {
+    deal : {
+      _id: '12345678',
+      details: {
+        submissionType: 'Automatic Inclusion Notice',
+        owningBank: {
+          name: 'Lloyds'
+        },
+        maker: {
+          firstname: 'Simon',
+          surname: 'Neumann',
+          bank: {
+            emails : ['xxx@yyy.com'] 
+            }
+        }
+      },
+      submissionDetails: {
+        supplierName: 'The Supplier name',
+        buyerName: 'The Buyer name',
+      },
+    }
+  };
+
+  beforeEach(() => {
+    wrapper = render(params);
+  });
+  
+  // TODO page title
+  it('should render  mga version', () => {
+    wrapper.expectText('[data-cy="mga-version"]').toRead('January 2020');
+  });
+
+  it('should render  bank', () => {
+    wrapper.expectText('[data-cy="deal-bank"]').toRead(params.deal.details.owningBank.name);
+  });
+
+  it('should render contact name', () => {
+    wrapper.expectText('[data-cy="contact-name"]').toRead(params.deal.details.maker.firstname + ' ' + params.deal.details.maker.surname );
+  });
+
+  it('should render email', () => {
+    wrapper.expectText('[data-cy="email"]').toRead(params.deal.details.maker.bank.emails[0] );
+  });
+
+  
+
+});
