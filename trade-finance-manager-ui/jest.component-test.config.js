a83f71c21e066aad65732b9523e314ab60120294
ttbarnes <tony@tonybarnes.me>
2020-12-09 16:10:36 +0000

    DTFS2-2721 - tfm - eligibility criteria answers - last table row styling, component tests

--- trade-finance-manager-ui/jest.component-test.config.js
+++ trade-finance-manager-ui/jest.component-test.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  testMatch: ['**/*.component-test.js'],
+  // testMatch: ['**/*.component-test.js'],
+  testMatch: ['**/eligibility-criteria-answers.component-test.js'],
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'babel-jest',
   },
