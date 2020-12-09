dc07c4a28ef5a8eaa01d0884d70c2d16a5b75c93
ttbarnes <tony@tonybarnes.me>
2020-12-09 16:14:30 +0000

    revert tfm ui jest config

--- trade-finance-manager-ui/jest.component-test.config.js
+++ trade-finance-manager-ui/jest.component-test.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // testMatch: ['**/*.component-test.js'],
-  testMatch: ['**/eligibility-criteria-answers.component-test.js'],
+  testMatch: ['**/*.component-test.js'],
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'babel-jest',
   },
