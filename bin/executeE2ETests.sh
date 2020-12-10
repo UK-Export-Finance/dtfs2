eabd9f862eeeec55e8a462ebc2247f9d573a4efb
Andrew Price <andrew@madebymoon.com>
2020-12-10 13:46:38 +0000

    Added TFM tests to local pipeline

--- bin/executeE2ETests.sh
+++ bin/executeE2ETests.sh
@@ -1,15 +1,25 @@
 HERE=$(pwd)
 LOG="$HERE/pipeline.log"
 
-start=`Start E2E Portal: date +%s`
+starttfm=`date +%s`
 
-cd e2e-tests/portal
+cd "$HERE/e2e-tests/trade-finance-manager"
 npx cypress run --spec "cypress/integration/**/*.spec.js" --config video=false
 testResult=$?
 
-end=`End E2E Portal: date +%s`
+endtfm=`date +%s`
+[ $testResult -eq 0 ] && echo "{\"stage\": \"pipeline:cypress:tests-tfm\", \"duration\": \"$((endtfm-starttfm))\", \"result\": \"pass\"}" >> "$LOG" || echo "{\"stage\": \"pipeline:cypress:tests-tfm\", \"duration\": \"$((endtfm-starttfm))\", \"result\": \"fail\"}" >> "$LOG"
 
-[ $testResult -eq 0 ] && echo "{\"stage\": \"pipeline:cypress:tests\", \"duration\": \"$((end-start))\", \"result\": \"pass\"}" >> "$LOG" || echo "{\"stage\": \"pipeline:cypress:tests\", \"duration\": \"$((end-start))\", \"result\": \"fail\"}" >> "$LOG"
 
+start=`date +%s`
+
+cd "$HERE/e2e-tests/portal"
+npx cypress run --spec "cypress/integration/**/*.spec.js" --config video=false
+testResult=$?
+
+end=`date +%s`
+[ $testResult -eq 0 ] && echo "{\"stage\": \"pipeline:cypress:tests-portal\", \"duration\": \"$((end-start))\", \"result\": \"pass\"}" >> "$LOG" || echo "{\"stage\": \"pipeline:cypress:tests-portal\", \"duration\": \"$((end-start))\", \"result\": \"fail\"}" >> "$LOG"
+
+cd "$HERE"
 
 exit $testResult
