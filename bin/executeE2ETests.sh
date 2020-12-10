f9d01c34c1cf653e3af1fe36c03f70940f2d634c
Andrew Price <andrew@madebymoon.com>
2020-12-10 11:57:17 +0000

    Fixed local run of pipeline to reference new location of cypress

--- bin/executeE2ETests.sh
+++ bin/executeE2ETests.sh
@@ -1,12 +1,13 @@
 HERE=$(pwd)
 LOG="$HERE/pipeline.log"
 
-start=`date +%s`
+start=`Start E2E Portal: date +%s`
 
+cd e2e-tests/portal
 npx cypress run --spec "cypress/integration/**/*.spec.js" --config video=false
 testResult=$?
 
-end=`date +%s`
+end=`End E2E Portal: date +%s`
 
 [ $testResult -eq 0 ] && echo "{\"stage\": \"pipeline:cypress:tests\", \"duration\": \"$((end-start))\", \"result\": \"pass\"}" >> "$LOG" || echo "{\"stage\": \"pipeline:cypress:tests\", \"duration\": \"$((end-start))\", \"result\": \"fail\"}" >> "$LOG"
 
