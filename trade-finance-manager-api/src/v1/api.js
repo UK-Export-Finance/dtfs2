b6d89b452a66d8fad3a08934beb78b960a3dffa0
Andrew Price <andrew@madebymoon.com>
2020-12-15 10:19:49 +0000

    DTFS-2575 - central submissions api - fixed tfm api tests

--- trade-finance-manager-api/src/v1/api.js
+++ trade-finance-manager-api/src/v1/api.js
@@ -20,6 +20,29 @@ const findOneDeal = async (dealId) => {
   }
 };
 
+
+const queryDeals = async (query, start = 0, pagesize = 0) => {
+  try {
+    const response = await axios({
+      method: 'post',
+      url: `${urlRoot}/v1/deals/query`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        query,
+        start,
+        pagesize,
+      },
+    });
+
+    return response.data;
+  } catch (err) {
+    return new Error('error with token');// do something proper here, but for now just reject failed logins..
+  }
+};
+
 module.exports = {
   findOneDeal,
+  queryDeals,
 };
