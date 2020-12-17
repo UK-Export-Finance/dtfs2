b2321d10f596cde85cea7a8613c8286587335743
Andrew Price <andrew@madebymoon.com>
2020-12-17 14:46:45 +0000

    DTFS2-2941 Restricted access to /admin

---
+++ portal/server/routes/role-validator.js
@@ -0,0 +1,28 @@
+const ukefAdminRoles = ['ukef_operations'];
+
+const userRoleIsValid = (requiredRoles, user) => {
+  if (!requiredRoles || requiredRoles.length === 0) {
+    return true;
+  }
+
+  const userHasOneOfTheRequiredRoles = ukefAdminRoles.some((adminRole) => user.roles.includes(adminRole))
+        || requiredRoles.some((role) => user.roles.includes(role));
+  return userHasOneOfTheRequiredRoles;
+};
+
+const validate = (opts) => {
+  const requiredRoles = opts ? opts.role : null;
+
+  return (req, res, next) => {
+    if (userRoleIsValid(requiredRoles, req.session.user)) {
+      next();
+    } else {
+      res.redirect('/not-found');
+    }
+  };
+};
+
+module.exports = {
+  userRoleIsValid,
+  validate,
+};
