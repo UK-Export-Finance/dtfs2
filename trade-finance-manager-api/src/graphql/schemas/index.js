ec39ba924685e1e63e51ecadac46d0df2528db5d
SimonTheDeveloper <mail@simon-neumann.com>
2021-01-03 01:47:51 +0000

    parties page

--- trade-finance-manager-api/src/graphql/schemas/index.js
+++ trade-finance-manager-api/src/graphql/schemas/index.js
@@ -39,8 +39,44 @@ type DealSubmissionDetails {
   supplyContractDescription: String
   supplyContractCurrency: String
   supplyContractValue: String
-  buyerName: String
-  destinationCountry: String
+  buyerName: String,
+  buyerAddressCountry: String,
+  buyerAddressLine1: String,
+  buyerAddressLine2: String,
+  buyerAddressLine3: String,
+  buyerAddressPostcode: String,
+  buyerAddressTown: String,
+  destinationCountry: String,
+  indemnifierAddressCountry: String
+  indemnifierAddressLine1: String,
+  indemnifierAddressLine2: String,
+  indemnifierAddressLine3: String,
+  indemnifierAddressPostcode: String,
+  indemnifierAddressTown: String,
+  indemnifierCorrespondenceAddressCountry: String,
+  indemnifierCorrespondenceAddressLine1: String,
+  indemnifierCorrespondenceAddressLine2: String,
+  indemnifierCorrespondenceAddressLine3: String,
+  indemnifierCorrespondenceAddressPostcode: String,
+  indemnifierCorrespondenceAddressTown: String,
+  indemnifierName: String,
+  industryClass: String,
+  industrySector: String,
+  supplierAddressCountry: String,
+  supplierCountry: String,
+  supplierAddressLine1: String,
+  supplierAddressLine2: String,
+  supplierAddressLine3: String,
+  supplierAddressPostcode: String,
+  supplierAddressTown: String,
+  supplierCompaniesHouseRegistrationNumber: String,
+  supplierCorrespondenceAddressCountry: String,
+  supplierCorrespondenceAddressLine1: String,
+  supplierCorrespondenceAddressLine2: String,
+  supplierCorrespondenceAddressLine3: String,
+  supplierCorrespondenceAddressPostcode: String,
+  supplierCorrespondenceAddressTown: String,
+  supplierAddress: String
 }
 
 type DealEligibilityCriterion {
@@ -51,11 +87,23 @@ type DealEligibilityCriterion {
   answer: String
 }
 
+type DealEligibility {
+  agentAddressCountry: String
+  agentAddressLine1: String
+  agentAddressLine2: String
+  agentAddressLine3: String
+  agentAddressPostcode: String
+  agentAddressTown: String
+  agentName: String
+  agentAlias: String
+}
+
 type Deal {
   _id: String!
   details: DealDetails
   submissionDetails: DealSubmissionDetails
   eligibilityCriteria: [DealEligibilityCriterion!]
+  eligibility: [DealEligibility!]
 }
 
 type Query {
