89b6a49f903e10c861da34e860942ca69f5d1d84
Andrew Price <andrew@madebymoon.com>
2021-01-04 17:45:09 +0000

    DTFS2-2948 change password require current pwd
    DTFS2-2948 change password require current pwd e2e tests

--- e2e-tests/portal/cypress/integration/pages/user/changePassword.js
+++ e2e-tests/portal/cypress/integration/pages/user/changePassword.js
@@ -1,9 +1,11 @@
 const changePassword = {
   // visit: () => cy.visit('/'),
+  currentPassword: () => cy.get('[data-cy="current-password"]'),
+  currentPasswordError: () => cy.get('#currentPassword-error'),
   password: () => cy.get('[data-cy="password"]'),
-  passwordError: () => cy.get(`#password-error`),
+  passwordError: () => cy.get('#password-error'),
   confirmPassword: () => cy.get('[data-cy="confirmPassword"]'),
   submit: () => cy.get('[data-cy="submit"]'),
-}
+};
 
 module.exports = changePassword;
