64ebd1f02f2ed97b5c71ee9c4ce426b61c4cd220
Andrew Price <andrew@madebymoon.com>
2021-01-04 11:35:05 +0000

    Added graphql debug

--- trade-finance-manager-api/src/v1/controllers/deal.controller.js
+++ trade-finance-manager-api/src/v1/controllers/deal.controller.js
@@ -3,6 +3,7 @@ const api = require('../api');
 
 const findOneDeal = async (_id) => {
   const deal = await api.findOneDeal(_id);
+  console.log('findOneDeal', { deal, mapped: mapDeal(deal) });
   return mapDeal(deal);
 };
 
