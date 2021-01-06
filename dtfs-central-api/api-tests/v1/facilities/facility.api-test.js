fb5b169c5f65e562201bac7c7a8a6a825ee16997
ttbarnes <tony@tonybarnes.me>
2021-01-06 16:46:02 +0000

    DTFS2-3084 - central-api - facilities CRUD

--- dtfs-central-api/api-tests/v1/facilities/facility.api-test.js
+++ dtfs-central-api/api-tests/v1/facilities/facility.api-test.js
@@ -2,14 +2,52 @@ const wipeDB = require('../../wipeDB');
 
 const app = require('../../../src/createApp');
 const api = require('../../api')(app);
+const aDeal = require('../deal-builder');
 
 const newFacility = {
   facilityType: 'bond',
+  associatedDealId: '123123456',
 };
 
+const mockUser = {
+  _id: '123456789',
+  username: 'temp',
+  roles: [],
+  bank: {
+    id: '956',
+    name: 'Barclays Bank',
+  },
+};
+
+const newDeal = aDeal({
+  details: {
+    bankSupplyContractName: 'mock name',
+    bankSupplyContractID: 'mock id',
+    maker: mockUser,
+  },
+  editedBy: [],
+  eligibility: {
+    status: 'Not started',
+    criteria: [{}],
+  }
+});
+
+const createDeal = async () => {
+  const { body, status } = await api.post(newDeal).to('/v1/deals');
+  return body;
+}
+
 describe('/v1/facilities', () => {
+  let dealId;
+
   beforeEach(async () => {
+    await wipeDB.wipe(['deals']);
     await wipeDB.wipe(['facilities']);
+
+    const deal = await createDeal();
+
+    dealId = deal._id;
+    newFacility.associatedDealId = dealId;
   });
 
   describe('POST /v1/facilities', () => {
@@ -20,6 +58,7 @@ describe('/v1/facilities', () => {
 
       expect(typeof body._id).toEqual('string');
       expect(body.facilityType).toEqual(newFacility.facilityType);
+      expect(body.associatedDealId).toEqual(newFacility.associatedDealId);
       expect(typeof body.createdDate).toEqual('string');
     });
 
@@ -33,19 +72,30 @@ describe('/v1/facilities', () => {
       expect(facility3.body._id - facility2.body._id).toEqual(1);
     });
 
+    it('returns 404 when associatedDealId is not found', async () => {
+      newFacility.associatedDealId = '1234';
+      const { body, status } = await api.post(newFacility).to('/v1/facilities');
+
+      expect(status).toEqual(404);
+    });
+
     describe('when required fields are missing', () => {
       it('returns 400 with validation errors', async () => {
         const postBody = {
           facilityType: '',
+          associatedDealId: '',
         };
 
         const { body, status } = await api.post(postBody).to('/v1/facilities');
 
         expect(status).toEqual(400);
-        expect(body.validationErrors.count).toEqual(1);
+        expect(body.validationErrors.count).toEqual(2);
 
         expect(body.validationErrors.errorList.facilityType).toBeDefined();
         expect(body.validationErrors.errorList.facilityType.text).toEqual('Enter the Facility type');
+
+        expect(body.validationErrors.errorList.associatedDealId).toBeDefined();
+        expect(body.validationErrors.errorList.associatedDealId.text).toEqual('Enter the Associated deal id');
       });
     });
 
@@ -53,6 +103,7 @@ describe('/v1/facilities', () => {
       it('returns 400 with validation errors', async () => {
         const postBody = {
           facilityType: 'invalid-facility',
+          associatedDealId: '123123456'
         };
 
         const { body, status } = await api.post(postBody).to('/v1/facilities');
@@ -66,27 +117,28 @@ describe('/v1/facilities', () => {
     });
   });
 
-  // describe('GET /v1/facilities/:id', () => {
-  //   it('returns the requested resource', async () => {
-  //     const postResult = await api.post(newFacility).to('/v1/facilities');
-  //     const newId = postResult.body._id;
+  describe('GET /v1/facilities/:id', () => {
+    it('returns the requested resource', async () => {
+      const postResult = await api.post(newFacility).to('/v1/facilities');
+      const newId = postResult.body._id;
 
-  //     const { status, body } = await api.get(`/v1/facilities/${newId}`);
+      const { status, body } = await api.get(`/v1/facilities/${newId}`);
 
-  //     expect(status).toEqual(200);
-  //     expect(body.deal).toEqual(expectAddedFields(newFacility));
-  //   });
+      expect(status).toEqual(200);
+      expect(body._id).toEqual(newId);
+      expect(typeof body.createdDate).toEqual('string');
+    });
 
-  //   it('404s requests for unknown ids', async () => {
-  //     const { status } = await api.get('/v1/facilities/12345678910');
+    it('404s requests for unknown ids', async () => {
+      const { status } = await api.get('/v1/facilities/12345678910');
 
-  //     expect(status).toEqual(404);
-  //   });
-  // });
+      expect(status).toEqual(404);
+    });
+  });
 
   describe('PUT /v1/facilities/:id', () => {
     it('404s requests for unknown ids', async () => {
-      const { status } = await api.put(updatedFacility).to('/v1/facilities/123456789012');
+      const { status } = await api.put({}).to('/v1/facilities/123456789012');
 
       expect(status).toEqual(404);
     });
@@ -94,16 +146,53 @@ describe('/v1/facilities', () => {
     it('returns the updated facility', async () => {
       const postResult = await api.post(newFacility).to('/v1/facilities');
       const createdFacility = postResult.body;
+
       const updatedFacility = {
         ...createdFacility,
         facilityValue: 123456,
       };
 
-      const { status, body } = await api.put(updatedFacility).to(`/v1/facilities/${createdFacility._id}`);
+      const { body, status } = await api.put(updatedFacility).to(`/v1/facilities/${createdFacility._id}`);
 
       expect(status).toEqual(200);
+      expect(typeof body.lastEdited).toEqual('string');
+      expect(body.facilityValue).toEqual(updatedFacility.facilityValue);
+    });
 
-      // expect(body).toEqual(expectAddedFieldsWithEditedBy(updatedFacility, mockUser));
+    it('updates the facility', async () => {
+      const postResult = await api.post(newFacility).to('/v1/facilities');
+      const createdFacility = postResult.body;
+
+      const updatedFacility = {
+        ...createdFacility,
+        facilityValue: 123456,
+      };
+
+      await api.put(updatedFacility).to(`/v1/facilities/${createdFacility._id}`);
+
+      const { body } = await api.get(`/v1/facilities/${createdFacility._id}`);
+
+      expect(typeof body.lastEdited).toEqual('string');
+      expect(body.facilityValue).toEqual(updatedFacility.facilityValue);
+    });
+  });
+
+  describe('DELETE /v1/facilities/:id', () => {
+    it('404s requests for unknown ids', async () => {
+      const { status } = await api.remove('/v1/facilities/12345678910');
+
+      expect(status).toEqual(404);
+    });
+
+    it('deletes the facility', async () => {
+      const { body } = await api.post(newFacility).to('/v1/facilities');
+
+      const deleteResponse = await api.remove(`/v1/facilities/${body._id}`);
+      expect(deleteResponse.status).toEqual(200);
+
+      const { status } = await api.get(`/v1/facilities/${body._id}`);
+
+      expect(status).toEqual(404);
     });
   });
 });
