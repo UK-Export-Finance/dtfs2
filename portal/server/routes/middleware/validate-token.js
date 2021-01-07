fe6a0a49f58011f2154796f4173b5cad87004762
Andrew Price <andrew@madebymoon.com>
2021-01-07 18:17:15 +0000

    DTFS2-2964 Concurrent sessions

---
+++ portal/server/routes/middleware/validate-token.js
@@ -0,0 +1,15 @@
+import api from '../../api';
+
+const validateToken = async (req, res, next) => {
+  const { userToken } = req.session;
+
+  if (await api.validateToken(userToken)) {
+    next();
+  } else {
+    req.session.destroy(() => {
+      res.redirect('/');
+    });
+  }
+};
+
+export default validateToken;
