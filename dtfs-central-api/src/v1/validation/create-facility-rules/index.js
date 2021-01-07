a35c33e76f73186a6e057ece76adc28079ec1034
ttbarnes <tony@tonybarnes.me>
2021-01-07 12:03:59 +0000

    DTFS2-3084 - central-api - creating/updating a facility updates the deal

--- dtfs-central-api/src/v1/validation/create-facility-rules/index.js
+++ dtfs-central-api/src/v1/validation/create-facility-rules/index.js
@@ -1,9 +1,11 @@
 const facilityType = require('./facility-type');
 const associatedDealId = require('./associated-deal-id');
+const userObject = require('./user-object');
 
 const rules = [
   facilityType,
   associatedDealId,
+  userObject,
 ];
 
 module.exports = (facility) => {
