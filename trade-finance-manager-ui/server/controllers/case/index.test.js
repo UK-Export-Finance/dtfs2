860305c0feb4296b34caf0d09d1900d3bfd860ea
ttbarnes <tony@tonybarnes.me>
2021-01-13 12:59:34 +0000

    tfm - add tests for ui routes/controllers

--- trade-finance-manager-ui/server/controllers/case/index.test.js
+++ trade-finance-manager-ui/server/controllers/case/index.test.js
@@ -5,7 +5,7 @@ import { mockRes } from '../../test-mocks';
 const res = mockRes();
 
 describe('controllers - case', () => {
-  describe('GET', () => {
+  describe('GET case deals', () => {
     describe('when deal exists', () => {
       const mockDeal = {
         _id: '1000023',
@@ -49,4 +49,50 @@ describe('controllers - case', () => {
       });
     });
   });
+
+  describe('GET case parties', () => {
+    describe('when deal exists', () => {
+      const mockDeal = {
+        _id: '1000023',
+        mock: true,
+      };
+
+      beforeEach(() => {
+        api.getDeal = () => Promise.resolve(mockDeal);
+      });
+
+      it('should render deal template with data', async () => {
+        const req = {
+          params: {
+            _id: mockDeal._id, // eslint-disable-line no-underscore-dangle
+          },
+        };
+
+        await caseController.getCaseParties(req, res);
+        expect(res.render).toHaveBeenCalledWith('case/parties/parties.njk', {
+          deal: mockDeal,
+          active_sheet: 'parties',
+          dealId: req.params._id, // eslint-disable-line no-underscore-dangle
+        });
+      });
+    });
+
+    describe('when deal does NOT exist', () => {
+      beforeEach(() => {
+        api.getDeal = () => Promise.resolve();
+      });
+
+      it('should redirect to not-found route', async () => {
+        const req = {
+          params: {
+            _id: '1',
+          },
+        };
+
+        await caseController.getCaseParties(req, res);
+        expect(res.redirect).toHaveBeenCalledWith('/not-found');
+      });
+    });
+  });
+
 });
