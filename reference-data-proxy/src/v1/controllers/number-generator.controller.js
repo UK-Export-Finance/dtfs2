06d4c1abc94f678d4475ef3fde1a00d0547a4dc0
ttbarnes <tony@tonybarnes.me>
2021-01-19 13:19:09 +0000

    DTFS2-2998 - acbs beginnings

--- reference-data-proxy/src/v1/controllers/number-generator.controller.js
+++ reference-data-proxy/src/v1/controllers/number-generator.controller.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const CONSTANTS = require('../../constants');
+const { checkDealId } = require('./acbs.controller');
 
 const numberTypeIsValid = (numberType) => {
   if (!Object.values(CONSTANTS.NUMBER_GENERATOR).includes(numberType)) {
@@ -9,13 +10,7 @@ const numberTypeIsValid = (numberType) => {
   return true;
 };
 
-exports.create = async (req, res) => {
-  const numberType = Number(req.params.numberType);
-
-  if (!numberTypeIsValid(numberType)) {
-    return res.status(400).send('Invalid number type.');
-  }
-
+const callNumberGenerator = async (numberType) => {
   const response = await axios({
     method: 'post',
     url: process.env.MULESOFT_API_NUMBER_GENERATOR_URL,
@@ -31,10 +26,50 @@ exports.create = async (req, res) => {
       },
     ],
   }).catch((catchErr) => catchErr);
+  
+  return response;
+}
 
-  const { status, data } = response;
+exports.create = async (req, res) => {
+  const numberType = Number(req.params.numberType);
 
-  return res.status(status).send({
-    id: data.maskedId,
-  });
+  if (!numberTypeIsValid(numberType)) {
+    return res.status(400).send('Invalid number type.');
+  }
+
+  // const response = await axios({
+  //   method: 'post',
+  //   url: process.env.MULESOFT_API_NUMBER_GENERATOR_URL,
+  //   auth: {
+  //     username: process.env.MULESOFT_API_KEY,
+  //     password: process.env.MULESOFT_API_SECRET,
+  //   },
+  //   data: [
+  //     {
+  //       numberTypeId: numberType,
+  //       createdBy: 'Portal v2/TFM',
+  //       requestingSystem: 'Portal v2/TFM',
+  //     },
+  //   ],
+  // }).catch((catchErr) => catchErr);
+
+  const { status, data } = await callNumberGenerator(numberType);
+  
+  const numberGeneratorId = data.maskedId;
+  
+  // TODO check with ACBS
+  if (numberType === CONSTANTS.NUMBER_GENERATOR.DEAL) {
+    const { status } = await checkDealId();
+
+    if (status === 404) {
+      // deal id is not being used 
+      
+    } else {
+      // deal id is already in use so we need to get another one.
+    }
+  }
+
+  // return res.status(status).send({
+  //   id: data.maskedId,
+  // });
 };
