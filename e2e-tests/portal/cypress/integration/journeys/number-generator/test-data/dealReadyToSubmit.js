8541156d5ebf5ae6826f6b01538d05909417d604
ttbarnes <tony@tonybarnes.me>
2021-01-25 15:41:05 +0000

    DTFS2-2998 - e2e test for TFM checker submitting a deal

---
+++ e2e-tests/portal/cypress/integration/journeys/number-generator/test-data/dealReadyToSubmit.js
@@ -0,0 +1,23 @@
+const moment = require('moment');
+const dealThatJustNeedsConversionDate = require('./dealThatJustNeedsConversionDate');
+
+module.exports = () => {
+  const now = moment();
+
+  // doing a complete serialize+deserialize here...
+  // ran into issues destructuring things into our new object; cypress was keeping references
+  // between my bits of test data, so updating 1 deal would cause the other to update..
+  const deal = JSON.parse(JSON.stringify(dealThatJustNeedsConversionDate()));
+
+  deal.submissionDetails['supplyContractConversionDate-day'] = `${now.format('DD')}`;
+  deal.submissionDetails['supplyContractConversionDate-month'] = `${now.format('MM')}`;
+  deal.submissionDetails['supplyContractConversionDate-year'] = `${now.format('YYYY')}`;
+
+  deal.loanTransactions.items[0].requestedCoverStartDate = moment().utc().valueOf();
+
+  const aMonthInTheFuture = moment().add(1, 'month');
+  deal.loanTransactions.items[0]['coverEndDate-day'] = aMonthInTheFuture.format('DD');
+  deal.loanTransactions.items[0]['coverEndDate-month'] = aMonthInTheFuture.format('MM');
+  deal.loanTransactions.items[0]['coverEndDate-year'] = moment(aMonthInTheFuture).format('YYYY');
+  return deal;
+};
