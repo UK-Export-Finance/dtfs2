792e5591d041ac28c568147781fe76d2f8208c32
Andrew Price <andrew@madebymoon.com>
2021-01-29 15:54:04 +0000

    GEF added REDIS for shared session between Portal & GEF UI

--- gef-ui/server/routes/middleware/validate-token.js
+++ gef-ui/server/routes/middleware/validate-token.js
@@ -1,18 +1,14 @@
 import api from '../../api';
 
 const validateToken = async (req, res, next) => {
-  // TODO ADD REDIS so Portal & GEF can share token
-  next();
-
-  // const { userToken } = req.session;
-  // if (await api.validateToken(userToken)) {
-  //   next();
-  // } else {
-  //   req.session.destroy(() => {
-  //     console.log('Invalid user token');
-  //     res.redirect('/');
-  //   });
-  // }
+  const { userToken } = req.session;
+  if (await api.validateToken(userToken)) {
+    next();
+  } else {
+    req.session.destroy(() => {
+      res.redirect('/');
+    });
+  }
 };
 
 export default validateToken;
