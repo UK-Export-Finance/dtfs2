d9c1527db0140c398c8c4dc10f07f35a81e5f312
Richard <info@monkemedia.co.uk>
2021-02-09 17:34:55 +0000

    First commit

---
+++ gef-ui/server/routes/mandatory-criteria.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { 
+  getMandatoryCriteria,
+  validateMandatoryCriteria
+} from '../controllers/mandatory-criteria'
+import validateToken from './middleware/validate-token';
+
+const router = express.Router()
+
+router.use('/*', validateToken);
+router.get('/mandatory-criteria', (req, res) => getMandatoryCriteria(req, res))
+router.post('/mandatory-criteria', (req, res) => validateMandatoryCriteria(req, res))
+
+export default router
