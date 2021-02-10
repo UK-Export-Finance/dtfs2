cd2f9f2939e9f9e347c96656c2fb9bcf60ee17ae
ttbarnes <tony@tonybarnes.me>
2021-02-10 16:24:37 +0000

    DTFS2-3085 - update portal e2e tests to createFacilities

--- e2e-tests/portal/cypress/integration/journeys/workflow/typeB/typeB-confirmation-acknowledged-actionCode-011-with-submitted-facilities.spec.js
+++ e2e-tests/portal/cypress/integration/journeys/workflow/typeB/typeB-confirmation-acknowledged-actionCode-011-with-submitted-facilities.spec.js
@@ -11,6 +11,11 @@ const dealReadyToSubmitWithAlreadySubmittedIssuedFacilities = require('./test-da
 
 context('A checker submits a deal; workflow responds with a confirmation_acknowledged status and empty facility statuses', () => {
   let deal;
+  let dealId;
+  const dealFacilities = {
+    bonds: [],
+    loans: [],
+  };
 
   beforeEach(() => {
     // [dw] at time of writing, the portal was throwing exceptions; this stops cypress caring
@@ -25,7 +30,28 @@ context('A checker submits a deal; workflow responds with a confirmation_acknowl
     cy.insertOneDeal(dealReadyToSubmitWithAlreadySubmittedIssuedFacilities, MAKER_LOGIN)
       .then((insertedDeal) => {
         deal = insertedDeal;
+        dealId = deal._id; // eslint-disable-line no-underscore-dangle
+
+        const { mockFacilities } = dealReadyToSubmitWithAlreadySubmittedIssuedFacilities;
+
+        cy.createFacilities(dealId, mockFacilities, MAKER_LOGIN).then((createdFacilities) => {
+          const bonds = createdFacilities.filter((f) => f.facilityType === 'bond');
+          const loans = createdFacilities.filter((f) => f.facilityType === 'loan');
+
+          dealFacilities.bonds = bonds;
+          dealFacilities.loans = loans;
+        });
       });
+  });
+
+  afterEach(() => {
+    dealFacilities.bonds.forEach((facility) => {
+      cy.deleteFacility(facility._id, MAKER_LOGIN); // eslint-disable-line no-underscore-dangle
+    });
+
+    dealFacilities.loans.forEach((facility) => {
+      cy.deleteFacility(facility._id, MAKER_LOGIN); // eslint-disable-line no-underscore-dangle
+    });
   });
 
   it('Checker submits a deal; workflow responds; deal and facilities statuses are updated', () => {
@@ -51,7 +77,7 @@ context('A checker submits a deal; workflow responds with a confirmation_acknowl
       },
       bonds: [
         {
-          BSS_portal_facility_id: deal.bondTransactions.items[0]._id,
+          BSS_portal_facility_id: dealFacilities.bonds[0]._id,
           BSS_ukef_facility_id: '54321',
           BSS_status: '',
           BSS_comments: 'blahblah blah blahblah',
@@ -59,7 +85,7 @@ context('A checker submits a deal; workflow responds with a confirmation_acknowl
       ],
       loans: [
         {
-          EWCS_portal_facility_id: deal.loanTransactions.items[0]._id,
+          EWCS_portal_facility_id: dealFacilities.loans[0]._id,
           EWCS_ukef_facility_id: '65432',
           EWCS_status: '',
           EWCS_comments: 'blahblah blah blahblah',
@@ -72,14 +98,14 @@ context('A checker submits a deal; workflow responds with a confirmation_acknowl
       expect(text.trim()).to.equal('Accepted by UKEF (with conditions)');
     });
 
-    const bondId = deal.bondTransactions.items[0]._id; // eslint-disable-line no-underscore-dangle
+    const bondId = dealFacilities.bonds[0]._id; // eslint-disable-line no-underscore-dangle
     const bondRow = pages.contract.bondTransactionsTable.row(bondId);
 
     bondRow.bondStatus().invoke('text').then((text) => {
       expect(text.trim()).to.equal('Acknowledged');
     });
 
-    const loanId = deal.loanTransactions.items[0]._id; // eslint-disable-line no-underscore-dangle
+    const loanId = dealFacilities.loans[0]._id; // eslint-disable-line no-underscore-dangle
     const loanRow = pages.contract.loansTransactionsTable.row(loanId);
 
     loanRow.loanStatus().invoke('text').then((text) => {
