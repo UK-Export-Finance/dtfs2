a459aa4bc9b6a68ed10e5a76ab0a1a731b8bfa87
ttbarnes <tony@tonybarnes.me>
2021-02-11 16:47:15 +0000

    DTFS2-3085 - tfm e2e test using createFacilities command

--- e2e-tests/trade-finance-manager/cypress/support/commands.js
+++ e2e-tests/trade-finance-manager/cypress/support/commands.js
@@ -8,7 +8,8 @@ Cypress.Commands.add('login', require('./ui/logIn'));
 Cypress.Commands.add('insertOneDeal', require('./portal-api/insertOneDeal'));
 Cypress.Commands.add('insertManyDeals', require('./portal-api/insertManyDeals'));
 Cypress.Commands.add('deleteDeals', require('./portal-api/deleteDeals'));
+Cypress.Commands.add('createFacilities', require('./portal-api/createFacilities'));
 
-Cypress.Commands.add('createFacility', require('./central-api/createFacility'));
+// Cypress.Commands.add('createFacility', require('./central-api/createFacility'));
 Cypress.Commands.add('updateFacility', require('./central-api/updateFacility'));
 Cypress.Commands.add('deleteFacility', require('./central-api/deleteFacility'));
