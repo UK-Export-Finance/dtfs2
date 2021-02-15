6402827006113cbbbe68feb40e51f8723735ba3f
Richard <info@monkemedia.co.uk>
2021-02-15 14:22:58 +0000

    error handler added

--- gef-ui/server/models/mandatory-criteria/index.js
+++ gef-ui/server/models/mandatory-criteria/index.js
@@ -3,8 +3,14 @@ import Api from '../../services/api'
 const getCriteria = async (token) => {
   const api = new Api(token)
 
-  const response = await api.getMandatoryCriteria()
-  return response
+  try {
+    const response = await api.getMandatoryCriteria()
+
+    return response
+  } catch (err) {
+    throw err
+  }
+  
 }
 
 export {
