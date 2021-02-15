8cf426bc29fdbc1c374bc39c5957e49c69a0e9d7
Richard <info@monkemedia.co.uk>
2021-02-15 16:54:00 +0000

    removed helpers

--- gef-ui/server/helpers/formattedTimestamp.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-
-const formattedTimestamp = (timestamp) => {
-  const utc = moment(parseInt(timestamp, 10));
-  const dt = moment(utc);
-  return moment(dt).isValid() ? dt.format() : '';
-};
-
-export default formattedTimestamp;
