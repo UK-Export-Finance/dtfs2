1177d364e22f7054e4854c8a40adcb9183051bf9
Richard <info@monkemedia.co.uk>
2021-02-15 09:46:04 +0000

    if token is undefined then redirect to login page

--- gef-ui/server/services/api.js
+++ gef-ui/server/services/api.js
@@ -3,6 +3,11 @@ import Axios from './axios'
 export default function Api (token) {
   Axios.defaults.headers.common['Authorization'] = token
 
+  this.validateToken = async function () {
+    const response = await Axios.get('/validate')
+    return response.status === 200
+  }
+
   this.getMandatoryCriteria = async function () {
     const response = await Axios.get('/1')
     return response.data
