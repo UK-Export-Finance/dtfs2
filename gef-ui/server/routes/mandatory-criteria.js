1e70eb9e616b56125b728339aef2705cfe0ea31f
Richard <info@monkemedia.co.uk>
2021-02-15 15:57:34 +0000

    added back validateMandatoryCriteria endpoint

--- gef-ui/server/routes/mandatory-criteria.js
+++ gef-ui/server/routes/mandatory-criteria.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { 
   getMandatoryCriteria,
-  // validateMandatoryCriteria
+  validateMandatoryCriteria
 } from '../controllers/mandatory-criteria'
 import validateToken from './middleware/validate-token'
 
 const router = express.Router()
 
 router.get('/mandatory-criteria', validateToken, (req, res) => getMandatoryCriteria(req, res))
-// router.post('/mandatory-criteria', (req, res) => validateMandatoryCriteria(req, res))
+router.post('/mandatory-criteria', validateToken, (req, res) => validateMandatoryCriteria(req, res))
 
 export default router
