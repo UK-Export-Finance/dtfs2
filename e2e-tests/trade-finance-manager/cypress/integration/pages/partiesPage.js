7706e0f106d26293b511834eeeddadef4a032f3b
ttbarnes <tony@tonybarnes.me>
2021-02-16 17:31:33 +0000

    DTFS2-2885 - tfm - parties page - bond issuer - facilities table e2e test

--- e2e-tests/trade-finance-manager/cypress/integration/pages/partiesPage.js
+++ e2e-tests/trade-finance-manager/cypress/integration/pages/partiesPage.js
@@ -7,6 +7,15 @@ const partiesPage = {
   agentEditLink: () => cy.get('[data-cy="parties-agent"] [data-cy="edit-party-link"]'),
   indemnifierArea: () => cy.get('[data-cy="parties-indemnifier"]'),
   indemnifierEditLink: () => cy.get('[data-cy="parties-indemnifier"] [data-cy="edit-party-link"]'),
+  bondIssuerFacilitiesTable: {
+    row: (facilityId) => {
+      const row = cy.get(`[data-cy="facility-${facilityId}"]`);
+      return {
+        row,
+        facilityId: () => row.get(`[data-cy="facility-${facilityId}-ukef-facility-id-link"]`),
+      };
+    },
+  },
 };
 
 module.exports = partiesPage;
