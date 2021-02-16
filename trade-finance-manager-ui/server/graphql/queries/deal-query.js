ff5ed529454161a4125e22b66895f40ae2b59939
ttbarnes <tony@tonybarnes.me>
2021-02-16 17:25:07 +0000

    DTFS2-2885 - tfm - fix bond-issuer table/query issues from rebase

--- trade-finance-manager-ui/server/graphql/queries/deal-query.js
+++ trade-finance-manager-ui/server/graphql/queries/deal-query.js
@@ -4,103 +4,6 @@ const dealQuery = gql`
   query Deal($id: ID!) {
     deal(_id: $id) {
       _id
-      ukefDealId,
-      details {
-        status,
-        submissionDate,
-        submissionType,
-        owningBank {
-          name,
-          emails
-        },
-        maker {
-          firstname,
-          surname,
-          email,
-        },
-        bankSupplyContractID,
-        bankSupplyContractName,
-      }
-      totals {
-        facilitiesValueInGBP,
-        facilitiesUkefExposure
-      }
-      facilities {
-        _id,
-        ukefFacilityID,
-        facilityProduct {
-          code
-        },
-        facilityType,
-        ukefFacilityType,
-        facilityStage,
-        facilityValueExportCurrency,
-        facilityValue,
-        coverEndDate,
-        ukefExposure,
-        coveredPercentage,
-        bondIssuer
-      }
-      eligibility {
-        agentAddressCountry,
-        agentAddressLine1,
-        agentAddressLine2,
-        agentAddressLine3,
-        agentAddressPostcode,
-        agentAddressTown,
-        agentName,
-        agentAlias
-      }
-      eligibilityCriteria {
-        id,
-        answer,
-        description,
-        descriptionList
-      }
-      submissionDetails {
-        supplierName,
-        supplyContractDescription,
-        destinationCountry,
-        supplyContractCurrency,
-        supplyContractValue,
-        buyerName,
-        buyerAddressCountry,
-        buyerAddressLine1,
-        buyerAddressLine2,
-        buyerAddressLine3,
-        buyerAddressPostcode,
-        buyerAddressTown,
-        indemnifierAddressCountry,
-        indemnifierAddressLine1,
-        indemnifierAddressLine2,
-        indemnifierAddressLine3,
-        indemnifierAddressPostcode,
-        indemnifierAddressTown,
-        indemnifierCorrespondenceAddressCountry,
-        indemnifierCorrespondenceAddressLine1,
-        indemnifierCorrespondenceAddressLine2,
-        indemnifierCorrespondenceAddressLine3,
-        indemnifierCorrespondenceAddressPostcode,
-        indemnifierCorrespondenceAddressTown,
-        indemnifierName,
-        industryClass,
-        industrySector,
-        supplierAddressCountry,
-        supplierCountry,
-        supplierAddressLine1,
-        supplierAddressLine2,
-        supplierAddressLine3,
-        supplierAddressPostcode,
-        supplierAddressTown,
-        supplierCompaniesHouseRegistrationNumber,
-        supplierCorrespondenceAddressCountry,
-        supplierCorrespondenceAddressLine1,
-        supplierCorrespondenceAddressLine2,
-        supplierCorrespondenceAddressLine3,
-        supplierCorrespondenceAddressPostcode,
-        supplierCorrespondenceAddressTown,
-        smeType
-      }
       tfm {
         submissionDetails {
           supplierPartyUrn
@@ -143,7 +46,8 @@ const dealQuery = gql`
           coverEndDate,
           ukefExposure,
           coveredPercentage,
-          bondIssuer
+          bondIssuer,
+          bankFacilityReference
         }
         eligibility {
           agentAddressCountry,
