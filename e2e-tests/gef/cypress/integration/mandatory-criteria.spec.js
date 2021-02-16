06a8ab794b30a9818d9cca1ef0ba7325ea4f48b3
Richard <info@monkemedia.co.uk>
2021-02-16 14:31:06 +0000

    Added pages object to integration testing

--- e2e-tests/gef/cypress/integration/mandatory-criteria.spec.js
+++ e2e-tests/gef/cypress/integration/mandatory-criteria.spec.js
@@ -1,4 +1,5 @@
 import relative from './relativeURL'
+import { mandatoryCriteria } from './pages/mandatory-criteria'
 
 context('Mandatory Criteria Page', () => {
   before(() => {
@@ -12,42 +13,42 @@ context('Mandatory Criteria Page', () => {
 
   beforeEach(() => {
     Cypress.Cookies.preserveOnce('connect.sid')
-    cy.visit(relative('/gef/mandatory-criteria'))
+    mandatoryCriteria.visit(relative('/gef/mandatory-criteria'))
   })
 
   describe('Visiting page', () => {
     it ('displays the header', () => {
-      cy.get('[data-cy="heading-caption"]')
-      cy.get('[data-cy="main-heading"]')
+      mandatoryCriteria.captionHead()
+      mandatoryCriteria.mainHeading()
     })
 
     it ('displays the mandatory criteria text', () => {
-      cy.get('[data-cy="mandatory-criteria"]')
+      mandatoryCriteria.mandatoryCriteriaText()
     })
   })
 
   describe('Clicking on Continue', () => {
     it ('returns error when no radio button has been selected', () => {
-      cy.get('[data-cy="form"]').submit()
-      cy.get('[data-cy="mandatory-criteria-error"]')
+      mandatoryCriteria.form().submit()
+      mandatoryCriteria.formError()
     })
 
     it('redirects the user to ** when they select False', () => {
-      cy.get('[data-cy="mandatory-criteria-false"]').click()
-      cy.get('[data-cy="form"]').submit()
+      mandatoryCriteria.falseRadio().click()
+      mandatoryCriteria.form().submit()
       cy.url().should('eq', relative('/gef/mandatory-criteria'))
     })
 
     it('redirects the user to the Name Application page when they select True', () => {
-      cy.get('[data-cy="mandatory-criteria-true"]').click()
-      cy.get('[data-cy="form"]').submit()
+      mandatoryCriteria.trueRadio().click()
+      mandatoryCriteria.form().submit()
       cy.url().should('eq', relative('/gef/name-application'))
     })
   })
 
   describe('Clicking on Cancel', () => {
     it('keeps the user on the same page FOR NOW', () => {
-      cy.get('[data-cy="cancel-button"]').click()
+      mandatoryCriteria.cancelButton().click()
       cy.url().should('eq', relative('/gef/mandatory-criteria'))
     })
   })
