443bd84b44845bd5b789850b1b9b4ac7e7d0f2f1
Richard <info@monkemedia.co.uk>
2021-02-16 17:50:09 +0000

    Lint changes

--- gef-ui/server/services/api.js
+++ gef-ui/server/services/api.js
@@ -1,6 +1,6 @@
 import Axios from './axios';
 
-const validateToken = async function (token) {
+const validateToken = async (token) => {
   try {
     Axios.defaults.headers.common.Authorization = token;
     const response = await Axios.get('/validate');
@@ -10,18 +10,14 @@ const validateToken = async function (token) {
   }
 };
 
-const getMandatoryCriteria = async function () {
-  try {
-    // const response = await Axios.get('/mandatory-criteria/latest')
-    const response = {
-      data: {
-        htmlText: '<p>Test</p>',
-      },
-    };
-    return response.data;
-  } catch (err) {
-    throw err;
-  }
+const getMandatoryCriteria = async () => {
+  // const response = await Axios.get('/mandatory-criteria/latest')
+  const response = {
+    data: {
+      htmlText: '<p>Test</p>',
+    },
+  };
+  return response.data;
 };
 
 export {
--- gef-ui/server/services/axios.js
+++ gef-ui/server/services/axios.js
@@ -5,8 +5,6 @@ require('dotenv').config();
 const { BASE_URL } = process.env;
 const VERSION = 'v1';
 
-console.log('poo', `${BASE_URL}/${VERSION}`);
-
 export default axios.create({
   baseURL: `${BASE_URL}/${VERSION}`,
   timeout: 1000,
