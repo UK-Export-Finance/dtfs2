13e5922b56840325007575b86af74e456a38e2ba
Andrew Price <andrew@madebymoon.com>
2021-02-17 15:06:52 +0000

    Fix for gef-ui e2e pipeline tests

--- gef-ui/server/routes/middleware/validate-token.js
+++ gef-ui/server/routes/middleware/validate-token.js
@@ -1,8 +1,8 @@
-import * as Api from '../../services/api';
+import * as api from '../../services/api';
 
 const validateToken = async (req, res, next) => {
   const { userToken } = req.session;
-  if (userToken && await Api.validateToken(userToken)) {
+  if (userToken && await api.validateToken(userToken)) {
     return next();
   }
 
