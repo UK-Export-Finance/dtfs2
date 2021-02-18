57f0fc130c0390bdde327358c8348b57c99838cc
Richard <info@monkemedia.co.uk>
2021-02-18 11:28:12 +0000

    Added unit and E2E tests

---
+++ e2e-tests/gef/cypress/integration/ineligible.spec.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+import relative from './relativeURL';
+import ineligible from './pages/ineligible';
+
+context('Ineligible Page', () => {
+  before(() => {
+    cy.fixture('login')
+      .then((res) => {
+        cy.login(res.MAKER);
+      });
+
+    cy.on('uncaught:exception', () => false);
+  });
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('connect.sid');
+    cy.visit(relative('/gef/ineligible'));
+  });
+
+  describe('Visiting page', () => {
+    it('displays the correct elements', () => {
+      ineligible.mainHeading();
+      ineligible.content();
+      ineligible.backButton();
+    });
+  });
+
+  describe('Clicking on Back Button', () => {
+    it('redirects user to ** page', () => {
+      ineligible.backButton().click();
+      cy.url().should('eq', relative('/gef/ineligible'));
+    });
+  });
+});
