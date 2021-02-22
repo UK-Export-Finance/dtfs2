c08d314dfaafc1c4bac22f890996f559fa08f27e
Richard <info@monkemedia.co.uk>
2021-02-22 18:18:41 +0000

    cleaned up tests

--- e2e-tests/gef/cypress/integration/name-application.spec.js
+++ e2e-tests/gef/cypress/integration/name-application.spec.js
@@ -30,41 +30,29 @@ context('Name Application Page', () => {
     });
   });
 
-  // describe('Clicking on Continue', () => {
-  //   it('shows error summary at top of page when no radio button has been selected', () => {
-  //     nameApplication.form().submit();
-  //     nameApplication.errorSummary();
-  //   });
+  describe('Clicking on Continue', () => {
+    it('shows error summary at top of page when fields have been left blank', () => {
+      nameApplication.form().submit();
+      nameApplication.errorSummary();
+    });
 
-  //   it('Clicking on error link in error summary takes you to correct field', () => {
-  //     nameApplication.form().submit();
-  //     nameApplication.errorSummary();
-  //     nameApplication.firstErrorLink().click();
-  //     cy.url().should('eq', relative('/gef/mandatory-criteria'));
-  //   });
+    it('shows validation error when no radio button has been selected', () => {
+      nameApplication.form().submit();
+      nameApplication.formError();
+    });
 
-  //   it('shows validation error when no radio button has been selected', () => {
-  //     nameApplication.form().submit();
-  //     nameApplication.formError();
-  //   });
+    it('Clicking on error link in error summary takes you to correct field', () => {
+      nameApplication.form().submit();
+      nameApplication.errorSummary();
+      nameApplication.firstErrorLink().click();
+      cy.url().should('eq', relative('/gef/name-application'));
+    });
+  });
 
-  //   it('redirects the user to ineligible page when they select `False`', () => {
-  //     nameApplication.falseRadio().click();
-  //     nameApplication.form().submit();
-  //     cy.url().should('eq', relative('/gef/ineligible'));
-  //   });
-
-  //   it('redirects the user to the Name Application page when they select `True`', () => {
-  //     nameApplication.trueRadio().click();
-  //     nameApplication.form().submit();
-  //     cy.url().should('eq', relative('/gef/name-application'));
-  //   });
-  // });
-
-  // describe('Clicking on Cancel', () => {
-  //   it('keeps the user on the same page FOR NOW', () => {
-  //     nameApplication.cancelButton().click();
-  //     cy.url().should('eq', relative('/gef/mandatory-criteria'));
-  //   });
-  // });
+  describe('Clicking on Cancel', () => {
+    it('keeps the user on the same page FOR NOW', () => {
+      nameApplication.cancelButton().click();
+      cy.url().should('eq', relative('/gef/name-application'));
+    });
+  });
 });
