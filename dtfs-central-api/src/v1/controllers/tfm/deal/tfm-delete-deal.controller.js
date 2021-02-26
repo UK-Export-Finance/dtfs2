97e14f65675d73834ed222bd4b1a12d3129ecc69
Andrew Price <andrew@madebymoon.com>
2021-02-26 15:25:47 +0000

    Linting

--- dtfs-central-api/src/v1/controllers/tfm/deal/tfm-delete-deal.controller.js
+++ dtfs-central-api/src/v1/controllers/tfm/deal/tfm-delete-deal.controller.js
@@ -2,12 +2,12 @@ const { findOneDeal } = require('./tfm-get-deal.controller');
 const db = require('../../../../drivers/db-client');
 
 exports.deleteDeal = async (req, res) => {
-  console.log('DELETE');
   findOneDeal(req.params.id, async (deal) => {
     if (deal) {
       const collection = await db.getCollection('tfm-deals');
       const facilitiesCollection = await db.getCollection('tfm-facilities');
       const status = await collection.deleteOne({ _id: req.params.id });
+      // eslint-disable-next-line no-underscore-dangle
       await facilitiesCollection.deleteMany({ facilitySnapshot: { associatedDealId: deal._id } });
       return res.status(200).send(status);
     }
