65ba76de5265b100aecc293e01e7b5507190a21a
Richard <info@monkemedia.co.uk>
2021-03-01 10:06:34 +0000

    First commit

--- gef-ui/server/routes/index.js
+++ gef-ui/server/routes/index.js
@@ -3,6 +3,7 @@ import mandatoryCriteriaRoutes from './mandatory-criteria';
 import nameApplicationRoutes from './name-application';
 import applicationDetailsRoutes from './application-details';
 import ineligibleRoutes from './ineligible';
+import automaticCoverRoutes from './automatic-cover';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use(mandatoryCriteriaRoutes);
 router.use(nameApplicationRoutes);
 router.use(ineligibleRoutes);
 router.use(applicationDetailsRoutes);
+router.use(automaticCoverRoutes);
 
 export default router;
