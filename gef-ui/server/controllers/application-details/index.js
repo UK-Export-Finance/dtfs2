7a76910712434b2b2156c30029148fdf007e3a83
Richard <info@monkemedia.co.uk>
2021-03-02 17:04:27 +0000

    Corrected broken unit tests

--- gef-ui/server/controllers/application-details/index.js
+++ gef-ui/server/controllers/application-details/index.js
@@ -9,62 +9,8 @@ const applicationDetails = async (req, res) => {
     const application = await api.getApplication(applicationId);
     const { exporterId } = application;
     const exporter = await api.getExporter(exporterId);
-    // const facilities = await api.getFacilities(applicationId);
+    const facilities = await api.getFacilities(applicationId);
     const exporterUrl = `/gef/application-details/${applicationId}`;
-
-    const mockedFacilities = {
-      status: 2,
-      items: [{
-        details: {
-          applicationId: '123',
-          type: 1,
-          hasBeenIssued: true,
-          name: 'My test',
-          startOnDayOfNotice: true,
-          coverStartDate: null,
-          coverEndDate: 1614682309534,
-          monthsOfCover: 7,
-          details: ['term-basis', 'committed-basis', 'other'],
-          detailsOther: null,
-          currency: 'GBP',
-          value: 2000,
-          coverPercentage: 20,
-          riskMarginPercentage: 14,
-          paymentType: Number,
-          createdAt: 1614682309534,
-          updatedAt: null,
-        },
-        validation: {
-          required: [],
-        },
-      },
-      {
-        details: {
-          applicationId: '123',
-          type: 2,
-          hasBeenIssued: true,
-          name: 'MONKEY',
-          startOnDayOfNotice: true,
-          banksMaximumLiability: 400000,
-          ukefMaximumLiability: 1600000,
-          coverStartDate: null,
-          coverEndDate: 1614682309534,
-          monthsOfCover: 7,
-          details: ['term-basis', 'committed-basis', 'other'],
-          detailsOther: null,
-          currency: 'GBP',
-          value: 2000,
-          coverPercentage: 20,
-          interestPercentage: 2.5,
-          paymentType: Number,
-          createdAt: 1614682309534,
-          updatedAt: null,
-        },
-        validation: {
-          required: [],
-        },
-      }],
-    };
 
     if (!application) {
       return res.render('partials/page-not-found');
@@ -76,8 +22,8 @@ const applicationDetails = async (req, res) => {
         rows: mapSummaryList(exporter, exporterItems(exporterUrl)),
       },
       facilities: {
-        status: status[mockedFacilities.status],
-        items: mockedFacilities.items.map((item) => ({
+        status: status[facilities.status],
+        items: facilities.items.map((item) => ({
           heading: facilityType[item.details.type],
           rows: mapSummaryList(item, facilityItems(exporterUrl)),
         })),
