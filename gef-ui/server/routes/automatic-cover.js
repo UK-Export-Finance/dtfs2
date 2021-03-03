61d7371ba240e86e5ad8028bc03d9f57986e52c5
Richard <info@monkemedia.co.uk>
2021-03-03 17:05:22 +0000

    Started adding form validation to the radio buttons

--- gef-ui/server/routes/automatic-cover.js
+++ gef-ui/server/routes/automatic-cover.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import {
   automaticCover,
-  // validateMandatoryCriteria,
+  validateAutomaticCover,
 } from '../controllers/automatic-cover';
 import validateToken from './middleware/validate-token';
 
 const router = express.Router();
 
 router.get('/automatic-cover', validateToken, (req, res) => automaticCover(req, res));
-// router.post('/mandatory-criteria', validateToken, (req, res) => validateMandatoryCriteria(req, res));
+router.post('/automatic-cover', validateToken, (req, res) => validateAutomaticCover(req, res));
 
 export default router;
