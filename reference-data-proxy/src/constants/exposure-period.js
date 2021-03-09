a8ab009e8a45d95c88788a4b5d98411d47adef5a
ttbarnes <tony@tonybarnes.me>
2021-03-09 14:23:07 +0000

    DTFS2-3404 - exposure-period api endpoint calling mulesoft

---
+++ reference-data-proxy/src/constants/exposure-period.js
@@ -0,0 +1,14 @@
+const FACILITY_TYPE = {
+  BOND: 'bond',
+  LOAN: 'loan',
+};
+
+const PRODUCT_GROUP = {
+  LOAN: 'EW',
+  BOND: 'BS',
+};
+
+module.exports = {
+  FACILITY_TYPE,
+  PRODUCT_GROUP,
+};
