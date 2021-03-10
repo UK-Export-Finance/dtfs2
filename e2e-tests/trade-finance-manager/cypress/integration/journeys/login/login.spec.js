e52a4031065423f7e2449eadbefe3f70d31e85f9
SimonTheDeveloper <mail@simon-neumann.com>
2021-03-10 17:17:05 +0000

    changed link to all deals page

--- e2e-tests/trade-finance-manager/cypress/integration/journeys/login/login.spec.js
+++ e2e-tests/trade-finance-manager/cypress/integration/journeys/login/login.spec.js
@@ -4,11 +4,11 @@ import MOCK_USERS from '../../../fixtures/users';
 
 context('User can login', () => {
   describe('Login page', () => {
-    it('should login and redirect to /deals/all when successful', () => {
+    it('should login and redirect to /deals when successful', () => {
       pages.landingPage.visit();
       pages.landingPage.email().type(MOCK_USERS[0].username);
       pages.landingPage.submitButton().click();
-      cy.url().should('eq', relative('/deals/all'));
+      cy.url().should('eq', relative('/deals'));
     });
 
     it('should not login and redirect to /deals when successful', () => {
