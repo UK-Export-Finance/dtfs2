0595fdddcbd56d75f7e88cecbc5683dedf7d2a06
ttbarnes <tony@tonybarnes.me>
2021-03-15 11:28:16 +0000

    DTFS2-3061 - tfm - task component test

---
+++ trade-finance-manager-ui/templates/case/tasks/task.component-test.js
@@ -0,0 +1,93 @@
+const pageRenderer = require('../../../component-tests/pageRenderer');
+const page = '../templates/case/tasks/task.njk';
+const render = pageRenderer(page);
+
+describe(page, () => {
+  let wrapper;
+  const params = {
+    dealId: '100200',
+    deal: {
+      details: {
+        ukefDealId: '1234',
+        submissionType: 'Automatic Inclusion Notice',
+      },
+      submissionDetails: {
+        supplierName: 'Supplier name',
+        buyerName: 'Buyer name',
+      },
+    },
+    task: {
+      title: 'Task title',
+      team: {
+        name: 'Underwriting support'
+      }
+    },
+    user: {
+      timezone: 'Europe/London',
+      firstName: 'Joe',
+      lastName: 'Bloggs',
+    },
+  };
+
+  beforeEach(() => {
+    wrapper = render(params);
+  });
+
+  it('should render task heading', () => {
+    wrapper.expectText('[data-cy="task-heading"]').toRead(params.task.title);
+  });
+
+  it('should render deal heading', () => {
+    wrapper.expectText('[data-cy="deal-heading"]').toRead('Deal');
+  });
+
+  it('should render deal subheading', () => {
+    const expected = `${params.deal.details.ukefDealId} ${params.deal.submissionDetails.supplierName} > ${params.deal.submissionDetails.buyerName}`;
+    wrapper.expectText('[data-cy="deal-subheading"]').toRead(expected);
+  });
+
+  it('should render deal subheading link to deal', () => {
+    wrapper.expectLink('[data-cy="deal-subheading-link"]')
+      .toLinkTo(`/case/${params.dealId}/deal`, params.deal.details.ukefDealId);
+  });
+
+  it('should render type of work heading', () => {
+    wrapper.expectText('[data-cy="type-of-work-heading"]').toRead('Type of work');
+  });
+
+  it('should render type of work subheading', () => {
+    wrapper.expectText('[data-cy="type-of-work-subheading"]').toRead(params.deal.details.submissionType);
+  });
+
+  it('should render team heading', () => {
+    wrapper.expectText('[data-cy="team-heading"]').toRead('Team');
+  });
+
+  it('should render team subheading', () => {
+    wrapper.expectText('[data-cy="team-subheading"]').toRead(params.task.team.name);
+  });
+
+  describe('form', () => {
+    it('should render `assigned to` select input', () => {
+      wrapper.expectElement('[data-cy="assigned-to-select-input"]').toExist();
+    });
+
+    it('should render `status` radio inputs', () => {
+      wrapper.expectElement('[data-cy="task-status-to-do"]').toExist();
+      wrapper.expectElement('[data-cy="task-status-in-progress"]').toExist();
+      wrapper.expectElement('[data-cy="task-status-done"]').toExist();
+    });
+
+    it('should render save button', () => {
+      wrapper.expectElement('[data-cy="save-button"]').toExist();
+      wrapper.expectText('[data-cy="save-button"]').toRead('Save and close');
+    });
+
+    it('should render close link', () => {
+      wrapper.expectLink('[data-cy="close-link"]')
+      .toLinkTo(`/case/${params.dealId}/tasks`, 'Close without saving');
+    });
+
+  });
+  
+});
