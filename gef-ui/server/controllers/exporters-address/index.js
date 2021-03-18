f4e76bf9964dbf210c4d23c796fb3c0972cae120
Richard <info@monkemedia.co.uk>
2021-03-18 14:17:22 +0000

    added errors

--- gef-ui/server/controllers/exporters-address/index.js
+++ gef-ui/server/controllers/exporters-address/index.js
@@ -19,7 +19,7 @@ const exportersAddress = async (req, res) => {
       errors: validationErrorHandler(postcodeError),
       companyName: details.companyName,
       correspondence: (postcodeError || addresses) ? 'true' : null,
-      selectedAddress: selectDropdownAddresses(addresses),
+      addressesForSelection: selectDropdownAddresses(addresses),
       addresses,
       registeredAddress,
       applicationId,
@@ -34,7 +34,7 @@ const validateExportersAddress = async (req, res) => {
   try {
     const { params, body } = req;
     const {
-      correspondence, postcode, selectedAddress, mappedAddresses,
+      correspondence, postcode, selectedAddress, addresses, addressesForSelection, addressLine1,
     } = body;
     const { applicationId } = params;
     const { exporterId } = await api.getApplication(applicationId);
@@ -42,6 +42,7 @@ const validateExportersAddress = async (req, res) => {
     const { registeredAddress } = details;
     let correspondenceError;
     let selectedAddressError;
+    let addressLine1Error;
 
     if (_isEmpty(correspondence)) {
       correspondenceError = {
@@ -69,14 +70,30 @@ const validateExportersAddress = async (req, res) => {
       };
     }
 
-    console.log('selected', parseFloat(selectedAddress));
-    console.log('address', JSON.parse(mappedAddresses).filter(((address) => address.value === parseFloat(selectedAddress))));
+    if (_isEmpty(addressLine1)) {
+      addressLine1Error = {
+        errRef: 'addressLine1',
+        errMsg: 'Enter the first line of the correspondence address',
+      };
+    }
+
+    const selectedAddressIndex = parseFloat(selectedAddress);
+    const addressesForSelectionWithSelection = JSON.parse(addressesForSelection).map((address) => {
+      if (address.value === selectedAddressIndex) {
+        return {
+          ...address,
+          selected: true,
+        };
+      }
+      return address;
+    });
 
     return res.render('partials/exporters-address.njk', {
-      errors: validationErrorHandler(correspondenceError || selectedAddressError),
+      errors: validationErrorHandler(correspondenceError || selectedAddressError || addressLine1Error),
       companyName: details.companyName,
-      addresses: JSON.parse(mappedAddresses),
-      populateAddressForm: '', // JSON.parse(mappedAddresses[parseFloat(selectedAddress)]),
+      addressesForSelection: addressesForSelectionWithSelection,
+      addresses: JSON.parse(addresses),
+      addressForm: JSON.parse(addresses)[selectedAddressIndex],
       registeredAddress,
       applicationId,
       correspondence,
@@ -122,6 +139,37 @@ const postcodeSearch = async (req, res) => {
       req.session.postcode = postcode;
       return res.redirect(`/gef/application-details/${applicationId}/exporters-address`);
     }
+    return res.render('partials/problem-with-service.njk');
+  }
+};
+
+const useSelectedAddress = async (req, res) => {
+  const { params, body } = req;
+  const { postcode } = body;
+  const { applicationId } = params;
+
+  if (_isEmpty(postcode)) {
+    req.session.postcodeError = {
+      errRef: 'postcode',
+      errMsg: 'Enter a postcode',
+    };
+    return res.redirect(`/gef/application-details/${applicationId}/exporters-address`);
+  }
+
+  try {
+    const addresses = await api.getAddressesByPostcode(postcode);
+
+    req.session.addresses = addresses;
+    req.session.postcode = postcode;
+
+    return res.redirect(`/gef/application-details/${applicationId}/exporters-address`);
+  } catch (err) {
+    if (err.status === 422) {
+      req.session.postcodeError = err.data;
+      req.session.postcode = postcode;
+      return res.redirect(`/gef/application-details/${applicationId}/exporters-address`);
+    }
+    return res.render('partials/problem-with-service.njk');
   }
 };
 
@@ -129,4 +177,5 @@ export {
   exportersAddress,
   validateExportersAddress,
   postcodeSearch,
+  useSelectedAddress,
 };
