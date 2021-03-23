f409a6f341c9e05073d612b104d05ccff1a4be32
ttbarnes <tony@tonybarnes.me>
2021-03-23 17:08:14 +0000

    DTFS2-3394 - tfm/portal - e2e test for loan changing from unissued to issued

--- e2e-tests/trade-finance-manager/cypress/integration/pages/facilityPage.js
+++ e2e-tests/trade-finance-manager/cypress/integration/pages/facilityPage.js
@@ -1,15 +1,16 @@
 const facilityPage = {
+  firstDrawdownAmountInExportCurrency: () => cy.get('[data-cy="first-drawdown-amount-in-export-currency"]'),
+  facilityStage: () => cy.get('[data-cy="facility-stage"]'),
+
   facilityValueExportCurrency: () => cy.get('[data-cy="facility-value-export-currency"]'),
   facilityValueGbp: () => cy.get('[data-cy="facility-value-gbp"]'),
+
   facilityMaximumUkefExposure: () => cy.get('[data-cy="facility-maximum-ukef-exposure"]'),
-  facilityTenor: () => cy.get('[data-cy="facility-tenor"]'),
 
-  facilityStage: () => cy.get('[data-cy="facility-stage"]'),
   facilityBankIssueNoticeReceived: () => cy.get('[data-cy="facility-bank-issue-notice-received"]'),
-
   facilityCoverStartDate: () => cy.get('[data-cy="facility-cover-start-date"]'),
   facilityCoverEndDate: () => cy.get('[data-cy="facility-cover-end-date"]'),
-
+  facilityTenor: () => cy.get('[data-cy="facility-tenor"]'),
 };
 
 module.exports = facilityPage;
