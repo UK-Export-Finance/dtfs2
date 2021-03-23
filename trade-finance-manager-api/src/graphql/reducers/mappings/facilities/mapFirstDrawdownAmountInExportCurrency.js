1427b229f9e9aacb5230c01a2295bb68e6c78cc0
ttbarnes <tony@tonybarnes.me>
2021-03-23 16:53:13 +0000

    DTFS2-3394 - tfm - fix facility mapping issue for different disbursementAmount values/formatting

--- trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFirstDrawdownAmountInExportCurrency.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFirstDrawdownAmountInExportCurrency.js
@@ -1,13 +1,18 @@
 const CONSTANTS = require('../../../../constants');
 const { formattedNumber } = require('../../../../utils/number');
+const { stripCommas } = require('../../../../utils/string');
 
 const mapFirstDrawdownAmountInExportCurrency = (facility) => {
   const { facilityProduct } = facility;
 
-  if (facilityProduct.code === CONSTANTS.FACILITIES.FACILITY_PRODUCT_CODE.LOAN) {
-    const formattedDisbursementAmount = formattedNumber(facility.disbursementAmount, 4, 4);
+  if ((facilityProduct.code === CONSTANTS.FACILITIES.FACILITY_PRODUCT_CODE.LOAN)
+    && facility.disbursementAmount) {
 
-    return `${facility.currency} ${formattedDisbursementAmount}`;
+    const strippedDisbursementAmount = stripCommas(facility.disbursementAmount);
+
+    const formattedDisbursementAmount = formattedNumber(strippedDisbursementAmount, 4, 4);
+
+    return `${facility.currency.id} ${formattedDisbursementAmount}`;
   }
 
   return null;
