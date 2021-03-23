e89e25e97a80b674a5cabba8b36fa62592e4b863
ttbarnes <tony@tonybarnes.me>
2021-03-23 15:55:25 +0000

    DTFS2-3393 - tfm/portal - e2e test for bond changing from unissued to issued

--- e2e-tests/trade-finance-manager/cypress/integration/pages/facilityPage.js
+++ e2e-tests/trade-finance-manager/cypress/integration/pages/facilityPage.js
@@ -3,6 +3,13 @@ const facilityPage = {
   facilityValueGbp: () => cy.get('[data-cy="facility-value-gbp"]'),
   facilityMaximumUkefExposure: () => cy.get('[data-cy="facility-maximum-ukef-exposure"]'),
   facilityTenor: () => cy.get('[data-cy="facility-tenor"]'),
+
+  facilityStage: () => cy.get('[data-cy="facility-stage"]'),
+  facilityBankIssueNoticeReceived: () => cy.get('[data-cy="facility-bank-issue-notice-received"]'),
+
+  facilityCoverStartDate: () => cy.get('[data-cy="facility-cover-start-date"]'),
+  facilityCoverEndDate: () => cy.get('[data-cy="facility-cover-end-date"]'),
+
 };
 
 module.exports = facilityPage;
