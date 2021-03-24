7b987004a3a6ce2f39ba994ee28a98955f46920a
ttbarnes <tony@tonybarnes.me>
2021-03-24 22:56:35 +0000

    DTFS2-2738 - tfm - price annd risk - side navigation active links

---
+++ trade-finance-manager-ui/templates/case/underwriting/pricing-and-risk/_macros/side-navigation.component-test.js
@@ -0,0 +1,26 @@
+const componentRenderer = require('../../../../../component-tests/componentRenderer');
+const component = '../templates/case/underwriting/pricing-and-risk/_macros/side-navigation.njk';
+
+const render = componentRenderer(component);
+
+describe(component, () => {
+  let wrapper;
+
+  it('should rennder 3 links', () => {
+    const params = {
+      caseId: '1234',
+    };
+
+    wrapper = render(params);
+
+    wrapper.expectLink('[data-cy="bank-security"] a')
+      .toLinkTo(`/case/${params.caseId}/underwriting/bank-security`, 'Bank security');
+
+    wrapper.expectLink('[data-cy="pricing-and-risk"] a')
+      .toLinkTo(`/case/${params.caseId}/underwriting/pricing-and-risk`, 'Pricing and risk');
+
+    wrapper.expectLink('[data-cy="underwriter-managers-decision"] a')
+      .toLinkTo(`/case/${params.caseId}/underwriting/underwriter-managers-decision`, 'Underwriter manager’s decision');
+  });
+
+});
