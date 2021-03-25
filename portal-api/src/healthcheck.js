1a747bcea1312e6c742f98f78a6bd5893afecaa3
Andrew Price <andrew@madebymoon.com>
2021-03-25 16:57:36 +0000

    Removed external services healthcheck on portal-api

--- portal-api/src/healthcheck.js
+++ portal-api/src/healthcheck.js
@@ -7,50 +7,50 @@ const shareTest = require('./fileshare-test');
 
 const router = express.Router();
 const GITHUB_SHA = process.env.GITHUB_SHA || 'undefined';
-const { MONGODB_URI } = process.env;
-const MONGO_INITDB_DATABASE = process.env.MONGO_INITDB_DATABASE || 'test';
-const STORAGE_ACCOUNT = process.env.AZURE_PORTAL_STORAGE_ACCOUNT;
+// const { MONGODB_URI } = process.env;
+// const MONGO_INITDB_DATABASE = process.env.MONGO_INITDB_DATABASE || 'test';
+// const STORAGE_ACCOUNT = process.env.AZURE_PORTAL_STORAGE_ACCOUNT;
 // const STORAGE_ACCESS_KEY = process.env.AZURE_PORTAL_STORAGE_ACCESS_KEY;
 
-async function pingMongo() {
-  if (!MONGODB_URI) {
-    return 'MONGODB_URI is empty.';
-  }
+// async function pingMongo() {
+//   if (!MONGODB_URI) {
+//     return 'MONGODB_URI is empty.';
+//   }
 
-  let client;
-  try {
-    client = await MongoClient.connect(MONGODB_URI);
-    return await client.db(MONGO_INITDB_DATABASE).listCollections().toArray();
-  } catch (error) {
-    return util.inspect(error);
-  } finally {
-    if (client) {
-      client.close();
-    }
-  }
-}
+//   let client;
+//   try {
+//     client = await MongoClient.connect(MONGODB_URI);
+//     return await client.db(MONGO_INITDB_DATABASE).listCollections().toArray();
+//   } catch (error) {
+//     return util.inspect(error);
+//   } finally {
+//     if (client) {
+//       client.close();
+//     }
+//   }
+// }
 
-async function pingStorage(doFetch) {
-  if (!doFetch) {
-    return {
-      STORAGE_ACCOUNT,
-    };
-  }
+// async function pingStorage(doFetch) {
+//   if (!doFetch) {
+//     return {
+//       STORAGE_ACCOUNT,
+//     };
+//   }
 
-  const fileshare = await shareTest();
-  return {
-    STORAGE_ACCOUNT,
-    fileshare,
-  };
-}
+//   const fileshare = await shareTest();
+//   return {
+//     STORAGE_ACCOUNT,
+//     fileshare,
+//   };
+// }
 
-async function pingNotify() {
-  const notifyClient = new NotifyClient(process.env.GOV_NOTIFY_API_KEY);
-  return notifyClient
-    .getAllTemplates('email')
-    .then((response) => response.statusCode)
-    .catch((error) => util.inspect(error));
-}
+// async function pingNotify() {
+//   const notifyClient = new NotifyClient(process.env.GOV_NOTIFY_API_KEY);
+//   return notifyClient
+//     .getAllTemplates('email')
+//     .then((response) => response.statusCode)
+//     .catch((error) => util.inspect(error));
+// }
 
 router.get('/healthcheck', (req, res) => {
   res.status(200).json({
