1b5ba401438db743f938f5625ab4676085198f55
ttbarnes <tony@tonybarnes.me>
2021-03-26 16:39:43 +0000

    DTFS2-3741 - tfm - total of facilities in gbp in case summary

--- trade-finance-manager-api/src/graphql/reducers/mappings/deal/mapTotals.api-test.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/deal/mapTotals.api-test.js
@@ -1,34 +1,24 @@
 const mapTotals = require('./mapTotals');
 const { formattedNumber } = require('../../../../utils/number');
-const { stripCommas } = require('../../../../utils/string');
 
 describe('mapTotals', () => {
   const mockFacilities = [
     {
-      facilitySnapshot: {
-        facilityValue: '1500.45',
-        currency: {
-          id: 'GBP',
-        },
-        ukefExposure: '80,000.00',
+      tfm: {
+        ukefExposure: 80000.00,
+        facilityValueInGBP: 2117.4290881821,
       },
     },
     {
-      facilitySnapshot: {
-        facilityValue: '3200.00',
-        currency: {
-          id: 'GBP',
-        },
-        ukefExposure: '23,000.00',
+      tfm: {
+        ukefExposure: 23000.00,
+        facilityValueInGBP: 1034.7800881821,
       },
     },
     {
-      facilitySnapshot: {
-        facilityValue: '150.10',
-        currency: {
-          id: 'GBP',
-        },
-        ukefExposure: '8,000.00',
+      tfm: {
+        ukefExposure: 8000.00,
+        facilityValueInGBP: 3200.567,
       },
     },
   ];
@@ -36,9 +26,9 @@ describe('mapTotals', () => {
   it('should return formatted total of all facility values', async () => {
     const result = mapTotals(mockFacilities);
 
-    const totalValue = Number(mockFacilities[0].facilitySnapshot.facilityValue)
-                       + Number(mockFacilities[1].facilitySnapshot.facilityValue)
-                       + Number(mockFacilities[2].facilitySnapshot.facilityValue);
+    const totalValue = Number(mockFacilities[0].tfm.facilityValueInGBP)
+                       + Number(mockFacilities[1].tfm.facilityValueInGBP)
+                       + Number(mockFacilities[2].tfm.facilityValueInGBP);
 
     const expected = `GBP ${formattedNumber(totalValue)}`;
     expect(result.facilitiesValueInGBP).toEqual(expected);
@@ -47,32 +37,11 @@ describe('mapTotals', () => {
   it('should return formatted total of all facilities ukefExposure', () => {
     const result = mapTotals(mockFacilities);
 
-    const asNumber = (str) => Number(stripCommas(str));
-
-    const totalUkefExposure = asNumber(mockFacilities[0].facilitySnapshot.ukefExposure)
-      + asNumber(mockFacilities[1].facilitySnapshot.ukefExposure)
-      + asNumber(mockFacilities[2].facilitySnapshot.ukefExposure);
+    const totalUkefExposure = mockFacilities[0].tfm.ukefExposure
+      + mockFacilities[1].tfm.ukefExposure
+      + mockFacilities[2].tfm.ukefExposure;
 
     const expected = `GBP ${formattedNumber(totalUkefExposure)}`;
     expect(result.facilitiesUkefExposure).toEqual(expected);
-  });
-
-  // TODO: until we figure out which API to use for conversion from non-GBP.
-  describe('when some facilities are NOT GBP', () => {
-    it('should not return facilitiesValueInGBP', () => {
-      const result = mapTotals([
-        ...mockFacilities,
-        {
-          facilitySnapshot: {
-            facilityValue: '150.10',
-            currency: {
-              id: 'USD',
-            },
-            ukefExposure: '8,000.00',
-          },
-        },
-      ]);
-      expect(result.facilitiesValueInGBP).toEqual(undefined);
-    });
   });
 });
