4e462869d55ff8ae8902f07250b0641b96c4c0a3
ttbarnes <tony@tonybarnes.me>
2021-03-27 15:44:29 +0000

    DTFS2-3059 - tfm - tasks - filter by team/user via graphql query/resolver

--- trade-finance-manager-api/src/graphql/resolvers/query-deal.js
+++ trade-finance-manager-api/src/graphql/resolvers/query-deal.js
@@ -1,12 +1,22 @@
 const dealReducer = require('../reducers/deal');
 const { findOneDeal } = require('../../v1/controllers/deal.controller');
+const { filterTasks } = require('./filters/filterTasks');
 
 require('dotenv').config();
 
-const queryDeal = async ({ _id }) => {
+const queryDeal = async ({ params }) => {
+  const { _id, tasksFilters } = params;
+
   const deal = await findOneDeal(_id);
 
-  return dealReducer(deal);
+  const filtered = {
+    ...deal,
+    tfm: {
+      ...deal.tfm,
+      tasks: filterTasks(deal.tfm.tasks, tasksFilters),
+    },
+  };
+  return dealReducer(filtered);
 };
 
 module.exports = queryDeal;
