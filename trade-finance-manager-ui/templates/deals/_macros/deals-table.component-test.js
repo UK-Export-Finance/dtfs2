c886f5ec5f3fe4b7949b7e015dce1fed1d08fcb2
ttbarnes <tony@tonybarnes.me>
2021-03-30 11:57:52 +0100

    DTFS2-3675 - tfm - fix deals-table component tests.

--- trade-finance-manager-ui/templates/deals/_macros/deals-table.component-test.js
+++ trade-finance-manager-ui/templates/deals/_macros/deals-table.component-test.js
@@ -1,66 +1,53 @@
 /* eslint-disable no-underscore-dangle */
 const componentRenderer = require('../../../component-tests/componentRenderer');
 
-const component = './deals-table.njk';
+const component = '../templates/deals/_macros/deals-table.njk';
 
 const render = componentRenderer(component);
 
-describe.skip(component, () => {
+describe(component, () => {
   let wrapper;
   const params = {
     deals: [
       {
         _id: '123',
-        ukefdealID: '0040004833',
-        dealProduct: {
-          code: 'BSS',
+        dealSnapshot: {
+          details: {
+            ukefDealId: '0040004833',
+            submissionType: 'Automatic Inclusion Notice',
+            owningBank: {
+              name: 'Bank name',
+            },
+          },
+          submissionDetails: {
+            supplierName: 'Supplier name',
+            buyerName: 'Buyer name',
+          },
         },
-        coverEndDate: '02 Nov 2021',
-        ukefExposure: 'GBP 1,234.00',
-        coveredPercentage: '20%',
-        dealType: 'Performance Bond',
-        dealValue: 'GBP 1,234',
-        dealValueExportCurrency: 'AUD 34000',
-        dealStage: 'Commitment',
+        tfm: {
+          product: 'EWCS',
+          stage: 'Confirmed',
+        },
       },
       {
         _id: '456',
-        ukefdealID: '0040004833',
-        dealProduct: {
-          code: 'EWCS',
+        dealSnapshot: {
+          details: {
+            ukefDealId: '0040004834',
+            submissionType: 'Automatic Inclusion Notice',
+            owningBank: {
+              name: 'Bank name',
+            },
+          },
+          submissionDetails: {
+            supplierName: 'Supplier name',
+            buyerName: 'Buyer name',
+          },
         },
-        coverEndDate: '04 Dec 2021',
-        ukefExposure: 'GBP 2,469.00',
-        coveredPercentage: '20%',
-        dealValue: 'GBP 1,234',
-        dealValueExportCurrency: 'AUD 34000',
-        dealStage: 'Issued',
-      },
-      {
-        _id: '789',
-        ukefdealID: '0040004833',
-        dealProduct: {
-          code: 'EWCS',
+        tfm: {
+          product: 'EWCS',
+          stage: 'Confirmed',
         },
-        coverEndDate: '04 Dec 2021',
-        ukefExposure: 'GBP 2,469.00',
-        coveredPercentage: '20%',
-        dealValue: '',
-        dealValueExportCurrency: 'AUD 34000',
-        dealStage: 'Commitment',
-      },
-      {
-        _id: '112',
-        ukefdealID: '0040004833',
-        dealProduct: {
-          code: '',
-        },
-        coverEndDate: '04 Dec 2021',
-        ukefExposure: 'GBP 2,469.00',
-        coveredPercentage: '20%',
-        dealValue: 'GBP 1,234',
-        dealValueExportCurrency: 'AUD 34000',
-        dealStage: 'Issued',
       },
     ],
     totals: {
@@ -75,7 +62,7 @@ describe.skip(component, () => {
 
   describe('table headings', () => {
     it('should render `deal id` table heading', () => {
-      wrapper.expectText('[data-cy="deals-table-heading-deal-id"]').toRead('deal ID');
+      wrapper.expectText('[data-cy="deals-table-heading-deal-id"]').toRead('Deal ID');
     });
 
     it('should render `product` table heading', () => {
@@ -86,142 +73,85 @@ describe.skip(component, () => {
       wrapper.expectText('[data-cy="deals-table-heading-type"]').toRead('Type');
     });
 
+    it('should render `exporter` table heading', () => {
+      wrapper.expectText('[data-cy="deals-table-heading-exporter"]').toRead('Exporter');
+    });
+
+    it('should render `buyer` table heading', () => {
+      wrapper.expectText('[data-cy="deals-table-heading-buyer"]').toRead('Buyer');
+    });
+
+    it('should render `bank` table heading', () => {
+      wrapper.expectText('[data-cy="deals-table-heading-bank"]').toRead('Bank');
+    });
+
     it('should render `stage` table heading', () => {
       wrapper.expectText('[data-cy="deals-table-heading-stage"]').toRead('Stage');
     });
 
-    it('should render `tenor` table heading', () => {
-      wrapper.expectText('[data-cy="deals-table-heading-tenor"]').toRead('Tenor');
-    });
-
-    it('should render `cover end date` table heading', () => {
-      wrapper.expectText('[data-cy="deals-table-heading-cover-end-date"]').toRead('Cover end date');
-    });
-
-    it('should render `value (export currency)` table heading', () => {
-      wrapper.expectText('[data-cy="deals-table-heading-value-export-currency"]').toRead('Value (export currency)');
-    });
-
-    it('should render `value (GBP)` table heading', () => {
-      wrapper.expectText('[data-cy="deals-table-heading-value-gbp"]').toRead('Value (GBP)');
-    });
-
-    it('should render `UKEF exposure` table heading', () => {
-      wrapper.expectText('[data-cy="deals-table-heading-ukef-exposure"]').toRead('UKEF exposure');
+    it('should render `date received` table heading', () => {
+      wrapper.expectText('[data-cy="deals-table-heading-date-received"]').toRead('Date received');
     });
   });
 
 
   describe('for each deal', () => {
-    it('should render ukefdealID link, linking to deal id', () => {
+    it('should render ukefDealId link, linking to deal id', () => {
       params.deals.forEach((deal) => {
         const selector = `[data-cy="deal-${deal._id}-ukef-deal-id-link"]`;
 
         wrapper.expectLink(selector).toLinkTo(
-          `/case/deal/${deal._id}`,
-          deal.ukefdealID,
+          `/case/${deal._id}/deal`,
+          deal.dealSnapshot.details.ukefDealId,
         );
       });
     });
 
     describe('dealProduct table cell', () => {
       it('should render value', () => {
-        const deals = params.deals.filter((f) => f.dealProduct.code !== '');
-
-        deals.forEach((deal) => {
+        params.deals.forEach((deal) => {
           const cellSelector = `[data-cy="deal-${deal._id}-product"]`;
-          wrapper.expectText(cellSelector).toRead(deal.dealProduct.code);
+          wrapper.expectText(cellSelector).toRead(deal.tfm.product);
         });
-      });
-
-      it('should render a dash when dealProduct is empty', () => {
-        const deal = params.deals.find((f) => f.dealProduct.code === '');
-
-        const cellSelector = `[data-cy="deal-${deal._id}-type"]`;
-        wrapper.expectText(cellSelector).toRead('-');
       });
     });
 
     describe('dealType table cell', () => {
-      it('should render dealType when deal is bond', () => {
-        const bond = params.deals.find((f) => f.dealProduct.code === 'BSS');
+      it('should render submissionType', () => {
 
-        const cellSelector = `[data-cy="deal-${bond._id}-type"]`;
-        wrapper.expectText(cellSelector).toRead(bond.dealType);
+        params.deals.forEach((deal) => {
+          const cellSelector = `[data-cy="deal-${deal._id}-type"]`;
+          wrapper.expectText(cellSelector).toRead(deal.dealSnapshot.details.submissionType);
+        });
       });
+    });
 
-      it('should render a dash when deal is loan', () => {
-        const deal = params.deals.find((f) => f.dealProduct.code !== 'BSS');
+    it('should render exporter name table cell', () => {
+      params.deals.forEach((deal) => {
+        const cellSelector = `[data-cy="deal-${deal._id}-exporterName"]`;
+          wrapper.expectText(cellSelector).toRead(deal.dealSnapshot.submissionDetails.supplierName);
+      });
+    });
 
-        const cellSelector = `[data-cy="deal-${deal._id}-type"]`;
-        wrapper.expectText(cellSelector).toRead('-');
+    it('should render buyer name table cell', () => {
+      params.deals.forEach((deal) => {
+        const cellSelector = `[data-cy="deal-${deal._id}-buyerName"]`;
+        wrapper.expectText(cellSelector).toRead(deal.dealSnapshot.submissionDetails.buyerName);
+      });
+    });
+
+    it('should render buyer name table cell', () => {
+      params.deals.forEach((deal) => {
+        const cellSelector = `[data-cy="deal-${deal._id}-bank"]`;
+        wrapper.expectText(cellSelector).toRead(deal.dealSnapshot.details.owningBank.name);
       });
     });
 
     it('should render dealStage table cell', () => {
       params.deals.forEach((deal) => {
         const cellSelector = `[data-cy="deal-${deal._id}-stage"]`;
-        wrapper.expectText(cellSelector).toRead(deal.dealStage);
+        wrapper.expectText(cellSelector).toRead(deal.tfm.stage);
       });
-    });
-
-    it('should render coverEndDate table cell', () => {
-      params.deals.forEach((deal) => {
-        const cellSelector = `[data-cy="deal-${deal._id}-cover-end-date"]`;
-        wrapper.expectText(cellSelector).toRead(`${deal.coverEndDate} (expected)`);
-      });
-    });
-
-    it('should render `value (export currency)` table cell', () => {
-      params.deals.forEach((deal) => {
-        const cellSelector = `[data-cy="deal-${deal._id}-value-export-currency"]`;
-        wrapper.expectText(cellSelector).toRead(deal.dealValueExportCurrency);
-      });
-    });
-
-    describe('`dealValue in GBP` table cell', () => {
-      it('should render', () => {
-        const deals = params.deals.filter((f) => f.dealValue !== '');
-
-        deals.forEach((deal) => {
-          const cellSelector = `[data-cy="deal-${deal._id}-value-gbp"]`;
-          wrapper.expectText(cellSelector).toRead(deal.dealValue);
-        });
-      });
-
-      it('should render a dash when dealValue is empty', () => {
-        const deal = params.deals.find((f) => f.dealValue === '');
-
-        const cellSelector = `[data-cy="deal-${deal._id}-value-gbp"]`;
-        wrapper.expectText(cellSelector).toRead('-');
-      });
-    });
-
-
-    it('should render ukefExposure', () => {
-      params.deals.forEach((deal) => {
-        const cellSelector = `[data-cy="deal-${deal._id}-ukef-exposure"]`;
-        wrapper.expectText(cellSelector).toRead(`${deal.ukefExposure}`);
-      });
-    });
-
-    it('should render coveredPercentage', () => {
-      params.deals.forEach((deal) => {
-        const cellSelector = `[data-cy="deal-${deal._id}-covered-percentage"]`;
-        wrapper.expectText(cellSelector).toRead(`(${deal.coveredPercentage})`);
-      });
-    });
-  });
-
-  describe('totals row', () => {
-    it('should render totals.dealsValueInGBP', () => {
-      const cellSelector = '[data-cy="deals-total-value"]';
-      wrapper.expectText(cellSelector).toRead(params.totals.dealsValueInGBP);
-    });
-
-    it('should render totals.dealsUkefExposure', () => {
-      const cellSelector = '[data-cy="deals-total-ukef-exposure"]';
-      wrapper.expectText(cellSelector).toRead(params.totals.dealsUkefExposure);
     });
   });
 });
