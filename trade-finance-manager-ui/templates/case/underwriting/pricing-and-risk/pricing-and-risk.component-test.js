3d59ce142d2be88a26ce9818008bea565f658a0c
ttbarnes <tony@tonybarnes.me>
2021-03-30 22:52:57 +0100

    DTFS2-3783 - tfm - pricing-and-risk - templates organisation

--- trade-finance-manager-ui/templates/case/underwriting/pricing-and-risk/pricing-and-risk.component-test.js
+++ trade-finance-manager-ui/templates/case/underwriting/pricing-and-risk/pricing-and-risk.component-test.js
@@ -47,61 +47,14 @@ describe(page, () => {
     wrapper.expectText('[data-cy="pricing-and-risk-heading"]').toRead('Pricing and risk');
   });
 
-  describe('exporter section', () => {
-
-    it('should render Exporter heading', () => {
-      const wrapper = render(params);
-      wrapper.expectText('[data-cy="exporter-heading"]').toRead('Exporter');
-    });
-
-    it('should render link to add credit rating', () => {
-      const wrapper = render(params);
-      wrapper.expectLink('[data-cy="add-credit-rating-link"]')
-        .toLinkTo(`/case/${params.dealId}/underwriting/pricing-and-risk/edit`, `Add a credit rating for ${params.deal.submissionDetails.supplierName}`);
-    });
-
-    describe('with params.exporterCreditRating', () => {
-      it('should NOT render link to add credit rating', () => {
-        params = {
-          ...params,
-          tfm: {
-            exporterCreditRating: 'Good (BB-)',
-          },
-        };
-
-        const wrapper = render(params);
-        wrapper.expectElement('[data-cy="add-credit-rating-link"]').notToExist();
-      });
-
-      it('should render credit rating table', () => {
-        params = {
-          ...params,
-          tfm: {
-            exporterCreditRating: 'Good (BB-)',
-          },
-        };
-
-        const wrapper = render(params);
-        wrapper.expectElement('[data-cy="credit-rating-table"]').toExist();
-      });
-    });
-
+  it('should render exporter section', () => {
+    const wrapper = render(params);
+    wrapper.expectElement('[data-cy="section-exporter"]').toExist();
   });
 
-  describe('facilities section', () => {
-    
-    it('should render Facilities heading', () => {
-      const wrapper = render(params);
-      wrapper.expectText('[data-cy="facilities-heading"]').toRead('Facilities');
-    });
-
-    it('should render facility-pricing-risk-table for each facility', () => {
-      const wrapper = render(params);
-
-      params.deal.facilities.forEach((facility) => {
-        wrapper.expectElement(`[data-cy="facility-${facility._id}-pricing-risk-table"]`).toExist();
-      });
-    });
-
+  it('should render facilities section', () => {
+    const wrapper = render(params);
+    wrapper.expectElement('[data-cy="section-facilities"]').toExist();
   });
+
 });
