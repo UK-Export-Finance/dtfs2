eb3b68f4e34a47df3f7b2e5edb8ea21e1ff705dc
Richard <info@monkemedia.co.uk>
2021-04-06 10:35:23 +0100

    just playing

--- gef-ui/server/controllers/about-facility/index.js
+++ gef-ui/server/controllers/about-facility/index.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import * as api from '../../services/api';
 import { FACILITY_TYPE } from '../../../constants';
 
@@ -5,11 +6,12 @@ const aboutFacility = async (req, res) => {
   const { params } = req;
   const { facilityId } = params;
 
-
   try {
     const { details } = await api.getFacility(facilityId);
+    const { coverStartDate } = details;
     const facilityTypeString = FACILITY_TYPE[details.type].toLowerCase();
     const hasCoverStartDate = JSON.stringify(details.hasCoverStartDate);
+    console.log('details', moment(1617275806087, 'DD/MM/YYYY'));
     return res.render('partials/about-facility.njk', {
       facilityType: facilityTypeString,
       hasCoverStartDate: hasCoverStartDate !== 'null' ? hasCoverStartDate : null,
