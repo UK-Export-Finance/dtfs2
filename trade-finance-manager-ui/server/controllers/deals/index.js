eae0457c824f88ea0862b65e2ce6133b79ab15dd
ttbarnes <tony@tonybarnes.me>
2021-04-06 17:14:15 +0100

    tfm - fix login e2e test

--- trade-finance-manager-ui/server/controllers/deals/index.js
+++ trade-finance-manager-ui/server/controllers/deals/index.js
@@ -2,22 +2,10 @@ import api from '../../api';
 
 const getDeals = async (req, res) => {
   const deals = await api.getDeals();
+
   if (!deals) {
     return res.redirect('/not-found');
   }
-
-  const compare = (a, b) => {
-    if (b.dealSnapshot.details.submissionDate < a.dealSnapshot.details.submissionDate) {
-      return -1;
-    }
-    if (b.dealSnapshot.details.submissionDate > a.dealSnapshot.details.submissionDate) {
-      return 1;
-    }
-    return 0;
-  };
-
-  deals.deals.sort((a, b) =>
-    compare(a, b));
 
   return res.render('deals/deals.njk', {
     deals,
