23452b9077292b2a02a0579c87f33f9c262cb57b
Richard <info@monkemedia.co.uk>
2021-04-07 14:56:04 +0100

    Unit tests

--- gef-ui/server/controllers/about-facility/index.test.js
+++ gef-ui/server/controllers/about-facility/index.test.js
@@ -1,4 +1,4 @@
-import aboutFacility from './index';
+import { aboutFacility, validateAboutFacility } from './index';
 import * as api from '../../services/api';
 
 const MockResponse = () => {
@@ -13,6 +13,7 @@ const MockRequest = () => {
   req.params = {};
   req.query = {};
   req.body = {};
+  req.params.applicationId = '123';
   req.params.facilityId = 'xyz';
   return req;
 };
@@ -21,24 +22,47 @@ const MockAboutFacilityResponse = () => {
   const res = {};
   res.details = {
     type: 'CASH',
+    name: 'Foundry4',
+    hasBeenIssued: true,
+    monthsOfCover: null,
+    coverStartDate: '2030-01-02T00:00:00.000+00:00',
+    hasCoverStartDate: true,
+    coverEndDate: null,
   };
   return res;
 };
+
+const updateFacilitySpy = jest.spyOn(api, 'updateFacility').mockImplementationOnce(() => Promise.resolve());
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe('GET Facility', () => {
-  it('renders the `Facility` template', async () => {
+describe('GET About Facility', () => {
+  it('renders the `About Facility` template', async () => {
     const mockResponse = new MockResponse();
     const mockRequest = new MockRequest();
     const mockAboutFacilityResponse = new MockAboutFacilityResponse();
+
+    mockRequest.query.status = 'change';
 
     api.getFacility = () => Promise.resolve(mockAboutFacilityResponse);
     await aboutFacility(mockRequest, mockResponse);
     expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
       facilityType: 'CASH',
+      facilityName: 'Foundry4',
+      hasBeenIssued: true,
+      monthsOfCover: null,
+      hasCoverStartDate: 'true',
+      coverStartDateDay: '2',
+      coverStartDateMonth: '1',
+      coverStartDateYear: '2030',
+      coverEndDateMonth: null,
+      coverEndDateYear: null,
+      facilityTypeString: 'cash',
+      applicationId: '123',
+      facilityId: 'xyz',
+      status: 'change',
     }));
   });
 
@@ -49,5 +73,37 @@ describe('GET Facility', () => {
     api.getFacility = () => Promise.reject();
     await aboutFacility(mockRequest, mockResponse);
     expect(mockResponse.render).toHaveBeenCalledWith('partials/problem-with-service.njk');
+  });
+});
+
+describe('Validate About Facility', () => {
+  it('redirects user to application page if application page if save and return is set to true', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    const mockAboutFacilityResponse = new MockAboutFacilityResponse();
+    mockRequest.query.saveAndReturn = 'true';
+    mockRequest.body.facilityType = 'CASH';
+
+    api.updateFacility = () => Promise.resolve(mockAboutFacilityResponse);
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.redirect).toHaveBeenCalledWith('/gef/application-details/123');
+  });
+
+  it('sets the correct date format using single day, month and year values', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    const mockAboutFacilityResponse = new MockAboutFacilityResponse();
+    mockRequest.body.facilityType = 'CASH';
+    mockRequest.body['cover-start-date-day'] = '03';
+    mockRequest.body['cover-start-date-month'] = '12';
+    mockRequest.body['cover-start-date-year'] = '2022';
+
+    api.updateFacility = () => Promise.resolve(mockAboutFacilityResponse);
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(updateFacilitySpy).toHaveBeenCalledWith({
+      coverStartDate: '123',
+    });
   });
 });
