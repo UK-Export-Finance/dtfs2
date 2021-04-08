fdf75b210892fc40ad7f99ee723fced5f1afd2da
Richard <info@monkemedia.co.uk>
2021-04-08 11:29:08 +0100

    Setting up E2E tests

---
+++ e2e-tests/gef/cypress/integration/pages/about-facility.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+const aboutFacility = {
+  errorSummary: () => cy.get('[data-cy="error-summary"]'),
+  backLink: () => cy.get('[data-cy="back-link"]'),
+  headingCaption: () => cy.get('[data-cy="heading-caption"]'),
+  mainHeading: () => cy.get('[data-cy="main-heading"]'),
+  form: () => cy.get('[data-cy="form"]'),
+  facilityName: () => cy.get('[data-cy="facility-name"]'),
+  facilityNameError: () => cy.get('[data-cy="facility-name-error"]'),
+  coverStartDateError: () => cy.get('[data-cy="cover-start-date-error"]'),
+  coverStartDateDay: () => cy.get('[data-cy="cover-start-date-day"]'),
+  coverStartDateMonth: () => cy.get('[data-cy="cover-start-date-month"]'),
+  coverStartDateYear: () => cy.get('[data-cy="cover-start-date-year"]'),
+  hasCoverStartDateError: () => cy.get('[data-cy="has-cover-start-date-error"]'),
+  coverStartDateYearYes: () => cy.get('[data-cy="cover-start-date-year-yes"]'),
+  coverStartDateYearNo: () => cy.get('[data-cy="cover-start-date-year-no"]'),
+  coverEndDateError: () => cy.get('[data-cy="cover-end-date-error"]'),
+  coverEndDateDay: () => cy.get('[data-cy="cover-end-date-day"]'),
+  coverEndDateMonth: () => cy.get('[data-cy="cover-end-date-month"]'),
+  coverEndDateYear: () => cy.get('[data-cy="cover-end-date-year"]'),
+  monthsOfCover: () => cy.get('[data-cy="months-of-cover"]'),
+  monthsOfCoverError: () => cy.get('[data-cy="months-of-cover-error"]'),
+  continueButton: () => cy.get('[data-cy="continue-button"]'),
+  saveAndReturnButton: () => cy.get('[data-cy="save-and-return-button"]'),
+};
+
+export default aboutFacility;
