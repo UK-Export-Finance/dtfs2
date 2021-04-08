2caacb0711074324843cbc69f5f6e10a94992bc6
Richard <info@monkemedia.co.uk>
2021-04-08 11:10:17 +0100

    Completed unit tests

--- gef-ui/server/controllers/about-facility/index.test.js
+++ gef-ui/server/controllers/about-facility/index.test.js
@@ -32,8 +32,6 @@ const MockAboutFacilityResponse = () => {
   return res;
 };
 
-const updateFacilitySpy = jest.spyOn(api, 'updateFacility').mockImplementationOnce(() => Promise.resolve());
-
 afterEach(() => {
   jest.clearAllMocks();
 });
@@ -93,6 +91,7 @@ describe('Validate About Facility', () => {
   it('sets the correct date format using single day, month and year values', async () => {
     const mockResponse = new MockResponse();
     const mockRequest = new MockRequest();
+    const updateFacilitySpy = jest.spyOn(api, 'updateFacility').mockImplementationOnce(() => Promise.resolve());
     mockRequest.body.facilityType = 'CASH';
     mockRequest.query.saveAndReturn = 'true';
     mockRequest.body['cover-start-date-day'] = '3';
@@ -127,5 +126,184 @@ describe('Validate About Facility', () => {
         errorSummary: expect.arrayContaining([{ href: '#monthsOfCover', text: expect.any(String) }]),
       }),
     }));
+  });
+
+  it('shows error message if no facility name has been provided', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    mockRequest.body.facilityType = 'CASH';
+    mockRequest.body.facilityName = 'name';
+    mockRequest.body.hasBeenIssued = 'true';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.not.arrayContaining([{ href: '#facilityName', text: expect.any(String) }]),
+      }),
+    }));
+
+    mockRequest.body.facilityName = '';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.arrayContaining([{ href: '#facilityName', text: expect.any(String) }]),
+      }),
+    }));
+  });
+
+  it('shows error message if no hasCoverStartDate radio button has been selected', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    mockRequest.body.facilityType = 'CASH';
+    mockRequest.body.hasBeenIssued = 'true';
+    mockRequest.body.hasCoverStartDate = 'true';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.not.arrayContaining([{ href: '#hasCoverStartDate', text: expect.any(String) }]),
+      }),
+    }));
+
+    mockRequest.body.hasCoverStartDate = '';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.arrayContaining([{ href: '#hasCoverStartDate', text: expect.any(String) }]),
+      }),
+    }));
+  });
+
+  it('shows error message if no coverStartDateDay or coverStartDateMonth or coverStartDateYear has been provided', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    mockRequest.body.facilityType = 'CASH';
+    mockRequest.body.hasBeenIssued = 'true';
+    mockRequest.body.hasCoverStartDate = 'false';
+    mockRequest.body['cover-start-date-day'] = '10';
+    mockRequest.body['cover-start-date-month'] = '11';
+    mockRequest.body['cover-start-date-year'] = '2022';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.not.arrayContaining([{ href: '#coverStartDate', text: expect.any(String) }]),
+      }),
+    }));
+
+    mockRequest.body['cover-start-date-day'] = '';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.arrayContaining([{ href: '#coverStartDate', text: expect.any(String) }]),
+      }),
+    }));
+
+    mockRequest.body['cover-start-date-day'] = '10';
+    mockRequest.body['cover-start-date-month'] = '';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.arrayContaining([{ href: '#coverStartDate', text: expect.any(String) }]),
+      }),
+    }));
+  });
+
+  it('shows error message if no coverEndDateDay or coverEndDateMonth or coverEndDateYear has been provided', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    mockRequest.body.facilityType = 'CASH';
+    mockRequest.body.hasBeenIssued = 'true';
+    mockRequest.body['cover-end-date-day'] = '10';
+    mockRequest.body['cover-end-date-month'] = '11';
+    mockRequest.body['cover-end-date-year'] = '2022';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.not.arrayContaining([{ href: '#coverEndDate', text: expect.any(String) }]),
+      }),
+    }));
+
+    mockRequest.body['cover-end-date-day'] = '';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.arrayContaining([{ href: '#coverEndDate', text: expect.any(String) }]),
+      }),
+    }));
+
+    mockRequest.body['cover-end-date-day'] = '10';
+    mockRequest.body['cover-end-date-month'] = '';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.arrayContaining([{ href: '#coverEndDate', text: expect.any(String) }]),
+      }),
+    }));
+  });
+
+  it('shows error message if no monthsOfcover has been provided', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    mockRequest.body.facilityType = 'CASH';
+    mockRequest.body.hasBeenIssued = 'false';
+    mockRequest.body.monthsOfCover = '';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/about-facility.njk', expect.objectContaining({
+      errors: expect.objectContaining({
+        errorSummary: expect.arrayContaining([{ href: '#monthsOfCover', text: expect.any(String) }]),
+      }),
+    }));
+  });
+
+  it('redirects user to provided facility page if all of method passes', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    mockRequest.body.facilityType = 'CASH';
+    mockRequest.body.hasBeenIssued = 'true';
+    mockRequest.body.facilityName = 'Name';
+    mockRequest.body.hasCoverStartDate = 'true';
+    mockRequest.body.hasBeenIssued = 'true';
+    mockRequest.body.monthsOfCover = '10';
+    mockRequest.body['cover-start-date-day'] = '01';
+    mockRequest.body['cover-start-date-month'] = '05';
+    mockRequest.body['cover-start-date-year'] = '2022';
+    mockRequest.body['cover-end-date-day'] = '05';
+    mockRequest.body['cover-end-date-month'] = '12';
+    mockRequest.body['cover-end-date-year'] = '2023';
+
+    await validateAboutFacility(mockRequest, mockResponse);
+
+    expect(mockResponse.redirect).toHaveBeenCalledWith('/gef/application-details/123/facilities/xyz/provided-facility');
+  });
+
+  it('redirects user to `problem with service` page if there is an issue with the API', async () => {
+    const mockResponse = new MockResponse();
+    const mockRequest = new MockRequest();
+    mockRequest.query.saveAndReturn = 'true';
+    mockRequest.body.facilityType = 'CASH';
+
+    api.updateFacility = () => Promise.reject();
+    await validateAboutFacility(mockRequest, mockResponse);
+    expect(mockResponse.render).toHaveBeenCalledWith('partials/problem-with-service.njk');
   });
 });
