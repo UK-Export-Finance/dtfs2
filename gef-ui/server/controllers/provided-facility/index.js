798ba81e04daa16098f2a8df39cb8c1b31ec906f
Richard <info@monkemedia.co.uk>
2021-04-12 12:18:48 +0100

    Fixed issues provided facility

--- gef-ui/server/controllers/provided-facility/index.js
+++ gef-ui/server/controllers/provided-facility/index.js
@@ -1,5 +1,6 @@
 import * as api from '../../services/api';
 import { FACILITY_TYPE } from '../../../constants';
+import { isTrueSet, validationErrorHandler } from '../../utils/helpers';
 
 const providedFacility = async (req, res) => {
   const { params, query } = req;
@@ -8,7 +9,8 @@ const providedFacility = async (req, res) => {
 
   try {
     const { details } = await api.getFacility(facilityId);
-    const facilityTypeString = FACILITY_TYPE[details.type].toLowerCase();
+    const facilityTypeConst = FACILITY_TYPE[details.type];
+    const facilityTypeString = facilityTypeConst ? facilityTypeConst.toLowerCase() : '';
 
     return res.render('partials/provided-facility.njk', {
       facilityType: FACILITY_TYPE[details.type],
@@ -27,8 +29,32 @@ const providedFacility = async (req, res) => {
 const validateProvidedFacility = async (req, res) => {
   const { params, body, query } = req;
   const { applicationId, facilityId } = params;
-  const { details, detailsOther } = body;
-  const { saveAndReturn } = query;
+  const { facilityType, detailsOther } = body;
+  const { saveAndReturn, status } = query;
+  const providedFacilityErrors = [];
+  const facilityTypeConst = FACILITY_TYPE[body.facilityType];
+  const facilityTypeString = facilityTypeConst ? facilityTypeConst.toLowerCase() : '';
+  const details = Array.isArray(body.details) ? body.details : [body.details];
+
+  if (body.details && body.details.includes('OTHER') && !body.detailsOther) {
+    providedFacilityErrors.push({
+      errRef: 'detailsOther',
+      errMsg: 'Enter details for "Other"',
+    });
+  }
+
+  if (providedFacilityErrors.length > 0) {
+    return res.render('partials/provided-facility.njk', {
+      errors: validationErrorHandler(providedFacilityErrors),
+      details,
+      facilityType,
+      detailsOther,
+      facilityTypeString,
+      applicationId,
+      facilityId,
+      status,
+    });
+  }
 
   try {
     await api.updateFacility(facilityId, {
@@ -36,7 +62,7 @@ const validateProvidedFacility = async (req, res) => {
       detailsOther,
     });
 
-    if (saveAndReturn) {
+    if (isTrueSet(saveAndReturn) || status === 'change') {
       return res.redirect(`/gef/application-details/${applicationId}`);
     }
 
