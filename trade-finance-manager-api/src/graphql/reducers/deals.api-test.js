66d7a02bb94ad771e61fc0a0bf961d0304d7e0fb
ttbarnes <tony@tonybarnes.me>
2021-04-13 16:41:40 +0100

    DTFS2-3536 - tfm - sort deals by submissionDate

---
+++ trade-finance-manager-api/src/graphql/reducers/deals.api-test.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-underscore-dangle */
+
+const dealsReducer = require('./deals');
+const mapSubmissionDetails = require('./mapSubmissionDetails');
+const mapDealTfm = require('./mappings/deal/dealTfm/mapDealTfm');
+
+const MOCK_DEAL = require('../../v1/__mocks__/mock-deal-AIN-submitted');
+
+
+// TODO/NOTE:
+// this is just an initial unit test to cover date sorting.
+// need to improve test coverage.
+
+const createMockDeal = (submissionDate) => ({
+  ...MOCK_DEAL._id,
+  dealSnapshot: {
+    ...MOCK_DEAL,
+    details: {
+      ...MOCK_DEAL.details,
+      submissionDate,
+    },
+  },
+  tfm: {
+    supplyContractValueInGBP: 1234,
+  },
+});
+
+const expectedDealShape = (deal) => ({
+  ...deal,
+  dealSnapshot: {
+    ...deal.dealSnapshot,
+    submissionDetails: mapSubmissionDetails(deal.dealSnapshot.submissionDetails),
+  },
+  tfm: {
+    ...mapDealTfm(deal.tfm),
+    product: 'BSS & EWCS',
+  },
+});
+
+describe('reducer - deals', () => {
+  it('should return deals sorted by most recent submissionDate', () => {
+    const mockDeals = [
+      createMockDeal(1606900616651),
+      createMockDeal(1606900616652),
+      createMockDeal(1606900616653),
+      createMockDeal(1606900616654),
+      createMockDeal(1606900616655),
+    ];
+
+    const result = dealsReducer(mockDeals);
+
+    const expected = {
+      count: mockDeals.length,
+      deals: [
+        expectedDealShape(createMockDeal(1606900616655)),
+        expectedDealShape(createMockDeal(1606900616654)),
+        expectedDealShape(createMockDeal(1606900616653)),
+        expectedDealShape(createMockDeal(1606900616652)),
+        expectedDealShape(createMockDeal(1606900616651)),
+      ],
+    };
+
+    expect(result.count).toEqual(expected.count);
+    expect(result.deals).toEqual(expected.deals);
+  });
+});
+
+/* eslint-enable no-underscore-dangle */
