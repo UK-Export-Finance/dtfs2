78e5bd8849ae4fb7c5244dd6f2267b947046452b
ttbarnes <tony@tonybarnes.me>
2021-04-14 13:06:21 +0100

    DTFS2-3664 - tfm - MIA tasks creation

--- trade-finance-manager-api/src/constants/teams.js
+++ trade-finance-manager-api/src/constants/teams.js
@@ -3,6 +3,21 @@ const UNDERWRITING_SUPPORT = {
   name: 'Underwriting support',
 };
 
+const UNDERWRITER_MANAGERS = {
+  id: 'UNDERWRITER_MANAGERS',
+  name: 'Underwriter managers',
+};
+
+const UNDERWRITERS = {
+  id: 'UNDERWRITERS',
+  name: 'Underwriters',
+};
+
+const RISK_MANAGERS = {
+  id: 'RISK_MANAGERS',
+  name: 'Risk managers',
+};
+
 const BUSINESS_SUPPORT = {
   id: 'BUSINESS_SUPPORT',
   name: ' Business support group',
@@ -10,5 +25,9 @@ const BUSINESS_SUPPORT = {
 
 module.exports = {
   UNDERWRITING_SUPPORT,
+  UNDERWRITER_MANAGERS,
+  UNDERWRITERS,
+  RISK_MANAGERS,
   BUSINESS_SUPPORT,
 };
+
