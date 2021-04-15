a7dad56fa51ec1650e373c5edade9a37653b83c1
Richard <info@monkemedia.co.uk>
2021-04-15 11:00:32 +0100

    Aded E2E tests

---
+++ e2e-tests/gef/cypress/integration/facility-value.spec.js
@@ -0,0 +1,166 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+import relative from './relativeURL';
+import facilityValue from './pages/facility-value';
+import CREDENTIALS from '../fixtures/credentials.json';
+
+const applications = [];
+let token;
+
+context('Facility Value Page', () => {
+  before(() => {
+    cy.reinsertMocks();
+    cy.apiLogin(CREDENTIALS.MAKER)
+      .then((tok) => {
+        token = tok;
+      })
+      .then(() => cy.apiFetchAllApplications(token))
+      .then(({ body }) => {
+        body.items.forEach((item) => {
+          cy.apiFetchAllFacilities(item._id, token)
+            .then((res) => {
+              applications.push({
+                id: item._id,
+                facilities: res.body.items.filter((it) => it.details.applicationId === item._id),
+              });
+            });
+        });
+      });
+    cy.login(CREDENTIALS.MAKER);
+
+    cy.on('uncaught:exception', () => false);
+  });
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('connect.sid');
+    console.log(applications);
+  });
+
+  describe('Visiting page as cash facility', () => {
+    it('displays the correct elements', () => {
+      cy.visit(relative(`/gef/application-details/${applications[2].id}/facilities/${applications[2].facilities[1].details._id}/facility-value`));
+      facilityValue.backLink();
+      facilityValue.headingCaption();
+      facilityValue.mainHeading();
+      facilityValue.hiddenFacilityType().should('be', 'invisible');
+      facilityValue.valueLabel().should('contain', 'cash');
+      facilityValue.valueSuffix().should('contain', 'EUR');
+      facilityValue.doneButton();
+    });
+
+    it('redirects user to `facility currency` page when clicking on `Back` Link', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+      facilityValue.backLink().click();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-currency`));
+    });
+
+    it('hides back button if visiting page with `change` query', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value?status=change`));
+      facilityValue.backLink().should('not.be', 'visible');
+    });
+  });
+
+  describe('Visiting page as contingent facility', () => {
+    it('displays the correct elements', () => {
+      cy.visit(relative(`/gef/application-details/${applications[2].id}/facilities/${applications[2].facilities[2].details._id}/facility-value`));
+      facilityValue.backLink();
+      facilityValue.headingCaption();
+      facilityValue.mainHeading();
+      facilityValue.hiddenFacilityType().should('be', 'invisible');
+      facilityValue.valueLabel().should('contain', 'contingent');
+      facilityValue.valueSuffix().should('contain', 'YEN');
+      facilityValue.doneButton();
+    });
+  });
+
+  describe('Percentage of cover field', () => {
+    it('only allows the user to enter a value between 1 and 80', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+      facilityValue.percentageCover().type(0);
+      facilityValue.doneButton().click();
+      facilityValue.errorSummary();
+      facilityValue.percentageCoverError();
+
+      facilityValue.percentageCover().clear();
+      facilityValue.percentageCover().type(-1);
+      facilityValue.doneButton().click();
+      facilityValue.errorSummary();
+      facilityValue.percentageCoverError();
+
+      facilityValue.percentageCover().clear();
+      facilityValue.percentageCover().type('a');
+      facilityValue.doneButton().click();
+      facilityValue.errorSummary();
+      facilityValue.percentageCoverError();
+
+      facilityValue.percentageCover().clear();
+      facilityValue.percentageCover().type(81);
+      facilityValue.doneButton().click();
+      facilityValue.errorSummary();
+      facilityValue.percentageCoverError();
+
+      facilityValue.percentageCover().clear();
+      facilityValue.percentageCover().type(80);
+      facilityValue.doneButton().click();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}`));
+
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+      facilityValue.percentageCover().clear();
+      facilityValue.percentageCover().type(1);
+      facilityValue.doneButton().click();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}`));
+
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+      facilityValue.percentageCover().clear();
+      facilityValue.percentageCover().type(79);
+      facilityValue.doneButton().click();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}`));
+    });
+  });
+
+  describe('Interest margin Percentage field', () => {
+    it('only allows the user to enter a value between 0 and 100, and allows decimal places', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+      facilityValue.interestPercentage().clear();
+      facilityValue.interestPercentage().type(-1);
+      facilityValue.doneButton().click();
+      facilityValue.errorSummary();
+      facilityValue.interestPercentageError();
+
+      facilityValue.interestPercentage().clear();
+      facilityValue.interestPercentage().type('a');
+      facilityValue.doneButton().click();
+      facilityValue.errorSummary();
+      facilityValue.interestPercentageError();
+
+      facilityValue.interestPercentage().clear();
+      facilityValue.interestPercentage().type(101);
+      facilityValue.doneButton().click();
+      facilityValue.errorSummary();
+      facilityValue.interestPercentageError();
+
+      facilityValue.interestPercentage().clear();
+      facilityValue.interestPercentage().type(1);
+      facilityValue.doneButton().click();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}`));
+
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+      facilityValue.interestPercentage().clear();
+      facilityValue.interestPercentage().type(100);
+      facilityValue.doneButton().click();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}`));
+
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+      facilityValue.interestPercentage().clear();
+      facilityValue.interestPercentage().type(79);
+      facilityValue.doneButton().click();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}`));
+
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+      facilityValue.interestPercentage().clear();
+      facilityValue.interestPercentage().type(10.1);
+      facilityValue.doneButton().click();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}`));
+    });
+  });
+});
---
+++ e2e-tests/gef/cypress/integration/pages/facility-value.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+const facilityValue = {
+  errorSummary: () => cy.get('[data-cy="error-summary"]'),
+  backLink: () => cy.get('[data-cy="back-link"]'),
+  headingCaption: () => cy.get('[data-cy="heading-caption"]'),
+  mainHeading: () => cy.get('[data-cy="main-heading"]'),
+  hiddenFacilityType: () => cy.get('[data-cy="hidden-facility-type"]'),
+  value: () => cy.get('[data-cy="value"]'),
+  valueLabel: () => cy.get('[data-cy="value-label"]'),
+  valueError: () => cy.get('[data-cy="value-error"]'),
+  valueSuffix: () => cy.get('[data-cy="value-suffix"]'),
+  percentageCover: () => cy.get('[data-cy="percentage-cover"]'),
+  percentageCoverError: () => cy.get('[data-cy="percentage-cover-error"]'),
+  interestPercentage: () => cy.get('[data-cy="interest-percentage"]'),
+  interestPercentageError: () => cy.get('[data-cy="interest-percentage-error"]'),
+  yenCheckbox: () => cy.get('[data-cy="yen-checkbox"]'),
+  doneButton: () => cy.get('[data-cy="done-button"]'),
+};
+
+export default facilityValue;
