aa8acc5d0dd6aa45fd08506b931f7fd2c2e66d28
ttbarnes <tony@tonybarnes.me>
2021-04-16 11:16:19 +0100

    DTFS2-3664- portal-tfm submission - e2e test for MIA tasks creation

---
+++ e2e-tests/submit-to-trade-finance-manager/cypress/integration/journeys/portal/test-data/MIA-deal/dealReadyToSubmit.js
@@ -0,0 +1,27 @@
+const moment = require('moment');
+const dealThatJustNeedsConversionDate = require('./dealThatJustNeedsConversionDate');
+
+module.exports = () => {
+  const now = moment();
+
+  const deal = JSON.parse(JSON.stringify(dealThatJustNeedsConversionDate));
+
+  deal.submissionDetails['supplyContractConversionDate-day'] = `${now.format('DD')}`;
+  deal.submissionDetails['supplyContractConversionDate-month'] = `${now.format('MM')}`;
+  deal.submissionDetails['supplyContractConversionDate-year'] = `${now.format('YYYY')}`;
+
+  deal.mockFacilities[0].requestedCoverStartDate = moment().utc().valueOf();
+  deal.mockFacilities[1].requestedCoverStartDate = moment().utc().valueOf();
+
+  const aMonthInTheFuture = moment().add(1, 'month');
+
+  deal.mockFacilities[0]['coverEndDate-day'] = aMonthInTheFuture.format('DD');
+  deal.mockFacilities[0]['coverEndDate-month'] = aMonthInTheFuture.format('MM');
+  deal.mockFacilities[0]['coverEndDate-year'] = moment(aMonthInTheFuture).format('YYYY');
+
+  deal.mockFacilities[1]['coverEndDate-day'] = aMonthInTheFuture.format('DD');
+  deal.mockFacilities[1]['coverEndDate-month'] = aMonthInTheFuture.format('MM');
+  deal.mockFacilities[1]['coverEndDate-year'] = moment(aMonthInTheFuture).format('YYYY');
+
+  return deal;
+};
