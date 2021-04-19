7198cb7c8e6cdcf64c5a03296b999ea0c0c8f9b4
ttbarnes <tony@tonybarnes.me>
2021-04-19 15:06:17 +0100

    DTFS2-3664 - tfm - MIA tasks - e2e test

--- e2e-tests/trade-finance-manager/cypress/integration/pages/tasksPage.js
+++ e2e-tests/trade-finance-manager/cypress/integration/pages/tasksPage.js
@@ -2,14 +2,14 @@ const tasksPage = {
   tasksTableRows: () => cy.get('[data-cy="tasks-table"] tbody tr'),
   taskGroupTable: () => cy.get('[data-cy="task-group-table"]'),
   tasks: {
-    row: (taskId) => {
-      const row = cy.get(`[data-cy="task-${taskId}-row"]`);
+    row: (groupId, taskId) => {
+      const row = cy.get(`[data-cy="task-group-${groupId}-task-${taskId}-row"]`);
       return {
         row,
-        link: () => row.get(`[data-cy="task-table-row-${taskId}-link"]`),
+        link: () => row.get(`[data-cy="task-table-row-group-${groupId}-task-${taskId}-link"]`),
         title: () => row.get(`[data-cy="task-table-row-${taskId}-title"]`),
         assignedTo: () => row.get(`[data-cy="task-table-row-${taskId}-user-full-name"]`),
-        status: () => row.get(`[data-cy="task-table-row-${taskId}-status"]`),
+        status: () => row.get(`[data-cy="task-table-row-group-${groupId}-task-${taskId}-status"]`),
       };
     },
   },
