ee46d2fe27a65b83936831db55f223d313050e3e
ttbarnes <tony@tonybarnes.me>
2021-04-20 11:32:32 +0100

    linting

--- trade-finance-manager-api/src/v1/controllers/update-portal-deal-status.js
+++ trade-finance-manager-api/src/v1/controllers/update-portal-deal-status.js
@@ -8,7 +8,7 @@ const updatePortalDealStatus = async (dealId, submissionType) => {
       CONSTANTS.DEALS.DEAL_STATUS_PORTAL.SUBMISSION_ACKNOWLEDGED,
     );
   }
-  
+
   if (submissionType === CONSTANTS.DEALS.SUBMISSION_TYPE.MIA) {
     await api.updatePortalDealStatus(
       dealId,
@@ -18,9 +18,9 @@ const updatePortalDealStatus = async (dealId, submissionType) => {
     await api.updatePortalDealStatus(
       dealId,
       CONSTANTS.DEALS.DEAL_STATUS_PORTAL.IN_PROGRESS,
-    );    
+    );
   }
-    
+
   return null;
 };
 
