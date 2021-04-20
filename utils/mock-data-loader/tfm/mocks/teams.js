25cabe8386b5292e4b1521c01f3758d565220b8f
ttbarnes <tony@tonybarnes.me>
2021-04-20 10:01:34 +0100

    add new teams to constants/mocks

--- utils/mock-data-loader/tfm/mocks/teams.js
+++ utils/mock-data-loader/tfm/mocks/teams.js
@@ -15,4 +15,16 @@ module.exports = [
     id: 'BUSINESS_SUPPORT',
     name: ' Business support group',
   },
+  {
+    id: 'UNDERWRITER_MANAGERS',
+    name: 'Underwriter managers',
+  },
+  {
+    id: 'UNDERWRITERS',
+    name: 'Underwriters',
+  },
+  {
+    id: 'RISK_MANAGERS',
+    name: 'Risk managers',
+  },
 ];
