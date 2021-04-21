62886b2c5e4abc5e64906cfd1561737bdce76a31
Andrew Price <andrew@madebymoon.com>
2021-04-21 16:02:20 +0100

    dtfs2-3700 Remove activity tab

--- e2e-tests/trade-finance-manager/cypress/integration/partials/caseSubNavigation.js
+++ e2e-tests/trade-finance-manager/cypress/integration/partials/caseSubNavigation.js
@@ -3,7 +3,6 @@ const partial = {
   dealLink: () => cy.get('[data-cy="deal-link"]'),
   partiesLink: () => cy.get('[data-cy="parties-link"]'),
   documentsLink: () => cy.get('[data-cy="documents-link"]'),
-  activityLink: () => cy.get('[data-cy="activity-link"]'),
   underwritingLink: () => cy.get('[data-cy="underwriting-link"]'),
   linkedDealsLink: () => cy.get('[data-cy="linked-deals-link"]'),
 };
