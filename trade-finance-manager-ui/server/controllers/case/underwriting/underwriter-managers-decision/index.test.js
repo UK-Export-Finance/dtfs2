4fdca19e2b503c2267e1ca8663558d4317d262fc
ttbarnes <tony@tonybarnes.me>
2021-04-28 17:07:05 +0100

    DTFS2-3682 - tfm - managers decision - ui route test coverage

--- trade-finance-manager-ui/server/controllers/case/underwriting/underwriter-managers-decision/index.test.js
+++ trade-finance-manager-ui/server/controllers/case/underwriting/underwriter-managers-decision/index.test.js
@@ -125,6 +125,53 @@ describe('GET underwriting - underwriting managers decision', () => {
   });
 });
 
+describe('GET underwriting - underwriting managers decision', () => {
+  describe('when deal exists', () => {
+    beforeEach(() => {
+      api.getDeal = () => Promise.resolve(mockDeal);
+    });
+
+    it('should render template with data', async () => {
+      const req = {
+        params: {
+          _id: dealId,
+        },
+        session,
+      };
+
+      await underwriterManagersDecisionController.getUnderwriterManagersDecisionSubmitted(req, res);
+
+      expect(res.render).toHaveBeenCalledWith('case/underwriting/managers-decision/managers-decision.njk', {
+        activePrimaryNavigation: 'manage work',
+        activeSubNavigation: 'underwriting',
+        activeSideNavigation: 'underwriter managers decision',
+        deal: mockDeal.dealSnapshot,
+        tfm: mockDeal.tfm,
+        dealId: mockDeal.dealSnapshot._id, // eslint-disable-line no-underscore-dangle
+        user: session.user,
+      });
+    });
+  });
+
+  describe('when deal does NOT exist', () => {
+    beforeEach(() => {
+      api.getDeal = () => Promise.resolve();
+    });
+
+    it('should redirect to not-found route', async () => {
+      const req = {
+        params: {
+          _id: '1',
+        },
+        session,
+      };
+
+      await underwriterManagersDecisionController.getUnderwriterManagersDecisionSubmitted(req, res);
+      expect(res.redirect).toHaveBeenCalledWith('/not-found');
+    });
+  });
+});
+
 describe('POST underwriting - underwriting managers decision', () => {
   describe('when there are NO validation errors', () => {
     const apiUpdateSpy = jest.fn(() => Promise.resolve({
