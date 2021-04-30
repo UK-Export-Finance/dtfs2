76ebd870b32bb7f9e53fed021522ed3276db0e57
ttbarnes <tony@tonybarnes.me>
2021-04-30 15:09:03 +0100

    DTFS2-3753 - tfm - submitting managers decision maps and updates portal deal status

--- trade-finance-manager-api/src/constants/deals.js
+++ trade-finance-manager-api/src/constants/deals.js
@@ -10,16 +10,13 @@ const SUBMISSION_TYPE = {
   MIN: 'Manual Inclusion Notice',
 };
 
-const DEAL_STATUS_PORTAL = {
-  SUBMITTED: 'Submitted',
-  SUBMISSION_ACKNOWLEDGED: 'Acknowledged by UKEF',
-  IN_PROGRESS: 'In progress by UKEF',
-};
-
 const DEAL_STAGE_TFM = {
   CONFIRMED: 'Confirmed',
   APPLICATION: 'Application',
   IN_PROGRESS: 'In progress',
+  APPROVED_WITH_CONDITIONS: 'Approved with conditions',
+  APPROVED_WITHOUT_CONDITIONS: 'Approved without conditions',
+  DECLINED: 'Declined',
 };
 
 const CREDIT_RATING = {
@@ -35,12 +32,21 @@ const PROBABILITY_OF_DEFAULT = {
   LESS_THAN_14_PERCENT: 'Less than 14.1%',
 };
 
+const DEAL_STATUS_PORTAL = {
+  SUBMITTED: 'Submitted',
+  SUBMISSION_ACKNOWLEDGED: 'Acknowledged by UKEF',
+  IN_PROGRESS: 'In progress by UKEF',
+  APPROVED: 'Accepted by UKEF (without conditions)',
+  APPROVED_WITH_CONDITIONS: 'Accepted by UKEF (with conditions)',
+  REFUSED: 'Rejected by UKEF',
+};
+
 module.exports = {
   DEAL_PRODUCT_CODE,
   SUBMISSION_TYPE,
-  DEAL_STATUS_PORTAL,
   DEAL_STAGE_TFM,
   CREDIT_RATING,
   LOSS_GIVEN_DEFAULT,
   PROBABILITY_OF_DEFAULT,
+  DEAL_STATUS_PORTAL,
 };
