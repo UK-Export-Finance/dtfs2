1908328d8fddd2069707be34847f32764c4112e9
Andrew Price <andrew@madebymoon.com>
2021-05-10 13:21:45 +0100

    dtfs2-3904 increased wait timings for submit e2e tests

--- e2e-tests/submit-to-trade-finance-manager/cypress/integration/journeys/portal/submit-deal-adds-risk-profile-to-facilities/submit-deal-adds-risk-profile-to-facilities.spec.js
+++ e2e-tests/submit-to-trade-finance-manager/cypress/integration/journeys/portal/submit-deal-adds-risk-profile-to-facilities/submit-deal-adds-risk-profile-to-facilities.spec.js
@@ -64,6 +64,7 @@ context('Portal to TFM deal submission', () => {
     cy.url().should('include', '/dashboard');
 
 
+    cy.wait(25000); // wait for TFM to do it's thing
     //---------------------------------------------------------------
     // user login to TFM
     //---------------------------------------------------------------
