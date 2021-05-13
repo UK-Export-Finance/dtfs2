9af0e9ef230b231889b73c3d0025b33374aa68f2
ttbarnes <tony@tonybarnes.me>
2021-05-13 16:44:02 +0100

    linting

--- reference-data-proxy/src/utils/object.js
+++ reference-data-proxy/src/utils/object.js
@@ -2,13 +2,13 @@ const objectIsEmpty = (obj) => {
   if (!obj) {
     return true;
   }
-  
+
   if (Object.keys(obj).length === 0 && obj.constructor === Object) {
     return true;
   }
 
   return false;
-}
+};
 
 module.exports = {
   objectIsEmpty,
