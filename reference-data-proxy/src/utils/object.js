e900cd504833382f65c2437cb1ba4bc9de7b485a
ttbarnes <tony@tonybarnes.me>
2021-05-13 16:13:44 +0100

    fix tfm-portal e2e test where premium-schedule api call errors/times out

---
+++ reference-data-proxy/src/utils/object.js
@@ -0,0 +1,15 @@
+const objectIsEmpty = (obj) => {
+  if (!obj) {
+    return true;
+  }
+  
+  if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+    return true;
+  }
+
+  return false;
+}
+
+module.exports = {
+  objectIsEmpty,
+};
