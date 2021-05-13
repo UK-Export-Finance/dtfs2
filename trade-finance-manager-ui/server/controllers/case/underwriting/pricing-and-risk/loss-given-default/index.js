8db61a6920a7bfead5632bbdf52813aa62687c3b
ttbarnes <tony@tonybarnes.me>
2021-05-13 12:32:05 +0100

    DTFS2-3803 - tfm - underwriting pricing and risk - split out controllers

---
+++ trade-finance-manager-ui/server/controllers/case/underwriting/pricing-and-risk/loss-given-default/index.js
@@ -0,0 +1,99 @@
+import api from '../../../../../api';
+import {
+  userIsInTeam,
+} from '../../../../../helpers/user';
+import CONSTANTS from '../../../../../constants';
+
+const getUnderWritingLossGivenDefault = async (req, res) => {
+  const dealId = req.params._id; // eslint-disable-line no-underscore-dangle
+  const deal = await api.getDeal(dealId);
+
+  const { user } = req.session;
+  const userCanEdit = userIsInTeam(user, CONSTANTS.TEAMS.UNDERWRITING_SUPPORT);
+
+  if (!deal || !userCanEdit) {
+    return res.redirect('/not-found');
+  }
+
+  return res.render('case/underwriting/pricing-and-risk/loss-given-default.njk', {
+    activePrimaryNavigation: 'manage work',
+    activeSubNavigation: 'underwriting',
+    activeSideNavigation: 'pricing and risk',
+    deal: deal.dealSnapshot,
+    tfm: deal.tfm,
+    dealId: deal.dealSnapshot._id, // eslint-disable-line no-underscore-dangle
+    user: req.session.user,
+  });
+};
+
+const postUnderWritingLossGivenDefault = async (req, res) => {
+  const dealId = req.params._id; // eslint-disable-line no-underscore-dangle
+  const deal = await api.getDeal(dealId);
+
+  const { user } = req.session;
+  const userCanEdit = userIsInTeam(user, CONSTANTS.TEAMS.UNDERWRITING_SUPPORT);
+
+  if (!deal || !userCanEdit) {
+    return res.redirect('/not-found');
+  }
+
+  let validationErrors;
+  let errorMsg;
+
+  const { lossGivenDefault } = req.body;
+
+  // eslint-disable-next-line eqeqeq
+  if (Number(lossGivenDefault) != lossGivenDefault
+    || Number(lossGivenDefault) < 1
+    || Number(lossGivenDefault) > 100) {
+    errorMsg = 'Enter a value between 1 - 100';
+  }
+
+  if (!lossGivenDefault) {
+    errorMsg = 'Enter a loss given default';
+  }
+
+  if (errorMsg) {
+    validationErrors = {
+      count: 1,
+      errorList: {
+        lossGivenDefault: {
+          text: errorMsg,
+          order: '1',
+        },
+      },
+      summary: [{
+        text: errorMsg,
+        href: '#lossGivenDefault',
+      }],
+    };
+
+
+    return res.render('case/underwriting/pricing-and-risk/loss-given-default.njk', {
+      activePrimaryNavigation: 'manage work',
+      activeSubNavigation: 'underwriting',
+      activeSideNavigation: 'pricing and risk',
+      deal: deal.dealSnapshot,
+      tfm: {
+        ...deal.tfm,
+        lossGivenDefault,
+      },
+      dealId: deal.dealSnapshot._id, // eslint-disable-line no-underscore-dangle
+      user: req.session.user,
+      validationErrors,
+    });
+  }
+
+  const update = {
+    lossGivenDefault,
+  };
+
+  await api.updateLossGivenDefault(dealId, update);
+
+  return res.redirect(`/case/${dealId}/underwriting/pricing-and-risk`);
+};
+
+export default {
+  getUnderWritingLossGivenDefault,
+  postUnderWritingLossGivenDefault,
+};
