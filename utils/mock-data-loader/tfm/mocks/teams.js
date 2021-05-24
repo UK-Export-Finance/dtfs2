4442ee06115fe818ec0773f5a5a0c7ee0639a33f
ttbarnes <tony@tonybarnes.me>
2021-05-24 12:25:39 +0100

    add tfm team emails to mock data

--- utils/mock-data-loader/tfm/mocks/teams.js
+++ utils/mock-data-loader/tfm/mocks/teams.js
@@ -10,21 +10,26 @@ module.exports = [
   {
     id: 'UNDERWRITING_SUPPORT',
     name: 'Underwriting support',
+    email: 'maker5@ukexportfinance.gov.uk',
   },
   {
     id: 'BUSINESS_SUPPORT',
     name: ' Business support group',
+    email: 'maker3@ukexportfinance.gov.uk',
   },
   {
     id: 'UNDERWRITER_MANAGERS',
     name: 'Underwriter managers',
+    email: 'checker2@ukexportfinance.gov.uk',
   },
   {
     id: 'UNDERWRITERS',
     name: 'Underwriters',
+    email: 'checker5@ukexportfinance.gov.uk',
   },
   {
     id: 'RISK_MANAGERS',
     name: 'Risk managers',
+    email: 'checker1@ukexportfinance.gov.uk',
   },
 ];
