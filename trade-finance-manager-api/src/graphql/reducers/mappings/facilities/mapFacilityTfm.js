f3ae55c97c01a98ef957ea035e4553490acda614
ttbarnes <tony@tonybarnes.me>
2021-05-25 17:04:24 +0100

    premium schedule and premium totals mapping in api instead of ui

--- trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityTfm.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityTfm.js
@@ -1,10 +1,13 @@
 const mapUkefExposure = require('./mapUkefExposure');
+const mapPremiumSchedule = require('./mapPremiumSchedule');
+const mapPremiumTotals = require('./mapPremiumTotals');
 
 const mapFacilityTfm = (facilityTfm, dealTfm) =>
   ({
     ...facilityTfm,
     ukefExposure: mapUkefExposure(facilityTfm),
-    premiumSchedule: facilityTfm.premiumSchedule,
+    premiumSchedule: mapPremiumSchedule(facilityTfm.premiumSchedule),
+    premiumTotals: mapPremiumTotals(facilityTfm.premiumSchedule),
     creditRating: dealTfm.exporterCreditRating,
   });
 module.exports = mapFacilityTfm;
