034301ddf6216d5064d021963050cd1fbee8f7e3
ttbarnes <tony@tonybarnes.me>
2021-05-27 13:54:56 +0100

    DTFS2-4189 - fix tfm premium schedule missing amounts

--- trade-finance-manager-ui/server/nunjucks-configuration/formatAsCurrency.js
+++ trade-finance-manager-ui/server/nunjucks-configuration/formatAsCurrency.js
@@ -1,9 +1,11 @@
-
 const formatAsCurrency = (str) => {
-  const isEmpty = !(str && str.trim().length > 0);
+  const currencyString = String(str);
+  const isEmpty = !(currencyString && currencyString.trim().length > 0);
+
   let amount = 0;
+
   if (!isEmpty) {
-    amount = parseFloat(str, 10);
+    amount = parseFloat(currencyString, 10);
     amount = amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
   return isEmpty ? '-' : amount;
