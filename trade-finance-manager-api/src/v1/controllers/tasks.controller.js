731f1c3624bd147ae0499e4544bf8cbb5b9dfc78
ttbarnes <tony@tonybarnes.me>
2021-05-27 10:37:09 +0100

    simplify task email switch case

--- trade-finance-manager-api/src/v1/controllers/tasks.controller.js
+++ trade-finance-manager-api/src/v1/controllers/tasks.controller.js
@@ -75,147 +75,85 @@ const generateTaskUrl = (urlOrigin, dealId, task) => {
   return `${urlOrigin}/case/${dealId}/tasks/${groupId}/${taskId}`;
 };
 
+const generateTaskEmailVariables = (urlOrigin, task, dealId, exporterName, ukefDealId) => ({
+  taskTitle: task.title,
+  taskUrl: generateTaskUrl(urlOrigin, dealId, task),
+  exporterName,
+  ukefDealId,
+});
+
 const sendUpdatedTaskEmail = async (task, deal, urlOrigin) => {
-  let emailVariables = {};
   let templateId;
   let sendToEmailAddress;
   let team;
 
   const { dealSnapshot } = deal;
-  const { submissionDetails, details } = dealSnapshot;
+  const {
+    _id: dealId,
+    submissionDetails,
+    details,
+  } = dealSnapshot;
 
   const { 'supplier-name': exporterName } = submissionDetails;
   const { ukefDealId } = details;
 
+  let emailVariables = generateTaskEmailVariables(
+    urlOrigin,
+    task,
+    dealId,
+    exporterName,
+    ukefDealId,
+  );
+
   switch (task.title) {
     case CONSTANTS.TASKS.AIN_AND_MIA.GROUP_1.CREATE_OR_LINK_SALESFORCE:
+      templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_SALEFORCE_NEW_DEAL;
+
       emailVariables = {
         exporterName,
         ukefDealId,
       };
-
-      templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_SALEFORCE_NEW_DEAL;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
 
     case CONSTANTS.TASKS.MIA_GROUP_1_TASKS.FILE_ALL_DEAL_EMAILS:
-      emailVariables = {
-        taskTitle: CONSTANTS.TASKS.MIA_GROUP_1_TASKS.FILE_ALL_DEAL_EMAILS,
-        taskUrl: generateTaskUrl(urlOrigin, deal.dealSnapshot._id, task),
-        exporterName,
-        ukefDealId,
-      };
-
       templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_READY_TO_START;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
-    
+
     case CONSTANTS.TASKS.MIA_GROUP_1_TASKS.CREATE_CREDIT_ANALYSIS_DOCUMENT:
-      emailVariables = {
-        taskTitle: CONSTANTS.TASKS.MIA_GROUP_1_TASKS.CREATE_CREDIT_ANALYSIS_DOCUMENT,
-        taskUrl: generateTaskUrl(urlOrigin, deal.dealSnapshot._id, task),
-        exporterName,
-        ukefDealId,
-      };
-
       templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_READY_TO_START;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
 
     case CONSTANTS.TASKS.MIA_GROUP_2_TASKS.COMPLETE_ADVERSE_HISTORY_CHECK:
-      emailVariables = {
-        taskTitle: CONSTANTS.TASKS.MIA_GROUP_2_TASKS.COMPLETE_ADVERSE_HISTORY_CHECK,
-        taskUrl: generateTaskUrl(urlOrigin, deal.dealSnapshot._id, task),
-        exporterName,
-        ukefDealId,
-      };
-
       templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_READY_TO_START;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
 
     case CONSTANTS.TASKS.MIA_GROUP_3_TASKS.CHECK_EXPOSURE:
-      emailVariables = {
-        taskTitle: CONSTANTS.TASKS.MIA_GROUP_3_TASKS.CHECK_EXPOSURE,
-        taskUrl: generateTaskUrl(urlOrigin, deal.dealSnapshot._id, task),
-        exporterName,
-        ukefDealId,
-      };
-
       templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_READY_TO_START;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
 
     case CONSTANTS.TASKS.MIA_GROUP_3_TASKS.GIVE_EXPORTER_A_CREDIT_RATING:
-      emailVariables = {
-        taskTitle: CONSTANTS.TASKS.MIA_GROUP_3_TASKS.GIVE_EXPORTER_A_CREDIT_RATING,
-        taskUrl: generateTaskUrl(urlOrigin, deal.dealSnapshot._id, task),
-        exporterName,
-        ukefDealId,
-      };
-
       templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_READY_TO_START;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
 
     case CONSTANTS.TASKS.MIA_GROUP_3_TASKS.COMPLETE_CREDIT_ANALYSIS:
-      emailVariables = {
-        taskTitle: CONSTANTS.TASKS.MIA_GROUP_3_TASKS.COMPLETE_CREDIT_ANALYSIS,
-        taskUrl: generateTaskUrl(urlOrigin, deal.dealSnapshot._id, task),
-        exporterName,
-        ukefDealId,
-      };
-
       templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_READY_TO_START;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
 
     case CONSTANTS.TASKS.MIA_GROUP_4_TASKS.CHECK_THE_CREDIT_ANALYSIS:
-      emailVariables = {
-        taskTitle: CONSTANTS.TASKS.MIA_GROUP_4_TASKS.CHECK_THE_CREDIT_ANALYSIS,
-        taskUrl: generateTaskUrl(urlOrigin, deal.dealSnapshot._id, task),
-        exporterName,
-        ukefDealId,
-      };
-
       templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_READY_TO_START;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
 
     case CONSTANTS.TASKS.MIA_GROUP_4_TASKS.APPROVE_OR_DECLINE_THE_DEAL:
-      emailVariables = {
-        taskTitle: CONSTANTS.TASKS.MIA_GROUP_4_TASKS.APPROVE_OR_DECLINE_THE_DEAL,
-        taskUrl: generateTaskUrl(urlOrigin, deal.dealSnapshot._id, task),
-        exporterName,
-        ukefDealId,
-      };
-
       templateId = CONSTANTS.EMAIL_TEMPLATE_IDS.TASK_READY_TO_START;
-      team = await api.findOneTeam(task.team && task.team.id);
-      sendToEmailAddress = team.email;
-
       break;
 
     default:
   }
 
   if (templateId) {
+    team = await api.findOneTeam(task.team && task.team.id);
+    sendToEmailAddress = team.email;
+
     return sendTfmEmail(
       templateId,
       sendToEmailAddress,
