d76d8ecb6d973daaabb928838b5a5574d8a8f5d3
ttbarnes <tony@tonybarnes.me>
2021-06-07 15:30:47 +0100

    DTFS2-3559 - activity feed displaying submission date

---
+++ trade-finance-manager-ui/server/controllers/case/activity/index.js
@@ -0,0 +1,25 @@
+import api from '../../../api';
+
+const getActivity = async (req, res) => {
+  const dealId = req.params._id; // eslint-disable-line no-underscore-dangle
+  const deal = await api.getDeal(dealId);
+
+  if (!deal) {
+    return res.redirect('/not-found');
+  }
+
+  const { user } = req.session;
+
+  return res.render('case/activity/activity.njk', {
+    activePrimaryNavigation: 'manage work',
+    activeSubNavigation: 'activity',
+    deal: deal.dealSnapshot,
+    tfm: deal.tfm,
+    dealId: deal.dealSnapshot._id, // eslint-disable-line no-underscore-dangle
+    user,
+  });
+};
+
+export default {
+  getActivity,
+};
