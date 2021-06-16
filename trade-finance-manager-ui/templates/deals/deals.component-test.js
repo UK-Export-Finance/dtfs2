6834d2b70764f42150d650c9b53e4eb6df3df14b
ttbarnes <tony@tonybarnes.me>
2021-06-16 13:21:13 +0100

    add tfm ui component tests

---
+++ trade-finance-manager-ui/templates/deals/deals.component-test.js
@@ -0,0 +1,30 @@
+const pageRenderer = require('../../component-tests/pageRenderer');
+
+const page = '../templates/deals/deals.njk';
+const render = pageRenderer(page);
+
+describe(page, () => {
+  let wrapper;
+  const params = {
+    heading: 'All deals',
+    deals: [],
+    user: {},
+  };
+
+  beforeEach(() => {
+    wrapper = render(params);
+  });
+
+  it('should render heading', () => {
+    wrapper.expectElement('[data-cy="deals-heading"]').toExist();
+    wrapper.expectText('[data-cy="deals-heading"]').toRead(params.heading);
+  });
+
+  it('should render search form component', () => {
+    wrapper.expectElement('[data-cy="ukef-search-form"]').toExist();
+  });
+
+  it('should render deals table component', () => {
+    wrapper.expectElement('[data-cy="deals-table"]').toExist();
+  });
+});
