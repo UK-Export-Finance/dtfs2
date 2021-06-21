1201900602d40cf779ce14308a41d833bae5afb3
ttbarnes <tony@tonybarnes.me>
2021-06-21 13:50:41 +0100

    DTFS2-3066 - sortBy - constants

---
+++ trade-finance-manager-ui/server/constants/deals.js
@@ -0,0 +1,14 @@
+const TFM_SORT_BY = {
+  ASCENDING: 'ascending',
+  DESCENDING: 'descending',
+};
+
+const TFM_SORT_BY_DEFAULT = {
+  field: 'dealSnapshot.details.submissionDate',
+  order: TFM_SORT_BY.ASCENDING,
+};
+
+module.exports = {
+  TFM_SORT_BY,
+  TFM_SORT_BY_DEFAULT,
+};
