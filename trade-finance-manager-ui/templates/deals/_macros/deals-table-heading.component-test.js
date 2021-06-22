872b24ebd0a35365d9ddecc032f11cc1bf17e246
ttbarnes <tony@tonybarnes.me>
2021-06-22 10:54:16 +0100

    DTFS2-3066 - pass fieldName to deals-table heading

--- trade-finance-manager-ui/templates/deals/_macros/deals-table-heading.component-test.js
+++ trade-finance-manager-ui/templates/deals/_macros/deals-table-heading.component-test.js
@@ -13,6 +13,7 @@ describe(component, () => {
   describe('when params.activeSortByOrder does not match params.buttonValue', () => {
     it('should render aria-sort label with default `ascending` value', () => {
       params = {
+        fieldName: 'testing',
         buttonText: 'test',
         activeSortByField: 'dealSnapshot.details.someOtherField',
         activeSortByOrder: 'descending',
@@ -20,13 +21,14 @@ describe(component, () => {
       };
 
       wrapper = render(params);
-      wrapper.expectAriaSort('[data-cy="deals-table-heading-deal-id"]').toEqual('ascending');
+      wrapper.expectAriaSort(`[data-cy="deals-table-heading-${params.fieldName}"]`).toEqual('ascending');
     });
   });
 
   describe('when params.activeSortByOrder matches params.buttonValue', () => {
     it('should render aria-sort label with params.activeSortByOrder value', () => {
       params = {
+        fieldName: 'testing',
         buttonText: 'test',
         activeSortByField: 'dealSnapshot.details.ukefDealId',
         activeSortByOrder: 'descending',
@@ -34,46 +36,51 @@ describe(component, () => {
       };
 
       wrapper = render(params);
-      wrapper.expectAriaSort('[data-cy="deals-table-heading-deal-id"]').toEqual(params.activeSortByOrder);
+      wrapper.expectAriaSort(`[data-cy="deals-table-heading-${params.fieldName}"]`).toEqual(params.activeSortByOrder);
     });
   });
 
   describe('button', () => {
     it('should render text from params', () => {
       params = {
+        fieldName: 'testing',
         buttonText: 'Test',
       };
 
       wrapper = render(params);
-      wrapper.expectText('[data-cy="deals-table-heading-deal-id"]').toRead(params.buttonText);
+      wrapper.expectText(`[data-cy="deals-table-heading-${params.fieldName}"]`).toRead(params.buttonText);
     });
 
     it('should render value attribute from params', () => {
       params = {
+        fieldName: 'testing',
         buttonValue: 'dealSnapshot.details.ukefDealId',
       };
 
       wrapper = render(params);
-      wrapper.expectInput('[data-cy="deals-table-heading-deal-id-button"]').toHaveValue(params.buttonValue);
+      wrapper.expectInput(`[data-cy="deals-table-heading-${params.fieldName}-button"]`).toHaveValue(params.buttonValue);
     });
 
     it('should render default name attribute', () => {
-      params = {};
+      params = {
+        fieldName: 'testing',
+      };
 
       wrapper = render(params);
-      wrapper.expectElement('[data-cy="deals-table-heading-deal-id-button"]').toHaveAttribute('name', 'ascending');
+      wrapper.expectElement(`[data-cy="deals-table-heading-${params.fieldName}-button"]`).toHaveAttribute('name', 'ascending');
     });
 
     describe('when fieldHasActiveSortBy and when params.activeSortByOrder equals default `ascending`', () => {
       it('should render `descending` name attribute`', () => {
         params = {
+          fieldName: 'testing',
           activeSortByField: 'dealSnapshot.details.ukefDealId',
           buttonValue: 'dealSnapshot.details.ukefDealId',
           activeSortByOrder: 'ascending',
         };
 
         wrapper = render(params);
-        wrapper.expectElement('[data-cy="deals-table-heading-deal-id-button"]').toHaveAttribute('name', 'descending');
+        wrapper.expectElement(`[data-cy="deals-table-heading-${params.fieldName}-button"]`).toHaveAttribute('name', 'descending');
       });
     });
   });
