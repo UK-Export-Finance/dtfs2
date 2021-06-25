b23dc9358fd139c8de11d2729a3c9d2f5b3a56fc
ttbarnes <tony@tonybarnes.me>
2021-06-25 15:35:45 +0100

    tfm e2e tests - creating deals and facilties with dynamic ukefId via number generator

--- e2e-tests/trade-finance-manager/cypress/support/portal-api/createFacilities.js
+++ e2e-tests/trade-finance-manager/cypress/support/portal-api/createFacilities.js
@@ -1,14 +1,28 @@
 const { logIn, createFacilities } = require('./api');
+const { getIdFromNumberGenerator } = require('../reference-data-api/api');
 
 module.exports = (dealId, facilities, user) => {
   console.log('createFacilities::');
 
   logIn(user).then((token) => {
-    createFacilities(
-      dealId,
-      facilities,
-      user,
-      token,
-    ).then((createdFacilities) => createdFacilities);
+    const facilitiesWithUkefIds = [];
+
+    facilities.forEach((facilityToInsert) => {
+      getIdFromNumberGenerator('facility').then(({ id: numberGeneratorId }) => {
+        const facilityWithId = facilityToInsert;
+        facilityWithId.ukefFacilityID = numberGeneratorId;
+
+        facilitiesWithUkefIds.push(facilityWithId);
+
+        if (facilitiesWithUkefIds.length === facilities.length) {
+          createFacilities(
+            dealId,
+            facilities,
+            user,
+            token,
+          ).then((createdFacilities) => createdFacilities);
+        }
+      });
+    });
   });
 };
