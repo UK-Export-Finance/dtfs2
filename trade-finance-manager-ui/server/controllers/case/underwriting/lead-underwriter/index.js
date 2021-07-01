bf11b76211ee6868357f744f53c47a06575e36fa
ttbarnes <tony@tonybarnes.me>
2021-07-01 11:58:15 +0100

    DTFS2-3724 - lead underwriter `assign` route/page

--- trade-finance-manager-ui/server/controllers/case/underwriting/lead-underwriter/index.js
+++ trade-finance-manager-ui/server/controllers/case/underwriting/lead-underwriter/index.js
@@ -2,78 +2,6 @@ import api from '../../../../api';
 import CONSTANTS from '../../../../constants';
 import mapAssignToSelectOptions from '../../../../helpers/map-assign-to-select-options';
 
-// export const mapAssignToSelectOptions = (assignedToUserId = '', currentUser, allTeamMembers) => {
-//   // eslint-disable-next-line no-underscore-dangle
-//   const isCurrentlyAssignedToUser = isAssignedToUser(assignedToUserId, currentUser._id);
-
-//   const currentUserFullName = userFullName(currentUser);
-//   const assignToMeCopy = `${currentUserFullName} (Assign to me)`;
-
-//   const currentUserId = currentUser._id; // eslint-disable-line no-underscore-dangle
-
-//   const mappedTeamMembersSelectOptions = mapTeamMembersSelectOptions(
-//     allTeamMembers,
-//     assignedToUserId,
-//     currentUserId,
-//   );
-
-//   const isUnassigned = assignedToUserId === 'Unassigned';
-
-//   // 3 possible states:
-//   // is assigned to someone that is not the currently logged in user.
-//   // is unassigned
-//   // is assigned to me/currently logged in user.
-
-//   let mapped = [
-//     {
-//       value: currentUserId,
-//       text: assignToMeCopy,
-//       selected: false,
-//     },
-//     {
-//       value: 'Unassigned',
-//       text: 'Unassigned',
-//       selected: false,
-//     },
-//     ...mappedTeamMembersSelectOptions,
-//   ];
-
-//   if (isUnassigned) {
-//     mapped = [
-//       {
-//         value: 'Unassigned',
-//         text: 'Unassigned',
-//         selected: true,
-//       },
-//       {
-//         value: currentUserId,
-//         text: assignToMeCopy,
-//         selected: false,
-//       },
-//       ...mappedTeamMembersSelectOptions,
-//     ];
-//   }
-
-//   if (isCurrentlyAssignedToUser) {
-//     mapped = [
-//       {
-//         value: 'Unassigned',
-//         text: 'Unassigned',
-//         selected: false,
-//       },
-//       {
-//         value: currentUserId,
-//         text: currentUserFullName,
-//         selected: true,
-//       },
-//       ...mappedTeamMembersSelectOptions,
-//     ];
-//   }
-
-//   return mapped;
-// };
-
-
 const getLeadUnderwriter = async (req, res) => {
   const dealId = req.params._id; // eslint-disable-line no-underscore-dangle
   const deal = await api.getDeal(dealId);
@@ -85,7 +13,6 @@ const getLeadUnderwriter = async (req, res) => {
   const { user } = req.session;
 
   const allTeamMembers = await api.getTeamMembers(CONSTANTS.TEAMS.UNDERWRITERS);
-  console.log('*** allTeamMembers \n', allTeamMembers);
 
   return res.render('case/underwriting/lead-underwriter/lead-underwriter.njk', {
     activePrimaryNavigation: 'manage work',
@@ -99,7 +26,67 @@ const getLeadUnderwriter = async (req, res) => {
   });
 };
 
+const getAssignLeadUnderwriter = async (req, res) => {
+  const dealId = req.params._id; // eslint-disable-line no-underscore-dangle
+  const deal = await api.getDeal(dealId);
+
+  if (!deal) {
+    return res.redirect('/not-found');
+  }
+
+  const { user } = req.session;
+
+  const allTeamMembers = await api.getTeamMembers(CONSTANTS.TEAMS.UNDERWRITERS);
+
+  return res.render('case/underwriting/lead-underwriter/assign-lead-underwriter.njk', {
+    activeSubNavigation: 'underwriting',
+    activeSideNavigation: 'lead underwriter',
+    deal: deal.dealSnapshot,
+    tfm: deal.tfm,
+    dealId: deal.dealSnapshot._id, // eslint-disable-line no-underscore-dangle
+    user,
+    assignToSelectOptions: mapAssignToSelectOptions('', user, allTeamMembers),
+  });
+};
+
+const postAssignLeadUnderwriter = async (req, res) => {
+  const dealId = req.params._id; // eslint-disable-line no-underscore-dangle
+  const deal = await api.getDeal(dealId);
+
+  if (!deal) {
+    return res.redirect('/not-found');
+  }
+
+  // const { user } = req.session;
+
+  // const allTeamMembers = await api.getTeamMembers(CONSTANTS.TEAMS.UNDERWRITERS);
+
+  // return res.render('case/underwriting/lead-underwriter/assign-lead-underwriter.njk', {
+  //   activeSubNavigation: 'underwriting',
+  //   activeSideNavigation: 'lead underwriter',
+  //   deal: deal.dealSnapshot,
+  //   tfm: deal.tfm,
+  //   dealId: deal.dealSnapshot._id, // eslint-disable-line no-underscore-dangle
+  //   user,
+  //   assignToSelectOptions: mapAssignToSelectOptions('', user, allTeamMembers),
+  // });
+
+  const {
+    assignedTo: assignedToValue, // will be user._id or `Unassigned`
+  } = req.body;
+
+  const update = {
+    userId: assignedToValue,
+  };
+
+  await api.updateUnderwriterManagersDecision(dealId, update);
+
+  return res.redirect(`/case/${dealId}/underwriting/lead-underwriter`);
+};
+
 
 export default {
   getLeadUnderwriter,
+  getAssignLeadUnderwriter,
+  postAssignLeadUnderwriter,
 };
