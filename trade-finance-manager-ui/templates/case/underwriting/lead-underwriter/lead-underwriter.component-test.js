5cc214dc996ceb843c59b81f282e9569944d8aa0
ttbarnes <tony@tonybarnes.me>
2021-07-08 16:56:49 +0100

    DTFS2-3724 - lead underwriter ui component tests

--- trade-finance-manager-ui/templates/case/underwriting/lead-underwriter/lead-underwriter.component-test.js
+++ trade-finance-manager-ui/templates/case/underwriting/lead-underwriter/lead-underwriter.component-test.js
@@ -5,8 +5,13 @@ const page = '../templates/case/underwriting/lead-underwriter/lead-underwriter.n
 const render = pageRenderer(page);
 
 describe(page, () => {
-  const params = {
+  let params = {
     dealId: '1234',
+    currentLeadUnderWriter: {
+      firstName: 'Test',
+      lastName: 'Testing',
+      email: 'hello@test.com',
+    },
   };
 
   it('should render heading', () => {
@@ -19,11 +24,59 @@ describe(page, () => {
     wrapper.expectText('[data-cy="lead-underwriter-heading"]').toRead('Lead Underwriter');
   });
 
-  it('should render `assign` link', () => {
+  it('should NOT render `assign` link', () => {
     const wrapper = render(params);
-    wrapper.expectLink('[data-cy="assign-lead-underwriter-link"]').toLinkTo(
-      `/case/${params.dealId}/underwriting/lead-underwriter/assign`,
-      'Assign a lead underwriter',
-    );
+    wrapper.expectElement('[data-cy="assign-lead-underwriter-link"]').notToExist();
+  });
+
+  it('should NOT render `change` link', () => {
+    const wrapper = render(params);
+    wrapper.expectElement('[data-cy="change-lead-underwriter-link"]').notToExist();
+  });
+
+  it('should render lead underwriter name', () => {
+    const wrapper = render(params);
+    const expected = `${params.currentLeadUnderWriter.firstName} ${params.currentLeadUnderWriter.lastName}`;
+    wrapper.expectText('[data-cy="lead-underwriter-name"]').toRead(expected);
+  });
+
+  it('should render lead underwriter email', () => {
+    const wrapper = render(params);
+    wrapper.expectText('[data-cy="lead-underwriter-email"]').toRead(params.currentLeadUnderWriter.email);
+  });
+
+  describe('with params.userCanEdit', () => {
+    beforeEach(() => {
+      params = {
+        ...params,
+        userCanEdit: true,
+      };
+    });
+
+    it('should render `change` link', () => {
+      const wrapper = render(params);
+      wrapper.expectLink('[data-cy="change-lead-underwriter-link"]').toLinkTo(
+        `/case/${params.dealId}/underwriting/lead-underwriter/assign`,
+        'Change',
+      );
+    });
+  });
+
+  describe('with no params.currentLeadUnderWriter and params.userCanEdit', () => {
+    beforeEach(() => {
+      params = {
+        dealId: '1234',
+        userCanEdit: true,
+      };
+    });
+
+    it('should render assign link', () => {
+      const wrapper = render(params);
+
+      wrapper.expectLink('[data-cy="assign-lead-underwriter-link"]').toLinkTo(
+        `/case/${params.dealId}/underwriting/lead-underwriter/assign`,
+        'Assign a lead underwriter',
+      );
+    });
   });
 });
