198099428fc9810fa469dc4ec1dca9bee42a17eb
Andrew Price <andrew@madebymoon.com>
2021-07-08 12:05:08 +0100

    Disabled API calls on healthchecks for performance gains.

--- trade-finance-manager-ui/server/healthcheck.js
+++ trade-finance-manager-ui/server/healthcheck.js
@@ -1,43 +1,43 @@
 import express from 'express';
-import axios from 'axios';
+// import axios from 'axios';
 // import companiesHouseAPI from './companies-house-api';
 
 const healthcheck = express.Router();
 const GITHUB_SHA = process.env.GITHUB_SHA || 'undefined';
-const apiUrl = process.env.TRADE_FINANCE_MANAGER_API_URL;
-const referenceDataProxyUrl = process.env.REFERENCE_DATA_PROXY_URL;
+// const apiUrl = process.env.TRADE_FINANCE_MANAGER_API_URL;
+// const referenceDataProxyUrl = process.env.REFERENCE_DATA_PROXY_URL;
 
-const apiHealthCheck = async () => {
-  try {
-    const response = await axios({
-      method: 'get',
-      url: `${apiUrl}/healthcheck`,
-    });
-    return response.data;
-  } catch (err) {
-    return err;
-  }
-};
+// const apiHealthCheck = async () => {
+//   try {
+//     const response = await axios({
+//       method: 'get',
+//       url: `${apiUrl}/healthcheck`,
+//     });
+//     return response.data;
+//   } catch (err) {
+//     return err;
+//   }
+// };
 
-const refDataHealthCheck = async () => {
-  try {
-    const response = await axios({
-      method: 'get',
-      url: `${referenceDataProxyUrl}/healthcheck`,
-    });
-    return response.data;
-  } catch (err) {
-    return err;
-  }
-};
+// const refDataHealthCheck = async () => {
+//   try {
+//     const response = await axios({
+//       method: 'get',
+//       url: `${referenceDataProxyUrl}/healthcheck`,
+//     });
+//     return response.data;
+//   } catch (err) {
+//     return err;
+//   }
+// };
 
 healthcheck.get('/healthcheck', async (req, res) => {
   res.status(200).json({
     ui: {
       commit_hash: GITHUB_SHA,
     },
-    api: await apiHealthCheck(),
-    referenceData: await refDataHealthCheck(),
+    // api: await apiHealthCheck(),
+    // referenceData: await refDataHealthCheck(),
   });
 });
 
