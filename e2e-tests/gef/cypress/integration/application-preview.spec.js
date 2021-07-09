b59405915db2d07252f927cf1b72e4f421ee0d6f
eddieleeper <eddie.leeper@gmail.com>
2021-07-09 11:51:39 +0100

    DTFS2-3967 Create checker preview page (#147)
    * DTFS2-3967 Create checker preview page

---
+++ e2e-tests/gef/cypress/integration/application-preview.spec.js
@@ -0,0 +1,86 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+import relative from './relativeURL';
+import applicationPreview from './pages/application-preview';
+import CREDENTIALS from '../fixtures/credentials.json';
+
+const applicationIds = [];
+
+context('Application Checker Preview Page', () => {
+  before(() => {
+    cy.on('uncaught:exception', () => false);
+    cy.reinsertMocks();
+    cy.apiLogin(CREDENTIALS.CHECKER)
+      .then((token) => token)
+      .then((token) => {
+        cy.apiFetchAllApplications(token);
+      })
+      .then(({ body }) => {
+        body.items.forEach((item) => {
+          applicationIds.push(item._id);
+        });
+      });
+    cy.login(CREDENTIALS.MAKER);
+  });
+
+  beforeEach(() => {
+    cy.on('uncaught:exception', () => false);
+    Cypress.Cookies.preserveOnce('connect.sid');
+    cy.visit(relative(`/gef/application-details/${applicationIds[2]}/preview`));
+  });
+
+  describe('Visiting checker preview page', () => {
+    it('displays the application summary section', () => {
+      applicationPreview.dealId();
+      applicationPreview.status();
+      applicationPreview.exporter();
+      applicationPreview.bankReference();
+      applicationPreview.dateCreated();
+      applicationPreview.createdBy();
+      applicationPreview.checkedBy();
+      applicationPreview.product();
+      applicationPreview.submissionType();
+      applicationPreview.facilitySummary();
+    });
+
+    it('displays the Task and comment entries', () => {
+      applicationPreview.task();
+      applicationPreview.comments();
+    });
+
+    it('displays the application banner', () => {
+      applicationPreview.applicationBanner();
+      applicationPreview.backLink();
+      applicationPreview.bankReference()
+        .should('have.text', 'HSBC 123');
+    });
+
+    it('displays the correct exporter elements', () => {
+      applicationPreview.exporterHeading();
+      applicationPreview.exporterSummaryList();
+    });
+
+    it('displays the correct automatic cover elements', () => {
+      applicationPreview.automaticCoverHeading();
+      applicationPreview.automaticCoverSummaryList();
+    });
+
+    it('displays the correct facility summary elements', () => {
+      applicationPreview.facilityHeading();
+      applicationPreview.facilitySummaryList();
+    });
+
+    it('displays the correct submit elements', () => {
+      applicationPreview.submitHeading();
+      applicationPreview.submitButton();
+      applicationPreview.returnButton();
+    });
+  });
+
+  describe('When the application is not complete, visiting checker preview page', () => {
+    it('redirects to the applications and notices page', () => {
+      cy.visit(relative(`/gef/application-details/${applicationIds[1]}/preview`));
+      cy.location('pathname').should('eq', '/dashboard/0');
+    });
+  });
+});
