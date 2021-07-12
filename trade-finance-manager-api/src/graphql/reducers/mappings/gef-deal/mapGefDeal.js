0556ab5ac1d3233f3533825176284de6552b529c
ttbarnes <tony@tonybarnes.me>
2021-07-12 16:48:39 +0100

    DTFS2-4414 - initial tfm gef deal mapping

--- trade-finance-manager-api/src/graphql/reducers/mappings/gef-deal/mapGefDeal.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/gef-deal/mapGefDeal.js
@@ -1,17 +1,66 @@
-const mapGefDeal = (deal) => ({
-  _id: deal._id,
-  dealSnapshot: {
+const mapGefDeal = (deal) => {
+  const mapped = {
     _id: deal._id,
-    details: {},
-    totals: {},
-    facilities: [],
-    submissionDetails: {},
-    eligibilityCriteria: [],
-    eligibility: {},
-    dealFiles: {},
-  },
-  tfm: {},
-});
+    dealSnapshot: {
+      _id: deal._id,
+      details: {
+        ukefDealId: 'UKEF-ID-TODO',
+        bankSupplyContractID: deal.dealSnapshot.bankInternalRefName,
+        bankSupplyContractName: deal.dealSnapshot.additionalRefName,
+        // currently not in GEF data:
+        //
+        // submissionType
+        // owningBank: {
+        //   name
+        // }
+        // submissionDate
+        // ukefDealId
+        // might need to get maker details on submission
+        // so it's not stored in the GEF deal.
+        // maker: {
+        //   fistname
+        //   surname
+        //   email
+        // }      
+      },
+      totals: {
+        // need to look into facilities in GEF deal
+        // facilitiesValueInGBP
+        // facilitiesUkefExposure
+      },
+      facilities: [],
+      submissionDetails: {
+        // supplierType: 'HELLOOO TONY 123',
+        supplierName: deal.dealSnapshot.exporter.companyName,
+        // currently not in GEF data:
+        // supplierType (TFM label is either "Exporter" or "Tier 1 supplier")
+        // buyerName
+        // supplyContractDescription ("Export description")
+        // supplyContractCurrency ("Export contract value")
+
+        // unsure what to use
+        // destinationCountry ("Destination country") < should this be exporter.registeredAddress.country?
+      },
+      // TODO: all eligibility (is this coverTerms in GEF?)
+      eligibilityCriteria: [],
+      eligibility: {
+        // currently not in GEF data / unsure what to use:
+        // agentName
+      },
+      dealFiles: {},
+    },
+    // tfm object will be created when submitted to TFM
+    tfm: {
+      // stage
+      // dateReceived
+      //
+      // TODO: for all deals -
+      // TFM case summary should use dateReceived, not details.submissionDate
+    },
+  };
+
+  return mapped;
+};
 
 
 module.exports = mapGefDeal;
