9414ba9885bb792667480054bfd99ee3588e1645
Andrew Price <andrew@madebymoon.com>
2021-07-13 20:50:59 +0100

    fix/e2e timeout fixes - eStore debug

--- reference-data-proxy/src/v1/api-estore.js
+++ reference-data-proxy/src/v1/api-estore.js
@@ -5,7 +5,7 @@ const postToAPI = async (apiEndpoint, apiData) => {
     return false;
   }
 
-  console.log('Calling eStore API: ', apiEndpoint);
+  console.log('Calling eStore API: ', `${process.env.MULESOFT_API_UKEF_ESTORE_EA_URL}/${apiEndpoint}`);
 
   const response = await axios({
     method: 'post',
