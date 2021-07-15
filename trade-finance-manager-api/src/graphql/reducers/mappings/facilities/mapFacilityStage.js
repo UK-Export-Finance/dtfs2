519b98145938c77de4fb1abd5ae04db05cd4c2fc
ttbarnes <tony@tonybarnes.me>
2021-07-15 14:19:54 +0100

    DTFS2-4414 -tfm facility stage mapping allowing for gef facility issued flag

--- trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityStage.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityStage.js
@@ -3,12 +3,21 @@ const CONSTANTS = require('../../../../constants');
 const mapFacilityStage = (facilityStage) => {
   let mapped;
 
-  if (facilityStage === CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL.UNISSUED
-    || facilityStage === CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL.CONDITIONAL) {
-    mapped = CONSTANTS.FACILITIES.FACILITY_STAGE.COMMITMENT;
-  } else if (facilityStage === CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL.ISSUED
-    || facilityStage === CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL.UNCONDITIONAL) {
+  // NOTE: we have two data sources.
+  // - Bond & Loan facilities have facilityStage string of 'issued' or 'unissued'.
+  // - Cash & Contingent facilities have hasBeenIssued boolean.
+  // TODO: add hasBeenIssued boolean to Bond & Loan facilities.
+
+  const isIssuedGefFacility = (facilityStage === true);
+
+  if (facilityStage === CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL.ISSUED
+    || facilityStage === CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL.UNCONDITIONAL
+    || isIssuedGefFacility) {
     mapped = CONSTANTS.FACILITIES.FACILITY_STAGE.ISSUED;
+  } else if (facilityStage === CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL.UNISSUED
+    || facilityStage === CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL.CONDITIONAL
+    || !isIssuedGefFacility) {
+    mapped = CONSTANTS.FACILITIES.FACILITY_STAGE.COMMITMENT;
   }
 
   return mapped;
