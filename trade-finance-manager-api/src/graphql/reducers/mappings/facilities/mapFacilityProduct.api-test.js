759dba119acf30254ad0fcc77e3375fdc68ad40a
ttbarnes <tony@tonybarnes.me>
2021-07-15 15:44:44 +0100

    DTFS2-4414 - tfm facility product and type allowing for GEF facilities

--- trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityProduct.api-test.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityProduct.api-test.js
@@ -3,9 +3,7 @@ const mapFacilityProduct = require('./mapFacilityProduct');
 describe('mapFacilityProduct', () => {
   describe('when facilityType is `bond`', () => {
     it('should return bond product name and code', () => {
-      const result = mapFacilityProduct({
-        facilityType: 'bond',
-      });
+      const result = mapFacilityProduct('bond');
       expect(result).toEqual({
         code: 'BSS',
         name: 'Bond Support Scheme',
@@ -15,12 +13,30 @@ describe('mapFacilityProduct', () => {
 
   describe('when facilityType is `loan`', () => {
     it('should return loan product name and code', () => {
-      const result = mapFacilityProduct({
-        facilityType: 'loan',
-      });
+      const result = mapFacilityProduct('loan');
       expect(result).toEqual({
         code: 'EWCS',
         name: 'Export Working Capital Scheme',
+      });
+    });
+  });
+
+  describe('when facilityType is `CASH`', () => {
+    it('should return Cash product name and GEF product code', () => {
+      const result = mapFacilityProduct('CASH');
+      expect(result).toEqual({
+        code: 'GEF',
+        name: 'Cash',
+      });
+    });
+  });
+
+  describe('when facilityType is `CASH`', () => {
+    it('should return Cash product name and GEF product code', () => {
+      const result = mapFacilityProduct('CONTINGENT');
+      expect(result).toEqual({
+        code: 'GEF',
+        name: 'Contingent',
       });
     });
   });
--- trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityType.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityType.js
@@ -3,6 +3,7 @@ const { capitalizeFirstLetter } = require('../../../../utils/string');
 
 const mapFacilityType = (facility) => {
   const { facilityProduct } = facility;
+
   if (facilityProduct.code === CONSTANTS.FACILITIES.FACILITY_PRODUCT_CODE.BOND) {
     return facility.bondType;
   }
@@ -11,6 +12,15 @@ const mapFacilityType = (facility) => {
     return capitalizeFirstLetter(CONSTANTS.FACILITIES.FACILITY_TYPE.LOAN);
   }
 
+  if (facilityProduct.name === CONSTANTS.FACILITIES.FACILITY_PRODUCT_NAME.CASH) {
+    return `${CONSTANTS.FACILITIES.FACILITY_PRODUCT_NAME.CASH} facility`;
+  }
+
+  if (facilityProduct.name === CONSTANTS.FACILITIES.FACILITY_PRODUCT_NAME.CONTINGENT) {
+    return `${CONSTANTS.FACILITIES.FACILITY_PRODUCT_NAME.CONTINGENT} facility`;
+  }
+
+  // TODO gef versions with facility at the end
   return null;
 };
 
