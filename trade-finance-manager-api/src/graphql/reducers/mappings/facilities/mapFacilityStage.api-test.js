bade31924c0aa84f741c0ec31478ab12414c3cb1
ttbarnes <tony@tonybarnes.me>
2021-07-15 14:19:54 +0100

    DTFS2-4414 -tfm facility stage mapping allowing for gef facility issued flag

--- trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityStage.api-test.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapFacilityStage.api-test.js
@@ -1,6 +1,27 @@
 const mapFacilityStage = require('./mapFacilityStage');
 
 describe('mapFacilityStage', () => {
+  describe('when facilityStage is `Issued`', () => {
+    it('should return facilityStage as `Issued`', () => {
+      const result = mapFacilityStage('Issued');
+      expect(result).toEqual('Issued');
+    });
+  });
+
+  describe('when facilityStage is `Unconditional`', () => {
+    it('should return facilityStage as `Issued`', () => {
+      const result = mapFacilityStage('Unconditional');
+      expect(result).toEqual('Issued');
+    });
+  });
+
+  describe('when facilityStage is true boolean', () => {
+    it('should return facilityStage as `Commitment`', () => {
+      const result = mapFacilityStage(true);
+      expect(result).toEqual('Issued');
+    });
+  });
+
   describe('when facilityStage is `Unissued`', () => {
     it('should return facilityStage as `Commitment`', () => {
       const result = mapFacilityStage('Unissued');
@@ -15,17 +36,10 @@ describe('mapFacilityStage', () => {
     });
   });
 
-  describe('when facilityStage is `Issued`', () => {
-    it('should return facilityStage as `Issued`', () => {
-      const result = mapFacilityStage('Issued');
-      expect(result).toEqual('Issued');
-    });
-  });
-
-  describe('when facilityStage is `Unconditional`', () => {
-    it('should return facilityStage as `Issued`', () => {
-      const result = mapFacilityStage('Unconditional');
-      expect(result).toEqual('Issued');
+  describe('when facilityStage is false boolean', () => {
+    it('should return facilityStage as `Commitment`', () => {
+      const result = mapFacilityStage(false);
+      expect(result).toEqual('Commitment');
     });
   });
 });
