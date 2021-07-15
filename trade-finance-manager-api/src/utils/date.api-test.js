90c78a243bb934a44fd4782305ff5350f91fdf29
ttbarnes <tony@tonybarnes.me>
2021-07-15 15:47:58 +0100

    DTFS2-4414 - tfm function to convert date to timestamp

--- trade-finance-manager-api/src/utils/date.api-test.js
+++ trade-finance-manager-api/src/utils/date.api-test.js
@@ -2,6 +2,7 @@ const {
   formatYear,
   formatDate,
   formatTimestamp,
+  convertDateToTimestamp,
 } = require('./date');
 
 describe('utils - date', () => {
@@ -32,6 +33,13 @@ describe('utils - date', () => {
     it('should return a date in YYYY-MM-DD format', () => {
       const result = formatTimestamp(1618842665642);
       expect(result).toEqual('2021-04-19');
+    });
+  });
+
+  describe('convertDateToTimestamp', () => {
+    it('should return a timestamp', () => {
+      const result = convertDateToTimestamp('2023-12-31 00:00:00.000Z');
+      expect(typeof result).toEqual('string');
     });
   });
 });
