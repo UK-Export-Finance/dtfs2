f81b49c46dce9a01b3d7b800b6ac026cc7f9bc92
ttbarnes <tony@tonybarnes.me>
2021-07-16 14:13:32 +0100

    DTFS2-4414 - tfm deal totals mapping gef facility values

--- trade-finance-manager-api/src/graphql/reducers/mappings/gef-facilities/mapGefFacilities.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/gef-facilities/mapGefFacilities.js
@@ -3,20 +3,9 @@ const mapGefFacility = require('./mapGefFacility');
 const mapGefFacilities = (facilities, dealSnapshot) => {
   const mappedFacilities = [];
 
-  console.log('mapGefFacilities......facilities \n', facilities);
-  // console.log('mapGefFacilities......dealSnapshot \n', dealSnapshot);
-
   facilities.forEach((f) => {
-    // mappedFacilities.push({
-    //   _id: f._id, // eslint-disable-line no-underscore-dangle
-    //   facilitySnapshot: mapGefFacility(f, dealSnapshot),
-    //   tfm: {},
-    // });
-
     mappedFacilities.push(mapGefFacility(f, dealSnapshot));
   });
-
-  console.log('mapGefFacilities......mappedFacilities \n', mappedFacilities);
 
   return mappedFacilities;
 };
