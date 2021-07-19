8c3548871acb1d787a6003b6f075ebee7a10a418
ttbarnes <tony@tonybarnes.me>
2021-07-19 15:13:41 +0100

    DTFS2-4414 - tfm gef facility mapping - unit tests

---
+++ trade-finance-manager-api/src/graphql/reducers/mappings/gef-facilities/mapGefFacilityDates.api-test.js
@@ -0,0 +1,72 @@
+const moment = require('moment');
+const mapGefFacilityDates = require('./mapGefFacilityDates');
+const mapCoverEndDate = require('../facilities/mapCoverEndDate');
+const mapTenorDate = require('../facilities/mapTenorDate');
+const { convertDateToTimestamp } = require('../../../../utils/date');
+
+const MOCK_GEF_DEAL = require('../../../../v1/__mocks__/mock-gef-deal');
+
+describe('mapGefFacilityDates', () => {
+  const mockFacility = {
+    ...MOCK_GEF_DEAL.facilities[0],
+    facilityStage: 'Issued',
+  };
+
+  const mockFacilityTfm = {
+    exposurePeriodInMonths: 12,
+  };
+
+  it('should return inclusionNoticeReceived as deal submissionDate if manualInclusionNoticeSubmissionDate is empty', () => {
+    const result = mapGefFacilityDates(mockFacility, mockFacilityTfm, MOCK_GEF_DEAL);
+    expect(result.inclusionNoticeReceived).toEqual(MOCK_GEF_DEAL.submissionDate);
+  });
+
+  it('should return manualInclusionNoticeSubmissionDate as deal submissionDate if manualInclusionNoticeSubmissionDate has a value', () => {
+    const mockMinDeal = {
+      ...MOCK_GEF_DEAL,
+      manualInclusionNoticeSubmissionDate: '16069006199999',
+    };
+
+    const result = mapGefFacilityDates(mockFacility, mockFacilityTfm, mockMinDeal);
+
+    expect(result.inclusionNoticeReceived).toEqual(mockMinDeal.manualInclusionNoticeSubmissionDate);
+  });
+
+  it('should return bankIssueNoticeReceived as issuedFacilitySubmittedToUkefTimestamp', () => {
+    const result = mapGefFacilityDates(mockFacility, mockFacilityTfm, MOCK_GEF_DEAL);
+
+    expect(result.bankIssueNoticeReceived).toEqual(mockFacility.issuedFacilitySubmittedToUkefTimestamp);
+  });
+
+  it('should return coverStartDate as timestamp', () => {
+    const result = mapGefFacilityDates(mockFacility, mockFacilityTfm, MOCK_GEF_DEAL);
+
+    const expected = convertDateToTimestamp(mockFacility.coverStartDate);
+
+    expect(result.coverStartDate).toEqual(expected);
+  });
+
+  it('should return mapped coverEndDate', () => {
+    const result = mapGefFacilityDates(mockFacility, mockFacilityTfm, MOCK_GEF_DEAL);
+
+    const expected = mapCoverEndDate(
+      moment(mockFacility.coverEndDate).format('DD'),
+      moment(mockFacility.coverEndDate).format('MM'),
+      moment(mockFacility.coverEndDate).format('YYYY'),
+    );
+
+    expect(result.coverEndDate).toEqual(expected);
+  });
+
+  it('should return mapped tenor', () => {
+    const result = mapGefFacilityDates(mockFacility, mockFacilityTfm, MOCK_GEF_DEAL);
+
+    const expected = mapTenorDate(
+      mockFacility.facilityStage,
+      mockFacility.monthsOfCover,
+      mockFacilityTfm.exposurePeriodInMonths,
+    );
+
+    expect(result.tenor).toEqual(expected);
+  });
+});
