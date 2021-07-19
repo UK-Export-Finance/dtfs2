efe7a7f6ba6f6f1172c209802b6a7de7f69c6b1e
ttbarnes <tony@tonybarnes.me>
2021-07-19 11:47:52 +0100

    DTFS2-4414 - tfm gef deal mapping - unit tests

---
+++ trade-finance-manager-api/src/graphql/reducers/mappings/gef-deal/mapGefDeal.api-test.js
@@ -0,0 +1,26 @@
+const mapGefDeal = require('./mapGefDeal');
+const mapGefDealSnapshot = require('./mapGefDealSnapshot');
+const MOCK_GEF_DEAL = require('../../../../v1/__mocks__/mock-gef-deal');
+
+describe('mapGefDeal', () => {
+  it('should return mapped deal', () => {
+    const mockDeal = {
+      _id: MOCK_GEF_DEAL._id,
+      dealSnapshot: {
+        ...MOCK_GEF_DEAL,
+        facilities: [],
+      },
+      tfm: {},
+    };
+
+    const result = mapGefDeal(mockDeal);
+
+    const expected = {
+      _id: MOCK_GEF_DEAL._id,
+      dealSnapshot: mapGefDealSnapshot(mockDeal.dealSnapshot),
+      tfm: {},
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
--- trade-finance-manager-api/src/graphql/reducers/mappings/gef-deal/mapGefDeal.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/gef-deal/mapGefDeal.js
@@ -1,20 +1,6 @@
 const mapGefDealSnapshot = require('./mapGefDealSnapshot');
 
 const mapGefDeal = (deal) => {
-  // fields that need to be in GEF data:
-  // submissionType
-  // submissionDate
-  // ukefDealId
-  // bankName
-  // maker {
-  //   firstname
-  //   surname
-  //   email
-  // }
-  // dealCurrency(e.g 'EUR')
-  // dealValue(e.g 1234)
-  // eligibility []
-
   const mapped = {
     _id: deal._id,
     dealSnapshot: mapGefDealSnapshot(deal.dealSnapshot),
