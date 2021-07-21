b8361e4ef538831daf58b5970a3b7d326a5094e4
David Lewis <dave@davehlewis.com>
2021-07-21 13:58:02 +0100

    DTFS2-4120: add resolver and schema for gef deals

--- portal-api/src/graphql/resolvers/index.js
+++ portal-api/src/graphql/resolvers/index.js
@@ -2,11 +2,13 @@
 const queryAllDeals = require('./query-all-deals');
 const queryDeals = require('./query-deals');
 const queryTransactions = require('./query-transactions');
+const queryGefDeals = require('./query-gef-deals');
 
 const resolvers = {
   Query: {
     allDeals: queryAllDeals,
     deals: queryDeals,
+    gefDeals: queryGefDeals,
     transactions: queryTransactions,
   },
 };
