46c223fc62adae00f6c35db5202b0dc49e3a5594
David Lewis <dave@davehlewis.com>
2021-07-27 12:02:23 +0100

    DTFS2-4328: pad company no to always be 8 chars

--- reference-data-proxy/src/v1/controllers/companies-house.controller.js
+++ reference-data-proxy/src/v1/controllers/companies-house.controller.js
@@ -3,9 +3,11 @@ const axios = require('axios');
 exports.lookup = async (req, res) => {
   const { companyRegNo } = req.params;
 
+  const sanitisedRegNo = companyRegNo.padStart(8, '0');
+
   const response = await axios({
     method: 'get',
-    url: `${process.env.COMPANIES_HOUSE_API_URL}/company/${companyRegNo}`,
+    url: `${process.env.COMPANIES_HOUSE_API_URL}/company/${sanitisedRegNo}`,
     auth: {
       username: process.env.COMPANIES_HOUSE_API_KEY,
     },
