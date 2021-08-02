40946ea4dd6099a60c7232e3abfcef91dcce807c
ttbarnes <tony@tonybarnes.me>
2021-08-02 12:12:32 +0100

    DTFS2-4545 - fix audit-supply contracts e2e test for multiple deals by 1 bank

--- e2e-tests/portal/cypress/integration/journeys/maker/reports/audit-supply-contracts/report-should-only-show-data-from-users-bank.spec.js
+++ e2e-tests/portal/cypress/integration/journeys/maker/reports/audit-supply-contracts/report-should-only-show-data-from-users-bank.spec.js
@@ -3,6 +3,7 @@ const { auditSupplyContracts } = reports;
 
 const mockUsers = require('../../../../../fixtures/mockUsers');
 const BANK1_MAKER = mockUsers.find((user) => (user.roles.includes('maker') && user.bank.name === 'UKEF test bank (Delegated)'));
+const BANK1_MAKER_CHECKER = mockUsers.find((user) => (user.roles.includes('maker') && user.roles.includes('checker') && user.bank.name === 'UKEF test bank (Delegated)'));
 const BANK2_MAKER = mockUsers.find((user) => (user.roles.includes('maker') && user.bank.name === 'UKEF test bank (Delegated) 2'));
 
 const {
@@ -13,6 +14,8 @@ const {
 } = require('../../../../../fixtures/transaction-dashboard-data');
 
 context('Audit - Report', () => {
+  let bank1Deals = [];
+
   beforeEach(() => {
     // [dw] at time of writing, the portal was throwing exceptions; this stops cypress caring
     cy.on('uncaught:exception', (err, runnable) => {
@@ -25,17 +28,24 @@ context('Audit - Report', () => {
     cy.deleteDeals(BANK1_MAKER);
     cy.deleteDeals(BANK2_MAKER);
 
-    cy.insertManyDeals([aDealWithOneBond, aDealWithTenLoans], BANK1_MAKER);
+    cy.insertManyDeals([aDealWithOneBond, aDealWithTenLoans], BANK1_MAKER).then((insertedDeals) => {
+      bank1Deals = [...bank1Deals, ...insertedDeals];
+    });
+
+    cy.insertManyDeals([aDealWithOneBond], BANK1_MAKER_CHECKER).then((insertedDeals) => {
+      bank1Deals = [...bank1Deals, ...insertedDeals];
+    });
 
     cy.insertManyDeals([aDealWithTenLoansAndTenBonds, aDealWithOneLoan], BANK2_MAKER);
   });
 
   it('can be filtered by bank', () => {
-    cy.login(BANK2_MAKER);
+    cy.login(BANK1_MAKER);
     auditSupplyContracts.visit();
 
     auditSupplyContracts.totalItems().invoke('text').then((text) => {
-      expect(text.trim()).equal('(2 items)');
+      const expected = `(${bank1Deals.length} items)`;
+      expect(text.trim()).equal(expected);
     });
   });
 });
