fbba4dd135117dfd595afff9a79a3335310ce7a9
ttbarnes <tony@tonybarnes.me>
2021-08-03 16:18:45 +0100

    DTFS2-4361 - fix tfm deals api test after findOneDeal rename to findOneTfmDeal

--- trade-finance-manager-api/api-tests/v1/deals/deals.api-test.js
+++ trade-finance-manager-api/api-tests/v1/deals/deals.api-test.js
@@ -1,4 +1,4 @@
-const { findOneDeal, findOnePortalDeal } = require('../../../src/v1/controllers/deal.controller');
+const { findOneTfmDeal, findOnePortalDeal } = require('../../../src/v1/controllers/deal.controller');
 const mapDeal = require('../../../src/v1/mappings/map-deal');
 const MOCK_DEAL = require('../../../src/v1/__mocks__/mock-deal');
 
@@ -7,12 +7,12 @@ const mappedDeal = mapDeal(MOCK_DEAL);
 describe('deal controller', () => {
   describe('find one TFM deal', () => {
     it('should return false if deal doesn\'t exist', async () => {
-      const deal = await findOneDeal('NO_DEAL_ID');
+      const deal = await findOneTfmDeal('NO_DEAL_ID');
       expect(deal).toEqual(false);
     });
 
     it('should return a deal', async () => {
-      const deal = await findOneDeal(MOCK_DEAL._id);
+      const deal = await findOneTfmDeal(MOCK_DEAL._id);
       expect(deal).toMatchObject({
         dealSnapshot: mappedDeal,
       });
