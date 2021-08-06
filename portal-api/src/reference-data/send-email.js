b7502f662b69b55096b713b973ab43ef7ed9a68b
David Lewis <dave@davehlewis.com>
2021-08-06 15:00:16 +0100

    DTFS2-3987: add email update to gef status change

---
+++ portal-api/src/reference-data/send-email.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const referenceProxyUrl = process.env.REFERENCE_DATA_PROXY_URL;
+
+const sendEmail = async (
+  templateId,
+  sendToEmailAddress,
+  emailVariables,
+) => {
+  try {
+    const { data } = await axios({
+      method: 'post',
+      url: `${referenceProxyUrl}/email`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        templateId,
+        sendToEmailAddress,
+        emailVariables,
+      },
+    });
+    return data;
+  } catch (err) {
+    console.log(`Error sending email to ${sendToEmailAddress}: ${err}`);
+    return false;
+  }
+};
+
+module.exports = sendEmail;
