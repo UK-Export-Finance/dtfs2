8e459fa23272d4a2e90ff9de32ff20f87a7b7633
David Lewis <dave@davehlewis.com>
2021-08-06 15:00:16 +0100

    DTFS2-3987: add email update to gef status change

--- portal-api/src/constants/index.js
+++ portal-api/src/constants/index.js
@@ -2,10 +2,12 @@ const FACILITIES = require('./facilities');
 const DEAL = require('./deal');
 const LOGINRESULTS = require('./login-results');
 const USER = require('./user');
+const EMAIL_TEMPLATE_IDS = require('./email-template-ids');
 
 module.exports = {
   FACILITIES,
   DEAL,
   LOGINRESULTS,
   USER,
+  EMAIL_TEMPLATE_IDS,
 };
