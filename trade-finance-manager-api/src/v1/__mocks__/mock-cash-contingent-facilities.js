1029d68a35d5fafa39a66c023f49062413e4dc4a
ttbarnes <tony@tonybarnes.me>
2021-08-12 14:54:44 +0100

    DTFS2-4361 - split out mock gef facilities

---
+++ trade-finance-manager-api/src/v1/__mocks__/mock-cash-contingent-facilities.js
@@ -0,0 +1,30 @@
+const MOCK_CASH_CONTINGENT_FACILIIES = [
+  {
+    _id: 'MOCK_CASH_FACILITY_ISSUED',
+    applicationId: 'MOCK_GEF_DEAL',
+    coverEndDate: '2021-12-12T00:00:00.000Z',
+    coverPercentage: 12,
+    coverStartDate: null,
+    createdAt: 1628693855675.0,
+    currency: 'EUR',
+    details: [
+      'RESOLVING',
+    ],
+    detailsOther: '',
+    hasBeenIssued: true,
+    interestPercentage: 24,
+    monthsOfCover: null,
+    name: 'issued1',
+    paymentType: null,
+    shouldCoverStartOnSubmission: true,
+    submittedAsIssuedDate: '1628770126495',
+    type: 'CASH',
+    ukefExposure: 1481472,
+    updatedAt: 1628770126497.0,
+    value: 123456,
+    ukefFacilityId: '123456',
+  },
+];
+
+
+module.exports = MOCK_CASH_CONTINGENT_FACILIIES;
