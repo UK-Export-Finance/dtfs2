3767fc87e20bcf3298788014ebe5e3c3d9a74089
ttbarnes <tony@tonybarnes.me>
2021-08-16 16:44:32 +0100

    DTFS2-4239 - simplify issued/unissued facility checks for deal submit email

--- trade-finance-manager-api/src/v1/helpers/notify-template-formatters.js
+++ trade-finance-manager-api/src/v1/helpers/notify-template-formatters.js
@@ -14,51 +14,47 @@ const generateFacilitiesListString = (facilities) => facilities.reduce((acc, fac
 }, '');
 
 const generateBSSListString = (facilities) => {
-  if (facilities.length) {
-    const bssList = facilities.reduce((acc, facility) => {
-      const {
-        bondType, ukefFacilityID, bankReferenceNumber, uniqueIdentificationNumber,
-      } = facility;
+  const bssList = facilities.reduce((acc, facility) => {
+    const {
+      bondType, ukefFacilityID, bankReferenceNumber, uniqueIdentificationNumber,
+    } = facility;
 
-      const bankReference = uniqueIdentificationNumber || bankReferenceNumber;
-      const bankRefString = bankReference
-        ? `*Your bank ref: ${bankReference}\n`
-        : '';
+    const bankReference = uniqueIdentificationNumber || bankReferenceNumber;
+    const bankRefString = bankReference
+      ? `*Your bank ref: ${bankReference}\n`
+      : '';
 
-      return `${acc}*${bondType}\n${bankRefString}*UKEF facility ID: ${ukefFacilityID} \n\n`;
-    }, '');
+    return `${acc}*${bondType}\n${bankRefString}*UKEF facility ID: ${ukefFacilityID} \n\n`;
+  }, '');
 
-    if (bssList.length) {
-      const heading = '#Bond Support Scheme\n\n';
-      return `${heading}${bssList}`;
-    }
+  if (bssList.length) {
+    const heading = '#Bond Support Scheme\n\n';
+    return `${heading}${bssList}`;
   }
 
-  return [];
+  return '';
 };
 
 const generateEWCSListString = (facilities) => {
-  if (facilities.length) {
-    const ewcsList = facilities.reduce((acc, facility) => {
-      const {
-        ukefFacilityID, bankReferenceNumber, uniqueIdentificationNumber,
-      } = facility;
+  const ewcsList = facilities.reduce((acc, facility) => {
+    const {
+      ukefFacilityID, bankReferenceNumber, uniqueIdentificationNumber,
+    } = facility;
 
-      const bankReference = uniqueIdentificationNumber || bankReferenceNumber;
-      const bankRefString = bankReference
-        ? `*Your bank ref: ${bankReference}\n`
-        : '';
+    const bankReference = uniqueIdentificationNumber || bankReferenceNumber;
+    const bankRefString = bankReference
+      ? `*Your bank ref: ${bankReference}\n`
+      : '';
 
-      return `${acc}${bankRefString}*UKEF facility ID: ${ukefFacilityID}\n\n`;
-    }, '');
+    return `${acc}${bankRefString}*UKEF facility ID: ${ukefFacilityID}\n\n`;
+  }, '');
 
-    if (ewcsList.length) {
-      const heading = '#Export Working Capital Scheme\n\n';
-      return `${heading}${ewcsList}`;
-    }
+  if (ewcsList.length) {
+    const heading = '#Export Working Capital Scheme\n\n';
+    return `${heading}${ewcsList}`;
   }
 
-  return [];
+  return '';
 };
 
 module.exports = {
