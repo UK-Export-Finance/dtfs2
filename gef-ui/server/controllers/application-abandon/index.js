dfb8ce2c64c5681ce748577b2b847fc117df074d
pbradley-f4 <paul.bradley@foundry4.com>
2021-08-17 10:48:04 +0100

    Initial abandon journey

---
+++ gef-ui/server/controllers/application-abandon/index.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-underscore-dangle */
+import * as api from '../../services/api';
+import { PROGRESS } from '../../../constants';
+import Application from '../../models/application';
+
+export const confirmAbandonApplication = async (req, res, next) => {
+  const {
+    params,
+    session,
+  } = req;
+  const { applicationId } = params;
+  const { user, userToken } = session;
+
+  try {
+    const application = await Application.findById(applicationId, user, userToken);
+    if (!application) {
+      // 404 not found or unauthorised
+      return res.redirect('/dashboard');
+    }
+
+    res.render('application-abandon.njk', { application });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const abandonApplication = async (req, res, next) => {
+  const { params } = req;
+  const { applicationId } = params;
+  try {
+    await api.setApplicationStatus(applicationId, PROGRESS.ABANDONED);
+  } catch (err) {
+    return next(err);
+  }
+  return res.redirect('/dashboard');
+};
+
+export default {
+  confirmAbandonApplication,
+  abandonApplication,
+};
