8cc42df080ad4f8411381b8722c119cf155661eb
pbradley-f4 <paul.bradley@foundry4.com>
2021-08-17 12:50:40 +0100

    Add tests

---
+++ gef-ui/server/routes/__tests__/application-abandon.test.js
@@ -0,0 +1,28 @@
+import validateToken from '../middleware/validate-token';
+
+const getSpy = jest.fn();
+const postSpy = jest.fn();
+jest.doMock('express', () => ({
+  Router() {
+    return {
+      get: getSpy,
+      post: postSpy,
+    };
+  },
+}));
+
+describe('Routes', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line global-require
+    require('../application-abandon');
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('Sets up all routes', () => {
+    expect(getSpy).toHaveBeenCalledWith('/application-details/:applicationId/abandon', validateToken, expect.any(Function));
+    expect(postSpy).toHaveBeenCalledWith('/application-details/:applicationId/abandon', validateToken, expect.any(Function));
+  });
+});
