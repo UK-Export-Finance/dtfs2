c96525ca9a19d7b3e7a63bee0934185947a639f7
ttbarnes <tony@tonybarnes.me>
2021-08-18 12:37:37 +0100

    DTFS2-4457 - update ui component test

--- trade-finance-manager-ui/templates/case/underwriting/pricing-and-risk/probability-of-default.component-test.js
+++ trade-finance-manager-ui/templates/case/underwriting/pricing-and-risk/probability-of-default.component-test.js
@@ -30,7 +30,7 @@ describe(page, () => {
     });
 
     it('should render probability of default input', () => {
-      wrapper.expectInput('[data-cy="input-probability-of-default"]').toHaveValue(params.tfm.probabilityOfDefault);
+      wrapper.expectInput('[data-cy="input-probability-of-default"]').toHaveValue(String(params.tfm.probabilityOfDefault));
     });
 
     it('should render save & close buttons', () => {
