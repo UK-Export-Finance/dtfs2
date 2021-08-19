cb97a9011b784efd20c32e5b80734517de5bf13e
ttbarnes <tony@tonybarnes.me>
2021-08-19 13:16:28 +0100

    DTFS2-4511 - update mock GEF deals in mock TFM api

--- trade-finance-manager-api/src/v1/__mocks__/mock-cash-contingent-facilities.js
+++ trade-finance-manager-api/src/v1/__mocks__/mock-cash-contingent-facilities.js
@@ -24,6 +24,31 @@ const MOCK_CASH_CONTINGENT_FACILIIES = [
     value: 123456,
     ukefFacilityId: '123456',
   },
+  {
+    _id: 'MOCK_CONTINGENT_FACILITY_ISSUED',
+    applicationId: 'MOCK_GEF_DEAL',
+    coverEndDate: '2021-12-12T00:00:00.000Z',
+    coverPercentage: 12,
+    coverStartDate: null,
+    createdAt: 1628693855675.0,
+    currency: 'EUR',
+    details: [
+      'RESOLVING',
+    ],
+    detailsOther: '',
+    hasBeenIssued: true,
+    interestPercentage: 24,
+    monthsOfCover: 10,
+    name: 'issued1',
+    paymentType: null,
+    shouldCoverStartOnSubmission: true,
+    submittedAsIssuedDate: '1628770126495',
+    type: 'CONTINGENT',
+    ukefExposure: 1481472,
+    updatedAt: 1628770126497.0,
+    value: 123456,
+    ukefFacilityId: '123456',
+  },
 ];
 
 
