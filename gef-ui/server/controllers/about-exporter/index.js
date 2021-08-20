6dee23614488856065ecf23a615084689380c014
pbradley-f4 <paul.bradley@foundry4.com>
2021-08-20 14:52:33 +0100

    Update valdation logic

--- gef-ui/server/controllers/about-exporter/index.js
+++ gef-ui/server/controllers/about-exporter/index.js
@@ -46,49 +46,45 @@ const validateAboutExporter = async (req, res) => {
     const { exporterId } = await api.getApplication(applicationId);
     const { details } = await api.getExporter(exporterId);
 
-    // Don't validate form if user clicks on 'return to application` button
-    if (!saveAndReturn) {
-      // Only validate industries if it contains more than 1 SIC code
-      if (!body.selectedIndustry && (details.industries && details.industries.length > 1)) {
-        aboutExporterErrors.push({
-          errRef: 'selectedIndustry',
-          errMsg: 'Select most appropriate industry',
-        });
-      }
+    // Only validate industries if it contains more than 1 SIC code
+    if (!saveAndReturn && !body.selectedIndustry && (details.industries && details.industries.length > 1)) {
+      aboutExporterErrors.push({
+        errRef: 'selectedIndustry',
+        errMsg: 'Select most appropriate industry',
+      });
+    }
 
-      if (!body.smeType) {
-        aboutExporterErrors.push({
-          errRef: 'smeType',
-          errMsg: 'Select type of small to medium enterprise (SME)',
-        });
-      }
-
-      if (!body.probabilityOfDefault) {
-        aboutExporterErrors.push({
-          errRef: 'probabilityOfDefault',
-          errMsg: 'Enter the probability of default',
-        });
-      }
-
-      if (!body.isFinanceIncreasing) {
-        aboutExporterErrors.push({
-          errRef: 'isFinanceIncreasing',
-          errMsg: 'Select whether your financing to the exporter is increasing as a result of this new GEF facility ',
-        });
-      }
+    if (!saveAndReturn && !body.smeType) {
+      aboutExporterErrors.push({
+        errRef: 'smeType',
+        errMsg: 'Select type of small to medium enterprise (SME)',
+      });
     }
 
     const minPercentage = 0;
     const maxPercentage = 14.1;
     const probabilityOfDefault = Number(body.probabilityOfDefault);
-    if (
-      Number.isNaN(probabilityOfDefault)
-      || probabilityOfDefault >= maxPercentage
-      || probabilityOfDefault <= minPercentage
+
+    if (!saveAndReturn && !body.probabilityOfDefault) {
+      aboutExporterErrors.push({
+        errRef: 'probabilityOfDefault',
+        errMsg: 'Enter the probability of default',
+      });
+    } else if (body.probabilityOfDefault
+    && (Number.isNaN(probabilityOfDefault)
+    || probabilityOfDefault >= maxPercentage
+    || probabilityOfDefault <= minPercentage)
     ) {
       aboutExporterErrors.push({
         errRef: 'probabilityOfDefault',
         errMsg: `You must enter a percentage between ${minPercentage + 0.01}% to ${maxPercentage - 0.01}%`,
+      });
+    }
+
+    if (!saveAndReturn && !body.isFinanceIncreasing) {
+      aboutExporterErrors.push({
+        errRef: 'isFinanceIncreasing',
+        errMsg: 'Select whether your financing to the exporter is increasing as a result of this new GEF facility ',
       });
     }
 
