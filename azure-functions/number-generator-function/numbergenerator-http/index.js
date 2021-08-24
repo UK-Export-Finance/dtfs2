45f5601ddd2372b85a2e361ddb3f86cd5f08119a
Andrew Price <andrew@madebymoon.com>
2021-08-24 16:05:19 +0100

    dtfs2-3793 Number Generator function - debugs

--- azure-functions/number-generator-function/numbergenerator-http/index.js
+++ azure-functions/number-generator-function/numbergenerator-http/index.js
@@ -2,10 +2,17 @@
 
 const numberGeneratorHttp = async (context, req) => {
   try {
+    return {
+      status: 200,
+      body: {
+        a: 1,
+      },
+    };
     const client = df.getClient(context);
     const instanceId = await client.startNew(req.params.functionName, undefined, req.body);
     return client.createCheckStatusResponse(context.bindingData.req, instanceId);
   } catch (err) {
+    console.log('GENERATOR ERROR', JSON.stringify(err));
     return { err };
   }
 };
