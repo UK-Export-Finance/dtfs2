5d867b3a0bc357092d8e16e9839d219cca9d9fc9
ttbarnes <tony@tonybarnes.me>
2021-08-25 13:19:59 +0100

    add log to TFM graphql authentication if env var is missing

--- trade-finance-manager-api/src/graphql/key-authentication/index.js
+++ trade-finance-manager-api/src/graphql/key-authentication/index.js
@@ -1,6 +1,10 @@
 const graphqlKeyPermissions = (req) => {
   const token = req.headers.authorization;
 
+  if (!process.env.UKEF_TFM_API_SYSTEM_KEY) {
+    console.error('TFM API - UKEF_TFM_API_SYSTEM_KEY missing');
+  }
+
   return {
     read: token === process.env.UKEF_TFM_API_SYSTEM_KEY || token === process.env.UKEF_TFM_API_REPORTS_KEY,
     write: token === process.env.UKEF_TFM_API_SYSTEM_KEY,
