73f174104258a6a9eb0d9384776fa99d76a1abd4
ttbarnes <tony@tonybarnes.me>
2021-08-26 10:14:16 +0100

    DTFS2-4677 maBBankFacilityReference using facilityType

--- trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapBankFacilityReference.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapBankFacilityReference.js
@@ -1,26 +1,17 @@
+const CONSTANTS = require('../../../../constants');
+
 const mapBankFacilityReference = (facility) => {
-  let mapped;
+  const { ukefFacilityType } = facility;
 
-  // currently, we don't always have facilityType.
-  // this is a hacky fallback/workaround for initial TFM development.
-  // TODO: remove this once DTFS2-3054 is completed.
-
-  // only loans have interestMarginFee
-  const isLoan = facility.interestMarginFee;
-
-  // only bonds have bondType
-  const isBond = facility.bondType;
-
-
-  if (isLoan && facility.bankReferenceNumber) {
-    mapped = facility.bankReferenceNumber;
+  if (ukefFacilityType === CONSTANTS.FACILITIES.FACILITY_TYPE.LOAN) {
+    return facility.bankReferenceNumber;
   }
 
-  if (isBond && facility.uniqueIdentificationNumber) {
-    mapped = facility.uniqueIdentificationNumber;
+  if (ukefFacilityType === CONSTANTS.FACILITIES.FACILITY_TYPE.BOND) {
+    return facility.uniqueIdentificationNumber;
   }
 
-  return mapped;
+  return null;
 };
 
 module.exports = mapBankFacilityReference;
