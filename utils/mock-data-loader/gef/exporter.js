e57d1b499b4aeeaa03d7c7cbda95beee238e7149
ttbarnes <tony@tonybarnes.me>
2021-09-06 08:59:39 +0100

    simplify mock GEF exporters

--- utils/mock-data-loader/gef/exporter.js
+++ utils/mock-data-loader/gef/exporter.js
@@ -1,64 +1,18 @@
 const faker = require('faker');
 
-const EXPORTER = [{
-// Not started - the mock inserter will ignore this line as the updatedTime is bumped
-// but leave it in as its index based
-},
-{ // Half Completed
-  companiesHouseRegistrationNumber: null,
-  companyName: faker.company.companyName(),
-  registeredAddress: null,
-  correspondenceAddress: null,
-  selectedIndustry: {
-    code: '1003',
-    name: 'Manufacturing',
-    class: {
-      code: '25300',
-      name: 'Manufacture of steam generators, except central heating hot water boilers',
+const EXPORTER = [
+  {
+    companiesHouseRegistrationNumber: 123456789,
+    companyName: faker.company.companyName(),
+    registeredAddress: {
+      line1: faker.address.streetName(),
+      line2: faker.address.streetAddress(),
+      county: faker.address.county(),
+      country: 'GB',
+      postcode: 'AB1 1AB',
     },
-  },
-  industries: [
-    {
-      code: '1003',
-      name: 'Manufacturing',
-      class: {
-        code: '25300',
-        name: 'Manufacture of steam generators, except central heating hot water boilers',
-      },
-    },
-  ],
-  smeType: 'MEDIUM',
-  probabilityOfDefault: 67,
-  isFinanceIncreasing: false,
-}, { // Completed
-  companiesHouseRegistrationNumber: 123456789,
-  companyName: faker.company.companyName(),
-  registeredAddress: {
-    line1: faker.address.streetName(),
-    line2: faker.address.streetAddress(),
-    county: faker.address.county(),
-    country: 'GB',
-    postcode: 'AB1 1AB',
-  },
-  correspondenceAddress: null,
-  selectedIndustry: {
-    code: '1003',
-    name: 'Manufacturing',
-    class: {
-      code: '25620',
-      name: 'Machining',
-    },
-  },
-  industries: [
-    {
-      code: '1003',
-      name: 'Manufacturing',
-      class: {
-        code: '25300',
-        name: 'Manufacture of steam generators, except central heating hot water boilers',
-      },
-    },
-    {
+    correspondenceAddress: null,
+    selectedIndustry: {
       code: '1003',
       name: 'Manufacturing',
       class: {
@@ -66,18 +20,36 @@ const EXPORTER = [{
         name: 'Machining',
       },
     },
-    {
-      code: '1003',
-      name: 'Manufacturing',
-      class: {
-        code: '28110',
-        name: 'Manufacture of engines and turbines, except aircraft, vehicle and cycle engines',
+    industries: [
+      {
+        code: '1003',
+        name: 'Manufacturing',
+        class: {
+          code: '25300',
+          name: 'Manufacture of steam generators, except central heating hot water boilers',
+        },
       },
-    },
-  ],
-  smeType: 'MICRO',
-  probabilityOfDefault: 45.5,
-  isFinanceIncreasing: true,
-}];
+      {
+        code: '1003',
+        name: 'Manufacturing',
+        class: {
+          code: '25620',
+          name: 'Machining',
+        },
+      },
+      {
+        code: '1003',
+        name: 'Manufacturing',
+        class: {
+          code: '28110',
+          name: 'Manufacture of engines and turbines, except aircraft, vehicle and cycle engines',
+        },
+      },
+    ],
+    smeType: 'MICRO',
+    probabilityOfDefault: 45.5,
+    isFinanceIncreasing: true,
+  },
+];
 
 module.exports = EXPORTER;
