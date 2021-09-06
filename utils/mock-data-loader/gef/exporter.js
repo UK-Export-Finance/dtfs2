a0764ea3e6cc8ba04b9ac860adf4ae8a70501b9b
ttbarnes <tony@tonybarnes.me>
2021-09-06 13:11:03 +0100

    revert gef mock data loader changes

--- utils/mock-data-loader/gef/exporter.js
+++ utils/mock-data-loader/gef/exporter.js
@@ -1,18 +1,64 @@
 const faker = require('faker');
 
-const EXPORTER = [
-  {
-    companiesHouseRegistrationNumber: 123456789,
-    companyName: faker.company.companyName(),
-    registeredAddress: {
-      line1: faker.address.streetName(),
-      line2: faker.address.streetAddress(),
-      county: faker.address.county(),
-      country: 'GB',
-      postcode: 'AB1 1AB',
+const EXPORTER = [{
+  // Not started - the mock inserter will ignore this line as the updatedTime is bumped
+  // but leave it in as its index based
+},
+{ // Half Completed
+  companiesHouseRegistrationNumber: null,
+  companyName: faker.company.companyName(),
+  registeredAddress: null,
+  correspondenceAddress: null,
+  selectedIndustry: {
+    code: '1003',
+    name: 'Manufacturing',
+    class: {
+      code: '25300',
+      name: 'Manufacture of steam generators, except central heating hot water boilers',
     },
-    correspondenceAddress: null,
-    selectedIndustry: {
+  },
+  industries: [
+    {
+      code: '1003',
+      name: 'Manufacturing',
+      class: {
+        code: '25300',
+        name: 'Manufacture of steam generators, except central heating hot water boilers',
+      },
+    },
+  ],
+  smeType: 'MEDIUM',
+  probabilityOfDefault: 67,
+  isFinanceIncreasing: false,
+}, { // Completed
+  companiesHouseRegistrationNumber: 123456789,
+  companyName: faker.company.companyName(),
+  registeredAddress: {
+    line1: faker.address.streetName(),
+    line2: faker.address.streetAddress(),
+    county: faker.address.county(),
+    country: 'GB',
+    postcode: 'AB1 1AB',
+  },
+  correspondenceAddress: null,
+  selectedIndustry: {
+    code: '1003',
+    name: 'Manufacturing',
+    class: {
+      code: '25620',
+      name: 'Machining',
+    },
+  },
+  industries: [
+    {
+      code: '1003',
+      name: 'Manufacturing',
+      class: {
+        code: '25300',
+        name: 'Manufacture of steam generators, except central heating hot water boilers',
+      },
+    },
+    {
       code: '1003',
       name: 'Manufacturing',
       class: {
@@ -20,36 +66,18 @@ const EXPORTER = [
         name: 'Machining',
       },
     },
-    industries: [
-      {
-        code: '1003',
-        name: 'Manufacturing',
-        class: {
-          code: '25300',
-          name: 'Manufacture of steam generators, except central heating hot water boilers',
-        },
+    {
+      code: '1003',
+      name: 'Manufacturing',
+      class: {
+        code: '28110',
+        name: 'Manufacture of engines and turbines, except aircraft, vehicle and cycle engines',
       },
-      {
-        code: '1003',
-        name: 'Manufacturing',
-        class: {
-          code: '25620',
-          name: 'Machining',
-        },
-      },
-      {
-        code: '1003',
-        name: 'Manufacturing',
-        class: {
-          code: '28110',
-          name: 'Manufacture of engines and turbines, except aircraft, vehicle and cycle engines',
-        },
-      },
-    ],
-    smeType: 'MICRO',
-    probabilityOfDefault: 45.5,
-    isFinanceIncreasing: true,
-  },
-];
+    },
+  ],
+  smeType: 'MICRO',
+  probabilityOfDefault: 45.5,
+  isFinanceIncreasing: true,
+}];
 
 module.exports = EXPORTER;
