60d2b7b25410130f4211cab0ed1afcc0cae41b59
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2021-09-20 17:33:05 +0100

    DTFS-4812 : 0.1 ACBS Test

--- azure-functions/acbs-function/mappings/party/exporter.js
+++ azure-functions/acbs-function/mappings/party/exporter.js
@@ -1,5 +1,6 @@
 const { now } = require('../../helpers/date');
 const { getSmeType, getPartyNames } = require('./helpers');
+const CONSTANTS = require('../../constants');
 
 /*
 Field mapping based on email from Gareth Ashby 15/03/2021
@@ -11,25 +12,31 @@ Field mapping based on email from Gareth Ashby 15/03/2021
   sme                       string  Workflow sme type code  4 digit code - default if unknown 70
   citizenshipClass          string  If Customer domicile country is UK set to '1' otherwise '2'
   officerRiskDate           date    yyyy-MM-dd i.e. 2019-10-21, Date of creation (we use current date)
-*/
+  */
 
-const exporter = ({ deal, acbsReference }) => {
-  const { submissionDetails } = deal.dealSnapshot;
+  const exporter = ({ deal, acbsReference }) => {
+  //Get Deal's Snapshot
+  const { submissionDetails } = deal.hasOwnProperty("dealSnapshot") ? deal.dealSnapshot : '';
 
-  const countryCode = submissionDetails['supplier-address-country'] && submissionDetails['supplier-address-country'].code;
-  const citizenshipClass = countryCode === 'GBR' ? '1' : '2';
+  //Get Product Type i.e. GEF
+  const product = deal.hasOwnProperty("dealSnapshot") ? deal.dealSnapshot.dealType : '';
 
-  const partyNames = getPartyNames(submissionDetails['supplier-name']);
+  if(submissionDetails !== "")
+  {
+    const countryCode = if(product === CONSTANTS.PRODUCT.TYPE.GEF) ? submissionDetails.exporter.registeredAddress.country : submissionDetails['supplier-address-country'] && submissionDetails['supplier-address-country'].code;
+    const citizenshipClass = countryCode === (product === CONSTANTS.PRODUCT.TYPE.GEF ? 'GB' : 'GBR') ? '1' : '2';
+    const partyNames = getPartyNames(product === CONSTANTS.PRODUCT.TYPE.GEF ?  submissionDetails.exporter.companyName : submissionDetails['supplier-name']);
 
-  return {
-    alternateIdentifier: deal.tfm.parties.exporter.partyUrn,
-    industryClassification: acbsReference.supplierAcbsIndustryCode,
-    smeType: getSmeType(submissionDetails['sme-type']),
-    citizenshipClass,
-    officerRiskDate: now(),
-    countryCode,
-    ...partyNames,
-  };
+    return {
+      alternateIdentifier: deal.tfm.parties.exporter.partyUrn,
+      industryClassification: acbsReference.supplierAcbsIndustryCode,
+      smeType: getSmeType(product === CONSTANTS.PRODUCT.TYPE.GEF ? submissionDetails.exporter.smeType : submissionDetails['sme-type']),
+      citizenshipClass,
+      officerRiskDate: now(),
+      countryCode,
+      ...partyNames,
+    };
+  }
 };
 
 module.exports = exporter;
