17c069d48384b8ed465576fc68256520246b1f7b
ttbarnes <tony@tonybarnes.me>
2021-09-23 16:46:56 +0100

    DTFS2-4751 - split out central API portal definitions

--- dtfs-central-api/src/v1/swagger-definitions/portal/deal-bss.js
+++ dtfs-central-api/src/v1/swagger-definitions/portal/deal-bss.js
@@ -338,129 +338,15 @@
  *               loanInGbp:
  *                 type: string
  *                 example: '8,000'
- *   FacilityBSS:
+ *   DealsBSS:
  *     type: object
  *     properties:
- *       _id:
- *         type: string
- *         example: 123abc
- *       associatedDealId:
- *         type: string
- *         example: 456abc
- *       facilityType:
- *         type: string
- *         example: bond
- *       createdDate:
- *         type: string
- *         example: '1632392646415'
- *       lastEdited:
- *         type: string
- *         example: '1632392778918'
- *       status:
- *         type: string
- *         example: Acknowledged by UKEF
- *       previousStatus:
- *         type: string
- *         example: Completed
- *       ukefFacilityID:
- *         type: string
- *         example: '0030034644'
- *       facilityStage:
- *         type: string
- *         example: Issued
- *       requestedCoverStartDate:
- *         type: string
- *         example: 1632392644463.0
- *       requestedCoverStartDate-day:
- *         type: string
- *         example: 10
- *       requestedCoverStartDate-month:
- *         type: string
- *         example: 03
- *       requestedCoverStartDate-year:
- *         type: string
- *         example: 2021
- *       coverEndDate-day:
- *         type: string
- *         example: 23
- *       coverEndDate-month:
- *         type: string
- *         example: 10
- *       coverEndDate-year:
- *         type: string
- *         example: 2021
- *       issueFacilityDetailsSubmitted:
- *         type: boolean
- *         example: true
- *       issuedFacilitySubmittedToUkefTimestamp:
- *         type: string
- *         example: '1632392674598'
- *       issuedFacilitySubmittedToUkefBy:
- *         $ref: '#/definitions/User'
- *       hasBeenAcknowledged:
- *         type: boolean
- *         example: true
- *       conversionRate:
- *         type: string
- *         example: '100'
- *       coveredPercentage:
- *         type: string
- *         example: '24'
- *       currencySameAsSupplyContractCurrency:
- *         type: string
- *         example: true
- *       facilityValue:
- *         type: string
- *         eaxmple: '2400.00'
- *       currency:
- *         type: object
- *         properties:
- *           id:
- *             type: string
- *             example: USD
- *           text:
- *             type: string
- *             example: USD - US Dollars
- *       ukefGuaranteeInMonths:
- *         type: string
- *         example: '12'
- *       uniqueIdentificationNumber:
- *         type: string
- *         example: '1234'
- *       guaranteeFeePayableByBank:
- *         type: string
- *         example: '10.8000'
- *       minimumRiskMarginFee:
- *         type: string
- *         example: '10'
- *       riskMarginFee:
- *         type: string
- *         example: '12'
- *       ukefExposure:
- *         type: string
- *         example: '567.00'
- *       dayCountBasis:
- *         type: string
- *         example: '365'
- *       feeType:
- *         type: string
- *         example: 'In arrear'
- *       feeFrequency:
- *         type: string
- *         example: 'Monthly'
- *       bondType:
- *         type: string
- *         example: Advance payment guarantee
- *       bondBeneficiary:
- *         type: string
- *         example: The beneficiary
- *       bondIssuer:
- *         type: string
- *         example: An issuer
- *
- *   FacilitiesBSS:
- *     type: array
- *     items:
- *       type: object
- *       $ref: '#/definitions/FacilityBSS'
+ *       deals:
+ *         type: array
+ *         items:
+ *           type: object
+ *           $ref: '#/definitions/DealBSS'
+ *       count:
+ *         type: integer
+ *         example: 1
  */
