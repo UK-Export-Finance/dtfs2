ddf94859c25f8f9f57fdd4c2fe4081e4133222c0
ttbarnes <tony@tonybarnes.me>
2021-09-28 10:23:30 +0100

    combine TFM init data functions into one

---
+++ trade-finance-manager-api/src/v1/controllers/deal-add-tfm-data/index.js
@@ -0,0 +1,42 @@
+const api = require('../../api');
+const { dateReceived: addDateReceived } = require('./dateReceived');
+const addDealProduct = require('./dealProduct');
+const addDealPricingAndRisk = require('./dealPricingAndRisk');
+const addDealStage = require('./dealStage');
+const DEFAULTS = require('../../defaults');
+
+const addTfmDealData = async (deal) => {
+  if (!deal) {
+    return false;
+  }
+
+  const {
+    _id: dealId,
+    submissionDate,
+    submissionType,
+    status,
+    tfm,
+  } = deal;
+
+
+  const dealUpdate = {
+    tfm: {
+      ...tfm,
+      dateReceived: addDateReceived(submissionDate),
+      history: DEFAULTS.HISTORY,
+      parties: {},
+      product: addDealProduct(deal),
+      stage: addDealStage(status, submissionType),
+      ...addDealPricingAndRisk(deal),
+    },
+  };
+
+  const updatedDeal = await api.updateDeal(dealId, dealUpdate);
+
+  return {
+    ...deal,
+    tfm: updatedDeal.tfm,
+  };
+};
+
+module.exports = addTfmDealData;
