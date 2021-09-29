fa03005d98a4f64c9c07251a2b746db6bfe98e16
ttbarnes <tony@tonybarnes.me>
2021-09-29 18:46:41 +0100

    DTFS2-4945 - reference data proxy api docs

---
+++ reference-data-proxy/src/v1/swagger-definitions/ordnance-survey.js
@@ -0,0 +1,24 @@
+/**
+ * @openapi
+ * definitions:
+ *   OrdnanceSurveyResponseBody:
+ *     type: array
+ *     items:
+ *       type: object
+ *       properties:
+ *         DPA:
+ *           type: object
+ *           properties:
+ *             ADDRESS:
+ *               type: string
+ *               example: TREATS, 6, BRIDGE STREET, LONDON, SW1A 2JR
+ *             POSTCODE:
+ *               type: string
+ *               examxple: SW1A 2JR
+ *             X_COORDINATE:
+ *               type: integer
+ *               example: 530272.43
+ *             Y_COORDINATE:
+ *               type: integer
+ *               example: 179690.67
+ */
