dece2c987ddf86d01d1b5bfc99f2e43663ffd1a0
ttbarnes <tony@tonybarnes.me>
2021-09-30 09:48:19 +0100

    DTFS2-4945 - update ACBS issue facility swagger definition

--- reference-data-proxy/src/v1/swagger-definitions/acbs.js
+++ reference-data-proxy/src/v1/swagger-definitions/acbs.js
@@ -39,7 +39,7 @@
  *       supplierName:
  *         type: string
  *         example: 'The Supplier'
-  *   ACBSIssueFacilityResponseBody:
+ *   ACBSIssueFacilityResponseBody:
  *     type: object
  *     properties:
  *       facilityId:
@@ -47,4 +47,107 @@
  *         example: abc1234
  *       issuedFacilityMaster:
  *         type: object
+ *         properties:
+ *           submittedToACBS:
+ *             type: string
+ *             example: '2021-09-30T09:42:31+01:00'
+ *           receivedFromACBS:
+ *             type: string
+ *             example: '2021-20-02T08:32:31+01:00'
+ *           dealIdentifier:
+ *             type: string
+ *             example: '0020900040'
+ *           facilityIdentifier:
+ *             type: string
+ *             example: '0020900041'
+ *           portfolioIdentifier:
+ *             type: string
+ *             example: 'E1'
+ *           dealBorrowerIdentifier:
+ *             type: string
+ *             example: '00000000'
+ *           maximumLiability:
+ *             type: integer
+ *             example: 501927.75
+ *           productTypeId:
+ *             type: string
+ *             example: '250'
+ *           capitalConversionFactorCode:
+ *             type: string
+ *             example: '8'
+ *           productTypeName:
+ *             type: string
+ *             example: 'BOND'
+ *           currency:
+ *             type: string
+ *             example: 'CAD'
+ *           guaranteeCommencementDate:
+ *             type: string
+ *             example: '2018-10-11'
+ *           guaranteeExpiryDate:
+ *             type: string
+ *             example: '2019-01-23'
+ *           nextQuarterEndDate:
+ *             type: string
+ *             example: '2018-12-31'
+ *           facilityInitialStatus:
+ *             type: string
+ *             example: 'P'
+ *           facilityOverallStatus:
+ *             type: string
+ *             example: 'D'
+ *           delegationType:
+ *             type: string
+ *             example: 'A'
+ *           intrestOrFeeRate:
+ *             type: integer
+ *             example: 2.35
+ *           facilityStageCode:
+ *             type: string
+ *             example: '06'
+ *           exposurePeriod:
+ *             type: string
+ *             example: '18'
+ *           creditRatingCode:
+ *             type: string
+ *             example: '14'
+ *           guaranteePercentage:
+ *             type: integer
+ *             example: 80
+ *           premiumFrequencyCode:
+ *             type: string
+ *             example: '2'
+ *           riskCountryCode:
+ *             type: string
+ *             example: 'GBR'
+ *           riskStatusCode:
+ *             type: string
+ *             example: '03'
+ *           effectiveDate:
+ *             type: string
+ *             example: '2018-10-11'
+ *           foreCastPercentage:
+ *             type: integer
+ *             example: 75
+ *           issueDate:
+ *             type: string
+ *             example: '2018-10-11'
+ *           description:
+ *             type: string
+ *             example: 'D:TRUEFORM-WAY-FINDER POSTS'
+ *           agentBankIdentifier:
+ *             type: string
+ *             example: '00000000'
+ *           obligorPartyIdentifier:
+ *             type: string
+ *             example: '00510701'
+ *           obligorName:
+ *             type: string
+ *             example: 'Automation KE 97'
+ *           obligorIndustryClassification:
+ *             type: string
+ *             example: '0116'
+ *           probabilityOfDefault:
+ *             type: integer
+ *             example: 14
  */
