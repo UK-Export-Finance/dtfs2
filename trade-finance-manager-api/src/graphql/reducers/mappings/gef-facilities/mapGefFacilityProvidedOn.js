17ab717c52eafbc95bfdaae95d7c803d7881a147
ttbarnes <tony@tonybarnes.me>
2021-10-04 11:11:07 +0100

    DTFS2-4383 - TFM mapping GEF facility `provided on basis` ids and values

--- trade-finance-manager-api/src/graphql/reducers/mappings/gef-facilities/mapGefFacilityProvidedOn.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/gef-facilities/mapGefFacilityProvidedOn.js
@@ -1,7 +1,14 @@
-const { titleCaseString } = require('../../../../utils/string');
+const CONSTANTS = require('../../../../constants');
 
 const mapGefFacilityProvidedOn = (details) =>
-  details.map((d) => titleCaseString(d));
+  details.map((basis) => {
+    // 'Other basis' should not display in TFM UI.
+    if (basis !== CONSTANTS.FACILITIES.FACILITY_PROVIDED_ON_GEF.OTHER.ID) {
+      return CONSTANTS.FACILITIES.FACILITY_PROVIDED_ON_GEF[basis].TEXT;
+    }
+
+    return null;
+  });
 
 
 module.exports = mapGefFacilityProvidedOn;
