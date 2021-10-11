a182db1e9526cf49ccf956ee4711f4e772ed4ca4
David Lewis <dave@davehlewis.com>
2021-10-11 12:16:05 +0100

    fix component tests for contract nav

--- portal/component-tests/contract/components/contract-tabs.component-test.js
+++ portal/component-tests/contract/components/contract-tabs.component-test.js
@@ -1,70 +1,56 @@
 const componentRenderer = require('../../componentRenderer');
+
 const component = 'contract/components/contract-tabs.njk';
 const render = componentRenderer(component);
 
 describe(component, () => {
-  const deal = {_id:123};
+  const deal = { _id: 123 };
 
   describe("when called with selected = 'view'", () => {
     let wrapper;
 
-    beforeAll( () => {
-      wrapper = render({deal, selected: 'view'});
-    })
-
-    it("disables the link to the view tab", () =>{
-      wrapper.expectLink('[data-cy="view-tab"]').toLinkTo(`#`,'View');
+    beforeAll(() => {
+      wrapper = render({ deal, selected: 'view' });
     });
 
-    it("provides a link to the comments tab", () =>{
-      wrapper.expectLink('[data-cy="comments-tab"]').toLinkTo(`/contract/${deal._id}/comments`,'Comments');
+    it('provides a link to the comments tab', () => {
+      wrapper.expectLink('[data-cy="comments-tab"]').toLinkTo(`/contract/${deal._id}/comments`, 'Comments');
     });
 
-    it("provides a link to the preview tab", () =>{
-      wrapper.expectLink('[data-cy="check-deal-details-tab"]').toLinkTo(`/contract/${deal._id}/submission-details`,'Check deal details');
+    it('provides a link to the preview tab', () => {
+      wrapper.expectLink('[data-cy="check-deal-details-tab"]').toLinkTo(`/contract/${deal._id}/submission-details`, 'Check deal details');
     });
-
   });
 
   describe("when called with selected = 'comments'", () => {
     let wrapper;
 
-    beforeAll( () => {
-      wrapper = render({deal, selected: 'comments'});
-    })
-
-    it("provides a link to the view tab", () =>{
-      wrapper.expectLink('[data-cy="view-tab"]').toLinkTo(`/contract/${deal._id}`,'View');
+    beforeAll(() => {
+      wrapper = render({ deal, selected: 'comments' });
     });
 
-    it("disables the link to the comments tab", () =>{
-      wrapper.expectLink('[data-cy="comments-tab"]').toLinkTo(`#`,'Comments');
+    it('provides a link to the view tab', () => {
+      wrapper.expectLink('[data-cy="view-tab"]').toLinkTo(`/contract/${deal._id}`, 'View');
     });
 
-    it("provides a link to the preview tab", () =>{
-      wrapper.expectLink('[data-cy="check-deal-details-tab"]').toLinkTo(`/contract/${deal._id}/submission-details`,'Check deal details');
+    it('provides a link to the preview tab', () => {
+      wrapper.expectLink('[data-cy="check-deal-details-tab"]').toLinkTo(`/contract/${deal._id}/submission-details`, 'Check deal details');
     });
-
   });
 
   describe("when called with selected = 'submission-details'", () => {
     let wrapper;
 
-    beforeAll( () => {
-      wrapper = render({deal, selected: 'submission-details'});
-    })
-
-    it("provides a link to the view tab", () =>{
-      wrapper.expectLink('[data-cy="view-tab"]').toLinkTo(`/contract/${deal._id}`,'View');
+    beforeAll(() => {
+      wrapper = render({ deal, selected: 'submission-details' });
     });
 
-    it("provides a link to the comments tab", () =>{
-      wrapper.expectLink('[data-cy="comments-tab"]').toLinkTo(`/contract/${deal._id}/comments`,'Comments');
+    it('provides a link to the view tab', () => {
+      wrapper.expectLink('[data-cy="view-tab"]').toLinkTo(`/contract/${deal._id}`, 'View');
     });
 
-    it("disables the link to the preview tab", () =>{
-      wrapper.expectLink('[data-cy="check-deal-details-tab"]').toLinkTo(`#`,'Check deal details');
+    it('provides a link to the comments tab', () => {
+      wrapper.expectLink('[data-cy="comments-tab"]').toLinkTo(`/contract/${deal._id}/comments`, 'Comments');
     });
-
   });
 });
