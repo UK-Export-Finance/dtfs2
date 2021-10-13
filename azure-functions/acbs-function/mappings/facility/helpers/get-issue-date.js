0748957ca2a9d84e0d8a1490776a8a1d9ae3ca2b
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2021-10-13 14:46:09 +0100

    Rectified property validation

--- azure-functions/acbs-function/mappings/facility/helpers/get-issue-date.js
+++ azure-functions/acbs-function/mappings/facility/helpers/get-issue-date.js
@@ -3,7 +3,7 @@ const { formatTimestamp } = require('../../../helpers/date');
 const isIssued = require('./is-issued');
 
 const getIssueDate = (facility, submissionDate) => {
-  if (facility.facilitySnapshot.facilityStage) {
+  if (facility.facilitySnapshot) {
     return isIssued(facility.facilitySnapshot.facilityStage)
       ? formatTimestamp(facility.facilitySnapshot.requestedCoverStartDate)
       : formatTimestamp(submissionDate);
