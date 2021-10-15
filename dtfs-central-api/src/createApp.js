e22be0ffa580207c086fd2d6f4cd901355b10df4
Gabriel Ignat <gabriel.ignat@hotmail.com>
2021-10-15 12:42:41 +0100

    feat: removed bodyParser and added express.compressions

--- dtfs-central-api/src/createApp.js
+++ dtfs-central-api/src/createApp.js
@@ -1,7 +1,6 @@
-const bodyParser = require('body-parser');
-// const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
+const compression = require('compression');
 
 const {
   BANK_ROUTE,
@@ -23,22 +22,17 @@ const {
   swaggerRoutes,
 } = require('./v1/routes');
 
-// const { CORS_ORIGIN } = process.env;
 
 const app = express();
-app.use(bodyParser.json({ type: 'application/json' }));
 app.use(healthcheck);
+app.use(express.json());
+app.use(compression());
 
 app.use(`/v1/${BANK_ROUTE}`, bankRoutes);
 app.use(`/v1/${PORTAL_ROUTE}`, portalRoutes);
 app.use(`/v1/${TFM_ROUTE}`, tfmRoutes);
 app.use(`/v1/${USER_ROUTE}`, userRoutes);
 app.use(`/v1/${SWAGGER_ROUTE}`, swaggerRoutes);
-
-// app.use(cors({
-//   origin: CORS_ORIGIN,
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// }));
 
 // Return 200 on get to / to confirm to Azure that
 // the container has started successfully:
