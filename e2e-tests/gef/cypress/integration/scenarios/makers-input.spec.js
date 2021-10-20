f33f0edfba8d62342f22676071604305d542a81e
ttbarnes <tony@tonybarnes.me>
2021-10-20 19:00:07 +0100

    update/fix GEF e2e tests to fill in eligibility form after removing from mock data. Clean up comments

--- e2e-tests/gef/cypress/integration/scenarios/makers-input.spec.js
+++ e2e-tests/gef/cypress/integration/scenarios/makers-input.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-undef */
 import relative from '../relativeURL';
 import applicationDetails from '../pages/application-details';
+import automaticCover from '../pages/automatic-cover';
 import applicationSubmission from '../pages/application-submission';
 import CREDENTIALS from '../../fixtures/credentials.json';
 import applicationPreview from '../pages/application-preview';
@@ -26,18 +27,28 @@ context('Review application when returned to maker', () => {
 
   beforeEach(() => {
     Cypress.Cookies.preserveOnce('connect.sid');
-    // login as the maker and submit
-    // then login as the checker and return to the maker with a comment.
+    // login as the maker
     cy.login(CREDENTIALS.MAKER);
     cy.visit(relative(`/gef/application-details/${applicationIds[2]}`));
+
+    // Make the deal an Automatic Inclusion Application
+    applicationDetails.automaticCoverDetailsLink().click();
+    automaticCover.automaticCoverTerm().each(($el, index) => {
+      $el.find('[data-cy="automatic-cover-true"]').trigger('click');
+    });
+    automaticCover.saveAndReturnButton().click();
+
+    // submit the deal with a comment
     applicationDetails.submitButton().click();
-    applicationSubmission.commentsField().type('DTFS2-4536 Comments from maker');
+    applicationSubmission.commentsField().type('Hello');
     applicationSubmission.submitButton().click();
     applicationSubmission.confirmation();
+
+    // then login as the checker and return to the maker with a comment
     cy.login(CREDENTIALS.CHECKER);
     cy.visit(relative(`/gef/application-details/${applicationIds[2]}`));
     applicationPreview.returnButton().click();
-    returnToMaker.comment().type('DTFS2-4536 return to maker comment by the checker');
+    returnToMaker.comment().type('Nope');
     returnToMaker.submitButton().click();
     cy.location('pathname').should('contain', 'dashboard');
     cy.login(CREDENTIALS.MAKER);
@@ -54,7 +65,7 @@ context('Review application when returned to maker', () => {
       // it displays the comments from the checker
       applicationPreview.task();
       applicationPreview.comments();
-      applicationPreview.comments().contains('DTFS2-4536 return to maker comment by the checker');
+      applicationPreview.comments().contains('Nope');
 
       // it allows the maker to submit to be checked at the bank
       applicationDetails.submitButton().click();
