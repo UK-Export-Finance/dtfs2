16bea54cdcab15bc47a13a63cf9c596b194ec197
ttbarnes <tony@tonybarnes.me>
2021-10-20 17:28:11 +0100

    remove submissionType from mock gef applications. This is calculated dynamically

--- utils/mock-data-loader/gef/application.js
+++ utils/mock-data-loader/gef/application.js
@@ -1,21 +1,21 @@
 const APPLICATION = [{
+  // not started
   bankId: '9',
   bankInternalRefName: 'Barclays 123',
   additionalRefName: null,
   mandatoryVersionId: '123', // further down the line you may want exact mongoIDs
-  submissionType: 'Automatic Inclusion Notice',
 }, {
+  // in progress
   bankId: '9',
   bankInternalRefName: 'UKEF Test 123',
   additionalRefName: '',
   mandatoryVersionId: '123',
-  submissionType: 'Automatic Inclusion Notice',
 }, {
+  // completed
   bankId: '9',
   bankInternalRefName: 'HSBC 123',
   additionalRefName: 'Some Additional Reference',
   mandatoryVersionId: '123',
-  submissionType: 'Automatic Inclusion Notice',
 }];
 
 module.exports = APPLICATION;
