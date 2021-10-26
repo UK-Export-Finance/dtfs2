96af773213f73f6e874576c74d491362cafb70fd
ttbarnes <tony@tonybarnes.me>
2021-10-26 14:41:22 +0100

    remove unused GEF eligibilityCriteria validation function

--- portal-api/src/v1/gef/controllers/validation/eligibilityCriteria.js
+++ portal-api/src/v1/gef/controllers/validation/eligibilityCriteria.js
@@ -30,14 +30,9 @@ const eligibilityCriteriaStatus = (answers) => {
   return STATUS.IN_PROGRESS;
 };
 
-const eligibilityCriteriaValidation = (answers) => ({
-  required: hasRequiredItems(answers),
-});
-
 module.exports = {
   getAnsweredItems,
   isAutomaticCover,
   eligibilityCriteriaStatus,
-  eligibilityCriteriaValidation,
 };
 
