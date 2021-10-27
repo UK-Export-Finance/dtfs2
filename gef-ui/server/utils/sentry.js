43cf9fbf2a3a31d7705c741e19856ddfd7a6c3b6
Gabriel Ignat <gabriel.ignat@hotmail.com>
2021-10-27 09:05:51 +0100

    added sentry to gef-ui

--- gef-ui/server/utils/sentry.js
+++ gef-ui/server/utils/sentry.js
@@ -1,5 +1,6 @@
 const { CaptureConsole } = require('@sentry/integrations');
 const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
 const express = require('express');
 
 const sentry = express.Router();
@@ -9,16 +10,28 @@ sentry.use(Sentry.Handlers.requestHandler());
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  integrations: [new CaptureConsole(
-    {
-      // array of methods that should be captured
-      // defaults to ['log', 'info', 'warn', 'error', 'debug', 'assert']
-      levels: ['error'],
-    },
-  )],
-  tracesSampleRate: 1.0,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ sentry }),
+    new CaptureConsole(
+      {
+        levels: ['error'], // defaults to ['log', 'info', 'warn', 'error', 'debug', 'assert']
+      },
+    )],
+  attachStacktrace: true, // attach stack traces to exceptions and messages
+  debug: true, // attempt to print out useful debugging information if something goes wrong
+  tracesSampleRate: 1.0, // defaults to 1.0
 });
 
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+sentry.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+sentry.use(Sentry.Handlers.tracingHandler());
+
+// The error handler must be before any other error middleware and after all controllers
 sentry.use(Sentry.Handlers.errorHandler());
 
 module.exports = sentry;
