b05f86408d3b13b752ed324e3efebe1ed5ba625e
ttbarnes <tony@tonybarnes.me>
2021-10-27 17:01:30 +0100

    DTFS2-4516 - TFM function to get facilities by type

---
+++ trade-finance-manager-api/src/v1/helpers/get-facilities-by-type.js
@@ -0,0 +1,17 @@
+const CONSTANTS = require('../../constants');
+
+const getFacilitiesByType = (facilities) => ({
+  bonds: facilities.filter(({ facilityType }) =>
+    facilityType === CONSTANTS.FACILITIES.FACILITY_TYPE.BOND),
+   
+  loans: facilities.filter(({ facilityType }) =>
+    facilityType === CONSTANTS.FACILITIES.FACILITY_TYPE.LOAN),
+
+  cashes: facilities.filter(({ facilityType }) =>
+    facilityType === CONSTANTS.FACILITIES.FACILITY_TYPE.CASH),
+
+  contingents: facilities.filter(({ facilityType }) =>
+    facilityType === CONSTANTS.FACILITIES.FACILITY_TYPE.CONTINGENT),
+});
+
+module.exports = getFacilitiesByType;
