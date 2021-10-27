b0c669ca38c9950efeaf6d923c62e09636978adb
ttbarnes <tony@tonybarnes.me>
2021-10-27 18:42:43 +0100

    DTFS2-4516 - TFM - GEF content strings for AIN-MIN confirmation email

---
+++ trade-finance-manager-api/src/v1/emails/AIN-MIN-confirmation/gef-facilities-content-strings.js
@@ -0,0 +1,29 @@
+const GEF_FACILITIES_CONTENT_STRINGS = {
+  HEADINGS: {
+    CASH: 'Here are your cash module details for your records',
+    CONTINGENT: 'Here are your contingent obligation details for your records',
+  },
+  LIST_ITEM_TITLES: {
+    // keep the same names as the actual field names, for easy reference/usage
+    CASH: {
+      ukefFacilityID: 'UKEF facility ID',
+      _id: 'Cash module facility ID',
+      bankReference: 'Cash module type',
+      facilityStage: 'Facility stage',
+      // TODO requestedCoverStartDate: 'Requested cover start date',
+      coverStartDate: 'Cover start date',
+      value: 'Cash module facility value',
+      currencyCode: 'Cash module currency',
+      interestPercentage: 'Risk interest margin %',
+      coverPercentage: 'Covered percentage',
+      // TODO need clarification minimumRiskMarginFee: 'Minimum risk margin fee',
+      guaranteeFee: 'Guarantee fee payable by bank',
+      ukefExposure: 'UKEF exposure',
+      feeType: 'Premium type',
+      feeFrequency: 'Premium frequency',
+      dayCountBasis: 'Day count basis',
+    }
+  },
+};
+
+module.exports = GEF_FACILITIES_CONTENT_STRINGS;
