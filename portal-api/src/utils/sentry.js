616a8b50d7eedf3970b8f6519c8476ee6e12a4f3
Gabriel Ignat <gabriel.ignat@hotmail.com>
2021-10-28 20:49:33 +0100

    updating the settings in sentry

--- portal-api/src/utils/sentry.js
+++ portal-api/src/utils/sentry.js
@@ -8,8 +8,6 @@ app.disable('x-powered-by');
 const sentry = express.Router();
 const { SENTRY_DSN } = process.env;
 
-sentry.use(Sentry.Handlers.requestHandler());
-
 Sentry.init({
   dsn: SENTRY_DSN,
   integrations: [
@@ -18,13 +16,12 @@ Sentry.init({
     // enable Express.js middleware tracing
     new Tracing.Integrations.Express({ app }),
     new ExtraErrorData({ depth: 10 }),
-    // new ExtraErrorData({ depth: 10 }),
     new CaptureConsole(
       {
         levels: ['error'], // defaults to ['log', 'info', 'warn', 'error', 'debug', 'assert']
       },
     )],
-  attachStacktrace: true, // attach stack traces to exceptions and messages
+  //   attachStacktrace: true, // attach stack traces to exceptions and messages
   //   debug: true, // attempt to print out useful debugging information if something goes wrong
   tracesSampleRate: 1.0, // defaults to 1.0
 });
