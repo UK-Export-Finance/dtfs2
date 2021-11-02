752b61aaf28b5760ed6ffce91db3e6b4071b7455
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2021-11-02 13:09:46 +0000

    GEF Deal value correction
    * GEF deal value should be values of all the facilities in GBP converted value

--- azure-functions/acbs-function/mappings/deal/helpers/get-deal-value.js
+++ azure-functions/acbs-function/mappings/deal/helpers/get-deal-value.js
@@ -4,7 +4,7 @@ const getDealValue = (deal) => {
   let total = 0;
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < deal.dealSnapshot.facilities.length; i++) {
-    total += getFacilityValue(deal.dealSnapshot.facilities[i].facilitySnapshot);
+    total += getFacilityValue(deal.dealSnapshot.facilities[i]);
   }
   return total;
 };
