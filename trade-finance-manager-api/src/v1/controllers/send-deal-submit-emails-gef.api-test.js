0d85aab4f76ec19b4ed373711bb13ead0430eb37
ttbarnes <tony@tonybarnes.me>
2021-11-03 16:02:16 +0000

    DTFS2-4516 - split out TFM deal submit emails api tests

---
+++ trade-finance-manager-api/src/v1/controllers/send-deal-submit-emails-gef.api-test.js
@@ -0,0 +1,100 @@
+const {
+  sendFirstTaskEmail,
+  sendDealSubmitEmails,
+  generateBssDealAinMinConfirmationEmailVariables,
+  sendMiaAcknowledgement,
+} = require('./send-deal-submit-emails');
+const { generateFacilityLists } = require('../helpers/notify-template-formatters');
+const mapSubmittedDeal = require('../mappings/map-submitted-deal');
+const { gefFacilitiesList } = require('../emails/AIN-MIN-confirmation/gef-facilities-list');
+const generateAinMinConfirmationEmailVars = require('../emails/AIN-MIN-confirmation/generate-email-variables');
+
+const CONSTANTS = require('../../constants');
+const api = require('../api');
+
+const MOCK_NOTIFY_EMAIL_RESPONSE = require('../__mocks__/mock-notify-email-response');
+
+const sendEmailApiSpy = jest.fn(() => Promise.resolve(
+  MOCK_NOTIFY_EMAIL_RESPONSE,
+));
+
+describe('send-deal-submit-emails - GEF', () => {
+  beforeEach(() => {
+    api.sendEmail.mockClear();
+    api.sendEmail = sendEmailApiSpy;
+  });
+
+  describe('GEF deal - AIN', () => {
+    let mockGefDealAin;
+
+    beforeEach(async () => {
+      mockGefDealAin = await api.findOneDeal('MOCK_GEF_DEAL');
+    });
+
+    it('should call sendEmail and return object of sent emails ', async () => {
+      const mappedDeal = mapSubmittedDeal(mockGefDealAin);
+
+      const result = await sendDealSubmitEmails(mappedDeal);
+
+      const facilityLists = gefFacilitiesList(mappedDeal.facilities);
+
+      const expectedEmailVariables = generateAinMinConfirmationEmailVars(
+        mappedDeal,
+        facilityLists,
+      );
+
+      expect(sendEmailApiSpy).toHaveBeenCalled();
+
+      const lastSendEmailCall = sendEmailApiSpy.mock.calls[1];
+
+      expect(lastSendEmailCall).toEqual([
+        CONSTANTS.EMAIL_TEMPLATE_IDS.GEF_DEAL_SUBMIT_CONFIRMATION,
+        mappedDeal.maker.email,
+        { ...expectedEmailVariables },
+      ]);
+
+      expect(result).toEqual({
+        firstTaskEmail: await sendFirstTaskEmail(mappedDeal),
+        emailAcknowledgementMIA: await sendMiaAcknowledgement(mappedDeal),
+        emailAcknowledgementAinMin: MOCK_NOTIFY_EMAIL_RESPONSE,
+      });
+    });
+  });
+
+  describe('GEF deal - MIN', () => {
+    let mockGefDealMin;
+
+    beforeEach(async () => {
+      mockGefDealMin = await api.findOneDeal('MOCK_GEF_DEAL_MIN');
+    });
+
+    it('should call sendEmail and return object of sent emails ', async () => {
+      const mappedDeal = mapSubmittedDeal(mockGefDealMin);
+
+      const result = await sendDealSubmitEmails(mappedDeal);
+
+      const facilityLists = gefFacilitiesList(mappedDeal.facilities);
+
+      const expectedEmailVariables = generateAinMinConfirmationEmailVars(
+        mappedDeal,
+        facilityLists,
+      );
+
+      expect(sendEmailApiSpy).toHaveBeenCalled();
+
+      const lastSendEmailCall = sendEmailApiSpy.mock.calls[1];
+
+      expect(lastSendEmailCall).toEqual([
+        CONSTANTS.EMAIL_TEMPLATE_IDS.GEF_DEAL_SUBMIT_CONFIRMATION,
+        mappedDeal.maker.email,
+        { ...expectedEmailVariables },
+      ]);
+
+      expect(result).toEqual({
+        firstTaskEmail: await sendFirstTaskEmail(mappedDeal),
+        emailAcknowledgementMIA: await sendMiaAcknowledgement(mappedDeal),
+        emailAcknowledgementAinMin: MOCK_NOTIFY_EMAIL_RESPONSE,
+      });
+    });
+  });
+});
