f82498ce993d4814bcc3e72ff2b58894e4df1e34
Gabriel Ignat <gabriel.ignat@hotmail.com>
2021-11-05 11:42:50 +0000

    re-enabled the e2e tests

--- e2e-tests/submit-to-trade-finance-manager/cypress/integration/journeys/portal/MIA-underwriter-managers-decision-updates-portal-deal/TFM-MIA-underwriter-managers-decision-approved-without-conditions-updates-portal-deal.spec.js
+++ e2e-tests/submit-to-trade-finance-manager/cypress/integration/journeys/portal/MIA-underwriter-managers-decision-updates-portal-deal/TFM-MIA-underwriter-managers-decision-approved-without-conditions-updates-portal-deal.spec.js
@@ -1,106 +1,105 @@
-// TODO: investigate failing tests DTFS2-5037
-// import relative from '../../../relativeURL';
-// import portalPages from '../../../../../../portal/cypress/integration/pages';
-// import tfmPages from '../../../../../../trade-finance-manager/cypress/integration/pages';
-// import tfmPartials from '../../../../../../trade-finance-manager/cypress/integration/partials';
+import relative from '../../../relativeURL';
+import portalPages from '../../../../../../portal/cypress/integration/pages';
+import tfmPages from '../../../../../../trade-finance-manager/cypress/integration/pages';
+import tfmPartials from '../../../../../../trade-finance-manager/cypress/integration/partials';
 
-// import MOCK_USERS from '../../../../../../portal/cypress/fixtures/mockUsers';
-// import MOCK_DEAL_READY_TO_SUBMIT from '../test-data/MIA-deal/dealReadyToSubmit';
+import MOCK_USERS from '../../../../../../portal/cypress/fixtures/mockUsers';
+import MOCK_DEAL_READY_TO_SUBMIT from '../test-data/MIA-deal/dealReadyToSubmit';
 
-// const MAKER_LOGIN = MOCK_USERS.find((user) => (user.roles.includes('maker') && user.username === 'BANK1_MAKER1'));
-// const CHECKER_LOGIN = MOCK_USERS.find((user) => (user.roles.includes('checker') && user.username === 'BANK1_CHECKER1'));
+const MAKER_LOGIN = MOCK_USERS.find((user) => (user.roles.includes('maker') && user.username === 'BANK1_MAKER1'));
+const CHECKER_LOGIN = MOCK_USERS.find((user) => (user.roles.includes('checker') && user.username === 'BANK1_CHECKER1'));
 
-// context('Portal to TFM deal submission', () => {
-//   let deal;
-//   let dealId;
-//   const dealFacilities = [];
+context('Portal to TFM deal submission', () => {
+  let deal;
+  let dealId;
+  const dealFacilities = [];
 
-//   before(() => {
-//     cy.deleteTfmDeals();
-//     cy.insertManyDeals([MOCK_DEAL_READY_TO_SUBMIT()], MAKER_LOGIN).then((insertedDeals) => {
-//       [deal] = insertedDeals;
-//       dealId = deal._id;
+  before(() => {
+    cy.deleteTfmDeals();
+    cy.insertManyDeals([MOCK_DEAL_READY_TO_SUBMIT()], MAKER_LOGIN).then((insertedDeals) => {
+      [deal] = insertedDeals;
+      dealId = deal._id;
 
-//       const { mockFacilities } = deal;
+      const { mockFacilities } = deal;
 
-//       cy.createFacilities(dealId, mockFacilities, MAKER_LOGIN).then((createdFacilities) => {
-//         dealFacilities.push(...createdFacilities);
-//       });
-//     });
-//   });
+      cy.createFacilities(dealId, mockFacilities, MAKER_LOGIN).then((createdFacilities) => {
+        dealFacilities.push(...createdFacilities);
+      });
+    });
+  });
 
-//   after(() => {
-//     dealFacilities.forEach(({ _id }) => {
-//       cy.deleteFacility(_id, MAKER_LOGIN);
-//     });
-//     cy.deleteTfmDeals();
-//   });
+  after(() => {
+    dealFacilities.forEach(({ _id }) => {
+      cy.deleteFacility(_id, MAKER_LOGIN);
+    });
+    cy.deleteTfmDeals();
+  });
 
-//   it('Portal MIA deal is submitted to UKEF. TFM Underwriter manager submits `Accepted with conditions` decision, Portal deal status is updated, comments/conditions display', () => {
-//     //---------------------------------------------------------------
-//     // portal maker submits deal for review
-//     //---------------------------------------------------------------
-//     cy.login(MAKER_LOGIN);
-//     portalPages.contract.visit(deal);
-//     portalPages.contract.proceedToReview().click();
-//     cy.url().should('eq', relative(`/contract/${dealId}/ready-for-review`));
+  it('Portal MIA deal is submitted to UKEF. TFM Underwriter manager submits `Accepted with conditions` decision, Portal deal status is updated, comments/conditions display', () => {
+    //---------------------------------------------------------------
+    // portal maker submits deal for review
+    //---------------------------------------------------------------
+    cy.login(MAKER_LOGIN);
+    portalPages.contract.visit(deal);
+    portalPages.contract.proceedToReview().click();
+    cy.url().should('eq', relative(`/contract/${dealId}/ready-for-review`));
 
-//     portalPages.contractReadyForReview.comments().type('go');
-//     portalPages.contractReadyForReview.readyForCheckersApproval().click();
+    portalPages.contractReadyForReview.comments().type('go');
+    portalPages.contractReadyForReview.readyForCheckersApproval().click();
 
-//     //---------------------------------------------------------------
-//     // portal checker submits deal to ukef
-//     //---------------------------------------------------------------
-//     cy.login(CHECKER_LOGIN);
-//     portalPages.contract.visit(deal);
-//     portalPages.contract.proceedToSubmit().click();
+    //---------------------------------------------------------------
+    // portal checker submits deal to ukef
+    //---------------------------------------------------------------
+    cy.login(CHECKER_LOGIN);
+    portalPages.contract.visit(deal);
+    portalPages.contract.proceedToSubmit().click();
 
-//     portalPages.contractConfirmSubmission.confirmSubmit().check();
-//     portalPages.contractConfirmSubmission.acceptAndSubmit().click(deal);
+    portalPages.contractConfirmSubmission.confirmSubmit().check();
+    portalPages.contractConfirmSubmission.acceptAndSubmit().click(deal);
 
-//     // expect to land on the /dashboard page with a success message
-//     cy.url().should('include', '/dashboard');
+    // expect to land on the /dashboard page with a success message
+    cy.url().should('include', '/dashboard');
 
-//     //---------------------------------------------------------------
-//     // Underwriter Manager logs in to TFM
-//     //---------------------------------------------------------------
-//     // Cypress.config('tfmUrl') returns incorrect url...
-//     const tfmRootUrl = 'http://localhost:5003';
+    //---------------------------------------------------------------
+    // Underwriter Manager logs in to TFM
+    //---------------------------------------------------------------
+    // Cypress.config('tfmUrl') returns incorrect url...
+    const tfmRootUrl = 'http://localhost:5003';
 
-//     cy.forceVisit(tfmRootUrl);
+    cy.forceVisit(tfmRootUrl);
 
-//     tfmPages.landingPage.email().type('UNDERWRITER_MANAGER_1');
-//     tfmPages.landingPage.submitButton().click();
+    tfmPages.landingPage.email().type('UNDERWRITER_MANAGER_1');
+    tfmPages.landingPage.submitButton().click();
 
-//     const row = tfmPages.dealsPage.dealsTable.row(dealId);
-//     row.dealLink().click();
-//     cy.url().should('eq', `${tfmRootUrl}/case/${dealId}/deal`);
+    const row = tfmPages.dealsPage.dealsTable.row(dealId);
+    row.dealLink().click();
+    cy.url().should('eq', `${tfmRootUrl}/case/${dealId}/deal`);
 
-//     //---------------------------------------------------------------
-//     // Underwriter Manager submits a decision
-//     //---------------------------------------------------------------
-//     tfmPartials.caseSubNavigation.underwritingLink().click();
-//     tfmPartials.underwritingSubNav.underwriterManagerDecisionLink().click();
-//     tfmPages.managersDecisionPage.addDecisionLink().click();
+    //---------------------------------------------------------------
+    // Underwriter Manager submits a decision
+    //---------------------------------------------------------------
+    tfmPartials.caseSubNavigation.underwritingLink().click();
+    tfmPartials.underwritingSubNav.underwriterManagerDecisionLink().click();
+    tfmPages.managersDecisionPage.addDecisionLink().click();
 
-//     tfmPages.managersDecisionPage.decisionRadioInputApproveWithoutConditions().click();
-//     tfmPages.managersDecisionPage.submitButton().click();
+    tfmPages.managersDecisionPage.decisionRadioInputApproveWithoutConditions().click();
+    tfmPages.managersDecisionPage.submitButton().click();
 
-//     //---------------------------------------------------------------
-//     // Go back to Portal
-//     //---------------------------------------------------------------
-//     cy.login(CHECKER_LOGIN);
-//     portalPages.contract.visit(deal);
+    //---------------------------------------------------------------
+    // Go back to Portal
+    //---------------------------------------------------------------
+    cy.login(CHECKER_LOGIN);
+    portalPages.contract.visit(deal);
 
-//     //---------------------------------------------------------------
-//     // Portal deal status should be updated
-//     //---------------------------------------------------------------
-//     portalPages.contract.previousStatus().invoke('text').then((text) => {
-//       expect(text.trim()).to.equal('In progress by UKEF');
-//     });
+    //---------------------------------------------------------------
+    // Portal deal status should be updated
+    //---------------------------------------------------------------
+    portalPages.contract.previousStatus().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('In progress by UKEF');
+    });
 
-//     portalPages.contract.status().invoke('text').then((text) => {
-//       expect(text.trim()).to.equal('Accepted by UKEF (without conditions)');
-//     });
-//   });
-// });
+    portalPages.contract.status().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Accepted by UKEF (without conditions)');
+    });
+  });
+});
