0e4ba9b0eac97c12312ad86430b1c3a4b965de39
Tony Barnes <tony@tonybarnes.me>
2021-11-10 12:31:48 +0000

    fix/update GEF statusLabel nunjuck filter used in Deals page (#473)

--- gef-ui/server/nunjucks-configuration/filter-getStatusLabel.js
+++ gef-ui/server/nunjucks-configuration/filter-getStatusLabel.js
@@ -6,9 +6,9 @@ const statusLabelMap = {
   SUBMITTED_TO_UKEF: 'Submitted',
   UKEF_ACKNOWLEDGED: 'Acknowledged by UKEF',
   UKEF_IN_PROGRESS: 'In progress by UKEF',
-  UKEF_APPROVED_WITH_CONDITIONS: 'Accepted (with conditions)',
-  UKEF_APPROVED_WITHOUT_CONDITIONS: 'Accepted (without conditions)',
-  UKEF_REFUSED: 'Rejected by UKEF',
+  UKEF_ACCEPTED_CONDITIONAL: 'Accepted by UKEF (with conditions)',
+  UKEF_ACCEPTED_UNCONDITIONAL: 'Accepted by UKEF (without conditions)',
+  UKEF_DECLINED: 'Rejected by UKEF',
   EXPIRED: 'Expired',
   WITHDRAWN: 'Withdrawn',
 };
