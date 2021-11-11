4e36153e15cc30cc0c36c521039a25b152e0d23e
ttbarnes <tony@tonybarnes.me>
2021-11-11 15:23:06 +0000

    remove console.log

--- trade-finance-manager-api/src/graphql/resolvers/query-deals-light.js
+++ trade-finance-manager-api/src/graphql/resolvers/query-deals-light.js
@@ -4,7 +4,6 @@ const { findTfmDealsLight } = require('../../v1/controllers/deal.controller');
 require('dotenv').config();
 
 const getDealsLight = async (queryParams) => {
-  console.log('getDealsLight..... queryParams \n', queryParams);
   const { deals } = await findTfmDealsLight(queryParams);
 
   const reducedDeals = dealsLightReducer(deals);
