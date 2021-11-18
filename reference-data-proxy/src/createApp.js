dd2dcba0ecfae71f581757298d92a4d0f2de22bf
Gabriel Ignat <gabriel.ignat@hotmail.com>
2021-11-18 09:50:41 +0000

    fix: Disable helmet (temporarily) (#489)

--- reference-data-proxy/src/createApp.js
+++ reference-data-proxy/src/createApp.js
@@ -3,7 +3,7 @@ const express = require('express');
 const compression = require('compression');
 const { CaptureConsole } = require('@sentry/integrations');
 const Sentry = require('@sentry/node');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 
 const healthcheck = require('./healthcheck');
 
@@ -13,11 +13,12 @@ const { openRouter } = require('./v1/routes');
 const swaggerRoutes = require('./v1/swagger-routes');
 
 const app = express();
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  }),
-);
+// TODO re-enable Helmet (Jira - 4998)
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: false,
+//   }),
+// );
 app.use(express.json());
 app.use(compression());
 
