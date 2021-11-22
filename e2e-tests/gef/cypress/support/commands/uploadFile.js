96771acdf79def41c5daf75721498ae0b76ff736
Gabriel Ignat <gabriel.ignat@hotmail.com>
2021-11-22 08:25:12 +0000

    updated file upload settings for e2e

--- e2e-tests/gef/cypress/support/commands/uploadFile.js
+++ e2e-tests/gef/cypress/support/commands/uploadFile.js
@@ -1,17 +1,17 @@
 const uploadFile = (fileName, uploadEndpoint, selector = '.moj-multi-file-upload__dropzone') => {
 // start watching the POST requests
-  cy.server({ method: 'POST' });
-  cy.route({
+//   cy.server({ method: 'POST' });
+  cy.intercept({
     method: 'POST',
-    url: uploadEndpoint,
+    path: uploadEndpoint,
   }).as('upload');
 
   cy.get(selector).attachFile(
     fileName, { subjectType: 'drag-n-drop' },
   );
 
-  cy.wait('@upload', { requestTimeout: 15000 });
-  cy.server({ enable: false });
+  cy.wait('@upload', { requestTimeout: 20000 });
+//   cy.server({ enable: false });
 };
 
 export default uploadFile;
