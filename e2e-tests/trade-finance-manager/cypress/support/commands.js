e9f65edc1e1fba4a45b72c5f6cce2a414daa2636
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2021-11-23 16:57:15 +0000

    fixed submit to ukef confirmation and added missing e2e file

--- e2e-tests/trade-finance-manager/cypress/support/commands.js
+++ e2e-tests/trade-finance-manager/cypress/support/commands.js
@@ -1,7 +1,6 @@
 // commands used to interact directly with TFM UI
 Cypress.Commands.add('login', require('./ui/logIn'));
 
-
 // commands used to interact directly with Deal API
 // NOTE: this will eventually become TFM API, that calls Deal API.
 // right now we only have TFM API that can find a deal, Portal API does the rest.
@@ -18,4 +17,3 @@ Cypress.Commands.add('updateTFMDeal', require('./central-api/updateTFMDeal'));
 Cypress.Commands.add('submitDeal', require('./trade-finance-manager-api/submitDeal'));
 Cypress.Commands.add('submitManyDeals', require('./trade-finance-manager-api/submitManyDeals'));
 Cypress.Commands.add('getUser', require('./trade-finance-manager-api/getUser'));
-
