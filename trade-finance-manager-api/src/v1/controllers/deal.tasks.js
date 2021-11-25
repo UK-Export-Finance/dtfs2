7cc240317785bdaac7ac4753ca83348f6a6ac508
Tony Barnes <tony@tonybarnes.me>
2021-11-25 19:00:45 +0000

    DTFS2-3549 - TFM tasks creation based on list of additional tasks (depending on deal data) (#504)
    * DTFS2-3549 - TFM tasks ability to create additional tasks. Add isConditional flag to tasks
    
    * DTFS2-3549 - create TFM tasks with only 'additional tasks' and isConditional task flags. No 'excluded tasks'
    
    * DTFS2-3549 - rename mockGroupTasks in api test

--- trade-finance-manager-api/src/v1/controllers/deal.tasks.js
+++ trade-finance-manager-api/src/v1/controllers/deal.tasks.js
@@ -3,6 +3,63 @@ const DEFAULTS = require('../defaults');
 const CONSTANTS = require('../../constants');
 const { createTasks } = require('../helpers/create-tasks');
 
+/**
+ * Conditions for adding 'create or match parties' task
+ **/
+const shouldCreatePartiesTask = (deal) => {
+  const { tfm } = deal;
+  const exporterPartyUrn = tfm.parties.exporter.partyUrn;
+
+  if (exporterPartyUrn && exporterPartyUrn.length) {
+    return false;
+  }
+
+  return true;
+};
+
+/**
+ * Conditions for adding 'check agent' task
+ **/
+const shouldCreateAgentCheckTask = (deal) => {
+  const {
+    dealType,
+    submissionType,
+  } = deal;
+
+  if (dealType === CONSTANTS.DEALS.DEAL_TYPE.BSS_EWCS
+    && submissionType === CONSTANTS.DEALS.SUBMISSION_TYPE.MIA) {
+    const { eligibility } = deal;
+
+    const eligibilityCriterion11 = eligibility.criteria.find((criterion) =>
+      criterion.id === 11);
+
+    const eligibilityCriteria11isFalse = eligibilityCriterion11.answer === false;
+
+    if (eligibilityCriteria11isFalse) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+/**
+ * Additional/conditional tasks to include, depending on deal data.
+ **/
+const listAdditionalTasks = (deal) => {
+  let additionalTasks = [];
+
+  if (shouldCreatePartiesTask(deal)) {
+    additionalTasks.push(CONSTANTS.TASKS.AIN_AND_MIA.GROUP_1.MATCH_OR_CREATE_PARTIES);
+  }
+
+  if (shouldCreateAgentCheckTask(deal)) {
+    additionalTasks.push(CONSTANTS.TASKS.MIA_GROUP_1_TASKS.COMPLETE_AGENT_CHECK);
+  }
+
+  return additionalTasks;
+};
+
 const createDealTasks = async (deal) => {
   if (!deal) {
     return false;
@@ -14,21 +71,11 @@ const createDealTasks = async (deal) => {
     tfm,
   } = deal;
 
-  let tasks;
+  const additionalTasks = listAdditionalTasks(deal);
 
-  const exporterPartyUrn = tfm.parties.exporter.partyUrn;
-
-  let excludedTasks = [];
-
-  if (exporterPartyUrn) {
-    excludedTasks = [
-      CONSTANTS.TASKS.AIN_AND_MIA.GROUP_1.MATCH_OR_CREATE_PARTIES,
-    ];
-  }
-
-  tasks = createTasks(
+  const tasks = createTasks(
     submissionType,
-    excludedTasks,
+    additionalTasks,
   );
 
   const dealUpdate = {
@@ -46,4 +93,9 @@ const createDealTasks = async (deal) => {
   };
 };
 
-exports.createDealTasks = createDealTasks;
+module.exports = {
+  shouldCreatePartiesTask,
+  shouldCreateAgentCheckTask,
+  listAdditionalTasks,
+  createDealTasks,
+};
--- trade-finance-manager-api/src/v1/helpers/create-tasks.api-test.js
+++ trade-finance-manager-api/src/v1/helpers/create-tasks.api-test.js
@@ -24,7 +24,7 @@ describe('defaults - tasks creation', () => {
   });
 
   describe('createGroupTasks', () => {
-    const mockTasks = [
+    const mockGroupTasks = [
       {
         title: 'Title A',
         team: 'Team A',
@@ -35,8 +35,15 @@ describe('defaults - tasks creation', () => {
       },
     ];
 
+    const mockGroupTasksWithConditionalFlags = [
+      { title: 'Task A', team: 'Test team' },
+      { title: 'Task B', team: 'Test team', isConditional: true },
+      { title: 'Task C', team: 'Test team' },
+      { title: 'Task D', team: 'Test team', isConditional: true },
+    ];
+
     it('should return array of tasks with incremented task id', () => {
-      const result = createGroupTasks(mockTasks, 2);
+      const result = createGroupTasks(mockGroupTasks, 2);
 
       const expected = [
         {
@@ -60,7 +67,7 @@ describe('defaults - tasks creation', () => {
 
     describe('when the given groupId is 1', () => {
       it('should add `canEdit` to the first task in group 1', () => {
-        const result = createGroupTasks(mockTasks, 1);
+        const result = createGroupTasks(mockGroupTasks, 1);
 
         const expected = [
           {
@@ -85,41 +92,55 @@ describe('defaults - tasks creation', () => {
       });
     });
 
-    describe('when excludedTasks array of strings is passed', () => {
-      it('should NOT return any tasks in a group that match a title in excludedTasks, retaining incremental task ids', () => {
-        const mockGroupTasks = [
-          { title: 'Task A', team: 'Test team' },
-          { title: 'Task B', team: 'Test team' },
-          { title: 'Task C', team: 'Test team' },
-          { title: 'Task D', team: 'Test team' },
-        ];
-
+    describe('when no additionalTasks are passed and a task has isConditional flag', () => {
+      it('should NOT add isConditional tasks', () => {
         const mockGroupId = 1;
 
-        const mockExcludedTasks = [
+        const result = createGroupTasks(
+          mockGroupTasksWithConditionalFlags,
+          mockGroupId,
+        );
+
+        const conditionalTasks = mockGroupTasksWithConditionalFlags.filter((task) => task.isConditional === true);
+
+        const conditionalTask1 = conditionalTasks[0];
+        const conditionalTask2 = conditionalTasks[1];
+
+        const conditionalTask1InResult = result.find((task) => task.title === conditionalTask1.title);
+        expect(conditionalTask1InResult).toBeUndefined();
+
+        const conditionalTask2InResult = result.find((task) => task.title === conditionalTask2.title);
+        expect(conditionalTask2InResult).toBeUndefined();
+      });
+    });
+
+    describe('when additionalTasks array of strings is passed', () => {
+      it('should return any additional tasks (with isConditional flag) in a group that match a title in additionalTasks, retaining incremental task ids', () => {
+        const mockGroupId = 1;
+
+        const mockAdditionalTasks = [
           'Task B',
-          'Task C',
+          'Task D',
         ];
 
-        const result = createGroupTasks(mockGroupTasks, mockGroupId, mockExcludedTasks);
+        const result = createGroupTasks(
+          mockGroupTasksWithConditionalFlags,
+          mockGroupId,
+          mockAdditionalTasks,
+        );
 
         const expected = [
           {
             id: '1',
-            groupId: 1,
-            title: 'Task A',
-            team: 'Test team',
+            groupId: mockGroupId,
+            ...mockGroupTasksWithConditionalFlags[0],
             ...NEW_TASK,
             status: 'To do',
             canEdit: true,
           },
-          {
-            id: '2',
-            groupId: 1,
-            title: 'Task D',
-            team: 'Test team',
-            ...NEW_TASK,
-          },
+          { id: '2', groupId: mockGroupId, ...mockGroupTasksWithConditionalFlags[1], ...NEW_TASK },
+          { id: '3', groupId: mockGroupId, ...mockGroupTasksWithConditionalFlags[2], ...NEW_TASK },
+          { id: '4', groupId: mockGroupId, ...mockGroupTasksWithConditionalFlags[3], ...NEW_TASK },
         ];
 
         expect(result).toEqual(expected);
@@ -142,18 +163,18 @@ describe('defaults - tasks creation', () => {
       expect(result).toEqual(expected);
     });
 
-    it('should return AIN tasks array with createGroupTasks and excludedTasks', () => {
-      const mockExcludedTasks = [
+    it('should return AIN tasks array with createGroupTasks and additionalTasks', () => {
+      const mockAdditionalTasks = [
         CONSTANTS.TASKS.AIN_AND_MIA.GROUP_1.CREATE_OR_LINK_SALESFORCE
       ];
 
-      const result = createTasksAIN(mockExcludedTasks);
+      const result = createTasksAIN(mockAdditionalTasks);
 
       const expected = [
         {
           groupTitle: CONSTANTS.TASKS.AIN.GROUP_1.GROUP_TITLE,
           id: 1,
-          groupTasks: createGroupTasks(CONSTANTS.TASKS.AIN.GROUP_1.TASKS, 1, mockExcludedTasks),
+          groupTasks: createGroupTasks(CONSTANTS.TASKS.AIN.GROUP_1.TASKS, 1, mockAdditionalTasks),
         },
       ];
 
@@ -191,33 +212,33 @@ describe('defaults - tasks creation', () => {
       expect(result).toEqual(expected);
     });
 
-    it('should return MIA tasks array with createGroupTasks and excludedTasks', () => {
-      const mockExcludedTasks = [
+    it('should return MIA tasks array with createGroupTasks and additionalTasks', () => {
+      const mockAdditionalTasks = [
         CONSTANTS.TASKS.AIN_AND_MIA.GROUP_1.MATCH_OR_CREATE_PARTIES
       ];
 
-      const result = createTasksMIA(mockExcludedTasks);
+      const result = createTasksMIA(mockAdditionalTasks);
 
       const expected = [
         {
           groupTitle: CONSTANTS.TASKS.MIA.GROUP_1.GROUP_TITLE,
           id: 1,
-          groupTasks: createGroupTasks(CONSTANTS.TASKS.MIA.GROUP_1.TASKS, 1, mockExcludedTasks),
+          groupTasks: createGroupTasks(CONSTANTS.TASKS.MIA.GROUP_1.TASKS, 1, mockAdditionalTasks),
         },
         {
           groupTitle: CONSTANTS.TASKS.MIA.GROUP_2.GROUP_TITLE,
           id: 2,
-          groupTasks: createGroupTasks(CONSTANTS.TASKS.MIA.GROUP_2.TASKS, 2, mockExcludedTasks),
+          groupTasks: createGroupTasks(CONSTANTS.TASKS.MIA.GROUP_2.TASKS, 2, mockAdditionalTasks),
         },
         {
           groupTitle: CONSTANTS.TASKS.MIA.GROUP_3.GROUP_TITLE,
           id: 3,
-          groupTasks: createGroupTasks(CONSTANTS.TASKS.MIA.GROUP_3.TASKS, 3, mockExcludedTasks),
+          groupTasks: createGroupTasks(CONSTANTS.TASKS.MIA.GROUP_3.TASKS, 3, mockAdditionalTasks),
         },
         {
           groupTitle: CONSTANTS.TASKS.MIA.GROUP_4.GROUP_TITLE,
           id: 4,
-          groupTasks: createGroupTasks(CONSTANTS.TASKS.MIA.GROUP_4.TASKS, 4, mockExcludedTasks),
+          groupTasks: createGroupTasks(CONSTANTS.TASKS.MIA.GROUP_4.TASKS, 4, mockAdditionalTasks),
         },
       ];
 
