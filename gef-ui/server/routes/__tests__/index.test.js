643fe33ba9cd0d6317405d5eb62cfb229800f9fd
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2021-11-25 14:17:51 +0000

    DTFS2-4124: Missing routes test

--- gef-ui/server/routes/__tests__/index.test.js
+++ gef-ui/server/routes/__tests__/index.test.js
@@ -8,6 +8,7 @@ import applicationDetailsRoutes from '../application-details';
 import applicationAbandonRoutes from '../application-abandon';
 import applicationSubmissionRoutes from '../application-submission';
 import submitToUkefRoutes from '../submit-to-ukef';
+import reviewDecisionRoutes from '../review-decision';
 import companiesHouseRoutes from '../companies-house';
 import exportersAddressRoutes from '../exporters-address';
 import enterExportersCorrespondenceAddressRoutes from '../enter-exporters-correspondence-address';
@@ -54,6 +55,7 @@ describe('Routes', () => {
     expect(useSpy).toHaveBeenCalledWith(applicationAbandonRoutes);
     expect(useSpy).toHaveBeenCalledWith(applicationSubmissionRoutes);
     expect(useSpy).toHaveBeenCalledWith(submitToUkefRoutes);
+    expect(useSpy).toHaveBeenCalledWith(reviewDecisionRoutes);
     expect(useSpy).toHaveBeenCalledWith(companiesHouseRoutes);
     expect(useSpy).toHaveBeenCalledWith(exportersAddressRoutes);
     expect(useSpy).toHaveBeenCalledWith(enterExportersCorrespondenceAddressRoutes);
---
+++ gef-ui/server/routes/__tests__/review-decision.test.js
@@ -0,0 +1,28 @@
+import validateToken from '../../middleware/validateToken';
+
+const getSpy = jest.fn();
+const postSpy = jest.fn();
+jest.doMock('express', () => ({
+  Router() {
+    return {
+      get: getSpy,
+      post: postSpy,
+    };
+  },
+}));
+
+describe('Routes', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line global-require
+    require('../review-decision');
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('Sets up all routes', () => {
+    expect(getSpy).toHaveBeenCalledWith('/application-details/:applicationId/review-decision', validateToken, expect.any(Function));
+    expect(postSpy).toHaveBeenCalledWith('/application-details/:applicationId/review-decision', validateToken, expect.any(Function));
+  });
+});
