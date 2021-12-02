868d9ba486cc46bda00a7175ed3aba7685be4257
ttbarnes <tony@tonybarnes.me>
2021-12-02 16:18:17 +0000

    DTFS2-4174 - TFM tasks e2e tests for MIA/dependency logic. Split out e2e test helper functions

---
+++ e2e-tests/trade-finance-manager/cypress/integration/journeys/case/tasks/tasks-helpers.js
@@ -0,0 +1,174 @@
+import MOCK_MIA_TASKS from '../../../../fixtures/tasks-MIA';
+import pages from '../../../pages';
+
+const getGroup = (groupId) =>
+  MOCK_MIA_TASKS.find((g) => g.id === groupId);
+
+const MIA_TASKS_STRUCTURE = {
+  1: {
+    totalGroupTasks: getGroup(1).groupTasks.length,
+  },
+  2: {
+    totalGroupTasks: getGroup(2).groupTasks.length,
+  },
+  3: {
+    totalGroupTasks: getGroup(3).groupTasks.length,
+  },
+  4: {
+    totalGroupTasks: getGroup(4).groupTasks.length,
+  },
+};
+
+const submitTaskInProgress = (groupId, taskId, userId) => {
+  const firstGroupFirstTask = pages.tasksPage.tasks.row(groupId, taskId);
+  firstGroupFirstTask.link().click();
+
+  pages.taskPage.assignedToSelectInput().select(userId);
+  pages.taskPage.taskStatusRadioInputInProgress().click();
+  pages.taskPage.submitButton().click();
+};
+
+const submitTaskComplete = (groupId, taskId, userId) => {
+  const firstGroupFirstTask = pages.tasksPage.tasks.row(groupId, taskId);
+  firstGroupFirstTask.link().click();
+
+  pages.taskPage.assignedToSelectInput().select(userId);
+  pages.taskPage.taskStatusRadioInputDone().click();
+  pages.taskPage.submitButton().click();
+};
+
+const assertCannotClickPreviousTask = (currentGroupId, currentTaskId) => {
+  if (currentGroupId !== 1) {
+    const isFirstTaskInGroup = currentTaskId === 1;
+
+    if (isFirstTaskInGroup) {
+      const previousGroup = MIA_TASKS_STRUCTURE[currentGroupId - 1];
+      const previousGroupId = currentGroupId - 1;
+
+      const lastTaskIdInPreviousGroup = previousGroup.totalGroupTasks;
+
+      const lastTaskRow = pages.tasksPage.tasks.row(previousGroupId, lastTaskIdInPreviousGroup);
+
+      lastTaskRow.link().should('not.exist');
+    }
+  } else if (currentTaskId !== 1) {
+    const previousTaskId = currentTaskId - 1;
+
+    const lastTaskRow = pages.tasksPage.tasks.row(currentGroupId, previousTaskId);
+
+    lastTaskRow.link().should('not.exist');
+  }
+};
+
+const assertCompleteTask = (groupId, taskId) => {
+  const row = pages.tasksPage.tasks.row(groupId, taskId);
+
+  row.link().should('not.exist');
+
+  row.status().invoke('text').then((text) => {
+    expect(text.trim()).to.equal('Done');
+  });
+};
+
+const assertTaskStatusAndLink = (groupId, taskId, expectedStatus) => {
+  const taskRow = pages.tasksPage.tasks.row(groupId, taskId);
+
+  taskRow.status().invoke('text').then((text) => {
+    expect(text.trim()).to.equal(expectedStatus);
+  });
+
+  taskRow.link().should('exist');
+};
+
+const assertNextTaskStatus = (currentGroupId, currentTaskId) => {
+  const currentGroup = MIA_TASKS_STRUCTURE[currentGroupId];
+  const isLastTaskInGroup = currentGroup.totalGroupTasks === currentTaskId;
+  const isLastGroup = currentGroupId === 4;
+
+  if (isLastTaskInGroup && !isLastGroup) {
+    // check the next task in the next group
+    const nextGroupId = currentGroupId + 1;
+
+    const nextGroupFirstTaskRow = pages.tasksPage.tasks.row(nextGroupId, 1);
+
+    nextGroupFirstTaskRow.status().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('To do');
+    });
+  } else if (!isLastTaskInGroup && !isLastGroup) {
+    // check the next task in current group
+    const nextTaskId = currentTaskId + 1;
+
+    const nextTaskRow = pages.tasksPage.tasks.row(currentGroupId, nextTaskId);
+
+    nextTaskRow.status().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('To do');
+    });
+  }
+};
+
+const assertCannotClickNextTask = (currentGroupId, currentTaskId) => {
+  const currentGroup = MIA_TASKS_STRUCTURE[currentGroupId];
+  const isLastTaskInGroup = currentGroup.totalGroupTasks === currentTaskId;
+  const isLastGroup = currentGroupId === 4;
+
+  if (isLastTaskInGroup && !isLastGroup) {
+    // check the next task in the next group
+    const nextGroupId = currentGroupId + 1;
+
+    const nextGroupFirstTaskRow = pages.tasksPage.tasks.row(nextGroupId, 1);
+
+    nextGroupFirstTaskRow.link().should('not.exist');
+
+    nextGroupFirstTaskRow.status().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Cannot start yet');
+    });
+  } else if (!isLastTaskInGroup && !isLastGroup) {
+    // check the next task in current group
+    const nextTaskId = currentTaskId + 1;
+
+    const nextTaskRow = pages.tasksPage.tasks.row(currentGroupId, nextTaskId);
+
+    nextTaskRow.link().should('not.exist');
+
+    nextTaskRow.status().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Cannot start yet');
+    });
+  }
+};
+
+const submitTaskCompleteAndAssertOtherTasks = (groupId, taskId, userId) => {
+  submitTaskInProgress(groupId, taskId, userId);
+
+  pages.tasksPage.filterRadioAllTasks().click();
+
+  assertCannotClickNextTask(groupId, taskId);
+
+  submitTaskComplete(groupId, taskId, userId);
+
+  pages.tasksPage.filterRadioAllTasks().click();
+
+  assertCompleteTask(groupId, taskId);
+
+  assertNextTaskStatus(groupId, taskId);
+
+  assertCannotClickPreviousTask(groupId, taskId);
+};
+
+const assertTaskStatus = (row, expectedStatus) => {
+  row.status().invoke('text').then((text) => {
+    expect(text.trim()).to.equal(expectedStatus);
+  });
+};
+
+module.exports = {
+  getGroup,
+  MIA_TASKS_STRUCTURE,
+  submitTaskInProgress,
+  submitTaskComplete,
+  assertCompleteTask,
+  assertTaskStatusAndLink,
+  assertNextTaskStatus,
+  assertCannotClickNextTask,
+  submitTaskCompleteAndAssertOtherTasks,
+  assertTaskStatus,
+};
