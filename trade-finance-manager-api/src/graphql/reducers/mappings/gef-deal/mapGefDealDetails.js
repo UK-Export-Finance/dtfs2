ca34a1474154d4631173603a64ee7c70f6b06dac
ttbarnes <tony@tonybarnes.me>
2021-12-07 16:41:37 +0000

    add dateOfLastAction mapping to TFM GEF deal graphQL mapping

--- trade-finance-manager-api/src/graphql/reducers/mappings/gef-deal/mapGefDealDetails.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/gef-deal/mapGefDealDetails.js
@@ -3,6 +3,7 @@ const mapGefDealDetails = (dealSnapshot) => ({
   bankSupplyContractID: dealSnapshot.bankInternalRefName,
   bankSupplyContractName: dealSnapshot.additionalRefName,
   submissionType: dealSnapshot.submissionType,
+  dateOfLastAction: dealSnapshot.updatedAt,
   owningBank: {
     name: dealSnapshot.bank.name,
     emails: dealSnapshot.bank.emails,
