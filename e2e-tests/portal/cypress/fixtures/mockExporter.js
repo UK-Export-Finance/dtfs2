ce6e638cf1e042910928a2aa617f9c5f3e78969d
zainzzkk <51957827+Zainzzkk@users.noreply.github.com>
2021-12-07 15:20:22 +0000

    DTFS2-4567 Exporter Column In Alphabetic Order (#519)
    * added sort, debugging e2e test
    
    * added e2e test for order and added mocks
    
    * minor fixes to e2e tests and removed reinsert mocks from portal e2e
    
    * reverted numTestsKeptInMemory

---
+++ e2e-tests/portal/cypress/fixtures/mockExporter.js
@@ -0,0 +1,103 @@
+const exporterOne = {
+  companiesHouseRegistrationNumber: '08547313',
+  companyName: 'Delta',
+  registeredAddress: {
+    line1: null,
+    line2: null,
+    county: null,
+    country: null,
+    postcode: null,
+  },
+  correspondenceAddress: null,
+  selectedIndustry: {
+    code: '1003',
+    name: 'Manufacturing',
+    class: {
+      code: '25620',
+      name: 'Machining',
+    },
+  },
+  industries: [],
+  smeType: 'MICRO',
+  probabilityOfDefault: 45.5,
+  isFinanceIncreasing: true,
+};
+
+const exporterTwo = {
+  companiesHouseRegistrationNumber: '08547313',
+  companyName: 'Charlie',
+  registeredAddress: {
+    line1: null,
+    line2: null,
+    county: null,
+    country: null,
+    postcode: null,
+  },
+  correspondenceAddress: null,
+  selectedIndustry: {
+    code: '1003',
+    name: 'Manufacturing',
+    class: {
+      code: '25620',
+      name: 'Machining',
+    },
+  },
+  industries: [],
+  smeType: 'MICRO',
+  probabilityOfDefault: 45.5,
+  isFinanceIncreasing: true,
+};
+
+const exporterThree = {
+  companiesHouseRegistrationNumber: '08547313',
+  companyName: 'Alpha',
+  registeredAddress: {
+    line1: null,
+    line2: null,
+    county: null,
+    country: null,
+    postcode: null,
+  },
+  correspondenceAddress: null,
+  selectedIndustry: {
+    code: '1003',
+    name: 'Manufacturing',
+    class: {
+      code: '25620',
+      name: 'Machining',
+    },
+  },
+  industries: [],
+  smeType: 'MICRO',
+  probabilityOfDefault: 45.5,
+  isFinanceIncreasing: true,
+};
+
+const exporterFour = {
+  companiesHouseRegistrationNumber: '08547313',
+  companyName: '',
+  registeredAddress: {
+    line1: null,
+    line2: null,
+    county: null,
+    country: null,
+    postcode: null,
+  },
+  correspondenceAddress: null,
+  selectedIndustry: {
+    code: '1003',
+    name: 'Manufacturing',
+    class: {
+      code: '25620',
+      name: 'Machining',
+    },
+  },
+  industries: [],
+  smeType: 'MICRO',
+  probabilityOfDefault: 45.5,
+  isFinanceIncreasing: true,
+};
+
+module.exports = {
+  exporterOne, exporterTwo, exporterThree, exporterFour,
+};
