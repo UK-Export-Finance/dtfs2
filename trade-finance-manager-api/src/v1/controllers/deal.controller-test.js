f11e4e36cfbcc2ebc4fa0914525aee96f55d4939
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2021-12-14 11:40:44 +0000

    DTFS2-5053 : API call reduction + Additional test cases

--- trade-finance-manager-api/src/v1/controllers/deal.controller-test.js
+++ trade-finance-manager-api/src/v1/controllers/deal.controller-test.js
@@ -1,9 +1,17 @@
 const MOCK_DEAL_MIA_SUBMITTED = require('../__mocks__/mock-deal-MIA-submitted');
+const MOCK_DEAL_MIN_GEF = require('../__mocks__/mock-gef-deal-MIN');
+const MOCK_DEAL_AIN_SUBMITTED = require('../__mocks__/mock-deal-AIN-submitted');
 
 const dealController = require('./deal.controller');
 
 describe('dealCanBeSubmittedToACBS()', () => {
-  it('Should ascertain whether the deal can be submitted to the ACBS', async () => {
-    expect(dealController.dealCanBeSubmittedToACBS(MOCK_DEAL_MIA_SUBMITTED)).toEqual(false);
+  it('Should return `FALSE` as the deal is a MIA', async () => {
+    expect(dealController.dealCanBeSubmittedToACBS(MOCK_DEAL_MIA_SUBMITTED.details.submissionType)).toEqual(false);
+  });
+  it('Should return `TRUE` as the deal is a MIN', async () => {
+    expect(dealController.dealCanBeSubmittedToACBS(MOCK_DEAL_MIN_GEF.submissionType)).toEqual(true);
+  });
+  it('Should return `TRUE` as the deal is a AIN', async () => {
+    expect(dealController.dealCanBeSubmittedToACBS(MOCK_DEAL_AIN_SUBMITTED.details.submissionType)).toEqual(true);
   });
 });
