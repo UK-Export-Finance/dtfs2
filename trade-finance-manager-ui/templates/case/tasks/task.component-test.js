b8344d7b39c9c43ce2e140ad4fbd641273661b35
ttbarnes <tony@tonybarnes.me>
2021-12-16 12:21:29 +0000

    fix/update TFM UI tests after moving submissionType to the top level

--- trade-finance-manager-ui/templates/case/tasks/task.component-test.js
+++ trade-finance-manager-ui/templates/case/tasks/task.component-test.js
@@ -8,9 +8,9 @@ describe(page, () => {
   const params = {
     dealId: '100200',
     deal: {
+      submissionType: 'Automatic Inclusion Notice',
       details: {
         ukefDealId: '1234',
-        submissionType: 'Automatic Inclusion Notice',
       },
       submissionDetails: {
         supplierName: 'Supplier name',
