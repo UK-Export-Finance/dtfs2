f729aa6b9a32751fd1ab77f3d7cefc5ffa6d0f35
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2021-12-23 15:10:18 +0000

    DTFS2-4149 : Added await to dealHasAllUkefIds()

--- trade-finance-manager-api/src/v1/helpers/dealHasAllUkefIds.api-test.js
+++ trade-finance-manager-api/src/v1/helpers/dealHasAllUkefIds.api-test.js
@@ -1,8 +1,12 @@
 const dealHasAllUkefIds = require('./dealHasAllUkefIds');
 const MOCK_DEAL = require('../__mocks__/mock-deal-MIN-second-submit-facilities-unissued-to-issued');
+const NO_UKEF_ID_MOCK_DEAL = require('../__mocks__/mock-deal-no-ukef-id');
 
 describe('dealHasAllUkefIds()', () => {
   it('Should return TRUE as deal and the facility have UKEF IDs', () => {
-    expect(dealHasAllUkefIds(MOCK_DEAL._id)).toEqual(true);
+    expect(await dealHasAllUkefIds(MOCK_DEAL._id)).toEqual(true);
+  });
+  it('Should return FALSE as deal and the facility does not have UKEF IDs', () => {
+    expect(await dealHasAllUkefIds(NO_UKEF_ID_MOCK_DEAL._id)).toEqual(false);
   });
 });
