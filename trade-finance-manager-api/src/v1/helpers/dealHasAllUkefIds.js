fa728271090a63ab3a4a908d66933d6cd9c0a45a
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2021-12-23 15:34:25 +0000

    DTFS2-4149 : Correct property path based on deal type

--- trade-finance-manager-api/src/v1/helpers/dealHasAllUkefIds.js
+++ trade-finance-manager-api/src/v1/helpers/dealHasAllUkefIds.js
@@ -1,6 +1,7 @@
 const {
   findOneTfmDeal,
 } = require('../controllers/deal.controller');
+const CONSTANTS = require('../../constants');
 
 /**
  * Verified whether the deal and all its facilities have a UKEF ID
@@ -11,7 +12,9 @@ const dealHasAllUkefIds = async (dealId) => {
   const deal = await findOneTfmDeal(dealId);
 
   if (deal && deal.dealSnapshot && deal.dealSnapshot.facilities) {
-    const dealHasId = !!deal.dealSnapshot.ukefDealId;
+    const dealHasId = deal.dealSnapshot.dealType === CONSTANTS.DEALS.DEAL_TYPE.GEF
+      ? !!deal.dealSnapshot.ukefDealId
+      : !!deal.dealSnapshot.details.ukefDealId;
     const facilitiesHaveIds = deal.dealSnapshot.facilities.filter((f) => !f.facilitySnapshot.ukefFacilityId).length === 0;
     return dealHasId && facilitiesHaveIds;
   }
