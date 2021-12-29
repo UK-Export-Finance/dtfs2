54e8f17c8246e7734ffecb1893b6465a7221f0b0
ttbarnes <tony@tonybarnes.me>
2021-12-29 15:17:56 +0000

    fix portal UI dashboard/deals test

--- portal/server/controllers/dashboard/deals.test.js
+++ portal/server/controllers/dashboard/deals.test.js
@@ -23,7 +23,9 @@ describe('controllers/dashboard', () => {
   let res;
   beforeEach(() => {
     req = {
-      body: {},
+      body: {
+        createdByYou: true,
+      },
       params: { page: 1 },
       session: {
         dashboardFilters: 'mock-filters',
@@ -39,40 +41,30 @@ describe('controllers/dashboard', () => {
 
   describe('allDeals', () => {
     it('renders the correct template', async () => {
+      const mockDeals = [
+        {
+          _id: 'mockDeal',
+          exporter: { companyName: 'mock company' },
+          product: PRODUCT.BSS_EWCS,
+          createdAt: 1234,
+        },
+        {
+          _id: 'mockDeal2',
+          exporter: { companyName: 'mock company' },
+          product: PRODUCT.GEF,
+          createdAt: 5678,
+        },
+      ];
+
       getApiData.mockResolvedValue({
         count: 2,
-        deals: [
-          {
-            _id: 'mockDeal',
-            exporter: { companyName: 'mock company' },
-            product: PRODUCT.BSS_EWCS,
-            createdAt: 1234,
-          },
-          {
-            _id: 'mockDeal2',
-            exporter: { companyName: 'mock company' },
-            product: PRODUCT.GEF,
-            createdAt: 5678,
-          },
-        ],
+        deals: mockDeals,
       });
 
       await allDeals(req, res);
 
       expect(res.render).toHaveBeenCalledWith('dashboard/deals.njk', {
-        deals: [
-          {
-            _id: 'mockDeal2',
-            exporter: 'mock company',
-            product: PRODUCT.BSS_EWCS,
-            updatedAt: 5678,
-          },
-          {
-            _id: 'mockDeal',
-            exporter: 'mock company',
-            product: PRODUCT.GEF,
-            updatedAt: 1234,
-          }],
+        deals: mockDeals,
         pages: {
           totalPages: 1,
           currentPage: 1,
@@ -84,6 +76,7 @@ describe('controllers/dashboard', () => {
           id: 'mock-user',
           roles: ['maker', 'checker'],
         },
+        createdByYou: req.body.createdByYou,
       });
     });
 
