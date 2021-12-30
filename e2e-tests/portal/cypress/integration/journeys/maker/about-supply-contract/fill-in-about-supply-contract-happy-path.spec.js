b2473698e10c60a0c6c3c32935e83827553e7a09
ttbarnes <tony@tonybarnes.me>
2021-12-30 16:46:14 +0000

    fix dashboard e2e tests after data changes

--- e2e-tests/portal/cypress/integration/journeys/maker/about-supply-contract/fill-in-about-supply-contract-happy-path.spec.js
+++ e2e-tests/portal/cypress/integration/journeys/maker/about-supply-contract/fill-in-about-supply-contract-happy-path.spec.js
@@ -16,8 +16,8 @@ context('about-supply-contract', () => {
     const aDealWithAboutSupplyContractInStatus = (status) => {
       const candidates = twentyOneDeals
         .filter((aDeal) => (aDeal.submissionDetails && status === aDeal.submissionDetails.status)
-          && (aDeal.details && aDeal.status === 'Draft')
-          && (aDeal.details && !aDeal.details.submissionDate));
+          && (aDeal.status === 'Draft')
+          && (!aDeal.details || !aDeal.details.submissionDate));
 
       const aDeal = candidates[0];
       if (!aDeal) {
