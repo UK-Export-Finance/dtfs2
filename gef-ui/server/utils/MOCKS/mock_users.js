46f7990692c4ce80bf5b76e630593ba83feaf540
ttbarnes <tony@tonybarnes.me>
2021-12-31 13:23:07 +0000

    linting

--- gef-ui/server/utils/MOCKS/mock_users.js
+++ gef-ui/server/utils/MOCKS/mock_users.js
@@ -1,29 +1,29 @@
 const MOCK_MAKER = {
   _id: '61cdde40055cf301acf98064',
-    username: 'BANK1_MAKER1',
-    roles: [ 
-      'maker',
+  username: 'BANK1_MAKER1',
+  roles: [
+    'maker',
+  ],
+  bank: {
+    id: '9',
+    name: 'UKEF test bank (Delegated)',
+    mga: [
+      'mga_ukef_1.docx', 
+      'mga_ukef_2.docx',
     ],
-    bank: {
-      id: '9',
-      name: 'UKEF test bank (Delegated)',
-      mga: [ 
-        'mga_ukef_1.docx', 
-        'mga_ukef_2.docx',
-      ],
-      emails: [ 
-        'maker1@ukexportfinance.gov.uk', 
-        'checker1@ukexportfinance.gov.uk',
-      ],
-      companiesHouseNo: 'UKEF0001',
-      partyUrn: '00318345',
-    },
-    lastLogin: '1640950583636',
-    firstname: 'Tamil',
-    surname: 'Rahani',
-    email: 'maker1@ukexportfinance.gov.uk',
-    timezone: 'Europe/London',
-    'user-status': 'active',
+    emails: [
+      'maker1@ukexportfinance.gov.uk',
+      'checker1@ukexportfinance.gov.uk',
+    ],
+    companiesHouseNo: 'UKEF0001',
+    partyUrn: '00318345',
+  },
+  lastLogin: '1640950583636',
+  firstname: 'Tamil',
+  surname: 'Rahani',
+  email: 'maker1@ukexportfinance.gov.uk',
+  timezone: 'Europe/London',
+  'user-status': 'active',
 };
 
 module.exports = {
