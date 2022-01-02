cc26de3ee5f3bcff8830549802f5fc40b5038e94
ttbarnes <tony@tonybarnes.me>
2022-01-02 15:25:44 +0000

    GEF deal/application controller api test asserting maker

--- portal-api/api-tests/v1/gef/application.api-test.js
+++ portal-api/api-tests/v1/gef/application.api-test.js
@@ -77,6 +77,7 @@ describe(baseUrl, () => {
             status: expect.any(String),
             updatedAt: expect.any(Number),
           },
+          maker: aMaker,
           eligibility: {
             criteria: mockEligibilityCriteriaLatestVersion.terms.map((criterion) => ({
               ...criterion,
@@ -122,6 +123,7 @@ describe(baseUrl, () => {
           status: expect.any(String),
           updatedAt: expect.any(Number),
         },
+        maker: aMaker,
         eligibility: {
           criteria: mockEligibilityCriteriaLatestVersion.terms.map((criterion) => ({
             ...criterion,
