0927de66e4264db1597a03eec614519a72e44207
ttbarnes <tony@tonybarnes.me>
2022-01-05 15:55:06 +0000

    DTFS2-5184 - remove old bssDeals and gefDeals imports/exports

--- portal/server/controllers/dashboard/index.js
+++ portal/server/controllers/dashboard/index.js
@@ -1,10 +1,8 @@
-const { bssDeals, gefDeals, allDeals } = require('./deals');
+const { allDeals } = require('./deals');
 const { bssFacilities, gefFacilities } = require('./facilities');
 
 module.exports = {
   allDeals,
-  bssDeals,
-  gefDeals,
   bssFacilities,
   gefFacilities,
 };
