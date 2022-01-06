62e8be526f361cf8edf687534a62ec7c74a5ef90
ttbarnes <tony@tonybarnes.me>
2022-01-06 12:21:23 +0000

    DTFS2-5184 - improve portal graphQL deals query api tests. Previous approach is flaky, now have true graphQL query test.

--- portal-api/api-tests/graphql/queries/query-all-deals.api-test.js
+++ portal-api/api-tests/graphql/queries/query-all-deals.api-test.js
@@ -1,194 +1,59 @@
-const wipeDB = require('../../wipeDB');
-const aDeal = require('../../v1/deals/deal-builder');
+const { createTestClient } = require('apollo-server-testing');
+const { ApolloServer } = require('apollo-server-express');
+const { applyMiddleware } = require('graphql-middleware');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const gql = require('graphql-tag');
 
-const app = require('../../../src/createApp');
-const testUserCache = require('../../api-test-users');
+const typeDefs = require('../../../src/graphql/schemas');
+const resolvers = require('../../../src/graphql/resolvers');
+const dealsReducer = require('../../../src/graphql/reducers/deals');
+const { MOCK_ALL_DEALS } = require('../mocks/deals');
 
-const { as } = require('../../api')(app);
+jest.mock('../../../src/v1/api', () => require('../mocks/api'));
 
-const dealsQuery = `
-query {
-  allDeals {
-    count
-    deals {
-      _id
-      status
-      bankInternalRefName
-      exporter
-      product
-      submissionType
-      updatedAt
+const GET_DEALS = gql`
+  query {
+    allDeals {
+      count
+      deals {
+        _id
+        status
+        bankInternalRefName
+        exporter
+        product
+        submissionType
+        updatedAt
+      }
     }
   }
-}`;
+`;
 
-const dealsPaginationQuery = `
-query {
-  allDeals(params: { start:5, pagesize: 3}) {
-    count
-    deals {
-      _id
-      status
-      bankInternalRefName
-      exporter
-      product
-      submissionType
-      updatedAt
-    }
-  }
-}`;
+describe('graphql query - allDeals', () => {
+  let query;
 
-const dealsFilterStatusQuery = `
-query {
-  allDeals(params: { filters: [{ field: "status", value: "Draft"}]}) {
-    count
-    deals {
-      status
-      bankInternalRefName
-    }
-  }
-}`;
+  beforeAll(() => {
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    const schemaWithMiddleware = applyMiddleware(schema);
 
-const dealsFilterNotStatusQuery = `
-query {
-  allDeals(params: { filters: [{ field: "status", value: "Draft", operator: "ne"}]}) {
-    count
-    deals {
-      _id
-      status
-      bankInternalRefName
-    }
-  }
-}`;
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      schema: schemaWithMiddleware,
+    });
 
-const queryBody = {
-  query: dealsQuery,
-};
-
-describe('/graphql query deals', () => {
-  let noRoles;
-  let anHSBCMaker;
-  let aBarclaysMaker;
-  let aBarclaysChecker;
-  let aSuperuser;
-
-  const mockDeals = [
-    aDeal({ bankInternalRefName: 'bank1-0', status: 'Draft' }),
-    aDeal({ bankInternalRefName: 'bank1-1', status: 'Draft' }),
-    aDeal({ bankInternalRefName: 'bank1-2', status: 'Draft' }),
-    aDeal({ bankInternalRefName: 'bank1-3', status: 'Draft' }),
-
-    aDeal({ bankInternalRefName: 'bank1-4', status: 'Submitted' }),
-    aDeal({ bankInternalRefName: 'bank1-5', status: 'Submitted' }),
-
-    aDeal({ bankInternalRefName: 'bank2-0', status: 'Acknowledged by UKEF' }),
-    aDeal({ bankInternalRefName: 'bank2-1', status: 'Acknowledged by UKEF' }),
-  ];
-
-  beforeAll(async () => {
-    const testUsers = await testUserCache.initialise(app);
-    noRoles = testUsers().withoutAnyRoles().one();
-    aBarclaysMaker = testUsers().withRole('maker').withBankName('Barclays Bank').one();
-    aBarclaysChecker = testUsers().withRole('checker').withBankName('Barclays Bank').one();
-    anHSBCMaker = testUsers().withRole('maker').withBankName('HSBC').one();
-    aSuperuser = testUsers().superuser().one();
-
-    await as(anHSBCMaker).post(mockDeals[0]).to('/v1/deals');
-    await as(anHSBCMaker).post(mockDeals[1]).to('/v1/deals');
-    await as(anHSBCMaker).post(mockDeals[2]).to('/v1/deals');
-    await as(anHSBCMaker).post(mockDeals[3]).to('/v1/deals');
-    await as(anHSBCMaker).post(mockDeals[4]).to('/v1/deals');
-    await as(anHSBCMaker).post(mockDeals[5]).to('/v1/deals');
-    await as(aBarclaysMaker).post(mockDeals[6]).to('/v1/deals');
-    await as(aBarclaysMaker).post(mockDeals[7]).to('/v1/deals');
+    // use the test server to create a query function
+    const { query: doQuery } = createTestClient(server);
+    query = doQuery;
   });
 
-  beforeEach(async () => {
-    await wipeDB.wipe(['deals']);
-    await wipeDB.wipe(['facilities']);
-  });
-
-  describe('/graphql list all deals', () => {
-    it('401s requests that do not present a valid Authorization token', async () => {
-      const { status } = await as().post(queryBody).to('/graphql');
-
-      expect(status).toEqual(401);
+  it('should return deals via dealsReducer', async () => {
+    const { data } = await query({
+      query: GET_DEALS,
     });
 
-    it('does not return any deals if the user does not have a maker or checker role', async () => {
-      // const deal = aDeal({ additionalRefName: 'bank1-0', bankInternalRefName: 'mockSupplyContractId' });
-      // await as(anHSBCMaker).post(deal).to('/v1/deals');
-
-      const { status, body } = await as(noRoles).post(queryBody).to('/graphql');
-
-      expect(status).toEqual(200);
-      expect(body.data.allDeals.count).toEqual(null);
-      expect(body.data.allDeals.deals).toEqual(null);
-    });
-
-    it('returns deals when the user has maker role', async () => {
-      const { status, body } = await as(anHSBCMaker).post(queryBody).to('/graphql');
-
-      expect(status).toEqual(200);
-      expect(body.data.allDeals.count).not.toBeNull();
-      expect(body.data.allDeals.deals).not.toBeNull();
-    });
-
-    it('returns deals when the user has checker role', async () => {
-      const { status, body } = await as(aBarclaysChecker).post(queryBody).to('/graphql');
-
-      expect(status).toEqual(200);
-      expect(body.data.allDeals.count).not.toBeNull();
-      expect(body.data.allDeals.deals).not.toBeNull();
-    });
-
-    it('returns all created deals', async () => {
-      const { body, status } = await as(aSuperuser).post(queryBody).to('/graphql');
-      expect(status).toEqual(200);
-
-      mockDeals.forEach((mockDeal) => {
-        const dealExists = body.data.allDeals.deals.find((d) =>
-          d.bankInternalRefName === mockDeal.bankInternalRefName);
-        expect(dealExists).toBeDefined();
-      });
-    });
-  });
-
-  describe('/graphql list deals pagination', () => {
-    it('returns a list of deals paginated by start/pagesize', async () => {
-      const { body, status } = await as(aSuperuser).post({ query: dealsPaginationQuery }).to('/graphql');
-      expect(status).toEqual(200);
-
-      // dealsPaginationQuery pageSize is 3, so the total count should be greater than this.
-      expect(body.data.allDeals.count).toBeGreaterThan(3);
-
-      // actual deals returned should equal the pageSize.
-      expect(body.data.allDeals.deals.length).toEqual(3);
-      expect(body.data.allDeals.count).toEqual(8);
-    });
-  });
-
-  describe('/graphql list deals filters', () => {
-    it('returns a list of deals filtered by equal filters', async () => {
-      const { status, body } = await as(anHSBCMaker).post({ query: dealsFilterStatusQuery }).to('/graphql');
-
-      expect(status).toEqual(200);
-
-      expect(body.data.allDeals.deals.every((deal) => deal.status === 'Draft')).toBe(true);
-
-      const totalDraftDeals = mockDeals.filter((mockDeal) => mockDeal.status === 'Draft').length;
-      expect(body.data.allDeals.count).toEqual(totalDraftDeals);
-    });
-
-    it('returns a list of deals filtered by NOT equal filter', async () => {
-      const { status, body } = await as(anHSBCMaker).post({ query: dealsFilterNotStatusQuery }).to('/graphql');
-
-      expect(status).toEqual(200);
-
-      expect(body.data.allDeals.deals.every((deal) => deal.status !== 'Draft')).toBe(true);
-
-      const totalNonDraftDeals = mockDeals.filter((mockDeal) => mockDeal.status !== 'Draft').length;
-      expect(body.data.allDeals.count).toEqual(totalNonDraftDeals);
-    });
+    expect(data.allDeals.count).toEqual(MOCK_ALL_DEALS.length);
+    
+    const expected = dealsReducer(MOCK_ALL_DEALS);
+    expect(data.allDeals.deals).toEqual(expected);
   });
 });
--- portal-api/api-tests/graphql/queries/query-deals.api-test.js
+++ portal-api/api-tests/graphql/queries/query-deals.api-test.js
@@ -1,384 +1,65 @@
-const wipeDB = require('../../wipeDB');
-const aDeal = require('../../v1/deals/deal-builder');
+const { createTestClient } = require('apollo-server-testing');
+const { ApolloServer } = require('apollo-server-express');
+const { applyMiddleware } = require('graphql-middleware');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const gql = require('graphql-tag');
 
-const app = require('../../../src/createApp');
-const testUserCache = require('../../api-test-users');
+const typeDefs = require('../../../src/graphql/schemas');
+const resolvers = require('../../../src/graphql/resolvers');
+const dealsReducer = require('../../../src/graphql/reducers/deals');
+const { MOCK_DEALS } = require('../mocks/deals');
 
-const { as } = require('../../api')(app);
+jest.mock('../../../src/v1/api', () => require('../mocks/api'));
 
-const dealsQuery = `
-query {
-  deals {
-    status {
-      code
-    }
+const GET_DEALS = gql`
+  query {
     deals {
-      _id
-      updatedAt
-      additionalRefName
-      bankInternalRefName
+      count
+      deals {
+        _id
+        dealType
+        submissionType
+        status
+        updatedAt
+        bankInternalRefName
+        bank {
+          id
+        }
+      }
     }
   }
-}`;
+`;
 
-const dealsPaginationQuery1 = `
-query {
-  deals(params: { start:2, pagesize: 2}) {
-    status {
-      code
-    }
-    count
-    deals {
-      _id
-      updatedAt
-      additionalRefName
-      bankInternalRefName
-    }
-  }
-}`;
+describe('graphql query - deals', () => {
+  let query;
 
+  beforeAll(() => {
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    const schemaWithMiddleware = applyMiddleware(schema);
 
-const dealsPaginationQuery2 = `
-query {
-  deals(params: { start:5, pagesize: 3}) {
-    status {
-      code
-    }
-    count
-    deals {
-      _id
-      updatedAt
-      additionalRefName
-      bankInternalRefName
-    }
-  }
-}`;
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      schema: schemaWithMiddleware,
+      context: {
+        user: {
+          bank: { id: '9' },
+        },
+      },
+    });
 
-const dealsFilterStatusQuery = `
-query {
-  deals(params: { filters: [{ field: "status", value: "Draft"}]}) {
-    status {
-      code
-    }
-    count
-    deals {
-      _id
-      updatedAt
-      additionalRefName
-      bankInternalRefName
-      status
-    }
-  }
-}`;
-
-const dealsFilterStatusPaginatedQuery = `
-query {
-  deals(params: {  start:1, pagesize: 2, filters: [{ field: "status", value: "Draft"}]}) {
-    status {
-      code
-    }
-    count
-    deals {
-      _id
-      updatedAt
-      additionalRefName
-      bankInternalRefName
-      status
-    }
-  }
-}`;
-
-const dealsFilterNotStatusQuery = `
-query {
-  deals(params: { filters: [{ field: "status", value: "Draft", operator: "ne"}]}) {
-    status {
-      code
-    }
-    count
-    deals {
-      _id
-      updatedAt
-      additionalRefName
-      bankInternalRefName
-      status
-    }
-  }
-}`;
-
-const queryBody = {
-  query: dealsQuery,
-};
-
-describe('/graphql query deals', () => {
-  let noRoles;
-  let anHSBCMaker;
-  let aBarclaysMaker;
-  let aBarclaysChecker;
-  let aSuperuser;
-
-  beforeAll(async () => {
-    const testUsers = await testUserCache.initialise(app);
-    noRoles = testUsers().withoutAnyRoles().one();
-    aBarclaysMaker = testUsers().withRole('maker').withBankName('Barclays Bank').one();
-    aBarclaysChecker = testUsers().withRole('checker').withBankName('Barclays Bank').one();
-    anHSBCMaker = testUsers().withRole('maker').withBankName('HSBC').one();
-    aSuperuser = testUsers().superuser().one();
+    // use the test server to create a query function
+    const { query: doQuery } = createTestClient(server);
+    query = doQuery;
   });
 
-  beforeEach(async () => {
-    await wipeDB.wipe(['deals']);
-    await wipeDB.wipe(['facilities']);
-  });
-
-  describe('/graphql list deals', () => {
-    it('401s requests that do not present a valid Authorization token', async () => {
-      const { status } = await as().post(queryBody).to('/graphql');
-
-      expect(status).toEqual(401);
+  it('should return deals', async () => {
+    const { data } = await query({
+      query: GET_DEALS,
     });
 
-    it('401s requests that do not come from a user with role=maker || role=checker', async () => {
-      const { body } = await as(noRoles).post(queryBody).to('/graphql');
-      expect(body.data.deals.status.code).toEqual(401);
-    });
+    expect(data.deals.count).toEqual(MOCK_DEALS.length);
 
-    it('accepts requests that come from a user with role=maker', async () => {
-      const { body } = await as(anHSBCMaker).post(queryBody).to('/graphql');
-      expect(body.data.deals.status.code).toEqual(200);
-    });
-
-
-    it('accepts requests that come from a user with role=checker', async () => {
-      const { body } = await as(aBarclaysChecker).post(queryBody).to('/graphql');
-      expect(body.data.deals.status.code).toEqual(200);
-    });
-
-    it('returns a list of deals ordered by "updated", filtered by <user>.bank.id', async () => {
-      const deals = [
-        aDeal({ additionalRefName: 'bank1-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-1', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-2', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank2-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank2-1', bankInternalRefName: 'mockSupplyContractId' }),
-      ];
-
-      await as(aBarclaysMaker).post(deals[4]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[2]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[1]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[0]).to('/v1/deals');
-      await as(aBarclaysMaker).post(deals[3]).to('/v1/deals');
-
-      const { body } = await as(anHSBCMaker).post(queryBody).to('/graphql');
-      expect(body.data.deals.status.code).toEqual(200);
-
-      // expect to see deals in reverse order; most recent on top..
-      expect(body.data.deals.deals.length).toEqual(3);
-      body.data.deals.deals.forEach((deal, index) => {
-        expect(deal.additionalRefName).toEqual(deals[index].additionalRefName);
-      });
-    });
-
-    it('returns a list of deals ordered by "updated" if <user>.bank.id == *', async () => {
-      const deals = [
-        aDeal({ additionalRefName: 'bank1-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-1', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-2', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank2-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank2-1', bankInternalRefName: 'mockSupplyContractId' }),
-      ];
-
-      const submitOrder = [4, 1, 2, 0, 3];
-
-      await as(aBarclaysMaker).post(deals[submitOrder[0]]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[submitOrder[1]]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[submitOrder[2]]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[submitOrder[3]]).to('/v1/deals');
-      await as(aBarclaysMaker).post(deals[submitOrder[4]]).to('/v1/deals');
-
-      const { body } = await as(aSuperuser).post(queryBody).to('/graphql');
-      expect(body.data.deals.status.code).toEqual(200);
-
-      // expect deals in reverse order;  most recent should be first..
-      submitOrder.reverse();
-      body.data.deals.deals.forEach((deal, index) => {
-        expect(deal.additionalRefName).toEqual(deals[submitOrder[index]].additionalRefName);
-      });
-    });
-  });
-
-  describe('/graphql list deals pagination', () => {
-    it('returns a list of deals, ordered by "updated", paginated by start/pagesize, filtered by <user>.bank.id', async () => {
-      const deals = [
-        aDeal({ additionalRefName: 'bank1-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-1', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-2', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-3', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-4', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-5', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank2-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank2-1', bankInternalRefName: 'mockSupplyContractId' }),
-      ];
-
-      await as(anHSBCMaker).post(deals[0]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[1]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[2]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[3]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[4]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[5]).to('/v1/deals');
-
-      await as(aBarclaysMaker).post(deals[6]).to('/v1/deals');
-      await as(aBarclaysMaker).post(deals[7]).to('/v1/deals');
-
-      //      const { status, body } = await as(anHSBCMaker).get('/v1/deals/2/2');
-      const { body } = await as(anHSBCMaker).post({ query: dealsPaginationQuery1 }).to('/graphql');
-      expect(body.data.deals.status.code).toEqual(200);
-
-      // expect deals in reverse order; most recent first..
-
-      expect(body.data.deals.deals[0]).toMatchObject(deals[3]);
-      expect(body.data.deals.deals[1]).toMatchObject(deals[2]);
-
-      expect(body.data.deals.count).toEqual(6);
-    });
-
-
-    it('returns a list of deals, ordered by "updated", paginated by start/pagesize, if <user>.bank.id == *', async () => {
-      const deals = [
-        aDeal({ additionalRefName: 'bank1-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-1', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-2', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-3', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-4', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank1-5', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank2-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ additionalRefName: 'bank2-1', bankInternalRefName: 'mockSupplyContractId' }),
-      ];
-
-      await as(anHSBCMaker).post(deals[0]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[1]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[2]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[3]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[4]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[5]).to('/v1/deals');
-
-      await as(aBarclaysMaker).post(deals[6]).to('/v1/deals');
-      await as(aBarclaysMaker).post(deals[7]).to('/v1/deals');
-
-      //      const { status, body } = await as(aSuperuser).get('/v1/deals/5/3');
-      const { body } = await as(aSuperuser).post({ query: dealsPaginationQuery2 }).to('/graphql');
-      expect(body.data.deals.status.code).toEqual(200);
-
-      // expect deals in reverse order - when we slice the last 3 deals we should get 2/1/0
-      expect(body.data.deals.deals[0]).toMatchObject(deals[2]);
-      expect(body.data.deals.deals[1]).toMatchObject(deals[1]);
-      expect(body.data.deals.deals[2]).toMatchObject(deals[0]);
-
-
-      expect(body.data.deals.count).toEqual(8);
-    });
-  });
-
-  describe('/graphql list deals filters', () => {
-    it('returns a list of deals, ordered by "updated", filtered by equal filters', async () => {
-      const deals = [
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-1', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-2', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-3', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Submitted', additionalRefName: 'bank1-4', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-5', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Acknowledged by UKEF', additionalRefName: 'bank2-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Acknowledged by UKEF', additionalRefName: 'bank2-1', bankInternalRefName: 'mockSupplyContractId' }),
-      ];
-
-      await as(anHSBCMaker).post(deals[0]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[1]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[2]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[3]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[4]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[5]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[6]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[7]).to('/v1/deals');
-
-      //      const { status, body } = await as(anHSBCMaker).get('/v1/deals/2/2');
-      const { body } = await as(anHSBCMaker).post({ query: dealsFilterStatusQuery }).to('/graphql');
-
-      expect(body.data.deals.status.code).toEqual(200);
-
-      // expect deals in reverse order; most recent first..
-      expect(body.data.deals.deals[0]).toMatchObject(deals[5]);
-      expect(body.data.deals.deals[1]).toMatchObject(deals[3]);
-      expect(body.data.deals.deals[2]).toMatchObject(deals[2]);
-      expect(body.data.deals.deals[3]).toMatchObject(deals[1]);
-      expect(body.data.deals.deals[4]).toMatchObject(deals[0]);
-
-      expect(body.data.deals.count).toEqual(5);
-    });
-
-    it('returns a list of deals, ordered by "updated", paginated and filtered by equal filters', async () => {
-      const deals = [
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-1', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-2', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-3', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Submitted', additionalRefName: 'bank1-4', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-5', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Acknowledged by UKEF', additionalRefName: 'bank2-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Acknowledged by UKEF', additionalRefName: 'bank2-1', bankInternalRefName: 'mockSupplyContractId' }),
-      ];
-
-      await as(anHSBCMaker).post(deals[0]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[1]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[2]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[3]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[4]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[5]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[6]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[7]).to('/v1/deals');
-
-      //      const { status, body } = await as(anHSBCMaker).get('/v1/deals/2/2');
-      const { body } = await as(anHSBCMaker).post({ query: dealsFilterStatusPaginatedQuery }).to('/graphql');
-
-      expect(body.data.deals.status.code).toEqual(200);
-
-      // expect deals of 2nd page in reverse order; most recent first..
-      expect(body.data.deals.deals[0]).toMatchObject(deals[3]);
-      expect(body.data.deals.deals[1]).toMatchObject(deals[2]);
-
-      expect(body.data.deals.count).toEqual(5);
-    });
-
-    it('returns a list of deals, ordered by "updated", filtered by not equal filter', async () => {
-      const deals = [
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-1', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-2', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-3', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Submitted', additionalRefName: 'bank1-4', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Draft', additionalRefName: 'bank1-5', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Acknowledged by UKEF', additionalRefName: 'bank2-0', bankInternalRefName: 'mockSupplyContractId' }),
-        aDeal({ status: 'Acknowledged by UKEF', additionalRefName: 'bank2-1', bankInternalRefName: 'mockSupplyContractId' }),
-      ];
-
-      await as(anHSBCMaker).post(deals[0]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[1]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[2]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[3]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[4]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[5]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[6]).to('/v1/deals');
-      await as(anHSBCMaker).post(deals[7]).to('/v1/deals');
-
-      const { body } = await as(anHSBCMaker).post({ query: dealsFilterNotStatusQuery }).to('/graphql');
-
-      expect(body.data.deals.status.code).toEqual(200);
-
-      // expect deals in reverse order; most recent first..
-      expect(body.data.deals.deals[0]).toMatchObject(deals[7]);
-      expect(body.data.deals.deals[1]).toMatchObject(deals[6]);
-      expect(body.data.deals.deals[2]).toMatchObject(deals[4]);
-
-      expect(body.data.deals.count).toEqual(3);
-    });
+    expect(data.deals.deals).toEqual(MOCK_DEALS);
   });
 });
