5adc3e62e4e0c358f6808a440df223132561354c
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-01-10 11:09:02 +0000

    fix: cover end date

--- trade-finance-manager-api/src/graphql/resolvers/query-facilities.js
+++ trade-finance-manager-api/src/graphql/resolvers/query-facilities.js
@@ -1,14 +1,15 @@
+const { format } = require('date-fns');
+
 const { getAllFacilities } = require('../../v1/controllers/facility.controller');
 const { formattedNumber } = require('../../utils/number');
 // list all facilities from the database
 exports.queryAllFacilities = async (queryParams) => {
   const rawFacilities = await getAllFacilities(queryParams.params);
-
   const facilities = [];
-  let facility;
   // eslint-disable-next-line no-restricted-syntax
   for (const item of rawFacilities) {
-    facility = item.tfmFacilities;
+    const facility = item.tfmFacilities;
+    facility.coverEndDate = item.tfmFacilities.coverEndDate ? format(new Date(item.tfmFacilities.coverEndDate), 'dd MM yyyy') : '';
     facility.value = `${item.tfmFacilities.currency} ${formattedNumber(item.tfmFacilities.value)}`;
     facility.facilityType = item.tfmFacilities.facilityType.toLowerCase();
     facilities.push(facility);
