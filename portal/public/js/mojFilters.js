9eafeb272a8db05c0a9bf2f13fd849d2706b8edd
ttbarnes <tony@tonybarnes.me>
2022-01-14 14:32:57 +0000

    DTFS2-4341 - MOJ filter script in it's own file

---
+++ portal/public/js/mojFilters.js
@@ -0,0 +1,2 @@
+var DTFS;void 0!==MOJFrontend.FilterToggleButton&&new MOJFrontend.FilterToggleButton({bigModeMediaQuery:"(min-width: 48.063em)",startHidden:!0,toggleButton:{container:$(".moj-action-bar__filter"),showText:"Show filter",hideText:"Hide filter",classes:"govuk-button--secondary",attributes:{"data-cy":"show-hide-filters-toggle-button"}},filter:{container:$(".moj-filter")}}),(DTFS=void 0===DTFS?{}:DTFS).mojFilters={};
+//# sourceMappingURL=mojFilters.js.map
---
+++ portal/scripts/moj-filters.js
@@ -0,0 +1,18 @@
+if (typeof MOJFrontend.FilterToggleButton !== 'undefined') {
+  new MOJFrontend.FilterToggleButton({
+    bigModeMediaQuery: '(min-width: 48.063em)',
+    startHidden: true,
+    toggleButton: {
+      container: $('.moj-action-bar__filter'),
+      showText: 'Show filter',
+      hideText: 'Hide filter',
+      classes: 'govuk-button--secondary',
+      attributes: {
+        'data-cy': 'show-hide-filters-toggle-button'
+      }
+    },
+    filter: {
+      container: $('.moj-filter'),
+    },
+  });
+}
