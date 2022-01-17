4c07f8d9a3bfc6d375389c73e186d93795ae096d
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-01-17 09:05:29 +0000

    Removed the Test Hook API microservice (#597)

--- test-hook-api/src/fileshare/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const sendTypeB = require('./sendTypeB');
-
-const routes = express.Router();
-
-routes.route('/typeB').post(sendTypeB);
-
-module.exports = routes;
