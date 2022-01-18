ab78175f287ed6bcfc35e19b80c605b3909a8cef
ttbarnes <tony@tonybarnes.me>
2022-01-18 11:52:13 +0000

    DTFS2-4341 - fix/update db-helpers api tests

--- portal-api/api-tests/graphql/resolvers/helpers/db-helpers.api-test.js
+++ portal-api/api-tests/graphql/resolvers/helpers/db-helpers.api-test.js
@@ -7,12 +7,18 @@ describe('/graphql resolvers - helpers - db-helpers', () => {
   describe('createDbQuery', () => {
     it('should return formatted db query object', () => {
       const mockOperator = 'and';
-      const mockValue = 'a value';
+      const mockField = 'fieldName';
+      const mockValue = ['first value', 'second value'];
 
-      const result = createDbQuery(mockOperator, mockValue);
+      const result = createDbQuery(mockOperator, mockField, mockValue);
 
       const expectedOperatorString = `$${mockOperator}`;
-      const expected = { [`${expectedOperatorString}`]: mockValue };
+      const expected = {
+        [`${expectedOperatorString}`]: [
+          { [mockField]: mockValue[0] },
+          { [mockField]: mockValue[1] },
+        ],
+      };
 
       expect(result).toEqual(expected);
     });
