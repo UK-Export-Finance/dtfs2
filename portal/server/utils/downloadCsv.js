254c81420395ee9e73c71df9a9cf8aa8719fd1a7
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-01-18 17:45:39 +0000

    Fix download reports

--- portal/server/utils/downloadCsv.js
+++ portal/server/utils/downloadCsv.js
@@ -4,7 +4,7 @@ const downloadCsv = (res, fileName, fields, data) => {
   const json2csv = new Parser({ fields });
   const csv = json2csv.parse(data);
   res.header('Content-Type', 'text/csv');
-  res.attachment(fileName);
+  res.attachment(`${fileName}.csv`);
   return res.send(csv);
 };
 
