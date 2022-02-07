4c34c439f2cb1215e319ec07400fec728af65a2e
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2022-02-07 12:49:38 +0000

    DTFS2-2203-2219 Header and eligibility criteria accessibility fixes (#648)
    * accessibility fixes for header and added e2e tests
    
    * merge main and code fixes and test file changes
    
    * made header into 1 file with if statement
    
    * deleted unnecessary files

---
+++ e2e-tests/gef/cypress/integration/pages/page-banner.js
@@ -0,0 +1,13 @@
+const pageBanner = {
+  header: () => cy.get('[data-cy="header"]'),
+  homeLink: () => cy.get('[data-cy="gov-home-link-header"]'),
+  crownLogo: () => cy.get('[data-cy="gov-crown-svg-header"]'),
+  crownLogoTitle: () => cy.get('[data-cy="gov-crown-svg-title-header"]'),
+  serviceName: () => cy.get('[data-cy="serviceName-header"]'),
+  navigation: () => cy.get('[data-cy="navigation-header"]'),
+  userName: () => cy.get('[data-cy="username-header"]'),
+  profile: () => cy.get('[data-cy="profile-header"]'),
+  logout: () => cy.get('[data-cy="logout-header"]'),
+};
+
+module.exports = pageBanner;
