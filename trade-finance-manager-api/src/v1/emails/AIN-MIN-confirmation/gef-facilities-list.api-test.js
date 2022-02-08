9921f3d896f2f1bc5a5de5715126b2d0062b5e87
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2022-02-08 11:20:57 +0000

    removed requested-cover-start-date from email template and updated test

--- trade-finance-manager-api/src/v1/emails/AIN-MIN-confirmation/gef-facilities-list.api-test.js
+++ trade-finance-manager-api/src/v1/emails/AIN-MIN-confirmation/gef-facilities-list.api-test.js
@@ -95,8 +95,20 @@ describe('generate AIN/MIN confirmation email facilities list email variable/str
 
         const result = facilityFieldsObj(mockFacilityWithCoverStartOnSubmission);
 
-        expect(result.requestedCoverStartDate).toEqual(mockFacility.coverStartDate);
-        expect(result.shouldCoverStartOnSubmission).toBeUndefined();
+        expect(result.coverStartDate).toEqual(format(Number(mockFacility.coverStartDate), 'do MMMM yyyy'));
+      });
+    });
+
+    describe('when facility.shouldCoverStartOnSubmission is true', () => {
+      it('should add requestedCoverStartDate field with the coverStartDate value, and not add shouldCoverStartOnSubmission', () => {
+        const mockFacilityWithCoverStartOnSubmission = {
+          ...mockFacility,
+          shouldCoverStartOnSubmission: true,
+        };
+
+        const result = facilityFieldsObj(mockFacilityWithCoverStartOnSubmission);
+
+        expect(result.coverStartDate).toEqual(format(Number(mockFacility.coverStartDate), 'do MMMM yyyy'));
       });
     });
   });
