18084afeebf826601b0819c4aa6a877d64363078
ttbarnes <tony@tonybarnes.me>
2022-02-09 16:29:16 +0000

    DTFS2-4615 - ensure dashboard facility filters are removed when navigating away

--- e2e-tests/portal/cypress/integration/journeys/dashboard/dashboard-facilities/dashboard-facilities-filters-reset-after-navigating-away.spec.js
+++ e2e-tests/portal/cypress/integration/journeys/dashboard/dashboard-facilities/dashboard-facilities-filters-reset-after-navigating-away.spec.js
@@ -2,13 +2,16 @@ const relative = require('../../../relativeURL');
 const MOCK_USERS = require('../../../../fixtures/users');
 const {
   header,
-  dashboardDeals,
+  dashboardFacilities,
 } = require('../../../pages');
 const {
   dashboardSubNavigation,
   dashboardFilters,
 } = require('../../../partials');
-const { BSS_DEAL_MIA } = require('../fixtures');
+const {
+  BSS_DEAL_AIN,
+  BSS_FACILITY_BOND,
+} = require('../fixtures');
 
 const { BANK1_MAKER1, ADMIN } = MOCK_USERS;
 
@@ -19,13 +22,19 @@ context('Dashboard Deals filters - reset after applying and navigating away', ()
     cy.deleteGefApplications(ADMIN);
     cy.deleteDeals(ADMIN);
 
-    cy.insertOneDeal(BSS_DEAL_MIA, BANK1_MAKER1);
+    cy.insertOneDeal(BSS_DEAL_AIN, BANK1_MAKER1).then((deal) => {
+      const dealId = deal._id;
+
+      const facilities = [BSS_FACILITY_BOND];
+
+      cy.createFacilities(dealId, facilities, BANK1_MAKER1);
+    });
   });
 
   before(() => {
     cy.login(BANK1_MAKER1);
-    dashboardDeals.visit();
-    cy.url().should('eq', relative('/dashboard/deals/0'));
+    dashboardFacilities.visit();
+    cy.url().should('eq', relative('/dashboard/facilities/0'));
   });
 
   it('resets filters after navigating away from the dashboard', () => {
@@ -33,32 +42,31 @@ context('Dashboard Deals filters - reset after applying and navigating away', ()
     dashboardFilters.showHideButton().click();
 
     // apply filter 1
-    dashboardDeals.filters.panel.form.status.draft.checkbox().click();
+    dashboardFacilities.filters.panel.form.type.bond.checkbox().click();
 
     // apply filter 2
-    dashboardFilters.panel.form.submissionType.MIA.checkbox().click();
+    dashboardFilters.panel.form.submissionType.AIN.checkbox().click();
 
     // submit filters
     dashboardFilters.panel.form.applyFiltersButton().click();
 
-    cy.url().should('eq', relative('/dashboard/deals/0'));
+    cy.url().should('eq', relative('/dashboard/facilities/0'));
 
     // check the filters are applied
     dashboardFilters.showHideButton().click();
 
-    dashboardDeals.filters.panel.form.status.draft.checkbox().should('be.checked');
-    dashboardFilters.panel.form.submissionType.MIA.checkbox().should('be.checked');
-
+    dashboardFacilities.filters.panel.form.type.bond.checkbox().should('be.checked');
+    dashboardFilters.panel.form.submissionType.AIN.checkbox().should('be.checked');
     // navigate somewhere else
+    cy.visit('/dashboard/deals');
+    cy.url().should('eq', relative('/dashboard/deals/0'));
+
+    // go back to facilities dashboard
     dashboardSubNavigation.facilities().click();
     cy.url().should('eq', relative('/dashboard/facilities/0'));
 
-    // go back to dashboard
-    header.dashboard().click();
-    cy.url().should('eq', relative('/dashboard/deals/0'));
-
     // previously applied filters should not be applied
-    dashboardDeals.filters.panel.form.status.draft.checkbox().should('not.be.checked');
-    dashboardFilters.panel.form.submissionType.MIA.checkbox().should('not.be.checked');
+    dashboardFacilities.filters.panel.form.type.bond.checkbox().should('not.be.checked');
+    dashboardFilters.panel.form.submissionType.AIN.checkbox().should('not.be.checked');
   });
 });
