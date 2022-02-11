dd3ef55f5a18f74569c70294471f49d8c5d9db29
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-02-11 10:53:38 +0000

    DTFS2-5355 : Constants to object value

--- trade-finance-manager-api/src/v1/helpers/get-submission-date.api-test.js
+++ trade-finance-manager-api/src/v1/helpers/get-submission-date.api-test.js
@@ -5,14 +5,14 @@ const MOCK_GEF_MIN_DEAL = require('../__mocks__/mock-gef-deal-MIN');
 
 describe('Ascertain deal submission date', () => {
   it('Should return `submissionDate` as deal is an AIN', () => {
-    expect(getSubmissionDate(MOCK_GEF_AIN_DEAL)).toEqual(1626169888809);
+    expect(getSubmissionDate(MOCK_GEF_AIN_DEAL)).toEqual(Number(MOCK_GEF_AIN_DEAL.submissionDate));
   });
 
   it('Should return `submissionDate` as deal is a MIA', () => {
-    expect(getSubmissionDate(MOCK_GEF_MIA_DEAL)).toEqual(1626169888809);
+    expect(getSubmissionDate(MOCK_GEF_MIA_DEAL)).toEqual(Number(MOCK_GEF_MIA_DEAL.submissionDate));
   });
 
   it('Should return `manualInclusionNoticeSubmissionDate` as deal is a MIN', () => {
-    expect(getSubmissionDate(MOCK_GEF_MIN_DEAL)).toEqual(1234);
+    expect(getSubmissionDate(MOCK_GEF_MIN_DEAL)).toEqual(MOCK_GEF_MIN_DEAL.manualInclusionNoticeSubmissionDate);
   });
 });
