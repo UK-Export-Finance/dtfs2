c7e556315128a89e0ffcec6b2b5eab4339b56911
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2022-03-01 10:05:23 +0000

    added e2e tests for new header

--- e2e-tests/trade-finance-manager/cypress/integration/partials/header.js
+++ e2e-tests/trade-finance-manager/cypress/integration/partials/header.js
@@ -1,6 +1,10 @@
 const partial = {
   userLink: () => cy.get('[data-cy="header-user-link"]'),
   signOutLink: () => cy.get('[data-cy="header-sign-out-link"]'),
+  ukefLogo: () => cy.get('[data-cy="header-ukef-logo"]'),
+  headerName: () => cy.get('[data-cy="header-TFM-header-name"]'),
+  betaBanner: () => cy.get('[data-cy="beta-banner"]'),
+  betaBannerHref: () => cy.get('[data-cy="beta-feedback-link"]'),
 };
 
 module.exports = partial;
