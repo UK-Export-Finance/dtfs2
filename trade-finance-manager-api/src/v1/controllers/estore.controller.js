7f7efa4a3b1ada83c173d23d877b445a7bcbafd8
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-03-02 14:12:46 +0000

    Disabled eStore integration (temporarily)

--- trade-finance-manager-api/src/v1/controllers/estore.controller.js
+++ trade-finance-manager-api/src/v1/controllers/estore.controller.js
@@ -1,11 +1,12 @@
-const mapCreateEstore = require('../mappings/map-create-estore');
+// const mapCreateEstore = require('../mappings/map-create-estore');
 
-const api = require('../api');
+// const api = require('../api');
 
 const createEstoreFolders = async (deal) => {
-  const eStoreInput = mapCreateEstore(deal);
+  // const eStoreInput = mapCreateEstore(deal);
 
-  const estore = await api.createEstoreFolders(eStoreInput);
+  // const estore = await api.createEstoreFolders(eStoreInput);
+  const estore = {};
   return {
     ...deal,
     tfm: {
