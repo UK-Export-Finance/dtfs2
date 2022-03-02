87cd70c1962712eefbcdfb21803c56488a5b8b08
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2022-03-02 09:22:44 +0000

    DTFS2-5412 TFM beta banner and logo (#745)
    * add beta banner and change logo on tfm header
    
    * Updating the style
    
    * added e2e tests for new header
    
    * added alt and title
    
    Co-authored-by: Gabriel Ignat <gabriel.ignat@hotmail.com>

--- e2e-tests/trade-finance-manager/cypress/integration/partials/primaryNavigation.js
+++ e2e-tests/trade-finance-manager/cypress/integration/partials/primaryNavigation.js
@@ -1,5 +1,6 @@
 const partial = {
   allDealsLink: () => cy.get('[data-cy="all-deals-link"]'),
+  allFacilitiesLink: () => cy.get('[data-cy="all-facilities-link"]'),
 };
 
 module.exports = partial;
