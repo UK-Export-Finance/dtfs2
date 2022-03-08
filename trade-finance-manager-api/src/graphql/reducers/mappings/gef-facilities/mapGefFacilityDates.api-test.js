1fff450c995349132a47283eabd72df10e87a0e6
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2022-03-08 09:53:29 +0000

    changed test wording

--- trade-finance-manager-api/src/graphql/reducers/mappings/gef-facilities/mapGefFacilityDates.api-test.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/gef-facilities/mapGefFacilityDates.api-test.js
@@ -72,7 +72,7 @@ describe('mapGefFacilityDates', () => {
     expect(result.tenor).toEqual(expected);
   });
 
-  it('could just say should not map coverEndDate when date is null', () => {
+  it('should not map coverEndDate when date is null', () => {
     mockFacility.coverEndDate = null;
     const result = mapGefFacilityDates(mockFacility, mockFacilityTfm, MOCK_GEF_DEAL);
     // undefined as not set in function
