360856dbaefab24f281eedd186125ef93e9eebee
ttbarnes <tony@tonybarnes.me>
2022-03-09 12:28:15 +0000

    remove/revert addition of invalid GEF mandatory criteria version

--- utils/mock-data-loader/gef/mandatoryCriteriaVersioned.js
+++ utils/mock-data-loader/gef/mandatoryCriteriaVersioned.js
@@ -66,66 +66,6 @@ const MANDATORY_CRITERIA_VERSIONED = [
     ],
   },
   {
-    version: 30,
-    isInDraft: false,
-    title: 'Confirm eligibility (mandatory criteria)',
-    introText: 'You must confirm that all the following will be true for this application on the date that cover starts.',
-    criteria: [
-      {
-        id: '1',
-        body: 'The Bank has received an Exporter Declaration and the Bank Team is not aware that any information contained in that Exporter Declaration is inaccurate in any material respect.',
-      },
-      {
-        id: '2',
-        body: 'The Bank has complied with its policies and procedures in relation to the Facility.',
-      },
-      {
-        id: '3',
-        body: 'The Exporter satisfies the following condition: Each of the Exporter and its Parent Obligor (if any) is an Eligible Person.',
-      },
-      {
-        id: '4',
-        body: 'The Exporter satisfies the following condition: neither the Exporter nor its UK Parent Obligor (if any) is an Affected Person.',
-      },
-      {
-        id: '5',
-        body: 'Facility Letter satisfies the following conditions: it is expressed to be governed by laws of England and Wales, Scotland or Northern Ireland.',
-      },
-      {
-        id: '6',
-        body: 'Facility Letter satisfies the following conditions:',
-        childList: [
-          'Utilisations under the Facility Letter are payable on demand or',
-          'it provides that the Bank has the right to accelerate the Facility if any of the circumstances set out in paragraph 9.4 of the Exporter Declaration occur.',
-        ],
-      },
-      {
-        id: '7',
-        body: 'The Bank is the sole and legal beneficial owner of, and has good title to, the Facility and any Utilisations made thereunder.',
-        childList: [
-          'The Bank has not made any Disposal (other than a Permitted Disposal) of any of its right, title and interest in the Facility or any Utilisation thereunder; and',
-          'The Bank has not made any Risk Transfer (other than a Permitted Risk Transfer) to any person in relation to the Facility or any Utilisation thereunder.',
-        ]
-      },
-      {
-        id: '8',
-        body: 'The Bank’s right, title and interest in and to the Facility, and any Utilisation thereunder (including any indebtedness, obligation or liability of each Obligor) is:',
-        childList: [
-          'free and clear of any Security or Quasi-Security (other than Permitted Security); and',
-          'freely assignable by the Bank without the need to obtain the consent of any Obligor or other person.',
-        ]
-      },
-      {
-        id: '9',
-        body: 'The Bank is entitled to provide information and copies of records and documents to UK Export Finance including pursuant to clauses 13.2(d) (Information about Covered Facilities) and 13.3 (Inspection) and in particular it is not prevented or restricted in doing so by the terms of any agreement entered into with any Obligor. ',
-      },
-      {
-        id: '10',
-        body: 'The Exporter is the only borrower under the relevant Facility Letter.',
-      },
-    ],
-  },
-  {
     version: 0.1,
     createdAt: new Date('2021-01-04T00:00'),
     updatedAt: null,
