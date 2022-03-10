211ccfcf319edf037f197eda1c59efb6fb0bff4e
ttbarnes <tony@tonybarnes.me>
2022-03-10 16:57:33 +0000

    fix BSS mandatory criteria not displaying. Add e2e test coverage

--- e2e-tests/portal/cypress/integration/journeys/maker/before-you-start.spec.js
+++ e2e-tests/portal/cypress/integration/journeys/maker/before-you-start.spec.js
@@ -5,7 +5,30 @@ const MOCK_USERS = require('../../../fixtures/users');
 
 const { BANK1_MAKER1 } = MOCK_USERS;
 
-context('Red Line eligibility checking', () => {
+context('Red Line eligibility checking (`before you start` page)', () => {
+  it('should render headings, intro and mandatory criteria', () => {
+    cy.createBSSSubmission(BANK1_MAKER1);
+    cy.url().should('eq', relative('/before-you-start'));
+
+    pages.beforeYouStart.mandatoryCriteriaHeading().should('be.visible');
+    pages.beforeYouStart.mandatoryCriteriaHeading().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Mandatory criteria');
+    });
+
+    pages.beforeYouStart.mandatoryCriteriaSubHeading().should('be.visible');
+    pages.beforeYouStart.mandatoryCriteriaSubHeading().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('Does this deal meet all UKEF\'s mandatory criteria?');
+    });
+
+    pages.beforeYouStart.mandatoryCriteriaIntro().should('be.visible');
+    pages.beforeYouStart.mandatoryCriteriaIntro().invoke('text').then((text) => {
+      expect(text.trim()).to.equal('To proceed with this submission, you need to be able to affirm that all the following mandatory criteria are or will be true for this deal on the date that cover starts.');
+    });
+
+    const LATEST_MANDATORY_CRITERIA_TOTAL_CRITERIONS = 5;
+    pages.beforeYouStart.mandatoryCriterion().should('have.length', LATEST_MANDATORY_CRITERIA_TOTAL_CRITERIONS);
+  });
+
   describe('When the `Mandatory criteria` form is submitted without confirming an answer', () => {
     it('should display validation error', () => {
       cy.createBSSSubmission(BANK1_MAKER1);
@@ -41,7 +64,7 @@ context('Red Line eligibility checking', () => {
     cy.url().should('eq', relative('/dashboard/deals/0'));
   });
 
-  it('A deal that passes red-line checks can progress to enter supply detaile', () => {
+  it('A deal that passes red-line checks can progress to enter supply contract details', () => {
     cy.createBSSSubmission(BANK1_MAKER1);
 
     pages.beforeYouStart.true().click();
