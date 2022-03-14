184a3a3637c9e03e6e79e0f775f0b4ce737394ab
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2022-03-14 21:16:35 +0000

    DTFS2-4703 : Currency properties (#788)
    * DTFS2-4703 : Currency properties
    
    * Update : `facilityValueInGBP` property has been updated to `valueInGBP`
    * Addition : `exchangeRate` property has been added, holding the mid-rate used during currency conversion.
    
    * DTFS2-4703 :  property added
    
    * DTFS2-4703 : Property reverted back
    
    * DTFS2-4703 : Property path update
    
    Co-authored-by: Abhi Markan <amarkan@ukexportfinance.gov.uk>

--- azure-functions/acbs-function/mappings/facility/helpers/get-facility-currency-exchange-rate.js
+++ azure-functions/acbs-function/mappings/facility/helpers/get-facility-currency-exchange-rate.js
@@ -4,6 +4,6 @@
  * @param {Object} facility Facility object
  * @returns {Integer} Facility exchange rate
  */
-const getCurrencyExchangeRate = (facility) => Number(facility.facilitySnapshot.conversionRate);
+const getCurrencyExchangeRate = (facility) => Number(facility.tfm.exchangeRate);
 
 module.exports = getCurrencyExchangeRate;
