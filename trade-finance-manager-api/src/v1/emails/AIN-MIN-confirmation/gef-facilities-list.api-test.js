b2a1efb1bc03009e582b0b4b17b7b26c56195556
Tony Barnes <tony@tonybarnes.me>
2022-03-21 16:56:10 +0000

    DTFS2-5453 - add facility coverEndDate to TFM - GEF AIN-MIN confirmation email (#829)

--- trade-finance-manager-api/src/v1/emails/AIN-MIN-confirmation/gef-facilities-list.api-test.js
+++ trade-finance-manager-api/src/v1/emails/AIN-MIN-confirmation/gef-facilities-list.api-test.js
@@ -72,6 +72,7 @@ describe('generate AIN/MIN confirmation email facilities list email variable/str
         bankReference: mockFacility.bankReference,
         hasBeenIssued: mapIssuedValue(mockFacility.hasBeenIssued),
         coverStartDate: format(Number(mockFacility.coverStartDate), 'do MMMM yyyy'),
+        coverEndDate: format(Number(mockFacility.coverEndDate), 'do MMMM yyyy'),
         value: mockFacility.value,
         currencyCode: mockFacility.currencyCode,
         interestPercentage: `${mockFacility.interestPercentage}%`,
