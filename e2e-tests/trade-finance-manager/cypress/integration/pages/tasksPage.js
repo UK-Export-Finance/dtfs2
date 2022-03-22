4896d07f8f45b981f64eb022bb4904a3d5f22501
ttbarnes <tony@tonybarnes.me>
2022-03-22 09:00:09 +0000

    DTFS2-5525 - TFM tasks - title selector with groupId

--- e2e-tests/trade-finance-manager/cypress/integration/pages/tasksPage.js
+++ e2e-tests/trade-finance-manager/cypress/integration/pages/tasksPage.js
@@ -6,7 +6,7 @@ const tasksPage = {
       cy.get(`[data-cy="task-group-${groupId}-task-${taskId}-row"]`).as('row');
       return {
         link: () => cy.get('@row').get(`[data-cy="task-table-row-group-${groupId}-task-${taskId}-link"]`),
-        title: () => cy.get('@row').get(`[data-cy="task-table-row-${taskId}-title"]`),
+        title: () => cy.get('@row').get(`[data-cy="task-table-row-group-${groupId}-task-${taskId}-title"]`),
         assignedTo: () => cy.get('@row').get(`[data-cy="task-table-row-group-${groupId}-task-${taskId}-user-full-name"]`),
         dateStarted: () => cy.get('@row').get(`[data-cy="task-table-row-group-${groupId}-task-${taskId}-date-started"]`),
         dateCompleted: () => cy.get('@row').get(`[data-cy="task-table-row-group-${groupId}-task-${taskId}-date-completed"]`),
