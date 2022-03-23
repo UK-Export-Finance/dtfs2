fe43a07739222b40be7ffd0c2d8f512e921db468
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-03-23 16:27:20 +0000

    DTFS2-5576 : Facility bugs

--- azure-functions/acbs-function/mappings/facility/helpers/get-loan-maximum-liability.js
+++ azure-functions/acbs-function/mappings/facility/helpers/get-loan-maximum-liability.js
@@ -2,22 +2,17 @@ const CONSTANTS = require('../../../constants');
 
 /**
  * `GEF` = 10% of amount.
- * `Loan` = disbursement amount, else `amount`.
+ * `Loan` or `Bond` = amount
  * @param {Float} amount Facility UKEF exposure
  * @param {Object} facility Facility
  * @param {Object} dealType Deal type
  */
 const getLoanMaximumLiability = (amount, facility, dealType) => {
   let ukefExposure;
-  // GEF
-  if (dealType === CONSTANTS.PRODUCT.TYPE.GEF) {
-    ukefExposure = amount * 0.10;
-  } else {
-  // BSS/EWCS
-    ukefExposure = facility.facilitySnapshot.type === CONSTANTS.FACILITY.FACILITY_TYPE.LOAN
-      ? facility.facilitySnapshot.disbursementAmount
-      : amount;
-  }
+
+  ukefExposure = dealType === CONSTANTS.PRODUCT.TYPE.GEF
+    ? ukefExposure = amount * 0.10 // GEF
+    : amount; // BSS/EWCS
 
   return Number(Number(ukefExposure).toFixed(2));
 };
--- azure-functions/acbs-function/mappings/facility/helpers/get-premium-frequency-code.js
+++ azure-functions/acbs-function/mappings/facility/helpers/get-premium-frequency-code.js
@@ -1,25 +1,35 @@
 const mapFeeFrequency = require('./map-fee-frequency');
+const mapFeeType = require('./map-fee-type');
 const CONSTANTS = require('../../../constants');
 
+/**
+ * Fee frequency for the facility, if `At Maturity` then `Annually` else respective fee frequency code.
+ * @param {Object} facilitySnapshot Facility snapshot object
+ * @returns {String} Fee-frequency ACBS code
+ */
 const getPremiumFrequencyCode = (facilitySnapshot) => {
   const feeFrequency = mapFeeFrequency(facilitySnapshot);
+  const feeType = mapFeeType(facilitySnapshot);
+
+  if (feeType === CONSTANTS.FACILITY.FEE_TYPE.AT_MATURITY) {
+    return CONSTANTS.FACILITY.MASTER_FACILITY_FEE_FREQUENCY_ACBS_CODE.ANNUALLY;
+  }
 
   switch (feeFrequency) {
     case CONSTANTS.FACILITY.FEE_FREQUENCY.MONTHLY:
-      return '3';
+      return CONSTANTS.FACILITY.MASTER_FACILITY_FEE_FREQUENCY_ACBS_CODE.MONTHLY;
 
     case CONSTANTS.FACILITY.FEE_FREQUENCY.QUARTERLY:
-      return '2';
+      return CONSTANTS.FACILITY.MASTER_FACILITY_FEE_FREQUENCY_ACBS_CODE.QUARTERLY;
 
     case CONSTANTS.FACILITY.FEE_FREQUENCY.SEMI_ANNUALLY:
-      return '1';
+      return CONSTANTS.FACILITY.MASTER_FACILITY_FEE_FREQUENCY_ACBS_CODE.SEMI_ANNUALLY;
 
     case CONSTANTS.FACILITY.FEE_FREQUENCY.ANNUALLY:
-      return '4';
+      return CONSTANTS.FACILITY.MASTER_FACILITY_FEE_FREQUENCY_ACBS_CODE.ANNUALLY;
 
     default:
-      // Default to semi-annually until ACBS can handle facilities without fee frequency
-      return '1';
+      return CONSTANTS.FACILITY.MASTER_FACILITY_FEE_FREQUENCY_ACBS_CODE.ANNUALLY;
   }
 };
 
