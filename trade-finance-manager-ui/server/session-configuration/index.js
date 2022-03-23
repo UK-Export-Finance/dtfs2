d0a297a4ce672a1e6cdf4bf21927ee36807f9e94
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-03-23 14:10:47 +0000

    re-added redis

--- trade-finance-manager-ui/server/session-configuration/index.js
+++ trade-finance-manager-ui/server/session-configuration/index.js
@@ -1,10 +1,10 @@
 // Fail-safe fallback to a 256-bit random value:
 
 const crypto = require('crypto');
-// const session = require('express-session');
-// const redis = require('redis');
+const session = require('express-session');
+const redis = require('redis');
 
-// const RedisStore = require('connect-redis')(session);
+const RedisStore = require('connect-redis')(session);
 
 const sessionConfig = () => {
   const SESSION_SECRET = process.env.SESSION_SECRET || crypto.randomBytes(256 / 8).toString('hex');
@@ -18,33 +18,32 @@ const sessionConfig = () => {
   if (process.env.REDIS_HOSTNAME) {
     console.info(`Connecting to redis server: redis://${process.env.REDIS_HOSTNAME} `);
 
-    // TODO: Update Redis
-    // let redisOptions = {};
+    let redisOptions = {};
 
-    // if (process.env.REDIS_KEY) {
-    //   redisOptions = {
-    //     auth_pass: process.env.REDIS_KEY,
-    //     tls: { servername: process.env.REDIS_HOSTNAME },
-    //   };
-    // }
+    if (process.env.REDIS_KEY) {
+      redisOptions = {
+        auth_pass: process.env.REDIS_KEY,
+        tls: { servername: process.env.REDIS_HOSTNAME },
+      };
+    }
 
-    // const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOSTNAME, redisOptions);
+    const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOSTNAME, redisOptions);
 
-    // redisClient.on('error', (err) => {
-    //   console.info(`Unable to connect to Redis: ${process.env.REDIS_HOSTNAME}`, { err });
-    // });
+    redisClient.on('error', (err) => {
+      console.info(`Unable to connect to Redis: ${process.env.REDIS_HOSTNAME}`, { err });
+    });
 
-    // redisClient.on('ready', () => {
-    //   console.info('REDIS ready');
-    // });
+    redisClient.on('ready', () => {
+      console.info('REDIS ready');
+    });
 
-    // redisClient.on('connect', () => {
-    //   console.info('REDIS connected');
-    // });
+    redisClient.on('connect', () => {
+      console.info('REDIS connected');
+    });
 
-    // const sessionStore = new RedisStore({ client: redisClient });
+    const sessionStore = new RedisStore({ client: redisClient });
 
-    // sessionOptions.store = sessionStore;
+    sessionOptions.store = sessionStore;
   } else {
     console.error('No REDIS configured, using default MemoryStore');
   }
