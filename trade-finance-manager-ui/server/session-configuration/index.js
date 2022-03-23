2a83f58da498bb951f75b6b219d62781bd292b90
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-03-23 18:07:54 +0000

    Remove MGA links from Portal footer

--- trade-finance-manager-ui/server/session-configuration/index.js
+++ trade-finance-manager-ui/server/session-configuration/index.js
@@ -18,32 +18,32 @@ const sessionConfig = () => {
   if (process.env.REDIS_HOSTNAME) {
     console.info(`Connecting to redis server: redis://${process.env.REDIS_HOSTNAME} `);
 
-    let redisOptions = {};
+    const redisOptions = {};
 
-    if (process.env.REDIS_KEY) {
-      redisOptions = {
-        auth_pass: process.env.REDIS_KEY,
-        tls: { servername: process.env.REDIS_HOSTNAME },
-      };
-    }
+    // if (process.env.REDIS_KEY) {
+    //   redisOptions = {
+    //     auth_pass: process.env.REDIS_KEY,
+    //     tls: { servername: process.env.REDIS_HOSTNAME },
+    //   };
+    // }
 
-    const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOSTNAME, redisOptions);
+    // const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOSTNAME, redisOptions);
 
-    redisClient.on('error', (err) => {
-      console.info(`Unable to connect to Redis: ${process.env.REDIS_HOSTNAME}`, { err });
-    });
+    // redisClient.on('error', (err) => {
+    //   console.info(`Unable to connect to Redis: ${process.env.REDIS_HOSTNAME}`, { err });
+    // });
 
-    redisClient.on('ready', () => {
-      console.info('REDIS ready');
-    });
+    // redisClient.on('ready', () => {
+    //   console.info('REDIS ready');
+    // });
 
-    redisClient.on('connect', () => {
-      console.info('REDIS connected');
-    });
+    // redisClient.on('connect', () => {
+    //   console.info('REDIS connected');
+    // });
 
-    const sessionStore = new RedisStore({ client: redisClient });
+    // const sessionStore = new RedisStore({ client: redisClient });
 
-    sessionOptions.store = sessionStore;
+    // sessionOptions.store = sessionStore;
   } else {
     console.error('No REDIS configured, using default MemoryStore');
   }
