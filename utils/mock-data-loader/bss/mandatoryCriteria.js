3419372d2d7cb6ad4b5ae4c1bc28285ac24cb6f1
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-03-27 17:19:17 +0100

    Fixed the link to the Financial Dificulty Model on BSS MC page (#862)

--- utils/mock-data-loader/bss/mandatoryCriteria.js
+++ utils/mock-data-loader/bss/mandatoryCriteria.js
@@ -176,7 +176,7 @@ const MANDATORY_CRITERIA = [
         items: [
           {
             id: 4,
-            copy: 'The Bank Customer (to include both the Supplier and any Parent Obligor) is an <a href="/financial_difficulty_model_1.1.0.xlsx" class="govuk-link">Eligible Person</a>',
+            copy: 'The Bank Customer (to include both the Supplier and any Parent Obligor) is an <a href="/assets/files/financial_difficulty_model_1.1.0.xlsx" class="govuk-link">Eligible Person</a>',
           },
         ],
       },
