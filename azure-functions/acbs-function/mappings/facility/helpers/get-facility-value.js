15c1fee48faac8339edf1f2fbb614715cf0688b6
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-03-31 17:05:18 +0100

    DTFS2-5608 : Facility value

--- azure-functions/acbs-function/mappings/facility/helpers/get-facility-value.js
+++ azure-functions/acbs-function/mappings/facility/helpers/get-facility-value.js
@@ -1,12 +1,13 @@
 /**
  * Returns the facility value, if base currency then total facility value
- * else total GBP converted facility value.
+ * else total GBP converted facility value. If `GBP` in multi-currency facilities
+ * then facility value for specific GBP facility.
  * @param {Object} facility Facility object
  * @param {String | Boolean} baseCurrency Deal base currency
  * @returns {Float} Facility value
  */
 const getFacilityValue = (facility, baseCurrency) => {
-  const amount = baseCurrency
+  const amount = baseCurrency || !facility.tfm.facilityValueInGBP
     ? facility.facilitySnapshot.value
     : facility.tfm.facilityValueInGBP;
 
