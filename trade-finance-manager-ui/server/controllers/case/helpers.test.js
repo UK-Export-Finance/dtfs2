96e01babe873cbe5a33140e27b23872d54237d80
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2022-04-14 15:26:16 +0100

    feat: added amendment button and tests (#937)

--- trade-finance-manager-ui/server/controllers/case/helpers.test.js
+++ trade-finance-manager-ui/server/controllers/case/helpers.test.js
@@ -1,8 +1,10 @@
-/* eslint-disable no-underscore-dangle */
 import {
   getGroup,
   getTask,
+  showAmendmentButton,
 } from './helpers';
+
+const CONSTANTS = require('../../constants');
 
 describe('case - helpers', () => {
   describe('getGroup', () => {
@@ -61,6 +63,92 @@ describe('case - helpers', () => {
       expect(result).toEqual(null);
     });
   });
-});
 
-/* eslint-enable no-underscore-dangle */
+  describe('showAmendmentButton()', () => {
+    const deal = {
+      dealSnapshot: {},
+      tfm: {},
+    };
+    let userTeam;
+    it('return true if AIN and confirmed and PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.AIN;
+      userTeam = ['PIM'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.CONFIRMED;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(true);
+    });
+
+    it('return false if AIN and confirmed and not PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.AIN;
+      userTeam = ['UNDERWRITER_MANAGERS'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.CONFIRMED;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(false);
+    });
+
+    it('return false if AIN and not confirmed and PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.AIN;
+      userTeam = ['PIM'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.UKEF_APPROVED_WITH_CONDITIONS;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(false);
+    });
+
+    it('return true if MIN and confirmed and PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.MIN;
+      userTeam = ['PIM'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.CONFIRMED;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(true);
+    });
+
+    it('return false if MIN and confirmed and not PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.MIN;
+      userTeam = ['UNDERWRITER_MANAGERS'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.CONFIRMED;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(false);
+    });
+
+    it('return false if MIN and not confirmed and PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.MIN;
+      userTeam = ['PIM'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.UKEF_APPROVED_WITH_CONDITIONS;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(false);
+    });
+
+    it('return false if MIA and confirmed and PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.MIA;
+      userTeam = ['PIM'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.CONFIRMED;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(false);
+    });
+
+    it('return false if MIA and confirmed and not PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.MIA;
+      userTeam = ['UNDERWRITER_MANAGERS'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.CONFIRMED;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(false);
+    });
+
+    it('return false if MIA and not confirmed and PIM', () => {
+      deal.dealSnapshot.submissionType = CONSTANTS.DEAL.SUBMISSION_TYPE.MIA;
+      userTeam = ['PIM'];
+      deal.tfm.stage = CONSTANTS.DEAL.DEAL_STAGE.UKEF_APPROVED_WITH_CONDITIONS;
+
+      const result = showAmendmentButton(deal, userTeam);
+      expect(result).toEqual(false);
+    });
+  });
+});
