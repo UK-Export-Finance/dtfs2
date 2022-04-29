7aaf76884bcebda6f927f942ca1add00480c2b82
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-04-29 16:02:01 +0100

    feat(DTFS2-4288): add amendment request date. add `amendment` endpoints in central-api  (#974)

--- dtfs-central-api/src/constants/index.js
+++ dtfs-central-api/src/constants/index.js
@@ -1,9 +1,11 @@
 const DEALS = require('./deals');
 const FACILITIES = require('./facilities');
 const ROUTES = require('./routes');
+const AMENDMENT = require('./amendments.constant');
 
 module.exports = {
   DEALS,
   FACILITIES,
   ROUTES,
+  AMENDMENT
 };
