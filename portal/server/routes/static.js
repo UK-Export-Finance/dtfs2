d0b11e6e5657cb6850965a8b081dbfbf58efa8a9
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2022-05-03 13:02:37 +0100

    DTFS2-static : Security.txt file update (#975)
    security.txt static content updates:
    
    * Location
    * Policy URL
    * Contact URL
    * Last updated timestamp

--- portal/server/routes/static.js
+++ portal/server/routes/static.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/security.txt', (req, res) => {
+router.get('/.well-known/security.txt', (req, res) => {
   res.type('text/plain');
 
   res.write('# DTFS Portal 2.0');
   res.write('\n');
-  res.write('Policy: https://www.gov.uk/help/report-vulnerability');
+  res.write('Policy: https://www.gov.uk/guidance/report-a-vulnerability-on-a-ukef-system');
   res.write('\n');
-  res.write('Contact: https://hackerone.com/44c348eb-e030-4273-b445-d4a2f6f83ba8/embedded_submissions/new');
+  res.write('Contact: https://hackerone.com/7af14fd9-fe4e-4f39-bea1-8f8a364061b8/embedded_submissions/new');
   res.write('\n\n');
   res.write('Contact: https://get-a-guarantee-for-export-finance.service.gov.uk/feedback');
   res.write('\n');
@@ -19,7 +19,7 @@ router.get('/security.txt', (req, res) => {
   res.write('\n');
   res.write('Hiring: https://www.civilservicejobs.service.gov.uk/');
   res.write('\n\n');
-  res.write('Last-Updated: 23-03-2022 13:22:00+00:00 GMT');
+  res.write('Last-Updated: 03-05-2022 10:31:01 UTC');
 
   res.send();
 });
