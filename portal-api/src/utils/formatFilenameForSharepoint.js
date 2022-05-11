de6430b3f732d95ed471425fb3875167c81fe2b7
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-05-11 20:58:44 +0100

    fix: formatted the eStore file name and exporter name (#978)

--- portal-api/src/utils/formatFilenameForSharepoint.js
+++ portal-api/src/utils/formatFilenameForSharepoint.js
@@ -4,17 +4,15 @@ valid characters listed below:
 
 1. alphabetic characters A through Z
 2. numeric characters 0 through 9
-3. -
-4. _
 
 see
-https://ukef-dtfs.atlassian.net/jira/software/projects/DTFS2/boards/2?assignee=5e42c0d090dfb70c9e60741b&selectedIssue=DTFS2-509 // eslint-disable-line max-len
+https://ukef-dtfs.atlassian.net/browse/DTFS2-509
 */
 
 const formatFilenameForSharepoint = (filenameWithExtension) => {
   const [extension] = filenameWithExtension.match(/\.[^/.]+/g).reverse();
   const filenameWithoutExtension = filenameWithExtension.replace(/\.[^/.]+$/, '');
-  const sanitisedFilename = filenameWithoutExtension.replace(/[^0-9a-zA-Z_-\S]|,/g, '_');
+  const sanitisedFilename = filenameWithoutExtension.replace(/[\W_]+/g, '_');
 
   return `${sanitisedFilename}${extension}`;
 };
