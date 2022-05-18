087344ad85c377e83e3ba3fe1b7368492e05ce3d
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-05-18 13:00:08 +0100

    feat: Added `facilityValue` and `coverEndDate` to amendments. (#979)

--- trade-finance-manager-ui/server/controllers/case/amendments/amendmentEffectiveDate.controller.js
+++ trade-finance-manager-ui/server/controllers/case/amendments/amendmentEffectiveDate.controller.js
@@ -1,74 +1,72 @@
 const { format, fromUnixTime } = require('date-fns');
 const api = require('../../../api');
-const { amendmentEffectiveDateValidation } = require('./validation/amendmentEffectiveDate.validate');
+const { effectiveDateValidation } = require('./validation/amendmentEffectiveDate.validate');
 const { AMENDMENT_STATUS } = require('../../../constants/amendments');
 
 const getAmendmentEffectiveDate = async (req, res) => {
-  try {
-    const { facilityId, amendmentId } = req.params;
-    const { data: amendment, status } = await api.getAmendmentById(facilityId, amendmentId);
-
-    if (status !== 200) {
-      return res.redirect('/not-found');
-    }
-
-    const { dealId } = amendment;
-    let amendmentEffectiveDateDay = '';
-    let amendmentEffectiveDateMonth = '';
-    let amendmentEffectiveDateYear = '';
-
-    const isEditable = amendment.status === AMENDMENT_STATUS.IN_PROGRESS;
-    if (amendment.amendmentEffectiveDate) {
-      amendmentEffectiveDateDay = format(fromUnixTime(amendment.amendmentEffectiveDate), 'dd');
-      amendmentEffectiveDateMonth = format(fromUnixTime(amendment.amendmentEffectiveDate), 'M');
-      amendmentEffectiveDateYear = format(fromUnixTime(amendment.amendmentEffectiveDate), 'yyyy');
-    }
-
-    return res.render('case/amendments/amendment-effective-date.njk', {
-      dealId,
-      facilityId,
-      isEditable,
-      amendmentEffectiveDateDay,
-      amendmentEffectiveDateMonth,
-      amendmentEffectiveDateYear,
-    });
-  } catch (err) {
-    console.error('Unable to get the amendment effective date page', { err });
-    return res.redirect('/not-found');
-  }
-};
-const postAmendmentEffectiveDate = async (req, res) => {
   const { facilityId, amendmentId } = req.params;
   const { data: amendment, status } = await api.getAmendmentById(facilityId, amendmentId);
-  if (status === 200) {
-    const { amendmentEffectiveDate, errorsObject, amendmentEffectiveDateErrors } = await amendmentEffectiveDateValidation(req.body);
 
-    if (amendmentEffectiveDateErrors.length > 0) {
-      const isEditable = amendment.status === AMENDMENT_STATUS.IN_PROGRESS;
-      return res.render('case/amendments/amendment-effective-date.njk', {
-        dealId: amendment.dealId,
-        facilityId,
-        isEditable,
-        amendmentEffectiveDateDay: errorsObject.amendmentEffectiveDateDay,
-        amendmentEffectiveDateMonth: errorsObject.amendmentEffectiveDateMonth,
-        amendmentEffectiveDateYear: errorsObject.amendmentEffectiveDateYear,
-        errors: errorsObject.errors,
-      });
-    }
-
-    try {
-      const payload = { amendmentEffectiveDate };
-      const update = await api.updateAmendment(facilityId, amendmentId, payload);
-
-      if (update) {
-        return res.redirect(`/case/${amendment.dealId}/facility/${facilityId}/amendment/${amendmentId}/amendment-options`);
-      }
-      return res.redirect(`/case/${amendment.dealId}/facility/${facilityId}/amendment/${amendmentId}/amendment-effective-date`);
-    } catch (err) {
-      console.error('There was a problem adding the amendment effective date %O', { response: err?.response?.data });
-    }
+  if (status !== 200) {
+    return res.redirect('/not-found');
   }
-  return res.redirect(`/case/${amendment.dealId}/facility/${facilityId}#amendments`);
+
+  const { dealId } = amendment;
+  let effectiveDateDay = '';
+  let effectiveDateMonth = '';
+  let effectiveDateYear = '';
+
+  const isEditable = amendment.status === AMENDMENT_STATUS.IN_PROGRESS;
+  if (amendment.effectiveDate) {
+    effectiveDateDay = format(fromUnixTime(amendment.effectiveDate), 'dd');
+    effectiveDateMonth = format(fromUnixTime(amendment.effectiveDate), 'M');
+    effectiveDateYear = format(fromUnixTime(amendment.effectiveDate), 'yyyy');
+  }
+
+  return res.render('case/amendments/amendment-effective-date.njk', {
+    dealId,
+    facilityId,
+    isEditable,
+    effectiveDateDay,
+    effectiveDateMonth,
+    effectiveDateYear,
+    user: req.session.user,
+  });
+};
+
+const postAmendmentEffectiveDate = async (req, res) => {
+  const { facilityId, amendmentId } = req.params;
+  const { data: amendment } = await api.getAmendmentById(facilityId, amendmentId);
+  const { effectiveDate, errorsObject, effectiveDateErrors } = await effectiveDateValidation(req.body);
+  const { dealId } = amendment;
+
+  if (effectiveDateErrors.length) {
+    const isEditable = amendment.status === AMENDMENT_STATUS.IN_PROGRESS;
+    return res.render('case/amendments/amendment-effective-date.njk', {
+      dealId: amendment.dealId,
+      facilityId,
+      isEditable,
+      effectiveDateDay: errorsObject.effectiveDateDay,
+      effectiveDateMonth: errorsObject.effectiveDateMonth,
+      effectiveDateYear: errorsObject.effectiveDateYear,
+      errors: errorsObject.errors,
+      user: req.session.user,
+    });
+  }
+
+  try {
+    const payload = { effectiveDate };
+    const { status } = await api.updateAmendment(facilityId, amendmentId, payload);
+
+    if (status === 200) {
+      return res.redirect(`/case/${amendment.dealId}/facility/${facilityId}/amendment/${amendmentId}/amendment-options`);
+    }
+    console.error('Unable to update the amendment effective date');
+    return res.redirect(`/case/${amendment.dealId}/facility/${facilityId}/amendment/${amendmentId}/amendment-effective-date`);
+  } catch (err) {
+    console.error('There was a problem adding the amendment effective date %O', { response: err?.response?.data });
+    return res.redirect(`/case/${dealId}/facility/${facilityId}#amendments`);
+  }
 };
 
 module.exports = { getAmendmentEffectiveDate, postAmendmentEffectiveDate };
