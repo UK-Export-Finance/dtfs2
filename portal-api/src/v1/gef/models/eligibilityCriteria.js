97bbfee4c03f7080b5d39ba11cbd46b32f20670d
Gabriel Ignat <gabriel.ignat@hotmail.com>
2022-05-18 14:27:02 +0100

    removed `product` property

--- portal-api/src/v1/gef/models/eligibilityCriteria.js
+++ portal-api/src/v1/gef/models/eligibilityCriteria.js
@@ -1,7 +1,6 @@
 class EligibilityCriteria {
   constructor(req) {
     this.version = req.version;
-    this.product = req.product;
     this.isInDraft = req.isInDraft;
     this.criteria = req.criteria;
     this.createdAt = Date.now();
