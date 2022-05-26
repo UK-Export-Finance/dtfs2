a0669834d7d39b19ceb6947a587b6b03f36fdb46
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-05-26 17:31:31 +0100

    DTFS2-5777 : Deal stage

--- utils/data-migration/helpers/deal.js
+++ utils/data-migration/helpers/deal.js
@@ -9,18 +9,23 @@ const CONSTANTS = require('../constant');
  * @returns {String} TFM deal status if `Submitted` otherwise an empty string.
  */
 const getDealTfmStage = (deal) => {
-  if (deal.status === CONSTANTS.DEAL.PORTAL_STATUS.SUBMITTED_TO_UKEF) {
+  if (deal.status === CONSTANTS.DEAL.PORTAL_STATUS.SUBMITTED_TO_UKEF
+    || deal.status === CONSTANTS.DEAL.PORTAL_STATUS.UKEF_ACKNOWLEDGED) {
     if (deal.submissionType === CONSTANTS.DEAL.SUBMISSION_TYPE.AIN) {
-      return CONSTANTS.DEALS.DEAL_STAGE_TFM.CONFIRMED;
+      return CONSTANTS.DEAL.TFM_STATUS.CONFIRMED;
     }
 
     if (deal.submissionType === CONSTANTS.DEAL.SUBMISSION_TYPE.MIA) {
-      return CONSTANTS.DEALS.DEAL_STAGE_TFM.APPLICATION;
+      return CONSTANTS.DEAL.TFM_STATUS.APPLICATION;
     }
 
     if (deal.submissionType === CONSTANTS.DEAL.SUBMISSION_TYPE.MIN) {
-      return CONSTANTS.DEALS.DEAL_STAGE_TFM.CONFIRMED;
+      return CONSTANTS.DEAL.TFM_STATUS.CONFIRMED;
     }
+  }
+
+  if (deal.status === CONSTANTS.DEAL.PORTAL_STATUS.ABANDONED) {
+    return CONSTANTS.DEAL.TFM_STATUS.ABANDONED;
   }
 
   return '';
