52cfc44a93c8693ef1141230b1df3516481dbbd4
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-06-14 09:29:14 +0100

    DTFS2-5777 : TFM GEF Migration flag

--- trade-finance-manager-api/src/v1/controllers/deal.submit.controller.js
+++ trade-finance-manager-api/src/v1/controllers/deal.submit.controller.js
@@ -122,7 +122,7 @@ const submitDealAfterUkefIds = async (dealId, dealType, checker) => {
     return api.updateDeal(dealId, updatedDealWithCreateEstore);
   }
 
-  if (dealHasBeenResubmit) {
+  if (dealHasBeenResubmit || migrationScript) {
     const { tfm: tfmDeal } = await findOneTfmDeal(dealId);
 
     /**
