0296a3c67c921869da5e95959d8a1637047a3edc
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2022-06-19 14:31:30 +0100

    feat(DTFS2-5856-5857-5842-5845): Amendments - UW and bank decision emails (#1021)

---
+++ trade-finance-manager-api/src/v1/__mocks__/amendmentVariables.js
@@ -0,0 +1,189 @@
+const { DEALS: { AMENDMENT_UW_DECISION } } = require('../../constants');
+
+const ukefDealId = '3333';
+const ukefFacilityId = '4444';
+const facilityId = '1111';
+const amendmentId = '2222';
+const conditions = 'ABCD';
+const declined = 'EFGH';
+
+const approvedWithoutConditionsBothAmendments = {
+  user: {
+    firstname: 'Bob',
+    surname: 'Smith',
+    email: 'test@test.com',
+  },
+  dealSnapshot: {
+    ukefDealId,
+    exporter: {
+      companyName: 'Tester',
+    },
+    bankInternalRefName: 'UKEF',
+  },
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.APPROVED_WITHOUT_CONDITIONS,
+      coverEndDate: AMENDMENT_UW_DECISION.APPROVED_WITHOUT_CONDITIONS,
+      managersDecisionEmail: true,
+    },
+    ukefFacilityId,
+  },
+  facilityId,
+  amendmentId,
+};
+
+const approvedWithoutConditionsOneAmendment = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.APPROVED_WITHOUT_CONDITIONS,
+      managersDecisionEmail: true,
+    },
+    ukefFacilityId,
+  },
+};
+
+const approvedWithConditionsBothAmendments = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS,
+      coverEndDate: AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS,
+      managersDecisionEmail: true,
+      conditions,
+    },
+    ukefFacilityId,
+  },
+};
+
+const approvedWithConditionsOneAmendment = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS,
+      managersDecisionEmail: true,
+      conditions,
+    },
+    ukefFacilityId,
+  },
+};
+
+const approvedWithWithoutConditionsBothAmendments = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS,
+      coverEndDate: AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS,
+      managersDecisionEmail: true,
+      conditions,
+    },
+    ukefFacilityId,
+  },
+};
+
+const approvedWithConditionsDeclined = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS,
+      coverEndDate: AMENDMENT_UW_DECISION.DECLINED,
+      managersDecisionEmail: true,
+      conditions,
+      declined,
+    },
+    ukefFacilityId,
+  },
+};
+
+const approvedWithConditionsDeclinedSwapped = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.DECLINED,
+      coverEndDate: AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS,
+      managersDecisionEmail: true,
+      conditions,
+      declined,
+    },
+    ukefFacilityId,
+  },
+};
+
+const approvedWithoutConditionsDeclined = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.APPROVED_WITHOUT_CONDITIONS,
+      coverEndDate: AMENDMENT_UW_DECISION.DECLINED,
+      managersDecisionEmail: true,
+      declined,
+    },
+    ukefFacilityId,
+  },
+};
+
+const approvedWithoutConditionsDeclinedSwapped = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.DECLINED,
+      coverEndDate: AMENDMENT_UW_DECISION.APPROVED_WITHOUT_CONDITIONS,
+      managersDecisionEmail: true,
+      declined,
+    },
+    ukefFacilityId,
+  },
+};
+
+const declinedBothAmendments = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.DECLINED,
+      coverEndDate: AMENDMENT_UW_DECISION.DECLINED,
+      managersDecisionEmail: true,
+      declined,
+    },
+    ukefFacilityId,
+  },
+};
+
+const declinedOneAmendment = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: AMENDMENT_UW_DECISION.DECLINED,
+      managersDecisionEmail: true,
+      declined,
+    },
+    ukefFacilityId,
+  },
+};
+
+const wrongAmendments = {
+  ...approvedWithoutConditionsBothAmendments,
+  amendment: {
+    ukefDecision: {
+      value: 'test',
+      coverEndDate: 'test',
+      managersDecisionEmail: true,
+      declined,
+    },
+    ukefFacilityId,
+  },
+};
+
+module.exports = {
+  approvedWithoutConditionsBothAmendments,
+  approvedWithoutConditionsOneAmendment,
+  approvedWithConditionsBothAmendments,
+  approvedWithConditionsOneAmendment,
+  approvedWithWithoutConditionsBothAmendments,
+  approvedWithConditionsDeclined,
+  approvedWithConditionsDeclinedSwapped,
+  approvedWithoutConditionsDeclined,
+  approvedWithoutConditionsDeclinedSwapped,
+  declinedBothAmendments,
+  declinedOneAmendment,
+  wrongAmendments,
+};
--- trade-finance-manager-api/src/v1/helpers/amendment.helpers.js
+++ trade-finance-manager-api/src/v1/helpers/amendment.helpers.js
@@ -1,27 +1,210 @@
 const api = require('../api');
 const sendTfmEmail = require('../controllers/send-tfm-email');
-const CONSTANTS = require('../../constants');
-const { amendmentEmailVariables } = require('../emails/amendments/automatic-approval-email-variables');
+const { AMENDMENT_UW_DECISION, AMENDMENT_BANK_DECISION } = require('../../constants/deals');
+const EMAIL_TEMPLATE_IDS = require('../../constants/email-template-ids');
+const { automaticAmendmentEmailVariables } = require('../emails/amendments/automatic-approval-email-variables');
+const {
+  approvedWithWithoutConditionsDecision,
+  approvedWithConditionsDeclinedDecision,
+  approvedWithoutConditionsDeclinedDecision,
+  declinedDecision,
+  banksDecisionEmailVariables,
+} = require('../emails/amendments/manual-amendment-decision-email-variables');
 
+// checks if amendment exists and if eligible to send email
 const amendmentEmailEligible = (amendment) => {
-  if (amendment && (amendment?.automaticApprovalEmail)) {
+  if (amendment && (amendment?.automaticApprovalEmail || amendment?.ukefDecision?.managersDecisionEmail || amendment?.bankDecision?.banksDecisionEmail)) {
+    return true;
+  }
+  return false;
+};
+
+const isApprovedWithConditions = (ukefDecision) => {
+  const { value, coverEndDate } = ukefDecision;
+
+  if (value === AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS || coverEndDate === AMENDMENT_UW_DECISION.APPROVED_WITH_CONDITIONS) {
+    return true;
+  }
+  return false;
+};
+
+// checks if any are declined
+const isDeclined = (ukefDecision) => {
+  const { value, coverEndDate } = ukefDecision;
+
+  if (value === AMENDMENT_UW_DECISION.DECLINED || coverEndDate === AMENDMENT_UW_DECISION.DECLINED) {
+    return true;
+  }
+  return false;
+};
+
+// checks if value or coverEndDate are approved without conditions
+const isApprovedWithoutConditions = (ukefDecision) => {
+  const { value, coverEndDate } = ukefDecision;
+
+  if (value === AMENDMENT_UW_DECISION.APPROVED_WITHOUT_CONDITIONS || coverEndDate === AMENDMENT_UW_DECISION.APPROVED_WITHOUT_CONDITIONS) {
     return true;
   }
   return false;
 };
 
 const sendAutomaticAmendmentEmail = async (amendmentVariables) => {
-  const { user, dealSnapshot, amendment, facilityId, amendmentId } = amendmentVariables;
+  const { user, facilityId, amendmentId } = amendmentVariables;
+  const templateId = EMAIL_TEMPLATE_IDS.AUTOMATIC_AMENDMENT;
+  const emailVariables = automaticAmendmentEmailVariables(amendmentVariables);
 
-  const emailResponse = await sendTfmEmail(
-    CONSTANTS.EMAIL_TEMPLATE_IDS.AUTOMATIC_AMENDMENT,
-    user.email,
-    amendmentEmailVariables(amendment, dealSnapshot, user),
-  );
+  const emailResponse = await sendTfmEmail(templateId, user.email, emailVariables);
   // if successful, then updates flag to say email has been sent
   if (emailResponse) {
     await api.updateFacilityAmendment(facilityId, amendmentId, { automaticApprovalEmailSent: true });
   }
 };
 
-module.exports = { amendmentEmailEligible, sendAutomaticAmendmentEmail };
+// updates flag if managers decision email sent so not sent again
+const managersDecisionUpdateEmailConfirmation = async (facilityId, amendmentId) => {
+  const payload = {
+    ukefDecision: {
+      managersDecisionEmailSent: true,
+    },
+  };
+  await api.updateFacilityAmendment(facilityId, amendmentId, payload);
+};
+
+const emailApprovedWithWithoutConditions = async (amendmentVariables) => {
+  const { user, facilityId, amendmentId } = amendmentVariables;
+  const templateId = EMAIL_TEMPLATE_IDS.MANUAL_AMENDMENT_DECISION_APPROVED_W_CONDITIONS;
+  const emailVariables = approvedWithWithoutConditionsDecision(amendmentVariables);
+
+  const emailResponse = await sendTfmEmail(templateId, user.email, emailVariables);
+  // if successful, then updates flag to say email has been sent
+  if (emailResponse) {
+    await managersDecisionUpdateEmailConfirmation(facilityId, amendmentId);
+  }
+};
+
+const emailApprovedWithoutConditions = async (amendmentVariables) => {
+  const { user, facilityId, amendmentId } = amendmentVariables;
+  const templateId = EMAIL_TEMPLATE_IDS.MANUAL_AMENDMENT_DECISION_APPROVED_WO_CONDITIONS;
+  const emailVariables = approvedWithWithoutConditionsDecision(amendmentVariables);
+
+  const emailResponse = await sendTfmEmail(templateId, user.email, emailVariables);
+  // if successful, then updates flag to say email has been sent
+  if (emailResponse) {
+    await managersDecisionUpdateEmailConfirmation(facilityId, amendmentId);
+  }
+};
+
+const emailApprovedWithConditionsDeclined = async (amendmentVariables) => {
+  const { user, facilityId, amendmentId } = amendmentVariables;
+  const templateId = EMAIL_TEMPLATE_IDS.MANUAL_AMENDMENT_DECISION_APPROVED_W_CONDITIONS_DECLINED;
+  const emailVariables = approvedWithConditionsDeclinedDecision(amendmentVariables);
+
+  const emailResponse = await sendTfmEmail(templateId, user.email, emailVariables);
+  // if successful, then updates flag to say email has been sent
+  if (emailResponse) {
+    await managersDecisionUpdateEmailConfirmation(facilityId, amendmentId);
+  }
+};
+
+const emailApprovedWithoutConditionsDeclined = async (amendmentVariables) => {
+  const { user, facilityId, amendmentId } = amendmentVariables;
+  const templateId = EMAIL_TEMPLATE_IDS.MANUAL_AMENDMENT_DECISION_APPROVED_WO_CONDITIONS_DECLINED;
+  const emailVariables = approvedWithoutConditionsDeclinedDecision(amendmentVariables);
+
+  const emailResponse = await sendTfmEmail(templateId, user.email, emailVariables);
+  // if successful, then updates flag to say email has been sent
+  if (emailResponse) {
+    await managersDecisionUpdateEmailConfirmation(facilityId, amendmentId);
+  }
+};
+
+const emailDeclined = async (amendmentVariables) => {
+  const { user, facilityId, amendmentId } = amendmentVariables;
+  const templateId = EMAIL_TEMPLATE_IDS.MANUAL_AMENDMENT_DECISION_DECLINED;
+  const emailVariables = declinedDecision(amendmentVariables);
+
+  const emailResponse = await sendTfmEmail(templateId, user.email, emailVariables);
+  // if successful, then updates flag to say email has been sent
+  if (emailResponse) {
+    await managersDecisionUpdateEmailConfirmation(facilityId, amendmentId);
+  }
+};
+
+const sendManualDecisionAmendmentEmail = async (amendmentVariables) => {
+  const { amendment } = amendmentVariables;
+  const { ukefDecision } = amendment;
+
+  try {
+    // if one is approved with conditions and not declined
+    if (isApprovedWithConditions(ukefDecision) && !isDeclined(ukefDecision)) {
+      await emailApprovedWithWithoutConditions(amendmentVariables);
+
+      // if only approved without conditions (and not declined or with conditions)
+    } else if (isApprovedWithoutConditions(ukefDecision) && !isDeclined(ukefDecision) && !isApprovedWithConditions(ukefDecision)) {
+      await emailApprovedWithoutConditions(amendmentVariables);
+
+      // if approved with conditions and declined only
+    } else if (isApprovedWithConditions(ukefDecision) && isDeclined(ukefDecision) && !isApprovedWithoutConditions(ukefDecision)) {
+      await emailApprovedWithConditionsDeclined(amendmentVariables);
+
+      // if approved without conditions and declined only
+    } else if (isApprovedWithoutConditions(ukefDecision) && isDeclined(ukefDecision) && !isApprovedWithConditions(ukefDecision)) {
+      await emailApprovedWithoutConditionsDeclined(amendmentVariables);
+
+      // if declined only
+    } else if (isDeclined(ukefDecision) && !isApprovedWithConditions(ukefDecision) && !isApprovedWithoutConditions(ukefDecision)) {
+      await emailDeclined(amendmentVariables);
+    } else {
+      console.error('Incorrect ukefDecision passed for manual amendment email');
+    }
+  } catch (error) {
+    console.error('Error sending manual amendment underwriter decision email', { error });
+  }
+};
+
+// updates flag if managers decision email sent so not sent again
+const banksDecisionUpdateEmailConfirmation = async (facilityId, amendmentId) => {
+  const payload = {
+    bankDecision: {
+      banksDecisionEmailSent: true,
+    },
+  };
+  await api.updateFacilityAmendment(facilityId, amendmentId, payload);
+};
+
+const emailBankDecision = async (amendmentVariables, templateId) => {
+  const { user, facilityId, amendmentId } = amendmentVariables;
+  const emailVariables = banksDecisionEmailVariables(amendmentVariables);
+
+  const emailResponse = await sendTfmEmail(templateId, user.email, emailVariables);
+  // if successful, then updates flag to say email has been sent
+  if (emailResponse) {
+    await banksDecisionUpdateEmailConfirmation(facilityId, amendmentId);
+  }
+};
+
+const sendManualBankDecisionEmail = async (amendmentVariables) => {
+  const { amendment } = amendmentVariables;
+  const { bankDecision } = amendment;
+
+  const { PROCEED, WITHDRAW } = AMENDMENT_BANK_DECISION;
+
+  try {
+    if (bankDecision.decision === PROCEED) {
+      await emailBankDecision(amendmentVariables, EMAIL_TEMPLATE_IDS.MANUAL_AMENDMENT_BANK_PROCEED);
+    } else if (bankDecision.decision === WITHDRAW) {
+      await emailBankDecision(amendmentVariables, EMAIL_TEMPLATE_IDS.MANUAL_AMENDMENT_BANK_WITHDRAW);
+    } else {
+      console.error('Incorrect bankDecision passed for manual amendment email');
+    }
+  } catch (error) {
+    console.error('Error sending manual amendment bank decision email', { error });
+  }
+};
+
+module.exports = {
+  amendmentEmailEligible,
+  sendAutomaticAmendmentEmail,
+  sendManualDecisionAmendmentEmail,
+  sendManualBankDecisionEmail,
+};
