afd15ac9a56784d9af7a1ec7b7f2cd2e6d8b8b05
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2022-06-30 17:50:44 +0100

    Revert "fix(DTFS2-5709): Conversion rate date bug (#1047)" (#1056)
    This reverts commit e966e0209ecb34ed7dd94fa4f42e248f4e725330.
    
    Co-authored-by: Abhi Markan <amarkan@ukexportfinance.gov.uk>

--- portal-api/src/constants/index.js
+++ portal-api/src/constants/index.js
@@ -3,7 +3,6 @@ const DEAL = require('./deal');
 const LOGINRESULTS = require('./login-results');
 const USER = require('./user');
 const EMAIL_TEMPLATE_IDS = require('./email-template-ids');
-const CURRENCY = require('./currency');
 
 module.exports = {
   FACILITIES,
@@ -11,5 +10,4 @@ module.exports = {
   LOGINRESULTS,
   USER,
   EMAIL_TEMPLATE_IDS,
-  CURRENCY
 };
--- portal-api/src/v1/validation/submission-details-rules/supply-contract-conversion-rate-date-is-required-for-non-GBP.js
+++ portal-api/src/v1/validation/submission-details-rules/supply-contract-conversion-rate-date-is-required-for-non-GBP.js
@@ -2,67 +2,63 @@ const moment = require('moment');
 
 const { orderNumber } = require('../../../utils/error-list-order-number');
 const { hasValue } = require('../../../utils/string');
-const { CURRENCY } = require('../../../constants/currency');
 
-module.exports = (submissionDetails, errorList, deal) => {
+module.exports = (submissionDetails, errorList) => {
   const newErrorList = { ...errorList };
 
-  // only run this validation if first submission - submissionDate does not exist on first submission
-  if (!deal?.details?.submissionDate) {
-    // check to see if we're a non-GBP currency
-    if (hasValue(submissionDetails.supplyContractCurrency) && submissionDetails.supplyContractCurrency.id !== CURRENCY.GBP) {
-      const day = submissionDetails['supplyContractConversionDate-day'];
-      const month = submissionDetails['supplyContractConversionDate-month'];
-      const year = submissionDetails['supplyContractConversionDate-year'];
+  // check to see if we're a non-GBP currency
+  if (hasValue(submissionDetails.supplyContractCurrency) && submissionDetails.supplyContractCurrency.id !== 'GBP') {
+    const day = submissionDetails['supplyContractConversionDate-day'];
+    const month = submissionDetails['supplyContractConversionDate-month'];
+    const year = submissionDetails['supplyContractConversionDate-year'];
 
-      // if we have all the values, check that the date..
-      if (hasValue(day) && hasValue(month) && hasValue(year)) {
-        const date = moment(`${year}-${month}-${day}`);
-        const now = moment(`${moment().format('YYYY')}-${moment().format('MM')}-${moment().format('DD')}`);
-        const thirtyDaysAgo = moment(`${moment().format('YYYY')}-${moment().format('MM')}-${moment().format('DD')}`).subtract(30, 'day');
+    // if we have all the values, check that the date..
+    if (hasValue(day) && hasValue(month) && hasValue(year)) {
+      const date = moment(`${year}-${month}-${day}`);
+      const now = moment(`${moment().format('YYYY')}-${moment().format('MM')}-${moment().format('DD')}`);
+      const thirtyDaysAgo = moment(`${moment().format('YYYY')}-${moment().format('MM')}-${moment().format('DD')}`).subtract(30, 'day');
 
-        // can't be in the future
-        if (date.isAfter(now)) {
-          newErrorList.supplyContractConversionDate = {
-            order: orderNumber(newErrorList),
-            text: 'Supply Contract conversion date cannot be in the future',
-          };
-        }
-
-        // can't be more than 30 days old
-        if (date.isBefore(thirtyDaysAgo)) {
-          newErrorList.supplyContractConversionDate = {
-            order: orderNumber(newErrorList),
-            text: 'Supply Contract conversion date cannot be more than 30 days in the past',
-          };
-        }
-      } else {
-      // if we don't have all the values, raise an error against the date..
+      // can't be in the future
+      if (date.isAfter(now)) {
         newErrorList.supplyContractConversionDate = {
           order: orderNumber(newErrorList),
-          text: 'Supply Contract conversion date is required for non-GBP currencies',
+          text: 'Supply Contract conversion date cannot be in the future',
         };
+      }
 
-        // if we have one or more fields, but not all the fields - flag the missing fields as required
-        if (hasValue(day) || hasValue(month) || hasValue(year)) {
-          if (!hasValue(day)) {
-            newErrorList['supplyContractConversionDate-day'] = {
-              order: orderNumber(newErrorList),
-              text: 'Supply Contract conversion date Day is required for non-GBP currencies',
-            };
-          }
-          if (!hasValue(month)) {
-            newErrorList['supplyContractConversionDate-month'] = {
-              order: orderNumber(newErrorList),
-              text: 'Supply Contract conversion date Month is required for non-GBP currencies',
-            };
-          }
-          if (!hasValue(year)) {
-            newErrorList['supplyContractConversionDate-year'] = {
-              order: orderNumber(newErrorList),
-              text: 'Supply Contract conversion date Year is required for non-GBP currencies',
-            };
-          }
+      // can't be more than 30 days old
+      if (date.isBefore(thirtyDaysAgo)) {
+        newErrorList.supplyContractConversionDate = {
+          order: orderNumber(newErrorList),
+          text: 'Supply Contract conversion date cannot be more than 30 days in the past',
+        };
+      }
+    } else {
+      // if we don't have all the values, raise an error against the date..
+      newErrorList.supplyContractConversionDate = {
+        order: orderNumber(newErrorList),
+        text: 'Supply Contract conversion date is required for non-GBP currencies',
+      };
+
+      // if we have one or more fields, but not all the fields - flag the missing fields as required
+      if (hasValue(day) || hasValue(month) || hasValue(year)) {
+        if (!hasValue(day)) {
+          newErrorList['supplyContractConversionDate-day'] = {
+            order: orderNumber(newErrorList),
+            text: 'Supply Contract conversion date Day is required for non-GBP currencies',
+          };
+        }
+        if (!hasValue(month)) {
+          newErrorList['supplyContractConversionDate-month'] = {
+            order: orderNumber(newErrorList),
+            text: 'Supply Contract conversion date Month is required for non-GBP currencies',
+          };
+        }
+        if (!hasValue(year)) {
+          newErrorList['supplyContractConversionDate-year'] = {
+            order: orderNumber(newErrorList),
+            text: 'Supply Contract conversion date Year is required for non-GBP currencies',
+          };
         }
       }
     }
