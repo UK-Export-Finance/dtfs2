db5a2718e5cf9126baed46728fe4b5b7f7ba54ee
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-07-04 12:03:06 +0100

    underwriterManagersDecision property check

--- trade-finance-manager-api/src/v1/controllers/should-update-deal-from-MIA-to-MIN.js
+++ trade-finance-manager-api/src/v1/controllers/should-update-deal-from-MIA-to-MIN.js
@@ -3,12 +3,14 @@ const CONSTANTS = require('../../constants');
 const shouldUpdateDealFromMIAtoMIN = (deal, tfmDeal) => {
   const isMIA = deal.submissionType === CONSTANTS.DEALS.SUBMISSION_TYPE.MIA;
 
-  const hasApprovedDecision = (tfmDeal.underwriterManagersDecision
+  if (Object.prototype.hasOwnProperty.call(tfmDeal, 'underwriterManagersDecision')) {
+    const hasApprovedDecision = (tfmDeal.underwriterManagersDecision
     && (tfmDeal.underwriterManagersDecision.decision === CONSTANTS.DEALS.DEAL_STAGE_TFM.UKEF_APPROVED_WITH_CONDITIONS
     || tfmDeal.underwriterManagersDecision.decision === CONSTANTS.DEALS.DEAL_STAGE_TFM.UKEF_APPROVED_WITHOUT_CONDITIONS));
 
-  if (isMIA && hasApprovedDecision) {
-    return true;
+    if (isMIA && hasApprovedDecision) {
+      return true;
+    }
   }
 
   return false;
