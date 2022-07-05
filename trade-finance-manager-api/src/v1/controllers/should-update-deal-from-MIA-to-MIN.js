fcb10d625eb716335da2b9f027949742c51068e0
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-07-05 15:06:45 +0100

    DTFS2-bug : Property check

--- trade-finance-manager-api/src/v1/controllers/should-update-deal-from-MIA-to-MIN.js
+++ trade-finance-manager-api/src/v1/controllers/should-update-deal-from-MIA-to-MIN.js
@@ -3,7 +3,7 @@ const CONSTANTS = require('../../constants');
 const shouldUpdateDealFromMIAtoMIN = (deal, tfmDeal) => {
   const isMIA = deal.submissionType === CONSTANTS.DEALS.SUBMISSION_TYPE.MIA;
 
-  if (Object.prototype.hasOwnProperty.call(tfmDeal, 'underwriterManagersDecision')) {
+  if (tfmDeal.underwriterManagersDecision) {
     const hasApprovedDecision = (tfmDeal.underwriterManagersDecision
     && (tfmDeal.underwriterManagersDecision.decision === CONSTANTS.DEALS.DEAL_STAGE_TFM.UKEF_APPROVED_WITH_CONDITIONS
     || tfmDeal.underwriterManagersDecision.decision === CONSTANTS.DEALS.DEAL_STAGE_TFM.UKEF_APPROVED_WITHOUT_CONDITIONS));
