c793c3dde0b1958f9e521e3eda49602c9ee19347
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2022-07-12 09:08:59 +0100

    removed try catch from graphql query

--- trade-finance-manager-ui/server/api.js
+++ trade-finance-manager-ui/server/api.js
@@ -27,18 +27,13 @@ const getDeal = async (id, tasksFilters, activityFilters) => {
     activityFilters,
   };
 
-  try {
-    const response = await apollo('GET', dealQuery, queryParams);
+  const response = await apollo('GET', dealQuery, queryParams);
 
-    if (response.errors || response.networkError) {
-      console.error('TFM UI - GraphQL error querying deal ', response.errors || response.networkError.result.errors);
-    }
-
-    return response.data.deal;
-  } catch (err) {
-    console.error(`TFM UI - GraphQL error querying deal ${id}`, { err });
-    return null;
+  if (response.errors || response.networkError) {
+    console.error('TFM UI - GraphQL error querying deal ', response.errors || response.networkError.result.errors);
   }
+
+  return response.data.deal;
 };
 
 const getFacilities = async (queryParams) => {
