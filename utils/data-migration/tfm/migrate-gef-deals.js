ccdd66f2900a12cff94753a597a753490105cea0
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-07-22 09:24:57 +0100

    DTFS2-5777 : Typo rectification

--- utils/data-migration/tfm/migrate-gef-deals.js
+++ utils/data-migration/tfm/migrate-gef-deals.js
@@ -30,7 +30,7 @@ const getDeals = (filter = null) => getCollection(CONSTANTS.DATABASE.TABLES.DEAL
  * Updates TFM deal with provided updates object.
  * @param {String} ukefDealId UKEF Deal ID
  * @param {Object} updates Updates object in JSON
- * @returns {Promise} Resolve if successfull otherwise Reject
+ * @returns {Promise} Resolve if successful otherwise Reject
  */
 const setTfmDeal = (ukefDealId, updates) => update(CONSTANTS.DATABASE.TABLES.TFM_DEAL, ukefDealId, updates);
 
@@ -111,7 +111,7 @@ const submitTfmDeal = async () => {
  * Updates `tfm-deals` collection with update object.
  * @param {String} ukefDealId UKEF Deal ID
  * @param {Object} update Object comprising of updates to be applied
- * @returns {Promise} Resolve is success, otherwise Reject accomplied with an error message.
+ * @returns {Promise} Resolve is success, otherwise Reject accompanied with an error message.
  */
 const updateTfmDeal = (ukefDealId, updates) => {
   try {
@@ -127,7 +127,7 @@ const updateTfmDeal = (ukefDealId, updates) => {
 
 /**
  * Parses action sheets from defined directory.
- * Interested cell looksups are defined in `searches` in following format:
+ * Interested cell lookups are defined in `searches` in following format:
  * [property path, cell name, response field index]
  * @returns {Promise} Parsed data from the action sheets in `JSON` as promise resolve, otherwise reject.
  */
@@ -243,115 +243,116 @@ const updateAmendment = async (facilityId, amendmentId, data) => {
   }
 };
 
-const migrateGefAmendments = async () => {
-  const amendmentType = [
-    'Decrease in Value',
-    'Increase in Value',
-    'Increase in value',
-    'Extension',
-    'Extension,Value Increase, UKEF ML % reduction',
-    'Cover period increase',
-    'GE Extension',
-    'GE Extension, Increase in Value',
-    'Increase in Value, Cover percentage increase',
-    'ML Increase'
-  ];
-  const { MANUAL, AUTOMATIC } = CONSTANTS.AMENDMENT.AMENDMENT_TYPE;
-  const { COMPLETED } = CONSTANTS.AMENDMENT.AMENDMENT_STATUS;
-  const { PROCEED } = CONSTANTS.AMENDMENT.AMENDMENT_BANK_DECISION;
-  const allGefAmendments = [];
-  let amendments22To23 = await open('./gef-amendments/gef-amendments2.xlsx', 'Amendments Apr 22 to Apr 23');
-  amendments22To23 = amendments22To23.filter((item) => item.Product === 'GEF' && amendmentType.includes(item['Type of amendment'].trim()) && item.Stage === COMPLETED);
-  allGefAmendments.push(...amendments22To23);
+// const migrateGefAmendments = async () => {
+//   const amendmentType = [
+//     'Decrease in Value',
+//     'Increase in Value',
+//     'Increase in value',
+//     'Extension',
+//     'Extension,Value Increase, UKEF ML % reduction',
+//     'Cover period increase',
+//     'GE Extension',
+//     'GE Extension, Increase in Value',
+//     'Increase in Value, Cover percentage increase',
+//     'ML Increase'
+//   ];
+//   const { MANUAL, AUTOMATIC } = CONSTANTS.AMENDMENT.AMENDMENT_TYPE;
+//   const { COMPLETED } = CONSTANTS.AMENDMENT.AMENDMENT_STATUS;
+//   const { PROCEED } = CONSTANTS.AMENDMENT.AMENDMENT_BANK_DECISION;
+//   const allGefAmendments = [];
+//   let amendments22To23 = await open('./gef-amendments/gef-amendments2.xlsx', 'Amendments Apr 22 to Apr 23');
+//   amendments22To23 = amendments22To23.filter((item) => item.Product === 'GEF' && amendmentType.includes(item['Type of amendment'].trim()) && item.Stage === COMPLETED);
+//   allGefAmendments.push(...amendments22To23);
 
-  let amendments21To22 = await open('./gef-amendments/gef-amendments2.xlsx', 'Amendments Apr 21 to Apr 22');
-  amendments21To22 = amendments21To22.filter((item) => item.Product === 'GEF' && amendmentType.includes(item['Type of amendment'].trim()) && item.Stage === COMPLETED);
-  allGefAmendments.push(...amendments21To22);
-  // console.log(allGefAmendments);
-  const json = [];
-  for (const amendment of allGefAmendments) {
-    // eslint-disable-next-line no-await-in-loop
-    const { amendmentId } = await createFacilityAmendment(amendment.facilityId);
-    const { facilityId } = amendment;
-    console.log(amendmentId, facilityId); // TODO: change to proper script print
-    const output = {};
-    output.requestDate = getUnixTime(new Date(excelDateToISODateString(amendment['Date received'])));
-    output.exporter = amendment.Exporter.trim();
-    output.ukefDealId = `00${amendment['UKEF Deal ID']}`;
-    output.ukefFacilityId = `00${amendment['UKEF Facility ID']}`;
-    output.status = amendment.Stage;
-    output.submittedByPim = true;
-    output.submittedAt = amendment.Moved ? getUnixTime(new Date(excelDateToISODateString(amendment.Moved))) : '';
-    output.requireUkefApproval = amendment['Notification or Request'] === MANUAL;
-    output.changeFacilityValue = amendment.changeFacilityValue;
-    output.changeCoverEndDate = amendment.changeCoverEndDate;
+//   let amendments21To22 = await open('./gef-amendments/gef-amendments2.xlsx', 'Amendments Apr 21 to Apr 22');
+//   amendments21To22 = amendments21To22.filter((item) => item.Product === 'GEF' && amendmentType.includes(item['Type of amendment'].trim()) && item.Stage === COMPLETED);
+//   allGefAmendments.push(...amendments21To22);
+//   const json = [];
+//   for (const amendment of allGefAmendments) {
+//     // eslint-disable-next-line no-await-in-loop
+//     const { amendmentId } = await createFacilityAmendment(amendment.facilityId);
+//     const { facilityId } = amendment;
+//     console.log(amendmentId, facilityId); // TODO: change to proper script print
+//     const output = {};
+//     output.requestDate = getUnixTime(new Date(excelDateToISODateString(amendment['Date received'])));
+//     output.exporter = amendment.Exporter.trim();
+//     output.ukefDealId = `00${amendment['UKEF Deal ID']}`;
+//     output.ukefFacilityId = `00${amendment['UKEF Facility ID']}`;
+//     output.status = amendment.Stage;
+//     output.submittedByPim = true;
+//     output.submittedAt = amendment.Moved ? getUnixTime(new Date(excelDateToISODateString(amendment.Moved))) : '';
+//     output.requireUkefApproval = amendment['Notification or Request'] === MANUAL;
+//     output.changeFacilityValue = amendment.changeFacilityValue;
+//     output.changeCoverEndDate = amendment.changeCoverEndDate;
 
-    if (amendment.changeCoverEndDate) {
-      output.currentCoverEndDate = getUnixTime(new Date(excelDateToISODateString(amendment.currentCoverEndDate)));
-      output.coverEndDate = getUnixTime(new Date(excelDateToISODateString(amendment.coverEndDate)));
-    }
+//     if (amendment.changeCoverEndDate) {
+//       output.currentCoverEndDate = getUnixTime(new Date(excelDateToISODateString(amendment.currentCoverEndDate)));
+//       output.coverEndDate = getUnixTime(new Date(excelDateToISODateString(amendment.coverEndDate)));
+//     }
 
-    if (amendment.changeFacilityValue) {
-      output.currentValue = amendment.currentValue;
-      output.value = amendment.value;
-      output.currency = amendment.currency;
-    }
+//     if (amendment.changeFacilityValue) {
+//       output.currentValue = amendment.currentValue;
+//       output.value = amendment.value;
+//       output.currency = amendment.currency;
+//     }
 
-    if (amendment['Notification or Request'] === AUTOMATIC) {
-      if (amendment.changeCoverEndDate) {
-        output.effectiveDate = getUnixTime(new Date(excelDateToISODateString(amendment['When Finished'])));
-      }
-    }
+//     if (amendment['Notification or Request'] === AUTOMATIC) {
+//       if (amendment.changeCoverEndDate) {
+//         output.effectiveDate = getUnixTime(new Date(excelDateToISODateString(amendment['When Finished'])));
+//       }
+//     }
 
-    if (amendment['Notification or Request'] === MANUAL) {
-      output.leadUnderwriterId = amendment.UnderwriterID;
-      output.ukefDecision = {
-        coverEndDate: amendment['CoverEndDate Decision'], // TODO: add UW Manager decision Approved with or without conditions
-        value: amendment['Value Decision'], // TODO: add UW Manager decision Approved with or without conditions
-        comments: amendment['PIM Comment/Status'],
-        conditions: amendment.conditions ? amendment.conditions : null, // TODO: add any conditions, if applicable
-        declined: amendment.declined ? amendment.declined : null, // TODO: add any reasons why it was declined. This is probably not applicable
-        managersDecisionEmail: true,
-        managersDecisionEmailSent: true,
-        submitted: true,
-        submittedAt: amendment['When Finished'] ? getUnixTime(new Date(excelDateToISODateString(amendment['When Finished']))) : '',
-        submittedBy: {
-          _id: amendment.UnderwriterID,
-          name: amendment.UnderwriterName,
-          email: amendment.UnderwriterEmail,
-          username: amendment.UnderwriterUsername,
-        },
-      };
-      output.bankDecision = {
-        decision: PROCEED,
-        banksDecisionEmail: true,
-        banksDecisionEmailSent: true,
-        submittedAt: amendment['When Finished'] ? getUnixTime(new Date(excelDateToISODateString(amendment['When Finished']))) : '',
-        effectiveDate: amendment['When Finished'] ? getUnixTime(new Date(excelDateToISODateString(amendment['When Finished']))) : '', // TODO: confirm
-        receivedDate: getUnixTime(new Date(excelDateToISODateString(amendment['Date received']))), // TODO: check for received date
-        submitted: true,
-        submittedBy: {
-          _id: amendment.PIMId,
-          name: amendment.PIMName,
-          email: amendment.PIMEmail,
-          username: amendment.PIMUsername,
-        }
-      };
-    }
+//     if (amendment['Notification or Request'] === MANUAL) {
+//       output.leadUnderwriterId = amendment.UnderwriterID;
+//       output.ukefDecision = {
+//         coverEndDate: amendment['CoverEndDate Decision'], // TODO: add UW Manager decision Approved with or without conditions
+//         value: amendment['Value Decision'], // TODO: add UW Manager decision Approved with or without conditions
+//         comments: amendment['PIM Comment/Status'],
+//         conditions: amendment.conditions ? amendment.conditions : null, // TODO: add any conditions, if applicable
+//         declined: amendment.declined ? amendment.declined : null, // TODO: add any reasons why it was declined. This is probably not applicable
+//         managersDecisionEmail: true,
+//         managersDecisionEmailSent: true,
+//         submitted: true,
+//         submittedAt: amendment['When Finished'] ? getUnixTime(new Date(excelDateToISODateString(amendment['When Finished']))) : '',
+//         submittedBy: {
+//           _id: amendment.UnderwriterID,
+//           name: amendment.UnderwriterName,
+//           email: amendment.UnderwriterEmail,
+//           username: amendment.UnderwriterUsername,
+//         },
+//       };
+//       output.bankDecision = {
+//         decision: PROCEED,
+//         banksDecisionEmail: true,
+//         banksDecisionEmailSent: true,
+//         submittedAt: amendment['When Finished'] ? getUnixTime(new Date(excelDateToISODateString(amendment['When Finished']))) : '',
+//         effectiveDate: amendment['When Finished'] ? getUnixTime(new Date(excelDateToISODateString(amendment['When Finished']))) : '', // TODO: confirm
+//         receivedDate: getUnixTime(new Date(excelDateToISODateString(amendment['Date received']))), // TODO: check for received date
+//         submitted: true,
+//         submittedBy: {
+//           _id: amendment.PIMId,
+//           name: amendment.PIMName,
+//           email: amendment.PIMEmail,
+//           username: amendment.PIMUsername,
+//         }
+//       };
+//     }
 
-    // eslint-disable-next-line no-await-in-loop
-    await updateAmendment(facilityId, amendmentId, output);
-    const taskUpdate = {
-      createTasks: true,
-      submittedByPim: true,
-      requireUkefApproval: output.requireUkefApproval
-    };
-    // eslint-disable-next-line no-await-in-loop
-    await updateAmendment(facilityId, amendmentId, taskUpdate);
+//     // eslint-disable-next-line no-await-in-loop
+//     await updateAmendment(facilityId, amendmentId, output);
+//     const taskUpdate = {
+//       createTasks: true,
+//       submittedByPim: true,
+//       requireUkefApproval: output.requireUkefApproval
+//     };
+//     // eslint-disable-next-line no-await-in-loop
+//     await updateAmendment(facilityId, amendmentId, taskUpdate);
 
-    json.push(output);
-  }
-};
+//     json.push(output);
+//   }
+// };
+
+// migrateGefAmendments();
 
 // ******************** MAIN *************************
 
@@ -380,5 +381,4 @@ const migrate = () => {
     });
 };
 
-// migrate();
-migrateGefAmendments();
+migrate();
