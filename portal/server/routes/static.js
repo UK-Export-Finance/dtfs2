cc08227aacfa99bb03b2b3beefc996005f87d816
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-09-09 14:04:09 +0100

    fix(DTFS2-6043): Hackerone link added to acknowledgment

--- portal/server/routes/static.js
+++ portal/server/routes/static.js
@@ -6,9 +6,10 @@ router.get('/.well-known/security.txt', (req, res) => {
   res.type('text/plain');
 
   res.write('Contact: https://www.gov.uk/contact/govuk\n');
+  res.write('Contact: https://hackerone.com/7af14fd9-fe4e-4f39-bea1-8f8a364061b8/embedded_submissions/new\n');
+  res.write('Contact: https://get-a-guarantee-for-export-finance.service.gov.uk/feedback\n');
   res.write('Expires: 2022-12-09T00:00:00.000Z\n');
   res.write('Acknowledgments: https://get-a-guarantee-for-export-finance.service.gov.uk/thanks.txt\n');
-  res.write('Acknowledgments: https://hackerone.com/7af14fd9-fe4e-4f39-bea1-8f8a364061b8/embedded_submissions/new\n');
   res.write('Preferred-Languages: en\n');
   res.write('Canonical: https://get-a-guarantee-for-export-finance.service.gov.uk/.well-known/security.txt\n');
   res.write('Policy: https://www.gov.uk/guidance/report-a-vulnerability-on-a-ukef-system\n');
