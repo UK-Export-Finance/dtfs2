97ad50b84c290f960e8c4cecdfff90eba825af70
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2022-09-12 20:50:38 +0100

    feat(DTFS2-5777): BSS/EWCS TFM Data migration

--- utils/data-migration/helpers/datafixes.js
+++ utils/data-migration/helpers/datafixes.js
@@ -1,10 +1,12 @@
 const CONSTANTS = require('../constant');
-const { getCollection, portalDealUpdate } = require('./database');
+const { getCollection, portalDealUpdate, tfmDealUpdate } = require('./database');
 const { workflow } = require('./io');
 /**
  * Data fixes helper functions
  */
 let allDeals = {};
+
+// ******************** DEALS *************************
 
 /**
  * Eligibility Criteria (deal.eligibility)
@@ -53,7 +55,7 @@ const comment = async () => {
   const comments = await workflow(CONSTANTS.WORKFLOW.FILES.COMMENTS);
 
   Object.values(allDeals).forEach((deal, index) => {
-    if (deal.maker) {
+    if (deal.details && deal.maker) {
       const { maker } = deal;
       let portalComments = [];
 
@@ -82,13 +84,57 @@ const comment = async () => {
   });
 };
 
+// ******************** TFM DEALS *************************
+
+/**
+ * Add Party URN to the TFM deal parties (tfm.parties)
+ * Following parties URN will be added (if available)
+ * 1. Agent (tfm.parties.agent.partyUrn)
+ * 2. Buyer (tfm.parties.buyer.partyUrn)
+ * 3. exporter (tfm.parties.exporter.partyUrn)
+ * 4. indemnifier (tfm.parties.indemnifier.partyUrn)
+ * @param {Object} deal TFM deal object
+ */
+const partyUrn = async () => {
+  const urns = await workflow(CONSTANTS.WORKFLOW.FILES.DEAL_PARTIES);
+
+  Object.values(allDeals).forEach((deal, index) => {
+    if (deal.dealSnapshot.details && deal.tfm.parties) {
+      // Process Party URNs
+      urns
+        .filter(({ DEAL }) => DEAL['UKEF DEAL ID'] === deal.dealSnapshot.details.ukefDealId)
+        .map(({ DEAL }) => {
+          if (DEAL.PARTY && DEAL.PARTY.URN) {
+            const { ROLE_TYPE, URN } = DEAL.PARTY;
+
+            switch (ROLE_TYPE) {
+              case 'BUYER':
+                allDeals[index].tfm.parties.buyer.partyUrn = URN;
+                break;
+              case 'EXPORTER':
+                allDeals[index].tfm.parties.exporter.partyUrn = URN;
+                break;
+              case 'INDEMNIFIER':
+                allDeals[index].tfm.parties.indemnifier.partyUrn = URN;
+                break;
+              default:
+                break;
+            }
+          }
+
+          return null;
+        });
+    }
+  });
+};
+
 /**
  * Update portal deal
  * @param {String} id Object ID
  * @param {Object} deal Updated deal object
  * @returns {Promise} `Resolve` upon success otherwise `Reject`
  */
-const update = async (id, deal) => portalDealUpdate(id, deal)
+const portalUpdate = async (id, deal) => portalDealUpdate(id, deal)
   .then((r) => Promise.resolve(r))
   .catch((e) => Promise.reject(new Error('Error updating portal deal: ', { e })));
 
@@ -108,18 +154,21 @@ const datafixes = async (deals) => {
     allDeals = deals;
     let updated = 0;
 
-    // Data fixes
+    // Deal - Data fixes
     await banking();
     await comment();
     eligibilityCriteria();
 
+    // Facility - Data fixes
+
     // Update portal
     const updates = allDeals.map(async (deal) => {
-      await update(deal._id, deal)
+      await portalUpdate(deal._id, deal)
         .then((r) => {
           if (r) {
             updated += 1;
             console.info('\x1b[33m%s\x1b[0m', `${updated}/${allDeals.length} data-fixed.`, '\n');
+
             return Promise.resolve(true);
           }
 
@@ -132,9 +181,12 @@ const datafixes = async (deals) => {
       .then(() => {
         if (updated === allDeals.length) {
           console.info('\x1b[33m%s\x1b[0m', `✅ All ${allDeals.length} deals have been data-fixed.`, '\n');
+
+          return Promise.resolve(allDeals);
         }
 
-        return Promise.resolve(allDeals);
+        console.error('\n\x1b[31m%s\x1b[0m', `🚩 ${updated}/${allDeals.length} have been data-fixed.\n`);
+        return Promise.reject();
       })
       .catch((e) => Promise.reject(e));
   }
@@ -142,6 +194,62 @@ const datafixes = async (deals) => {
   return Promise.reject(new Error('Empty data set for data fixes'));
 };
 
+/**
+ * Data fix TFM deals
+ * @param {Array} deals TFM deals object in an array
+ * @returns {Array} deals Data fixed TFM deals object in an array
+ */
+const datafixesTfm = async (deals) => {
+  console.info('\x1b[33m%s\x1b[0m', `➕ 4. Data-fixing ${CONSTANTS.DEAL.DEAL_TYPE.BSS_EWCS} TFM deals.`, '\n');
+
+  try {
+    if (deals && deals.length > 0) {
+    /**
+     * Save deals to global variable for independent data fixes
+     * functions.
+     */
+      allDeals = deals;
+      let updated = 0;
+
+      // TFM Deal - Data fixes
+      await partyUrn();
+
+      const updates = allDeals.map(async (deal) => {
+        await tfmDealUpdate(deal)
+          .then((r) => {
+            if (r) {
+              updated += 1;
+              console.info('\x1b[33m%s\x1b[0m', `${updated}/${allDeals.length} TFM data-fixed.`, '\n');
+
+              return Promise.resolve(true);
+            }
+
+            return Promise.reject();
+          })
+          .catch((e) => Promise.reject(e));
+      });
+
+      return Promise.all(updates)
+        .then(() => {
+          if (updated === allDeals.length) {
+            console.info('\x1b[33m%s\x1b[0m', `✅ All ${allDeals.length} deals have been data-fixed.`, '\n');
+
+            return Promise.resolve(allDeals);
+          }
+          console.error('\n\x1b[31m%s\x1b[0m', `🚩 ${updated}/${allDeals.length} have been TFM data-fixed.\n`);
+          return Promise.reject();
+        })
+        .catch((e) => Promise.reject(e));
+    }
+
+    return Promise.reject(new Error('Empty data set for data fixes'));
+  } catch (e) {
+    console.error('Error data-fixing TFM deal: ', { e });
+    return Promise.reject(e);
+  }
+};
+
 module.exports = {
   datafixes,
+  datafixesTfm,
 };
