5535d8ec2d8b17b5b6322f792d990a83c50389be
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2022-10-14 08:16:41 +0100

    fix(DTFS2-6023): GEF Facility amendment

--- azure-functions/acbs-function/mappings/facility/helpers/get-loan-amount-difference.js
+++ azure-functions/acbs-function/mappings/facility/helpers/get-loan-amount-difference.js
@@ -1,16 +1,32 @@
+const CONSTANTS = require('../../../constants');
+
+const { FACILITY } = CONSTANTS;
+
 /**
  * Calculates amended facility loan amount difference between amended amount
  * and previous amount.
  * Due to Mulesoft API restriction a new amended UKEF exposure amount cannot
  * be send in a payload, instead difference between the amount is expected.
+ *
+ * `GEF` facilities type will return 10% of the difference.
+ * `Bond` facility type will return the full difference.
  * @param {Integer} ukefExposure UKEF Exposure
+ * @param {String} type Facility Type
  * @param {Object} facilityMasterRecord Facility master record object
  * @returns {Integer} Loan amount difference, if null argument then returns `0`
  */
-const getLoanAmountDifference = (ukefExposure, facilityMasterRecord) => {
+const getLoanAmountDifference = (ukefExposure, type, facilityMasterRecord) => {
   if (ukefExposure && facilityMasterRecord) {
     const { maximumLiability } = facilityMasterRecord;
-    return ukefExposure - maximumLiability;
+    const difference = ukefExposure - maximumLiability;
+
+    // GEF Facility - 10% of difference
+    if (type === FACILITY.FACILITY_TYPE.CASH || type === FACILITY.FACILITY_TYPE.CONTINGENT) {
+      return difference * 0.1;
+    }
+
+    // Bond Facility - Full amount
+    return difference;
   }
 
   return 0;
