41b5614e2db9c6efe3e9278e31fbd5f9f504fbfe
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2022-12-14 16:47:19 +0000

    fix(DTFS2-6235): Code quality improvement and Data sanatisation

--- trade-finance-manager-api/src/utils/string.js
+++ trade-finance-manager-api/src/utils/string.js
@@ -1,24 +1,14 @@
-const isEmptyString = (str) => {
-  if (!str || ((typeof str === 'string' || str instanceof String) && !str.trim().length)) {
-    return true;
-  }
-  return false;
-};
+const isEmptyString = (str) => !str || ((typeof str === 'string' || str instanceof String) && !str.trim().length);
 
-const hasValue = (str) => {
-  if (str && !isEmptyString(str)) {
-    return true;
-  }
-  return false;
-};
+const hasValue = (str) => str && !isEmptyString(str);
 
-const stripCommas = (str) => str.replace(/,/g, '');
+const stripCommas = (str) => str.toString().replace(/,/g, '');
 
 const capitalizeFirstLetter = (str) =>
-  str.charAt(0).toUpperCase() + str.slice(1);
+  str.toString().charAt(0).toUpperCase() + str.slice(1);
 
 const lowercaseFirstLetter = (str) =>
-  str.charAt(0).toLowerCase() + str.slice(1);
+  str.toString().charAt(0).toLowerCase() + str.slice(1);
 
 module.exports = {
   isEmptyString,
