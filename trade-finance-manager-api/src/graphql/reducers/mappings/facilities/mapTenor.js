f107eeca418893d25415c8c286c4f3600ae103e8
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2022-12-20 15:59:05 +0000

    feat(DTFS2-6236): Return tenor across products for all facilities

--- trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapTenor.js
+++ trade-finance-manager-api/src/graphql/reducers/mappings/facilities/mapTenor.js
@@ -3,11 +3,17 @@ const { findLatestCompletedAmendment } = require('../../../helpers/amendment.hel
 
 // maps tenor from new amendment coverEndDate or from original facility
 const mapTenor = (facilitySnapshot, facilityTfm, facility) => {
-  const { facilityStage, ukefGuaranteeInMonths: monthsOfCover } = facilitySnapshot;
+  // Facility stage
+  const { hasBeenIssued } = facilitySnapshot;
+  /**
+   * `monthsOfCover`: GEF
+   * `ukefGuaranteeInMonths`: BSS/EWCS
+   */
+  const months = facilitySnapshot.ukefGuaranteeInMonths ?? facilitySnapshot.monthsOfCover;
+  // If issued
   const { exposurePeriodInMonths } = facilityTfm;
+  let exposurePeriod = exposurePeriodInMonths;
 
-  // sets original exposure period from facility
-  let updatedExposurePeriodInMonths = exposurePeriodInMonths;
   // if amendments
   if (facility?.amendments?.length) {
     const latestAmendmentTFM = findLatestCompletedAmendment(facility.amendments);
@@ -15,11 +21,11 @@ const mapTenor = (facilitySnapshot, facilityTfm, facility) => {
     // checks if exposure period in months in latest completed amendment
     if (latestAmendmentTFM?.amendmentExposurePeriodInMonths) {
       // sets updatedExposurePeriodInMonths as amendment exposure period
-      updatedExposurePeriodInMonths = latestAmendmentTFM.amendmentExposurePeriodInMonths;
+      exposurePeriod = latestAmendmentTFM.amendmentExposurePeriodInMonths;
     }
   }
 
-  return mapTenorDate(facilityStage, monthsOfCover, updatedExposurePeriodInMonths);
+  return mapTenorDate(hasBeenIssued, months, exposurePeriod);
 };
 
 module.exports = mapTenor;
