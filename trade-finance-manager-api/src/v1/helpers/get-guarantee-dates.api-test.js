55d9de20dca85d296451e325978cc38c7c4cc836
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-02-07 10:12:07 +0000

    feat(DTFS2-6284): Fixed API test

--- trade-finance-manager-api/src/v1/helpers/get-guarantee-dates.api-test.js
+++ trade-finance-manager-api/src/v1/helpers/get-guarantee-dates.api-test.js
@@ -1,50 +1,53 @@
 const moment = require('moment');
 const getGuaranteeDates = require('./get-guarantee-dates');
 
-const submissionDate = '2021-04-24';
-
+const submissionDate = '2023-01-01';
+const dealSubmissionDate = new Date(submissionDate).valueOf();
 const issuedFacility = {
   hasBeenIssued: true,
   coverStartDate: new Date(submissionDate).valueOf(),
   coverEndDate: moment().set({
-    date: Number('21'),
-    month: Number('05') - 1, // months are zero indexed
+    date: Number('01'),
+    month: Number('01') - 1, // months are zero indexed
     year: Number('2024'),
   }),
 };
-
 const unissuedFacility = {
   hasBeenIssued: false,
-  ukefGuaranteeInMonths: 24,
+  ukefGuaranteeInMonths: 12,
 };
 
-const dealSubmissionDate = new Date(submissionDate).valueOf();
-
-describe('get-guarantee-dates', () => {
-  describe('issued facility', () => {
-    it('should return the correct commencement dates', () => {
-      const {
-        guaranteeCommencementDate,
-        guaranteeExpiryDate,
-        effectiveDate,
-      } = getGuaranteeDates(issuedFacility, dealSubmissionDate);
-
-      expect(guaranteeCommencementDate).toEqual('2021-04-24');
-      expect(guaranteeExpiryDate).toEqual('2024-05-21');
-      expect(effectiveDate).toEqual(submissionDate);
-    });
-  });
-
-  describe('unissued facility', () => {
-    it('should return the correct commencement dates from deal submission date', () => {
+describe('Get guarantee dates in TFM', () => {
+  describe('When a facility is un-issued / commitment / 06', () => {
+    it('Should return the correct dates', () => {
       const {
         guaranteeCommencementDate,
         guaranteeExpiryDate,
         effectiveDate,
       } = getGuaranteeDates(unissuedFacility, dealSubmissionDate);
 
-      expect(guaranteeCommencementDate).toEqual('2021-07-24');
-      expect(guaranteeExpiryDate).toEqual('2023-07-24');
+      // Deal submission date
+      expect(guaranteeCommencementDate).toEqual(submissionDate);
+      // GCD + Tenor
+      expect(guaranteeExpiryDate).toEqual('2024-01-01');
+      // Deal submission date
+      expect(effectiveDate).toEqual(submissionDate);
+    });
+  });
+
+  describe('when a facility is Issued / 07', () => {
+    it('Should return the correct dates', () => {
+      const {
+        guaranteeCommencementDate,
+        guaranteeExpiryDate,
+        effectiveDate,
+      } = getGuaranteeDates(issuedFacility, dealSubmissionDate);
+
+      // Cover start date
+      expect(guaranteeCommencementDate).toEqual('2023-01-01');
+      // Cover end date
+      expect(guaranteeExpiryDate).toEqual('2024-01-01');
+      // Deal submission date
       expect(effectiveDate).toEqual(submissionDate);
     });
   });
