315b43b8a020e4d1cb387fa3c90c10c926ca1fd8
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-02-08 18:24:30 +0000

    fix(tfm): Deal submission logging

--- trade-finance-manager-api/src/v1/controllers/deal-add-tfm-data/dealStage.js
+++ trade-finance-manager-api/src/v1/controllers/deal-add-tfm-data/dealStage.js
@@ -3,9 +3,7 @@ const CONSTANTS = require('../../../constants');
 const dealStage = (status, submissionType) => {
   let tfmDealStage;
 
-  const hasSubmittedStatus = (status === CONSTANTS.DEALS.PORTAL_DEAL_STATUS.SUBMITTED_TO_UKEF);
-
-  if (hasSubmittedStatus) {
+  if (status === CONSTANTS.DEALS.PORTAL_DEAL_STATUS.SUBMITTED_TO_UKEF) {
     if (submissionType === CONSTANTS.DEALS.SUBMISSION_TYPE.AIN) {
       tfmDealStage = CONSTANTS.DEALS.DEAL_STAGE_TFM.CONFIRMED;
     }
