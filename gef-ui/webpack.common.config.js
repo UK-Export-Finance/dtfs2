bb8ffcb53c87c2fc580aa1ee09843206f974e363
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-02-27 16:43:46 +0000

    fix(avoid-double-clicks): Webpack conflicting names resolved

--- gef-ui/webpack.common.config.js
+++ gef-ui/webpack.common.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/js'),
     filename: '[name].js',
-    library: ['DTFS', '[name]'],
+    library: ['DTFS_GEF', '[name]'],
     libraryTarget: 'var',
   },
   target: ['web', 'es5'],
