b36d46c54d23a4a41f3ba55bdca7d6db971ca6a7
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-02-27 18:46:48 +0000

    fix(feat-avoid-double-clicks): npm run build

--- gef-ui/public/js/fileUpload.js
+++ gef-ui/public/js/fileUpload.js
@@ -1,2 +1,2 @@
-var DTFS;void 0!==MOJFrontend.MultiFileUpload&&new MOJFrontend.MultiFileUpload({container:$(".moj-multi-file-upload"),uploadUrl:"".concat(window.location.href,"/upload"),deleteUrl:"".concat(window.location.href,"/delete")}),(DTFS=void 0===DTFS?{}:DTFS).fileUpload={};
+var DTFS_GEF;void 0!==MOJFrontend.MultiFileUpload&&new MOJFrontend.MultiFileUpload({container:$(".moj-multi-file-upload"),uploadUrl:"".concat(window.location.href,"/upload"),deleteUrl:"".concat(window.location.href,"/delete")}),(DTFS_GEF=void 0===DTFS_GEF?{}:DTFS_GEF).fileUpload={};
 //# sourceMappingURL=fileUpload.js.map
--- gef-ui/public/js/jsEnabled.js
+++ gef-ui/public/js/jsEnabled.js
@@ -1,2 +1,2 @@
-var DTFS;document.body.className=document.body.className?"".concat(document.body.className," js-enabled"):"js-enabled",(DTFS=void 0===DTFS?{}:DTFS).jsEnabled={};
+var DTFS_GEF;document.body.className=document.body.className?"".concat(document.body.className," js-enabled"):"js-enabled",(DTFS_GEF=void 0===DTFS_GEF?{}:DTFS_GEF).jsEnabled={};
 //# sourceMappingURL=jsEnabled.js.map
