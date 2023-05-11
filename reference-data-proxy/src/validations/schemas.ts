751c6a69998d08fdbe57f0b6a6115a571bd4e529
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-05-11 10:41:36 +0100

    fix(DTFS2-6419): lint fixes

--- reference-data-proxy/src/validations/schemas.ts
+++ reference-data-proxy/src/validations/schemas.ts
@@ -2,53 +2,47 @@ import Joi from 'joi';
 
 export const schemas = {
   environments: Joi.object({
-    // Notify API
-    GOV_NOTIFY_API_KEY: Joi.string().required(),
+    // Internal
 
-    // ACBS
+    // 1. ACBS
     AZURE_ACBS_FUNCTION_URL: Joi.string().required(),
     MULESOFT_API_SECRET: Joi.string().required(),
     MULESOFT_API_KEY: Joi.string().required(),
-    // ACBS facility
     MULESOFT_API_ACBS_FACILITY_URL: Joi.string().required(),
-    // ACBS deal
     MULESOFT_API_ACBS_DEAL_URL: Joi.string().required(),
 
-    // eStore
+    // 2. PORT
+    PORT: Joi.number().default(5002),
+
+    // 3. MongoDB
+    MONGO_INITDB_DATABASE: Joi.string().required(),
+    MONGODB_URI: Joi.string().required(),
+
+    // External
+
+    // 1. APIM
+    APIM_MDM_URL: Joi.string().required(),
+    APIM_MDM_KEY: Joi.string().required(),
+    APIM_MDM_VALUE: Joi.string().required(),
+
+    // 2. GOV NOTIFY
+    GOV_NOTIFY_API_KEY: Joi.string().required(),
+
+    // 3. ESTORE
     MULESOFT_API_UKEF_ESTORE_EA_URL: Joi.string().required(),
     MULESOFT_API_UKEF_ESTORE_EA_KEY: Joi.string().required(),
     MULESOFT_API_UKEF_ESTORE_EA_SECRET: Joi.string().required(),
 
-    // Exposure period
-    MULESOFT_API_EXPOSURE_PERIOD_URL: Joi.string().required(),
-
-    // PORT - defaults to 5002
-    PORT: Joi.number().default(5002),
-
-    // MongoDB
-    MONGO_INITDB_DATABASE: Joi.string().required(),
-    MONGODB_URI: Joi.string().required(),
-
-    // Currency exchange
-    APIM_MDM_URL: Joi.string().required(),
-    APIM_MDM_KEY: Joi.string().required(),
-    APIM_MDM_VALUE: Joi.string().required(),
-
-    // Ordnance Survey
+    // 4. ORDNANCE SURVEY
     ORDNANCE_SURVEY_API_URL: Joi.string().required(),
     ORDNANCE_SURVEY_API_KEY: Joi.string().required(),
 
-    // Party DB
+    // 5. PARTY DB
     MULESOFT_API_PARTY_DB_URL: Joi.string().required(),
     MULESOFT_API_PARTY_DB_KEY: Joi.string().required(),
     MULESOFT_API_PARTY_DB_SECRET: Joi.string().required(),
 
-    // Premium schedule
-    APIM_MDM_URL: Joi.string().required(),
-    APIM_MDM_KEY: Joi.string().required(),
-    APIM_MDM_VALUE: Joi.string().required(),
-
-    // Companies House
+    // 6. COMPANIES HOUSE
     COMPANIES_HOUSE_API_KEY: Joi.string().required(),
     COMPANIES_HOUSE_API_URL: Joi.string().required(),
   }).unknown(),
