10942c0dc701605d76bb73ddc83da96453adf54c
Christian McCaffery <CMcCaffery@ukexportfinance.gov.uk>
2023-05-16 11:25:25 +0100

    Improve error messages and revert user ID validation to alphanumeric

--- trade-finance-manager-api/src/v1/validation/route-validators/route-validators.js
+++ trade-finance-manager-api/src/v1/validation/route-validators/route-validators.js
@@ -1,13 +1,13 @@
 const { param } = require('express-validator');
 
-const userParamNumericValidation = param('user').isMongoId().withMessage('The User ID (user) provided should be numeric');
-const facilityIdNumericValidation = param('facilityId').isMongoId().withMessage('The Facility ID (facilityId) provided should be numeric');
-const amendmentIdNumericValidation = param('amendmentId').isMongoId().withMessage('The Amendment ID (amendmentId) provided should be numeric');
-const dealIdNumericValidation = param('dealId').isMongoId().withMessage('The Amendment ID (amendmentId) provided should be numeric');
+const userParamNumericValidation = param('user').isAlphanumeric().withMessage('The User ID (user) provided should be alphanumeric, a Mongo ID or username');
+const facilityIdNumericValidation = param('facilityId').isMongoId().withMessage('The Facility ID (facilityId) provided should be a Mongo ID');
+const amendmentIdNumericValidation = param('amendmentId').isMongoId().withMessage('The Amendment ID (amendmentId) provided should be a Mongo ID');
+const dealIdNumericValidation = param('dealId').isMongoId().withMessage('The Amendment ID (amendmentId) provided should be a Mongo ID');
 const partyURNNumericValidation = param('urn')
   .isString()
   .matches(/^"\d+"$/)
-  .withMessage('The party URN (urn) provided should be numeric');
+  .withMessage('The party URN (urn) provided should be of the form /^"\d+"$/');
 
 exports.userIdValidation = [userParamNumericValidation];
 
