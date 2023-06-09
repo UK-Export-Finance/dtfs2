80f08e5de474324df64980cec828021dbaa88c14
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-06-09 17:22:13 +0100

    fix(console): removed console.log

--- portal-api/src/v1/users/routes.js
+++ portal-api/src/v1/users/routes.js
@@ -53,7 +53,6 @@ module.exports.create = async (req, res, next) => {
     delete req.body._csrf;
   }
   await findByEmail(req.body.email, (error, account) => {
-    console.log('===>', req.body.email, account);
     let userExists = {};
     if (account) {
       // User exists with same email address
