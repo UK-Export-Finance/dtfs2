d823ea0400af455965f6619c8952cdb6a30f5469
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-06-12 22:07:57 +0100

    feat(mongodb): unit test coverage

---
+++ portal-api/api-tests/helpers/escapeOperators.api-test.js
@@ -0,0 +1,242 @@
+const { escapeOperators } = require('../../src/v1/helpers/escapeOperators');
+
+describe('escapeOperators function', () => {
+  // Tests that the function returns an empty object when an empty filter object is passed
+  it('should test empty filter object', () => {
+    const filter = {};
+    const result = escapeOperators(filter);
+    expect(result).toEqual({});
+  });
+
+  // Tests that the function returns the same filter object when no AND operator is present
+  it('should test no and operator', () => {
+    const filter = { key: 'value' };
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that the function returns the same filter object when an invalid operator key is present
+  it('should test invalid operator key', () => {
+    const filter = { OR: [{ key: 'value' }] };
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that the function correctly escapes the AND operator when it is the only operator present
+  it('should test filter object with only and operator', () => {
+    const filter = { AND: [{ key: 'value' }] };
+    const result = escapeOperators(filter);
+    expect(result).toEqual({ $and: [{ key: 'value' }] });
+  });
+
+  // Tests that the function correctly escapes the OR operator when it is nested inside an AND operator
+  it('should test filter object with nested or operator inside and operator', () => {
+    const filter = {
+      AND: [
+        {
+          'bank.id': '9'
+        },
+        {
+          OR: [
+            {
+              status: "Ready for Checker's approval"
+            }
+          ]
+        }
+      ]
+    };
+    const result = escapeOperators(filter);
+    expect(result).toEqual({
+      $and: [
+        {
+          'bank.id': '9'
+        },
+        {
+          $or: [
+            {
+              status: "Ready for Checker's approval"
+            }
+          ]
+        }
+      ]
+    });
+  });
+
+  // Tests that the function returns null when null input is passed
+  it('should test null input', () => {
+    const filter = null;
+    const result = escapeOperators(filter);
+    expect(result).toBeNull();
+  });
+
+  // Tests that the function returns undefined when undefined input is passed
+  it('should test undefined input', () => {
+    const filter = undefined;
+    const result = escapeOperators(filter);
+    expect(result).toBeUndefined();
+  });
+
+  // Tests that the function returns the same input when non-object input is passed
+  it('should test non object input', () => {
+    const filter = 'string';
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that the function correctly handles an empty OR array inside an AND operator
+  it('should test empty or array inside and operator', () => {
+    const filter = { AND: [{ OR: [] }] };
+    const result = escapeOperators(filter);
+    expect(result).toEqual({ $and: [{ $or: [] }] });
+  });
+
+  // Tests that the function correctly deletes the AND operator key after escaping
+  it('should test delete and operator key', () => {
+    const filter = { AND: [{ key: 'value' }] };
+    const result = escapeOperators(filter);
+    expect(result).not.toHaveProperty('AND');
+  });
+
+  // Tests that an empty filter object returns the same object
+  it('should test empty filter object', () => {
+    const filter = {};
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that a filter object with no AND operator returns the same object
+  it('should test no and operator', () => {
+    const filter = {
+      'bank.id': '9'
+    };
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that a filter object with an invalid operator key returns the same object
+  it('should test invalid operator key', () => {
+    const filter = {
+      INVALID: [
+        {
+          'bank.id': '9'
+        }
+      ]
+    };
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that a filter object with only AND operator is correctly escaped
+  it('should test filter object with only and operator', () => {
+    const filter = {
+      AND: [
+        {
+          'bank.id': '9'
+        }
+      ]
+    };
+    const expected = {
+      $and: [
+        {
+          'bank.id': '9'
+        }
+      ]
+    };
+    const result = escapeOperators(filter);
+    expect(result).toEqual(expected);
+  });
+
+  // Tests that a filter object with nested OR operator inside AND operator is correctly escaped
+  it('should test filter object with nested or operator inside and operator', () => {
+    const filter = {
+      AND: [
+        {
+          'bank.id': '9'
+        },
+        {
+          OR: [
+            {
+              status: "Ready for Checker's approval"
+            }
+          ]
+        }
+      ]
+    };
+    const expected = {
+      $and: [
+        {
+          'bank.id': '9'
+        },
+        {
+          $or: [
+            {
+              status: "Ready for Checker's approval"
+            }
+          ]
+        }
+      ]
+    };
+    const result = escapeOperators(filter);
+    expect(result).toEqual(expected);
+  });
+
+  // Tests that null input returns null
+  it('should test null input', () => {
+    const filter = null;
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that undefined input returns undefined
+  it('should test undefined input', () => {
+    const filter = undefined;
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that non-object input returns the same input
+  it('should test non object input', () => {
+    const filter = 'not an object';
+    const result = escapeOperators(filter);
+    expect(result).toEqual(filter);
+  });
+
+  // Tests that empty OR array inside AND operator is correctly handled
+  it('should test empty or array inside and operator', () => {
+    const filter = {
+      AND: [
+        {
+          'bank.id': '9'
+        },
+        {
+          OR: []
+        }
+      ]
+    };
+    const expected = {
+      $and: [
+        {
+          'bank.id': '9'
+        },
+        {
+          $or: []
+        }
+      ]
+    };
+    const result = escapeOperators(filter);
+    expect(result).toEqual(expected);
+  });
+
+  // Tests that the AND operator key is deleted after escaping
+  it('should test delete and operator key after escaping', () => {
+    const filter = {
+      AND: [
+        {
+          'bank.id': '9'
+        }
+      ]
+    };
+    const result = escapeOperators(filter);
+    expect(result).not.toHaveProperty('AND');
+  });
+});
--- portal-api/src/v1/helpers/escapeOperators.js
+++ portal-api/src/v1/helpers/escapeOperators.js
@@ -1,10 +1,34 @@
 /**
+ * Objective:
+    The objective of the "escapeOperators" function is to escape the
+    "AND" and "OR" operators in a given filter object and return a new object with the escaped operators.
+
+Inputs:
+    "filter": a JavaScript object that may contain "AND" and "OR" operators.
+
+Flow:
+    Check if "filter" exists.
+    If "AND" operator exists in "filter", create a new object with "$and" key and value of "filter.AND".
+    For each element in "$and" array, check if "OR" operator exists.
+    If "OR" operator exists, create a new object with "$or" key and value of "e.OR", delete "OR" key from "e", and return the new object.
+    Replace the original element with the new object in the "$and" array.
+    Delete the "AND" key from the new object and return it.
+
+Outputs:
+    A new JavaScript object with escaped "AND" and "OR" operators.
+
+Additional aspects:
+    The function only escapes "AND" and "OR" operators, and does not modify any other keys or values in the input object.
+    If the input object does not contain "AND" or "OR" operators, the function simply returns the input object.
+ */
+
+/**
  * This function escapes MongoDB operators from the filter object
  * @param {Object} filter Object comprising of filters
  * @returns {Object} Escaped filter object
  */
 const escapeOperators = (filter) => {
-  if (filter) {
+  if (typeof filter === 'object' && filter !== null) {
     // Escape `AND` operator
     if ('AND' in filter) {
       const payload = {
@@ -12,7 +36,7 @@ const escapeOperators = (filter) => {
       };
 
       // Escape `OR` operator
-      const or = payload.$and.map((e) => {
+      payload.$and = payload.$and.map((e) => {
         if (e.OR) {
           const orPayload = {
             $or: e.OR,
@@ -25,8 +49,6 @@ const escapeOperators = (filter) => {
 
         return e;
       });
-
-      payload.$and = or;
 
       delete payload.AND;
 
