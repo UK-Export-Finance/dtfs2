a2c98a8962c081651b3ceb2a9ca710a2627674dd
ttbarnes <tony@tonybarnes.me>
2023-06-27 16:19:54 +0100

    security(SR3): checkApiKey middleware

---
+++ external-api/src/middleware/check-api-key/index.ts
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+import { Request, Response, NextFunction } from 'express';
+
+dotenv.config();
+
+const { API_KEY } = process.env;
+
+/**
+ * Check that the x-api-key header is valida
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @param {String} next Callback function name
+ */
+export const checkApiKey = (req: Request, res: Response, next: NextFunction) => {
+  const {
+    headers: { 'x-api-key': xApiKey },
+  } = req;
+
+  /**
+   * x-api-key is not provided.
+   * Reject the request.
+   */
+  if (!xApiKey) {
+    return res.status(400).send();
+  }
+
+  /**
+   * x-api-key is valid.
+   * Allow the request to continue.
+   */
+  if (xApiKey === API_KEY) {
+    return next();
+  }
+
+  /**
+   * x-api-key is invalid.
+   * Reject the reuqest
+   */
+  return res.status(401).send();
+};
