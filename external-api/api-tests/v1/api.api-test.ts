c079f82190e999d9857a0c5519f53c5636c5d8cc
ttbarnes <tony@tonybarnes.me>
2023-06-27 16:33:44 +0100

    chore(tech): fix failing unit test

--- external-api/api-tests/v1/api.api-test.ts
+++ external-api/api-tests/v1/api.api-test.ts
@@ -2,20 +2,25 @@ import { findACBSIndustrySector } from '../../src/v1/controllers/industry-sector
 
 describe('findACBSIndustrySector lookup', () => {
   it('should return status code from industry sector lookup', async () => {
-    const expectedBody = [
-      {
-        ukefSectorName: 'Electricity, gas, steam and air conditioning supply',
-        internalNo: null,
-        acbsSectorName: 'CIVIL: POWER',
-        acbsIndustryName: 'GAS',
-        created: '2017-04-01T00:00:00',
-        updated: '2017-06-28T11:01:29',
-        effectiveFrom: '2017-04-01T00:00:00',
-        effectiveTo: '9999-12-31T00:00:00',
-      },
-    ];
+    const expected = {
+      ukefSectorName: 'Electricity, gas, steam and air conditioning supply',
+      internalNo: null,
+      acbsSectorName: 'CIVIL: POWER',
+      acbsIndustryName: 'GAS',
+    };
+
     const { status, data } = await findACBSIndustrySector(35220);
+
     expect(status).toEqual(200);
-    expect(data).toMatchObject(expectedBody);
+
+    expect(data[0].ukefSectorName).toEqual(expected.ukefSectorName);
+    expect(data[0].internalNo).toEqual(expected.internalNo);
+    expect(data[0].acbsSectorName).toEqual(expected.acbsSectorName);
+    expect(data[0].acbsIndustryName).toEqual(expected.acbsIndustryName);
+
+    expect(data[0].created).toBeDefined();
+    expect(data[0].updated).toBeDefined();
+    expect(data[0].effectiveFrom).toBeDefined();
+    expect(data[0].effectiveTo).toBeDefined();
   });
 });
