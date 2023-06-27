a9553d68a0968cf0c0696744f9bb0615187834ce
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-06-27 16:07:12 +0100

    fix(apim/tfs): removed comments

--- azure-functions/acbs-function/api.api-test.js
+++ azure-functions/acbs-function/api.api-test.js
@@ -7,14 +7,12 @@ const {
 } = require('./api');
 
 describe('get function', () => {
-  // Tests that the function returns an API response object when endpoint is provided
   it('should test happy path returns api response object', async () => {
     const response = await getFacility('1');
     expect(response).toHaveProperty('status');
     expect(response).toHaveProperty('data');
   });
 
-  // Tests that the function returns an API response object for getFacility
   it('should test happy path returns api response object for get facility', async () => {
     const response = await getFacility();
     expect(response).toHaveProperty('status');
@@ -23,14 +21,12 @@ describe('get function', () => {
 });
 
 describe('post function', () => {
-  // Tests that the function returns an API response object when endpoint is provided
   it('should test happy path returns api response object', async () => {
     const response = await createParty({});
     expect(response).toHaveProperty('status');
     expect(response).toHaveProperty('data');
   });
 
-  // Tests that the function returns an API response object for getFacility
   it('should test happy path returns api response object for create party', async () => {
     const response = await createParty();
     expect(response).toHaveProperty('status');
@@ -39,14 +35,12 @@ describe('post function', () => {
 });
 
 describe('put function', () => {
-  // Tests that the function returns an API response object when endpoint is provided
   it('should test happy path returns api response object', async () => {
     const response = await updateFacility('1', 'amount', {}, 'abc123');
     expect(response).toHaveProperty('status');
     expect(response).toHaveProperty('data');
   });
 
-  // Tests that the function returns an API response object for getFacility
   it('should test happy path returns api response object for update facility', async () => {
     const response = await updateFacility();
     expect(response).toHaveProperty('status');
@@ -55,14 +49,12 @@ describe('put function', () => {
 });
 
 describe('patch function', () => {
-  // Tests that the function returns an API response object when endpoint is provided
   it('should test happy path returns api response object', async () => {
     const response = await updateFacilityLoan('1', 'amount', {}, 'abc123');
     expect(response).toHaveProperty('status');
     expect(response).toHaveProperty('data');
   });
 
-  // Tests that the function returns an API response object for getFacility
   it('should test happy path returns api response object for update facility loan', async () => {
     const response = await updateFacilityLoan();
     expect(response).toHaveProperty('status');
