2ae31022e7c23d00e1c03cb72afed2c69628ebd0
ttbarnes <tony@tonybarnes.me>
2023-06-30 09:55:32 +0100

    fix(SR-6): fix typo

--- portal/server/routes/middleware/headers/security/is-request-header-origin-valid/index.js
+++ portal/server/routes/middleware/headers/security/is-request-header-origin-valid/index.js
@@ -5,8 +5,9 @@ const getUrlOrigin = require('../get-url-origin');
  * Check if a request header's origin is:
  * 1) A valid URL
  * 2) Matches the services' origin
- * @param {String} URL
- * @returns {String} URL origin
+ * @param {String} True URL origin
+ * @param {String} Provided request header URL
+ * @returns {Boolean}
  */
 const isRequestHeaderOriginValid = (origin, requestHeader) => {
   if (isUrl(requestHeader)) {
