50015b1e1a8acf214166ba9eadc40de84a02710f
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2023-07-11 15:59:22 +0100

    fix(SR-5): Fix mock-data-loader user to have full permissions

--- utils/mock-data-loader/user-helper.js
+++ utils/mock-data-loader/user-helper.js
@@ -8,7 +8,7 @@ const mockDataLoaderUser = {
   surname: 'DataLoader',
   roles: ['editor', 'maker'],
   email: 're-insert-mocks-data-loader@ukexportfinance.gov.uk',
-  bank: MOCK_BANKS.find((bank) => bank.id === '9'),
+  bank: { id: '*' },
 };
 
 const createAndLogInAsInitialUser = async () => {
