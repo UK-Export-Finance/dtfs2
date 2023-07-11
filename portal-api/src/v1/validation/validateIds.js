316af22875e685fa9954bd6aeb1c780dd61f2e3e
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2023-07-11 10:49:44 +0100

    security/SR5 added documentation

--- portal-api/src/v1/validation/validateIds.js
+++ portal-api/src/v1/validation/validateIds.js
@@ -1,5 +1,12 @@
 const validator = require('validator');
 
+/**
+ * isValidMongoId
+ * validates that a passed mongoId is a valid one
+ * returns true if so, false if not valid
+ * @param {String} mongoId
+ * @returns {Boolean}
+ */
 const isValidMongoId = (mongoId) => {
   if (!mongoId) {
     return false;
