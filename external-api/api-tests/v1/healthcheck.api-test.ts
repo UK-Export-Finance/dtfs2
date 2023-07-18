e06d31f7de9fb1da43f91c761e853855fc25a7a5
ChristianMcc <37540516+ChristianMcc@users.noreply.github.com>
2023-07-18 09:29:48 +0100

    fix(SR-5): protect `external-api` from SSRF (#1848)
    Co-authored-by: Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
    Co-authored-by: Abhi Markan <32125108+abhi-markan@users.noreply.github.com>

--- external-api/api-tests/v1/healthcheck.api-test.ts
+++ external-api/api-tests/v1/healthcheck.api-test.ts
@@ -1,10 +1,11 @@
 import { app } from '../../src/createApp';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { get } = require('../api')(app);
+import { api } from '../api';
+
+const { get } = api(app);
 
 describe('Healthcheck', () => {
   it('returns 200 if server is healthy', async () => {
-    const res = await get(`/healthcheck`, null);
+    const res = await get(`/healthcheck`);
     expect(res.body.uptime).toBeGreaterThan(0);
     expect(res.status).toBe(200);
   });
--- portal-api/src/v1/gef/controllers/validation/external.js
+++ portal-api/src/v1/gef/controllers/validation/external.js
@@ -1,19 +1,29 @@
 /* eslint-disable consistent-return */
 const companiesHouseError = (err) => {
   let errMsg;
-  switch (err.response.data.errors[0].error) {
-    case 'company-profile-not-found':
-      errMsg = 'Invalid Companies House registration number';
-      break;
-    default:
-      errMsg = 'There was a problem getting the Companies House registration number';
-      break;
+  let errCode = undefined;
+  if (err.response?.data?.data === 'Invalid company registration number') {
+    errMsg = 'Invalid Companies House registration number';
+  } else {
+    switch (err.response?.data?.errors?.length > 0 && err.response?.data?.errors[0]?.error) {
+      case 'company-profile-not-found':
+        errMsg = 'Invalid Companies House registration number';
+        errCode = err.response?.data?.errors[0]?.error;
+        break;
+      default:
+        errMsg = 'There was a problem getting the Companies House registration number';
+        errCode = err.response?.data?.errors[0]?.error;
+        break;
+    }
   }
-  return [{
-    errCode: err.response.data.errors[0].error,
-    errRef: 'regNumber',
-    errMsg,
-  }];
+
+  return [
+    {
+      errCode,
+      errRef: 'regNumber',
+      errMsg,
+    },
+  ];
 };
 
 module.exports = {
