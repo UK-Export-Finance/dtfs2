283f1fdffd8a1d78f4918eb1f7fc77f54961c7f9
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2023-07-20 16:31:32 +0100

    fix(regex): fixed wrong REGEX name in portal-api

--- portal-api/src/constants/regex.js
+++ portal-api/src/constants/regex.js
@@ -4,4 +4,4 @@ exports.INDUSTRY_SECTOR_ID = /^[0-9]{1,8}$/;
 
 exports.COUNTRY_CODE = /^[A-Z]{3}$/;
 
-exports.COMPANIES_HOUSE_NUMBER = /^(([A-Z]{2}|[A-Z]\d{1}|\d{2})(\d{5,6}|\d{4,5}[A-Z]))$/;
+exports.COMPANIES_HOUSE_NUMBER_REGEX = /^(([A-Z]{2}|[A-Z]\d{1}|\d{2})(\d{5,6}|\d{4,5}[A-Z]))$/;
