29172a873649365cee675baaf3eff91c80128466
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2023-07-25 15:08:04 +0100

    security: remove vulnerable dependency

--- trade-finance-manager-api/graphql-query-tests/mutation-update-facility-risk-profile.api-test.js
+++ trade-finance-manager-api/graphql-query-tests/mutation-update-facility-risk-profile.api-test.js
@@ -1,4 +1,3 @@
-const { createTestClient } = require('apollo-server-testing');
 const { ApolloServer } = require('apollo-server-express');
 const { applyMiddleware } = require('graphql-middleware');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
@@ -18,21 +17,17 @@ const UPDATE_FACILITY_RISK_PROFILE = gql`
 `;
 
 describe('graphql mutation - update facility risk profile', () => {
-  let query;
+  let server;
 
   beforeAll(() => {
     const schema = makeExecutableSchema({ typeDefs, resolvers });
     const schemaWithMiddleware = applyMiddleware(schema);
 
-    const server = new ApolloServer({
+    server = new ApolloServer({
       typeDefs,
       resolvers,
       schema: schemaWithMiddleware,
     });
-
-    // use the test server to create a query function
-    const { query: doQuery } = createTestClient(server);
-    query = doQuery;
   });
 
   it('should return updated risk profile', async () => {
@@ -43,7 +38,7 @@ describe('graphql mutation - update facility risk profile', () => {
       },
     };
 
-    const { data } = await query({
+    const { data } = await server.executeOperation({
       query: UPDATE_FACILITY_RISK_PROFILE,
       variables: mutationVars,
     });
