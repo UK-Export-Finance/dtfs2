1b7eb952be099cd2c1d66dd5c08180073828e1a3
ChristianMcc <37540516+ChristianMcc@users.noreply.github.com>
2023-07-28 17:42:22 +0100

    fix(DTFS2-5656): Add middleware to strip _csrf token from body of requests (#1879)
    Co-authored-by: Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
    Co-authored-by: Abhi Markan <32125108+abhi-markan@users.noreply.github.com>

---
+++ trade-finance-manager-api/api-tests/v1/middleware/remove-csrf-token.api-test.js
@@ -0,0 +1,23 @@
+const removeCsrfToken = require('../../../src/v1/middleware/remove-csrf-token');
+const { mockReq, mockReqWithCsrf, mockRes, mockNext } = require('../mocks');
+
+describe('routes/middleware/remove-csrf-token', () => {
+  const next = mockNext;
+  const res = mockRes();
+
+  it('Request with csrf token should have csrf', () => {
+    const reqWithCsrfToken = mockReqWithCsrf();
+
+    removeCsrfToken(reqWithCsrfToken, res, next);
+
+    expect(reqWithCsrfToken?.body?._csrf).toBeUndefined();
+  });
+
+  it('Request without csrf token should be unaffected', () => {
+    const req = mockReq();
+
+    removeCsrfToken(req, res, next);
+
+    expect(req?.body?._csrf).toBeUndefined();
+  });
+});
