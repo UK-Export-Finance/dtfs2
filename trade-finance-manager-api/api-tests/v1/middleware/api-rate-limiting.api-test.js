430bf78ff3794bd859147e36bdac8ef85081394b
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-08-04 09:58:50 +0100

    refactor(SR-8): lint

--- trade-finance-manager-api/api-tests/v1/middleware/api-rate-limiting.api-test.js
+++ trade-finance-manager-api/api-tests/v1/middleware/api-rate-limiting.api-test.js
@@ -16,20 +16,20 @@ describe('api rate limiting', () => {
     sendRequestTimes = (numberOfRequestsToSend) => Promise.allSettled(Array.from({ length: numberOfRequestsToSend }, () => as(null).put(null).to('/v1/deals/submit')));
   });
 
-    // TODO SR-8: should we check this is not per endpoint? Should we check this is per ip?
-    it('returns a 429 response if more than RATE_LIMIT_THRESHOLD requests are made from the same IP to the same endpoint in 1 minute', async () => {
-      await sendRequestTimes(rateLimit);
+  // TODO SR-8: should we check this is not per endpoint? Should we check this is per ip?
+  it('returns a 429 response if more than RATE_LIMIT_THRESHOLD requests are made from the same IP to the same endpoint in 1 minute', async () => {
+    await sendRequestTimes(rateLimit);
 
-      const responseAfterRateLimitExceeded = (await sendRequestTimes(1))[0].value;
+    const responseAfterRateLimitExceeded = (await sendRequestTimes(1))[0].value;
 
-      expect(responseAfterRateLimitExceeded.status).toBe(429);
-    });
+    expect(responseAfterRateLimitExceeded.status).toBe(429);
+  });
 
-    it('returns a 400 response if exactly RATE_LIMIT_THRESHOLD requests are made from the same IP to the same endpoint in 1 minute', async () => {
-      await sendRequestTimes(rateLimit - 1);
+  it('returns a 400 response if exactly RATE_LIMIT_THRESHOLD requests are made from the same IP to the same endpoint in 1 minute', async () => {
+    await sendRequestTimes(rateLimit - 1);
 
-      const responseThatMeetsRateLimit = (await sendRequestTimes(1))[0].value;
+    const responseThatMeetsRateLimit = (await sendRequestTimes(1))[0].value;
 
-      expect(responseThatMeetsRateLimit.status).toBe(400);
-    });
+    expect(responseThatMeetsRateLimit.status).toBe(400);
+  });
 });
--- trade-finance-manager-api/src/generateApp.js
+++ trade-finance-manager-api/src/generateApp.js
@@ -26,74 +26,74 @@ initScheduler();
 configurePassport(passport);
 
 const generateApp = () => {
-    const app = express();
+  const app = express();
 
-    app.use(createRateLimit());
-    app.use(seo);
-    app.use(security);
-    app.use(express.json());
-    app.use(compression());
-    app.use(removeCsrfToken);
-    app.use(healthcheck);
-    app.use(passport.initialize());
-    app.post('/v1/login', loginController.login);
-    app.use('/v1', openRouter);
-    app.use('/v1', authRouter);
-    app.use(graphQlRouter);
-    
-    // MongoDB sanitisation
-    app.use(mongoSanitise({
-      allowDots: true,
-    }));
-    
-    const schema = makeExecutableSchema({ typeDefs, resolvers });
-    const schemaWithMiddleware = applyMiddleware(schema, graphqlPermissions);
-    
-    const startServer = async () => {
-      const apolloServer = new ApolloServer({
-        typeDefs,
-        resolvers,
-        formatResponse: (response, context) => {
-          const { graphqlPermissions: permissions } = context.context;
-    
-          const isAuthenticated = (permissions.read || permissions.write);
-    
-          const dataKeys = Object.keys(response.data);
-    
-          // if there is no data, object will be null.
-          const hasData = response.data[dataKeys[0]];
-    
-          if (isAuthenticated && !hasData) {
-            return {
-              errors: [
-                { message: 'Server error returning query.' },
-              ],
-            };
-          }
-    
-          return response;
-        },
-        context: ({ req }) => ({
-          graphqlPermissions: graphqlKeyAuthentication(req),
-          user: req.user,
-        }),
-        schema: schemaWithMiddleware,
-      });
-      await apolloServer.start();
-      apolloServer.applyMiddleware({ app });
-    };
-    startServer();
-    
-    // Return 200 on get to / to confirm to Azure that
-    // the container has started successfully:
-    const rootRouter = express.Router();
-    rootRouter.get('/', (req, res) => {
-      res.status(200).send();
+  app.use(createRateLimit());
+  app.use(seo);
+  app.use(security);
+  app.use(express.json());
+  app.use(compression());
+  app.use(removeCsrfToken);
+  app.use(healthcheck);
+  app.use(passport.initialize());
+  app.post('/v1/login', loginController.login);
+  app.use('/v1', openRouter);
+  app.use('/v1', authRouter);
+  app.use(graphQlRouter);
+
+  // MongoDB sanitisation
+  app.use(mongoSanitise({
+    allowDots: true,
+  }));
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const schemaWithMiddleware = applyMiddleware(schema, graphqlPermissions);
+
+  const startServer = async () => {
+    const apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      formatResponse: (response, context) => {
+        const { graphqlPermissions: permissions } = context.context;
+
+        const isAuthenticated = (permissions.read || permissions.write);
+
+        const dataKeys = Object.keys(response.data);
+
+        // if there is no data, object will be null.
+        const hasData = response.data[dataKeys[0]];
+
+        if (isAuthenticated && !hasData) {
+          return {
+            errors: [
+              { message: 'Server error returning query.' },
+            ],
+          };
+        }
+
+        return response;
+      },
+      context: ({ req }) => ({
+        graphqlPermissions: graphqlKeyAuthentication(req),
+        user: req.user,
+      }),
+      schema: schemaWithMiddleware,
     });
-    
-    rootRouter.use('/v1/api-docs', swaggerUi.serve);
-    
-    app.use('/', rootRouter);
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+  };
+  startServer();
+
+  // Return 200 on get to / to confirm to Azure that
+  // the container has started successfully:
+  const rootRouter = express.Router();
+  rootRouter.get('/', (req, res) => {
+    res.status(200).send();
+  });
+
+  rootRouter.use('/v1/api-docs', swaggerUi.serve);
+
+  app.use('/', rootRouter);
 
   return app;
 };
