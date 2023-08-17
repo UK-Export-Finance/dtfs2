3e63515e3250c4eb628e6b156c7f53d553d078f2
Luke Marrai <Luke.Marrai@softwire.com>
2023-08-17 18:06:16 +0100

    feat(DTFS2-6623): improve update-a-user e2e tests

--- e2e-tests/portal/cypress/e2e/journeys/admin/create-user/update-a-user.spec.js
+++ e2e-tests/portal/cypress/e2e/journeys/admin/create-user/update-a-user.spec.js
@@ -1,9 +1,7 @@
 const {
   header, users, createUser, editUser,
 } = require('../../../pages');
-const MOCK_USERS = require('../../../../fixtures/users');
-
-const { ADMIN } = MOCK_USERS;
+const { ADMIN } = require('../../../../fixtures/users');
 
 context('Admin user updates an existing user', () => {
   const userToUpdate = {
@@ -18,55 +16,75 @@ context('Admin user updates an existing user', () => {
 
   beforeEach(() => {
     cy.removeUserIfPresent(userToUpdate, ADMIN);
+    cy.login(ADMIN);
+    header.users().click();
   });
 
-  it('Go to users and back', () => {
-    // Login and go to the dashboard
-    cy.login(ADMIN);
-    cy.url().should('include', '/dashboard/deals');
-    header.users().click();
-
-    // Cancel
+  it('clicking cancel from the users pages takes you to the deals dashboard', () => {
     users.cancel().click();
-
-    // Back to the dashboard
     cy.url().should('include', '/dashboard/deals');
   });
 
-  it('Create a user, then edit the user and change their role(s)', () => {
-    // login and go to dashboard
-    cy.login(ADMIN);
-    header.users().click();
+  context('given a user already exists', () => {
+    const openPageToEdit = (user) => users.row(user).username().click();
 
-    // add user
-    users.addUser().click();
-    userToUpdate.roles.forEach((role) => {
-      createUser.role(role).click();
-    });
-    createUser.username().type(userToUpdate.username);
-    createUser.manualPassword().click();
-    createUser.password().type(userToUpdate.password);
-    createUser.confirmPassword().type(userToUpdate.password);
-    createUser.firstname().type(userToUpdate.firstname);
-    createUser.surname().type(userToUpdate.surname);
-    createUser.bank().select(userToUpdate.bank);
-    createUser.createUser().click();
-
-    users.row(userToUpdate).username().click();
-
-    // switch off everything we selected before
-    userToUpdate.roles.forEach((role) => {
-      editUser.role(role).click();
+    beforeEach(() => {
+      users.addUser().click();
+      userToUpdate.roles.forEach((role) => {
+        createUser.role(role).click();
+      });
+      createUser.username().type(userToUpdate.username);
+      createUser.manualPassword().click();
+      createUser.password().type(userToUpdate.password);
+      createUser.confirmPassword().type(userToUpdate.password);
+      createUser.firstname().type(userToUpdate.firstname);
+      createUser.surname().type(userToUpdate.surname);
+      createUser.bank().select(userToUpdate.bank);
+      createUser.createUser().click();
     });
 
-    editUser.role('checker').click();
-    editUser.firstname().type('{selectAll}{backspace}Homer');
-    editUser.surname().type('{selectAll}{backspace}Simpson');
+    it('changing their roles should display the new roles on the user dashboard', () => {
+      const newRole = 'checker';
+      openPageToEdit(userToUpdate);
 
-    editUser.save().click();
+      // switch off all of the users roles
+      userToUpdate.roles.forEach((role) => {
+        editUser.role(role).click();
+      });
 
-    users.row(userToUpdate).roles().invoke('text').then((text) => {
-      expect(text.trim()).to.equal('checker');
+      editUser.role(newRole).click();
+
+      editUser.save().click();
+
+      users.row(userToUpdate).roles().invoke('text').then((text) => {
+        expect(text.trim()).to.equal(newRole);
+      });
+    });
+
+    it('changing their details should display the new details when the edit page is reloaded', () => {
+      const newFirstName = 'new first name';
+      const newSurname = 'new surname';
+      const newRole = 'checker';
+
+      openPageToEdit(userToUpdate);
+
+      editUser.firstname().type(`{selectAll}{backspace}${newFirstName}`);
+      editUser.surname().type(`{selectAll}{backspace}${newSurname}`);
+      // switch off all of the users roles
+      userToUpdate.roles.forEach((role) => {
+        editUser.role(role).click();
+      });
+      editUser.role(newRole).click();
+      editUser.save().click();
+
+      openPageToEdit(userToUpdate);
+
+      editUser.firstname().should('have.value', newFirstName);
+      editUser.surname().should('have.value', newSurname);
+      userToUpdate.roles.forEach((role) => {
+        editUser.role(role).should('not.be.checked');
+      });
+      editUser.role(newRole).should('be.checked');
     });
   });
 });
