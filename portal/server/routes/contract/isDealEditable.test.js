4817515e6583f44039cc873d2970811628f5d580
Alex <alexbramhill@gmail.com>
2023-08-17 15:49:46 +0100

    feat(dtfs2-6624): refactored isDealEditable tests

--- portal/server/routes/contract/isDealEditable.test.js
+++ portal/server/routes/contract/isDealEditable.test.js
@@ -1,69 +1,87 @@
 import isDealEditable from './isDealEditable';
 
 describe('isDealEditable', () => {
-  const mockMakerUser = { roles: ['maker'] };
+  const makerRole = ['maker'];
+  const nonMakerRoles = ['checker', 'admin', 'read-only'];
 
-  describe('when user is NOT maker', () => {
+  function isDealEditableMakerRoleTests(roleToCombineWithMaker) {
+    let roles = [...makerRole];
+    if (roleToCombineWithMaker) {
+      roles = [...roles, roleToCombineWithMaker];
+    }
+
+    const mockUser = { roles };
+
+    describe("when deal status is NOT `Draft` or `Further Maker's input required`", () => {
+      it('should return false', () => {
+        const mockAcceptedDeal = {
+          status: 'Accepted by UKEF (with conditions)',
+          details: {},
+        };
+
+        const result = isDealEditable(mockAcceptedDeal, mockUser);
+        expect(result).toEqual(false);
+      });
+    });
+
+    describe('when deal has been submitted', () => {
+      it('should return false', () => {
+        const mockDeal = {
+          status: 'Draft',
+          details: {
+            submissionDate: 12345678,
+          },
+        };
+
+        const result = isDealEditable(mockDeal, mockUser);
+        expect(result).toEqual(false);
+      });
+    });
+
+    describe('deal status `Draft`, deal not submitted', () => {
+      it('should return true', () => {
+        const mockDeal = {
+          status: 'Draft',
+          details: {},
+        };
+
+        const result = isDealEditable(mockDeal, mockUser);
+        expect(result).toEqual(true);
+      });
+    });
+
+    describe("deal status `Further Maker's input required`, deal not submitted", () => {
+      it('should return true', () => {
+        const mockDeal = {
+          status: "Further Maker's input required",
+          details: {},
+        };
+
+        const result = isDealEditable(mockDeal, mockUser);
+        expect(result).toEqual(true);
+      });
+    });
+  }
+
+  describe('when user is maker', () => {
+    isDealEditableMakerRoleTests();
+  });
+
+  describe.each(nonMakerRoles)('when user is maker and has additional role (additional role: %S)', (nonMakerRole) => {
+    isDealEditableMakerRoleTests(nonMakerRole);
+  });
+
+  describe.each(nonMakerRoles)('when user is NOT maker (role: %s)', (roleToCheck) => {
     it('should return false', () => {
       const mockDeal = {
-        status: 'Further Maker\'s input required',
+        status: "Further Maker's input required",
         details: {},
       };
 
-      const checkerUser = { roles: ['checker'] };
+      const checkerUser = { roles: [roleToCheck] };
 
       const result = isDealEditable(mockDeal, checkerUser);
       expect(result).toEqual(false);
-    });
-  });
-
-  describe('when deal status is NOT `Draft` or `Further Maker\'s input required`', () => {
-    it('should return false', () => {
-      const mockAcceptedDeal = {
-        status: 'Accepted by UKEF (with conditions)',
-        details: {},
-      };
-
-      const result = isDealEditable(mockAcceptedDeal, mockMakerUser);
-      expect(result).toEqual(false);
-    });
-  });
-
-  describe('when deal has been submitted', () => {
-    it('should return false', () => {
-      const mockDeal = {
-        status: 'Draft',
-        details: {
-          submissionDate: 12345678,
-        },
-      };
-
-      const result = isDealEditable(mockDeal, mockMakerUser);
-      expect(result).toEqual(false);
-    });
-  });
-
-  describe('when user is maker, deal status `Draft`, deal not submitted', () => {
-    it('should return true', () => {
-      const mockDeal = {
-        status: 'Draft',
-        details: {},
-      };
-
-      const result = isDealEditable(mockDeal, mockMakerUser);
-      expect(result).toEqual(true);
-    });
-  });
-
-  describe('when user is maker, deal status `Further Maker\'s input required`, deal not submitted', () => {
-    it('should return true', () => {
-      const mockDeal = {
-        status: 'Further Maker\'s input required',
-        details: {},
-      };
-
-      const result = isDealEditable(mockDeal, mockMakerUser);
-      expect(result).toEqual(true);
     });
   });
 });
