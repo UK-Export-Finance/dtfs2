0c7499273d84c68631af9b3423b1289178a67ae3
Alex <alexbramhill@gmail.com>
2023-08-17 16:37:49 +0100

    feat(dtfs2-6624): refactored clone-deal-link tests

--- portal/component-tests/contract/components/clone-deal-link.component-test.js
+++ portal/component-tests/contract/components/clone-deal-link.component-test.js
@@ -4,49 +4,55 @@ const component = 'contract/components/clone-deal-link.njk';
 const render = componentRenderer(component);
 
 describe(component, () => {
-  describe('when viewed a maker', () => {
-    it('should be enabled', () => {
-      const user = { roles: ['maker'] };
-      const deals = [
-        { _id: '61f6fbaea2460c018a4189d1', status: 'Draft' },
-        { _id: '61f6fbaea2460c018a4189d2', status: "Further Maker's input required" },
-        { _id: '61f6fbaea2460c018a4189d3', status: 'Submitted' },
-        { _id: '61f6fbaea2460c018a4189d4', status: 'Rejected by UKEF' },
-        { _id: '61f6fbaea2460c018a4189d5', status: 'Abandoned' },
-        { _id: '61f6fbaea2460c018a4189d6', status: 'Acknowledged' },
-        { _id: '61f6fbaea2460c018a4189d7', status: 'Accepted by UKEF (without conditions)' },
-        { _id: '61f6fbaea2460c018a4189d8', status: 'Accepted by UKEF (with conditions)' },
-        { _id: '61f6fbaea2460c018a4189d9', status: "Ready for Checker's approval" },
-      ];
+  const makerRole = ['maker'];
+  const nonMakerRoles = ['checker', 'admin', 'read-only'];
+  const deals = [
+    { _id: '61f6fbaea2460c018a4189d1', status: 'Draft' },
+    { _id: '61f6fbaea2460c018a4189d2', status: "Further Maker's input required" },
+    { _id: '61f6fbaea2460c018a4189d3', status: 'Submitted' },
+    { _id: '61f6fbaea2460c018a4189d4', status: 'Rejected by UKEF' },
+    { _id: '61f6fbaea2460c018a4189d5', status: 'Abandoned' },
+    { _id: '61f6fbaea2460c018a4189d6', status: 'Acknowledged' },
+    { _id: '61f6fbaea2460c018a4189d7', status: 'Accepted by UKEF (without conditions)' },
+    { _id: '61f6fbaea2460c018a4189d8', status: 'Accepted by UKEF (with conditions)' },
+    { _id: '61f6fbaea2460c018a4189d9', status: "Ready for Checker's approval" },
+  ];
 
+  function makerRoleTests(roleToCombineWithMaker) {
+    let roles = [...makerRole];
+    if (roleToCombineWithMaker) {
+      roles = [...roles, roleToCombineWithMaker];
+    }
+    const mockUser = { roles };
+
+    it('should be enabled', () => {
       for (const deal of deals) {
-        const wrapper = render({ user, deal });
-        wrapper.expectLink('[data-cy="clone-deal-link"]')
-          .toLinkTo(`/contract/${deal._id}/clone/before-you-start`, 'Clone');
+        const wrapper = render({ user: mockUser, deal });
+        wrapper.expectLink('[data-cy="clone-deal-link"]').toLinkTo(`/contract/${deal._id}/clone/before-you-start`, 'Clone');
       }
     });
+  }
+
+  function nonMakerRoleTests(nonMakerRole) {
+    it('should not render at all', () => {
+      const mockUser = { roles: [nonMakerRole] };
+
+      for (const deal of deals) {
+        const wrapper = render({ user: mockUser, deal });
+        wrapper.expectLink('[data-cy="clone-deal-link"]').notToExist();
+      }
+    });
+  }
+
+  describe('when viewed with the role maker', () => {
+    makerRoleTests();
   });
 
-  describe('when viewed by a checker', () => {
-    it('should not render at all', () => {
-      const user = { roles: ['checker'] };
-      const deals = [
-        { _id: '61f6fbaea2460c018a4189d1', status: 'Draft' },
-        { _id: '61f6fbaea2460c018a4189d2', status: "Further Maker's input required" },
-        { _id: '61f6fbaea2460c018a4189d3', status: 'Submitted' },
-        { _id: '61f6fbaea2460c018a4189d4', status: 'Rejected by UKEF' },
-        { _id: '61f6fbaea2460c018a4189d5', status: 'Abandoned' },
-        { _id: '61f6fbaea2460c018a4189d6', status: 'Acknowledged' },
-        { _id: '61f6fbaea2460c018a4189d7', status: 'Accepted by UKEF (without conditions)' },
-        { _id: '61f6fbaea2460c018a4189d8', status: 'Accepted by UKEF (with conditions)' },
-        { _id: '61f6fbaea2460c018a4189d9', status: "Ready for Checker's approval" },
-      ];
+  describe.each(nonMakerRoles)('when viewed with roles maker and %s', (nonMakerRole) => {
+    makerRoleTests(nonMakerRole);
+  });
 
-      for (const deal of deals) {
-        const wrapper = render({ user, deal });
-        wrapper.expectLink('[data-cy="clone-deal-link"]')
-          .notToExist();
-      }
-    });
+  describe.each(nonMakerRoles)('when viewed with the role %s', (nonMakerRole) => {
+    nonMakerRoleTests(nonMakerRole);
   });
 });
