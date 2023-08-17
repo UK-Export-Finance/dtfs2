947a937e45949f5c76c5743f83b43936b630b79a
Alex <alexbramhill@gmail.com>
2023-08-17 12:33:07 +0100

    feat(dtfs2-6624): add read-only for getUserRoles

--- portal/server/helpers/getUserRoles.js
+++ portal/server/helpers/getUserRoles.js
@@ -2,10 +2,10 @@ const getUserRoles = (roles) => {
   const isMaker = roles.includes('maker');
   const isChecker = roles.includes('checker');
   // TODO DTFS2-6624 Remove is read-only if it serves no use
-  const isReadOnly = roles.includes('read-only')
-  const isReadOnlyForMaker = roles.includes('read-only') && !roles.includes('maker') 
-  const isReadOnlyForChecker =  roles.includes('read-only') && !roles.includes('checker')
-  
+  const isReadOnly = roles.includes('read-only');
+  const isReadOnlyForMaker = roles.includes('read-only') && !roles.includes('maker');
+  const isReadOnlyForChecker = roles.includes('read-only') && !roles.includes('checker');
+
   return {
     isMaker,
     isChecker,
