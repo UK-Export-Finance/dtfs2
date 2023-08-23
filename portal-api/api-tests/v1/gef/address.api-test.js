f3f00e001009d8ca3d185d4b4df2e79d796fdf67
Luke Marrai <Luke.Marrai@softwire.com>
2023-08-23 13:54:12 +0100

    feat(DTFS2-6626): gef address and company tests

---
+++ portal-api/api-tests/v1/gef/address.api-test.js
@@ -0,0 +1,30 @@
+const app = require('../../../src/createApp');
+const { UKEF_OPERATIONS, MAKER, DATA_ADMIN, ADMIN } = require('../../../src/v1/roles/roles');
+const testUserCache = require('../../api-test-users');
+const { withClientAuthenticationTests } = require('../../common-tests/client-authentication-tests');
+const { withRoleAuthorisationTests } = require('../../common-tests/role-authorisation-tests');
+
+const { as, get } = require('../../api')(app);
+
+// TODO DTFS2-6626: this sends out real API calls to an external API - do we want to prevent this?
+describe('/v1/gef/address/:postcode', () => {
+  const aPostcodeAddressUrl = '/v1/gef/address/W6 9PF';
+  let testUsers;
+
+  beforeAll(async () => {
+    testUsers = await testUserCache.initialise(app);
+  });
+
+  withClientAuthenticationTests({
+    makeRequestWithoutAuthHeader: () => get(aPostcodeAddressUrl),
+    makeRequestWithAuthHeader: (authHeader) => get(aPostcodeAddressUrl, { headers: { Authorization: authHeader } })
+  });
+
+  withRoleAuthorisationTests({
+    allowedRoles: [UKEF_OPERATIONS, MAKER, DATA_ADMIN, ADMIN],
+    getUserWithRole: (role) => testUsers().withRole(role).one(),
+    getUserWithoutAnyRoles: () => testUsers().withoutRole().one(),
+    makeRequestAsUser: (user) => as(user).get(aPostcodeAddressUrl),
+    successStatusCode: 200,
+  });
+});
---
+++ portal-api/api-tests/v1/gef/company.api-test.js
@@ -0,0 +1,30 @@
+const app = require('../../../src/createApp');
+const { UKEF_OPERATIONS, MAKER, DATA_ADMIN, ADMIN } = require('../../../src/v1/roles/roles');
+const testUserCache = require('../../api-test-users');
+const { withClientAuthenticationTests } = require('../../common-tests/client-authentication-tests');
+const { withRoleAuthorisationTests } = require('../../common-tests/role-authorisation-tests');
+
+const { as, get } = require('../../api')(app);
+
+// TODO DTFS2-6626: this sends out real API calls to an external API - do we want to prevent this?
+describe('/v1/gef/company/:number', () => {
+  const aCompanyNumberUrl = '/v1/gef/company/06771815';
+  let testUsers;
+
+  beforeAll(async () => {
+    testUsers = await testUserCache.initialise(app);
+  });
+
+  withClientAuthenticationTests({
+    makeRequestWithoutAuthHeader: () => get(aCompanyNumberUrl),
+    makeRequestWithAuthHeader: (authHeader) => get(aCompanyNumberUrl, { headers: { Authorization: authHeader } })
+  });
+
+  withRoleAuthorisationTests({
+    allowedRoles: [UKEF_OPERATIONS, MAKER, DATA_ADMIN, ADMIN],
+    getUserWithRole: (role) => testUsers().withRole(role).one(),
+    getUserWithoutAnyRoles: () => testUsers().withoutRole().one(),
+    makeRequestAsUser: (user) => as(user).get(aCompanyNumberUrl),
+    successStatusCode: 200,
+  });
+});
