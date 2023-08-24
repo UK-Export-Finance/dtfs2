0ec19304845f7024f47b1ba94f0d8093988e26ff
Luke Marrai <Luke.Marrai@softwire.com>
2023-08-24 02:14:37 +0100

    feat(DTFS2-6626): gef files

--- portal-api/api-tests/v1/gef/files.api-test.js
+++ portal-api/api-tests/v1/gef/files.api-test.js
@@ -3,9 +3,12 @@ const { ObjectId } = require('mongodb');
 const wipeDB = require('../../wipeDB');
 const app = require('../../../src/createApp');
 const testUserCache = require('../../api-test-users');
-const { as } = require('../../api')(app);
+const { as, get } = require('../../api')(app);
 const { uploadFile, deleteFile, readFile } = require('../../../src/drivers/fileshare');
 const CONSTANTS = require('../../../src/constants');
+const { withClientAuthenticationTests } = require('../../common-tests/client-authentication-tests');
+const { withRoleAuthorisationTests } = require('../../common-tests/role-authorisation-tests');
+const { MAKER, CHECKER, READ_ONLY, EDITOR, DATA_ADMIN, ADMIN, UKEF_OPERATIONS } = require('../../../src/v1/roles/roles');
 
 const baseUrl = '/v1/gef/files';
 const collectionName = 'files';
@@ -27,15 +30,17 @@ jest.mock('../../../src/drivers/fileshare', () => ({
 }));
 
 describe(baseUrl, () => {
+  const testBankName = 'Barclays Bank';
   let aMaker;
   let aChecker;
   let invalidMaker;
   let mockDeal;
+  let testUsers;
 
   beforeAll(async () => {
-    const testUsers = await testUserCache.initialise(app);
-    aMaker = testUsers().withRole('maker').withBankName('UKEF test bank (Delegated)').one();
-    aChecker = testUsers().withRole('checker').withBankName('UKEF test bank (Delegated)').one();
+    testUsers = await testUserCache.initialise(app);
+    aMaker = testUsers().withRole('maker').withBankName(testBankName).one();
+    aChecker = testUsers().withRole('checker').withBankName(testBankName).one();
     invalidMaker = testUsers().withRole('maker').withBankName('HSBC').one();
 
     await wipeDB.wipe([applicationCollectionName]);
@@ -147,9 +152,27 @@ describe(baseUrl, () => {
   });
 
   describe(`GET ${baseUrl}/:id`, () => {
-    it('rejects requests that do not present a valid Authorization token', async () => {
-      const { status } = await as().get(`${baseUrl}/1`);
-      expect(status).toEqual(401);
+    let oneFileUrl;
+
+    beforeEach(async () => {
+      const { body } = await as(aMaker)
+        .postMultipartForm({ parentId: mockDeal.body._id }, validFiles)
+        .to(baseUrl);
+      const createdId = body[0]._id;
+      oneFileUrl = `${baseUrl}/${createdId}`;
+    });
+
+    withClientAuthenticationTests({
+      makeRequestWithoutAuthHeader: () => get(oneFileUrl),
+      makeRequestWithAuthHeader: (authHeader) => get(oneFileUrl, { headers: { Authorization: authHeader } })
+    });
+
+    withRoleAuthorisationTests({
+      allowedRoles: [UKEF_OPERATIONS, MAKER, CHECKER, READ_ONLY, EDITOR, DATA_ADMIN, ADMIN],
+      getUserWithRole: (role) => testUsers().withBankName(testBankName).withRole(role).one(),
+      getUserWithoutAnyRoles: () => testUsers().withBankName(testBankName).withoutAnyRoles().one(),
+      makeRequestAsUser: (user) => as(user).get(oneFileUrl),
+      successStatusCode: 200,
     });
 
     it('returns 404 if files is not found', async () => {
@@ -158,21 +181,13 @@ describe(baseUrl, () => {
     });
 
     it('rejects requests that do not have access to deal', async () => {
-      const { body } = await as(aMaker)
-        .postMultipartForm({ parentId: mockDeal.body._id }, validFiles)
-        .to(baseUrl);
-
-      const { status } = await as(invalidMaker).get(`${baseUrl}/${body[0]._id}`);
+      const { status } = await as(invalidMaker).get(oneFileUrl);
 
       expect(status).toEqual(401);
     });
 
     it('returns file information when available', async () => {
-      const { body } = await as(aMaker)
-        .postMultipartForm({ parentId: mockDeal.body._id }, validFiles)
-        .to(baseUrl);
-
-      const { status } = await as(aMaker).get(`${baseUrl}/${body[0]._id}`);
+      const { status } = await as(aMaker).get(oneFileUrl);
 
       expect(status).toEqual(200);
     });
@@ -199,7 +214,7 @@ describe(baseUrl, () => {
       expect(status).toEqual(401);
     });
 
-    it('rejects requests that aare not by a maker', async () => {
+    it('rejects requests that are not by a maker', async () => {
       const { body } = await as(aMaker)
         .postMultipartForm({ parentId: mockDeal.body._id }, validFiles)
         .to(baseUrl);
@@ -233,9 +248,27 @@ describe(baseUrl, () => {
   });
 
   describe(`GET ${baseUrl}/:id/download`, () => {
-    it('rejects requests that do not present a valid Authorization token', async () => {
-      const { status } = await as().get(`${baseUrl}/1/download`);
-      expect(status).toEqual(401);
+    let oneFileDownloadUrl;
+
+    beforeEach(async () => {
+      const { body } = await as(aMaker)
+        .postMultipartForm({ parentId: mockDeal.body._id }, validFiles)
+        .to(baseUrl);
+      const createdId = body[0]._id;
+      oneFileDownloadUrl = `${baseUrl}/${createdId}/download`;
+    });
+
+    withClientAuthenticationTests({
+      makeRequestWithoutAuthHeader: () => get(oneFileDownloadUrl),
+      makeRequestWithAuthHeader: (authHeader) => get(oneFileDownloadUrl, { headers: { Authorization: authHeader } })
+    });
+
+    withRoleAuthorisationTests({
+      allowedRoles: [UKEF_OPERATIONS, MAKER, CHECKER, READ_ONLY, EDITOR, DATA_ADMIN, ADMIN],
+      getUserWithRole: (role) => testUsers().withBankName(testBankName).withRole(role).one(),
+      getUserWithoutAnyRoles: () => testUsers().withBankName(testBankName).withoutAnyRoles().one(),
+      makeRequestAsUser: (user) => as(user).get(oneFileDownloadUrl),
+      successStatusCode: 200,
     });
 
     it('returns 404 if files is not found', async () => {
@@ -244,11 +277,7 @@ describe(baseUrl, () => {
     });
 
     it('rejects requests that do not have access to deal', async () => {
-      const { body } = await as(aMaker)
-        .postMultipartForm({ parentId: mockDeal.body._id }, validFiles)
-        .to(baseUrl);
-
-      const { status } = await as(invalidMaker).get(`${baseUrl}/${body[0]._id}/download`);
+      const { status } = await as(invalidMaker).get(oneFileDownloadUrl);
 
       expect(status).toEqual(401);
     });
@@ -267,13 +296,9 @@ describe(baseUrl, () => {
     });
 
     it('posts 500 if there is an error', async () => {
-      const { body } = await as(aMaker)
-        .postMultipartForm({ parentId: mockDeal.body._id }, validFiles)
-        .to(baseUrl);
-
       readFile.mockRejectedValueOnce(new Error('mock error'));
 
-      const { status } = await as(aMaker).get(`${baseUrl}/${body[0]._id}/download`);
+      const { status } = await as(aMaker).get(oneFileDownloadUrl);
 
       expect(status).toEqual(500);
     });
