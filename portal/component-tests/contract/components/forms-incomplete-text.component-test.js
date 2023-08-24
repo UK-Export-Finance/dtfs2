a61c761fa8b7b2f0d5b2415293f9816e20fdbe26
Alex <alexbramhill@gmail.com>
2023-08-24 16:33:52 +0100

    feat(dtfs2-6624): add forms incomplete text component tests

--- portal/component-tests/contract/components/forms-incomplete-text.component-test.js
+++ portal/component-tests/contract/components/forms-incomplete-text.component-test.js
@@ -4,9 +4,12 @@ const component = 'contract/components/forms-incomplete-text.njk';
 const render = componentRenderer(component);
 
 describe(component, () => {
+  const makerRole = 'maker';
+  const nonMakerRoles = ['checker', 'admin', 'read-only'];
+
   describe('when viewed by a maker', () => {
     it("should display when deal status=Draft and status=Further Maker's input required and canFullyCalculateDealSummary flag is false", () => {
-      const user = { roles: ['maker'] };
+      const user = { roles: makerRole };
       const deals = [
         { _id: 1, status: 'Draft' },
         { _id: 2, status: "Further Maker's input required" },
@@ -19,6 +22,17 @@ describe(component, () => {
         const expected = 'Warning: Only the completed facilities will be processed in the financial summary data below.';
         wrapper.expectText('[data-cy="forms-incomplete"]').toRead(expected);
       }
+    });
+  });
+
+  describe.each(nonMakerRoles)('when viewed by a %s', (nonMakerRole) => {
+    it('should not display', () => {
+      const user = { roles: [nonMakerRole] };
+      const deal = { _id: 1, status: 'Draft' };
+      const canFullyCalculateDealSummary = false;
+
+      const wrapper = render({ user, deal, canFullyCalculateDealSummary });
+      wrapper.expectElement('[data-cy="forms-incomplete"]').notToExist();
     });
   });
 });
