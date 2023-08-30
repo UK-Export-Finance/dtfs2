a259ebf783faca02dabe629b5f90b21ace8c82b0
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-08-30 12:37:45 +0100

    refactor(DTFS2-6625): lint

--- portal/api-tests/common-tests/role-validation-api-tests.js
+++ portal/api-tests/common-tests/role-validation-api-tests.js
@@ -2,12 +2,12 @@ jest.mock('csurf', () => () => (req, res, next) => next());
 jest.mock('../../server/routes/middleware/csrf', () => ({
   ...(jest.requireActual('../../server/routes/middleware/csrf')),
   csrfToken: () => (req, res, next) => next(),
-}))
+}));
 jest.mock('../../server/api', () => ({
   ...(jest.requireActual('../../server/api')),
   login: jest.fn(),
   validateToken: () => true,
-}))
+}));
 
 const { login } = require('../../server/api');
 const { NON_ADMIN_ROLES, UKEF_ADMIN_ROLES } = require('../../server/constants');
@@ -20,10 +20,10 @@ const password = 'mock password';
 const loginAsRole = (role) => () => ({
   success: true,
   token: 'mock token',
-  user: { roles: [ role ]},
+  user: { roles: [role] },
 });
 
-const extractSessionCookie = (res) => res.headers['set-cookie'].pop().split(";")[0];
+const extractSessionCookie = (res) => res.headers['set-cookie'].pop().split(';')[0];
 
 const withRoleValidationApiTests = ({
   makeRequestWithHeaders,
@@ -38,44 +38,46 @@ const withRoleValidationApiTests = ({
     if (allowedRoles.length) {
       describe('allowed roles', () => {
         it.each(allowedRoles)(
-        `returns a ${successCode} response if the user has the '%s' role`, 
-        async (allowedRole) => {
-          login.mockImplementation(loginAsRole(allowedRole))
+          `returns a ${successCode} response if the user has the '%s' role`,
+          async (allowedRole) => {
+            login.mockImplementation(loginAsRole(allowedRole));
 
-          const sessionCookie = await post({ email, password }).to('/login').then(extractSessionCookie);
+            const sessionCookie = await post({ email, password }).to('/login').then(extractSessionCookie);
 
-          const response = await makeRequestWithHeaders({ 'Cookie': [sessionCookie] });
+            const response = await makeRequestWithHeaders({ Cookie: [sessionCookie] });
 
-          expect(response.status).toBe(successCode);
+            expect(response.status).toBe(successCode);
 
-          if (successHeaders) {
-            for (const [key, value] of Object.entries(successHeaders)) {
-              expect(response.headers[key]).toBe(value);
+            if (successHeaders) {
+              for (const [key, value] of Object.entries(successHeaders)) {
+                expect(response.headers[key]).toBe(value);
+              }
             }
-          }
-        });
+          },
+        );
       });
     }
 
     if (disallowedRoles.length) {
       describe('disallowed roles', () => {
         it.each(disallowedRoles)(
-        `returns a 302 response if the user has the '%s' role`, 
-        async (disallowedRole) => {
-          login.mockImplementation(loginAsRole(disallowedRole))
+          'returns a 302 response if the user has the \'%s\' role',
+          async (disallowedRole) => {
+            login.mockImplementation(loginAsRole(disallowedRole));
 
-          const sessionCookie = await post({ email, password }).to('/login').then(extractSessionCookie);
+            const sessionCookie = await post({ email, password }).to('/login').then(extractSessionCookie);
 
-          const response = await makeRequestWithHeaders({ 'Cookie': [sessionCookie] });
+            const response = await makeRequestWithHeaders({ Cookie: [sessionCookie] });
 
-          expect(response.status).toBe(302);
-          expect(response.headers.location).toBe('/');
-        });
+            expect(response.status).toBe(302);
+            expect(response.headers.location).toBe('/');
+          },
+        );
       });
     }
   });
-}
+};
 
 module.exports = {
-  withRoleValidationApiTests
-}
+  withRoleValidationApiTests,
+};
--- portal/api-tests/login-routes.api-test.js
+++ portal/api-tests/login-routes.api-test.js
@@ -4,46 +4,46 @@ const { get, post } = require('./create-api').createApi(app);
 const { NON_ADMIN_ROLES } = require('../server/constants');
 
 describe('login routes', () => {
-    describe('GET /login', () => {
-        withRoleValidationApiTests({
-            makeRequestWithHeaders: (headers) => get('/login', {}, headers),
-            allowedNonAdminRoles: NON_ADMIN_ROLES,
-            successCode: 200,
-        })
-    })
+  describe('GET /login', () => {
+    withRoleValidationApiTests({
+      makeRequestWithHeaders: (headers) => get('/login', {}, headers),
+      allowedNonAdminRoles: NON_ADMIN_ROLES,
+      successCode: 200,
+    });
+  });
 
-    describe('POST /login', () => {
-        withRoleValidationApiTests({
-            makeRequestWithHeaders: (headers) => post({}, headers).to('/login'),
-            allowedNonAdminRoles: NON_ADMIN_ROLES,
-            successCode: 200,
-        })
-    })
+  describe('POST /login', () => {
+    withRoleValidationApiTests({
+      makeRequestWithHeaders: (headers) => post({}, headers).to('/login'),
+      allowedNonAdminRoles: NON_ADMIN_ROLES,
+      successCode: 200,
+    });
+  });
 
-    describe('GET /logout', () => {
-        withRoleValidationApiTests({
-            makeRequestWithHeaders: (headers) => get('/logout', {}, headers),
-            allowedNonAdminRoles: NON_ADMIN_ROLES,
-            successCode: 302,
-            successHeaders: { location: '/login' }
-        })
-    })
+  describe('GET /logout', () => {
+    withRoleValidationApiTests({
+      makeRequestWithHeaders: (headers) => get('/logout', {}, headers),
+      allowedNonAdminRoles: NON_ADMIN_ROLES,
+      successCode: 302,
+      successHeaders: { location: '/login' },
+    });
+  });
 
-    describe('GET /reset-password', () => {
-        withRoleValidationApiTests({
-            makeRequestWithHeaders: (headers) => get('/reset-password', {}, headers),
-            allowedNonAdminRoles: NON_ADMIN_ROLES,
-            successCode: 200,
-        })
-    })
+  describe('GET /reset-password', () => {
+    withRoleValidationApiTests({
+      makeRequestWithHeaders: (headers) => get('/reset-password', {}, headers),
+      allowedNonAdminRoles: NON_ADMIN_ROLES,
+      successCode: 200,
+    });
+  });
 
-    describe('POST /reset-password', () => {
-        withRoleValidationApiTests({
-            makeRequestWithHeaders: (headers) => post({}, headers).to('/reset-password'),
-            allowedNonAdminRoles: NON_ADMIN_ROLES,
-            successCode: 200,
-        })
-    })
+  describe('POST /reset-password', () => {
+    withRoleValidationApiTests({
+      makeRequestWithHeaders: (headers) => post({}, headers).to('/reset-password'),
+      allowedNonAdminRoles: NON_ADMIN_ROLES,
+      successCode: 200,
+    });
+  });
 
-    // TODO DTFS2-6625: figure out how to test /reset-password/:pwdResetToken
+  // TODO DTFS2-6625: figure out how to test /reset-password/:pwdResetToken
 });
