0077d41e1aa76f76f5c0dcca6f98e23704ca5e0f
Luke Marrai <Luke.Marrai@softwire.com>
2023-08-31 16:32:45 +0100

    refactor(DTFS2-6626): refactor roles constant file

--- portal-api/src/v1/roles/roles.js
+++ portal-api/src/v1/roles/roles.js
@@ -1,19 +1,12 @@
-const MAKER = 'maker';
-const CHECKER = 'checker';
-const ADMIN = 'admin';
-const UKEF_OPERATIONS = 'ukef_operations';
-const READ_ONLY = 'read-only';
-const EDITOR = 'editor';
-const DATA_ADMIN = 'data-admin';
-const INTERFACE = 'interface';
-
-module.exports = {
-  MAKER,
-  CHECKER,
-  ADMIN,
-  UKEF_OPERATIONS,
-  READ_ONLY,
-  EDITOR,
-  DATA_ADMIN,
-  INTERFACE,
+const ROLES = {
+  MAKER: 'maker',
+  CHECKER: 'checker',
+  ADMIN: 'admin',
+  UKEF_OPERATIONS: 'ukef_operations',
+  READ_ONLY: 'read-only',
+  EDITOR: 'editor',
+  DATA_ADMIN: 'data-admin',
+  INTERFACE: 'interface',
 };
+
+module.exports = ROLES;
