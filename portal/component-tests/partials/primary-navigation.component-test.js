ff52bb12f11ed454f070a1ee0616d7f2cfbf965b
Alex <alexbramhill@gmail.com>
2023-08-31 14:12:31 +0100

    feat(dtfs2-6624): updated primary navigation to remove reports link for read-only user

---
+++ portal/component-tests/partials/primary-navigation.component-test.js
@@ -0,0 +1,72 @@
+const pageRenderer = require('../pageRenderer');
+
+const page = '_partials/primary-navigation.njk';
+const render = pageRenderer(page);
+
+const rolesToDisplayAllNavigationItems = ['admin'];
+const rolesToDisplayHomeAndReportsNavigationItems = ['maker', 'checker'];
+const rolesToDisplayHomeNavigationItem = ['read-only'];
+describe(page, () => {
+  let wrapper;
+
+  function itRendersAHomeLink() {
+    it('renders a home link', () => {
+      wrapper.expectLink('[data-cy="dashboard"]').toLinkTo('/dashboard', 'Home');
+    });
+  }
+
+  function itRendersAReportsLink() {
+    it('renders a reports link', () => {
+      wrapper.expectLink('[data-cy="reports"]').toLinkTo('/reports', 'Reports');
+    });
+  }
+
+  function itDoesNotRenderAReportsLink() {
+    it('does not render a reports link', () => {
+      wrapper.expectLink('[data-cy="reports"]').notToExist();
+    });
+  }
+
+  function itRendersAUsersLink() {
+    it('renders a users link', () => {
+      wrapper.expectLink('[data-cy="users"]').toLinkTo('/admin/users', 'Users');
+    });
+  }
+
+  function itDoesNotRenderAUsersLink() {
+    it('does not render a users link', () => {
+      wrapper.expectLink('[data-cy="users"]').notToExist();
+    });
+  }
+
+  describe.each(rolesToDisplayAllNavigationItems)('viewed by a %s', (role) => {
+    const user = { roles: [role] };
+    beforeAll(() => {
+      wrapper = render({ user });
+    });
+
+    itRendersAHomeLink();
+    itRendersAReportsLink();
+    itRendersAUsersLink();
+  });
+
+  describe.each(rolesToDisplayHomeAndReportsNavigationItems)('viewed by a %s', (role) => {
+    const user = { roles: [role] };
+    beforeAll(() => {
+      wrapper = render({ user });
+    });
+    itRendersAHomeLink();
+    itRendersAReportsLink();
+    itDoesNotRenderAUsersLink();
+  });
+
+  describe.each(rolesToDisplayHomeNavigationItem)('viewed by a %s', (role) => {
+    const user = { roles: [role] };
+    beforeAll(() => {
+      wrapper = render({ user });
+    });
+    itRendersAHomeLink();
+    itDoesNotRenderAReportsLink();
+    itDoesNotRenderAUsersLink();
+  });
+});
