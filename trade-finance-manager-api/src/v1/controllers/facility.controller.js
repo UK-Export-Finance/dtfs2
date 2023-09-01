39b7deff54ed0e0471f8925d8cb318be8eed7b00
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-09-01 10:53:14 +0100

    feat(DTFS2-6182): add error handling

--- trade-finance-manager-api/src/v1/controllers/facility.controller.js
+++ trade-finance-manager-api/src/v1/controllers/facility.controller.js
@@ -3,18 +3,23 @@ const { findOneTfmDeal } = require('./deal.controller');
 const facilityMapper = require('../graphql-mappings/facility');
 
 const getFacility = async (req, res) => {
-  const { facilityId } = req.params;
-  const facility = await api.findOneFacility(facilityId);
+  try {
+    const { facilityId } = req.params;
+    const facility = await api.findOneFacility(facilityId);
 
-  const { dealId } = facility.facilitySnapshot;
-  const deal = await findOneTfmDeal(dealId);
+    const { dealId } = facility.facilitySnapshot;
+    const deal = await findOneTfmDeal(dealId);
 
-  const { dealSnapshot, tfm: dealTfm } = deal;
-  const tfmFacility = facilityMapper(facility, dealSnapshot, dealTfm);
+    const { dealSnapshot, tfm: dealTfm } = deal;
+    const tfmFacility = facilityMapper(facility, dealSnapshot, dealTfm);
 
-  return res.status(200).json({
-    facility: tfmFacility
-  });
+    return res.status(200).send({
+      facility: tfmFacility
+    });
+  } catch (error) {
+    console.error('Error fetching facility %O', error);
+    return res.status(500).send(error.message);
+  }
 };
 
 const findOneFacility = async (_id) => {
