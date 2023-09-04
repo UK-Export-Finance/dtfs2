81f6a6a6df8963f26c185304b2df0a4e027871fc
Alex <alexbramhill@gmail.com>
2023-09-04 13:46:25 +0100

    feat(dtfs2-6624): add cannot clone a deal e2e spec, rename test data

---
+++ e2e-tests/portal/cypress/e2e/journeys/read-only/clone-deal/cannot-clone-a-deal.spec.js
@@ -0,0 +1,49 @@
+const pages = require('../../../pages');
+const fullyCompletedDeal = require('../fixtures/dealFullyCompleted');
+const MOCK_USERS = require('../../../../fixtures/users');
+
+const { BANK1_READONLY1, BANK1_MAKER1, ADMIN } = MOCK_USERS;
+
+context('Clone a deal', () => {
+  let deal;
+  let dealId;
+  const dealFacilities = {
+    bonds: [],
+    loans: [],
+  };
+
+  beforeEach(() => {
+    cy.deleteDeals(ADMIN);
+    cy.insertOneDeal(fullyCompletedDeal, BANK1_MAKER1).then((insertedDeal) => {
+      deal = insertedDeal;
+      dealId = deal._id;
+
+      const { mockFacilities } = fullyCompletedDeal;
+
+      cy.createFacilities(dealId, mockFacilities, BANK1_MAKER1).then((createdFacilities) => {
+        const bonds = createdFacilities.filter((f) => f.type === 'Bond');
+        const loans = createdFacilities.filter((f) => f.type === 'Loan');
+
+        dealFacilities.bonds = bonds;
+        dealFacilities.loans = loans;
+      });
+    });
+  });
+
+  after(() => {
+    dealFacilities.bonds.forEach((facility) => {
+      cy.deleteFacility(facility._id, BANK1_MAKER1);
+    });
+
+    dealFacilities.loans.forEach((facility) => {
+      cy.deleteFacility(facility._id, BANK1_MAKER1);
+    });
+  });
+
+  describe('When a user creates a deal', () => {
+    it('should have no clone a deal link', () => {
+      cy.loginGoToDealPage(BANK1_READONLY1, deal);
+      pages.contract.cloneDealLink().should('not.exist');
+    });
+  });
+});
