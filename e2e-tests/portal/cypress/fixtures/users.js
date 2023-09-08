a0def34e32d1ce553deade532844536a57f05988
Alex <alexbramhill@gmail.com>
2023-09-08 13:57:48 +0100

    feat(dtfs2-6624): update user roles to be constants

--- e2e-tests/portal/cypress/fixtures/users.js
+++ e2e-tests/portal/cypress/fixtures/users.js
@@ -1,23 +1,24 @@
+const { MAKER, CHECKER, READ_ONLY } = require('../../../../portal/server/constants/roles');
 const MOCK_USERS = require('../../../../utils/mock-data-loader/portal/users');
 const MOCK_TFM_USERS = require('../../../../utils/mock-data-loader/tfm/mocks/users');
 
-const BANK1_MAKER1 = MOCK_USERS.find((user) => user.roles.includes('maker') && user.username === 'BANK1_MAKER1');
+const BANK1_MAKER1 = MOCK_USERS.find((user) => user.roles.includes(MAKER) && user.username === 'BANK1_MAKER1');
 
-const BANK1_MAKER2 = MOCK_USERS.find((user) => user.roles.includes('maker') && user.username === 'BANK1_MAKER2');
+const BANK1_MAKER2 = MOCK_USERS.find((user) => user.roles.includes(MAKER) && user.username === 'BANK1_MAKER2');
 
-const BANK2_MAKER2 = MOCK_USERS.find((user) => user.roles.includes('maker') && user.username === 'BANK2_MAKER2');
+const BANK2_MAKER2 = MOCK_USERS.find((user) => user.roles.includes(MAKER) && user.username === 'BANK2_MAKER2');
 
-const BANK3_GEF_MAKER1 = MOCK_USERS.find((user) => user.roles.includes('maker') && user.username === 'BANK3_GEF_MAKER1');
+const BANK3_GEF_MAKER1 = MOCK_USERS.find((user) => user.roles.includes(MAKER) && user.username === 'BANK3_GEF_MAKER1');
 
-const BANK1_CHECKER1 = MOCK_USERS.find((user) => user.roles.includes('checker') && user.username === 'BANK1_CHECKER1');
+const BANK1_CHECKER1 = MOCK_USERS.find((user) => user.roles.includes(CHECKER) && user.username === 'BANK1_CHECKER1');
 
-const BANK1_READ_ONLY1 = MOCK_USERS.find((user) => user.roles.includes('read-only') && user.username === 'BANK1_READ_ONLY1');
+const BANK1_READ_ONLY1 = MOCK_USERS.find((user) => user.roles.includes(READ_ONLY) && user.username === 'BANK1_READ_ONLY1');
 
-const ADMIN = MOCK_USERS.find((user) => user.username === 'ADMIN');
+const ADMIN = MOCK_USERS.find((user) => user.username === ADMIN);
 
-const UKEF_OPERATIONS = MOCK_USERS.find((user) => user.username === 'UKEF_OPERATIONS');
+const UKEF_OPERATIONS = MOCK_USERS.find((user) => user.username === UKEF_OPERATIONS);
 
-const EDITOR = MOCK_USERS.find((user) => user.username === 'EDITOR');
+const EDITOR = MOCK_USERS.find((user) => user.username === EDITOR);
 
 // TFM
 const UNDERWRITER_MANAGER = MOCK_TFM_USERS.find((user) => user.teams.includes('UNDERWRITER_MANAGERS'));
@@ -29,7 +30,7 @@ const USER_WITH_INJECTION = {
   firstname: 'test',
   surname: 'injection',
   bank: 'HSBC',
-  roles: ['maker'],
+  roles: [MAKER],
 };
 
 module.exports = {
