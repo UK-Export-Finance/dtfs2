c1035b3e155a9d32273f713482f4a107541720b6
Luke Marrai <Luke.Marrai@softwire.com>
2023-09-11 08:58:10 +0100

    refactor(DTFS2-6626): use test constants for company reg number and postcode

--- portal-api/api-tests/v1/gef/address.api-test.js
+++ portal-api/api-tests/v1/gef/address.api-test.js
@@ -3,12 +3,13 @@ const { MAKER, DATA_ADMIN, READ_ONLY, ADMIN } = require('../../../src/v1/roles/r
 const testUserCache = require('../../api-test-users');
 const { withClientAuthenticationTests } = require('../../common-tests/client-authentication-tests');
 const { withRoleAuthorisationTests } = require('../../common-tests/role-authorisation-tests');
+const { POSTCODE } = require('../../fixtures/postcode');
 
 const { as, get } = require('../../api')(app);
 
 // TODO DTFS2-6626: this sends out real API calls to an external API - do we want to prevent this?
 describe('GET /v1/gef/address/:postcode', () => {
-  const aPostcodeAddressUrl = '/v1/gef/address/W6 9PF';
+  const aPostcodeAddressUrl = `/v1/gef/address/${POSTCODE.VALID}`;
   let testUsers;
 
   beforeAll(async () => {
--- portal-api/api-tests/v1/gef/company.api-test.js
+++ portal-api/api-tests/v1/gef/company.api-test.js
@@ -3,12 +3,13 @@ const { MAKER, DATA_ADMIN, READ_ONLY, ADMIN } = require('../../../src/v1/roles/r
 const testUserCache = require('../../api-test-users');
 const { withClientAuthenticationTests } = require('../../common-tests/client-authentication-tests');
 const { withRoleAuthorisationTests } = require('../../common-tests/role-authorisation-tests');
+const { COMPANIES_HOUSE_NUMBER } = require('../../fixtures/companies-house-number');
 
 const { as, get } = require('../../api')(app);
 
 // TODO DTFS2-6626: this sends out real API calls to an external API - do we want to prevent this?
 describe('GET /v1/gef/company/:number', () => {
-  const aCompanyNumberUrl = '/v1/gef/company/06771815';
+  const aCompanyNumberUrl = `/v1/gef/company/${COMPANIES_HOUSE_NUMBER.VALID}`;
   let testUsers;
 
   beforeAll(async () => {
