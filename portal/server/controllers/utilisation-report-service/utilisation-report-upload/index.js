0b2e098bfc831f29046bb5d9435a8753c43da6f4
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2023-09-11 18:18:25 +0100

    feat(FN-965): clean up code and fix primary nav. move multer code to multer file

--- portal/server/controllers/utilisation-report-service/utilisation-report-upload/index.js
+++ portal/server/controllers/utilisation-report-service/utilisation-report-upload/index.js
@@ -1,21 +1,36 @@
 const getUtilisationReportUpload = async (req, res) => {
   try {
-    return res.render('utilisation-reporting-service/partials/utilisation-report-upload.njk');
+    return res.render('utilisation-reporting-service/utilisation-report-upload/utilisation-report-upload.njk', {
+      user: req.session.user,
+      primaryNav: 'utilisation_report_upload',
+    });
   } catch (error) {
-    return res.render('partials/problem-with-service.njk');
+    return res.render('_partials/problem-with-service.njk', { user: req.session.user });
   }
 };
 
 const postUtilisationReportUpload = async (req, res) => {
   try {
-    if (res.locals.fileUploadError) {
-      return res.render('utilisation-reporting-service/partials/utilisation-report-upload.njk', {
-        error: res.locals.fileUploadError,
+    if (res?.locals?.fileUploadError) {
+      const errorSummary = [
+        {
+          text: res?.locals?.fileUploadError?.text,
+          href: '#utilisation-report-file-upload',
+        },
+      ];
+      return res.render('utilisation-reporting-service/utilisation-report-upload/utilisation-report-upload.njk', {
+        validationError: res?.locals?.fileUploadError,
+        errorSummary,
+        user: req.session.user,
+        primaryNav: 'utilisation_report_upload',
       });
     }
-    return res.render('utilisation-reporting-service/partials/utilisation-report-upload.njk');
+    return res.render('utilisation-reporting-service/utilisation-report-upload/utilisation-report-upload.njk', {
+      user: req.session.user,
+      primaryNav: 'utilisation_report_upload',
+    });
   } catch (error) {
-    return res.render('partials/problem-with-service.njk');
+    return res.render('_partials/problem-with-service.njk', { user: req.session.user });
   }
 };
 
