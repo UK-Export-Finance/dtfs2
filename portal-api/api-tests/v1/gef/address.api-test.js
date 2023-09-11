f52cf06cc571ff0fff5be52fd02ce19bd533a87a
Luke Marrai <Luke.Marrai@softwire.com>
2023-09-11 08:58:17 +0100

    feat(DTFS2-6626): prevent external api calls in address and company api tests

--- portal-api/api-tests/v1/gef/address.api-test.js
+++ portal-api/api-tests/v1/gef/address.api-test.js
@@ -4,16 +4,21 @@ const testUserCache = require('../../api-test-users');
 const { withClientAuthenticationTests } = require('../../common-tests/client-authentication-tests');
 const { withRoleAuthorisationTests } = require('../../common-tests/role-authorisation-tests');
 const { POSTCODE } = require('../../fixtures/postcode');
+const { ordnanceSurvey } = require('../../../src/external-api/api');
 
 const { as, get } = require('../../api')(app);
 
-// TODO DTFS2-6626: this sends out real API calls to an external API - do we want to prevent this?
 describe('GET /v1/gef/address/:postcode', () => {
   const aPostcodeAddressUrl = `/v1/gef/address/${POSTCODE.VALID}`;
   let testUsers;
 
   beforeAll(async () => {
     testUsers = await testUserCache.initialise(app);
+  });
+
+  beforeEach(() => {
+    ordnanceSurvey.getAddressesByPostcode = jest.fn();
+    ordnanceSurvey.getAddressesByPostcode.mockResolvedValueOnce({ data: { results: [] } });
   });
 
   withClientAuthenticationTests({
--- portal-api/api-tests/v1/gef/company.api-test.js
+++ portal-api/api-tests/v1/gef/company.api-test.js
@@ -4,16 +4,36 @@ const testUserCache = require('../../api-test-users');
 const { withClientAuthenticationTests } = require('../../common-tests/client-authentication-tests');
 const { withRoleAuthorisationTests } = require('../../common-tests/role-authorisation-tests');
 const { COMPANIES_HOUSE_NUMBER } = require('../../fixtures/companies-house-number');
+const { POSTCODE } = require('../../fixtures/postcode');
+const { companiesHouse } = require('../../../src/external-api/api');
 
 const { as, get } = require('../../api')(app);
 
-// TODO DTFS2-6626: this sends out real API calls to an external API - do we want to prevent this?
 describe('GET /v1/gef/company/:number', () => {
   const aCompanyNumberUrl = `/v1/gef/company/${COMPANIES_HOUSE_NUMBER.VALID}`;
   let testUsers;
 
   beforeAll(async () => {
     testUsers = await testUserCache.initialise(app);
+  });
+
+  beforeEach(() => {
+    companiesHouse.getCompanyProfileByCompanyRegistrationNumber = jest.fn();
+    companiesHouse.getCompanyProfileByCompanyRegistrationNumber.mockResolvedValueOnce({
+      data: {
+        sic_codes: [],
+        company_number: COMPANIES_HOUSE_NUMBER.VALID,
+        company_name: 'Test Co.',
+        registered_office_address: {
+          organisation_name: 'Test Org',
+          address_line_1: '1 Test Road',
+          address_line_2: 'Test Place',
+          locality: 'London',
+          postal_code: POSTCODE.VALID,
+          country: 'Great Britain',
+        },
+      }
+    });
   });
 
   withClientAuthenticationTests({
