77a1572105482bc074da252cb90d3f1d1bdb0e38
Luke Marrai <Luke.Marrai@softwire.com>
2023-09-14 17:36:08 +0100

    fix(DTFS2-6628): remove export keyword

--- gef-ui/test-helpers/common-role-lists.js
+++ gef-ui/test-helpers/common-role-lists.js
@@ -1,11 +1,11 @@
 const ROLES = require('../server/constants/roles');
 
-export const getRolesListExcluding = (rolesToExclude) => {
+const getRolesListExcluding = (rolesToExclude) => {
   const roleValues = Object.values(ROLES);
   return roleValues.filter((role) => rolesToExclude.includes(role) === false);
 };
 
-export const COMMON_ROLE_COMBINATIONS = {
+const COMMON_ROLE_COMBINATIONS = {
   NON_MAKER_ROLES: getRolesListExcluding([ROLES.MAKER]),
   NON_CHECKER_ROLES: getRolesListExcluding([ROLES.CHECKER]),
   NON_MAKER_OR_CHECKER_ROLES: getRolesListExcluding([ROLES.MAKER, ROLES.CHECKER]),
