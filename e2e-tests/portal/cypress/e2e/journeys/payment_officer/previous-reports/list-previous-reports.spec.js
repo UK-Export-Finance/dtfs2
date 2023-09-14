2e81dba9ca05f96dde103bfd0a067d5ce1bde857
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-09-14 16:42:08 +0100

    feat(FN-980): format e2e tests

--- e2e-tests/portal/cypress/e2e/journeys/payment_officer/previous-reports/list-previous-reports.spec.js
+++ e2e-tests/portal/cypress/e2e/journeys/payment_officer/previous-reports/list-previous-reports.spec.js
@@ -6,58 +6,58 @@ const relativeURL = require('../../../relativeURL');
 const { BANK1_MAKER1 } = MOCK_USERS;
 
 context('List previous utilisation reports', () => {
-    describe('Selecting 2022 as target year from the side navigation', () => {
-      it('Should add a query parameter for targetYear when selected', () => {
-        // TODO FN-980 update to payment officer role
-        cy.login(BANK1_MAKER1);
-        cy.visit(relativeURL('/previous-reports'));
-  
-        previousReports.sideNavigation2022Item().click();
-  
-        previousReports.currentUrl().should('contain', '?targetYear=2022');
-      });
+  describe('Selecting 2022 as target year from the side navigation', () => {
+    it('Should add a query parameter for targetYear when selected', () => {
+      // TODO FN-980 update to payment officer role
+      cy.login(BANK1_MAKER1);
+      cy.visit(relativeURL('/previous-reports'));
 
-      it('Heading should display target year when selected', () => {
-        // TODO FN-980 update to payment officer role
-        cy.login(BANK1_MAKER1);
-        cy.visit(relativeURL('/previous-reports'));
-  
-        previousReports.sideNavigation2022Item().click();
-  
-        previousReports.mainHeading().should('contain', '2022');
-      });
+      previousReports.sideNavigation2022Item().click();
 
-      it('List items for January and February 2022 displayed', () => {
-        // TODO FN-980 update to payment officer role
-        cy.login(BANK1_MAKER1);
-        cy.visit(relativeURL('/previous-reports'));
-  
-        previousReports.sideNavigation2022Item().click();
-  
-        previousReports.listItemLinkNovember().should('exist');
-        previousReports.listItemLinkDecember().should('exist');
-      });
+      previousReports.currentUrl().should('contain', '?targetYear=2022');
     });
 
-    describe('Selecting 2021 as target year from the side navigation - no reports submitted', () => {
-      it('Should add a query parameter for targetYear when selected', () => {
-        // TODO FN-980 update to payment officer role
-        cy.login(BANK1_MAKER1);
-        cy.visit(relativeURL('/previous-reports'));
-  
-        previousReports.sideNavigation2021Item().click();
-  
-        previousReports.currentUrl().should('contain', '?targetYear=2021');
-      });
+    it('Heading should display target year when selected', () => {
+      // TODO FN-980 update to payment officer role
+      cy.login(BANK1_MAKER1);
+      cy.visit(relativeURL('/previous-reports'));
 
-      it('Body text should display no reports submitted text', () => {
-        // TODO FN-980 update to payment officer role
-        cy.login(BANK1_MAKER1);
-        cy.visit(relativeURL('/previous-reports'));
-  
-        previousReports.sideNavigation2021Item().click();
-  
-        previousReports.bodyText().should('contain', 'No reports have been submitted.');
-      });
+      previousReports.sideNavigation2022Item().click();
+
+      previousReports.mainHeading().should('contain', '2022');
+    });
+
+    it('List items for January and February 2022 displayed', () => {
+      // TODO FN-980 update to payment officer role
+      cy.login(BANK1_MAKER1);
+      cy.visit(relativeURL('/previous-reports'));
+
+      previousReports.sideNavigation2022Item().click();
+
+      previousReports.listItemLinkNovember().should('exist');
+      previousReports.listItemLinkDecember().should('exist');
     });
   });
+
+  describe('Selecting 2021 as target year from the side navigation - no reports submitted', () => {
+    it('Should add a query parameter for targetYear when selected', () => {
+      // TODO FN-980 update to payment officer role
+      cy.login(BANK1_MAKER1);
+      cy.visit(relativeURL('/previous-reports'));
+
+      previousReports.sideNavigation2021Item().click();
+
+      previousReports.currentUrl().should('contain', '?targetYear=2021');
+    });
+
+    it('Body text should display no reports submitted text', () => {
+      // TODO FN-980 update to payment officer role
+      cy.login(BANK1_MAKER1);
+      cy.visit(relativeURL('/previous-reports'));
+
+      previousReports.sideNavigation2021Item().click();
+
+      previousReports.bodyText().should('contain', 'No reports have been submitted.');
+    });
+  });
+});
--- e2e-tests/portal/cypress/e2e/pages/utilisation-report-service/previousReports.js
+++ e2e-tests/portal/cypress/e2e/pages/utilisation-report-service/previousReports.js
@@ -1,11 +1,11 @@
 const page = {
-    mainHeading: () => cy.get('[data-cy="main-heading"]'),
-    bodyText: () => cy.get('[data-cy="paragraph"]'),
-    sideNavigation2022Item: () => cy.get('[data-cy="side-navigation-2022"]'),
-    sideNavigation2021Item: () => cy.get('[data-cy="side-navigation-2021"]'),
-    listItemLinkNovember: () => cy.get('[data-cy="list-item-link-November"]'),
-    listItemLinkDecember: () => cy.get('[data-cy="list-item-link-December"]'),
-    currentUrl: () => cy.url(),
+  mainHeading: () => cy.get('[data-cy="main-heading"]'),
+  bodyText: () => cy.get('[data-cy="paragraph"]'),
+  sideNavigation2022Item: () => cy.get('[data-cy="side-navigation-2022"]'),
+  sideNavigation2021Item: () => cy.get('[data-cy="side-navigation-2021"]'),
+  listItemLinkNovember: () => cy.get('[data-cy="list-item-link-November"]'),
+  listItemLinkDecember: () => cy.get('[data-cy="list-item-link-December"]'),
+  currentUrl: () => cy.url(),
 };
 
 module.exports = page;
