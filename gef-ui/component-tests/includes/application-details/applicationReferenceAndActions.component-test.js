474640993e861848f66d778d65a231ff2dab9330
Luke Marrai <Luke.Marrai@softwire.com>
2023-09-15 13:58:36 +0100

    feat(DTFS2-6628): remove admin and read-only role checks form applicationReferenceAndActions

--- gef-ui/component-tests/includes/application-details/applicationReferenceAndActions.component-test.js
+++ gef-ui/component-tests/includes/application-details/applicationReferenceAndActions.component-test.js
@@ -1,6 +1,6 @@
 const pageRenderer = require('../../pageRenderer');
 const { NON_MAKER_ROLES } = require('../../../test-helpers/common-role-lists');
-const { MAKER, READ_ONLY } = require('../../../server/constants/roles');
+const { MAKER } = require('../../../server/constants/roles');
 
 const page = 'includes/application-details/applicationReferenceAndActions.njk';
 const render = pageRenderer(page);
@@ -9,69 +9,42 @@ describe(page, () => {
   let wrapper;
   const dealId = '61e54dd5b578247e14575882';
   const status = 'Ready for Checker\'s approval';
-  const abandon = true;
-  const hasChangedFacilities = false;
-  const MIAReturnToMaker = false;
-  const returnToMakerNoFacilitiesChanged = false;
-  const isAdmin = false;
 
-  describe('when viewed without the userRoles param', () => {
-    beforeEach(() => {
+  describe('the \'Clone\' button', () => {
+    const cloneButtonSelector = '[data-cy="clone-gef-deal-link"]';
+
+    it('should not render when the page is rendered without the userRoles param', () => {
       wrapper = render({
         dealId,
         status,
       });
+      wrapper.expectLink(cloneButtonSelector).notToExist();
     });
 
-    it('should not render the \'Clone\' button', () => {
-      wrapper.expectLink('[data-cy="clone-gef-deal-link"]').notToExist();
-    });
-  });
-
-  describe(`when viewed with the role '${MAKER}'`, () => {
-    beforeEach(() => {
+    it('should not render when the page is rendered without the userRoles param', () => {
       wrapper = render({
         dealId,
         userRoles: [MAKER],
-        status,
       });
+      wrapper.expectLink(cloneButtonSelector).notToExist();
     });
 
-    it('should render the \'Clone\' button', () => {
-      wrapper.expectLink('[data-cy="clone-gef-deal-link"]').toLinkTo(`${dealId}/clone`, 'Clone');
-    });
-  });
-
-  describe.each(NON_MAKER_ROLES)('when viewed with the role \'%s\'', (nonMakerRole) => {
-    beforeEach(() => {
+    it.each(NON_MAKER_ROLES)('should not render when the page is rendered with the %s role only', (role) => {
       wrapper = render({
         dealId,
-        userRoles: [nonMakerRole],
         status,
-        abandon,
-        hasChangedFacilities,
-        MIAReturnToMaker,
-        returnToMakerNoFacilitiesChanged,
-        isAdmin,
+        userRoles: [role],
       });
+      wrapper.expectLink(cloneButtonSelector).notToExist();
     });
 
-    it('should NOT render the \'Clone\' button', () => {
-      wrapper.expectLink('[data-cy="clone-gef-deal-link"]').notToExist();
+    it('should render when the page is rendered with the maker role only', () => {
+      wrapper = render({
+        dealId,
+        status,
+        userRoles: [MAKER],
+      });
+      wrapper.expectLink(cloneButtonSelector).toLinkTo(`${dealId}/clone`, 'Clone');
     });
-
-    if (nonMakerRole === READ_ONLY) {
-      it('should NOT render the \'Abandon\' button', () => {
-        wrapper.expectLink('[data-cy="abandon-link"]').notToExist();
-      });
-
-      it('should NOT render the link to edit the bank\'s internal reference', () => {
-        wrapper.expectLink('[data-cy="edit-refname-link"]').notToExist();
-      });
-
-      it('should NOT render the link to add an additional reference or edit it', () => {
-        wrapper.expectLink('[data-cy="edit-addrefname-link"]').notToExist();
-      });
-    }
   });
 });
