5ee823fa6871b49be0816eb87c9293834eb1577a
Luke Marrai <Luke.Marrai@softwire.com>
2023-09-15 09:11:37 +0100

    fix(DTFS2-6628): add userRoles null safety to applicationReferenceAndActions

--- gef-ui/component-tests/includes/application-details/applicationReferenceAndActions.component-test.js
+++ gef-ui/component-tests/includes/application-details/applicationReferenceAndActions.component-test.js
@@ -7,6 +7,7 @@ const render = pageRenderer(page);
 
 describe(page, () => {
   let wrapper;
+  const dealId = '61e54dd5b578247e14575882';
   const status = 'Ready for Checker\'s approval';
   const abandon = true;
   const hasChangedFacilities = false;
@@ -14,22 +15,37 @@ describe(page, () => {
   const returnToMakerNoFacilitiesChanged = false;
   const isAdmin = false;
 
+  describe('when viewed without the userRoles param', () => {
+    beforeEach(() => {
+      wrapper = render({
+        dealId,
+        status,
+      });
+    });
+
+    it('should not render the \'Clone\' button', () => {
+      wrapper.expectLink('[data-cy="clone-gef-deal-link"]').notToExist();
+    });
+  });
+
   describe(`when viewed with the role '${MAKER}'`, () => {
     beforeEach(() => {
       wrapper = render({
+        dealId,
         userRoles: [MAKER],
         status,
       });
     });
 
     it('should render the \'Clone\' button', () => {
-      wrapper.expectElement('[data-cy="clone-gef-deal-link"]').toExist();
+      wrapper.expectLink('[data-cy="clone-gef-deal-link"]').toLinkTo(`${dealId}/clone`, 'Clone');
     });
   });
 
   describe.each(NON_MAKER_ROLES)('when viewed with the role \'%s\'', (nonMakerRole) => {
     beforeEach(() => {
       wrapper = render({
+        dealId,
         userRoles: [nonMakerRole],
         status,
         abandon,
@@ -41,20 +57,20 @@ describe(page, () => {
     });
 
     it('should NOT render the \'Clone\' button', () => {
-      wrapper.expectElement('[data-cy="clone-gef-deal-link"]').notToExist();
+      wrapper.expectLink('[data-cy="clone-gef-deal-link"]').notToExist();
     });
 
     if (nonMakerRole === READ_ONLY) {
       it('should NOT render the \'Abandon\' button', () => {
-        wrapper.expectElement('[data-cy="abandon-link"]').notToExist();
+        wrapper.expectLink('[data-cy="abandon-link"]').notToExist();
       });
 
       it('should NOT render the link to edit the bank\'s internal reference', () => {
-        wrapper.expectElement('[data-cy="edit-refname-link"]').notToExist();
+        wrapper.expectLink('[data-cy="edit-refname-link"]').notToExist();
       });
 
       it('should NOT render the link to add an additional reference or edit it', () => {
-        wrapper.expectElement('[data-cy="edit-addrefname-link"]').notToExist();
+        wrapper.expectLink('[data-cy="edit-addrefname-link"]').notToExist();
       });
     }
   });
