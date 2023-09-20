bb54940fe090769133a82c4e6912c10fe9144e78
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-09-20 14:15:16 +0100

    feat(FN-958): e2e formatting

--- e2e-tests/portal/cypress/e2e/journeys/service-options/service-options.spec.js
+++ e2e-tests/portal/cypress/e2e/journeys/service-options/service-options.spec.js
@@ -8,53 +8,53 @@ const { PAYMENT_OFFICER } = MOCK_USERS;
 const { ADMIN } = MOCK_USERS;
 
 context('List service options', () => {
-    describe('As a maker', () => {
-        it('displays link to portal, when selected directs to portal homepage', () => {
-            cy.login(BANK1_MAKER1);
-            cy.visit(relativeURL('/service-options'));
+  describe('As a maker', () => {
+    it('displays link to portal, when selected directs to portal homepage', () => {
+      cy.login(BANK1_MAKER1);
+      cy.visit(relativeURL('/service-options'));
 
-            serviceOptions.mainHeading().should('contain', "What do you want to do?");
-            serviceOptions.utilisationReportLink().should('not.exist');
+      serviceOptions.mainHeading().should('contain', 'What do you want to do?');
+      serviceOptions.utilisationReportLink().should('not.exist');
 
-            serviceOptions.portalLink().click();
-            serviceOptions.currentUrl().should('contain', `dashboard/deals`);
-        });
+      serviceOptions.portalLink().click();
+      serviceOptions.currentUrl().should('contain', 'dashboard/deals');
     });
+  });
 
-    describe('As a checker', () => {
-        it('displays link to portal, when selected directs to portal homepage', () => {
-            cy.login(BANK1_MAKER1);
-            cy.visit(relativeURL('/service-options'));
+  describe('As a checker', () => {
+    it('displays link to portal, when selected directs to portal homepage', () => {
+      cy.login(BANK1_MAKER1);
+      cy.visit(relativeURL('/service-options'));
 
-            serviceOptions.mainHeading().should('contain', "What do you want to do?");
-            serviceOptions.utilisationReportLink().should('not.exist');
+      serviceOptions.mainHeading().should('contain', 'What do you want to do?');
+      serviceOptions.utilisationReportLink().should('not.exist');
 
-            serviceOptions.portalLink().click();
-            serviceOptions.currentUrl().should('contain', `dashboard/deals`);
-        });
+      serviceOptions.portalLink().click();
+      serviceOptions.currentUrl().should('contain', 'dashboard/deals');
     });
+  });
 
-    describe('As a payment officer', () => {
-        it('displays link to utilisation report service, when selected directs to upload report page', () => {
-            cy.login(PAYMENT_OFFICER);
-            cy.visit(relativeURL('/service-options'));
+  describe('As a payment officer', () => {
+    it('displays link to utilisation report service, when selected directs to upload report page', () => {
+      cy.login(PAYMENT_OFFICER);
+      cy.visit(relativeURL('/service-options'));
 
-            serviceOptions.mainHeading().should('contain', "What do you want to do?");
-            serviceOptions.portalLink().should('not.exist');
+      serviceOptions.mainHeading().should('contain', 'What do you want to do?');
+      serviceOptions.portalLink().should('not.exist');
 
-            serviceOptions.utilisationReportLink().click();
-            serviceOptions.currentUrl().should('contain', `utilisation-report-upload`);
-        });
+      serviceOptions.utilisationReportLink().click();
+      serviceOptions.currentUrl().should('contain', 'utilisation-report-upload');
     });
+  });
 
-    describe('As admin', () => {
-        it('displays both links', () => {
-            cy.login(ADMIN);
-            cy.visit(relativeURL('/service-options'));
+  describe('As admin', () => {
+    it('displays both links', () => {
+      cy.login(ADMIN);
+      cy.visit(relativeURL('/service-options'));
 
-            serviceOptions.mainHeading().should('contain', "What do you want to do?");
-            serviceOptions.portalLink().should('exist');
-            serviceOptions.utilisationReportLink().should('exist');
-        });
+      serviceOptions.mainHeading().should('contain', 'What do you want to do?');
+      serviceOptions.portalLink().should('exist');
+      serviceOptions.utilisationReportLink().should('exist');
     });
+  });
 });
--- portal/server/controllers/service-options/index.js
+++ portal/server/controllers/service-options/index.js
@@ -1,21 +1,21 @@
 const { getUserRoles } = require('../../helpers');
 
 const getServiceOptions = async (req, res) => {
-    const { user } = req.session;
-    try {
-        const { isMaker, isChecker } = getUserRoles(user.roles);
-        const isPaymentOfficer = true;
-        return res.render('service-options/service-options.njk', {
-            user,
-            isMaker,
-            isChecker,
-            isPaymentOfficer,
-        });
-    } catch (error) {
-        return res.render('_partials/problem-with-service.njk', { user });
-    }
+  const { user } = req.session;
+  try {
+    const { isMaker, isChecker } = getUserRoles(user.roles);
+    const isPaymentOfficer = true;
+    return res.render('service-options/service-options.njk', {
+      user,
+      isMaker,
+      isChecker,
+      isPaymentOfficer,
+    });
+  } catch (error) {
+    return res.render('_partials/problem-with-service.njk', { user });
+  }
 };
 
 module.exports = {
-    getServiceOptions,
+  getServiceOptions,
 };
