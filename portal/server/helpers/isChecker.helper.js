d9513d787b75bda2b03acf7216c0e76d074c6b03
Luke Marrai <Luke.Marrai@softwire.com>
2023-09-20 09:24:02 +0100

    chore(DTFS2-6635): remove resolved TODO

--- portal/server/helpers/isChecker.helper.js
+++ portal/server/helpers/isChecker.helper.js
@@ -1,8 +1,6 @@
 const { CHECKER } = require('../constants/roles');
 const { equalArrays } = require('./equalArrays.helper');
 
-// TODO DTFS2-6635: this function returns false if a user has checker plus another role,
-// which is surprising. Is this correct?
 const isChecker = (roles) => {
   const checker = [CHECKER];
   return equalArrays(roles, checker);
