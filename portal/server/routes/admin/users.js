d41e24fc79e7c59792f1dc9470c4b95c4f09e418
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-09-21 10:50:59 +0100

    refactor(DTFS2-6679): commonise code for GET/POST /users/edit/:id

--- portal/server/routes/admin/users.js
+++ portal/server/routes/admin/users.js
@@ -128,7 +128,6 @@ router.post('/users/edit/:_id', async (req, res) => {
   ];
   const payload = constructPayload(req.body, payloadProperties);
   const { _id, userToken } = requestParams(req);
-  const { user } = req.session;
 
   const update = {
     ...payload,
