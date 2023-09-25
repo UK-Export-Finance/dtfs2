5c7b87b14a375d863bb1f8cb8b888f5c9fae35c9
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-09-25 10:04:10 +0100

    feat(FN-958): new test for read only

--- e2e-tests/portal/cypress/e2e/journeys/service-options/service-options.spec.js
+++ e2e-tests/portal/cypress/e2e/journeys/service-options/service-options.spec.js
@@ -3,7 +3,7 @@ const MOCK_USERS = require('../../../fixtures/users');
 const relativeURL = require('../../relativeURL');
 
 const {
-  BANK1_MAKER1, BANK1_CHECKER1, BANK1_PAYMENT_OFFICER1, ADMINNOMAKER,
+  BANK1_MAKER1, BANK1_CHECKER1, BANK1_PAYMENT_OFFICER1, ADMINNOMAKER, BANK1_READ_ONLY1
 } = MOCK_USERS;
 
 context('List service options', () => {
@@ -33,19 +33,6 @@ context('List service options', () => {
     });
   });
 
-  describe('As a payment officer', () => {
-    it('displays link to utilisation report service, when selected directs to upload report page', () => {
-      cy.login(BANK1_PAYMENT_OFFICER1);
-      cy.visit(relativeURL('/service-options'));
-
-      serviceOptions.mainHeading().should('contain', 'What do you want to do?');
-      serviceOptions.portalLink().should('not.exist');
-
-      serviceOptions.utilisationReportLink().click();
-      serviceOptions.currentUrl().should('contain', 'utilisation-report-upload');
-    });
-  });
-
   describe('As admin', () => {
     it('displays link to portal, when selected directs to portal homepage', () => {
       cy.login(ADMINNOMAKER);
@@ -56,6 +43,32 @@ context('List service options', () => {
 
       serviceOptions.portalLink().click();
       serviceOptions.currentUrl().should('contain', 'dashboard/deals');
+    });
+  });
+
+  describe('As read-only user', () => {
+    it('displays link to portal, when selected directs to portal homepage', () => {
+      cy.login(BANK1_READ_ONLY1);
+      cy.visit(relativeURL('/service-options'));
+
+      serviceOptions.mainHeading().should('contain', 'What do you want to do?');
+      serviceOptions.utilisationReportLink().should('not.exist');
+
+      serviceOptions.portalLink().click();
+      serviceOptions.currentUrl().should('contain', 'dashboard/deals');
+    });
+  });
+
+  describe('As a payment officer', () => {
+    it('displays link to utilisation report service, when selected directs to upload report page', () => {
+      cy.login(BANK1_PAYMENT_OFFICER1);
+      cy.visit(relativeURL('/service-options'));
+
+      serviceOptions.mainHeading().should('contain', 'What do you want to do?');
+      serviceOptions.portalLink().should('not.exist');
+
+      serviceOptions.utilisationReportLink().click();
+      serviceOptions.currentUrl().should('contain', 'utilisation-report-upload');
     });
   });
 });
