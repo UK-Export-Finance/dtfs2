361d2429b0ff2a79c2fde89c79eb45f706f908e9
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-09-27 09:35:42 +0100

    fix(spell-check): fixed spelling mistakes

--- azure-functions/acbs-function/mappings/party/buyer.js
+++ azure-functions/acbs-function/mappings/party/buyer.js
@@ -24,7 +24,7 @@ const buyer = ({ deal }) => {
 
   return {
     alternateIdentifier: deal.tfm.parties.buyer.partyUrn.substring(0, 20),
-    industryClassification: CONSTANTS.PARTY.INDUSTRY_CLASSFICATION.DEFAULT,
+    industryClassification: CONSTANTS.PARTY.INDUSTRY_CLASSIFICATION.DEFAULT,
     smeType: CONSTANTS.PARTY.SME_TYPE.NOT_KNOWN,
     citizenshipClass,
     officerRiskDate: now(),
