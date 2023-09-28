6644e66bf81893f1f12fe5d7b3b35e7a73913127
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-09-28 13:37:53 +0100

    feat(FN-958): formatting

--- e2e-tests/portal/cypress/support/portal/createBSSSubmission.js
+++ e2e-tests/portal/cypress/support/portal/createBSSSubmission.js
@@ -2,7 +2,7 @@ const pages = require('../../e2e/pages');
 
 module.exports = (opts) => {
   cy.login(opts);
-  
+
   pages.serviceOptions.portalLink().click();
   pages.dashboard.createNewSubmission().click();
 
