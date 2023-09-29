71fb3f824fb17e8ea0b4710bafedc4f2256893d8
AlexBramhill <47955140+AlexBramhill@users.noreply.github.com>
2023-09-29 18:19:37 +0100

    feat(DTFS-6702): update login journey (#2079)

--- portal-api/src/v1/users/login.controller.js
+++ portal-api/src/v1/users/login.controller.js
@@ -1,34 +1,35 @@
 const utils = require('../../crypto/utils');
-const {
-  userNotFound, userIsBlocked, incorrectPassword, userIsDisabled,
-} = require('../../constants/login-results');
+const { userIsBlocked, userIsDisabled, usernameOrPasswordIncorrect } = require('../../constants/login-results');
 const { findByUsername, updateLastLogin, incrementFailedLoginCount } = require('./controller');
 
-module.exports = (username, password) => new Promise((resolve) => {
-  findByUsername(username, async (error, user) => {
-    if (error) {
-      return resolve({ error });
-    }
+module.exports = (username, password) =>
+  new Promise((resolve) => {
+    findByUsername(username, async (error, user) => {
+      if (error) {
+        return resolve({ error });
+      }
 
-    if (user === null) {
-      return resolve({ error: userNotFound });
-    }
+      if (user === null) {
+        return resolve({ error: usernameOrPasswordIncorrect });
+      }
 
-    const passwordIncorrect = !utils.validPassword(password, user.hash, user.salt);
+      const passwordIncorrect = !utils.validPassword(password, user.hash, user.salt);
 
-    if (user.disabled) {
-      return resolve({ error: userIsDisabled });
-    }
+      if (passwordIncorrect) {
+        await incrementFailedLoginCount(user);
+        return resolve({ error: usernameOrPasswordIncorrect });
+      }
 
-    if (passwordIncorrect) {
-      await incrementFailedLoginCount(user);
-      return resolve({ error: incorrectPassword });
-    }
-    if (user['user-status'] === 'blocked') {
-      return resolve({ error: userIsBlocked });
-    }
-    const { sessionIdentifier, ...tokenObject } = utils.issueJWT(user);
+      if (user.disabled) {
+        return resolve({ error: userIsDisabled });
+      }
 
-    return updateLastLogin(user, sessionIdentifier, () => resolve({ user, tokenObject }));
+      if (user['user-status'] === 'blocked') {
+        return resolve({ error: userIsBlocked });
+      }
+
+      const { sessionIdentifier, ...tokenObject } = utils.issueJWT(user);
+
+      return updateLastLogin(user, sessionIdentifier, () => resolve({ user, tokenObject }));
+    });
   });
-});
