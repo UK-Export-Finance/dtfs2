b0c1c9316440c799e69d50fb7ce12a9aa15e0ff1
Luke Marrai <Luke.Marrai@softwire.com>
2023-10-05 22:39:53 +0100

    refactor(e2e-tests): add js-docs to RandomValueGenerator class

--- e2e-tests/support/random-value-generator.js
+++ e2e-tests/support/random-value-generator.js
@@ -1,5 +1,12 @@
 import { Chance } from 'chance';
 
+/**
+ * Class to generate random values for test data.
+ *
+ * RandomValueGenerator uses a fixed seed to make test runs repeatable (although
+ * calling a test on its own or calling changing the test order can change the
+ * random values that will be used).
+ */
 export class RandomValueGenerator {
   static #seed = 0;
 
