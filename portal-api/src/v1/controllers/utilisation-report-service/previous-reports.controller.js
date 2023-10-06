0a2e259096fc779fb6894ad5eeb33c8a24b345fe
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-10-06 12:09:21 +0100

    feat(FN-980): bugfix

--- portal-api/src/v1/controllers/utilisation-report-service/previous-reports.controller.js
+++ portal-api/src/v1/controllers/utilisation-report-service/previous-reports.controller.js
@@ -13,7 +13,8 @@ const getExistingYear = (groupedReports, year) => {
 };
 
 const getMonthName = (monthNumber) => {
-  const date = new Date();
+  // date is set to 1st Jan to avoid bug when today's month has more days than target month
+  const date = new Date(2023, 1, 1);
   // offset by 1 as January = 1 in Database
   date.setMonth(monthNumber - 1);
   return date.toLocaleString('default', { month: 'long' });
