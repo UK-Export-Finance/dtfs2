b7b74f3218831a2c027b6e914ccbf00b1e6ee95d
lmarrai-sw <44811960+lmarrai-sw@users.noreply.github.com>
2023-10-13 14:46:31 +0100

    fix(DTFS2-6182): remove passport from put deals submit (#2162)

--- trade-finance-manager-api/src/v1/routes.js
+++ trade-finance-manager-api/src/v1/routes.js
@@ -17,14 +17,15 @@ const validation = require('./validation/route-validators/route-validators');
 const handleValidationResult = require('./validation/route-validators/validation-handler');
 const checkApiKey = require('./middleware/headers/check-api-key');
 const { teamsRoutes } = require('./teams/routes');
-const { dealsRouter } = require('./deals/routes');
+const { dealsOpenRouter, dealsAuthRouter } = require('./deals/routes');
 
 openRouter.use(checkApiKey);
 authRouter.use(passport.authenticate('jwt', { session: false }));
 
 authRouter.route('/api-docs').get(swaggerUi.setup(swaggerSpec, swaggerUiOptions));
 
-authRouter.use('/', dealsRouter);
+openRouter.use('/', dealsOpenRouter);
+authRouter.use('/', dealsAuthRouter);
 
 /**
  * @openapi
