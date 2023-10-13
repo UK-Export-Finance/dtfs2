b15d85a6a79de37b7c5dd78f90542a108b1750cc
AlexBramhill <47955140+AlexBramhill@users.noreply.github.com>
2023-10-13 17:35:54 +0100

    feat(dtfs2-6716):replace graphql update task in tfm with rest (#2160)

--- trade-finance-manager-api/src/v1/controllers/tasks.controller.js
+++ trade-finance-manager-api/src/v1/controllers/tasks.controller.js
@@ -10,7 +10,7 @@ const mapSubmittedDeal = require('../mappings/map-submitted-deal');
 /**
  * Update a task in the task's group.
  * */
-const updateTask = (allTaskGroups, taskUpdate) =>
+const createUpdatedTask = (allTaskGroups, taskUpdate) =>
   allTaskGroups.map((tGroup) => {
     let group = tGroup;
 
@@ -37,7 +37,7 @@ const updateTask = (allTaskGroups, taskUpdate) =>
  * Update all tasks canEdit flag and status
  * Depending on what task has been changed.
  * */
-const updateAllTasks = async (allTaskGroups, groupId, taskUpdate, statusFrom, deal, urlOrigin) => {
+const createAllUpdatedTasks = async (allTaskGroups, groupId, taskUpdate, statusFrom, deal, urlOrigin) => {
   const taskEmailsToSend = [];
 
   let taskGroups = allTaskGroups.map((tGroup) => {
@@ -174,70 +174,96 @@ const updateAllTasks = async (allTaskGroups, groupId, taskUpdate, statusFrom, de
  * - Change the TFM dealStage (if deal is MIA and taskUpdate is first task).
  * - Updates the deal.
  * */
-const updateTfmTask = async (dealId, taskUpdate) => {
+const updateTfmTask = async ({ dealId, groupId, taskId, taskUpdate }) => {
   const unmappedDeal = await api.findOneDeal(dealId);
-  const deal = await mapSubmittedDeal(unmappedDeal);
+  if (!unmappedDeal) {
+    throw new Error(`Deal not found ${dealId}`);
+  }
+
+  const deal = mapSubmittedDeal(unmappedDeal);
 
   const allTasks = deal.tfm.tasks;
 
-  const { id: taskIdToUpdate, groupId, status: statusTo, urlOrigin } = taskUpdate;
+  const taskUpdateWithIds = { ...taskUpdate, groupId, id: taskId };
 
-  const group = getGroupById(allTasks, groupId);
-  const originalTask = getTaskInGroupById(group.groupTasks, taskIdToUpdate);
-  const statusFrom = originalTask.status;
+  const { status: statusTo, urlOrigin } = taskUpdate;
 
-  const updatedTask = await mapTaskObject(originalTask, taskUpdate, statusFrom);
-
-  const canUpdateTask = previousTaskIsComplete(allTasks, group, updatedTask.id) || taskCanBeEditedWithoutPreviousTaskComplete(group, updatedTask);
-
-  if (canUpdateTask) {
-    /**
-     * Update the task in the array
-     * */
-    const modifiedTasks = updateTask(allTasks, updatedTask);
-
-    /**
-     * Map over every task in all groups and
-     * update other tasks so they can be started.
-     * E.g If task 1 is completed, task 2 can then be started.
-     * Some other special conditions are in here:
-     * - e.g if X task is completed, an entire group of tasks can be started.
-     * */
-    const modifiedTasksWithEditStatus = await updateAllTasks(modifiedTasks, groupId, updatedTask, statusFrom, deal, urlOrigin);
-
-    /**
-     * Construct TFM update object
-     * */
-    const tfmDealUpdate = {
-      tfm: {
-        tasks: modifiedTasksWithEditStatus,
-      },
-    };
-
-    /**
-     * If the deal is MIA
-     * and the first task status is being changed to in progress or is completed immediately,
-     * the TFM deal stage needs to change to 'in progress'.
-     * */
-    const updateDealStage = shouldUpdateDealStage(deal.submissionType, taskIdToUpdate, groupId, statusFrom, statusTo);
-
-    if (updateDealStage) {
-      tfmDealUpdate.tfm.stage = CONSTANTS.DEALS.DEAL_STAGE_TFM.IN_PROGRESS_BY_UKEF;
-    }
-
-    /**
-     * Update the deal
-     * */
-    await api.updateDeal(dealId, tfmDealUpdate);
-
-    return updatedTask;
+  const group = getGroupById(allTasks, Number(groupId));
+  if (!group) {
+    throw new Error(`Group not found ${groupId}`);
   }
 
-  return originalTask;
+  const originalTask = getTaskInGroupById(group.groupTasks, taskId);
+  if (!originalTask) {
+    throw new Error(`Task not found ${taskId}`);
+  }
+  const statusFrom = originalTask.status;
+
+  const updatedTask = await mapTaskObject(originalTask, taskUpdateWithIds);
+
+  const canUpdateTask = previousTaskIsComplete(allTasks, group, taskId) || taskCanBeEditedWithoutPreviousTaskComplete(group, updatedTask);
+  if (!canUpdateTask) {
+    return originalTask;
+  }
+
+  /**
+   * Update the task in the array
+   * */
+  const modifiedTasks = createUpdatedTask(allTasks, updatedTask);
+
+  /**
+   * Map over every task in all groups and
+   * update other tasks so they can be started.
+   * E.g If task 1 is completed, task 2 can then be started.
+   * Some other special conditions are in here:
+   * - e.g if X task is completed, an entire group of tasks can be started.
+   * */
+  const modifiedTasksWithEditStatus = await createAllUpdatedTasks(modifiedTasks, groupId, updatedTask, statusFrom, deal, urlOrigin);
+
+  /**
+   * Construct TFM update object
+   * */
+  const tfmDealUpdate = {
+    tfm: {
+      tasks: modifiedTasksWithEditStatus,
+    },
+  };
+
+  /**
+   * If the deal is MIA
+   * and the first task status is being changed to in progress or is completed immediately,
+   * the TFM deal stage needs to change to 'in progress'.
+   * */
+  const updateDealStage = shouldUpdateDealStage(deal.submissionType, taskId, groupId, statusFrom, statusTo);
+
+  if (updateDealStage) {
+    tfmDealUpdate.tfm.stage = CONSTANTS.DEALS.DEAL_STAGE_TFM.IN_PROGRESS_BY_UKEF;
+  }
+
+  /**
+   * Update the deal
+   * */
+  await api.updateDeal(dealId, tfmDealUpdate);
+
+  return updatedTask;
+};
+
+const updateTask = async (req, res) => {
+  const { dealId, groupId, taskId } = req.params;
+  const taskUpdate = req.body;
+
+  try {
+    const result = await updateTfmTask({ dealId, groupId: parseInt(groupId, 10), taskId, taskUpdate });
+    return res.status(200).send(result);
+  } catch (error) {
+    console.error('Unable to update the task %s in group %s deal %s: %O', taskId, groupId, dealId, error);
+    return res.status(500).send({ data: 'Unable to update the task' });
+  }
 };
 
 module.exports = {
-  updateTask,
-  updateAllTasks,
+  createUpdatedTask,
+  createAllUpdatedTasks,
   updateTfmTask,
+  updateTask,
 };
---
+++ trade-finance-manager-api/src/v1/tasks/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const tasksController = require('../controllers/tasks.controller');
+const validation = require('../validation/route-validators/route-validators');
+const handleValidationResult = require('../validation/route-validators/validation-handler');
+
+const tasksRouter = express.Router();
+
+tasksRouter
+  .route('/deals/:dealId/tasks/:groupId/:taskId')
+  .put(validation.dealIdValidation, validation.groupIdValidation, validation.taskIdValidation, handleValidationResult, tasksController.updateTask);
+
+module.exports = {
+  tasksRouter,
+};
