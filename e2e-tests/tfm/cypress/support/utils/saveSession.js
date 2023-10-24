92684dc5d60b3a693c0dc2d58598ff4e5dfb8581
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-10-24 11:34:02 +0100

    fix(cypress): changed cookie preserve strategy

--- e2e-tests/tfm/cypress/support/utils/saveSession.js
+++ e2e-tests/tfm/cypress/support/utils/saveSession.js
@@ -1,11 +1,44 @@
-import 'cypress-v10-preserve-cookie';
-
-/**
-* Since cookies are cleared before individual tests execution.
-* Preserving the cookie, eliminates repeated logins.
-* Thus reduces execution time.
-*/
-export default () => cy.preserveCookieOnce(
+// E2E Cookies
+const cookies = [
   '__Host-DTFS-SID', // Session cookie
   '__Host-DTFS-CSRF', // CSRF cookie
-);
+];
+
+const flags = {
+  secure: true,
+  httpOnly: true,
+  path: '/',
+  log: false,
+};
+
+/**
+ * Saves cookies to Cypress environment
+ * variables for preservation.
+ * @returns {void}
+ */
+const saveSession = () => {
+  // Iterate through the cookies array
+  cookies.forEach((cookie) => {
+    // Fetch Cypress cookie
+    cy.getCookie(cookie)
+      .then((value) => {
+        // Cypress cookie name
+        const cypressCookie = `cookie_${cookie}`;
+
+        if (value) {
+          // Cookie exists, save to the environment
+          Cypress.env(cypressCookie, value);
+        } else {
+          // Fetch value from the environment
+          const saved = Cypress.env(cypressCookie);
+
+          if (saved?.value) {
+            // Save Cypress cookie
+            cy.setCookie(cookie, saved.value, flags);
+          }
+        }
+      });
+  });
+};
+
+export default saveSession;
