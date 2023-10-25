c3f7bd20f0c44c17aece933f4534084d1fca29dc
Joshua Binns <Joshua.Binns@softwire.com>
2023-10-25 10:31:00 +0100

    feat(FN-1363): update getUserRoles function
    feat(FN-1363): change getUserRoles null and undefined tests

--- portal/server/helpers/getUserRoles.js
+++ portal/server/helpers/getUserRoles.js
@@ -1,6 +1,16 @@
 const { MAKER, CHECKER, PAYMENT_REPORT_OFFICER, ADMIN, READ_ONLY } = require('../constants/roles');
 
+const defaultResult = {
+  isMaker: false,
+  isChecker: false,
+  isPaymentReportOfficer: false,
+  isAdmin: false,
+  isReadOnly: false,
+};
+
 const getUserRoles = (roles) => {
+  if (!roles) return defaultResult;
+  
   const isMaker = roles.includes(MAKER);
   const isChecker = roles.includes(CHECKER);
   const isPaymentReportOfficer = roles.includes(PAYMENT_REPORT_OFFICER);
--- portal/server/helpers/getUserRoles.test.js
+++ portal/server/helpers/getUserRoles.test.js
@@ -1,4 +1,4 @@
-const { MAKER, CHECKER } = require('../constants/roles');
+const { MAKER, CHECKER, ADMIN, READ_ONLY, PAYMENT_REPORT_OFFICER } = require('../constants/roles');
 const getUserRoles = require('./getUserRoles');
 
 describe('getUserRoles', () => {
@@ -11,7 +11,7 @@ describe('getUserRoles', () => {
       });
 
       it(`should return false when roles does NOT include ${role}`, () => {
-        const result = getUserRoles(['']);
+        const result = getUserRoles([]);
 
         expect(result[isRole]).toEqual(false);
       });
@@ -19,6 +19,20 @@ describe('getUserRoles', () => {
   }
 
   isRoleTests('isMaker', MAKER);
-
   isRoleTests('isChecker', CHECKER);
+  isRoleTests('isAdmin', ADMIN);
+  isRoleTests('isReadOnly', READ_ONLY);
+  isRoleTests('isPaymentReportOfficer', PAYMENT_REPORT_OFFICER);
+
+  it('should return false for all entries when roles is null', () => {
+    const result = getUserRoles(null);
+
+    Object.values(result).forEach((value) => expect(value).toEqual(false));
+  });
+
+  it('should return false for all entries when roles is undefined', () => {
+    const result = getUserRoles(undefined);
+
+    Object.values(result).forEach((value) => expect(value).toEqual(false));
+  });
 });
