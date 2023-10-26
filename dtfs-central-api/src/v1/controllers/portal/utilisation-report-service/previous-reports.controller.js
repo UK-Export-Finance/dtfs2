644a5ba85b44a0797c7f9e764e0b982ab2cd9576
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-10-26 08:56:50 +0100

    feat(FN-980): skip failing e2e tests & change error logged

--- dtfs-central-api/src/v1/controllers/portal/utilisation-report-service/previous-reports.controller.js
+++ dtfs-central-api/src/v1/controllers/portal/utilisation-report-service/previous-reports.controller.js
@@ -19,7 +19,7 @@ const getUtilisationReports = async (req, res) => {
     ]).toArray();
     res.status(200).send(filteredAndSortedUtilisationReports);
   } catch (error) {
-    console.error('Unable to get utilisation reports %O', error);
+    console.error('Unable to get utilisation reports %s', error);
     res.status(500).send({ status: 500, message: 'Failed to get utilisation reports' });
   }
 };
