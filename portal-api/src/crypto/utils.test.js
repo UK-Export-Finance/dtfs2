4f44033d3236985706858d3732d0f9d0ed492e05
Alex <alexbramhill@gmail.com>
2023-10-30 16:33:35 +0000

    fix(DTFS2-6759): fix token expiry not working

--- portal-api/src/crypto/utils.test.js
+++ portal-api/src/crypto/utils.test.js
@@ -5,7 +5,7 @@ const { MAKER } = require('../v1/roles/roles');
 const { LOGIN_STATUSES } = require('../constants');
 
 describe('crypto utils', () => {
-  const user = {
+  const USER = {
     username: 'HSBC-maker-1',
     password: 'P@ssword1234',
     firstname: 'Mister',
@@ -20,27 +20,33 @@ describe('crypto utils', () => {
     },
   };
 
-  const existingSessionIdentifier = crypto.randomBytes(32).toString('hex');
+  const EXISTING_SESSION_IDENTIFIER = crypto.randomBytes(32).toString('hex');
 
-  const userWithExistingSessionIdentifier = { ...user, sessionIdentifier: existingSessionIdentifier };
+  const USER_WITH_EXISTING_SESSION_IDENTIFIER = { ...USER, sessionIdentifier: EXISTING_SESSION_IDENTIFIER };
+
+  const DATE_NOW_IN_UNIX_TIME = Math.floor(Date.now().valueOf()/1000);
+
+  const SECONDS_IN_30_MINUTES = 30 * 60;
+
+  const SECONDS_IN_12_HOURS = 12 * 60 * 60;
 
   describe('issueValidUsernameAndPasswordJWT', () => {
     it('should not use an existing session identifier', () => {
-      const { sessionIdentifier } = issueValidUsernameAndPasswordJWT(userWithExistingSessionIdentifier);
-      expect(sessionIdentifier).not.toEqual(existingSessionIdentifier);
+      const { sessionIdentifier } = issueValidUsernameAndPasswordJWT(USER_WITH_EXISTING_SESSION_IDENTIFIER);
+      expect(sessionIdentifier).not.toEqual(EXISTING_SESSION_IDENTIFIER);
     });
 
     it('should return a session identifier', () => {
-      const { sessionIdentifier } = issueValidUsernameAndPasswordJWT(user);
+      const { sessionIdentifier } = issueValidUsernameAndPasswordJWT(USER);
       expect(is32ByteHex(sessionIdentifier)).toEqual(true);
     });
 
     it('should return a token with the expected payload', () => {
-      const { token, sessionIdentifier } = issueValidUsernameAndPasswordJWT(user);
+      const { token, sessionIdentifier } = issueValidUsernameAndPasswordJWT(USER);
       const decodedToken = jsonwebtoken.decode(token.replace('Bearer ', ''));
-      expect(decodedToken.iat).toBeDefined();
-      expect(decodedToken.exp).toBeDefined();
-      expect(decodedToken.username).toEqual(user.username);
+      expect(decodedToken.iat).toBe(DATE_NOW_IN_UNIX_TIME);
+      expect(decodedToken.exp).toBe(DATE_NOW_IN_UNIX_TIME + SECONDS_IN_30_MINUTES);
+      expect(decodedToken.username).toEqual(USER.username);
       expect(decodedToken.loginStatus).toEqual(LOGIN_STATUSES.VALID_USERNAME_AND_PASSWORD);
       expect(decodedToken.sessionIdentifier).toEqual(sessionIdentifier);
     });
@@ -53,30 +59,30 @@ describe('crypto utils', () => {
 
   describe('issueValid2faJWT', () => {
     it('should throw an error if no existing session identifier is present', () => {
-      expect(() => issueValid2faJWT(user)).toThrow('User does not have a session identifier');
+      expect(() => issueValid2faJWT(USER)).toThrow('User does not have a session identifier');
     });
 
     it('should return the input session identifier', () => {
-      const { sessionIdentifier } = issueValid2faJWT(userWithExistingSessionIdentifier);
+      const { sessionIdentifier } = issueValid2faJWT(USER_WITH_EXISTING_SESSION_IDENTIFIER);
       expect(is32ByteHex(sessionIdentifier)).toEqual(true);
-      expect(sessionIdentifier).toEqual(existingSessionIdentifier);
+      expect(sessionIdentifier).toEqual(EXISTING_SESSION_IDENTIFIER);
     });
 
     it('should return a token with the expected payload', () => {
-      const { token, sessionIdentifier } = issueValid2faJWT(userWithExistingSessionIdentifier);
+      const { token, sessionIdentifier } = issueValid2faJWT(USER_WITH_EXISTING_SESSION_IDENTIFIER);
       const decodedToken = jsonwebtoken.decode(token.replace('Bearer ', ''));
-      expect(decodedToken.iat).toBeDefined();
-      expect(decodedToken.exp).toBeDefined();
-      expect(decodedToken.username).toEqual(user.username);
+      expect(decodedToken.iat).toBe(DATE_NOW_IN_UNIX_TIME);
+      expect(decodedToken.exp).toBe(DATE_NOW_IN_UNIX_TIME + SECONDS_IN_12_HOURS);
+      expect(decodedToken.username).toEqual(USER.username);
       expect(decodedToken.loginStatus).toEqual(LOGIN_STATUSES.VALID_2FA);
       expect(decodedToken.sessionIdentifier).toEqual(sessionIdentifier);
-      expect(decodedToken.sessionIdentifier).toEqual(existingSessionIdentifier);
-      expect(decodedToken.roles).toEqual(user.roles);
-      expect(decodedToken.bank).toEqual(user.bank);
+      expect(decodedToken.sessionIdentifier).toEqual(EXISTING_SESSION_IDENTIFIER);
+      expect(decodedToken.roles).toEqual(USER.roles);
+      expect(decodedToken.bank).toEqual(USER.bank);
     });
 
     it('should return the correct expiry time', () => {
-      const { expires } = issueValid2faJWT(userWithExistingSessionIdentifier);
+      const { expires } = issueValid2faJWT(USER_WITH_EXISTING_SESSION_IDENTIFIER);
       expect(expires).toEqual('12h');
     });
   });
