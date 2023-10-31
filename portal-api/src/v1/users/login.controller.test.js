b8a246db7145242c74e72c60713b3a17765816f3
Alex <47955140+AlexBramhill@users.noreply.github.com>
2023-10-31 10:44:18 +0000

    fix(DTFS2-6759): fix token expiry (#2199)

--- portal-api/src/v1/users/login.controller.test.js
+++ portal-api/src/v1/users/login.controller.test.js
@@ -5,6 +5,7 @@ const controller = require('./controller');
 const utils = require('../../crypto/utils');
 const sendEmail = require('../email');
 const CONSTANTS = require('../../constants');
+const { FEATURE_FLAGS } = require('../../config/feature-flag.config');
 
 jest.mock('./controller', () => ({
   findByUsername: jest.fn(),
@@ -38,17 +39,29 @@ describe('login', () => {
     hash: HASH,
     salt: SALT,
   };
+  if (!FEATURE_FLAGS.MAGIC_LINK) {
+    it('returns the user and token when the user exists and the password is correct', async () => {
+      mockFindByUsernameSuccess(USER);
+      mockValidPasswordSuccess();
+      mockIssueJWTSuccess(USER);
+      mockUpdateLastLoginSuccess(USER);
 
-  it('returns the user and token when the user exists and the password is correct', async () => {
-    mockFindByUsernameSuccess(USER);
-    mockValidPasswordSuccess();
-    mockIssueJWTSuccess(USER);
-    mockUpdateLastLoginSuccess(USER);
+      const result = await login(USERNAME, PASSWORD);
 
-    const result = await login(USERNAME, PASSWORD);
+      expect(result).toEqual({ user: USER, tokenObject: TOKEN_OBJECT });
+    });
+  } else {
+    it('returns the token when the user exists and the password is correct', async () => {
+      mockFindByUsernameSuccess(USER);
+      mockValidPasswordSuccess();
+      mockIssueJWTSuccess(USER);
+      mockUpdateLastLoginSuccess(USER);
 
-    expect(result).toEqual({ user: USER, tokenObject: TOKEN_OBJECT });
-  });
+      const result = await login(USERNAME, PASSWORD);
+
+      expect(result).toEqual({ tokenObject: TOKEN_OBJECT });
+    });
+  }
 
   it("returns a 'usernameOrPasswordIncorrect' error when the user doesn't exist", async () => {
     mockFindByUsernameReturnsNullUser();
@@ -193,19 +206,23 @@ describe('sendSignInLinkEmail', () => {
     await sendSignInLinkEmail(EMAIL, FIRST_NAME, LAST_NAME, SIGN_IN_LINK);
 
     expect(sendEmail).toHaveBeenCalledTimes(1);
-    expect(sendEmail).toHaveBeenCalledWith(
-      CONSTANTS.EMAIL_TEMPLATE_IDS.SIGN_IN_LINK,
-      EMAIL,
-      { firstName: FIRST_NAME, lastName: LAST_NAME, signInLink: SIGN_IN_LINK, signInLinkExpiryMinutes: CONSTANTS.SIGN_IN_LINK_EXPIRY_MINUTES }
-    );
+    expect(sendEmail).toHaveBeenCalledWith(CONSTANTS.EMAIL_TEMPLATE_IDS.SIGN_IN_LINK, EMAIL, {
+      firstName: FIRST_NAME,
+      lastName: LAST_NAME,
+      signInLink: SIGN_IN_LINK,
+      signInLinkExpiryMinutes: CONSTANTS.SIGN_IN_LINK_EXPIRY_MINUTES,
+    });
   });
 
   it('returns the response from sendEmail', async () => {
-    when(sendEmail).calledWith(
-      CONSTANTS.EMAIL_TEMPLATE_IDS.SIGN_IN_LINK,
-      EMAIL,
-      { firstName: FIRST_NAME, lastName: LAST_NAME, signInLink: SIGN_IN_LINK, signInLinkExpiryMinutes: CONSTANTS.SIGN_IN_LINK_EXPIRY_MINUTES }
-    ).mockResolvedValueOnce(SEND_EMAIL_RESPONSE);
+    when(sendEmail)
+      .calledWith(CONSTANTS.EMAIL_TEMPLATE_IDS.SIGN_IN_LINK, EMAIL, {
+        firstName: FIRST_NAME,
+        lastName: LAST_NAME,
+        signInLink: SIGN_IN_LINK,
+        signInLinkExpiryMinutes: CONSTANTS.SIGN_IN_LINK_EXPIRY_MINUTES,
+      })
+      .mockResolvedValueOnce(SEND_EMAIL_RESPONSE);
 
     const result = await sendSignInLinkEmail(EMAIL, FIRST_NAME, LAST_NAME, SIGN_IN_LINK);
 
