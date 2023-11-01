7910919e752557fd4ac96237cf9f59520a282045
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-11-01 18:23:51 +0000

    feat(FN-980): update collection find and sort to get previous reports

--- dtfs-central-api/src/v1/controllers/portal/utilisation-report-service/previous-reports.controller.js
+++ dtfs-central-api/src/v1/controllers/portal/utilisation-report-service/previous-reports.controller.js
@@ -1,22 +1,14 @@
+const { ObjectId } = require('mongodb');
 const db = require('../../../../drivers/db-client');
 
 const getUtilisationReports = async (req, res) => {
   const { bankId } = req.params;
   try {
     const utilisationReportsCollection = await db.getCollection('utilisation-reports');
-    const filteredAndSortedUtilisationReports = await utilisationReportsCollection.aggregate([
-      {
-        $match: {
-          bankId,
-        },
-      },
-      {
-        $sort: {
-          year: 1,
-          month: 1,
-        },
-      },
-    ]).toArray();
+    const filteredAndSortedUtilisationReports = await utilisationReportsCollection
+    .find({ bankId: { $eq: ObjectId(bankId) } })
+    .sort({ year: 1, month: 1})
+    .toArray();
     res.status(200).send(filteredAndSortedUtilisationReports);
   } catch (error) {
     console.error('Unable to get utilisation reports %s', error);
