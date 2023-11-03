6ec2d2380d87569c8388db2e8811036367e8d881
Luke Marrai <Luke.Marrai@softwire.com>
2023-11-03 18:25:33 +0000

    chore: print the page body to the console when e2e tests error for investigations

--- e2e-tests/gef/cypress/support/e2e.js
+++ e2e-tests/gef/cypress/support/e2e.js
@@ -12,6 +12,18 @@ import submitDealAfterUkefIdsCall from './commands/submitDealAfterUkefIds';
 // Mitigates test fails due to js errors (third-party js)
 Cypress.on('uncaught:exception', () => false);
 
+// eslint-disable-next-line no-unused-vars
+Cypress.on('fail', (err, _runnable) => {
+  const pageBodyHtml = Cypress.$('body').html();
+
+  // Print the page body HTML to the console as part of the error if a test fails
+  // TODO: Remove this extra logging information when there are no more flakey e2e tests
+  // to investigate.
+  // eslint-disable-next-line no-param-reassign
+  err.message += `\n\n[DEBUG] The HTML of the page body is: ${JSON.stringify(pageBodyHtml)}`;
+  throw err;
+});
+
 // Preserve session cookie
 Cypress.Commands.add('saveSession', require('./commands/saveSession'));
 
