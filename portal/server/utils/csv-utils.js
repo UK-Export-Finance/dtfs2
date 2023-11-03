694110d644f561b7699a488202f6d42bbcd1abc0
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2023-11-03 16:57:22 +0000

    feat(FN-1432): review mark ups

--- portal/server/utils/csv-utils.js
+++ portal/server/utils/csv-utils.js
@@ -19,7 +19,7 @@ const columnIndexToExcelColumn = (index) => {
  * @returns {string | number} - cell value.
  */
 const extractCellValue = (cell) => {
-  const cellValue = cell.value?.result ? cell.value.result : cell.value;
+  const cellValue = cell.value?.result ?? cell.value;
   const cellValueWithoutNewLines = typeof cellValue === 'string' ? cellValue?.replace(/\r\n|\r|\n/g, ' ').trim() : cellValue;
   return cellValueWithoutNewLines;
 };
