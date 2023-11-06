3b6315e49c0cd41016fc3444123956524ead79d6
Joshua Binns <Joshua.Binns@softwire.com>
2023-11-06 11:11:29 +0000

    feat(FN-1088): add tests for list-previous-reports

--- e2e-tests/portal/cypress/e2e/journeys/payment-report-officer/upload-previous-utilisation-reports/show-overdue-reports.spec.js
+++ e2e-tests/portal/cypress/e2e/journeys/payment-report-officer/upload-previous-utilisation-reports/show-overdue-reports.spec.js
@@ -1,53 +1,13 @@
 const { uploadPreviousUtilisationReports } = require('../../../pages');
 const MOCK_USERS = require('../../../../fixtures/users');
 const relative = require('../../../relativeURL');
+const { overdueReportDetails } = require('../../../../fixtures/mockUtilisationReportDetails');
 
 const { BANK1_PAYMENT_REPORT_OFFICER1 } = MOCK_USERS;
 
 context('Show overdue reports', () => {
   before(() => {
-    const mockReports = [{
-      bank: {
-        id: '9',
-        name: 'BANK1',
-      },
-      month: 1,
-      year: 2023,
-      uploadedDate: '2023-01-05T10:35:15Z',
-      uploadedBy: {
-        id: '00000',
-        name: 'John Smith',
-      },
-      path: 'www.abc.com',
-    }, {
-      bank: {
-        id: '9',
-        name: 'BANK1',
-      },
-      month: 2,
-      year: 2023,
-      uploadedDate: '2023-02-06T11:30:10Z',
-      uploadedBy: {
-        id: '00001',
-        name: 'John Smith',
-      },
-      path: 'www.abc.com',
-    }, {
-      bank: {
-        id: '9',
-        name: 'BANK1',
-      },
-      month: 3,
-      year: 2023,
-      uploadedDate: '22023-03-07T12:25:20Z',
-      uploadedBy: {
-        id: '00002',
-        name: 'John Smith',
-      },
-      path: 'www.abc.com',
-    }];
-
-    cy.insertUtilisationReportDetails(mockReports);
+    cy.insertUtilisationReportDetails(overdueReportDetails);
   });
 
   after(() => {
---
+++ e2e-tests/portal/cypress/fixtures/mockUtilisationReportDetails.js
@@ -0,0 +1,75 @@
+const { BANK1_PAYMENT_REPORT_OFFICER1 } = require('./users');
+
+const BANK1 = {
+  id: '9',
+  name: 'BANK1',
+};
+
+const generateReportDetails = (year, month, uploadedDate) => {
+  const bank = BANK1;
+  const path = 'www.abc.com';
+  const uploadedBy = BANK1_PAYMENT_REPORT_OFFICER1;
+
+  let populatedUploadedDate;
+  if (uploadedDate) {
+    populatedUploadedDate = uploadedDate;
+  } else {
+    populatedUploadedDate = new Date(year, month - 1, 5, 12, 35).toISOString();
+  }
+
+  return {
+    bank,
+    month,
+    year,
+    uploadedDate: populatedUploadedDate,
+    uploadedBy,
+    path,
+  };
+};
+
+const generateReports = (startYear, startMonth, endYear, endMonth) => {
+  const reports = [];
+
+  if (startMonth < 1 || startMonth > 12) {
+    throw new Error('Cannot generate reports using supplied `startMonth` (must be between 1 and 12)');
+  }
+  if (endMonth < 1 || endMonth > 12) {
+    throw new Error('Cannot generate reports using supplied `endMonth` (must be between 1 and 12)');
+  }
+
+  let year = startYear;
+  let month = startMonth;
+  while (year < endYear || month <= endMonth) {
+    const reportDetails = generateReportDetails(year, month);
+    reports.push(reportDetails);
+
+    month += 1;
+    if (month > 12) {
+      year += 1;
+      month = 1;
+    }
+  }
+
+  return reports;
+};
+
+// Reports to be populated for 2020, 2022 and 2023 (ie. not 2021 to match specific test case)
+const previousReportDetails = generateReports(2020, 1, 2023, 1).filter((report) => report.year !== 2021);
+
+const generateOverdueReports = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+
+  const month = currentMonth - 4 > 0 ? currentMonth - 4 : 12 + currentMonth - 4;
+  const year = currentMonth - 4 > 0 ? currentYear : currentYear - 1;
+
+  return [generateReportDetails(year, month)];
+};
+
+const overdueReportDetails = generateOverdueReports();
+
+module.exports = {
+  previousReportDetails,
+  overdueReportDetails,
+};
