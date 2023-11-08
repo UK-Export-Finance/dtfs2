809f1afcab6126410ef53d13316cae75b488f2df
Alex <47955140+AlexBramhill@users.noreply.github.com>
2023-11-08 10:02:27 +0000

    feat(DTFS2-6757): add hasher test

--- portal-api/src/crypto/hasher.test.js
+++ portal-api/src/crypto/hasher.test.js
@@ -1,5 +1,11 @@
 const { when } = require('jest-when');
 const { Hasher } = require('./hasher');
+const crypto = require('node:crypto');
+jest.mock('node:crypto', () => {
+  return {
+    timingSafeEqual: jest.fn(),
+  };
+});
 
 describe('Hasher', () => {
   describe('hash', () => {
@@ -16,13 +22,9 @@ describe('Hasher', () => {
         generateHash: jest.fn(),
       };
 
-      when(hashStrategy.generateSalt)
-        .calledWith()
-        .mockReturnValueOnce(saltFromStrategy);
+      when(hashStrategy.generateSalt).calledWith().mockReturnValueOnce(saltFromStrategy);
 
-      when(hashStrategy.generateHash)
-        .calledWith(valueToHash, saltFromStrategy)
-        .mockReturnValueOnce(hashFromStrategy);
+      when(hashStrategy.generateHash).calledWith(valueToHash, saltFromStrategy).mockReturnValueOnce(hashFromStrategy);
 
       hasher = new Hasher(hashStrategy);
     });
@@ -35,6 +37,54 @@ describe('Hasher', () => {
     it('returns the hash generated from the target and salt from the hash strategy', () => {
       const { hash } = hasher.hash(valueToHash);
       expect(hash).toBe(hashFromStrategy);
+    });
+  });
+
+  describe('verifyHash', () => {
+    const saltFromStrategy = Buffer.from('00', 'hex');
+    const matchingHashFromStrategy = Buffer.from('01', 'hex');
+    const nonMatchingHashFromStrategy = Buffer.from('02', 'hex');
+    const matchingTarget = 'matching target';
+    const nonMatchingTarget = 'non matching target';
+
+    let hashStrategy;
+    let hasher;
+
+    beforeEach(() => {
+      hashStrategy = {
+        generateSalt: jest.fn(),
+        generateHash: jest.fn(),
+      };
+
+      when(hashStrategy.generateHash).calledWith(matchingTarget, saltFromStrategy).mockReturnValueOnce(matchingHashFromStrategy);
+      when(hashStrategy.generateHash).calledWith(nonMatchingTarget, saltFromStrategy).mockReturnValueOnce(nonMatchingHashFromStrategy);
+
+      hasher = new Hasher(hashStrategy);
+
+      when(crypto.timingSafeEqual).calledWith(matchingHashFromStrategy, matchingHashFromStrategy).mockReturnValueOnce(true);
+      when(crypto.timingSafeEqual).calledWith(nonMatchingHashFromStrategy, matchingHashFromStrategy).mockReturnValueOnce(false);
+    });
+
+    describe('when the hash matches', () => {
+      it('returns true', () => {
+        const result = hasher.verifyHash({
+          target: matchingTarget,
+          salt: saltFromStrategy,
+          hash: matchingHashFromStrategy,
+        });
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('when the hash does not ', () => {
+      it('returns false', () => {
+        const result = hasher.verifyHash({
+          target: nonMatchingTarget,
+          salt: saltFromStrategy,
+          hash: matchingHashFromStrategy,
+        });
+        expect(result).toBe(false);
+      });
     });
   });
 });
