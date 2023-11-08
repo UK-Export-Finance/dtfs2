1178a12cad7dff4117ae1e7b9ce735649b9ff839
Alex <47955140+AlexBramhill@users.noreply.github.com>
2023-11-08 17:20:50 +0000

    feat(DTFS2-6757): add error handling and tests

--- portal-api/test-helpers/unit-test-mocks/mock-user.js
+++ portal-api/test-helpers/unit-test-mocks/mock-user.js
@@ -16,4 +16,24 @@ const TEST_USER = {
   },
 };
 
-module.exports = { TEST_USER };
+const TEST_DATABASE_USER = {
+  _id: {
+    $oid: '075bcd157dcb851180e02a7c',
+  },
+  'user-status': 'active',
+  timezone: 'Europe/London',
+  username: 'HSBC-maker-1',
+  firstname: 'Mister',
+  surname: 'One',
+  email: 'one@email.com',
+  roles: [MAKER],
+  bank: {
+    id: '961',
+    name: 'HSBC',
+    emails: ['maker1@ukexportfinance.gov.uk', 'maker2@ukexportfinance.gov.uk'],
+  },
+  salt: '01',
+  hash: '02',
+  signInToken: { saltHex: '03', hashHex: '04' },
+};
+module.exports = { TEST_USER, TEST_DATABASE_USER };
