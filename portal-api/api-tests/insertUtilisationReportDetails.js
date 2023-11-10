0cd0172a2d071350f0694508c95b2314e79a0015
Joshua Binns <Joshua.Binns@softwire.com>
2023-11-10 12:35:17 +0000

    feat(FN-1088): small bug fix to portal-api api-tests

--- portal-api/api-tests/insertUtilisationReportDetails.js
+++ portal-api/api-tests/insertUtilisationReportDetails.js
@@ -2,12 +2,12 @@ const db = require('../src/drivers/db-client');
 const { DB_COLLECTIONS } = require('./fixtures/constants');
 
 const insertOneUtilisationReportDetails = async (report) => {
-  const collection = await db.getCollection([DB_COLLECTIONS.UTILISATION_REPORTS]);
+  const collection = await db.getCollection(DB_COLLECTIONS.UTILISATION_REPORTS);
   await collection.insertOne(report);
 };
 
 const insertManyUtilisationReportDetails = async (reports) => {
-  const collection = await db.getCollection([DB_COLLECTIONS.UTILISATION_REPORTS]);
+  const collection = await db.getCollection(DB_COLLECTIONS.UTILISATION_REPORTS);
   await collection.insertMany(reports);
 };
 
