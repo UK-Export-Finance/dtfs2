bf28d4f2046f0fa36309e6f8c2128d2aee6d66d2
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-11-10 09:41:19 +0000

    feat(FN-970): revert jest config change

--- portal/jest.config.js
+++ portal/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverageFrom: ['server/controllers/**/*.{js,}', 'server/routes/**/*.{js,}', 'server/helpers/**/*.{js,}', 'scripts/**/*.{js,}'],
   coverageDirectory: 'generated_reports/coverage/unit',
-  testMatch: ['**/getOneIndexedMonth.test.js'],
+  testMatch: ['**/*.test.js', '**/*.component-test.js'],
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'babel-jest',
     '^exceljs$': 'babel-jest',
