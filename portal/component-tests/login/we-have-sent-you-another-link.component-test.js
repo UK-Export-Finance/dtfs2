b5b7ec7320d21137b5047b743d3254af146bc67a
Luke Marrai <Luke.Marrai@softwire.com>
2023-11-13 08:42:02 +0000

    feat(DTFS2-6770): test that email is rendered on we-have-sent-you-another-link page

--- portal/component-tests/login/we-have-sent-you-another-link.component-test.js
+++ portal/component-tests/login/we-have-sent-you-another-link.component-test.js
@@ -4,13 +4,19 @@ const page = 'login/we-have-sent-you-another-link.njk';
 const render = pageRenderer(page);
 
 describe(page, () => {
+  const signInLinkTargetEmailAddress = 'user@example.com';
+  const regexForTextContainingTargetEmailAddress = /user@example\.com/;
   let wrapper;
 
   beforeEach(() => {
-    wrapper = render();
+    wrapper = render({ signInLinkTargetEmailAddress });
   });
 
-  it('should render link to request a new sign in link', () => {
+  it('should render email link to contact DTFS team', () => {
     wrapper.expectText('[data-cy="dtfs-email-link"]').toRead('DigitalService.TradeFinance@ukexportfinance.gov.uk');
+  });
+
+  it('should render the email address the sign in link has been sent to', () => {
+    wrapper.expectText('[data-cy="sign-in-link-target-email-address"]').toMatch(regexForTextContainingTargetEmailAddress);
   });
 });
