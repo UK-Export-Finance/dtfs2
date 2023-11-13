75e902db6d93d60e822a67b0f81d6939df2bddf1
Luke Marrai <Luke.Marrai@softwire.com>
2023-11-13 08:41:57 +0000

    feat(DTFS2-6770): render CSRF token on new sign in link pages

--- portal/component-tests/login/check-your-email.component-test.js
+++ portal/component-tests/login/check-your-email.component-test.js
@@ -4,13 +4,20 @@ const page = 'login/check-your-email.njk';
 const render = pageRenderer(page);
 
 describe(page, () => {
+  const csrfToken = 'a csrf token';
   let wrapper;
 
   beforeEach(() => {
-    wrapper = render();
+    wrapper = render({ csrfToken });
   });
 
   it('should render link to request a new sign in link', () => {
     wrapper.expectText('[data-cy="request-new-sign-in-link"]').toRead('Request a new sign in link');
+  });
+
+  it('should render a hidden input with the csrf token', () => {
+    const expectCsrfInput = wrapper.expectInput('input[name="_csrf"]');
+    expectCsrfInput.toHaveValue(csrfToken);
+    expectCsrfInput.toBeHidden();
   });
 });
