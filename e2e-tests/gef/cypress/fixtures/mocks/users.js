abefee7cc6809f7c9e7fa73d9689f2090c6f1028
Mark Roberts <Mark.Roberts@softwire.com>
2023-11-14 13:47:54 +0000

    feat(DTFS2-6794): add e2e tests for accessing GEF deals page

---
+++ e2e-tests/gef/cypress/fixtures/mocks/users.js
@@ -0,0 +1,20 @@
+const MOCK_PORTAL_USERS = require('../../../../../utils/mock-data-loader/portal/users');
+const MOCK_TFM_USERS = require('../../../../../utils/mock-data-loader/tfm/mocks/users');
+
+const { USER_ROLES } = require('../constants');
+
+const BANK1_MAKER1 = MOCK_PORTAL_USERS.find((user) => user.roles.includes(USER_ROLES.MAKER) && user.username === 'BANK1_MAKER1');
+
+const ADMIN = MOCK_PORTAL_USERS.find((user) => user.roles.includes(USER_ROLES.ADMIN) && user.username === 'ADMIN');
+
+const READ_ONLY = MOCK_PORTAL_USERS.find((user) => user.roles.includes(USER_ROLES.READ_ONLY) && user.username === 'READ_ONLY');
+
+// TFM Users
+const T1_USER_1 = MOCK_TFM_USERS.find((user) => user.username === 'T1_USER_1');
+
+module.exports = {
+  BANK1_MAKER1,
+  ADMIN,
+  READ_ONLY,
+  T1_USER_1,
+};
