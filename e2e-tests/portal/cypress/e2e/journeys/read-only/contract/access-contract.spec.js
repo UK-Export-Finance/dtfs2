8f4ac9edcdaff134e2687dec377457067a709e69
Mark Roberts <Mark.Roberts@softwire.com>
2023-11-15 09:29:13 +0000

    feat(DTFS2-6794): add e2e tests for admin & readonly user accessing contracts page

---
+++ e2e-tests/portal/cypress/e2e/journeys/read-only/contract/access-contract.spec.js
@@ -0,0 +1,52 @@
+const fullyCompletedDeal = require('../fixtures/dealFullyCompleted');
+const { READ_ONLY_ALL_BANKS, BANK1_MAKER1, ADMIN } = require('../../../../fixtures/users');
+const relative = require('../../../relativeURL');
+
+context('Access contract page', () => {
+  let deal;
+  let dealId;
+  const dealFacilities = {
+    bonds: [],
+    loans: [],
+  };
+
+  beforeEach(() => {
+    cy.deleteDeals(ADMIN);
+    cy.insertOneDeal(fullyCompletedDeal, BANK1_MAKER1).then((insertedDeal) => {
+      deal = insertedDeal;
+      dealId = deal._id;
+
+      const { mockFacilities } = fullyCompletedDeal;
+
+      cy.createFacilities(dealId, mockFacilities, BANK1_MAKER1).then((createdFacilities) => {
+        const bonds = createdFacilities.filter((f) => f.type === 'Bond');
+        const loans = createdFacilities.filter((f) => f.type === 'Loan');
+
+        dealFacilities.bonds = bonds;
+        dealFacilities.loans = loans;
+      });
+    });
+  });
+
+  after(() => {
+    cy.deleteDeals(ADMIN);
+
+    dealFacilities.bonds.forEach((facility) => {
+      cy.deleteFacility(facility._id, BANK1_MAKER1);
+    });
+
+    dealFacilities.loans.forEach((facility) => {
+      cy.deleteFacility(facility._id, BANK1_MAKER1);
+    });
+  });
+
+  it('allows read only user with all bank access to view contract page', () => {
+    cy.loginGoToDealPage(READ_ONLY_ALL_BANKS, deal);
+    cy.url().should('eq', relative(`/contract/${deal._id}`));
+  });
+
+  it('allows admin user to view contract page', () => {
+    cy.loginGoToDealPage(ADMIN, deal);
+    cy.url().should('eq', relative(`/contract/${deal._id}`));
+  });
+});
--- e2e-tests/portal/cypress/fixtures/users.js
+++ e2e-tests/portal/cypress/fixtures/users.js
@@ -16,6 +16,8 @@ const BANK1_READ_ONLY1 = MOCK_USERS.find((user) => user.roles.includes(USER_ROLE
 
 const ADMIN = MOCK_USERS.find((user) => user.username === 'ADMIN');
 
+const READ_ONLY_ALL_BANKS = MOCK_USERS.find((user) => user.roles.includes(USER_ROLES.READ_ONLY) && user.username === 'READ_ONLY' && user.bank.id === '*');
+
 // TFM
 const UNDERWRITER_MANAGER = MOCK_TFM_USERS.find((user) => user.teams.includes('UNDERWRITER_MANAGERS'));
 
@@ -39,4 +41,5 @@ module.exports = {
   ADMIN,
   UNDERWRITER_MANAGER,
   USER_WITH_INJECTION,
+  READ_ONLY_ALL_BANKS,
 };
