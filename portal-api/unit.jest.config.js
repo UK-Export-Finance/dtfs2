e76bed3b5aef3e98bb4d37d760abfa67b653cd7e
Francesca Stocco <fstocco@ukexportfinance.gov.uk>
2023-11-17 17:15:01 +0000

    feat(FN-967): update unit test config

--- portal-api/unit.jest.config.js
+++ portal-api/unit.jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{js,}'],
   coverageDirectory: 'generated_reports/coverage/unit',
-  testMatch: ['**/utilisation-report-upload.controller.test.js'],
+  testMatch: ['**/*.test.js'],
 };
