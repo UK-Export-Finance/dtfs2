f67bc6ec393a278cd076ba8388c60fbc04f5cf24
Mark Roberts <Mark.Roberts@softwire.com>
2023-11-17 12:04:20 +0000

    feat(DTFS2-6794): refactor tfm users

--- e2e-tests/e2e-fixtures/tfm-users.fixture.js
+++ e2e-tests/e2e-fixtures/tfm-users.fixture.js
@@ -1,77 +1,23 @@
-const defaultUserDetails = {
-  password: 'AbC!2345',
-  email: 'test@testing.com',
-  timezone: 'Europe/London',
-};
+const {
+  UNDERWRITER_MANAGER_1,
+  UNDERWRITER_MANAGER_2,
+  UNDERWRITER_1,
+  BUSINESS_SUPPORT_USER_1,
+  BUSINESS_SUPPORT_USER_2,
+  T1_USER_1,
+  RISK_MANAGER_1,
+  UNDERWRITING_SUPPORT_1,
+  PIM_USER_1,
+} = require('./users');
 
-export const UNDERWRITER_MANAGER_1 = {
-  username: 'UNDERWRITER_MANAGER_1',
-  teams: ['UNDERWRITER_MANAGERS'],
-  firstName: 'Benjamin',
-  lastName: 'Jones',
-  ...defaultUserDetails,
-};
-
-export const UNDERWRITER_MANAGER_2 = {
-  username: 'UNDERWRITER_MANAGER_2',
-  teams: ['UNDERWRITER_MANAGERS'],
-  firstName: 'Jonathan',
-  lastName: 'Roberts',
-  ...defaultUserDetails,
-};
-
-export const UNDERWRITER_1 = {
-  username: 'UNDERWRITER_1',
-  teams: ['UNDERWRITERS'],
-  firstName: 'Olivia',
-  lastName: 'Williams',
-  ...defaultUserDetails,
-};
-
-export const BUSINESS_SUPPORT_USER_1 = {
-  username: 'BUSINESS_SUPPORT_USER_1',
-  teams: ['BUSINESS_SUPPORT'],
-  firstName: 'John',
-  lastName: 'Davies',
-  ...defaultUserDetails,
-};
-
-export const BUSINESS_SUPPORT_USER_2 = {
-  username: 'BUSINESS_SUPPORT_USER_2',
-  teams: ['BUSINESS_SUPPORT'],
-  firstName: 'Sarah',
-  lastName: 'Walker',
-  ...defaultUserDetails,
-};
-
-export const T1_USER_1 = {
-  username: 'T1_USER_1',
-  teams: ['TEAM1'],
-  firstName: 'Joe',
-  lastName: 'Bloggs',
-  ...defaultUserDetails,
-};
-
-export const RISK_MANAGER_1 = {
-  username: 'RISK_MANAGER_1',
-  teams: ['RISK_MANAGERS'],
-  firstName: 'Steven',
-  lastName: 'Robinson',
-  ...defaultUserDetails,
-};
-
-export const UNDERWRITING_SUPPORT_1 = {
-  username: 'UNDERWRITING_SUPPORT_1',
-  teams: ['UNDERWRITING_SUPPORT'],
-  firstName: 'Ben',
-  lastName: 'Wilson',
-  ...defaultUserDetails,
-};
-
-export const PIM_USER_1 = {
-  username: 'PIM_USER_1',
-  teams: ['PIM', 'BUSINESS_SUPPORT'],
-  firstName: 'Adam',
-  lastName: 'Last',
-  ...defaultUserDetails,
+module.exports = {
+  UNDERWRITER_MANAGER_1,
+  UNDERWRITER_MANAGER_2,
+  UNDERWRITER_1,
+  BUSINESS_SUPPORT_USER_1,
+  BUSINESS_SUPPORT_USER_2,
+  T1_USER_1,
+  RISK_MANAGER_1,
+  UNDERWRITING_SUPPORT_1,
+  PIM_USER_1,
 };
--- e2e-tests/tfm/cypress/fixtures/users.js
+++
@@ -1,3 +0,0 @@
-const mocks = require('../../../../utils/mock-data-loader/tfm/mocks/users');
-// TODO (DTFS2-6794): Refactor this file to use e2e-fixtures
-module.exports = mocks;
