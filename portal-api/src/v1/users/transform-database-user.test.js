70ea08c37169239b4134742e2001057703af5e0d
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-11-20 17:35:15 +0000

    fix(DTFS2-6693): fix and improve transform database user(s) test

--- portal-api/src/v1/users/transform-database-user.test.js
+++ portal-api/src/v1/users/transform-database-user.test.js
@@ -1,26 +1,17 @@
 const { TEST_DATABASE_USER } = require('../../../test-helpers/unit-test-mocks/mock-user');
-
-const transformDatabaseUser = (user) => {
-  const userToReturn = { ...user };
-  if (userToReturn.signInToken) {
-    const { hashHex, saltHex } = userToReturn.signInToken;
-    userToReturn.signInToken = {
-      hash: Buffer.from(hashHex, 'hex'),
-      salt: Buffer.from(saltHex, 'hex'),
-    };
-  }
-  return userToReturn;
-};
+const { transformDatabaseUser } = require('./transform-database-user');
 
 describe('transformDatabaseUser', () => {
   describe('signinToken transformations', () => {
     it('returns the user with the signInToken hash and salt as Buffers', () => {
-      const { saltHex, hashHex } = TEST_DATABASE_USER.signInToken;
+      const { saltHex, hashHex, expiry } = TEST_DATABASE_USER.signInToken;
 
       const result = transformDatabaseUser(TEST_DATABASE_USER);
 
       expect(result.signInToken.salt).toEqual(Buffer.from(saltHex, 'hex'));
       expect(result.signInToken.hash).toEqual(Buffer.from(hashHex, 'hex'));
+      expect(result.signInToken.expiry).toEqual(expiry);
+      
     });
 
     it('returns the user without the signInToken hashHex and saltHex', () => {
