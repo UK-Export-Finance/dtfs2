fc47bc6a7a73c0044ff4a42a94317d8d10d083d9
Joshua Binns <Joshua.Binns@softwire.com>
2023-11-20 14:54:04 +0000

    feat(FN-1548): fix broken portal-api unit tests

--- portal-api/src/scheduler/jobs/helpers/utilisation-report-helpers.js
+++ portal-api/src/scheduler/jobs/helpers/utilisation-report-helpers.js
@@ -43,11 +43,10 @@ const getReportOverdueChaserDate = async () => {
   const bankHolidays = await externalApi.bankHolidays.getBankHolidayDatesForRegion(BANK_HOLIDAY_REGION.ENGLAND_AND_WALES);
   const businessDay = process.env.UTILISATION_REPORT_OVERDUE_CHASER_DATE_BUSINESS_DAYS_FROM_START_OF_MONTH;
 
-  const currentDate = new Date();
-  const reportPeriodDate = subMonths(currentDate, 1);
-  const oneIndexedReportPeriodMonth = reportPeriodDate.getMonth() + 1;
-  const reportPeriodYear = reportPeriodDate.getFullYear();
-  return getBusinessDayOfMonth(oneIndexedReportPeriodMonth, reportPeriodYear, bankHolidays, businessDay);
+  const reportDate = new Date();
+  const oneIndexedReportMonth = reportDate.getMonth() + 1;
+  const reportYear = reportDate.getFullYear();
+  return getBusinessDayOfMonth(oneIndexedReportMonth, reportYear, bankHolidays, businessDay);
 };
 
 /**
