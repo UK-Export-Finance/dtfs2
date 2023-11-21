f57fecaef3775615100c32f329fbb4b642a8da33
Alex <47955140+AlexBramhill@users.noreply.github.com>
2023-11-21 17:47:34 +0000

    feat(DTFS2-6757): review comments

--- utils/mock-data-loader/database/database.config.js
+++ utils/mock-data-loader/database/database.config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const dbName = process.env.MONGO_INITDB_DATABASE;
-// The below is different than MONGOD_URI env var as mock-data-loader does not run within the container
+// The below is different from the MONGODB_URI env var as mock-data-loader does not run within the container.
 const connectionString = 'mongodb://root:r00t@localhost:27017/?authMechanism=DEFAULT';
 
 module.exports = {
