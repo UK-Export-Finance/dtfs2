4130b4fb3e714aa4ff9dbd1310ca590855327080
Alex <47955140+AlexBramhill@users.noreply.github.com>
2023-11-21 17:59:54 +0000

    feat(DTFS2-6757): review comments

--- portal-api/test-helpers/api-test-helpers/database/database.config.js
+++ portal-api/test-helpers/api-test-helpers/database/database.config.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const dbName = process.env.MONGO_INITDB_DATABASE;
-const connectionString = process.env.MONGODB_URI;
+// The below is different from the MONGODB_URI env var to allow for this to be run both outside of the docker container and inside the docker container.
+const connectionString = process.env.TEST_HELPER_MONGODB_URI || process.env.MONGODB_URI;
 
 module.exports = {
   dbName,
