13f7058a03d9d8175f935c2974420b50640d6c24
MarRobSoftwire <108682750+MarRobSoftwire@users.noreply.github.com>
2023-11-21 13:55:37 +0000

    feat(DTFS2-6781): add typescript compatibility to portal api (#2302)
    Co-authored-by: Luke Marrai <Luke.Marrai@softwire.com>

--- portal-api/src/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-require('./azure-env');
-const app = require('./createApp');
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.info('✅ Portal API micro-service initialised on :%s', PORT));
