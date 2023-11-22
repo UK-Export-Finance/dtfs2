5385b11e6466e3fa3994be0c41fdbb20a2477d17
Joshua Binns <Joshua.Binns@softwire.com>
2023-11-22 16:03:44 +0000

    chore: refactor base url routing behaviour

--- portal/server/routes/middleware/validateRole/index.js
+++ portal/server/routes/middleware/validateRole/index.js
@@ -1,3 +1,6 @@
+const { ROLES } = require('../../../constants');
+const { getUserRoles } = require('../../../helpers');
+
 /**
  *
  * @param {array} requiredRoles  (i.e. ['maker'])
@@ -16,20 +19,38 @@ const userRoleIsValid = (requiredRoles, user) => {
   const userHasOneOfTheRequiredRoles = requiredRoles.some((role) => user.roles.includes(role));
   return userHasOneOfTheRequiredRoles;
 };
+
+/**
+ * Redirects unauthorised user to their respective
+ * home page.
+ * @param {import('express').Request} req - Express request object
+ * @param {import('express').Response} res - Express response object
+ */
+const redirectUnauthorisedUser = (req, res) => {
+  const { user } = req.session;
+  if (!user) {
+    return res.redirect('/login');
+  }
+
+  if (user.roles.some((role) => role === ROLES.PAYMENT_REPORT_OFFICER)) {
+    return res.redirect('/utilisation-report-upload');
+  }
+  return res.redirect('/dashboard/deals/0');
+};
+
 /**
  *
  * @param {object} opts  (i.e. { role: [MAKER] })
  *
  */
-const validateRole = (opts, getRedirectUrl = () => '/') => {
+const validateRole = (opts) => {
   const requiredRoles = opts ? opts.role : null;
 
   return (req, res, next) => {
     if (userRoleIsValid(requiredRoles, req.session.user)) {
-      next();
-    } else {
-      res.redirect(getRedirectUrl(req));
+      return next();
     }
+    return redirectUnauthorisedUser(req, res);
   };
 };
 
