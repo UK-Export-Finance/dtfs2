891ca0ed67fd60a408e71c59a8e6318ab0f2c084
Rithvik <rithvik0112@outlook.com>
2023-11-24 16:28:55 +0000

    feat(DTFS2-5659): extend component tests tfm ui

---
+++ trade-finance-manager-ui/component-tests/case/underwriting/pricing-and-risk1/_macros/section-exporter/exporter-table.component-test.js
@@ -0,0 +1,130 @@
+const componentRenderer = require('../../../../../componentRenderer');
+
+const component = '../templates/case/underwriting/pricing-and-risk/_macros/section-exporter/exporter-table.njk';
+
+const render = componentRenderer(component);
+
+describe(component, () => {
+  let wrapper;
+  const defaultParams = {
+    caseId: '1234',
+    exporterCreditRating: 'Good (BB-)',
+    userCanEditGeneral: false,
+    lossGivenDefault: '50',
+    probabilityOfDefault: '14.1',
+  };
+
+  describe('credit rating', () => {
+    it('should render table cell heading', () => {
+      wrapper = render(defaultParams);
+
+      wrapper.expectText('[data-cy="exporter-table-credit-rating-heading"]').toRead('Credit rating');
+    });
+
+    it('should render value', () => {
+      wrapper = render(defaultParams);
+
+      wrapper.expectText('[data-cy="exporter-table-credit-rating-value"]').toRead(defaultParams.exporterCreditRating);
+    });
+
+    describe('when there is no credit rating', () => {
+      it('should render `Not added` govukTag', () => {
+        const params = {
+          ...defaultParams,
+          exporterCreditRating: null,
+        };
+
+        wrapper = render(params);
+
+        wrapper.expectElement('[data-cy="exporter-table-credit-rating-value"]').notToExist();
+        wrapper.expectElement('[data-cy="exporter-table-credit-rating-not-added"]').toExist();
+        wrapper.expectText('[data-cy="exporter-table-credit-rating-not-added"]').toRead('Not added');
+      });
+    });
+
+    it('should NOT render `Change` link by default', () => {
+      wrapper = render(defaultParams);
+
+      wrapper.expectElement('[data-cy="exporter-table-change-credit-rating-link"]').notToExist();
+      wrapper.expectElement('[data-cy="exporter-table-change-loss-given-default-link"]').notToExist();
+      wrapper.expectElement('[data-cy="exporter-table-change-probability-of-default-link"]').notToExist();
+    });
+
+    describe('with params.exporterCreditRating and params.userCanEditGeneral', () => {
+      it('should render `Change` link', () => {
+        const params = {
+          ...defaultParams,
+          userCanEditGeneral: true,
+          exporterCreditRating: 'Good (BB-)',
+        };
+
+        wrapper = render(params);
+
+        wrapper.expectLink('[data-cy="exporter-table-change-credit-rating-link"]')
+          .toLinkTo(`/case/${params.caseId}/underwriting/pricing-and-risk/edit`, 'Change');
+      });
+    });
+
+    describe('with params.userCanEditGeneral', () => {
+      it('should render `Change` link', () => {
+        const params = {
+          ...defaultParams,
+          userCanEditGeneral: true,
+        };
+
+        wrapper = render(params);
+
+        wrapper.expectLink('[data-cy="exporter-table-change-loss-given-default-link"]')
+          .toLinkTo(`/case/${params.caseId}/underwriting/pricing-and-risk/loss-given-default`, 'Change');
+
+        wrapper.expectLink('[data-cy="exporter-table-change-probability-of-default-link"]')
+          .toLinkTo(`/case/${params.caseId}/underwriting/pricing-and-risk/probability-of-default`, 'Change');
+      });
+    });
+  });
+
+  describe('loss given default', () => {
+    it('should render table cell heading', () => {
+      wrapper = render(defaultParams);
+
+      wrapper.expectText('[data-cy="exporter-table-loss-given-default-heading"]').toRead('Loss given default');
+    });
+
+    it('should render value', () => {
+      wrapper = render(defaultParams);
+
+      wrapper.expectText('[data-cy="exporter-table-loss-given-default-value"]').toRead(`${defaultParams.lossGivenDefault}%`);
+    });
+
+    describe('when there is no loss given default', () => {
+      it('should render a dash', () => {
+        wrapper = render({});
+
+        wrapper.expectText('[data-cy="exporter-table-loss-given-default-value"]').toRead('-');
+      });
+    });
+  });
+
+  describe('probability of default', () => {
+    it('should render table cell heading', () => {
+      wrapper = render(defaultParams);
+
+      wrapper.expectText('[data-cy="exporter-table-probability-of-default-heading"]').toRead('Probability of default');
+    });
+
+    it('should render value', () => {
+      wrapper = render(defaultParams);
+
+      wrapper.expectText('[data-cy="exporter-table-probability-of-default-value"]')
+        .toRead(`Less than ${defaultParams.probabilityOfDefault}%`);
+    });
+
+    describe('when there is no probability of default', () => {
+      it('should render a dash', () => {
+        wrapper = render({});
+
+        wrapper.expectText('[data-cy="exporter-table-probability-of-default-value"]').toRead('-');
+      });
+    });
+  });
+});
---
+++ trade-finance-manager-ui/component-tests/case/underwriting/pricing-and-risk1/_macros/section-facilities/facility-pricing-risk-table.component-test.js
@@ -0,0 +1,73 @@
+const componentRenderer = require('../../../../../componentRenderer');
+
+const component = '../templates/case/underwriting/pricing-and-risk/_macros/section-facilities/facility-pricing-risk-table.njk';
+
+const render = componentRenderer(component);
+
+describe(component, () => {
+  let wrapper;
+  let params = {
+    caseId: '1234',
+    facility: {
+      _id: '1',
+      facilitySnapshot: {
+        ukefFacilityId: '100',
+        type: 'Loan',
+      },
+      tfm: {
+        riskProfile: 'Flat',
+      },
+    },
+  };
+
+  it('should render link to facility page', () => {
+    wrapper = render(params);
+
+    wrapper.expectLink(`[data-cy="facility-${params.facility._id}-ukef-facility-id-link"]`).toLinkTo(
+      `/case/${params.caseId}/facility/${params.facility._id}`,
+      params.facility.facilitySnapshot.ukefFacilityId,
+    );
+  });
+
+  it('should render facility type', () => {
+    wrapper = render(params);
+
+    wrapper.expectText(`[data-cy="facility-${params.facility._id}-type"]`).toRead(params.facility.facilitySnapshot.type);
+  });
+
+  describe('risk profile table row', () => {
+    it('should render heading', () => {
+      wrapper = render(params);
+
+      wrapper.expectText(`[data-cy="facility-${params.facility._id}-risk-profile-heading"]`).toRead('Risk profile');
+    });
+
+    it('should render value', () => {
+      wrapper = render(params);
+
+      wrapper.expectText(`[data-cy="facility-${params.facility._id}-risk-profile-value"]`).toRead(params.facility.tfm.riskProfile);
+    });
+
+    it('should NOT render change link', () => {
+      wrapper = render(params);
+
+      wrapper.expectElement(`[data-cy="facility-${params.facility._id}-change-risk-profile-link"]`).notToExist();
+    });
+
+    describe('with params.userCanEdit', () => {
+      it('should render change link', () => {
+        params = {
+          ...params,
+          userCanEdit: true,
+        };
+
+        wrapper = render(params);
+
+        const expectedLink = `/case/${params.caseId}/underwriting/pricing-and-risk/facility/${params.facility._id}/risk-profile`;
+
+        wrapper.expectLink(`[data-cy="facility-${params.facility._id}-change-risk-profile-link"]`)
+          .toLinkTo(expectedLink, 'Change');
+      });
+    });
+  });
+});
