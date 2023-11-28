0a152a476c4770b38df3a41b3f9ad14b82fbd167
Joshua Binns <Joshua.Binns@softwire.com>
2023-11-28 12:10:56 +0000

    feat(FN-1618): add dtfs central api endpoint for marking as done or not done

---
+++ dtfs-central-api/src/v1/controllers/utilisation-report-service/interfaces/utilisation-report-status.ts
@@ -0,0 +1,33 @@
+import { ObjectId } from 'mongodb';
+
+export const enum ReportStatus {
+  PENDING_RECONCILIATION = 'PENDING_RECONCILIATION',
+  REPORT_NOT_RECEIVED = 'REPORT_NOT_RECEIVED',
+};
+
+export interface ReportDetails {
+  month: number;
+  year: number;
+  bankId: string;
+};
+
+interface ReportId {
+  id: ObjectId;
+};
+
+type ReportWithStatus = {
+  status: ReportStatus;
+  report: ReportDetails | ReportId;
+};
+
+export interface PutReportStatusRequestBody {
+  reportsWithStatus: ReportWithStatus[];
+};
+
+export type ReportFilter = ReportId | {
+  month: number;
+  year: number;
+  bank: {
+    id: string;
+  };
+};
