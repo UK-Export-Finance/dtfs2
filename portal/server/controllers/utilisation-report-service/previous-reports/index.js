74f43d66b095b6c7e9867fb11a639ef2f6f407b3
Michael Todd <michael.todd@softwire.com>
2023-11-29 10:44:19 +0000

    fix(FN-976): fix report download link

--- portal/server/controllers/utilisation-report-service/previous-reports/index.js
+++ portal/server/controllers/utilisation-report-service/previous-reports/index.js
@@ -1,5 +1,6 @@
 const api = require('../../../api');
 const { getApiData } = require('../../../helpers');
+const { getMonthName } = require('../../../helpers/getMonthName');
 
 const getPreviousReports = async (req, res) => {
   const { user, userToken } = req.session;
@@ -26,7 +27,7 @@ const getPreviousReports = async (req, res) => {
 
     const reports = navItems?.length
       ? utilisation?.reports?.map((report) => ({
-          month: report.month,
+          month: getMonthName(report.month),
           filename: report.azureFileInfo?.filename,
           path: `/banks/${bankId}/utilisation-report-download/${report._id}`,
         }))
