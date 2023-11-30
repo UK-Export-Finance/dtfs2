ccda0d40f3e83b0d2127f721a5a7424e0ab17c50
Michael Todd <mtodd@ukexportfinance.gov.uk>
2023-11-30 11:00:11 +0000

    fix(FN-976): error handling

--- portal/server/controllers/utilisation-report-service/utilisation-report-download/index.js
+++ portal/server/controllers/utilisation-report-service/utilisation-report-download/index.js
@@ -1,17 +1,28 @@
 const stream = require('stream');
 const api = require('../../../api');
 
+/**
+ * Fetches a utilisation report DSV file for download
+ * @param {import('express').Request} req - Express request object
+ * @param {import('express').Response} res - Express response object
+ */
 const getReportDownload = async (req, res) => {
-  const { userToken } = req.session;
-  const { bankId, _id } = req.params;
+  try {
+    const { userToken } = req.session;
+    const { bankId, _id } = req.params;
 
-  const { data, headers } = await api.downloadUtilisationReport(userToken, bankId, _id);
+    const { data, headers } = await api.downloadUtilisationReport(userToken, bankId, _id);
 
-  res.set('content-disposition', headers['content-disposition']);
-  res.set('content-type', headers['content-type']);
+    res.set('content-disposition', headers['content-disposition']);
+    res.set('content-type', headers['content-type']);
 
-  const readStream = new stream.PassThrough();
-  data.pipe(readStream).pipe(res);
+    const readStream = new stream.PassThrough();
+    data.pipe(readStream).pipe(res);
+  } catch (error) {
+    const errorMessage = 'Failed to download utilisation report';
+    console.error(errorMessage, error);
+    res.status(error.response?.status ?? 500).send({ message: errorMessage });
+  }
 };
 
 module.exports = { getReportDownload };
