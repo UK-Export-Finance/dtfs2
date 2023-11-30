cba1c27ce52ea5878d61f710f09d9d47eb411ac8
Joshua Binns <Joshua.Binns@softwire.com>
2023-11-30 10:02:21 +0000

    feat(FN-1536): add swagger documentation

--- dtfs-central-api/src/v1/swagger-definitions/utilisationReport.js
+++ dtfs-central-api/src/v1/swagger-definitions/utilisationReport.js
@@ -42,4 +42,43 @@
  *           name:
  *             type: string
  *             example: 'UKEF test bank (Delegated)'
+ *   ReportStatus:
+ *     type: string
+ *     enum: ['REPORT_NOT_RECEIVED', 'PENDING_RECONCILIATION']
+ *     description: Status of the utilisation report
+ *   ReportStatusWithReportId:
+ *     type: object
+ *     properties:
+ *       status:
+ *         type: ReportStatus
+ *         example: 'REPORT_NOT_RECEIVED'
+ *         description: Report status to set
+ *       report:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: string
+ *             description: Mongo id of the report
+ *   ReportStatusWithBankId:
+ *     type: object
+ *     properties:
+ *       status:
+ *         type: ReportStatus
+ *         example: 'REPORT_NOT_RECEIVED'
+ *         description: Report status to set
+ *       report:
+ *         type: object
+ *         properties:
+ *           month:
+ *             type: number
+ *             example: 1
+ *             description: One-indexed month
+ *           year:
+ *             type: number
+ *             example: 2023
+ *             description: Full year
+ *           bankId:
+ *             type: string
+ *             example: '123'
+ *             description: Bank id
  */
