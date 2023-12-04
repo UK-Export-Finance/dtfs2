800a6cdf59599b2087453ac30a7da052e0f27d23
Mark Roberts <Mark.Roberts@softwire.com>
2023-12-04 15:36:54 +0000

    feat(DTFS2-6734): add component test for generate activity report page

---
+++ portal/component-tests/activity/generate-activity-report.component-test.js
@@ -0,0 +1,24 @@
+const pageRenderer = require('../pageRenderer');
+
+const page = 'activity/generate-activity-report.njk';
+const render = pageRenderer(page);
+
+describe(page, () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = render();
+  });
+
+  it('should render a back link', () => {
+    wrapper.expectText('[data-cy="back-link"]').toExist();
+  });
+
+  it('should render the search bar', () => {
+    wrapper.expect('[data-cy="users-search-bar"]').toExist();
+  });
+
+  it('should render the find user button', () => {
+    wrapper.expect('[data-cy="find-users-button"]').toRead('Find user');
+  });
+});
