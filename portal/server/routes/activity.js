a72d52507b4e0ba93bb7533d930458aac73e85eb
Mark Roberts <Mark.Roberts@softwire.com>
2023-12-04 14:32:46 +0000

    feat(DTFS2-6734): add functionality to generate activity report page and select users page template

--- portal/server/routes/activity.js
+++ portal/server/routes/activity.js
@@ -1,7 +1,54 @@
 const express = require('express');
+const { validationErrorHandler } = require('../helpers');
 
 const router = express.Router();
 
-router.get('/activity', async (req, res) => res.render('activity/index.njk'));
+router.get('/activity', async (req, res) => res.render('activity/generate-activity-report.njk', { user: req.session.user }));
+
+router.post('/activity', async (req, res) => {
+  const { usersSearchTerm } = req.body;
+
+  if (!usersSearchTerm) {
+    return res.render('activity/generate-activity-report.njk', {
+      user: req.session.user,
+      errors: validationErrorHandler([
+        {
+          errMsg: 'Enter the name or email address of the user you wish to be featured in the report',
+          errRef: 'searchTerm',
+        },
+      ]),
+    });
+  }
+
+  return res.redirect(`/activity/${usersSearchTerm}`);
+});
+
+router.get('/activity/:searchTerm', async (req, res) => {
+  const { searchTerm } = req.params;
+  const usersRadioButtonItems = [
+    {
+      value: {
+        username: 'test',
+        bank: {
+          name: 'UKEF',
+        },
+        email: 'test@example.com',
+      },
+      text: 'test (UKEF, test@example.com )',
+    },
+    {
+      value: {
+        username: 'test2',
+        bank: {
+          name: 'Barclays',
+        },
+        email: 'test2@example.com',
+      },
+      text: 'test2 (Barclays, test2@example.com )',
+    },
+  ];
+
+  res.render('activity/select-user.njk', { searchTerm, user: req.session.user, usersRadioButtonItems });
+});
 
 module.exports = router;
