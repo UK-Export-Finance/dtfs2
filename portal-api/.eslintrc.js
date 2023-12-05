b104d68982c85910b66f60e22574dd440dac25ed
lmarrai-sw <44811960+lmarrai-sw@users.noreply.github.com>
2023-12-05 16:05:16 +0000

    feat(DTFS2-6796): allow using sign in link in different session than it was sent (#2416)
    Co-authored-by: oscar-richardson-softwire <116292912+oscar-richardson-softwire@users.noreply.github.com>

--- portal-api/.eslintrc.js
+++ portal-api/.eslintrc.js
@@ -9,39 +9,14 @@ module.exports = {
     browser: true,
   },
   root: true,
-  rules: {
-    'class-methods-use-this': 'off',
-    'max-len': ['error', 160, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
-    'import/no-unresolved': 'error',
-    'no-console': ['error', { allow: ['info', 'error'] }],
-    'no-underscore-dangle': ['error', { allow: ['_id', '_csrf'] }],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.js', '**/*.api-test.js', '**/api-tests/**'] }],
-    'import/no-named-as-default': 'off',
-    'implicit-arrow-linebreak': 'off',
-    'comma-dangle': 'off',
-    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
-    'no-loop-func': 'off',
-    'no-unused-vars': ['error'],
-    'object-curly-newline': ['error', {
-      consistent: true,
-    }],
-    'no-restricted-syntax': 'off',
-    'no-await-in-loop': 'off',
-    'no-use-before-define': [
-      'error',
-      {
-        functions: false
-      }
-    ]
-  },
   ignorePatterns: ['**/node_modules/**'],
   parserOptions: baseParserOptions,
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
   overrides: [
     {
       files: ['*.ts'],
@@ -59,5 +34,41 @@ module.exports = {
         tsconfigRootDir: __dirname,
       },
     },
+    {
+      files: ['*.{j,t}s'],
+      rules: {
+        'class-methods-use-this': 'off',
+        'max-len': ['error', 160, 2, {
+          ignoreUrls: true,
+          ignoreComments: false,
+          ignoreRegExpLiterals: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        }],
+        'import/no-unresolved': 'error',
+        'no-console': ['error', { allow: ['info', 'error'] }],
+        'no-underscore-dangle': ['error', { allow: ['_id', '_csrf'] }],
+        'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.js', '**/*.api-test.js', '**/api-tests/**'] }],
+        'import/no-named-as-default': 'off',
+        'import/prefer-default-export': 'off',
+        'import/extensions': 'off',
+        'implicit-arrow-linebreak': 'off',
+        'comma-dangle': 'off',
+        'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+        'no-loop-func': 'off',
+        'no-unused-vars': ['error'],
+        'object-curly-newline': ['error', {
+          consistent: true,
+        }],
+        'no-restricted-syntax': 'off',
+        'no-await-in-loop': 'off',
+        'no-use-before-define': [
+          'error',
+          {
+            functions: false
+          }
+        ]
+      },
+    }
   ],
 };
