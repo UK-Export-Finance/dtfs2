a7eb678ac44ff309e9d42ea4d0b04624945b798a
Mark Roberts <Mark.Roberts@softwire.com>
2023-12-06 14:57:10 +0000

    feat(DTFS2-6734): update routing for activity search endpoints

--- portal/server/routes/admin/activity.js
+++ portal/server/routes/admin/activity.js
@@ -3,28 +3,26 @@ const { validationErrorHandler } = require('../../helpers');
 
 const router = express.Router();
 
-router.get('/activity', async (req, res) => res.render('admin/activity/search-users.njk', { user: req.session.user }));
+router.get('/activity/search', async (req, res) => {
+  const searchTerm = req.query.q;
 
-router.post('/activity', async (req, res) => {
-  const { usersSearchTerm } = req.body;
+  if (!searchTerm) {
+    return res.render('admin/activity/search-users.njk', { user: req.session.user, primaryNav: 'activity' });
+  }
 
-  if (!usersSearchTerm) {
+  if (searchTerm.length < 3) {
     return res.render('admin/activity/search-users.njk', {
       user: req.session.user,
       errors: validationErrorHandler([
         {
-          errMsg: 'Enter the name or email address of the user you wish to be featured in the report',
+          errMsg: 'The name or email address of the user must contain at least 3 characters',
           errRef: 'usersSearchTerm',
         },
       ]),
+      primaryNav: 'activity',
     });
   }
 
-  return res.redirect(`/activity/${usersSearchTerm}`);
-});
-
-router.get('/activity/:searchTerm', async (req, res) => {
-  const { searchTerm } = req.params;
   const usersRadioButtonItems = [
     {
       value: {
@@ -48,7 +46,43 @@ router.get('/activity/:searchTerm', async (req, res) => {
     },
   ];
 
-  res.render('admin/activity/select-user.njk', { searchTerm, user: req.session.user, usersRadioButtonItems });
+  return res.render('admin/activity/select-user.njk', {
+    searchTerm,
+    user: req.session.user,
+    usersRadioButtonItems,
+    primaryNav: 'activity',
+  });
+});
+
+router.post('/activity/search', async (req, res) => {
+  const { usersSearchTerm } = req.body;
+
+  if (!usersSearchTerm) {
+    return res.render('admin/activity/search-users.njk', {
+      user: req.session.user,
+      errors: validationErrorHandler([
+        {
+          errMsg: 'Enter the name or email address of the user you wish to be featured in the report',
+          errRef: 'usersSearchTerm',
+        },
+      ]),
+      primaryNav: 'activity',
+    });
+  }
+  if (usersSearchTerm.length < 3) {
+    return res.render('admin/activity/search-users.njk', {
+      user: req.session.user,
+      errors: validationErrorHandler([
+        {
+          errMsg: 'The name or email address of the user must contain at least 3 characters',
+          errRef: 'usersSearchTerm',
+        },
+      ]),
+      primaryNav: 'activity',
+    });
+  }
+
+  return res.redirect(301, `?q=${encodeURIComponent(usersSearchTerm)}`);
 });
 
 module.exports = router;
