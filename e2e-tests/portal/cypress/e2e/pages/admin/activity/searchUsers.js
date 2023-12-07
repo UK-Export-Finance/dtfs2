746fd1d11de8c8bf35bedf3babebac428426218d
Mark Roberts <Mark.Roberts@softwire.com>
2023-12-07 15:30:19 +0000

    feat(DTFS2-6734): fix e2e test

--- e2e-tests/portal/cypress/e2e/pages/admin/activity/searchUsers.js
+++ e2e-tests/portal/cypress/e2e/pages/admin/activity/searchUsers.js
@@ -1,9 +1,9 @@
 const page = {
   visit: () => cy.visit('/admin/activity/search'),
 
-  errorSummary: () => cy.get('[data-cy="error-summary"'),
-  searchTermError: () => cy.get('[data-cy="search-term-error"'),
-  searchBar: () => cy.get('[data-cy="users-search-bar"'),
+  errorSummary: () => cy.get('[data-cy="error-summary"]'),
+  searchTermError: () => cy.get('[data-cy="search-term-error"]'),
+  searchBar: () => cy.get('[data-cy="users-search-bar"]'),
   findUsersButton: () => cy.get('[data-cy="find-users-button"]'),
 };
 
