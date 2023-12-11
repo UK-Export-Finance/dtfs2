2fa2b44f4696dad35cf9c64c9c6bf0a6ab1fb785
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2023-12-11 16:11:09 +0000

    feat(DTFS2-6576): Updated `BSS/EWCS` eligibility criteria (#2434)
    Co-authored-by: Abhi Markan <abhi.markan@ukexportfinance.gov.uk>

--- trade-finance-manager-api/src/v1/content-strings/index.js
+++ trade-finance-manager-api/src/v1/content-strings/index.js
@@ -135,17 +135,11 @@ const DEAL = {
         },
         12: {
           text: 'The cover period is less than either:',
-          textList: [
-            '5 years',
-            'another period approved by UKEF',
-          ],
+          textList: ['5 years', 'another period approved by UKEF'],
         },
         13: {
           text: 'The UKEF exposure for this exporter, across all short term schemes, is less than either:',
-          textList: [
-            '£2 million',
-            'another limit approved by UKEF',
-          ],
+          textList: ['£2 million', 'another limit approved by UKEF'],
         },
         14: {
           text: 'None of the following apply:',
@@ -161,15 +155,7 @@ const DEAL = {
         },
         16: {
           text: 'The supply contract does not involve one of the following controlled sectors:',
-          textList: [
-            'sharp arms defence',
-            'nuclear',
-            'radiological',
-            'biological',
-            'human cloning',
-            'pornography',
-            'tobacco or gambling',
-          ],
+          textList: ['sharp arms defence', 'nuclear', 'radiological', 'biological', 'human cloning', 'pornography', 'tobacco or gambling'],
         },
         17: {
           text: 'The bank has completed due diligence to its own satisfaction, without needing to escalate to a relevant person.',
@@ -177,10 +163,7 @@ const DEAL = {
         },
         18: {
           text: 'The fees or interest apply to the whole cover period and have  been set in line with:',
-          textList: [
-            'the bank’s normal pricing policies',
-            'minimum or overall pricing requirements set by UKEF(if any)',
-          ],
+          textList: ['the bank’s normal pricing policies', 'minimum or overall pricing requirements set by UKEF(if any)'],
         },
       },
       5: {
@@ -190,17 +173,11 @@ const DEAL = {
         },
         12: {
           text: 'The cover period is less than either:',
-          textList: [
-            '5 years',
-            'another period approved by UKEF',
-          ],
+          textList: ['5 years', 'another period approved by UKEF'],
         },
         13: {
           text: 'The UKEF exposure for this exporter, across all short term schemes, is less than either:',
-          textList: [
-            '£5 million',
-            'another limit approved by UKEF',
-          ],
+          textList: ['£5 million', 'another limit approved by UKEF'],
         },
         14: {
           text: 'None of the following apply:',
@@ -236,10 +213,7 @@ const DEAL = {
         },
         18: {
           text: 'The fees or interest apply to the whole cover period and have  been set in line with:',
-          textList: [
-            'the bank’s normal pricing policies',
-            'minimum or overall pricing requirements set by UKEF(if any)',
-          ],
+          textList: ['the bank’s normal pricing policies', 'minimum or overall pricing requirements set by UKEF(if any)'],
         },
       },
       6: {
@@ -249,17 +223,11 @@ const DEAL = {
         },
         12: {
           text: 'The cover period is less than either:',
-          textList: [
-            '5 years',
-            'another period approved by UKEF',
-          ],
+          textList: ['5 years', 'another period approved by UKEF'],
         },
         13: {
           text: 'The UKEF exposure for this exporter, across all short term schemes, is less than either:',
-          textList: [
-            '£5 million',
-            'another limit approved by UKEF',
-          ],
+          textList: ['£5 million', 'another limit approved by UKEF'],
         },
         14: {
           text: 'None of the following apply:',
@@ -295,10 +263,57 @@ const DEAL = {
         },
         18: {
           text: 'The fees, interest rate and/or risk margin apply to the whole Cover Period, and have been set in accordance with:',
+          textList: ['The bank’s normal pricing policies', 'Include, if any, pricing requirements set by UKEF'],
+        },
+      },
+      7: {
+        11: {
+          text: 'There are no agents involved in the supply contract.',
+          textList: [],
+        },
+        12: {
+          text: 'The period between the Cover Start Date and the Cover End Date does not exceed:',
+          textList: ['for a Bond, the Bond Maximum Cover Period; and', 'for a Loan, the Loan Maximum Cover Period'],
+        },
+        13: {
+          text: 'The Covered Bank Exposure under the Transaction (converted (as at the date this representation is made) for this purpose into the Base Currency) is not more than the lesser of:',
+          textList: ['the Available Facility; and', ' the Available Obligor Covered Exposure Limit'],
+        },
+        14: {
+          text: 'None of the following apply:',
           textList: [
-            'The bank’s normal pricing policies',
-            'Include, if any, pricing requirements set by UKEF',
+            'a bond has already been issued',
+            'if a bond is issued, this was no more than 3 months before the bank submitted their application to UKEF',
+            'a loan has already been advanced',
           ],
+        },
+        15: {
+          text: 'The cover start date the bank has requested is not more than 3 months after the date they submitted their application to UKEF.',
+          textList: [],
+        },
+        16: {
+          text: 'The supply contract does not involve one of the following controlled sectors:',
+          textList: [
+            'sharp arms defence',
+            'nuclear',
+            'radiological',
+            'biological',
+            'human cloning',
+            'pornography',
+            'coal',
+            'oil',
+            'gas',
+            'fossil fuel energy',
+            'tobacco or gambling',
+          ],
+        },
+        17: {
+          text: 'The bank has completed due diligence to its own satisfaction, without needing to escalate to a relevant person.',
+          textList: [],
+        },
+        18: {
+          text: "Any applicable fees, interest rate and/or Risk Margin Fee apply to the whole Cover Period of the Covered Transaction, and have been set in accordance with the Bank's normal pricing policies and include, if any, overall pricing requirements notified by UKEF.",
+          textList: [],
         },
       },
     },
@@ -309,17 +324,11 @@ const DEAL = {
         },
         13: {
           text: 'The period between the bank submitting their inclusion notice and their requested cover start dates is no more than either:',
-          textList: [
-            '3 months',
-            'a longer period agreed with UKEF',
-          ],
+          textList: ['3 months', 'a longer period agreed with UKEF'],
         },
         14: {
           text: 'The covered limit is not more than whichever is the lowest of either the:',
-          textList: [
-            'available master guarantee limit',
-            'available obligor\'s limit',
-          ],
+          textList: ['available master guarantee limit', "available obligor's limit"],
         },
         15: {
           text: 'The exporter declaration confirms that the exporter is not involved in any of the following controlled sectors:',
@@ -339,10 +348,7 @@ const DEAL = {
         },
         17: {
           text: 'The fees or interest apply to the whole cover period and have been set in line with either:',
-          textList: [
-            'the bank’s normal pricing policies',
-            'minimum or overall pricing requirements set by UKEF (if any)',
-          ],
+          textList: ['the bank’s normal pricing policies', 'minimum or overall pricing requirements set by UKEF (if any)'],
         },
         18: {
           text: 'The facility base currency is an approved currency.',
@@ -357,17 +363,11 @@ const DEAL = {
         },
         13: {
           text: 'The period between the bank submitting their inclusion notice and their requested cover start dates is no more than either:',
-          textList: [
-            '3 months',
-            'a longer period agreed with UKEF',
-          ],
+          textList: ['3 months', 'a longer period agreed with UKEF'],
         },
         14: {
           text: 'The covered limit is not more than whichever is the lowest of either the:',
-          textList: [
-            'available master guarantee limit',
-            'available obligor\'s limit',
-          ],
+          textList: ['available master guarantee limit', "available obligor's limit"],
         },
         15: {
           text: 'The exporter declaration confirms that the exporter is not involved in any of the following controlled sectors:',
@@ -390,10 +390,7 @@ const DEAL = {
         },
         18: {
           text: 'The fees or interest apply to the whole cover period and have been set in line with either:',
-          textList: [
-            'the bank’s normal pricing policies',
-            'minimum or overall pricing requirements set by UKEF (if any)',
-          ],
+          textList: ['the bank’s normal pricing policies', 'minimum or overall pricing requirements set by UKEF (if any)'],
         },
         19: {
           text: 'The facility base currency is an approved currency.',
@@ -408,17 +405,11 @@ const DEAL = {
         },
         13: {
           text: 'The period between the bank submitting their inclusion notice and their requested cover start dates is no more than either:',
-          textList: [
-            '3 months',
-            'a longer period agreed with UKEF',
-          ],
+          textList: ['3 months', 'a longer period agreed with UKEF'],
         },
         14: {
           text: 'The covered limit is not more than whichever is the lowest of either the:',
-          textList: [
-            'available master guarantee limit',
-            'available obligor\'s limit',
-          ],
+          textList: ['available master guarantee limit', "available obligor's limit"],
         },
         15: {
           text: 'The exporter declaration confirms that the exporter is not involved in any of the following controlled sectors:',
@@ -441,10 +432,7 @@ const DEAL = {
         },
         18: {
           text: 'The fees or interest apply to the whole cover period and have been set in line with either:',
-          textList: [
-            'the bank’s normal pricing policies',
-            'minimum or overall pricing requirements set by UKEF (if any)',
-          ],
+          textList: ['the bank’s normal pricing policies', 'minimum or overall pricing requirements set by UKEF (if any)'],
         },
         19: {
           text: 'The facility base currency is an approved currency.',
