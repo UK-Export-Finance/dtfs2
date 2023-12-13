e22733b7201d8420768d23d6a5577819972b4497
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2023-12-13 09:01:11 +0000

    chore: update user variable

--- e2e-tests/e2e-fixtures/portal-users.fixture.js
+++ e2e-tests/e2e-fixtures/portal-users.fixture.js
@@ -1,25 +1,25 @@
-const MOCK_USERS = require('../../utils/mock-data-loader/portal/users');
+const USERS = require('../../utils/mock-data-loader/portal/users');
 const { PORTAL_USER_ROLES } = require('./constants.fixture');
 
-const BANK1_MAKER1 = MOCK_USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.username === 'BANK1_MAKER1');
+const BANK1_MAKER1 = USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.username === 'BANK1_MAKER1');
 
-const BANK1_MAKER2 = MOCK_USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.username === 'BANK1_MAKER2');
+const BANK1_MAKER2 = USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.username === 'BANK1_MAKER2');
 
-const BANK2_MAKER2 = MOCK_USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.username === 'BANK2_MAKER2');
+const BANK2_MAKER2 = USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.username === 'BANK2_MAKER2');
 
-const BANK3_GEF_MAKER1 = MOCK_USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.username === 'BANK3_GEF_MAKER1');
+const BANK3_GEF_MAKER1 = USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.username === 'BANK3_GEF_MAKER1');
 
-const BANK1_CHECKER1 = MOCK_USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.CHECKER) && user.username === 'BANK1_CHECKER1');
+const BANK1_CHECKER1 = USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.CHECKER) && user.username === 'BANK1_CHECKER1');
 
-const BANK1_READ_ONLY1 = MOCK_USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.READ_ONLY) && user.username === 'BANK1_READ_ONLY1');
+const BANK1_READ_ONLY1 = USERS.find((user) => user.roles.includes(PORTAL_USER_ROLES.READ_ONLY) && user.username === 'BANK1_READ_ONLY1');
 
-const ADMIN = MOCK_USERS.find((user) => user.username === 'ADMIN');
+const ADMIN = USERS.find((user) => user.username === 'ADMIN');
 
-const READ_ONLY_ALL_BANKS = MOCK_USERS.find(
+const READ_ONLY_ALL_BANKS = USERS.find(
   (user) => user.roles.includes(PORTAL_USER_ROLES.READ_ONLY) && user.username === 'READ_ONLY' && user.bank.id === '*',
 );
 
-const BANK1_MAKER_CHECKER1 = MOCK_USERS.find(
+const BANK1_MAKER_CHECKER1 = USERS.find(
   (user) => user.roles.includes(PORTAL_USER_ROLES.MAKER) && user.roles.includes(PORTAL_USER_ROLES.CHECKER) && user.username === 'BANK1_MAKER_CHECKER1',
 );
 
