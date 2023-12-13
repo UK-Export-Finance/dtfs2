6140fee946df4ccb5b970e55f7ec630e6d2d7ee9
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-12-13 23:37:00 +0000

    fix(DTFS2-6713): estoore documentation

--- trade-finance-manager-api/src/v1/controllers/estore.controller.js
+++ trade-finance-manager-api/src/v1/controllers/estore.controller.js
@@ -1,18 +1,23 @@
+const api = require('../api');
 const mapCreateEstore = require('../mappings/map-create-estore');
 
-const api = require('../api');
-
+/**
+ * This function creates eStore directories and files on Sharepoint.
+ * Response is relayed using CRON jobs.
+ * Function will return deal object with a blank `siteName`.
+ * @param {Object} deal Deal object
+ * @returns {Object} `tfm-deal` object with empty `siteName`
+ */
 const createEstoreFolders = async (deal) => {
+  const estoreDeal = deal;
+  // 1. Creates eStore object by mapping through the deal
   const eStoreInput = mapCreateEstore(deal);
-
+  // 2. Creates eStore directories and upload files
   await api.createEstoreFolders(eStoreInput);
-  return {
-    ...deal,
-    tfm: {
-      ...deal.tfm,
-      estore: {},
-    },
-  };
+  // 3. Add an empty `siteName`
+  estoreDeal.tfm.estore.siteName = '';
+
+  return estoreDeal;
 };
 
 module.exports = {
