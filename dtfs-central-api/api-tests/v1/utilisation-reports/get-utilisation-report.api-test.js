e0c36eb62980364c32217b4adb71c737c585fbe0
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2023-12-14 18:11:44 +0000

    chore(merge): fix central API tests

--- dtfs-central-api/api-tests/v1/utilisation-reports/get-utilisation-report.api-test.js
+++ dtfs-central-api/api-tests/v1/utilisation-reports/get-utilisation-report.api-test.js
@@ -19,8 +19,7 @@ describe('/v1/utilisation-reports/:_id', () => {
 
       // Assert
       expect(status).toEqual(400);
-      console.log(body);
-      expect(body?.errors?.msg).toEqual("Invalid MongoDB '_id' path param provided");
+      expect(body?.errors[0]?.msg).toEqual("Invalid MongoDB '_id' path param provided");
     });
 
     it('gets a utilisation report', async () => {
@@ -35,7 +34,7 @@ describe('/v1/utilisation-reports/:_id', () => {
       expect(status).toEqual(200);
 
       const expected = {
-        _id: insertedId.toString(),
+        _id: insertedId.id,
         ...MOCK_UTILISATION_REPORT,
       };
 
