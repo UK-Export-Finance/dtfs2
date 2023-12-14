e20b89281cb969800d1a2e6de299e0677fa61cb1
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2023-12-14 18:51:03 +0000

    merge(chore): eslint updates

--- dtfs-central-api/.eslintrc.js
+++ dtfs-central-api/.eslintrc.js
@@ -3,8 +3,7 @@ const baseParserOptions = {
 };
 
 module.exports = {
-  plugins: ['@typescript-eslint'],
-  extends: ['airbnb-base', 'prettier'],
+  extends: 'airbnb-base',
   env: {
     jest: true,
     browser: true,
@@ -25,7 +24,6 @@ module.exports = {
     ],
     'no-console': ['error', { allow: ['info', 'error'] }],
     'no-underscore-dangle': ['error', { allow: ['_id', '_csrf'] }],
-    'import/extensions': 'off',
     'import/no-named-as-default': 'off',
     'implicit-arrow-linebreak': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.js', '**/*.spec.js', '**/webpack.*.js', '**/api-tests/**', '**/__mocks__/**'] }],
@@ -33,14 +31,12 @@ module.exports = {
     'no-loop-func': 'off',
     'no-await-in-loop': 'off',
     'no-restricted-syntax': 'off',
-    'no-return-await': 'off',
     'no-use-before-define': [
       'error',
       {
         functions: false
       }
-    ],
-    'import/prefer-default-export': 'off',
+    ]
   },
   ignorePatterns: ['**/node_modules/**'],
   parserOptions: baseParserOptions,
