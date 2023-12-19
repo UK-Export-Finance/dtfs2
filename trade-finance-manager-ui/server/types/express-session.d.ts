0112d6008eb6206256c8629176c9f4a423e1133d
Michael Todd <michael.todd@softwire.com>
2023-12-19 16:53:39 +0000

    feat(FN-1483): add doc reference

--- trade-finance-manager-ui/server/types/express-session.d.ts
+++ trade-finance-manager-ui/server/types/express-session.d.ts
@@ -1,5 +1,6 @@
 import { TfmSessionUser } from './tfm-session-user';
 
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/express-session/index.d.ts#L199-L211
 declare module 'express-session' {
   interface SessionData {
     user: TfmSessionUser;
