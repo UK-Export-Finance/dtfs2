df3d0e4f82eb0c3480fdfb23253716841476ff81
Joshua Binns <Joshua.Binns@softwire.com>
2023-12-28 22:22:44 +0000

    feat(FN-1536): change name of error class

--- trade-finance-manager-ui/server/errors/invalid-update-report-status-request.error.test.ts
+++ trade-finance-manager-ui/server/errors/invalid-update-report-status-request.error.test.ts
@@ -1,17 +1,17 @@
-import { InvalidReportStatusRequestError } from './invalid-report-status-request.error';
+import { InvalidUpdateReportStatusRequestError } from './invalid-update-report-status-request.error';
 
-describe('InvalidReportStatusRequestError', () => {
+describe('InvalidUpdateReportStatusRequestError', () => {
   const message = 'an error message';
 
   it('exposes the message it was created with', () => {
     // Act
-    const exception = new InvalidReportStatusRequestError(message);
+    const exception = new InvalidUpdateReportStatusRequestError(message);
 
     // Assert
     expect(exception.message).toBe(message);
   });
 
-  // Test will pass if we ever manage to update to es6
+  // Test will pass if we ever update to es6
   // it('exposes the name of the exception', () => {
   //   // Act
   //   const exception = new InvalidReportStatusRequestError(message);
--- trade-finance-manager-ui/server/errors/invalid-update-report-status-request.error.ts
+++ trade-finance-manager-ui/server/errors/invalid-update-report-status-request.error.ts
@@ -1,4 +1,4 @@
-export class InvalidReportStatusRequestError extends Error {
+export class InvalidUpdateReportStatusRequestError extends Error {
   constructor(message: string) {
     super(message);
     this.name = this.constructor.name;
