543edee39232badec376898ada4c8520bd72486c
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-01-10 17:52:41 +0000

    fix(DTFS2-6910): fix e2e test

--- portal/server/utils/destroy-session-and-redirect-to-start.js
+++ portal/server/utils/destroy-session-and-redirect-to-start.js
@@ -4,13 +4,9 @@ function destroySessionAndRedirectToStart(req, res) {
   const startPageRedirect = Boolean(process.env.START_PAGE_REDIRECT);
   const redirectAddress = startPageRedirect ? PORTAL_URL : '/login';
 
-  if (req.session === undefined) {
+  req.session.destroy(() => {
     res.redirect(redirectAddress);
-  } else {
-    req.session.destroy(() => {
-      res.redirect(redirectAddress);
-    });
-  }
+  });
 }
 
 module.exports = destroySessionAndRedirectToStart;
