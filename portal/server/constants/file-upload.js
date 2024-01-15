f014047672a1336d2556bd58678f0150243e2701
Michael Todd <michael.todd@softwire.com>
2024-01-15 14:23:00 +0000

    feat(FN-2052): with const

--- portal/server/constants/file-upload.js
+++ portal/server/constants/file-upload.js
@@ -10,6 +10,7 @@ const FILE_UPLOAD = {
   ALLOWED_FORMATS: ['bmp', 'doc', 'docx', 'gif', 'jpeg', 'jpg', 'pdf', 'png', 'ppt', 'pptx', 'tif', 'txt', 'xls', 'xlsx'],
   ALLOWED_FORMATS_UTILISATION_REPORT: ['xlsx', 'csv'],
   ALLOWED_MIMETYPES_UTILISATION_REPORT: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel', 'text/csv'],
+  FILENAME_SUBMITTED_INDICATOR: 'SUBMITTED',
   MAX_FILE_SIZE: 12 * 1024 * 1024, // 12mb
   MAX_CELL_CHARACTER_COUNT: 15,
 };
