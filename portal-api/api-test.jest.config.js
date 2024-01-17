72d22e335a637a5ba8a305daac3521d3485d3daf
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-01-17 14:22:21 +0000

    fix(DTFS2-6963): update handling of logging in

--- portal-api/api-test.jest.config.js
+++ portal-api/api-test.jest.config.js
@@ -3,6 +3,6 @@ const commonSettings = require('./api-test-common.jest.config');
 module.exports = {
   collectCoverageFrom: ['src/**/*.{js,}'],
   coverageDirectory: 'generated_reports/coverage/api-test',
-  testMatch: ['**/*sign-in-link*.api-test.js'],
+  testMatch: ['**/*.api-test.js'],
   ...commonSettings,
 };
