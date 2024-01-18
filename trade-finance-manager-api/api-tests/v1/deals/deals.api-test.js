c4884af44a3d19416dd8349a90d3a73b3254445b
oscar-richardson-softwire <oscar.richardson@softwire.com>
2024-01-18 12:43:30 +0000

    feat(DTFS2-6931): improve deals API test

--- trade-finance-manager-api/api-tests/v1/deals/deals.api-test.js
+++ trade-finance-manager-api/api-tests/v1/deals/deals.api-test.js
@@ -5,12 +5,14 @@ const { mockFindOneDeal, mockQueryDeals } = require('../../../src/v1/__mocks__/c
 const api = require('../../../src/v1/api');
 const ALL_MOCK_DEALS = require('../../../src/v1/__mocks__/mock-deals');
 const { dealsLightReducer } = require('../../../src/v1/rest-mappings/deals-light');
+const { when } = require('jest-when');
 
 jest.mock('../../../src/v1/rest-mappings/deals-light');
 
 const mappedDeal = mapDeal(MOCK_DEAL);
 
-const mockTfmDeals = ALL_MOCK_DEALS.map((deal) => ({ _id: deal._id, dealSnapshot: deal, tfm: { supplyContractValueInGBP: 1 } }));
+const mockTfmDealsReduced = ALL_MOCK_DEALS.map((deal) => ({ _id: deal._id, dealSnapshot: deal, tfm: { supplyContractValueInGBP: 1 }, reduced: true }));
+const mockTfmDeals = mockTfmDealsReduced.map((deal) => { const { reduced: _removed, ...dealToReturn } = deal; return dealToReturn });
 const mockPagination = {
   totalItems: 3,
   currentPage: 0,
@@ -23,7 +25,7 @@ describe('deal controller', () => {
     mockFindOneDeal();
 
     dealsLightReducer.mockReset();
-    dealsLightReducer.mockImplementation((deals) => deals);
+    dealsLightReducer.mockImplementation((deals) => deals.map((deal) => ({ ...deal, reduced: true })));
   });
 
   describe('find one TFM deal', () => {
@@ -55,21 +57,56 @@ describe('deal controller', () => {
   describe('get all TFM deals', () => {
     it('returns all deals', async () => {
       api.queryDeals.mockReset();
-
       mockQueryDeals(mockTfmDeals, mockPagination);
 
       const queryParams = {};
       const req = { query: queryParams };
+
+      const mockSend = jest.fn();
       const res = {
-        send: jest.fn(),
-        status: jest.fn(() => res),
+        send: mockSend,
+        status: jest.fn().mockReturnThis(),
       };
+      when(mockSend).calledWith({ deals: mockTfmDealsReduced, pagination: mockPagination }).mockResolvedValueOnce('all deals');
 
-      await getDeals(req, res);
+      const response = await getDeals(req, res);
 
+      expect(api.queryDeals).toHaveBeenCalledWith({ queryParams });
+      expect(api.queryDeals).toHaveBeenCalledTimes(1);
       expect(res.status).toHaveBeenCalledWith(200);
-      expect(dealsLightReducer).toHaveBeenCalledTimes(1);
-      expect(res.send).toHaveBeenCalledWith({ deals: mockTfmDeals, pagination: mockPagination });
+      expect(res.status).toHaveBeenCalledTimes(1);
+      expect(response).toBe('all deals');
+    });
+
+    it('returns a 500 response with the error message if getting the deals throws an error', async () => {
+      const error = new Error('an error message');
+
+      api.queryDeals.mockReset();
+      when(api.queryDeals).calledWith(expect.anything()).mockImplementation(() => {
+        throw error;
+      });
+
+      const queryParams = {};
+      const req = { query: queryParams };
+
+      const mockSend = jest.fn();
+      const res = {
+        send: mockSend,
+        status: jest.fn().mockReturnThis(),
+      };
+      when(mockSend).calledWith('an error message').mockResolvedValueOnce('the error message');
+
+      const mockConsoleError = jest.spyOn(console, 'error').mockImplementation();
+
+      const response = await getDeals(req, res);
+
+      expect(mockConsoleError).toHaveBeenCalledWith(`Error fetching deals: ${error}`);
+      expect(mockConsoleError).toHaveBeenCalledTimes(1);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.status).toHaveBeenCalledTimes(1);
+      expect(response).toBe('the error message');
+
+      mockConsoleError.mockRestore();
     });
   });
 });
