5138d1f3366ae588d670e30f9a8857794a8af2b8
oscar-richardson-softwire <oscar.richardson@softwire.com>
2024-01-19 10:21:56 +0000

    refactor(DTFS2-6963): lint

--- portal-api/src/v1/users/sign-in-link.service.js
+++ portal-api/src/v1/users/sign-in-link.service.js
@@ -61,8 +61,7 @@ class SignInLinkService {
         target: signInToken,
         hash: databaseSignInToken.hash,
         salt: databaseSignInToken.salt,
-      }),
-    );
+      }),);
 
     if (matchingSignInTokenIndex === -1) {
       return SIGN_IN_LINK.STATUS.NOT_FOUND;
@@ -71,8 +70,8 @@ class SignInLinkService {
     const matchingSignInToken = databaseSignInTokens[matchingSignInTokenIndex];
 
     if (
-      this.#isSignInTokenIsInDate(matchingSignInToken) &&
-      this.#isSignInTokenIsLastIssued({ signInTokenIndex: matchingSignInTokenIndex, databaseSignInTokens })
+      this.#isSignInTokenIsInDate(matchingSignInToken)
+      && this.#isSignInTokenIsLastIssued({ signInTokenIndex: matchingSignInTokenIndex, databaseSignInTokens })
     ) {
       return SIGN_IN_LINK.STATUS.VALID;
     }
