2991bdb99993dd754498841bde6d8f2143044603
JoshBinns2000 <108285982+JoshBinns2000@users.noreply.github.com>
2024-01-22 16:31:01 +0000

    feat(FN-2077): change how `PDC` users behave on login (#2605)

---
+++ trade-finance-manager-ui/server/helpers/user.ts
@@ -0,0 +1,21 @@
+import { TfmSessionUser } from '../types/tfm-session-user';
+import { TeamId } from '../types/team-id';
+
+export const userFullName = (user: TfmSessionUser) => {
+  const { firstName, lastName } = user;
+  return `${firstName} ${lastName}`;
+};
+
+/**
+ * Function to return whether or not a user is in
+ * any of the teams provided
+ */
+export const userIsInTeam = (user: TfmSessionUser, teamIdList: TeamId[]) => user.teams.some((teamId) => teamIdList.includes(teamId));
+
+/**
+ * Function to return whether or not a user is in
+ * only the set of teams provided
+ */
+export const userIsOnlyInTeams = (user: TfmSessionUser, teamIdList: TeamId[]) => user.teams.length === teamIdList.length && user.teams.every((userTeam) => teamIdList.includes(userTeam));
+
+export const isAssignedToUser = (assignedToUserId: string, userId: string) => assignedToUserId === userId;
