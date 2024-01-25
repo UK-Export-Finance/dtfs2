05549a941c2b3d8bbc485a6de2f9090ba591b800
Mark Roberts <Mark.Roberts@softwire.com>
2024-01-25 11:25:41 +0000

    chore(DTFS2-4533): error handle invalid timestamps

--- portal/server/nunjucks-configuration/filter-localiseTimestamp.ts
+++ portal/server/nunjucks-configuration/filter-localiseTimestamp.ts
@@ -1,14 +1,20 @@
 import { formatInTimeZone } from 'date-fns-tz';
 
 export const filterLocaliseTimestamp = (
-  utcTimestamp: number,
+  utcTimestamp: number | string,
   format: string,
   targetTimezone: string,
 ) => {
   if (!utcTimestamp) {
     return '';
   }
-  const date = new Date(utcTimestamp);
+  const timestamp = parseInt(String(utcTimestamp), 10);
+
+  if (Number.isNaN(timestamp)) {
+    throw new Error(`Cannot convert timestamp ${utcTimestamp} to date`);
+  }
+
+  const date = new Date(timestamp);
 
   return formatInTimeZone(date, targetTimezone, format);
 };
