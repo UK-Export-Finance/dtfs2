b22561bf592aba3d5c76fdfedaf7e9bed9efd6e1
Mark Roberts <Mark.Roberts@softwire.com>
2024-02-01 15:51:01 +0000

    chore(DTFS2-6994): allow unit tests in portal-api to run ts

--- portal-api/unit.jest.config.js
+++ portal-api/unit.jest.config.js
@@ -2,6 +2,7 @@ const commonConfig = require('./jest.common.config');
 
 module.exports = {
   ...commonConfig,
+  preset: 'ts-jest/presets/js-with-babel',
   collectCoverageFrom: ['src/**/*.{js,ts}'],
   coverageDirectory: 'generated_reports/coverage/unit',
   testMatch: ['**/*.test.{js,ts}'],
