53a3e2dfbf69976edee7b4acace69ec3fbf0f2dd
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2024-02-02 09:58:37 +0000

    feat(DTFS2-6965): update docker files and imports

--- dtfs-central-api/src/createApp.js
+++ dtfs-central-api/src/createApp.js
@@ -1,5 +1,4 @@
-import { setupChangeStream } from './services/changeStream/setupChangeStream';
-
+const { setupChangeStream } = require('./services/changeStream/setupChangeStream');
 const { generateApp } = require('./generateApp');
 
 if (process.env.CHANGE_STREAM_ENABLED === 'true') {
