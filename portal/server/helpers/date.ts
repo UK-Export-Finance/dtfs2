e36acd2f22a6be4d61455618077f52bd6ac0a0d1
Mark Roberts <Mark.Roberts@softwire.com>
2024-02-09 12:13:22 +0000

    chore(DTFS2-4533): add docs for getNow method

--- portal/server/helpers/date.ts
+++ portal/server/helpers/date.ts
@@ -1,1 +1,10 @@
+/**
+ * Returns the current datetime as a 13 digit Unix timestamp: the time in 
+ * milliseconds that has elapsed since 1st January 1970 (UTC).
+ * 
+ * e.g. A Unix timestamp of 1702900314 is equivalent to an ISO 8601 date time
+ * stamp of '2023-12-18T11:51:54Z'
+ * 
+ * @returns {number}
+ */
 export const getNowAsEpoch = () => (Date.now());
