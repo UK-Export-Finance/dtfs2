b07424a78cc56a95e9dfbd49c1e790b0dbd80765
Mark Roberts <Mark.Roberts@softwire.com>
2024-02-15 14:46:34 +0000

    chore(DTFS2-6999): remove moment from tfm-ui filters

--- trade-finance-manager-ui/server/nunjucks-configuration/filter-formatDateString.js
+++
@@ -1,6 +0,0 @@
-const moment = require('moment');
-
-const formatDateString = (dateStr, fromFormat, toFormat = 'D MMM YYYY') =>
-  moment(dateStr, fromFormat).format(toFormat);
-
-module.exports = formatDateString;
--- trade-finance-manager-ui/server/nunjucks-configuration/index.js
+++ trade-finance-manager-ui/server/nunjucks-configuration/index.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const nunjucks = require('nunjucks');
 let mojFilters = require('@ministryofjustice/frontend/moj/filters/all')();
-const filterLocaliseTimestamp = require('./filter-localiseTimestamp');
-const filterFormatDateString = require('./filter-formatDateString');
+const { filterLocaliseTimestamp } = require('./filter-localiseTimestamp');
+const { filterFormatDateString } = require('./filter-formatDateString');
 const dashIfEmpty = require('./filter-dashIfEmpty');
 const displayName = require('./filter-displayName');
 const formatAsCurrency = require('./filter-formatAsCurrency');
