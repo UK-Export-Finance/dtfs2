0a150b3d6fdb9087edbd230f958d96c8431a96d3
Michael Todd <michael.todd@softwire.com>
2024-02-19 14:02:58 +0000

    feat(FN-2253): review updates

--- libs/common/src/types/mongodb-models/index.ts
+++
@@ -1,4 +0,0 @@
-export { AzureFileInfo } from './azure-file-info';
-export { Bank } from './banks';
-export { UtilisationData } from './utilisation-data';
-export { UtilisationReport } from './utilisation-reports';
