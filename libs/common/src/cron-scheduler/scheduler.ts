e46e5affbc22777c18eeb9602ca5f86cdf64343a
Michael Todd <michael.todd@softwire.com>
2024-02-21 12:55:52 +0000

    feat(FN-2146): update logs

--- libs/common/src/cron-scheduler/scheduler.ts
+++ libs/common/src/cron-scheduler/scheduler.ts
@@ -7,11 +7,11 @@ export const initialiseCronScheduler = (jobs: SchedulerJob[]) => {
     const { cronExpression, description, task } = job;
 
     if (!cron.validate(cronExpression)) {
-      console.error("Failed to add job '%s' due to invalid cron expression: '%s'", description, cronExpression);
+      console.error("Failed to add scheduled job '%s' due to invalid cron expression: '%s'", description, cronExpression);
       return;
     }
 
-    console.info("Adding scheduled job '%s'", description);
+    console.info("Adding scheduled job '%s' on schedule '%s'", description, cronExpression);
     cron
       .schedule(cronExpression, asyncTaskToSyncTask(taskWithErrorLogging(description, task)))
       .on('error', (error) => console.error("An error occurred scheduling job '%s': %O", description, error));
