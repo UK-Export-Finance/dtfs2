1981e27d21648c1dedd400166fa60a77155b5022
Mark Roberts <Mark.Roberts@softwire.com>
2024-02-21 17:15:44 +0000

    chore(DTFS2-6994): use js-with-ts jest preset

--- portal-api/unit.jest.config.js
+++ portal-api/unit.jest.config.js
@@ -2,7 +2,6 @@ const commonConfig = require('./jest.common.config');
 
 module.exports = {
   ...commonConfig,
-  preset: 'ts-jest/presets/js-with-babel',
   collectCoverageFrom: ['src/**/*.{js,ts}'],
   coverageDirectory: 'generated_reports/coverage/unit',
   testMatch: ['**/*.test.{js,ts}'],
