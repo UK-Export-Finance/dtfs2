48ad7afadbe666c162b9937e838cc1f7ac7967b6
Audrius Vaitonis <audrius.vaitonis@ukexportfinance.gov.uk>
2024-02-22 11:41:23 +0000

    feat(DTFS2-6892): removing unused cypress helper 'getJwtTokenForUser'

--- e2e-tests/tfm/cypress/support/trade-finance-manager-api/getJwtTokenForUser.js
+++
@@ -1,6 +0,0 @@
-const { getJwtTokenForUser } = require('./api');
-
-module.exports = (dealId, dealType, opts) => {
-  const { user } = opts;
-  return getJwtTokenForUser(dealId, dealType, null, user);
-};
