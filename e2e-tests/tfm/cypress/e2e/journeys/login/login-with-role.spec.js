749259d3b75d2dff848ba4796dd760e947e106d3
ttbarnes <tony@tonybarnes.me>
2024-02-22 14:23:28 +0000

    chore(DTFS2-6892): improve login-with-role e2e test

--- e2e-tests/tfm/cypress/e2e/journeys/login/login-with-role.spec.js
+++ e2e-tests/tfm/cypress/e2e/journeys/login/login-with-role.spec.js
@@ -2,23 +2,26 @@ import USERS from '../../../fixtures/users';
 import TEAMS from '../../../fixtures/teams';
 import relativeURL from '../../relativeURL';
 
-context('Login to tfm with specific roles', () => {
-  const findOneUserByTeamId = (teamId) => USERS.find((user) => user?.teams?.includes(teamId));
+const findOneUserByTeamId = (teamId) => USERS.find((user) => user?.teams?.includes(teamId));
 
-  const nonPdcTeams = Object.values(TEAMS).filter((team) => !team.includes('PDC'));
-  nonPdcTeams.forEach((team) => {
-    it(`should redirect to /deals after a login for users in '${team}' team`, () => {
-      const userInTeam = findOneUserByTeamId(team);
+const NON_PDC_TEAMS = Object.values(TEAMS).filter((team) => !team.includes('PDC'));
+const PDC_TEAMS = Object.values(TEAMS).filter((team) => team.includes('PDC'));
+
+context('Login to tfm with specific roles', () => {
+  NON_PDC_TEAMS.forEach((teamName) => {
+    it(`should redirect to /deals after a login for users in '${teamName}' team`, () => {
+      const userInTeam = findOneUserByTeamId(teamName);
+
       cy.login(userInTeam);
 
       cy.url().should('eq', relativeURL('/deals'));
     });
   });
 
-  const pdcTeams = Object.values(TEAMS).filter((team) => team.includes('PDC'));
-  pdcTeams.forEach((team) => {
-    it(`should redirect to /utilisation-reports after a login for users in '${team}' team`, () => {
-      const userInTeam = findOneUserByTeamId(team);
+  PDC_TEAMS.forEach((teamName) => {
+    it(`should redirect to /utilisation-reports after a login for users in '${teamName}' team`, () => {
+      const userInTeam = findOneUserByTeamId(teamName);
+
       cy.login(userInTeam);
 
       cy.url().should('eq', relativeURL('/utilisation-reports'));
