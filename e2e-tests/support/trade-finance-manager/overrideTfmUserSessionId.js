a8574073e7cc81a266c92f6c07454dd99d69f7a1
ttbarnes <tony@tonybarnes.me>
2024-02-22 14:10:32 +0000

    feat(DTFS2-6892): documentation improvements

--- e2e-tests/support/trade-finance-manager/overrideTfmUserSessionId.js
+++ e2e-tests/support/trade-finance-manager/overrideTfmUserSessionId.js
@@ -1,3 +1,11 @@
-module.exports = (username, sessionIdentifier) => {
+/**
+ * overrideTfmUserSessionId
+ * Override a TFM user's session ID/identifier
+ * @param {String} username
+ * @param {String} sessionIdentifier
+ */
+const overrideTfmUserSessionId = (username, sessionIdentifier) => {
   cy.task('overrideTfmUserSessionId', { username, sessionIdentifier });
 };
+
+module.exports = overrideTfmUserSessionId;
