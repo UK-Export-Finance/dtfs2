bec28909249b978bcad7b819555d53a79eee5e99
Mark Roberts <Mark.Roberts@softwire.com>
2024-02-22 17:06:31 +0000

    chore(DTFS2-6999): improve test coverage of formatDateString filter

--- trade-finance-manager-ui/server/nunjucks-configuration/filter-formatDateString.test.ts
+++ trade-finance-manager-ui/server/nunjucks-configuration/filter-formatDateString.test.ts
@@ -1,17 +1,52 @@
-import moment from 'moment';
 import { filterFormatDateString } from './filter-formatDateString';
 
 describe('nunjuck filters - formatDateString', () => {
-  it('should return formatted date from existing formatted date', () => {
-    const fromFormat = 'dd-MM-yyyy';
+  const testCases = [
+    {
+      description: 'should return correct date when formatting from `dd-MM-yyyy` to `dd/MM/yyyy`',
+      mockDate: '14-01-2023',
+      fromFormat: 'dd-MM-yyyy',
+      toFormat: 'dd/MM/yyyy',
+      expected: '14/01/2023',
+    },
+    {
+      description: 'should return correct date when formatting from `d M yy` to `dd/MM/yyyy`',
+      mockDate: '4 2 24',
+      fromFormat: 'd M yy',
+      toFormat: 'dd/MM/yyyy',
+      expected: '04/02/2024',
+    },
+    {
+      description: 'should return correct date when formatting from `do MMMM yyyy HH:mm` to `dd/MM/yyyy h:mmaaa`',
+      mockDate: '5th January 2021 15:01',
+      fromFormat: 'do MMMM yyyy HH:mm',
+      toFormat: 'dd/MM/yyyy h:mmaaa',
+      expected: '05/01/2021 3:01pm',
+    },
+    {
+      description: 'formats to `d MMM yyyy` when toFormat is not given',
+      mockDate: '03-01-2023',
+      fromFormat: 'dd-MM-yyyy',
+      expected: '3 Jan 2023',
+    },
+    {
+      description: 'should return `Invalid date` when date is invalid',
+      mockDate: '##/##/####',
+      fromFormat: 'dd-MM-yyyy',
+      toFormat: 'dd/MM/yyyy',
+      expected: 'Invalid date',
+    },
+    {
+      description: 'returns `Invalid date` when date string is invalid and toFormat is not given',
+      mockDate: '##/##/####',
+      fromFormat: 'dd/MM/yyyy',
+      expected: 'Invalid date',
+    }
+  ];
 
-    const mockDate = moment().format(fromFormat);
-
-    const toFormat = 'dd/MM/yyyy';
-
+  it.each(testCases)('$description', ({ mockDate, fromFormat, toFormat, expected}) => {
     const result = filterFormatDateString(mockDate, fromFormat, toFormat);
 
-    const expected = moment(mockDate, fromFormat).format(toFormat);
     expect(result).toEqual(expected);
   });
 });
