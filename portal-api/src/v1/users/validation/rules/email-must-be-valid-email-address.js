f54e1ce857caba6a941b1f42de051a69fe716ad3
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-02-26 09:33:05 +0000

    fix(DTFS2-6621): update to review comments

--- portal-api/src/v1/users/validation/rules/email-must-be-valid-email-address.js
+++ portal-api/src/v1/users/validation/rules/email-must-be-valid-email-address.js
@@ -2,7 +2,7 @@ const { isValidEmail } = require('../../../../utils/string');
 
 /**
  * Ensures that if the change has an email property, it is a valid email address
- * @param {Object} user the existing user (optional)
+ * @param {Object} user the existing user
  * @param {Object} change the changes to make
  * @returns {Array} either an empty array or an array containing an error object if the email is not a valid email address
  */
