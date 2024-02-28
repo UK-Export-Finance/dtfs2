393e71b0dca0eca335d0939c48fa4cd28d3f4ea1
Audrius Vaitonis <audrius.vaitonis@ukexportfinance.gov.uk>
2024-02-28 16:32:37 +0000

    feat(DTFS2-6892): typo in tfm-ui component test description

--- trade-finance-manager-ui/component-tests/sso/sso-accept-external-sso-post.component-test.js
+++ trade-finance-manager-ui/component-tests/sso/sso-accept-external-sso-post.component-test.js
@@ -1,6 +1,7 @@
 const pageRenderer = require('../pageRenderer');
 
 const page = '../templates/sso/accept-external-sso-post.njk';
+
 const render = pageRenderer(page);
 
 describe(page, () => {
@@ -10,23 +11,23 @@ describe(page, () => {
     wrapper = render();
   });
 
-  it('hiddent input "code" should exist', () => {
+  it('hidden input "code" should exist', () => {
     wrapper.expectElement('[name="code"]').toExist();
   });
 
-  it('hiddent input "formId" should exist', () => {
+  it('hidden input "formId" should exist', () => {
     wrapper.expectElement('[name="formId"]').toExist();
   });
 
-  it('hiddent input "clientInfo" should exist', () => {
+  it('hidden input "clientInfo" should exist', () => {
     wrapper.expectElement('[name="clientInfo"]').toExist();
   });
 
-  it('hiddent input "state" should exist', () => {
+  it('hidden input "state" should exist', () => {
     wrapper.expectElement('[name="state"]').toExist();
   });
 
-  it('hiddent input "sessionState" should exist', () => {
+  it('hidden input "sessionState" should exist', () => {
     wrapper.expectElement('[name="sessionState"]').toExist();
   });
 
