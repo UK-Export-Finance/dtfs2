3a1008771e4315a39db368d4c246321c0b269300
Ruby Lavender <ruby.lavender@softwire.com>
2024-02-29 15:00:51 +0000

    feat(FN-1861): update tfm and dtfs central api tests for downloading report

--- dtfs-central-api/api-tests/v1/utilisation-reports/get-utilisation-report.api-test.js
+++ dtfs-central-api/api-tests/v1/utilisation-reports/get-utilisation-report.api-test.js
@@ -1,47 +1,26 @@
-const wipeDB = require('../../wipeDB');
 const app = require('../../../src/createApp');
 const api = require('../../api')(app);
-const db = require('../../../src/drivers/db-client').default;
-const { DB_COLLECTIONS } = require('../../../src/constants/db-collections');
-const { MOCK_UTILISATION_REPORT } = require('../../mocks/utilisation-reports/utilisation-reports');
+const { MOCK_BANKS } = require('../../mocks/banks');
 
-const getUrl = (_id) => `/v1/utilisation-reports/${_id}`;
+const getUrl = (id) => `/v1/utilisation-reports/${id}`;
 
-describe('/v1/utilisation-reports/:_id', () => {
-  beforeAll(async () => {
-    await wipeDB.wipe([DB_COLLECTIONS.UTILISATION_REPORTS]);
-  });
-
+describe('/v1/utilisation-reports/:id', () => {
   describe('GET /v1/utilisation-reports/:_id', () => {
-    it('returns 400 when an invalid report MongoDB ID is provided', async () => {
+    it('returns 400 when an invalid report ID is provided', async () => {
       // Act
-      const { status, body } = await api.get(getUrl('invalid-mongo-id'));
+      const { status, body } = await api.get(getUrl('invalid-id'));
 
       // Assert
       expect(status).toEqual(400);
-      expect(body.errors.at(0).msg).toEqual("Invalid MongoDB '_id' path param provided");
+      expect(body.errors.at(0).msg).toEqual("Invalid 'id' path param provided");
     });
 
     it('gets a utilisation report', async () => {
-      // Arrange
-      const collection = await db.getCollection(DB_COLLECTIONS.UTILISATION_REPORTS);
-      const { insertedId } = await collection.insertOne(MOCK_UTILISATION_REPORT);
-      const _id = insertedId.toString();
-      const dateUploadedAsISOString = MOCK_UTILISATION_REPORT.dateUploaded.toISOString();
-
       // Act
-      const { body, status } = await api.get(getUrl(_id));
+      const { status } = await api.get(getUrl(MOCK_BANKS.BARCLAYS.id));
 
       // Assert
       expect(status).toEqual(200);
-
-      const expected = {
-        ...MOCK_UTILISATION_REPORT,
-        _id,
-        dateUploaded: dateUploadedAsISOString,
-      };
-
-      expect(body).toEqual(expected);
     });
   });
 });
