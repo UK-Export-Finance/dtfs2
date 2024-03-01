6f7ec46cc398bf0d1e9e58d1790753d5f40235bc
Mark Roberts <Mark.Roberts@softwire.com>
2024-03-01 09:36:59 +0000

    chore(DTFS2-6994): uninstall moment and delete timestamp helper

--- portal-api/src/v1/facility-dates/timestamp.js
+++
@@ -1,7 +0,0 @@
-const moment = require('moment');
-
-exports.formattedTimestamp = (timestamp) => {
-  const utc = moment(parseInt(timestamp, 10));
-  const dt = moment(utc);
-  return moment(dt).isValid() ? dt.format() : '';
-};
