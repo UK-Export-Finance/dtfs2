99f3bf4ee58e666b569bf0ed24718a99a5c1b148
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2024-03-03 00:08:29 +0000

    fix(DTFS2-6188): reverted back govuk to 4.6.0

---
+++ trade-finance-manager-ui/public/js/jsEnabled.js
@@ -0,0 +1,2 @@
+var DTFS_TFM;!function(){document.body.className=document.body.className?"".concat(document.body.className," js-enabled"):"js-enabled";var e=document.getElementById("tasks-filters");e&&(e.className="js-enabled"),(DTFS_TFM=void 0===DTFS_TFM?{}:DTFS_TFM).jsEnabled={}}();
+//# sourceMappingURL=jsEnabled.js.map
