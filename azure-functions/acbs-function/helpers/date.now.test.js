dd825174a7fb0b0f13acf1ff45d04b569f5bb15a
Mark Roberts <Mark.Roberts@softwire.com>
2024-03-04 10:57:50 +0000

    chore(DTFS2-6995): add test coverage for get now as iso string

--- azure-functions/acbs-function/helpers/date.now.test.js
+++ azure-functions/acbs-function/helpers/date.now.test.js
@@ -1,11 +1,18 @@
-const { format } = require('date-fns');
 const { now } = require('./date');
 
 describe('now', () => {
+  beforeAll(() => {
+    jest.useFakeTimers()
+      .setSystemTime(1709337600000); // Sat Mar 02 2024 00:00:00 GMT+0000
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it('returns the current date in the format `yyyy-MM-dd', () => {
     const result = now();
 
-    const expected = format(new Date(), 'yyyy-MM-dd');
-    expect(result).toBe(expected);
+    expect(result).toBe('2024-03-02');
   });
 });
