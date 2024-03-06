943aa65b9e93f110f8cb630aa6045bfaf7e8d31e
Ruby Lavender <ruby.lavender@softwire.com>
2024-03-06 11:51:53 +0000

    feat(FN-1861): add and format doc strings

--- portal/server/routes/middleware/validateSqlId/index.ts
+++ portal/server/routes/middleware/validateSqlId/index.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { isValidSqlId } from '../../../validation/validate-ids';
 
+/** Middleware to validate that the 'id' field found as a path param is a valid SQL id */
 export const validateSqlId = (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
 
