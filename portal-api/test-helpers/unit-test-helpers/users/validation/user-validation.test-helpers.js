a88687aa36398af918ff0b708e14f1ec06ca2295
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-03-06 19:47:19 +0000

    feat(DTFS2-2960): update to review comments

--- portal-api/test-helpers/unit-test-helpers/users/validation/user-validation.test-helpers.js
+++ portal-api/test-helpers/unit-test-helpers/users/validation/user-validation.test-helpers.js
@@ -1,25 +1,19 @@
 const { produce } = require('immer');
 
-const createTestCasesFromRules = ({ allRules }) => {
-  return produce({}, (draft) => {
+const createTestCasesFromRules = ({ allRules }) => produce({}, (draft) => {
     Object.keys(allRules).forEach((ruleType) => {
-      const testCaseName = ruleType + 'TestCases';
-      draft[testCaseName] = Object.entries(allRules[ruleType]).map(([description, rule]) => {
-        return {
+      const testCaseName = `${ruleType  }TestCases`;
+      draft[testCaseName] = Object.entries(allRules[ruleType]).map(([description, rule]) => ({
           description,
           rule,
-        };
-      });
+        }));
     });
   });
-};
 
-const getAllRulesFromAllRulesTestCases = ({ allRulesTestCases }) => {
-  return [
+const getAllRulesFromAllRulesTestCases = ({ allRulesTestCases }) => [
     ...allRulesTestCases.expectedRulesTestCases.map((testCase) => testCase.rule),
     ...allRulesTestCases.otherRulesTestCases.map((testCase) => testCase.rule),
   ];
-};
 
 const mockRulesToNotReturnAnErrorSynchronously = ({ rules }) => {
   rules.forEach((rule) => {
