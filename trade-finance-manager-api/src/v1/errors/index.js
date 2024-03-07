58dc9c765ebd56915c93551ac353974f5f1116c3
Audrius Vaitonis <audrius.vaitonis@ukexportfinance.gov.uk>
2024-03-07 17:19:49 +0000

    feat(DTFS2-6892): removing unused error class user-not-found

--- trade-finance-manager-api/src/v1/errors/index.js
+++ trade-finance-manager-api/src/v1/errors/index.js
@@ -1,7 +1,5 @@
-const UserNotFoundError = require('./user-not-found.error');
 const InvalidEnvironmentVariableError = require('./invalid-environment-variable.error');
 
 module.exports = {
-  UserNotFoundError,
   InvalidEnvironmentVariableError,
 };
