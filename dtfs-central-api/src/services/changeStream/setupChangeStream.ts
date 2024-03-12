4cdc86cb0c47abcbc7f96eeda3ae67036db3701b
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2024-03-12 12:32:48 +0000

    feat(DTFS2-6965): deploy with extra logging

--- dtfs-central-api/src/services/changeStream/setupChangeStream.ts
+++ dtfs-central-api/src/services/changeStream/setupChangeStream.ts
@@ -7,13 +7,27 @@ import { postAuditDetails } from './changeStreamApi';
  * @returns
  */
 export const setupChangeStream = async () => {
+  try {
   console.info('Setting up mongodb change stream');
   const database = await getClient();
   const changeStream = database.watch([], { fullDocument: 'updateLookup' });
   changeStream.on('change', (changeStreamDocument: ChangeStreamDocument) => {
+    console.info('Testing: ', changeStreamDocument);
     postAuditDetails(changeStreamDocument).catch((error) => {
       console.error('Error sending change stream update to API', error);
     });
   });
   console.info('Mongodb change stream initialised');
+} catch (error: any) {
+  console.error('Error setting up mongodb change stream', error);
+}
 };
+
+
+
+  // var cursor = database.watch(
+  //   [
+  //       { $match: { "operationType": { $in: ["insert", "update", "replace"] } } },
+  //       { $project: { "_id": 1, "fullDocument": 1, "ns": 1, "documentKey": 1 } }
+  //   ],
+  //   { fullDocument: "updateLookup" });
