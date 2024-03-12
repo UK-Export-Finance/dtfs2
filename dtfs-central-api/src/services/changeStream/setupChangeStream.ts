abc7ede801c56cb3b9614918d5fdfd28102412d9
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2024-03-12 14:21:35 +0000

    feat(DTFS2-6965): update matching parameters for operationType in changestream

--- dtfs-central-api/src/services/changeStream/setupChangeStream.ts
+++ dtfs-central-api/src/services/changeStream/setupChangeStream.ts
@@ -8,26 +8,27 @@ import { postAuditDetails } from './changeStreamApi';
  */
 export const setupChangeStream = async () => {
   try {
-  console.info('Setting up mongodb change stream');
-  const database = await getClient();
-  const changeStream = database.watch([], { fullDocument: 'updateLookup' });
-  changeStream.on('change', (changeStreamDocument: ChangeStreamDocument) => {
-    console.info('Testing: ', changeStreamDocument);
-    postAuditDetails(changeStreamDocument).catch((error) => {
-      console.error('Error sending change stream update to API', error);
+    console.info('Setting up mongodb change stream');
+    const database = await getClient();
+    const changeStream = database.watch(
+      [{ $match: { operationType: { $in: ['insert', 'update', 'replace'] } } }, { $project: { _id: 1, fullDocument: 1, ns: 1, documentKey: 1 } }],
+      { fullDocument: 'updateLookup' },
+    );
+    changeStream.on('change', (changeStreamDocument: ChangeStreamDocument) => {
+      console.info('Testing: ', changeStreamDocument);
+      postAuditDetails(changeStreamDocument).catch((error) => {
+        console.error('Error sending change stream update to API', error);
+      });
     });
-  });
-  console.info('Mongodb change stream initialised');
-} catch (error: any) {
-  console.error('Error setting up mongodb change stream', error);
-}
+    console.info('Mongodb change stream initialised');
+  } catch (error: any) {
+    console.error('Error setting up mongodb change stream', error);
+  }
 };
 
-
-
-  // var cursor = database.watch(
-  //   [
-  //       { $match: { "operationType": { $in: ["insert", "update", "replace"] } } },
-  //       { $project: { "_id": 1, "fullDocument": 1, "ns": 1, "documentKey": 1 } }
-  //   ],
-  //   { fullDocument: "updateLookup" });
+// var cursor = database.watch(
+//   [
+//       { $match: { "operationType": { $in: ["insert", "update", "replace"] } } },
+//       { $project: { "_id": 1, "fullDocument": 1, "ns": 1, "documentKey": 1 } }
+//   ],
+//   { fullDocument: "updateLookup" });
