99d09e325c217642e4b4de7ba5612c65d3fef595
Audrius Vaitonis <audrius.vaitonis@ukexportfinance.gov.uk>
2024-03-13 08:31:44 +0000

    feat(DTFS2-6892): in api-test setup check if we are runnin in dev laptop or in docker

--- trade-finance-manager-api/api-test-setup.jest.config.js
+++ trade-finance-manager-api/api-test-setup.jest.config.js
@@ -1,10 +1,13 @@
 
 const dotenv = require('dotenv');
+const fs = require('fs');
 
 dotenv.config();
 
-// The api-test does not run within the container and requires localhost domain to access DB.
-process.env.MONGODB_URI = process.env.MONGODB_URI.replace('dtfs-submissions-data', 'localhost');
+if (!fs.existsSync('/.dockerenv')) {
+  // The api-test does not run within the container and requires localhost domain to access DB.
+  process.env.MONGODB_URI = process.env.MONGODB_URI.replace('dtfs-submissions-data', 'localhost');
+}
 
 const db = require('./src/drivers/db-client');
 
