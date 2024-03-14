f98b7e237fab8756f3d83b5eeb0cab6f8f50daa2
Joshua Binns <Joshua.Binns@softwire.com>
2024-03-14 18:47:23 +0000

    feat(FN-1862): remove state machine mocks and improve tests

--- dtfs-central-api/src/v1/controllers/utilisation-report-service/put-utilisation-report-status.controller/index.test.ts
+++ dtfs-central-api/src/v1/controllers/utilisation-report-service/put-utilisation-report-status.controller/index.test.ts
@@ -1,11 +1,11 @@
 import { HttpStatusCode } from 'axios';
 import httpMocks from 'node-mocks-http';
-import { QueryRunner } from 'typeorm';
+import { QueryRunner, FindOptionsWhere } from 'typeorm';
 import { DbRequestSource, UTILISATION_REPORT_RECONCILIATION_STATUS, UtilisationReportEntity, UtilisationReportEntityMockBuilder } from '@ukef/dtfs2-common';
 import { SqlDbDataSource } from '@ukef/dtfs2-common/sql-db-connection';
 import { PutUtilisationReportStatusRequest, putUtilisationReportStatus } from '.';
 import { ReportWithStatus } from '../../../../types/utilisation-reports';
-import { UtilisationReportStateMachine } from '../../../../services/state-machines/utilisation-report/utilisation-report.state-machine';
+import { UtilisationReportRepo } from '../../../../repositories/utilisation-reports-repo';
 
 console.error = jest.fn();
 
@@ -81,19 +81,6 @@ describe('put-utilisation-report-status.controller', () => {
     );
   });
 
-  const getReportIdToStateMachineMapFromReports = (reports: UtilisationReportEntity[]): { [reportId: number]: UtilisationReportStateMachine } =>
-    reports.reduce(
-      (map, report) => ({
-        ...map,
-        [report.id]: UtilisationReportStateMachine.forReport(report),
-      }),
-      {},
-    );
-
-  const mockRepository = {
-    save: jest.fn(),
-  };
-
   const mockConnect = jest.fn();
   const mockStartTransaction = jest.fn();
   const mockCommitTransaction = jest.fn();
@@ -101,7 +88,7 @@ describe('put-utilisation-report-status.controller', () => {
   const mockRelease = jest.fn();
 
   const mockTransactionManager = {
-    getRepository: jest.fn(),
+    save: jest.fn(),
   };
 
   const mockQueryRunner = {
@@ -113,10 +100,19 @@ describe('put-utilisation-report-status.controller', () => {
     manager: mockTransactionManager,
   } as unknown as QueryRunner;
 
+  const utilisationReportRepoFindOneByOrFailSpy = jest.spyOn(UtilisationReportRepo, 'findOneByOrFail');
+
+  const mockFindOneByOrFail = (reports: UtilisationReportEntity[]) => (where: Parameters<typeof UtilisationReportRepo.findOneByOrFail>[0]) => {
+    const reportWithMatchingId = reports.find(({ id: reportId }) => reportId === (where as FindOptionsWhere<UtilisationReportEntity>).id);
+    if (!reportWithMatchingId) {
+      throw new Error('Failed to find a report with the matching id');
+    }
+    return Promise.resolve(reportWithMatchingId);
+  };
+
   describe('when trying to only mark reports as completed', () => {
     beforeEach(() => {
       jest.resetAllMocks();
-      jest.mocked(mockTransactionManager.getRepository).mockReturnValue(mockRepository);
     });
 
     it('tries to update the correct reports', async () => {
@@ -140,9 +136,7 @@ describe('put-utilisation-report-status.controller', () => {
         UtilisationReportEntityMockBuilder.forStatus('PENDING_RECONCILIATION').withId(reportsWithStatusForMarkingAsCompleted[1].reportId).build(),
       ];
 
-      const reportIdToStateMachineMap = getReportIdToStateMachineMapFromReports(existingReports);
-
-      jest.spyOn(UtilisationReportStateMachine, 'forReportId').mockImplementation((reportId) => Promise.resolve(reportIdToStateMachineMap[reportId]));
+      utilisationReportRepoFindOneByOrFailSpy.mockImplementation(mockFindOneByOrFail(existingReports));
 
       const createQueryRunnerSpy = jest.spyOn(SqlDbDataSource, 'createQueryRunner').mockReturnValue(mockQueryRunner);
 
@@ -157,12 +151,11 @@ describe('put-utilisation-report-status.controller', () => {
       expect(mockCommitTransaction).toHaveBeenCalledTimes(1);
       expect(mockRollbackTransaction).not.toHaveBeenCalled();
       expect(mockRelease).toHaveBeenCalled();
-      expect(mockTransactionManager.getRepository).toHaveBeenCalledWith(UtilisationReportEntity);
-      expect(mockRepository.save).toHaveBeenCalledTimes(reportsWithStatusForMarkingAsCompleted.length);
+      expect(mockTransactionManager.save).toHaveBeenCalledTimes(reportsWithStatusForMarkingAsCompleted.length);
 
       existingReports.forEach((report) => {
         report.updateWithStatus({ status: 'RECONCILIATION_COMPLETED', requestSource });
-        expect(mockRepository.save).toHaveBeenCalledWith(report);
+        expect(mockTransactionManager.save).toHaveBeenCalledWith(UtilisationReportEntity, report);
       });
     });
 
@@ -178,8 +171,7 @@ describe('put-utilisation-report-status.controller', () => {
 
       const existingReport = UtilisationReportEntityMockBuilder.forStatus('RECONCILIATION_COMPLETED').withId(reportWithStatus.reportId).build();
 
-      const existingReportStateMachine = UtilisationReportStateMachine.forReport(existingReport);
-      jest.spyOn(UtilisationReportStateMachine, 'forReportId').mockImplementation(() => Promise.resolve(existingReportStateMachine));
+      utilisationReportRepoFindOneByOrFailSpy.mockResolvedValue(existingReport);
 
       const createQueryRunnerSpy = jest.spyOn(SqlDbDataSource, 'createQueryRunner').mockReturnValue(mockQueryRunner);
 
@@ -195,15 +187,13 @@ describe('put-utilisation-report-status.controller', () => {
       expect(mockCommitTransaction).not.toHaveBeenCalled();
       expect(mockRollbackTransaction).toHaveBeenCalledTimes(1);
       expect(mockRelease).toHaveBeenCalled();
-      expect(mockTransactionManager.getRepository).not.toHaveBeenCalled();
-      expect(mockRepository.save).not.toHaveBeenCalled();
+      expect(mockTransactionManager.save).not.toHaveBeenCalled();
     });
   });
 
   describe('when trying to only mark reports as not completed', () => {
     beforeEach(() => {
       jest.resetAllMocks();
-      jest.mocked(mockTransactionManager.getRepository).mockReturnValue(mockRepository);
     });
 
     it('tries to update the correct reports', async () => {
@@ -238,9 +228,7 @@ describe('put-utilisation-report-status.controller', () => {
         UtilisationReportEntityMockBuilder.forStatus('RECONCILIATION_COMPLETED').withId(reportsWithStatusForMarkingAsNotCompleted[2].reportId).build(),
       ];
 
-      const reportIdToStateMachineMap = getReportIdToStateMachineMapFromReports(existingReports);
-
-      jest.spyOn(UtilisationReportStateMachine, 'forReportId').mockImplementation((reportId) => Promise.resolve(reportIdToStateMachineMap[reportId]));
+      utilisationReportRepoFindOneByOrFailSpy.mockImplementation(mockFindOneByOrFail(existingReports));
 
       const createQueryRunnerSpy = jest.spyOn(SqlDbDataSource, 'createQueryRunner').mockReturnValue(mockQueryRunner);
 
@@ -255,13 +243,12 @@ describe('put-utilisation-report-status.controller', () => {
       expect(mockCommitTransaction).toHaveBeenCalledTimes(1);
       expect(mockRollbackTransaction).not.toHaveBeenCalled();
       expect(mockRelease).toHaveBeenCalled();
-      expect(mockTransactionManager.getRepository).toHaveBeenCalledWith(UtilisationReportEntity);
-      expect(mockRepository.save).toHaveBeenCalledTimes(reportsWithStatusForMarkingAsNotCompleted.length);
+      expect(mockTransactionManager.save).toHaveBeenCalledTimes(reportsWithStatusForMarkingAsNotCompleted.length);
 
       existingReports.forEach((report, index) => {
         const expectedStatus = reportsWithStatusForMarkingAsNotCompleted[index].status;
         report.updateWithStatus({ status: expectedStatus, requestSource });
-        expect(mockRepository.save).toHaveBeenCalledWith(report);
+        expect(mockTransactionManager.save).toHaveBeenCalledWith(UtilisationReportEntity, report);
       });
     });
 
@@ -279,8 +266,7 @@ describe('put-utilisation-report-status.controller', () => {
 
         const existingReport = UtilisationReportEntityMockBuilder.forStatus(reportStatus).withId(reportWithStatus.reportId).build();
 
-        const existingReportStateMachine = UtilisationReportStateMachine.forReport(existingReport);
-        jest.spyOn(UtilisationReportStateMachine, 'forReportId').mockImplementation(() => Promise.resolve(existingReportStateMachine));
+        utilisationReportRepoFindOneByOrFailSpy.mockResolvedValue(existingReport);
 
         const createQueryRunnerSpy = jest.spyOn(SqlDbDataSource, 'createQueryRunner').mockReturnValue(mockQueryRunner);
 
@@ -296,8 +282,7 @@ describe('put-utilisation-report-status.controller', () => {
         expect(mockCommitTransaction).not.toHaveBeenCalled();
         expect(mockRollbackTransaction).toHaveBeenCalledTimes(1);
         expect(mockRelease).toHaveBeenCalled();
-        expect(mockTransactionManager.getRepository).not.toHaveBeenCalled();
-        expect(mockRepository.save).not.toHaveBeenCalled();
+        expect(mockTransactionManager.save).not.toHaveBeenCalled();
       },
     );
   });
