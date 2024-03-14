031d2c57375656bee613154ecf2ace4ad7394ae5
Ruby Lavender <ruby.lavender@softwire.com>
2024-03-14 16:53:27 +0000

    feat(FN-1861): increase memory limit and log heap usage

--- portal-api/api-test.jest.config.js
+++ portal-api/api-test.jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
   ...commonConfig,
   collectCoverageFrom: ['src/**/*.{js,ts}'],
   coverageDirectory: 'generated_reports/coverage/api-test',
-  testMatch: ['**/utilisation-report-service/*.api-test.{js,ts}'],
+  testMatch: ['**/*.api-test.{js,ts}'],
   setupFilesAfterEnv: ['./api-test-setup.jest.config.js'],
   testTimeout: 5000,
+  workerIdleMemoryLimit: '3200MB'
 };
