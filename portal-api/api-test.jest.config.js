ecdb981241757e99f3fa3d774d8640ec2e8a903b
Ruby Lavender <ruby.lavender@softwire.com>
2024-03-14 16:22:21 +0000

    feat(FN-1861): temporarily limit portal-api tests to just a few files for testing

--- portal-api/api-test.jest.config.js
+++ portal-api/api-test.jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ...commonConfig,
   collectCoverageFrom: ['src/**/*.{js,ts}'],
   coverageDirectory: 'generated_reports/coverage/api-test',
-  testMatch: ['**/*.api-test.{js,ts}'],
+  testMatch: ['**/utilisation-report-service/*.api-test.{js,ts}'],
   setupFilesAfterEnv: ['./api-test-setup.jest.config.js'],
   testTimeout: 5000,
 };
