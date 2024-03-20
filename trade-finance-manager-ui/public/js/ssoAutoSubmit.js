ee2b017f90042dd67cf3796343634534e46db79c
Audrius Vaitonis <audrius.vaitonis@ukexportfinance.gov.uk>
2024-03-20 11:58:38 +0000

    feat(DTFS2-6892): recompiled tfm-ui public file ssoAutoSubmit.js

--- trade-finance-manager-ui/public/js/ssoAutoSubmit.js
+++ trade-finance-manager-ui/public/js/ssoAutoSubmit.js
@@ -1,2 +1,2 @@
-var DTFS_TFM;!function(){var o=document.getElementById("acceptExternalSsoPostForm"),t=document.referrer,e=window.location.hostname;!o||"localhost"!==e&&0!==t.indexOf("https://login.microsoftonline.com/")||o.submit(),(DTFS_TFM=void 0===DTFS_TFM?{}:DTFS_TFM).ssoAutoSubmit={}}();
+var DTFS_TFM;!function(){var o,t,e=document.getElementById("acceptExternalSsoPostForm");e&&(o=window.location.hostname,t=document.referrer,"localhost"===o||0===t.indexOf("https://login.microsoftonline.com/"))&&e.submit(),(DTFS_TFM=void 0===DTFS_TFM?{}:DTFS_TFM).ssoAutoSubmit={}}();
 //# sourceMappingURL=ssoAutoSubmit.js.map
