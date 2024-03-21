65840a8ffc8224e26bcc4e311a717470be3793ef
Joshua Binns <Joshua.Binns@softwire.com>
2024-03-21 15:05:39 +0000

    feat(FN-1859): standardise api test timeout in dtfs-central-api

--- dtfs-central-api/api-test.jest.config.js
+++ dtfs-central-api/api-test.jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,ts}'],
   coverageDirectory: 'generated_reports/coverage/api-test',
   testMatch: ['**/*.api-test.{js,ts}'],
+  testTimeout: 80000,
   ...commonSettings,
 };
