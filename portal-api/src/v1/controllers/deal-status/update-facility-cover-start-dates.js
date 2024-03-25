3e7e5b93462c78ba1d08f047f1950455a3a4cd97
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2024-03-25 19:12:54 +0000

    fix(DTFS2-7061): added coverDateConfirmed flag upon facility update api call

--- portal-api/src/v1/controllers/deal-status/update-facility-cover-start-dates.js
+++ portal-api/src/v1/controllers/deal-status/update-facility-cover-start-dates.js
@@ -1,42 +1,56 @@
 const CONSTANTS = require('../../../constants');
+const { getNowAsEpoch } = require('../../helpers/date');
 const facilitiesController = require('../facilities.controller');
 
+/**
+ * Updates the cover start dates of facilities in a deal.
+ * @param {object} user - The user object representing the user performing the update.
+ * @param {object} deal - The deal object containing the facilities to be updated.
+ * @returns {object} - The modified deal object with updated facility cover start dates.
+ */
 const updateFacilityCoverStartDates = async (user, deal) => {
-  const modifiedDeal = deal;
+  try {
+    const modifiedDeal = { ...deal };
 
-  if (modifiedDeal.facilities.length) {
-    modifiedDeal.facilities.forEach(async (facilityId) => {
+    if (!modifiedDeal.facilities || !modifiedDeal.facilities?.length) {
+      console.error('No facilities found in deal %s', modifiedDeal._id);
+      return modifiedDeal;
+    }
+
+    for (const facilityId of modifiedDeal.facilities) {
       const facility = await facilitiesController.findOne(facilityId);
-
       const { facilityStage } = facility;
+      const canUpdate =
+        (facilityStage === CONSTANTS.FACILITIES.FACILITIES_STAGE.BOND.ISSUED || facilityStage === CONSTANTS.FACILITIES.FACILITIES_STAGE.LOAN.UNCONDITIONAL) &&
+        !facility.requestedCoverStartDate &&
+        !facility.coverDateConfirmed;
 
-      const shouldUpdate = ((facilityStage === CONSTANTS.FACILITIES.FACILITIES_STAGE.BOND.ISSUED
-                            || facilityStage === CONSTANTS.FACILITIES.FACILITIES_STAGE.LOAN.UNCONDITIONAL)
-                            && !facility.requestedCoverStartDate);
+      if (canUpdate) {
+        console.info('⚡ Updating facility %s cover start date', facilityId);
 
-      if (shouldUpdate) {
-        const now = Date.now();
         const today = new Date();
+        const nowAsEpoch = getNowAsEpoch();
 
-        facility.updatedAt = now;
-        facility.requestedCoverStartDate = now;
+        facility.updatedAt = nowAsEpoch;
+        facility.coverDateConfirmed = true;
+        facility.requestedCoverStartDate = nowAsEpoch;
         facility['requestedCoverStartDate-day'] = today.getDate();
         facility['requestedCoverStartDate-month'] = today.getMonth() + 1;
         facility['requestedCoverStartDate-year'] = today.getFullYear();
 
-        const { data } = await facilitiesController.update(
-          deal._id,
-          facilityId,
-          facility,
-          user,
-        );
-        return data;
-      }
-      return facility;
-    });
-  }
+        const { data } = await facilitiesController.update(deal._id, facilityId, facility, user);
 
-  return deal;
+        if (!data) {
+          console.error('Error updating facility cover start date for facility %s with response %o', facilityId, data);
+        }
+      }
+    }
+
+    return modifiedDeal;
+  } catch (error) {
+    console.error("An error occurred while updating %s deal's facilities cover start date %o", deal._id, error);
+    return deal;
+  }
 };
 
 module.exports = updateFacilityCoverStartDates;
