32cd6e1b8e8f0544167b0f88394dc22ef76bcce5
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2024-03-26 16:00:48 +0000

    fix(DTFS2-6144): set portal deal status to acknowledged for notice

--- trade-finance-manager-api/src/v1/controllers/update-portal-deal-status.js
+++ trade-finance-manager-api/src/v1/controllers/update-portal-deal-status.js
@@ -1,31 +1,37 @@
 const api = require('../api');
 const CONSTANTS = require('../../constants');
 
+/**
+ * Updates the status of a deal in a portal.
+ * @param {object} deal - The deal object containing properties `_id`, `dealType`, and `submissionType`.
+ * @returns {Promise<object>} - The updated deal object.
+ */
 const updatePortalDealStatus = async (deal) => {
-  const { _id: dealId, dealType, submissionType } = deal;
+  const { _id, dealType, submissionType } = deal;
+  const notice = [CONSTANTS.DEALS.SUBMISSION_TYPE.AIN, CONSTANTS.DEALS.SUBMISSION_TYPE.MIN];
+  const application = [CONSTANTS.DEALS.SUBMISSION_TYPE.MIA];
 
-  let newStatus;
+  let status;
 
-  if (submissionType === CONSTANTS.DEALS.SUBMISSION_TYPE.AIN || submissionType === CONSTANTS.DEALS.SUBMISSION_TYPE.MIN) {
-    newStatus = CONSTANTS.DEALS.PORTAL_DEAL_STATUS.UKEF_ACKNOWLEDGED;
-    if (dealType === CONSTANTS.DEALS.DEAL_TYPE.BSS_EWCS) {
-      await api.updatePortalBssDealStatus(dealId, newStatus);
-    }
-
-    if (dealType === CONSTANTS.DEALS.DEAL_TYPE.GEF) {
-      await api.updatePortalGefDealStatus(dealId, newStatus);
-    }
+  // Deal is an application (MIA)
+  if (application.includes(submissionType)) {
+    status = CONSTANTS.DEALS.PORTAL_DEAL_STATUS.IN_PROGRESS_BY_UKEF;
   }
 
-  if (submissionType === CONSTANTS.DEALS.SUBMISSION_TYPE.MIA) {
-    newStatus = CONSTANTS.DEALS.PORTAL_DEAL_STATUS.IN_PROGRESS_BY_UKEF;
-    if (dealType === CONSTANTS.DEALS.DEAL_TYPE.BSS_EWCS) {
-      await api.updatePortalBssDealStatus(dealId, newStatus);
-    }
+  // Deal is a notice (AIN, MIN)
+  if (notice.includes(submissionType)) {
+    status = CONSTANTS.DEALS.PORTAL_DEAL_STATUS.UKEF_ACKNOWLEDGED;
+  }
 
-    if (dealType === CONSTANTS.DEALS.DEAL_TYPE.GEF) {
-      await api.updatePortalGefDealStatus(dealId, newStatus);
-    }
+  if (!status) {
+    console.error('Cannot update portal deal %s status for submission type %s', _id, submissionType);
+    return deal;
+  }
+
+  if (dealType === CONSTANTS.DEALS.DEAL_TYPE.BSS_EWCS) {
+    await api.updatePortalBssDealStatus(_id, status);
+  } else if (dealType === CONSTANTS.DEALS.DEAL_TYPE.GEF) {
+    await api.updatePortalGefDealStatus(_id, status);
   }
 
   return deal;
