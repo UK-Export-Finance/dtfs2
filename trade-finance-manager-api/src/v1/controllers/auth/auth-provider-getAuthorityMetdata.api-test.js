12d35b553cd127763b8eb19bcb67a5863d318fdb
ttbarnes <tony@tonybarnes.me>
2024-03-28 13:32:24 +0000

    feat(DTFS-6892): simplify unit test

--- trade-finance-manager-api/src/v1/controllers/auth/auth-provider-getAuthorityMetdata.api-test.js
+++ trade-finance-manager-api/src/v1/controllers/auth/auth-provider-getAuthorityMetdata.api-test.js
@@ -3,19 +3,17 @@ const AuthProvider = require('./auth-provider');
 const { msalConfig, AZURE_SSO_TENANT_SUBDOMAIN } = require('./auth-provider-config');
 
 describe('AuthProvider - getAuthorityMetadata', () => {
-  let result;
-
   const mockAxiosResponse = {
     data: 'Mock response from Microsoft'
   };
 
-  beforeAll(async () => {
+  beforeAll(() => {
     axios.get = jest.fn().mockResolvedValue(mockAxiosResponse);
-
-    result = await AuthProvider.getAuthorityMetadata();
   });
 
-  it('should call axios.get', () => {
+  it('should call axios.get', async () => {
+    await AuthProvider.getAuthorityMetadata();
+
     expect(axios.get).toHaveBeenCalledTimes(1);
 
     const expectedAuthority = msalConfig.auth.authority;
@@ -26,7 +24,9 @@ describe('AuthProvider - getAuthorityMetadata', () => {
     expect(axios.get).toHaveBeenCalledWith(expected);
   });
 
-  it('should return the response from axios/microsoft', () => {
+  it('should return the response from axios/microsoft', async () => {
+    const result = await AuthProvider.getAuthorityMetadata();
+
     expect(axios.get).toHaveBeenCalled()
 
     expect(result).toEqual(mockAxiosResponse.data);
@@ -37,7 +37,7 @@ describe('AuthProvider - getAuthorityMetadata', () => {
       axios.get = jest.fn().mockRejectedValue();
 
       try {
-        result = await AuthProvider.getAuthorityMetadata();
+        await AuthProvider.getAuthorityMetadata();
       } catch (error) {
         const expectedError = 'Error TFM auth service - getting getAuthorityMetadata';
 
