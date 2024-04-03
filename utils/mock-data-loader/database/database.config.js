83ee86f2ac0898c6df6e4d44b29a86844e94af36
JoshBinns2000 <108285982+JoshBinns2000@users.noreply.github.com>
2024-04-03 17:24:30 +0100

    chore(FN-1853): create new mongo db connection string env var for tests (#2950)

--- utils/mock-data-loader/database/database.config.js
+++ utils/mock-data-loader/database/database.config.js
@@ -4,7 +4,7 @@ const { asString } = require('@ukef/dtfs2-common');
 dotenv.config();
 
 const dbName = asString(process.env.MONGO_INITDB_DATABASE, 'MONGO_INITDB_DATABASE');
-const connectionString = asString(process.env.MONGODB_URI, 'MONGODB_URI');
+const connectionString = asString(process.env.MONGODB_URI_FOR_TESTS, 'MONGODB_URI_FOR_TESTS');
 
 module.exports = {
   dbName,
