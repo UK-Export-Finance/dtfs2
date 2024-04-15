f4ae8d8ccd700ae5745f5bec94883d1e31622847
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-04-15 12:33:01 +0100

    feat(DTFS2-7085): use validateAuditDetails tests on put endpoint

--- dtfs-central-api/api-tests/v1/tfm/amendments/tfm-amendments-put.api-test.js
+++ dtfs-central-api/api-tests/v1/tfm/amendments/tfm-amendments-put.api-test.js
@@ -1,4 +1,5 @@
 const { generatePortalAuditDetails, generateTfmAuditDetails } = require('@ukef/dtfs2-common/src/helpers/change-stream/generate-audit-details');
+const { withValidateAuditDetailsTests } = require('../../../helpers/with-validate-audit-details.api-tests');
 const wipeDB = require('../../../wipeDB');
 const app = require('../../../../src/createApp');
 const api = require('../../../api')(app);
@@ -43,39 +44,54 @@ describe('PUT TFM amendments', () => {
   });
 
   describe('PUT /v1/tfm/facilities/:id/amendments/:amendmentId', () => {
-    it('should update an amendment based on facilityId and amendmentId', async () => {
-      const postResult = await api.post({ facility: newFacility, user: MOCK_PORTAL_USER }).to('/v1/portal/facilities');
-      const newId = postResult.body._id;
+    describe('with a valid facility and amendment', () => {
+      let facilityId;
+      let amendmentId;
 
-      await api
-        .put({ dealType: CONSTANTS.DEALS.DEAL_TYPE.BSS_EWCS, dealId, auditDetails: generatePortalAuditDetails(MOCK_PORTAL_USER._id) })
-        .to('/v1/tfm/deals/submit');
+      beforeEach(async () => {
+        const postResult = await api.post({ facility: newFacility, user: MOCK_PORTAL_USER }).to('/v1/portal/facilities');
+        facilityId = postResult.body._id;
+  
+        await api
+          .put({ dealType: CONSTANTS.DEALS.DEAL_TYPE.BSS_EWCS, dealId, auditDetails: generatePortalAuditDetails(MOCK_PORTAL_USER._id) })
+          .to('/v1/tfm/deals/submit');
 
-      const { status, body } = await api.post({ auditDetails: generateTfmAuditDetails(MOCK_TFM_USER._id) }).to(`/v1/tfm/facilities/${newId}/amendments/`);
-      const updatePayload = { createdBy: MOCK_PORTAL_USER };
-      const { body: bodyPutResponse } = await api
-        .put({ payload: { updatePayload }, auditDetails: generateTfmAuditDetails(MOCK_TFM_USER._id) })
-        .to(`/v1/tfm/facilities/${newId}/amendments/${body.amendmentId}`);
+          const { body } = await api.post({ auditDetails: generateTfmAuditDetails(MOCK_TFM_USER._id) }).to(`/v1/tfm/facilities/${facilityId}/amendments/`);
+          amendmentId = body.amendmentId;
+      });
 
-      const expected = {
-        dealId: expect.any(String),
-        facilityId: expect.any(String),
-        status: expect.any(String),
-        amendmentId: expect.any(String),
-        createdAt: expect.any(Number),
-        updatePayload,
-        updatedAt: expect.any(Number),
-        version: 1,
-        auditRecord: {
-          lastUpdatedAt: expect.any(String),
-          lastUpdatedByPortalUserId: null,
-          lastUpdatedByTfmUserId: MOCK_TFM_USER._id,
-          lastUpdatedByIsSystem: null,
-          noUserLoggedIn: null,
-        },
-      };
-      expect(status).toEqual(200);
-      expect(bodyPutResponse).toEqual(expected);
+      withValidateAuditDetailsTests({
+        makeRequest: (auditDetails) => api.put({ payload: { updatePayload: { createdBy: MOCK_PORTAL_USER } }, auditDetails }).to(`/v1/tfm/facilities/${facilityId}/amendments/${amendmentId}`),
+        validUserTypes: ['tfm'],
+      });
+
+      it('should update an amendment based on facilityId and amendmentId', async () => {
+        const updatePayload = { createdBy: MOCK_PORTAL_USER };
+        const { body: bodyPutResponse } = await api
+          .put({ payload: { updatePayload }, auditDetails: generateTfmAuditDetails(MOCK_TFM_USER._id) })
+          .to(`/v1/tfm/facilities/${facilityId}/amendments/${body.amendmentId}`);
+  
+        const expected = {
+          dealId: expect.any(String),
+          facilityId: expect.any(String),
+          status: expect.any(String),
+          amendmentId: expect.any(String),
+          createdAt: expect.any(Number),
+          updatePayload,
+          updatedAt: expect.any(Number),
+          version: 1,
+          auditRecord: {
+            lastUpdatedAt: expect.any(String),
+            lastUpdatedByPortalUserId: null,
+            lastUpdatedByTfmUserId: MOCK_TFM_USER._id,
+            lastUpdatedByIsSystem: null,
+            noUserLoggedIn: null,
+          },
+        };
+        expect(status).toEqual(200);
+        expect(bodyPutResponse).toEqual(expected);
+      });
+
     });
 
     it('should return 404 if facilityId and amendmentId are valid but are NOT associated to a record', async () => {
