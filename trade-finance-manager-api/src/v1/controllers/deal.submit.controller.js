f4cc1713eeaa62c576a3d23a766e28ef5ee0a372
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2024-04-15 15:00:58 +0100

    fix(DTFS2-7019): updated to use lodash delay

--- trade-finance-manager-api/src/v1/controllers/deal.submit.controller.js
+++ trade-finance-manager-api/src/v1/controllers/deal.submit.controller.js
@@ -1,3 +1,4 @@
+const { delay } = require('lodash');
 const { findOneTfmDeal, findOnePortalDeal, findOneGefDeal } = require('./deal.controller');
 const { addPartyUrns } = require('./deal.party-db');
 const { createDealTasks } = require('./deal.tasks');
@@ -19,7 +20,6 @@ const { updatePortalDealFromMIAtoMIN } = require('./update-portal-deal-from-MIA-
 const { sendDealSubmitEmails, sendAinMinAcknowledgement } = require('./send-deal-submit-emails');
 const mapSubmittedDeal = require('../mappings/map-submitted-deal');
 const { dealHasAllUkefIds, dealHasAllValidUkefIds } = require('../helpers/dealHasAllUkefIds');
-const delay = require('../helpers/delay');
 
 /**
  * Retrieves a deal from the portal based on the provided deal ID and deal type.
@@ -28,15 +28,7 @@ const delay = require('../helpers/delay');
  * @returns {Promise<object>} - The retrieved deal from the portal.
  */
 const getPortalDeal = async (dealId, dealType) => {
-  let deal;
-
-  /**
-   * 0.2s (200ms) delay
-   * Below is added to avoid any race condition being build up,
-   * where portal deal updates `deals` collection will not be
-   * fetched when updated on close proximity calls.
-   */
-  delay(200);
+  let deal;  
 
   if (dealType === CONSTANTS.DEALS.DEAL_TYPE.GEF) {
     deal = await findOneGefDeal(dealId);
@@ -55,7 +47,16 @@ const getPortalDeal = async (dealId, dealType) => {
  * Azure function
  */
 const submitDealAfterUkefIds = async (dealId, dealType, checker) => {
-  const deal = await getPortalDeal(dealId, dealType);
+  /**
+   * 0.2s (200ms) delay
+   * Below is added to avoid any race condition being build up,
+   * where portal deal updates `deals` collection will not be
+   * fetched when updated on close proximity calls.
+   */
+  await delay(() => {}, 200);
+
+  const deal = Promise.resolve();
+
   console.info('Setting essential deal properties in TFM for deal %s', dealId);
 
   if (!deal) {
