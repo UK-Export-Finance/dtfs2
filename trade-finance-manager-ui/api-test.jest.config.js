df5a322830a47f702d0278e08e4f3aaaa8f0b2aa
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2024-04-17 13:43:12 +0100

    fix(DTFS2-7105): consistent ignore files (#3035)
    Co-authored-by: Abhi Markan <amarkan@ukexportfinance.gov.uk>

--- trade-finance-manager-ui/api-test.jest.config.js
+++ trade-finance-manager-ui/api-test.jest.config.js
@@ -2,11 +2,8 @@ const commonConfig = require('./jest.common.config');
 
 module.exports = {
   ...commonConfig,
-  collectCoverageFrom: [
-    'server/**/*.{js,ts}',
-    'scripts/**/*.{js,ts}',
-  ],
-  coverageDirectory: 'generated_reports/coverage/api-test',
+  collectCoverageFrom: ['server/**/*.{js,ts}', 'scripts/**/*.{js,ts}'],
+  coverageReporters: ['text', 'text-summary'],
   testMatch: ['**/*.api-test.{js,ts}'],
   modulePathIgnorePatterns: ['prototype'],
   testTimeout: 80000,
