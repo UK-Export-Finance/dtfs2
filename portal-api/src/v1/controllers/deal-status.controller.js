8d90d38e6c0ce7e6dd4e5fd36a8a46fb1443c0ca
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2024-04-18 09:21:09 +0100

    fix(DTFS2-7091): validation before updating details deal object (#3037)
    Co-authored-by: Abhi Markan <amarkan@ukexportfinance.gov.uk>

--- portal-api/src/v1/controllers/deal-status.controller.js
+++ portal-api/src/v1/controllers/deal-status.controller.js
@@ -121,8 +121,6 @@ exports.update = async (req, res) => {
 
       updatedDeal = await updateSubmissionCount(updatedDeal, user);
 
-      console.info('Submission count has been updated for deal %s %o', dealId, deal);
-
       if (!updatedDeal?.details?.submissionDate) {
         updatedDeal = await createSubmissionDate(dealId, user);
       }
