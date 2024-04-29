daceda7c743767d2e0916e63980d1555ecbea9ba
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-04-29 16:49:47 +0100

    feat(dtfs2-7067): update tests

---
+++ portal/component-tests/test-helpers/with-contact-us-email-address.component-tests.js
@@ -0,0 +1,46 @@
+const pageRenderer = require('../pageRenderer');
+
+const page = 'login/temporarily-suspended.njk';
+
+module.exports.withContactUsEmailAddressTests = () => {
+  describe('with contact us email address tests', () => {
+    let wrapper;
+    let render;
+    let originalContactUsEmailAddress;
+
+    beforeEach(() => {
+      render = pageRenderer(page);
+      wrapper = render();
+    });
+    describe('when the contact us email is not set', () => {
+      beforeAll(() => {
+        originalContactUsEmailAddress = process.env.CONTACT_US_EMAIL_ADDRESS;
+        delete process.env.CONTACT_US_EMAIL_ADDRESS;
+      });
+
+      afterAll(() => {
+        process.env.CONTACT_US_EMAIL_ADDRESS = originalContactUsEmailAddress;
+      });
+
+      it('should render link to request a new sign in link', () => {
+        wrapper.expectText('[data-cy="dtfs-email-link"]').toRead('DigitalService.TradeFinance@ukexportfinance.gov.uk');
+      });
+    });
+
+    describe('when the contact us email is set', () => {
+      const exampleContactUsEmail = 'exampleContactUs@ukexportfinance.gov.uk';
+      beforeAll(() => {
+        originalContactUsEmailAddress = process.env.CONTACT_US_EMAIL_ADDRESS;
+        process.env.CONTACT_US_EMAIL_ADDRESS = exampleContactUsEmail;
+      });
+
+      afterAll(() => {
+        process.env.CONTACT_US_EMAIL_ADDRESS = originalContactUsEmailAddress;
+      });
+
+      it('should render link to request a new sign in link', () => {
+        wrapper.expectText('[data-cy="dtfs-email-link"]').toRead(exampleContactUsEmail);
+      });
+    });
+  });
+};
