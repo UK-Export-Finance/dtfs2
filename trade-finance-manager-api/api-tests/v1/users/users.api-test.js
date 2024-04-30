4633cdec0418b04a994e6ad447c21d75dcbf1cf5
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-04-30 17:26:10 +0100

    feat(DTFS2-7055): add transaction on tfm user deletion

--- trade-finance-manager-api/api-tests/v1/users/users.api-test.js
+++ trade-finance-manager-api/api-tests/v1/users/users.api-test.js
@@ -11,43 +11,49 @@ describe('user controller', () => {
     tokenUser = await testUserCache.initialise(app);
   });
 
-  it('should not create a new TFM user with malformed payload', async () => {
-    const { body } = await as(tokenUser).post({}).to('/v1/users');
-    expect(body).toEqual({});
+  describe('POST /v1/users', () => {
+    it('should not create a new TFM user with malformed payload', async () => {
+      const { body } = await as(tokenUser).post({}).to('/v1/users');
+      expect(body).toEqual({});
+    });
+
+    it('creates a new TFM user', async () => {
+      const user = MOCK_USERS[0];
+      delete user._id;
+
+      const { body } = await as(tokenUser).post(user).to('/v1/users');
+      userId = body.user._id;
+    });
   });
 
-  it('creates a new TFM user', async () => {
-    const user = MOCK_USERS[0];
-    delete user._id;
+  describe('GET /v1/users', () => {
+    it('returns the requested user if matched', async () => {
+      const expectedResponse = { _id: userId, ...MOCK_USERS[0], status: 'active' };
+      delete expectedResponse.password;
 
-    const { body } = await as(tokenUser).post(user).to('/v1/users');
-    userId = body.user._id;
+      const { status, body } = await as(tokenUser).get(`/v1/users/${userId}`);
+
+      // deleted as token is added on api insertion/login
+      delete expectedResponse.token;
+
+      expect(status).toEqual(200);
+      expect(body.user).toEqual(expectedResponse);
+    });
+
+    it('returns status 404 if userId not matched', async () => {
+      const _id = '6051d94564494924d38ce67c';
+
+      const { status, body } = await as(tokenUser).get(`/v1/users/${_id}`);
+      expect(status).toEqual(404);
+      expect(body.status).toEqual(404);
+      expect(body.message).toEqual('User does not exist');
+    });
   });
 
-  it('returns the requested user if matched', async () => {
-    const expectedResponse = { _id: userId, ...MOCK_USERS[0], status: 'active' };
-    delete expectedResponse.password;
-
-    const { status, body } = await as(tokenUser).get(`/v1/users/${userId}`);
-
-    // deleted as token is added on api insertion/login
-    delete expectedResponse.token;
-
-    expect(status).toEqual(200);
-    expect(body.user).toEqual(expectedResponse);
-  });
-
-  it('returns status 404 if userId not matched', async () => {
-    const _id = '6051d94564494924d38ce67c';
-
-    const { status, body } = await as(tokenUser).get(`/v1/users/${_id}`);
-    expect(status).toEqual(404);
-    expect(body.status).toEqual(404);
-    expect(body.message).toEqual('User does not exist');
-  });
-
-  it('removes the TFM user by _id', async () => {
-    const { status } = await as(tokenUser).remove().to(`/v1/users/${userId}`);
-    expect(status).toEqual(200);
+  describe('GET /v1/users', () => {
+    it('removes the TFM user by _id', async () => {
+      const { status } = await as(tokenUser).remove().to(`/v1/users/${userId}`);
+      expect(status).toEqual(200);
+    });
   });
 });
