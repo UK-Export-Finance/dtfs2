ab1f6512d7d417f2a4189bfc17f6ca4b20e51d13
Joshua Binns <Joshua.Binns@softwire.com>
2024-05-13 16:15:38 +0100

    feat(FN-1615): add default value for fee record status column

--- libs/common/src/sql-db-connection/migrations/1715613302494-AddFeeRecordStatusColumn.ts
+++ libs/common/src/sql-db-connection/migrations/1715613302494-AddFeeRecordStatusColumn.ts
@@ -1,16 +1,19 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddFeeRecordStatusColumn1714741183254 implements MigrationInterface {
-  name = 'AddFeeRecordStatusColumn1714741183254';
+export class AddFeeRecordStatusColumn1715613302494 implements MigrationInterface {
+  name = 'AddFeeRecordStatusColumn1715613302494';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             ALTER TABLE "FeeRecord"
-            ADD "status" nvarchar(255) NOT NULL
+            ADD "status" nvarchar(255) NOT NULL CONSTRAINT "DF_1eb3a649cce84deaa2a20390401" DEFAULT 'TO_DO'
         `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "FeeRecord" DROP CONSTRAINT "DF_1eb3a649cce84deaa2a20390401"
+        `);
     await queryRunner.query(`
             ALTER TABLE "FeeRecord" DROP COLUMN "status"
         `);
