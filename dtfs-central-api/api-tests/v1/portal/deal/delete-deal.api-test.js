d68fa53bd462509f8d1b5abf01f470976e7dc494
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-05-13 17:14:37 +0100

    feat(DTFS2-7055): add reusable test helper

---
+++ dtfs-central-api/api-tests/v1/portal/deal/delete-deal.api-test.js
@@ -0,0 +1,55 @@
+const { ObjectId } = require('mongodb');
+const {
+  generatePortalAuditDetails,
+  withDeletionAuditLogsTests,
+  generateMockPortalUserAuditDatabaseRecord,
+} = require('@ukef/dtfs2-common/change-stream');
+const app = require('../../../../src/createApp');
+const api = require('../../../api')(app);
+const { DEALS } = require('../../../../src/constants');
+const aDeal = require('../../deal-builder');
+const { MOCK_PORTAL_USER } = require('../../../mocks/test-users/mock-portal-user');
+
+const newDeal = aDeal({
+  dealType: DEALS.DEAL_TYPE.BSS_EWCS,
+  additionalRefName: 'mock name',
+  bankInternalRefName: 'mock id',
+  editedBy: [],
+  eligibility: {
+    status: 'Not started',
+    criteria: [{}],
+  },
+});
+
+describe('DELETE /v1/portal/deals', () => {
+  let documentToDeleteId;
+
+  if (process.env.CHANGE_STREAM_ENABLED === 'true') {
+    beforeEach(async () => {
+      const postResult = await api.post({ deal: newDeal, user: MOCK_PORTAL_USER }).to('/v1/portal/deals');
+      documentToDeleteId = new ObjectId(postResult.body._id);
+    });
+
+    withDeletionAuditLogsTests({
+      makeRequest: async () => {
+        await api
+          .remove({
+            auditDetails: generatePortalAuditDetails(MOCK_PORTAL_USER._id),
+          })
+          .to(`/v1/portal/deals/${documentToDeleteId}`);
+      },
+      collectionName: 'deals',
+      auditRecord: generateMockPortalUserAuditDatabaseRecord(MOCK_PORTAL_USER._id),
+      getDeletedDocumentId: () => documentToDeleteId,
+    });
+  } else {
+    it('returns 200 response', async () => {
+      const { status } = await api
+        .remove({
+          auditDetails: generatePortalAuditDetails(MOCK_PORTAL_USER._id),
+        })
+        .to(`/v1/portal/deals/${documentToDeleteId}`);
+      expect(status).toBe(200);
+    });
+  }
+});
--- libs/common/src/change-stream/test-helpers/index.ts
+++ libs/common/src/change-stream/test-helpers/index.ts
@@ -1,1 +1,2 @@
 export * from './generate-mock-audit-database-record';
+export * from './with-deletion-audit-logs.tests';
