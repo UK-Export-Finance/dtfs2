71781f4391e2a1547b857c044eb149131c382ae5
Audrius Vaitonis <audrius.vaitonis@ukexportfinance.gov.uk>
2024-05-21 20:57:25 +0100

    feat(DTFS2-7151): fix api-test in portal-api

--- portal-api/src/external-api/__mocks__/geospatial-addresses.js
+++ portal-api/src/external-api/__mocks__/geospatial-addresses.js
@@ -1,4 +1,26 @@
-const getAddressesByPostcode = jest.fn();
+const getAddressesByPostcode = async () => ({
+  status: 200,
+  data: [
+    {
+      organisationName: null,
+      addressLine1: '95 PORTLAND STREET',
+      addressLine2: null,
+      addressLine3: null,
+      locality: 'LONDON',
+      postalCode: 'SE17 2NR',
+      country: 'England',
+    },
+    {
+      organisationName: null,
+      addressLine1: '97 PORTLAND STREET',
+      addressLine2: null,
+      addressLine3: null,
+      locality: 'LONDON',
+      postalCode: 'SE17 2NR',
+      country: 'England',
+    },
+  ],
+});
 
 module.exports = {
   getAddressesByPostcode,
