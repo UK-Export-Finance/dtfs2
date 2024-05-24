f269360540669a4d55e959d2e7df8d55f98faeca
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-05-24 12:36:09 +0100

    fix(dtfs2-none): fix test

--- azure-functions/acbs-function/acbs-issue-facility/index.js
+++ azure-functions/acbs-function/acbs-issue-facility/index.js
@@ -42,59 +42,72 @@ module.exports = df.orchestrator(function* updateACBSfacility(context) {
       update: {},
     };
 
-    if (facilityId) {
-      // 1. GET Facility master record object
-      const { acbsFacility, etag } = yield context.df.callActivityWithRetry('activity-get-facility-master', retryOptions, { facilityId });
+    if (!facilityId) {
+      throw new Error('Invalid argument set');
+    }
+    
+    // 1. GET Facility master record object
+    const { acbsFacility, etag } = yield context.df.callActivityWithRetry('activity-get-facility-master', retryOptions, { facilityId });
 
-      if (acbsFacility && etag) {
-        // 2.1. Create updated facility master record object
-        const acbsFacilityMasterInput = mappings.facility.facilityUpdate(facilitySnapshot, acbsFacility, deal);
+    if (acbsFacility && etag) {
+      // 2.1. Create updated facility master record object
+      const acbsFacilityMasterInput = mappings.facility.facilityUpdate(facilitySnapshot, acbsFacility, deal);
 
-        // 2.2. PUT updated facility master record object
-        const issuedFacilityMaster = yield context.df.callActivityWithRetry('activity-update-facility-master', retryOptions, {
-          facilityId,
-          acbsFacilityMasterInput,
-          updateType: CONSTANTS.FACILITY.OPERATION.ISSUE,
-          etag,
-        });
+      // 2.2. PUT updated facility master record object
+      const issuedFacilityMaster = yield context.df.callActivityWithRetry('activity-update-facility-master', retryOptions, {
+        facilityId,
+        acbsFacilityMasterInput,
+        updateType: CONSTANTS.FACILITY.OPERATION.ISSUE,
+        etag,
+      });
 
-        // Records only created for `Issued` and `Activated` facilities only
-        // Loan record consumes ACBS exporter ID, extracted from master facility record
-        acbsParties.parties.exporter = {
-          partyIdentifier: acbsFacility.dealBorrowerIdentifier,
-        };
-        facilitySnapshot.update = {
-          ...acbsFacilityMasterInput,
-        };
+      // Records only created for `Issued` and `Activated` facilities only
+      // Loan record consumes ACBS exporter ID, extracted from master facility record
+      acbsParties.parties.exporter = {
+        partyIdentifier: acbsFacility.dealBorrowerIdentifier,
+      };
+      facilitySnapshot.update = {
+        ...acbsFacilityMasterInput,
+      };
 
-        // 3.1. Facility loan record
-        const acbsFacilityLoanInput = mappings.facility.facilityLoan(deal, facilitySnapshot, acbsParties);
+      // 3.1. Facility loan record
+      const acbsFacilityLoanInput = mappings.facility.facilityLoan(deal, facilitySnapshot, acbsParties);
 
-        // 3.2. Create facility loan record
-        const facilityLoan = yield context.df.callActivityWithRetry('activity-create-facility-loan', retryOptions, { facilityIdentifier: facilityId, acbsFacilityLoanInput });
+      // 3.2. Create facility loan record
+      const facilityLoan = yield context.df.callActivityWithRetry('activity-create-facility-loan', retryOptions, {
+        facilityIdentifier: facilityId,
+        acbsFacilityLoanInput,
+      });
 
-        // 4.1. Map Facility fixed-fee / premium schedule record(s)
-        const acbsFacilityFeeInput = mappings.facility.facilityFee(deal, facilitySnapshot);
+      // 4.1. Map Facility fixed-fee / premium schedule record(s)
+      const acbsFacilityFeeInput = mappings.facility.facilityFee(deal, facilitySnapshot);
 
-        // 4.2. Facility fixed-fee record(s) creation
-        if (Array.isArray(acbsFacilityFeeInput)) {
-          facilityFee = [];
-          // eslint-disable-next-line no-plusplus
-          for (let i = 0; i < acbsFacilityFeeInput.length; i++) {
-            const input = acbsFacilityFeeInput[i];
-            facilityFee.push(yield context.df.callActivityWithRetry('activity-create-facility-fee', retryOptions, { facilityIdentifier: facilityId, acbsFacilityFeeInput: input }));
-          }
-        } else {
-          facilityFee = yield context.df.callActivityWithRetry('activity-create-facility-fee', retryOptions, { facilityIdentifier: facilityId, acbsFacilityFeeInput });
+      // 4.2. Facility fixed-fee record(s) creation
+      if (Array.isArray(acbsFacilityFeeInput)) {
+        facilityFee = [];
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < acbsFacilityFeeInput.length; i++) {
+          const input = acbsFacilityFeeInput[i];
+          facilityFee.push(
+            yield context.df.callActivityWithRetry('activity-create-facility-fee', retryOptions, {
+              facilityIdentifier: facilityId,
+              acbsFacilityFeeInput: input,
+            }),
+          );
         }
-
-        return {
-          facilityId: facility._id,
-          issuedFacilityMaster,
-          facilityLoan,
-          facilityFee,
-        };
+      } else {
+        facilityFee = yield context.df.callActivityWithRetry('activity-create-facility-fee', retryOptions, {
+          facilityIdentifier: facilityId,
+          acbsFacilityFeeInput,
+        });
       }
+
+      return {
+        facilityId: facility._id,
+        issuedFacilityMaster,
+        facilityLoan,
+        facilityFee,
+      };
     }
 
     throw new Error('Invalid argument set');
