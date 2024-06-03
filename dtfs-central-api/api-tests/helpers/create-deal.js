54c117b27c09f8d48e1cf84b84237d932917be6d
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-06-03 14:34:49 +0100

    feat(dtfs2-6966): fix createdeal import for tests

--- dtfs-central-api/api-tests/helpers/create-deal.js
+++ dtfs-central-api/api-tests/helpers/create-deal.js
@@ -1,5 +1,7 @@
-import { generatePortalAuditDetails } from '@ukef/dtfs2-common/change-stream';
+const { generatePortalAuditDetails } = require('@ukef/dtfs2-common/change-stream');
 
-export const createDeal = async ({ api, deal, user }) => {
+const createDeal = async ({ api, deal, user }) => {
   return api.post({ deal, user, auditDetails: generatePortalAuditDetails(user._id) }).to('/v1/portal/deals');
 };
+
+module.exports = { createDeal };
