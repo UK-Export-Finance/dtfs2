5b5d60cd30241b145176a99bd1c5d3b7e74280a4
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-06-03 14:06:51 +0100

    feat(dtfs2-6966): update dtfs central

--- dtfs-central-api/src/v1/controllers/portal/facility/create-facility.controller.js
+++ dtfs-central-api/src/v1/controllers/portal/facility/create-facility.controller.js
@@ -5,7 +5,7 @@ const getCreateFacilityErrors = require('../../../validation/create-facility');
 const { findOneDeal } = require('../deal/get-deal.controller');
 const { addFacilityIdToDeal } = require('../deal/update-deal.controller');
 
-const createFacility = async (facility, user, routePath) => {
+const createFacility = async (facility, user, routePath, auditDetails) => {
   const collection = await db.getCollection(MONGO_DB_COLLECTIONS.FACILITIES);
 
   const { dealId } = facility;
@@ -20,13 +20,13 @@ const createFacility = async (facility, user, routePath) => {
   const response = await collection.insertOne(newFacility);
   const { insertedId } = response;
 
-  await addFacilityIdToDeal(dealId, insertedId, user, routePath);
+  await addFacilityIdToDeal(dealId, insertedId, user, routePath, auditDetails);
 
   return { _id: insertedId };
 };
 
 exports.createFacilityPost = async (req, res) => {
-  const { facility, user } = req.body;
+  const { facility, user, auditDetails } = req.body;
 
   if (!user) {
     return res.status(404).send();
@@ -47,7 +47,7 @@ exports.createFacilityPost = async (req, res) => {
   const deal = await findOneDeal(facility.dealId);
 
   if (deal) {
-    const createdFacility = await createFacility(facility, user, req.routePath);
+    const createdFacility = await createFacility(facility, user, req.routePath, auditDetails);
 
     return res.status(200).send(createdFacility);
   }
