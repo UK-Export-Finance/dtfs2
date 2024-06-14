3aee4535b3c491c9cfaf6b4ee4c6170e7b0c1da8
oscar-richardson-softwire <116292912+oscar-richardson-softwire@users.noreply.github.com>
2024-06-14 15:35:00 +0100

    feat(DTFS2-7167): add 'What is a Facility End Date' details drop-down to GEF 'About facility' page (#3238)

--- libs/common/src/helpers/index.ts
+++ libs/common/src/helpers/index.ts
@@ -3,4 +3,4 @@ export * from './validation';
 export * from './date';
 export * from './currency';
 export * from './schema';
-export { isTfmPaymentReconciliationFeatureFlagEnabled } from './is-feature-flag-enabled';
+export { isTfmPaymentReconciliationFeatureFlagEnabled, isFacilityEndDateFeatureFlagEnabled } from './is-feature-flag-enabled';
