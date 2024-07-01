1d1aad8eafc0dcebe8400a2ea08dc073be8d41f9
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-07-01 15:12:07 +0100

    feat(dtfs2-6966): fix tests

--- trade-finance-manager-api/api-tests/v1/deals/deals-submit.second-submission.api-test.js
+++ trade-finance-manager-api/api-tests/v1/deals/deals-submit.second-submission.api-test.js
@@ -594,8 +594,12 @@ describe('/v1/deals', () => {
 
         const facilityId = body.facilities.find((f) => f.hasBeenIssued === true)._id;
 
-        expect(updateGefFacilitySpy).toHaveBeenCalledWith(facilityId, {
-          hasBeenIssuedAndAcknowledged: true,
+        expect(updateGefFacilitySpy).toHaveBeenCalledWith({
+          facilityId,
+          facilityUpdate: {
+            hasBeenIssuedAndAcknowledged: true,
+          },
+          auditDetails: expectAnyPortalUserAuditDetails,
         });
       });
 
