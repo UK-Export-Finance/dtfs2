9c3fa884e576eb26e757338f688ba6deae605a1c
Ruby Lavender <ruby.lavender@softwire.com>
2024-07-05 17:33:43 +0100

    feat(FN-1811): disable report downloads from tfm when ff is on

--- trade-finance-manager-ui/server/middleware/index.ts
+++ trade-finance-manager-ui/server/middleware/index.ts
@@ -3,4 +3,5 @@ export { validateUserTeam } from './validateUserTeam';
 export { validateUser } from './user-validation';
 export { validateSqlId } from './validateSqlId';
 export { validateTfmPaymentReconciliationFeatureFlagIsEnabled } from './validateTfmPaymentReconciliationFeatureFlagIsEnabled';
+export { validateTfmPaymentReconciliationFeatureFlagIsNotEnabled } from './validateTfmPaymentReconciliationFeatureFlagIsNotEnabled';
 export { validatePostAddPaymentRequestBody } from './validatePostAddPaymentRequestBody';
--- trade-finance-manager-ui/server/routes/utilisation-reports/index.ts
+++ trade-finance-manager-ui/server/routes/utilisation-reports/index.ts
@@ -3,7 +3,13 @@ import express from 'express';
 import { PDC_TEAM_IDS } from '@ukef/dtfs2-common';
 import { getUtilisationReports } from '../../controllers/utilisation-reports';
 import { updateUtilisationReportStatus } from '../../controllers/utilisation-reports/update-utilisation-report-status';
-import { validateSqlId, validateUserTeam, validateTfmPaymentReconciliationFeatureFlagIsEnabled, validatePostAddPaymentRequestBody } from '../../middleware';
+import {
+  validateSqlId,
+  validateUserTeam,
+  validateTfmPaymentReconciliationFeatureFlagIsEnabled,
+  validatePostAddPaymentRequestBody,
+  validateTfmPaymentReconciliationFeatureFlagIsNotEnabled,
+} from '../../middleware';
 import { getReportDownload } from '../../controllers/utilisation-reports/report-download';
 import { getUtilisationReportReconciliationByReportId } from '../../controllers/utilisation-reports/utilisation-report-reconciliation-for-report';
 import { getFindReportsByYear } from '../../controllers/utilisation-reports/find-reports-by-year';
@@ -19,7 +25,13 @@ utilisationReportsRoutes.get('/', validateUserTeam(Object.values(PDC_TEAM_IDS)),
 
 utilisationReportsRoutes.post('/', validateUserTeam([PDC_TEAM_IDS.PDC_RECONCILE]), updateUtilisationReportStatus);
 
-utilisationReportsRoutes.get('/:id/download', validateUserTeam(Object.values(PDC_TEAM_IDS)), validateSqlId('id'), getReportDownload);
+utilisationReportsRoutes.get(
+  '/:id/download',
+  validateTfmPaymentReconciliationFeatureFlagIsNotEnabled,
+  validateUserTeam(Object.values(PDC_TEAM_IDS)),
+  validateSqlId('id'),
+  getReportDownload,
+);
 
 utilisationReportsRoutes.get('/find-reports-by-year', validateUserTeam(Object.values(PDC_TEAM_IDS)), getFindReportsByYear);
 
