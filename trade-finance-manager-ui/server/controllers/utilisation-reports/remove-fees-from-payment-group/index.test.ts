c928de1a9dfa1849084a15ee9157d2a55f25291c
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-07-12 10:31:23 +0100

    feat(FN-1719): test api call made for remove fees from payment endpoint

--- trade-finance-manager-ui/server/controllers/utilisation-reports/remove-fees-from-payment-group/index.test.ts
+++ trade-finance-manager-ui/server/controllers/utilisation-reports/remove-fees-from-payment-group/index.test.ts
@@ -2,6 +2,7 @@ import httpMocks from 'node-mocks-http';
 import { postRemoveFeesFromPaymentGroup } from '.';
 import api from '../../../api';
 import { aTfmSessionUser } from '../../../../test-helpers/test-data/tfm-session-user';
+import { RemoveFeesFromPaymentGroupFormRequestBody } from '../../../helpers/remove-fees-from-payment-group-helper';
 
 jest.mock('../../../api');
 
@@ -9,31 +10,40 @@ console.error = jest.fn();
 
 describe('controllers/utilisation-reports/remove-fees-from-payment', () => {
   describe('postRemoveFeesFromPaymentGroup', () => {
-    const userToken = 'abc123';
     const requestSession = {
       user: aTfmSessionUser(),
-      userToken,
+      userToken: 'abc123',
     };
+
+    beforeEach(() => {
+      jest.mocked(api.removeFeesFromPaymentGroup).mockResolvedValue();
+    });
 
     afterEach(() => {
       jest.resetAllMocks();
     });
 
-    it('renders the problem-with-service page when an error occurs', async () => {
+    it('removes the selected fee records from the payment', async () => {
       // Arrange
+      const selectedFeeRecordIds = [1, 2];
+      const requestBodyForPostRemoveFeesFromPayment: RemoveFeesFromPaymentGroupFormRequestBody = {
+        ...selectedFeeRecordIds.map((id) => ({ [`feeRecordId-${id}`]: 'on' })).reduce((acc, curr) => ({ ...acc, ...curr }), {}),
+        totalSelectableFeeRecords: selectedFeeRecordIds.length.toString(),
+      };
+
+      const reportId = '1';
+      const paymentId = '2';
       const { req, res } = httpMocks.createMocks({
         session: requestSession,
-        params: { reportId: '1', paymentId: '2' },
+        params: { reportId, paymentId },
+        body: requestBodyForPostRemoveFeesFromPayment,
       });
-
-      jest.mocked(api.removeFeesFromPaymentGroup).mockRejectedValue(new Error('Some error'));
 
       // Act
       await postRemoveFeesFromPaymentGroup(req, res);
 
       // Assert
-      expect(res._getRenderView()).toBe('_partials/problem-with-service.njk');
-      expect(res._getRenderData()).toEqual({ user: requestSession.user });
+      expect(api.removeFeesFromPaymentGroup).toHaveBeenCalledWith(reportId, paymentId, selectedFeeRecordIds, requestSession.user, requestSession.userToken);
     });
 
     it("redirects to '/utilisation-reports/:reportId/edit-payment/:paymentId'", async () => {
@@ -51,6 +61,23 @@ describe('controllers/utilisation-reports/remove-fees-from-payment', () => {
       // Assert
       expect(res._getRedirectUrl()).toBe(`/utilisation-reports/${reportId}/edit-payment/${paymentId}`);
       expect(res._isEndCalled()).toBe(true);
+    });
+
+    it('renders the problem-with-service page when an error occurs', async () => {
+      // Arrange
+      const { req, res } = httpMocks.createMocks({
+        session: requestSession,
+        params: { reportId: '1', paymentId: '2' },
+      });
+
+      jest.mocked(api.removeFeesFromPaymentGroup).mockRejectedValue(new Error('Some error'));
+
+      // Act
+      await postRemoveFeesFromPaymentGroup(req, res);
+
+      // Assert
+      expect(res._getRenderView()).toBe('_partials/problem-with-service.njk');
+      expect(res._getRenderData()).toEqual({ user: requestSession.user });
     });
   });
 });
