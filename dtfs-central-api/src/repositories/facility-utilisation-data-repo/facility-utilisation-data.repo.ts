827e7f0f58a8c4fde28ea48f149f2bd040ea4486
Joshua Binns <joshua.binns@softwire.com>
2024-07-15 16:49:06 +0100

    feat(FN-1724): update repo file

--- dtfs-central-api/src/repositories/facility-utilisation-data-repo/facility-utilisation-data.repo.ts
+++ dtfs-central-api/src/repositories/facility-utilisation-data-repo/facility-utilisation-data.repo.ts
@@ -1,14 +1,4 @@
 import { FacilityUtilisationDataEntity } from '@ukef/dtfs2-common';
 import { SqlDbDataSource } from '@ukef/dtfs2-common/sql-db-connection';
 
-export const FacilityUtilisationDataRepo = SqlDbDataSource.getRepository(FacilityUtilisationDataEntity).extend({
-  /**
-   * Checks if there exists a row in the facility utilisation data
-   * table with an id matching the supplied facility id
-   * @param facilityId - The facility id to check for
-   * @returns Whether or not an entity with the supplied id exists
-   */
-  async facilityUtilisationDataWithIdExists(facilityId: string): Promise<boolean> {
-    return await this.existsBy({ id: facilityId });
-  },
-});
+export const FacilityUtilisationDataRepo = SqlDbDataSource.getRepository(FacilityUtilisationDataEntity);
