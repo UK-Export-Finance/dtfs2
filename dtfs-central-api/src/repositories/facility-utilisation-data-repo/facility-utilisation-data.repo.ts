19118145cb3f505785871a281239560d84017316
Joshua Binns <joshua.binns@softwire.com>
2024-07-15 17:02:17 +0100

    feat(FN-1724): remove facility utilisation data repo

--- dtfs-central-api/src/repositories/facility-utilisation-data-repo/facility-utilisation-data.repo.ts
+++
@@ -1,4 +0,0 @@
-import { FacilityUtilisationDataEntity } from '@ukef/dtfs2-common';
-import { SqlDbDataSource } from '@ukef/dtfs2-common/sql-db-connection';
-
-export const FacilityUtilisationDataRepo = SqlDbDataSource.getRepository(FacilityUtilisationDataEntity);
