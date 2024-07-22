3d0bfc1fc2e61dece1e14aa81837f29fc57c417d
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-07-22 09:15:14 +0100

    feat(FN-3196): add e2e tests to check form values persisted

--- e2e-tests/tfm/cypress/e2e/journeys/utilisation-reports/edit-payment.spec.js
+++ e2e-tests/tfm/cypress/e2e/journeys/utilisation-reports/edit-payment.spec.js
@@ -15,7 +15,6 @@ import relative from '../../relativeURL';
 context(`${PDC_TEAMS.PDC_RECONCILE} users can edit payments`, () => {
   const reportId = 12;
   const paymentId = 15;
-  const feeRecordId = 7;
   const paymentCurrency = CURRENCY.GBP;
   const paymentAmount = 100;
 
@@ -48,7 +47,7 @@ context(`${PDC_TEAMS.PDC_RECONCILE} users can edit payments`, () => {
       .withReference(paymentReference)
       .build();
 
-  const aFeeRecordWithAmountStatusAndPayments = (amount, status, payments) =>
+  const aFeeRecordWithIdAmountStatusAndPayments = (feeRecordId, amount, status, payments) =>
     FeeRecordEntityMockBuilder.forReport(utilisationReport)
       .withId(feeRecordId)
       .withStatus(status)
@@ -65,7 +64,7 @@ context(`${PDC_TEAMS.PDC_RECONCILE} users can edit payments`, () => {
     cy.task(NODE_TASKS.INSERT_UTILISATION_REPORTS_INTO_DB, [utilisationReport]);
 
     const payment = aPaymentWithAmount(paymentAmount);
-    const feeRecord = aFeeRecordWithAmountStatusAndPayments(paymentAmount, FEE_RECORD_STATUS.MATCH, [payment]);
+    const feeRecord = aFeeRecordWithIdAmountStatusAndPayments(123, paymentAmount, FEE_RECORD_STATUS.MATCH, [payment]);
     cy.task(NODE_TASKS.INSERT_FEE_RECORDS_INTO_DB, [feeRecord]);
 
     pages.landingPage.visit();
@@ -100,12 +99,26 @@ context(`${PDC_TEAMS.PDC_RECONCILE} users can edit payments`, () => {
     cy.getInputByLabelText('Payment reference (optional)').should('have.value', paymentReference);
   });
 
-  it('should display errors when form submitted with invalid values and persist the inputted values', () => {
+  it('should display errors when form submitted with invalid values and persist the selected fees and inputted values', () => {
+    cy.task(NODE_TASKS.REMOVE_ALL_FEE_RECORDS_FROM_DB);
+    cy.task(NODE_TASKS.REMOVE_ALL_PAYMENTS_FROM_DB);
+
+    const payment = aPaymentWithAmount(200);
+    const feeRecord = aFeeRecordWithIdAmountStatusAndPayments(123, 70, FEE_RECORD_STATUS.DOES_NOT_MATCH, [payment]);
+    const anotherFeeRecord = aFeeRecordWithIdAmountStatusAndPayments(124, 7, FEE_RECORD_STATUS.DOES_NOT_MATCH, [payment]);
+    cy.task(NODE_TASKS.INSERT_FEE_RECORDS_INTO_DB, [feeRecord, anotherFeeRecord]);
+
+    const getFeeRecordCheckbox = (feeRecordId) => cy.get(`[type="checkbox"][id="feeRecordId-${feeRecordId}"]`);
+
+    cy.reload();
+
     pages.utilisationReportPage.clickPaymentLink(paymentId);
 
     cy.url().should('eq', relative(`/utilisation-reports/${reportId}/edit-payment/${paymentId}`));
 
     clearFormValues();
+
+    getFeeRecordCheckbox(feeRecord.id).check();
 
     cy.getInputByLabelText('Amount received').type('nonsense');
     cy.getInputByLabelText('Day').type('nonsense');
@@ -114,6 +127,9 @@ context(`${PDC_TEAMS.PDC_RECONCILE} users can edit payments`, () => {
     cy.getInputByLabelText('Payment reference (optional)').type('This is valid');
 
     pages.utilisationReportEditPaymentPage.clickSaveChangesButton();
+
+    getFeeRecordCheckbox(feeRecord.id).should('be.checked');
+    getFeeRecordCheckbox(anotherFeeRecord.id).should('not.be.checked');
 
     cy.get('a').should('contain', 'Enter a valid amount received');
     cy.get('a').should('contain', 'The date payment received must be a real date');
@@ -183,7 +199,7 @@ context(`${PDC_TEAMS.PDC_RECONCILE} users can edit payments`, () => {
     cy.task(NODE_TASKS.REMOVE_ALL_PAYMENTS_FROM_DB);
 
     const payment = aPaymentWithAmount(200);
-    const feeRecord = aFeeRecordWithAmountStatusAndPayments(200, FEE_RECORD_STATUS.MATCH, [payment]);
+    const feeRecord = aFeeRecordWithIdAmountStatusAndPayments(123, 200, FEE_RECORD_STATUS.MATCH, [payment]);
     cy.task(NODE_TASKS.INSERT_FEE_RECORDS_INTO_DB, [feeRecord]);
 
     cy.reload();
@@ -208,7 +224,7 @@ context(`${PDC_TEAMS.PDC_RECONCILE} users can edit payments`, () => {
     cy.task(NODE_TASKS.REMOVE_ALL_PAYMENTS_FROM_DB);
 
     const payment = aPaymentWithAmount(100);
-    const feeRecord = aFeeRecordWithAmountStatusAndPayments(200, FEE_RECORD_STATUS.DOES_NOT_MATCH, [payment]);
+    const feeRecord = aFeeRecordWithIdAmountStatusAndPayments(123, 200, FEE_RECORD_STATUS.DOES_NOT_MATCH, [payment]);
     cy.task(NODE_TASKS.INSERT_FEE_RECORDS_INTO_DB, [feeRecord]);
 
     cy.reload();
