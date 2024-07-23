6e65912c7e01153dea392ef282ffb50e5205c229
Joshua Binns <joshua.binns@softwire.com>
2024-07-23 11:15:36 +0100

    feat(FN-1725): tfm api mapping fixes

--- trade-finance-manager-api/src/v1/rest-mappings/helpers/amendment.helpers.js
+++ trade-finance-manager-api/src/v1/rest-mappings/helpers/amendment.helpers.js
@@ -1,7 +1,9 @@
 const { CURRENCY } = require('@ukef/dtfs2-common');
+const { orderBy } = require('lodash');
 const { formattedNumber } = require('../../../utils/number');
 const { decimalsCount, roundNumber } = require('../../helpers/number');
 const isValidFacility = require('./isValidFacility.helper');
+const { AMENDMENT_STATUS } = require('../../../constants/deals');
 
 // returns the formatted amendment value and currency (without conversion)
 const amendmentChangeValueExportCurrency = (amendment) => {
@@ -62,21 +64,33 @@ const calculateUkefExposure = (facilityValueInGBP, coverPercentage) => {
   return null;
 };
 
-// returns tfm object from latest amendment if it exists or null
+/**
+ * @typedef {object} LatestCompletedAmendment
+ * @property {{ currency: import('@ukef/dtfs2-common').Currency, value: number }} [value]
+ * @property {import('@ukef/dtfs2-common').UnixTimestamp} [coverEndDate]
+ * @property {number} [amendmentExposurePeriodInMonths]
+ * @property {{ exposure: number, timestamp: import('@ukef/dtfs2-common').UnixTimestamp }} [exposure]
+ */
+
+/**
+ * Get the latest completed amendment values
+ * @param {Record<string, unknown>[]} amendments
+ * @returns {LatestCompletedAmendment}
+ */
 const findLatestCompletedAmendment = (amendments) => {
   if (!amendments) {
-    return null;
-  }
-  // array reversed to get the latest amendment
-  const amendmentsReversed = [...amendments].reverse();
-  // reversed array checked for first tfm object and is returned
-  const tfmAmendmentObject = amendmentsReversed.find((amendment) => amendment.tfm);
-
-  if (tfmAmendmentObject) {
-    return tfmAmendmentObject.tfm;
+    return {};
   }
 
-  return null;
+  const completedAmendments = amendments.filter(({ status }) => status === AMENDMENT_STATUS.COMPLETED);
+  const sortedAmendments = orderBy(completedAmendments, ['updatedAt', 'version'], ['desc', 'asc']);
+  const foundAmendment = sortedAmendments.find((amendment) => amendment.tfm)?.tfm ?? {};
+  if (!foundAmendment.coverEndDate) {
+    foundAmendment.coverEndDate = sortedAmendments.find((amendment) => amendment.coverEndDate)?.coverEndDate;
+  }
+
+  const { value, amendmentExposurePeriodInMonths, exposure, coverEndDate } = foundAmendment;
+  return { value, coverEndDate, amendmentExposurePeriodInMonths, exposure };
 };
 
 // calculates the value for total exposure to return for a single amendment
