27595a2e8477e50fc420ee6bc73e85e5790840b7
Joshua Binns <joshua.binns@softwire.com>
2024-07-24 12:41:06 +0100

    feat(FN-1725): fix api test timeout

--- dtfs-central-api/api-test.jest.config.js
+++ dtfs-central-api/api-test.jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,ts}'],
   coverageReporters: ['text', 'text-summary'],
   testMatch: ['**/*.api-test.{js,ts}'],
-  testTimeout: 5000,
+  testTimeout: 80000,
   workerIdleMemoryLimit: '512MB',
   ...commonSettings,
 };
