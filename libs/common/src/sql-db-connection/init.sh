8ed1bd15590e801ac741324c89917a7f51dd69ca
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2024-07-26 14:05:09 +0100

    fix(DTFS2-7292): downgraded MSSQL to 2019

--- libs/common/src/sql-db-connection/init.sh
+++ libs/common/src/sql-db-connection/init.sh
@@ -1,54 +1,42 @@
 #!/bin/bash
 set -m
-
-execute_sql_command() {
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -Q "$1" > /dev/null
-  return $?
-}
-
-get_now_unix_timestamp() {
-  echo $(date +%s)
-}
-
 /opt/mssql/bin/sqlservr &
-
-readonly time_limit_seconds=60
-readonly interval_seconds=5
-readonly start_time=$(get_now_unix_timestamp)
 
 echo "⚡ Initialising MSSQL Server"
 
-# Wait for SQL Server to start
-while ! execute_sql_command "SELECT 1;"
-do
-  current_time=$(get_now_unix_timestamp)
-  elapsed_seconds=$((current_time - start_time))
+# Executes SQL query
+query() {
+  /opt/mssql-tools/bin/sqlcmd -C -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -Q "$1" >/dev/null
+  return $?
+}
 
-  if [ $elapsed_seconds -gt $time_limit_seconds ]; then
-    echo "❌ Error: Failed to create database '$SQL_DB_NAME' - SQL Server did not start within $time_limit_seconds seconds."
-    exit 1
-  fi
+# Initial DB setup
+initialise() {
+  # 1. Database
+  query "CREATE DATABASE [$SQL_DB_NAME];"
+  query "USE [$SQL_DB_NAME];"
 
-  echo "❌ MSSQL failed to start after $elapsed_seconds seconds. Waiting for $interval_seconds more seconds."
-  sleep $interval_seconds
+  # 2. User
+  query "CREATE LOGIN $SQL_DB_USERNAME WITH PASSWORD = '$SQL_DB_PASSWORD';"
+  query "CREATE USER $SQL_DB_USERNAME FOR LOGIN $SQL_DB_USERNAME;"
+
+  # 3. Permissions
+  query "ALTER ROLE db_datareader ADD MEMBER $SQL_DB_USERNAME;"
+  query "ALTER ROLE db_datawriter ADD MEMBER $SQL_DB_USERNAME;"
+  query "ALTER ROLE db_ddladmin ADD MEMBER $SQL_DB_USERNAME;"
+}
+
+# Boot check
+while ! query "SELECT 1;"; do
+  echo "❌ MSSQL boot has failed, retrying..."
+  sleep 3
 done
 
-echo "✅ MSSQL initialised, creating database '$SQL_DB_NAME'..."
+echo "⚠️ MSSQL is now running."
 
-# Create the database
-execute_sql_command "CREATE DATABASE [$SQL_DB_NAME];"
+initialise
 
-echo "⚡Creating user '$SQL_DB_USERNAME'..."
+echo "✅ MSSQL has been initialised."
 
-# Create a new login
-execute_sql_command "CREATE LOGIN $SQL_DB_USERNAME WITH PASSWORD = '$SQL_DB_PASSWORD';"
-
-# Create a new user in the database and map it to the login
-execute_sql_command "USE [$SQL_DB_NAME]; CREATE USER $SQL_DB_USERNAME FOR LOGIN $SQL_DB_USERNAME;"
-
-# Grant the new user required permissions
-execute_sql_command "USE [$SQL_DB_NAME]; ALTER ROLE db_datareader ADD MEMBER $SQL_DB_USERNAME;"
-execute_sql_command "USE [$SQL_DB_NAME]; ALTER ROLE db_datawriter ADD MEMBER $SQL_DB_USERNAME;"
-execute_sql_command "USE [$SQL_DB_NAME]; ALTER ROLE db_ddladmin ADD MEMBER $SQL_DB_USERNAME;"
-
+# Run process in the foreground
 fg
