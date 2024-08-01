287e3c18752eef08e090c469bcf106bd5e74b1bc
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-08-01 12:07:52 +0100

    chore(dtfs2-none): remove underscore

--- libs/common/src/middleware/api-error-handling.middleware.ts
+++ libs/common/src/middleware/api-error-handling.middleware.ts
@@ -6,7 +6,7 @@ import { ApiError } from '../errors';
  * Note: Express 4.x requires the route function to be wrapped in a try catch block
  * and to call `next(error)` to pass the error for this middleware to work
  */
-export const apiErrorHandling: ErrorRequestHandler = (error, _req, res, next) => {
+export const apiErrorHandling: ErrorRequestHandler = (error, req, res, next) => {
   if (error instanceof ApiError) {
     res.status(error.status).json(error.message);
   }
