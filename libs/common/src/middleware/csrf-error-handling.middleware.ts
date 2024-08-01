57ab7128bc6308f7994d2c2a285fb663152d0e17
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-08-01 12:01:58 +0100

    chore(dtfs2-none): remove underscore

--- libs/common/src/middleware/csrf-error-handling.middleware.ts
+++ libs/common/src/middleware/csrf-error-handling.middleware.ts
@@ -1,13 +1,14 @@
+import { HttpStatusCode } from 'axios';
 import { ErrorRequestHandler } from 'express';
 import { isHttpError } from 'http-errors';
 
 /**
  * Frontend middleware that handles CSRF token errors.
  */
-export const csrfErrorHandling: ErrorRequestHandler = (error, _req, res, next) => {
+export const csrfErrorHandling: ErrorRequestHandler = (error, req, res, next) => {
   if (isHttpError(error) && error.code === 'EBADCSRFTOKEN') {
     console.error("The user's CSRF token is incorrect, redirecting the user to /.");
-    res.status(error.statusCode || 500);
+    res.status(error.statusCode || HttpStatusCode.InternalServerError);
     res.redirect('/');
   } else {
     next(error);
