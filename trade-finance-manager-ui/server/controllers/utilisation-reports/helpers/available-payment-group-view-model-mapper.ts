7cdbbc0f0063d757ed81273ed415f5e6b76af403
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-08-01 09:43:58 +0100

    feat(FN-1727): wip - add and populate payment group radios

---
+++ trade-finance-manager-ui/server/controllers/utilisation-reports/helpers/available-payment-group-view-model-mapper.ts
@@ -0,0 +1,20 @@
+import { getFormattedCurrencyAndAmount, SelectedFeeRecordsAvailablePaymentDetails, SelectedFeeRecordsAvailablePaymentGroups } from '@ukef/dtfs2-common';
+import { AvailablePaymentGroupsViewModel, AvailablePaymentViewModelItem } from '../../../types/view-models';
+import { getRadioIdForPaymentGroup } from './available-payment-group-helper';
+
+const mapToExistingPaymentViewModelItem = (payment: SelectedFeeRecordsAvailablePaymentDetails): AvailablePaymentViewModelItem => ({
+  id: payment.id.toString(),
+  formattedCurrencyAndAmount: getFormattedCurrencyAndAmount({ currency: payment.currency, amount: payment.amount }),
+  reference: payment.reference,
+});
+
+export const mapToAvailablePaymentGroupViewModel = (availablePaymentGroupsData: SelectedFeeRecordsAvailablePaymentGroups): AvailablePaymentGroupsViewModel => {
+  if (!availablePaymentGroupsData) {
+    return [];
+  }
+
+  return availablePaymentGroupsData.map((paymentGroup) => ({
+    radioId: getRadioIdForPaymentGroup(paymentGroup),
+    payments: paymentGroup.map((payment) => mapToExistingPaymentViewModelItem(payment)),
+  }));
+};
