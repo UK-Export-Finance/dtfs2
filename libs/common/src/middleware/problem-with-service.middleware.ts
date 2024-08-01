ca0142a7d8a6074d18c9939b835e310026cec33a
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-08-01 12:11:51 +0100

    chore(dtfs2-none): remove underscore

--- libs/common/src/middleware/problem-with-service.middleware.ts
+++ libs/common/src/middleware/problem-with-service.middleware.ts
@@ -3,6 +3,6 @@ import { ErrorRequestHandler } from 'express';
 /**
  * Frontend middleware that renders problem with service if other middlewares do not handle the error.
  */
-export const problemWithService: ErrorRequestHandler = (error: { message?: string }, _req, res) => {
+export const problemWithService: ErrorRequestHandler = (error: { message?: string }, req, res) => {
   res.render('partials/problem-with-service.njk', { error });
 };
