63e9723ad6312255d64473981fd0d4f7f55d34f5
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-08-05 10:06:16 +0100

    feat(FN-1727): move payment group radio ID generation to mapper file

--- trade-finance-manager-ui/server/controllers/utilisation-reports/helpers/available-payment-group-helper.ts
+++
@@ -1,7 +0,0 @@
-import { SelectedFeeRecordsAvailablePaymentDetails } from '@ukef/dtfs2-common';
-import { AddToAnExistingPaymentRadioId } from '../../../types/add-to-an-existing-payment-radio-id';
-
-export const getRadioIdForPaymentGroup = (payments: SelectedFeeRecordsAvailablePaymentDetails[]): AddToAnExistingPaymentRadioId => {
-  const paymentIdList = payments.map(({ id }) => id).join(',');
-  return `paymentIds-${paymentIdList}`;
-};
--- trade-finance-manager-ui/server/controllers/utilisation-reports/helpers/available-payment-group-view-model-mapper.ts
+++ trade-finance-manager-ui/server/controllers/utilisation-reports/helpers/available-payment-group-view-model-mapper.ts
@@ -1,6 +1,11 @@
 import { getFormattedCurrencyAndAmount, SelectedFeeRecordsAvailablePaymentDetails, SelectedFeeRecordsAvailablePaymentGroups } from '@ukef/dtfs2-common';
 import { AvailablePaymentGroupsViewModel, AvailablePaymentViewModelItem } from '../../../types/view-models';
-import { getRadioIdForPaymentGroup } from './available-payment-group-helper';
+import { AddToAnExistingPaymentRadioId } from '../../../types/add-to-an-existing-payment-radio-id';
+
+const getRadioIdForPaymentGroup = (payments: SelectedFeeRecordsAvailablePaymentDetails[]): AddToAnExistingPaymentRadioId => {
+  const paymentIdList = payments.map(({ id }) => id).join(',');
+  return `paymentIds-${paymentIdList}`;
+};
 
 const mapToAvailablePaymentViewModelItem = (payment: SelectedFeeRecordsAvailablePaymentDetails): AvailablePaymentViewModelItem => ({
   id: payment.id.toString(),
