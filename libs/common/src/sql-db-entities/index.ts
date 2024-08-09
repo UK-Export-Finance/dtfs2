2afee2683a55db31f430969360f3bb44d426c48c
JoshBinns2000 <108285982+JoshBinns2000@users.noreply.github.com>
2024-08-09 11:06:19 +0100

    feat(FN-3187): create entity for fee record payment join table (#3382)

--- libs/common/src/sql-db-entities/index.ts
+++ libs/common/src/sql-db-entities/index.ts
@@ -5,4 +5,5 @@ export { PaymentEntity } from './payment';
 export { MonthAndYearPartialEntity, ReportPeriodPartialEntity } from './partial-entities';
 export { FacilityUtilisationDataEntity } from './facility-utilisation-data';
 export { PaymentMatchingToleranceEntity } from './payment-matching-tolerance';
+export { FeeRecordPaymentJoinTableEntity } from './join-tables';
 export * from './helpers';
---
+++ libs/common/src/sql-db-entities/join-tables/fee-record-payment/index.ts
@@ -0,0 +1,1 @@
+export * from './fee-record-payment.join-table.entity';
