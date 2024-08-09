28ea470d1beee499732e25cd8687636ad3e1841e
Joshua Binns <joshua.binns@softwire.com>
2024-08-09 09:07:02 +0100

    feat(FN-3187): create entity for fee record payment join table

---
+++ libs/common/src/sql-db-entities/join-tables/fee-record-payments-payment/fee-record-payments-payment.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, ManyToOne, JoinColumn, PrimaryColumn, Index } from 'typeorm';
+import { FeeRecordEntity } from '../../fee-record';
+import { PaymentEntity } from '../../payment';
+
+@Entity('fee_record_payments_payment')
+export class FeeRecordPaymentJoinTableEntity {
+  @PrimaryColumn({ nullable: false })
+  @Index('IDX_7a9b7aa849fc3bb09d80fa1f81')
+  feeRecordId!: number;
+
+  @ManyToOne(() => FeeRecordEntity, (feeRecord) => feeRecord.id, {
+    eager: false,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'feeRecordId', foreignKeyConstraintName: 'FK_7a9b7aa849fc3bb09d80fa1f812' })
+  feeRecord!: FeeRecordEntity;
+
+  @PrimaryColumn({ nullable: false })
+  @Index('IDX_23bbb10be5f2136a5a5086654e')
+  paymentId!: number;
+
+  @ManyToOne(() => PaymentEntity, (payment) => payment.id, {
+    eager: false,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'paymentId', foreignKeyConstraintName: 'FK_23bbb10be5f2136a5a5086654e8' })
+  payment!: PaymentEntity;
+}
---
+++ libs/common/src/sql-db-entities/join-tables/fee-record-payments-payment/index.ts
@@ -0,0 +1,1 @@
+export * from './fee-record-payments-payment.entity';
