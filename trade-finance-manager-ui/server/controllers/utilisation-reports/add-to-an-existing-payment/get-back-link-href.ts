6e28e55f0dddd79d87b5c636f1bdec4d509a857c
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-08-20 15:57:55 +0100

    feat(FN-3293): rename base url char count var naming

--- trade-finance-manager-ui/server/controllers/utilisation-reports/add-to-an-existing-payment/get-back-link-href.ts
+++ trade-finance-manager-ui/server/controllers/utilisation-reports/add-to-an-existing-payment/get-back-link-href.ts
@@ -1,6 +1,6 @@
 // "Recommended Security and Performance Max: 2048 CHARACTERS" - https://stackoverflow.com/a/48230425
 const MAX_URL_CHARACTERS = 2048;
-const ESTIMATED_BASE_URL_CHARACTERS = 100;
+const BASE_URL_CHARACTER_ALLOWANCE = 100;
 
 export const getBackLinkUrl = (reportId: string, feeRecordIds: number[]): string => {
   const urlWithoutParams = `/utilisation-reports/${reportId}`;
@@ -17,7 +17,7 @@ export const getBackLinkUrl = (reportId: string, feeRecordIds: number[]): string
 
   const urlWithParams = `${urlWithoutParams}?${feeRecordIdsURLParams}`;
 
-  const urlWithParamsMaxLength = MAX_URL_CHARACTERS - ESTIMATED_BASE_URL_CHARACTERS;
+  const urlWithParamsMaxLength = MAX_URL_CHARACTERS - BASE_URL_CHARACTER_ALLOWANCE;
   if (urlWithParams.length > urlWithParamsMaxLength) {
     console.error(
       `Back link URL with params exceeds maximum length (${urlWithParams.length} > ${urlWithParamsMaxLength}). Falling back to URL without params.`,
