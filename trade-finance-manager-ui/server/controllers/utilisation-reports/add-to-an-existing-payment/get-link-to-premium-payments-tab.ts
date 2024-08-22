ffa5d53b7da385d610010737c20e6dcd6c020b10
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-08-22 14:08:48 +0100

    feat(FN-3293): make vars inline & add docs to PP link gen

--- trade-finance-manager-ui/server/controllers/utilisation-reports/add-to-an-existing-payment/get-link-to-premium-payments-tab.ts
+++ trade-finance-manager-ui/server/controllers/utilisation-reports/add-to-an-existing-payment/get-link-to-premium-payments-tab.ts
@@ -1,7 +1,11 @@
-// "Recommended Security and Performance Max: 2048 CHARACTERS" - https://stackoverflow.com/a/48230425
-const MAX_URL_CHARACTERS = 2048;
-const BASE_URL_CHARACTER_ALLOWANCE = 150;
-
+/**
+ * Generates a link to the Premium Payments tab for a given utilisation report,
+ * including selected fee record IDs as query parameters if within URL length limit.
+ *
+ * @param reportId - The ID of the utilisation report.
+ * @param selectedFeeRecordIds - An array of selected fee record IDs to include in the URL.
+ * @returns A string representing the URL for the Premium Payments tab.
+ */
 export const getLinkToPremiumPaymentsTab = (reportId: string, selectedFeeRecordIds: number[]): string => {
   const urlWithoutParams = `/utilisation-reports/${reportId}`;
 
@@ -17,7 +21,10 @@ export const getLinkToPremiumPaymentsTab = (reportId: string, selectedFeeRecordI
 
   const urlWithParams = `${urlWithoutParams}?${urlParams}`;
 
-  const urlWithParamsMaxLength = MAX_URL_CHARACTERS - BASE_URL_CHARACTER_ALLOWANCE;
+  // "Recommended Security and Performance Max: 2048 CHARACTERS" - https://stackoverflow.com/a/48230425
+  const maxUrlCharacters = 2048;
+  const baseUrlCharacterAllowance = 150;
+  const urlWithParamsMaxLength = maxUrlCharacters - baseUrlCharacterAllowance;
   if (urlWithParams.length > urlWithParamsMaxLength) {
     // Passing the IDs as query params should handle up to ~189 seven digit IDs before we reach this limit.
     // (2048 - 150) = 1898 chars, 1898 / (7 digits per ID + 3 chars for percent-encoded comma) = ~189 seven digit IDs.
