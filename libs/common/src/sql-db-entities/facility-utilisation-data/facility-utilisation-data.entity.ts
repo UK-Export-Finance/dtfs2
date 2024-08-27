b97efc29a14474f67cb5e7f93f682303cbc0cd64
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-08-27 11:49:13 +0100

    feat(FN-3274): add missing docstrings to facility utilisation docstring

--- libs/common/src/sql-db-entities/facility-utilisation-data/facility-utilisation-data.entity.ts
+++ libs/common/src/sql-db-entities/facility-utilisation-data/facility-utilisation-data.entity.ts
@@ -9,8 +9,14 @@ import {
   UpdateWithCurrentReportPeriodDetailsParams,
 } from './facility-utilisation-data.types';
 
+/**
+ * Entity representing facility utilisation data
+ */
 @Entity('FacilityUtilisationData')
 export class FacilityUtilisationDataEntity extends AuditableBaseEntity {
+  /**
+   * Primary key for the entity
+   */
   @PrimaryColumn({ type: 'nvarchar', length: '10' })
   id!: string;
 
@@ -41,6 +47,16 @@ export class FacilityUtilisationDataEntity extends AuditableBaseEntity {
   @MonetaryColumn({ defaultValue: 0 })
   fixedFee!: number;
 
+  /**
+   * Creates a new FacilityUtilisationDataEntity
+   * @param facilityUtilisationData - Parameters for creating the entity
+   * @param facilityUtilisationData.id - The ID of the entity
+   * @param facilityUtilisationData.reportPeriod - The report period details
+   * @param facilityUtilisationData.utilisation - The facility utilisation
+   * @param facilityUtilisationData.fixedFee - The facility fixed fee
+   * @param facilityUtilisationData.requestSource - The source of the request
+   * @returns {FacilityUtilisationDataEntity}
+   */
   public static create({ id, reportPeriod, utilisation, fixedFee, requestSource }: CreateFacilityUtilisationDataParams): FacilityUtilisationDataEntity {
     const data = new FacilityUtilisationDataEntity();
     data.id = id;
@@ -51,6 +67,14 @@ export class FacilityUtilisationDataEntity extends AuditableBaseEntity {
     return data;
   }
 
+  /**
+   * Creates a new FacilityUtilisationDataEntity without utilisation and fixed fee
+   * @param facilityUtilisationData - Parameters for creating the entity
+   * @param facilityUtilisationData.id - The ID of the entity
+   * @param facilityUtilisationData.reportPeriod - The report period details
+   * @param facilityUtilisationData.requestSource - The source of the request
+   * @returns {FacilityUtilisationDataEntity}
+   */
   public static createWithoutUtilisationAndFixedFee({
     id,
     reportPeriod,
@@ -63,6 +87,14 @@ export class FacilityUtilisationDataEntity extends AuditableBaseEntity {
     return data;
   }
 
+  /**
+   * Updates the entity with current report period details
+   * @param facilityUtilisationData - Parameters for updating the entity
+   * @param facilityUtilisationData.fixedFee - The updated fixed fee
+   * @param facilityUtilisationData.utilisation - The updated utilisation
+   * @param facilityUtilisationData.reportPeriod - The updated report period details
+   * @param facilityUtilisationData.requestSource - The source of the update request
+   */
   public updateWithCurrentReportPeriodDetails({ fixedFee, utilisation, reportPeriod, requestSource }: UpdateWithCurrentReportPeriodDetailsParams): void {
     this.fixedFee = fixedFee;
     this.utilisation = utilisation;
