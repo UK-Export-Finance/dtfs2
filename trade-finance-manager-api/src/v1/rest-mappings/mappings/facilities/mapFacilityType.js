d4814935c7ecd91a3893404c2dd6f7658c13994e
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-08-29 14:46:52 +0100

    feat(dtfs2-7315): update map facility type imports and add docs

--- trade-finance-manager-api/src/v1/rest-mappings/mappings/facilities/mapFacilityType.js
+++ trade-finance-manager-api/src/v1/rest-mappings/mappings/facilities/mapFacilityType.js
@@ -1,6 +1,29 @@
-const { FACILITY_TYPE, FACILITY_TYPE_MAPPED } = require('@ukef/dtfs2-common');
+const { FACILITY_TYPE, MAPPED_FACILITY_TYPE } = require('@ukef/dtfs2-common');
 const CONSTANTS = require('../../../../constants');
 
+/**
+ * @description Maps facility type to a different, tfm-api specific facility type.
+ * Note there is a difference between this facility type, the input facility type,
+ * and how the facility type is displayed in the front end.
+ *
+ * @returns {string} the mapped facility type
+ *
+ * @example
+ * // returns 'Bid Bond'
+ * mapFacilityType({...facility, code: 'BSS', bondType: 'Bid Bond'})
+ *
+ * @example
+ * // returns 'Loan'
+ * mapFacilityType({...facility, code: 'EWCS'})
+ *
+ * @example
+ * // returns 'Cash Facility'
+ * mapFacilityType({...facility, ukefFacilityType: 'Cash'})
+ *
+ * @example
+ * // returns 'Contingent Facility'
+ * mapFacilityType({...facility, ukefFacilityType: 'Contingent'})
+ */
 const mapFacilityType = (facility) => {
   const { facilityProduct, ukefFacilityType } = facility;
 
@@ -9,15 +32,15 @@ const mapFacilityType = (facility) => {
   }
 
   if (facilityProduct.code === CONSTANTS.FACILITIES.FACILITY_PRODUCT_CODE.LOAN) {
-    return FACILITY_TYPE_MAPPED.LOAN;
+    return MAPPED_FACILITY_TYPE.LOAN;
   }
 
   if (ukefFacilityType === FACILITY_TYPE.CASH) {
-    return FACILITY_TYPE_MAPPED.CASH;
+    return MAPPED_FACILITY_TYPE.CASH;
   }
 
   if (ukefFacilityType === FACILITY_TYPE.CONTINGENT) {
-    return FACILITY_TYPE_MAPPED.CONTINGENT;
+    return MAPPED_FACILITY_TYPE.CONTINGENT;
   }
 
   return null;
