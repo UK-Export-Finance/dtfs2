02d4412dc72fb801aae13b381ba7c39abe6b8c81
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-08-30 17:45:57 +0100

    feat(dtfs2-7315): update comments on mapped facility type

--- libs/common/src/constants/facility-type-mapped.ts
+++ libs/common/src/constants/facility-type-mapped.ts
@@ -3,14 +3,16 @@
  * in the file 'mapFacilityType.js'.
  *
  * They are slightly different from the usual facility types and are tech debt.
+ * They should not be introduced anywhere else.
  *
- * These are subtly different from how we format types for user facing areas (ie bond),
- * and if making user facing facility types, a different constant should be used
+ * These types are subtly different from how we format types for user facing areas (ie bonds are formatted differently),
+ * and if making user facing facility types, this constant should not be used.
  */
 export const MAPPED_FACILITY_TYPE = {
-  // Bond is derived from bondType in these mappers and is deliberately omitted here
-  // Loan is identical to facility type, but is provided to avoid confusion
-  // if types are ever introduced fully and driven from the constant
+  // Bond is derived from bondType in these mappers and
+  // is deliberately omitted here as it cannot be determined due to lacking bondType constants
+  // Loan is identical to facility type, but is provided to avoid confusion if
+  // types are ever introduced fully and need to use this constant
   LOAN: 'Loan',
   CASH: 'Cash facility',
   CONTINGENT: 'Contingent facility',
