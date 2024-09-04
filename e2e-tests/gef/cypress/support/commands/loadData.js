877b4c7c62002c633d7e19422b2f94c45477f7cf
Alex <47955140+AlexBramhill@users.noreply.github.com>
2024-09-04 16:17:18 +0100

    feat(dtfs2-6892): remove failOnNonZeroExit: false from gef load data script in e2e tests

--- e2e-tests/gef/cypress/support/commands/loadData.js
+++ e2e-tests/gef/cypress/support/commands/loadData.js
@@ -6,7 +6,7 @@
 
 const loadData = () => {
   // Ensure project root directory
-  cy.exec('if [ -d "dtfs2" ]; then cd dtfs2; fi', { failOnNonZeroExit: false });
+  cy.exec('if [ -d "dtfs2" ]; then cd dtfs2; fi');
   // Load mock data
   cy.exec('cd ../../utils && npm run mock-data-loader');
 };
