77c0ecdc0db2266d9bbe32600dd8cc6db1210a8a
ttbarnes <tony@tonybarnes.me>
2024-09-04 17:23:25 +0100

    chore(DTFS2-7328): fix/update e2e test

--- e2e-tests/gef/cypress/e2e/facility/unissued-facilities/unissued-facilities-change-all-MIN.spec.js
+++ e2e-tests/gef/cypress/e2e/facility/unissued-facilities/unissued-facilities-change-all-MIN.spec.js
@@ -130,10 +130,6 @@ context('Unissued Facilities MIN - change all to issued from unissued table', ()
       errorSummary().contains('Select if you want UKEF cover to start on the day you issue the facility');
       errorSummary().contains('Enter a cover end date');
 
-      if (facilityEndDateEnabled) {
-        aboutFacilityUnissued.isUsingFacilityEndDateError().contains('Select if there is an end date for this facility');
-      }
-
       // entering date in the past for issue date
       aboutFacilityUnissued.issueDateDay().type(dateConstants.fourDaysAgoDay);
       aboutFacilityUnissued.issueDateMonth().type(dateConstants.fourDaysAgoMonth);
