3461b73b8a28092d7b75767ae4e8a830b3302e51
Vamshidhar Reddy <vreddy@ukexportfinance.gov.uk>
2024-09-04 16:39:22 +0100

    fix(DTFS2-6655): resolved comments on PR

--- e2e-tests/portal/cypress/support/portal/createBssApplicationReadyForCheck.js
+++ e2e-tests/portal/cypress/support/portal/createBssApplicationReadyForCheck.js
@@ -1,55 +1,76 @@
-const dashboard = require('../../e2e/pages/dashboard');
-const selectScheme = require('../../e2e/pages/selectScheme');
-const beforeYouStart = require('../../e2e/pages/beforeYouStart');
-const bankDetails = require('../../e2e/pages/bankDetails');
-const contract = require('../../e2e/pages/contract');
-const aboutSupplier = require('../../e2e/pages/contract/about/about-supplier');
-const aboutBuyer = require('../../e2e/pages/contract/about/about-buyer');
-const aboutFinancial = require('../../e2e/pages/contract/about/about-financial');
-const eligibilityCriteria = require('../../e2e/pages/eligibilityCriteria');
-const eligibilityPreview = require('../../e2e/pages/eligibilityPreview');
-const bondDetails = require('../../e2e/pages/bondDetails');
-const bondFinancialDetails = require('../../e2e/pages/bondFinancialDetails');
-const bondFeeDetails = require('../../e2e/pages/bondFeeDetails');
-const contractReadyForReview = require('../../e2e/pages/contract/contract-readyForReview');
+// This file is used to create a BSS application that is ready for check
+const {
+  dashboard,
+  selectScheme,
+  beforeYouStart,
+  bankDetails,
+  contract,
+  eligibilityCriteria,
+  eligibilityPreview,
+  bondDetails,
+  bondFinancialDetails,
+  bondFeeDetails,
+  contractReadyForReview,
+} = require('../../e2e/pages');
+const { aboutSupplier, aboutBuyer, aboutFinancial } = require('../../e2e/pages/contract');
 const login = require('./login');
 const MOCK_USERS = require('../../../../e2e-fixtures/index');
 
 const { BANK1_MAKER1 } = MOCK_USERS;
 
 const createBssApplicationReadyForCheck = () => {
+  // Log in as BANK1_MAKER1
   login(BANK1_MAKER1);
+
+  // Navigate to create a new BSS deal
   dashboard.createNewSubmission().click();
+
+  // Select BSS scheme
   selectScheme.bss().click();
   selectScheme.continue().click();
+
+  // Select True before starting the application
   beforeYouStart.true().click();
   beforeYouStart.submit().click();
-  bankDetails.bankDealId().type('12345678');
-  bankDetails.bankDealName().type('BSS Deal');
+
+  // Fill in bank deal id, bank name
+  bankDetails.bankDealId().type('12345678', { delay: 0 });
+  bankDetails.bankDealName().type('BSS Deal', { delay: 0 });
   bankDetails.submit().click();
+
+  // Select Supplier Details Link
   contract.aboutSupplierDetailsLink().click();
+
+  // Fill in details about supplier
   aboutSupplier.supplierType().select('Exporter');
-  aboutSupplier.supplierCompaniesHouseRegistrationNumber().type('12345678');
+  aboutSupplier.supplierCompaniesHouseRegistrationNumber().type('12345678', { delay: 0 });
   aboutSupplier.supplierSearchCompaniesHouse().click();
   aboutSupplier.supplierAddress().country().select('United Kingdom');
   aboutSupplier.supplierCorrespondenceAddressSame().click();
   aboutSupplier.smeTypeSmall().click();
-  aboutSupplier.supplyContractDescription().type('Supply Contract Description');
+  aboutSupplier.supplyContractDescription().type('Supply Contract Description', { delay: 0 });
   aboutSupplier.notLegallyDistinct().click();
   aboutSupplier.nextPage().click();
-  aboutBuyer.buyerName().type('Buyer Name');
+
+  // Fill in details about buyer
+  aboutBuyer.buyerName().type('Buyer Name', { delay: 0 });
   aboutBuyer.buyerAddress().country().select('United Kingdom');
-  aboutBuyer.buyerAddress().line1().type('Line 1');
-  aboutBuyer.buyerAddress().line2().type('Line 2');
-  aboutBuyer.buyerAddress().line3().type('Line 3');
-  aboutBuyer.buyerAddress().town().type('Town');
-  aboutBuyer.buyerAddress().postcode().type('AB1 2CD');
+  aboutBuyer.buyerAddress().line1().type('Line 1', { delay: 0 });
+  aboutBuyer.buyerAddress().line2().type('Line 2', { delay: 0 });
+  aboutBuyer.buyerAddress().line3().type('Line 3', { delay: 0 });
+  aboutBuyer.buyerAddress().town().type('Town', { delay: 0 });
+  aboutBuyer.buyerAddress().postcode().type('AB1 2CD', { delay: 0 });
   aboutBuyer.destinationOfGoodsAndServices().select('United Kingdom');
   aboutBuyer.nextPage().click();
-  aboutFinancial.supplyContractValue().type('100000');
+
+  // Fill in financial details
+  aboutFinancial.supplyContractValue().type('100000', { delay: 0 });
   aboutFinancial.supplyContractCurrency().select('GBP - UK Sterling');
   aboutFinancial.saveAndGoBack().click();
+
   contract.eligibilityCriteriaLink().click();
+
+  // Select automatic cover details
   eligibilityCriteria.eligibilityCriteriaTrue(11).click();
   eligibilityCriteria.eligibilityCriteriaTrue(12).click();
   eligibilityCriteria.eligibilityCriteriaTrue(13).click();
@@ -59,22 +80,33 @@ const createBssApplicationReadyForCheck = () => {
   eligibilityCriteria.eligibilityCriteriaTrue(17).click();
   eligibilityCriteria.eligibilityCriteriaTrue(18).click();
   eligibilityCriteria.nextPageButton().click();
+
   eligibilityPreview.saveGoBackButton().click();
+
   contract.addBondButton().click();
+
+  // Fill in bond details
   bondDetails.bondTypeInput().select('Advance payment guarantee');
   bondDetails.facilityStageUnissuedInput().click();
-  bondDetails.ukefGuaranteeInMonthsInput().type('12');
+  bondDetails.ukefGuaranteeInMonthsInput().type('12', { delay: 0 });
   bondDetails.submit().click();
-  bondFinancialDetails.facilityValueInput().type('100000');
+
+  // Fill in bond financial details
+  bondFinancialDetails.facilityValueInput().type('100000', { delay: 0 });
   bondFinancialDetails.currencySameAsSupplyContractCurrencyYesInput().click();
-  bondFinancialDetails.riskMarginFeeInput().type('10');
-  bondFinancialDetails.coveredPercentageInput().type('80');
+  bondFinancialDetails.riskMarginFeeInput().type('10', { delay: 0 });
+  bondFinancialDetails.coveredPercentageInput().type('80', { delay: 0 });
   bondFinancialDetails.submit().click();
+
+  // Fill in bond fee details
   bondFeeDetails.feeTypeAtMaturityInput().click();
   bondFeeDetails.dayCountBasis365Input().click();
   bondFeeDetails.saveGoBackButton().click();
+
   contract.proceedToReview().click();
-  contractReadyForReview.comments().type('Ready for checkers approval');
+
+  // Submit contract for review
+  contractReadyForReview.comments().type('Ready for checkers approval', { delay: 0 });
   contractReadyForReview.readyForCheckersApproval().click();
 };
 
