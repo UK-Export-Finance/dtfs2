112ac6a358c22fd1c957fe22f67249441df76341
Joshua Binns <joshua.binns@softwire.com>
2024-09-06 15:19:22 +0100

    feat(FN-2310): e2e test fix

--- e2e-tests/tfm/cypress/e2e/pages/utilisation-reports/utilisationReportPage.js
+++ e2e-tests/tfm/cypress/e2e/pages/utilisation-reports/utilisationReportPage.js
@@ -3,7 +3,7 @@ const utilisationReportPage = {
   keyingSheetTabLink: () => cy.get('a[data-cy="bank-report-tab-keying-sheet"]'),
   premiumPaymentsTab: {
     getPaymentLink: (paymentId) => cy.get(`a[data-cy="premium-payments-tab-edit-payment-link--paymentId-${paymentId}"]`),
-    clickPaymentLink: (paymentId) => cy.get(`a[data-cy="edit-payment-link--paymentId-${paymentId}"]`).click(),
+    clickPaymentLink: (paymentId) => cy.get(`a[data-cy="premium-payments-tab-edit-payment-link--paymentId-${paymentId}"]`).click(),
     getFacilityIdFilterInput: () => cy.getInputByLabelText('Filter by facility ID'),
     submitFacilityIdFilter: () => cy.get('button[data-cy="facility-filter-submit-button"]').click(),
     generateKeyingDataButton: () => cy.get('[data-cy="generate-keying-data-button"]'),
