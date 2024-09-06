5b92fbd6aaa80f1df26c73ce80c08ee93d996516
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-06 11:13:07 +0100

    feat(dtfs2-6892): refactor get function with guard clause, update return type

--- trade-finance-manager-api/src/v1/controllers/auth/auth-service/get-existing-tfm-user.js
+++ trade-finance-manager-api/src/v1/controllers/auth/auth-service/get-existing-tfm-user.js
@@ -4,30 +4,30 @@ const userController = require('../../user/user.controller');
  * get
  * Get an existing TFM user with a matching Entra use email(s)
  * Note: an Entra user could have just a primary email, no secondary email.
- * @param {import('src/types/auth/azure-user-info-response-account').AzureUserInfoResponseAccount} entraUser: Entra user data
- * @returns {Promise<import('src/types/auth/get-user-response').GetUserResponse>}
+ * @param {import('src/types/auth/azure-user-info-response-account').AzureUserInfoResponseAccount} entraUser Entra user data
+ * @returns {Promise<import('src/types/auth/get-user-response').GetUserResponse | {}>}
  */
 const get = async (entraUser) => {
+  if (!entraUser || !entraUser.idTokenClaims) {
+    return {};
+  }
+
   try {
-    if (entraUser && entraUser.idTokenClaims) {
-      console.info('TFM auth service - Getting existing TFM user');
+    console.info('TFM auth service - Getting existing TFM user');
 
-      /** @constant {import('src/types/auth/azure-user-info-response-account').AzureIdTokenClaims} Entra claims */
-      const claims = entraUser.idTokenClaims;
+    /** @constant {import('src/types/auth/azure-user-info-response-account').AzureIdTokenClaims} Entra claims */
+    const claims = entraUser.idTokenClaims;
 
-      // verified_primary_email is always array.
-      let emails = claims.verified_primary_email;
-      // verified_primary_email is always array.
-      if (claims.verified_secondary_email) {
-        emails = [...emails, ...claims.verified_secondary_email];
-      }
-
-      const getUserResponse = await userController.findByEmails(emails);
-
-      return getUserResponse;
+    // verified_primary_email is always array.
+    let emails = claims.verified_primary_email;
+    // verified_primary_email is always array.
+    if (claims.verified_secondary_email) {
+      emails = [...emails, ...claims.verified_secondary_email];
     }
 
-    return {};
+    const getUserResponse = await userController.findByEmails(emails);
+
+    return getUserResponse;
   } catch (error) {
     console.error('TFM auth service - Error - Getting existing TFM user %O', error);
 
