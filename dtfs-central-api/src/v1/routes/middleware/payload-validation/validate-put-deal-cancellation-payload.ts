49772a573d0758d4c0458e96fd002a22aa87b095
Beth Thomas <beth.thomas@softwire.com>
2024-09-10 16:36:17 +0100

    feat(DTFS2-7294): update zod schema for additional type safety

--- dtfs-central-api/src/v1/routes/middleware/payload-validation/validate-put-deal-cancellation-payload.ts
+++ dtfs-central-api/src/v1/routes/middleware/payload-validation/validate-put-deal-cancellation-payload.ts
@@ -1,8 +1,9 @@
 import z from 'zod';
+import { AuditDetails, TfmDealCancellation } from '@ukef/dtfs2-common';
 import { AuditDetailsSchema } from './schemas';
 import { createValidationMiddlewareForSchema } from './create-validation-middleware-for-schema.ts';
 
-const PutDealCancellationSchema = z.object({
+const PutDealCancellationSchema: z.ZodType<{ dealCancellationUpdate: Partial<TfmDealCancellation>; auditDetails: AuditDetails }> = z.object({
   dealCancellationUpdate: z
     .object({
       reason: z.string(),
