5a6d7146ee426d39933f22974b4df14218b719e1
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2024-09-10 18:24:50 +0100

    fix(DTFS2-6713): estore buyer, deal, facilities and documents upload (#3390)
    Co-authored-by: root <root@UKEF-DRTL224>

--- external-api/src/helpers/types/estore.ts
+++ external-api/src/helpers/types/estore.ts
@@ -15,11 +15,12 @@ export type EstoreRequest = CustomExpressRequest<{
  *
  * Possible values for this type are:
  * - 'sites'
+ * - 'terms/facilities'
  * - 'buyer'
  * - 'deals'
- * - 'terms/facilities'
+ * - 'facilities'
  * - 'documents'
  *
  * This type is used to specify valid categories within the e-store.
  */
-export type Category = 'sites' | 'buyer' | 'deals' | 'terms/facilities' | 'documents';
+export type Category = 'sites' | 'terms/facilities' | 'buyers' | 'deals' | 'facilities' | 'documents';
