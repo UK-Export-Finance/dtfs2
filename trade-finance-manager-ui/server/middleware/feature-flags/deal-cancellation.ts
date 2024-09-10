753d0316ae1f66247e9bf705c570742044321a1c
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-09-10 10:45:34 +0100

    feat(DTFS2-7348): add middleware to route

---
+++ trade-finance-manager-ui/server/middleware/feature-flags/deal-cancellation.ts
@@ -0,0 +1,14 @@
+import { RequestHandler } from 'express';
+import { isTfmFacilityEndDateFeatureFlagEnabled } from '@ukef/dtfs2-common/';
+
+/**
+ * Middleware to check if the deal cancellation feature flag is enabled
+ */
+export const validateDealCancellationEnabled: RequestHandler = (_req, res, next) => {
+  // TODO: DTFS2-7348 use the correct Feature Flag here
+  if (!isTfmFacilityEndDateFeatureFlagEnabled()) {
+    return res.redirect(404, '/not-found');
+  }
+
+  return next();
+};
--- trade-finance-manager-ui/server/routes/case/cancellation.ts
+++ trade-finance-manager-ui/server/routes/case/cancellation.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+import { TEAM_IDS } from '@ukef/dtfs2-common';
 import { getReasonForCancellation } from '../../controllers/case/cancellation/reason-for-cancellation.controller';
+import { validateUserTeam } from '../../middleware';
+import { validateDealCancellationEnabled } from '../../middleware/feature-flags/deal-cancellation';
 
 export const cancellationRouter = Router();
 
-cancellationRouter.route('/reason').get(getReasonForCancellation);
+cancellationRouter.use(validateDealCancellationEnabled, validateUserTeam([TEAM_IDS.PIM]));
+
+cancellationRouter.get('/reason', getReasonForCancellation);
