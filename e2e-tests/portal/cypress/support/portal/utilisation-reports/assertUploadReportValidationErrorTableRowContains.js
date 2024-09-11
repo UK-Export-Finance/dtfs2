4959f5558c6895df7f68d4e8515005678f87239a
Ruby Lavender <ruby.lavender@softwire.com>
2024-09-11 12:28:37 +0100

    feat(FN-3162): use assertText helper

--- e2e-tests/portal/cypress/support/portal/utilisation-reports/assertUploadReportValidationErrorTableRowContains.js
+++ e2e-tests/portal/cypress/support/portal/utilisation-reports/assertUploadReportValidationErrorTableRowContains.js
@@ -1,34 +1,9 @@
 const { utilisationReportUpload } = require('../../../e2e/pages');
 
 module.exports = ({ tableRowIndex, message, exporter, row, column, entry }) => {
-  utilisationReportUpload
-    .validationErrorMessage(tableRowIndex)
-    .invoke('text')
-    .then((text) => {
-      expect(text.trim()).to.equal(message);
-    });
-  utilisationReportUpload
-    .validationErrorExporter(tableRowIndex)
-    .invoke('text')
-    .then((text) => {
-      expect(text.trim()).to.equal(exporter ?? '-');
-    });
-  utilisationReportUpload
-    .validationErrorRow(tableRowIndex)
-    .invoke('text')
-    .then((text) => {
-      expect(text.trim()).to.equal(row ?? '-');
-    });
-  utilisationReportUpload
-    .validationErrorColumn(tableRowIndex)
-    .invoke('text')
-    .then((text) => {
-      expect(text.trim()).to.equal(column ?? '-');
-    });
-  utilisationReportUpload
-    .validationErrorValue(tableRowIndex)
-    .invoke('text')
-    .then((text) => {
-      expect(text.trim()).to.equal(entry ?? '');
-    });
+  cy.assertText(utilisationReportUpload.validationErrorMessage(tableRowIndex), message);
+  cy.assertText(utilisationReportUpload.validationErrorExporter(tableRowIndex), exporter ?? '-');
+  cy.assertText(utilisationReportUpload.validationErrorRow(tableRowIndex), row ?? '-');
+  cy.assertText(utilisationReportUpload.validationErrorColumn(tableRowIndex), column ?? '-');
+  cy.assertText(utilisationReportUpload.validationErrorValue(tableRowIndex), entry ?? '');
 };
