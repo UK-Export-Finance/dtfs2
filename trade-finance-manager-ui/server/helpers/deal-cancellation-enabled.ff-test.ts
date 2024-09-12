91e4786497c82bcea2f29816c5f278d686bd5a2e
Beth Thomas <beth.thomas@softwire.com>
2024-09-12 09:42:22 +0100

    feat(DTFS2-7294): refactor unit test to use new ff pattern

---
+++ trade-finance-manager-ui/server/helpers/deal-cancellation-enabled.ff-test.ts
@@ -0,0 +1,24 @@
+import { DEAL_SUBMISSION_TYPE } from '@ukef/dtfs2-common';
+import { dealCancellationEnabled } from './deal-cancellation-enabled';
+
+describe('dealCancellationEnabled', () => {
+  describe('when `FF_TFM_FACILITY_END_DATE_ENABLED` is set to false', () => {
+    it('should return false if the deal type is MIA', () => {
+      const result = dealCancellationEnabled(DEAL_SUBMISSION_TYPE.MIA);
+
+      expect(result).toEqual(false);
+    });
+
+    it('should return true if the deal type is MIN', () => {
+      const result = dealCancellationEnabled(DEAL_SUBMISSION_TYPE.MIN);
+
+      expect(result).toEqual(true);
+    });
+
+    it('should return true if the deal type is AIN', () => {
+      const result = dealCancellationEnabled(DEAL_SUBMISSION_TYPE.AIN);
+
+      expect(result).toEqual(true);
+    });
+  });
+});
--- trade-finance-manager-ui/server/helpers/deal-cancellation-enabled.test.ts
+++ trade-finance-manager-ui/server/helpers/deal-cancellation-enabled.test.ts
@@ -1,42 +1,8 @@
-import { DEAL_SUBMISSION_TYPE, isTfmDealCancellationFeatureFlagEnabled } from '@ukef/dtfs2-common';
+import { DEAL_SUBMISSION_TYPE } from '@ukef/dtfs2-common';
 import { dealCancellationEnabled } from './deal-cancellation-enabled';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-jest.mock('@ukef/dtfs2-common', () => ({
-  ...jest.requireActual('@ukef/dtfs2-common'),
-  isTfmDealCancellationFeatureFlagEnabled: jest.fn(),
-}));
-
 describe('dealCancellationEnabled', () => {
-  describe('when TFM Deal Cancellation feature flag is enabled', () => {
-    beforeEach(() => {
-      jest.mocked(isTfmDealCancellationFeatureFlagEnabled).mockReturnValue(true);
-    });
-
-    it('should return false if the deal type is MIA', () => {
-      const result = dealCancellationEnabled(DEAL_SUBMISSION_TYPE.MIA);
-
-      expect(result).toEqual(false);
-    });
-
-    it('should return true if the deal type is MIN', () => {
-      const result = dealCancellationEnabled(DEAL_SUBMISSION_TYPE.MIN);
-
-      expect(result).toEqual(true);
-    });
-
-    it('should return true if the deal type is AIN', () => {
-      const result = dealCancellationEnabled(DEAL_SUBMISSION_TYPE.AIN);
-
-      expect(result).toEqual(true);
-    });
-  });
-
-  describe('when TFM Deal Cancellation feature flag is disabled', () => {
-    beforeEach(() => {
-      jest.mocked(isTfmDealCancellationFeatureFlagEnabled).mockReturnValue(false);
-    });
-
+  describe('when `FF_TFM_FACILITY_END_DATE_ENABLED` is set to false', () => {
     it.each([DEAL_SUBMISSION_TYPE.MIA, DEAL_SUBMISSION_TYPE.MIN, DEAL_SUBMISSION_TYPE.AIN])('should return false if the deal type is %s', (type) => {
       const result = dealCancellationEnabled(type);
 
