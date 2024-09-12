77cc70577c8b4e5ff77137da11d5a967b57335fb
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-12 15:00:49 +0100

    feat(dtfs2-6616): update tests

--- azure-functions/acbs-function/mappings/facility/facility-guarantee-amend.test.js
+++ azure-functions/acbs-function/mappings/facility/facility-guarantee-amend.test.js
@@ -1,20 +1,49 @@
+const { to2Decimals } = require('../../helpers/currency');
 const facilityGuaranteeAmend = require('./facility-guarantee-amend');
 
+jest.mock('../../helpers/currency', () => ({
+  to2Decimals: jest.fn(),
+}));
+
 describe('facilityGuaranteeAmend', () => {
+  const to2DecimalsResponse = 'to2DecimalsResponse';
+  beforeEach(() => {
+    jest.resetAllMocks();
+    to2Decimals.mockImplementation(() => to2DecimalsResponse);
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
   describe('when there is an amendment amount', () => {
     const amendment = { amount: 123.456 };
+    it('calls to2Decimals with the amendment amount', () => {
+      facilityGuaranteeAmend(amendment);
+      expect(to2Decimals).toHaveBeenCalledWith(123.456);
+    });
 
-    it('returns the target amount rounded to 2 decimal places', () => {
+    it('returns the result of to2Decimals to guaranteedLimit', () => {
       const result = facilityGuaranteeAmend(amendment);
-      expect(result).toEqual({ guaranteedLimit: 123.46 });
+      expect(result).toEqual({ guaranteedLimit: to2DecimalsResponse });
     });
   });
 
-  describe('when there is a guarantee expiry date', () => {
-    const amendment = { facilityGuaranteeDates: { guaranteeExpiryDate: '2022-01-01' } };
-    it('returns the guarantee expiry date', () => {
-      const result = facilityGuaranteeAmend(amendment);
-      expect(result).toEqual({ expirationDate: '2022-01-01' });
+  describe('when there is a facilityGuaranteeDates object', () => {
+    describe('when there is not a guarantee expiry date', () => {
+      it('returns an empty object', () => {
+        const amendment = { facilityGuaranteeDates: {} };
+        const result = facilityGuaranteeAmend(amendment);
+        expect(result).toEqual({});
+      });
+    });
+
+    describe('when there is a guarantee expiry date', () => {
+      it('returns the guarantee expiry date', () => {
+        const amendment = { facilityGuaranteeDates: { guaranteeExpiryDate: '2022-01-01' } };
+        const result = facilityGuaranteeAmend(amendment);
+        expect(result).toEqual({ expirationDate: '2022-01-01' });
+      });
     });
   });
 
@@ -22,14 +51,16 @@ describe('facilityGuaranteeAmend', () => {
     const amendment = { amount: 123.456, facilityGuaranteeDates: { guaranteeExpiryDate: '2022-01-01' } };
     it('returns the target amount rounded to 2 decimal places and the guarantee expiry date', () => {
       const result = facilityGuaranteeAmend(amendment);
-      expect(result).toEqual({ guaranteedLimit: 123.46, expirationDate: '2022-01-01' });
+      expect(result).toEqual({ guaranteedLimit: to2DecimalsResponse, expirationDate: '2022-01-01' });
     });
   });
 
   describe('when there is no amendment amount or guarantee expiry date', () => {
     const amendment = {};
-    const result = facilityGuaranteeAmend(amendment);
-    expect(result).toEqual({});
+    it('returns an empty object', () => {
+      const result = facilityGuaranteeAmend(amendment);
+      expect(result).toEqual({});
+    });
   });
 
   describe('when there are extra fields', () => {
@@ -40,7 +71,7 @@ describe('facilityGuaranteeAmend', () => {
     };
     it('returns the target amount rounded to 2 decimal places and the guarantee expiry date', () => {
       const result = facilityGuaranteeAmend(amendment);
-      expect(result).toEqual({ guaranteedLimit: 123.46, expirationDate: '2022-01-01' });
+      expect(result).toEqual({ guaranteedLimit: to2DecimalsResponse, expirationDate: '2022-01-01' });
     });
   });
 });
