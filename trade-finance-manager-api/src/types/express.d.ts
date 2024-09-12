211d5d4819b4b4e6855b30cf7a65ad46252569a5
Beth Thomas <beth.thomas@softwire.com>
2024-09-12 16:12:14 +0100

    feat(DTFS2-7294): add comment to express file

--- trade-finance-manager-api/src/types/express.d.ts
+++ trade-finance-manager-api/src/types/express.d.ts
@@ -1,5 +1,6 @@
 import { TfmSessionUser } from './tfm-session-user.ts';
 
+// This is needed so that we can access the user property on the Request object which is used for generating audit details
 declare global {
   declare namespace Express {
     export interface Request {
