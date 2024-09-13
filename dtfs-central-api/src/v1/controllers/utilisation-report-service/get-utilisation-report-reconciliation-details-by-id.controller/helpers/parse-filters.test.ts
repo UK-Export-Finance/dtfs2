0f775ce86216a8f4753aaefa656148c66a1455a8
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-09-13 13:59:26 +0100

    feat(FN-2311): update 'expected' const naming

--- dtfs-central-api/src/v1/controllers/utilisation-report-service/get-utilisation-report-reconciliation-details-by-id.controller/helpers/parse-filters.test.ts
+++ dtfs-central-api/src/v1/controllers/utilisation-report-service/get-utilisation-report-reconciliation-details-by-id.controller/helpers/parse-filters.test.ts
@@ -4,37 +4,37 @@ describe('parsePremiumPaymentsFilters', () => {
   it('returns an empty object when premiumPaymentsFilters is undefined', () => {
     // Arrange
     const premiumPaymentsFilters = undefined;
-    const expectedParsedFilters = {};
+    const expected = {};
 
     // Act
     const premiumPaymentsTabParsedFilters = parsePremiumPaymentsFilters(premiumPaymentsFilters);
 
     // Assert
-    expect(premiumPaymentsTabParsedFilters).toEqual(expectedParsedFilters);
+    expect(premiumPaymentsTabParsedFilters).toEqual(expected);
   });
 
   it('returns an object with undefined facilityId when an invalid facilityId is provided', () => {
     // Arrange
     const premiumPaymentsFilters = { facilityId: 'invalid-facility-id' };
-    const expectedParsedFilters = { facilityId: undefined };
+    const expected = { facilityId: undefined };
 
     // Act
     const premiumPaymentsTabParsedFilters = parsePremiumPaymentsFilters(premiumPaymentsFilters);
 
     // Assert
-    expect(premiumPaymentsTabParsedFilters).toEqual(expectedParsedFilters);
+    expect(premiumPaymentsTabParsedFilters).toEqual(expected);
   });
 
   it('returns an object with valid facilityId when a valid facilityId is provided', () => {
     // Arrange
     const validFacilityId = '123456789';
     const premiumPaymentsFilters = { facilityId: validFacilityId };
-    const expectedParsedFilters = { facilityId: validFacilityId };
+    const expected = premiumPaymentsFilters;
 
     // Act
     const premiumPaymentsTabParsedFilters = parsePremiumPaymentsFilters(premiumPaymentsFilters);
 
     // Assert
-    expect(premiumPaymentsTabParsedFilters).toEqual(expectedParsedFilters);
+    expect(premiumPaymentsTabParsedFilters).toEqual(expected);
   });
 });
