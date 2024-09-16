2d0be985d28a2612a0e4cccd5755763db21d6969
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-16 17:43:49 +0100

    feat(dtfs2-6892): removed unused teamIds type

--- libs/common/src/types/tfm/team-id.ts
+++ libs/common/src/types/tfm/team-id.ts
@@ -6,5 +6,3 @@ export type PdcTeamId = ValuesOf<typeof PDC_TEAM_IDS>;
 export type NonPdcTeamId = ValuesOf<typeof NON_PDC_TEAM_IDS>;
 
 export type TeamId = ValuesOf<typeof TEAM_IDS>;
-
-export type TeamIds = Record<string, string>;
