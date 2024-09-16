d5b6de76f80fb124d940fc492a682cd37aafd26a
Beth Thomas <beth.thomas@softwire.com>
2024-09-16 11:16:14 +0100

    feat(DTFS2-7294): add mock to test

--- trade-finance-manager-api/api-tests/v1/deal-cancellation/deal-cancellation.api-test.ts
+++ trade-finance-manager-api/api-tests/v1/deal-cancellation/deal-cancellation.api-test.ts
@@ -1,21 +1,45 @@
 import { ObjectId } from 'mongodb';
 import { generateTfmAuditDetails } from '@ukef/dtfs2-common/change-stream';
 import createApi from '../../api';
+import api from '../../../src/v1/api';
 import app from '../../../src/createApp';
 import testUserCache from '../../api-test-users';
 import { MOCK_TFM_SESSION_USER } from '../../../src/v1/__mocks__/mock-tfm-session-user.ts';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+jest.mock('../../../src/v1/api', () => ({
+  ...jest.requireActual('../../../src/v1/api'),
+  updateDealCancellation: jest.fn(),
+}));
+
 const { as, put } = createApi(app);
 
-describe('/v1/deals/:id/cancellation', () => {
-  const validId = new ObjectId().toString();
+const validId = new ObjectId().toString();
 
+const mockUpdateResult = {
+  acknowledged: true,
+  modifiedCount: 1,
+  matchedCount: 1,
+  upsertedCount: 1,
+  upsertedId: new ObjectId(validId),
+};
+
+describe('/v1/deals/:id/cancellation', () => {
   const payload = {
     dealCancellationUpdate: { reason: 'test reason' },
     auditDetails: generateTfmAuditDetails(MOCK_TFM_SESSION_USER._id),
   };
 
   describe('PUT /v1/deals/:id/cancellation', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+      jest.mocked(api.updateDealCancellation).mockResolvedValue(mockUpdateResult);
+    });
+
+    afterAll(() => {
+      jest.restoreAllMocks();
+    });
+
     const getTfmDealCancellationUpdateUrl = ({ id }: { id: string }) => `/v1/deals/${id}/cancellation`;
 
     it('returns a 401 response when user is not authenticated', async () => {
@@ -41,7 +65,7 @@ describe('/v1/deals/:id/cancellation', () => {
       expect(response.status).toEqual(400);
     });
 
-    it('puts the deal cancellation for an authenticated user', async () => {
+    it('updates the deal cancellation for an authenticated user', async () => {
       // Arrange
       const tokenUser = await testUserCache.initialise(app);
       const url = getTfmDealCancellationUpdateUrl({ id: validId });
@@ -49,9 +73,11 @@ describe('/v1/deals/:id/cancellation', () => {
       // Act
       const response = await as(tokenUser).put(payload).to(url);
 
+      const expectedResponse = { ...mockUpdateResult, upsertedId: validId };
+
       // Assert
-      expect(response.body).not.toBeNull();
       expect(response.status).toEqual(200);
+      expect(response.body).toEqual(expectedResponse);
     });
   });
 });
