47ce15a784a0c13301912221f83b9c787f7ec1aa
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-16 17:52:38 +0100

    feat(dtfs2-6892): fixed GetUserResponse type to be accurate

--- trade-finance-manager-api/src/types/auth/get-user-response.ts
+++ trade-finance-manager-api/src/types/auth/get-user-response.ts
@@ -1,3 +1,3 @@
 import { TfmUser } from '../db-models/tfm-users';
 
-export type GetUserResponse = { found: boolean; canProceed?: boolean; user?: TfmUser };
+export type GetUserResponse = { found: false } | { found: true; canProceed: false } | { found: true; canProceed: true; user: TfmUser };
