00f4a1de6574a5f1b5475fe2cdb82e2555bd8951
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-18 13:56:52 +0100

    feat(dtfs2-6892): add typedef to MockTfmUser

--- trade-finance-manager-api/src/v1/__mocks__/mock-users.js
+++ trade-finance-manager-api/src/v1/__mocks__/mock-users.js
@@ -1,3 +1,22 @@
+/**
+ * This typedef is kept deliberately separate from existing tfm user type defs,
+ * as there is no type safety on the mock data. This means we cannot reliably assume that
+ * data that should be a constant is a constant -- i.e. the teams field.
+ * @typedef {object} MockTfmUser
+ * @property {string} _id - The unique identifier of the user.
+ * @property {string} azureOid - The Azure Object ID of the user.
+ * @property {string} username - The username of the user.
+ * @property {string} email - The email address of the user.
+ * @property {string[]} teams - The teams the user belongs to.
+ * @property {string} timezone - The timezone of the user.
+ * @property {string} firstName - The first name of the user.
+ * @property {string} lastName - The last name of the user.
+ */
+
+/**
+ * Mock TFM Users
+ * @type {MockTfmUser[]}
+ */
 const MOCK_USERS = [
   {
     _id: '6051d94564494924d38ce67c',
