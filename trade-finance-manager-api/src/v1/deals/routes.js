0cb565e6ad514547162fca823494244dc18de4c3
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-09-18 09:31:32 +0100

    feat(DTFS2-7370): remove RBAC from ammendment route

--- trade-finance-manager-api/src/v1/deals/routes.js
+++ trade-finance-manager-api/src/v1/deals/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { TEAM_IDS, validateDealCancellationEnabled } = require('@ukef/dtfs2-common');
+const { validateDealCancellationEnabled } = require('@ukef/dtfs2-common');
 const dealSubmit = require('../controllers/deal.submit.controller');
 const amendmentController = require('../controllers/amendment.controller');
 const dealController = require('../controllers/deal.controller');
@@ -7,7 +7,6 @@ const dealCancellationController = require('../controllers/deal-cancellation.con
 const dealUnderwriterManagersDecisionController = require('../controllers/deal-underwriter-managers-decision.controller');
 const validation = require('../validation/route-validators/route-validators');
 const handleExpressValidatorResult = require('../validation/route-validators/express-validator-result-handler');
-const { validateUserHasAtLeastOneAllowedTeam } = require('../middleware/validate-user-is-in-at-least-one-allowed-team');
 
 const dealsOpenRouter = express.Router();
 
@@ -85,12 +84,7 @@ dealsAuthRouter
 
 dealsAuthRouter
   .route('/deals/:dealId/amendments/:status?/:type?')
-  .get(
-    validateUserHasAtLeastOneAllowedTeam([TEAM_IDS.PIM]),
-    validation.dealIdValidation,
-    handleExpressValidatorResult,
-    amendmentController.getAmendmentsByDealId,
-  );
+  .get(validation.dealIdValidation, handleExpressValidatorResult, amendmentController.getAmendmentsByDealId);
 
 dealsAuthRouter
   .route('/deals/:dealId/underwriting/lead-underwriter')
