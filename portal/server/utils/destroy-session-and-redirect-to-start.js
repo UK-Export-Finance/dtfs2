ea489614f47f25fc1a686da90184a41d8449bbf8
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-18 17:09:35 +0100

    feat(dtfs2-6892): revert previous destroy session changes for portal-ui

--- portal/server/utils/destroy-session-and-redirect-to-start.js
+++ portal/server/utils/destroy-session-and-redirect-to-start.js
@@ -1,3 +1,5 @@
+const { PORTAL_URL } = require('../constants');
+
 /**
  * Destroys a user's session and redirects them to the start page.
  * @param {object} req
@@ -5,8 +7,11 @@
  * @returns {void}
  */
 function destroySessionAndRedirectToStart(req, res) {
+  const startPageRedirect = Boolean(process.env.START_PAGE_REDIRECT);
+  const redirectAddress = startPageRedirect ? PORTAL_URL : '/login';
+
   req.session.destroy(() => {
-    res.redirect('/login');
+    res.redirect(redirectAddress);
   });
 }
 
