82ba8ad12809afd27481911e3cb08e26dabc27c1
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-18 18:05:13 +0100

    feat(dtfs2-6892): add todo regarding whether azureOid is optional

--- libs/common/src/types/mongo-db-models/tfm-users.ts
+++ libs/common/src/types/mongo-db-models/tfm-users.ts
@@ -3,6 +3,12 @@ import { TeamId } from '../tfm/team-id';
 import { UnixTimestamp } from '../date';
 
 export type TfmUser = WithId<{
+  /**
+   * TODO: DTFS2-6892:
+   * Consider whether AzureOid is optional because it is only added
+   * when the user logs in for the first time with SSO.
+   */
+  azureOid?: string;
   username: string;
   email: string;
   teams: TeamId[];
