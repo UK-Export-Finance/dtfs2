918d9e28cdcd52e503974d1b3b0ef8b71a70db2a
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-19 16:42:22 +0100

    feat(dtfs2-6892): add tests to error handling for process sso redirect if missing body params

--- trade-finance-manager-api/src/v1/controllers/auth/auth.controller.process-sso-redirect.test.js
+++ trade-finance-manager-api/src/v1/controllers/auth/auth.controller.process-sso-redirect.test.js
@@ -2,7 +2,14 @@ const { processSsoRedirect } = require('./auth.controller');
 const authService = require('./auth-service');
 
 describe('auth controller', () => {
-  const req = {};
+  const getReq = () => ({
+    body: {
+      pkceCodes: 'mock',
+      authCodeRequest: 'mock',
+      code: 'mock',
+      state: 'mock',
+    },
+  });
 
   const res = {
     status: jest.fn().mockReturnThis(),
@@ -14,40 +21,63 @@ describe('auth controller', () => {
   };
 
   describe('processSsoRedirect()', () => {
-    const mockProcessSsoRedirectResponse = { redirect: true };
-
-    req.body = {
-      pkceCodes: 'mock',
-      authCodeRequest: 'mock',
-      code: 'mock',
-      state: 'mock',
-    };
-
-    beforeAll(async () => {
-      authService.processSsoRedirect = jest.fn().mockResolvedValue(mockProcessSsoRedirectResponse);
-
-      await processSsoRedirect(req, res);
-    });
-
-    it('should call authService.processSsoRedirect', () => {
-      expect(authService.processSsoRedirect).toHaveBeenCalledTimes(1);
-    });
-
-    it('should call res.status and res.send', () => {
-      expect(res.status).toHaveBeenCalledWith(200);
-
-      expect(res.send).toHaveBeenCalledWith(mockProcessSsoRedirectResponse);
-    });
-
-    describe('when there is an error', () => {
-      it('should call res.status=500 with an error message', async () => {
-        authService.processSsoRedirect = jest.fn().mockRejectedValue(mockError);
+    describe('when there is a missing field', () => {
+      it('should return a 400 status with error message containing the missing field', async () => {
+        const req = getReq();
+        delete req.body.state;
 
         await processSsoRedirect(req, res);
 
-        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.send).toHaveBeenCalledWith('Invalid request, missing fields: state');
+      });
 
-        expect(res.send).toHaveBeenCalledWith({ data: mockError.message });
+      describe('when there are missing fields', () => {
+        it('should return a 400 status with error message containing the missing field', async () => {
+          const req = getReq();
+          delete req.body.state;
+          delete req.body.code;
+          await processSsoRedirect(req, res);
+
+          expect(res.status).toHaveBeenCalledWith(400);
+          expect(res.send).toHaveBeenCalledWith(expect.stringContaining('Invalid request, missing fields:'));
+          expect(res.send).toHaveBeenCalledWith(expect.stringContaining('state'));
+          expect(res.send).toHaveBeenCalledWith(expect.stringContaining('code'));
+        });
+      });
+    });
+
+    describe('when there are no missing fields', () => {
+      const req = getReq();
+
+      const mockProcessSsoRedirectResponse = { redirect: true };
+
+      beforeAll(async () => {
+        authService.processSsoRedirect = jest.fn().mockResolvedValue(mockProcessSsoRedirectResponse);
+
+        await processSsoRedirect(req, res);
+      });
+
+      it('should call authService.processSsoRedirect', () => {
+        expect(authService.processSsoRedirect).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call res.status and res.send', () => {
+        expect(res.status).toHaveBeenCalledWith(200);
+
+        expect(res.send).toHaveBeenCalledWith(mockProcessSsoRedirectResponse);
+      });
+
+      describe('when there is an error', () => {
+        it('should call res.status=500 with an error message', async () => {
+          authService.processSsoRedirect = jest.fn().mockRejectedValue(mockError);
+
+          await processSsoRedirect(req, res);
+
+          expect(res.status).toHaveBeenCalledWith(500);
+
+          expect(res.send).toHaveBeenCalledWith({ data: mockError.message });
+        });
       });
     });
   });
