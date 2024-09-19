505aad34a15637e49d07c1f67762525486abf70e
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-09-19 16:25:37 +0100

    feat(DTFS2-7370): add better logging to RBAC middleware

--- trade-finance-manager-api/src/v1/middleware/validate-user-is-in-at-least-one-allowed-team.ts
+++ trade-finance-manager-api/src/v1/middleware/validate-user-is-in-at-least-one-allowed-team.ts
@@ -14,7 +14,7 @@ export const validateUserHasAtLeastOneAllowedTeam =
     const userHasAtLeastOneAllowedTeam = allowedTeams.some((allowedTeam) => user.teams.includes(allowedTeam));
 
     if (!userHasAtLeastOneAllowedTeam) {
-      console.error('Unauthorised access for %s.', user);
+      console.error('Unauthorised access, expected user team to be one of: %s, but user was: %s.', allowedTeams, user);
 
       return res.status(HttpStatusCode.Forbidden).send({ success: false, msg: "You don't have access to this page" });
     }
