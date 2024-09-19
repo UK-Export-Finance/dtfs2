72b88015599479a53479b047940de3fc0221113c
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-09-19 11:18:18 +0100

    feat(DTFS2-7392): extract supertest api types

--- trade-finance-manager-api/api-tests/api-test-users.ts
+++ trade-finance-manager-api/api-tests/api-test-users.ts
@@ -1,13 +1,14 @@
 import { WithoutId } from 'mongodb';
 import { cloneDeep } from 'lodash';
 import { MONGO_DB_COLLECTIONS, TeamId, TfmUser } from '@ukef/dtfs2-common';
-import { createApi, TestAs, TestRequestWithoutHeaders } from './api';
+import { createApi } from './api';
 import MOCK_USERS from '../src/v1/__mocks__/mock-users';
 import { TestUser } from './types/test-user';
 import { wipeCollection } from './database-helper';
 import { genPassword } from '../src/utils/crypto.util';
 import { mongoDbClient } from '../src/drivers/db-client';
 import { STATUS as USER_STATUS } from '../src/constants/user';
+import { TestAs, TestRequestWithoutHeaders } from './types/test-api';
 
 type MockUserWithoutTokenOrId = Omit<TestUser, 'token' | '_id'>;
 
---
+++ trade-finance-manager-api/api-tests/types/test-api.ts
@@ -0,0 +1,59 @@
+import { AnyObject } from '@ukef/dtfs2-common';
+import { Response } from 'supertest';
+import { IncomingHttpHeaders } from 'http';
+import { TestUser } from './test-user';
+import { File } from './file';
+
+export type TestRequestWithoutHeaders = (data: AnyObject) => {
+  to: (url: string) => Promise<Response>;
+};
+
+type TestGetWithHeaders = (
+  url: string,
+  {
+    headers,
+    query,
+  }?: {
+    headers: IncomingHttpHeaders | undefined;
+    query: AnyObject | undefined;
+  },
+) => Promise<Response>;
+
+type TestPutWithHeaders = (
+  url: string,
+  data: AnyObject,
+  {
+    headers,
+  }?: {
+    headers: IncomingHttpHeaders | undefined;
+  },
+) => Promise<Response>;
+
+type TestPostEach = (list: AnyObject[]) => {
+  to: (url: string) => Promise<Response[]>;
+};
+
+type TestPutMultipartForm = (
+  data: AnyObject,
+  files?: File[],
+) => {
+  to: (url: string) => Promise<Response>;
+};
+
+type TestGetWithoutHeaders = (url: string) => Promise<Response>;
+
+export type TestAs = (user: TestUser) => {
+  post: TestRequestWithoutHeaders;
+  postEach: TestPostEach;
+  put: TestRequestWithoutHeaders;
+  putMultipartForm: TestPutMultipartForm;
+  get: TestGetWithoutHeaders;
+  remove: TestRequestWithoutHeaders;
+};
+
+export type TestApi = {
+  as: TestAs;
+  post: TestRequestWithoutHeaders;
+  get: TestGetWithHeaders;
+  put: TestPutWithHeaders;
+};
