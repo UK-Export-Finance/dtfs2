525f4745a4611b52e27c5043c7e242fb4202f2de
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-19 14:42:08 +0100

    feat(dtfs2-6892): add error handling for process sso redirect if missing required body param

--- trade-finance-manager-api/src/v1/controllers/auth/auth.controller.js
+++ trade-finance-manager-api/src/v1/controllers/auth/auth.controller.js
@@ -1,3 +1,4 @@
+const { InvalidPayloadError, ApiError } = require('@ukef/dtfs2-common');
 const authProvider = require('./auth-provider');
 const authService = require('./auth-service');
 
@@ -17,11 +18,22 @@ exports.processSsoRedirect = async (req, res) => {
   try {
     const { pkceCodes, authCodeRequest, code, state } = req.body;
 
+    const requiredFields = { pkceCodes, authCodeRequest, code, state };
+    const missingFields = Object.keys(requiredFields).filter((key) => !requiredFields[key]);
+
+    if (missingFields.length) {
+      throw new InvalidPayloadError(`Invalid request, missing fields: ${missingFields.join(', ')}`);
+    }
+
     const loginResult = await authService.processSsoRedirect({ pkceCodes, authCodeRequest, code, state });
 
     return res.status(200).send(loginResult);
   } catch (err) {
     console.error('Error processing sso login: %O', err);
+
+    if (err instanceof ApiError) {
+      return res.status(err.code).send({ data: err.message });
+    }
 
     return res.status(500).send({ data: err.message });
   }
