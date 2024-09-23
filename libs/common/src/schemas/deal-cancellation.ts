0fe4635d144651e848a3cede480783ad07356fca
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-09-23 10:28:49 +0100

    feat(DTFS2-7353): remove zod type helper

--- libs/common/src/schemas/deal-cancellation.ts
+++ libs/common/src/schemas/deal-cancellation.ts
@@ -1,11 +1,10 @@
 import z from 'zod';
-import { TfmDealCancellation, ZodTypeFromObject } from '../types';
 import { MAX_CHARACTER_COUNT } from '../constants';
 
 /**
  * Deal Cancellation schema to validate an object is of type `TfmDealCancellation`
  */
-export const DEAL_CANCELLATION: z.ZodObject<ZodTypeFromObject<TfmDealCancellation>> = z
+export const DEAL_CANCELLATION = z
   .object({
     reason: z.string().max(MAX_CHARACTER_COUNT),
     bankRequestDate: z.number(),
--- libs/common/src/types/zod-type-from-object.ts
+++
@@ -1,8 +0,0 @@
-import z from 'zod';
-
-/**
- * Helper to map T into `ZodRawShape`. This allows an object to be passed into generics like `z.ZodObject`
- */
-export type ZodTypeFromObject<T extends object> = {
-  [K in keyof T]: z.ZodType<T[K]>;
-};
