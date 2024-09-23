3bd4fbb6f07170720103e0fe999357680d0860a4
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-09-23 12:26:46 +0100

    feat(DTFS2-7393): move all tests into fed-disabled directory

---
+++ e2e-tests/gef/cypress/e2e/facility/facility-end-date-disabled/pre-submission/facility-currency.spec.js
@@ -0,0 +1,102 @@
+import relative from '../../../relativeURL';
+import { backLink, errorSummary, headingCaption, mainHeading, form, continueButton, saveAndReturnButton } from '../../../partials';
+import facilityCurrency from '../../../pages/facility-currency';
+import { BANK1_MAKER1 } from '../../../../../../e2e-fixtures/portal-users.fixture';
+
+const applications = [];
+let token;
+
+context('Facility Currency Page', () => {
+  before(() => {
+    cy.loadData();
+    cy.apiLogin(BANK1_MAKER1)
+      .then((tok) => {
+        token = tok;
+      })
+      .then(() => cy.apiFetchAllGefApplications(token))
+      .then(({ body }) => {
+        body.items.forEach((item) => {
+          cy.apiFetchAllFacilities(item._id, token).then((res) => {
+            applications.push({
+              id: item._id,
+              facilities: res.body.items.filter((it) => it.details.dealId === item._id),
+            });
+          });
+        });
+      });
+    cy.login(BANK1_MAKER1);
+  });
+
+  beforeEach(() => {
+    cy.saveSession();
+  });
+
+  describe('Visiting page as cash facility', () => {
+    it('displays the correct elements', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-currency`));
+      backLink();
+      headingCaption();
+      mainHeading().contains('cash');
+      mainHeading().should('not.contain', 'contingent');
+      facilityCurrency.hiddenFacilityType().should('not.be.visible');
+      form();
+      continueButton();
+      saveAndReturnButton();
+    });
+
+    it('shows YEN checkbox checked', () => {
+      cy.visit(relative(`/gef/application-details/${applications[2].id}/facilities/${applications[2].facilities[2].details._id}/facility-currency`));
+      facilityCurrency.yenCheckbox().should('be.checked');
+    });
+
+    it('redirects user to `provided facility` page when clicking on `Back` Link', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-currency`));
+      cy.clickBackLink();
+      cy.url().should(
+        'eq',
+        relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/provided-facility`),
+      );
+    });
+
+    it('shows error message when no radio button has been selected', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-currency`));
+      cy.clickContinueButton();
+      errorSummary();
+      facilityCurrency.currencyError();
+    });
+
+    it('takes you to `Facility value` page when clicking on `Continue` button', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-currency`));
+      facilityCurrency.yenCheckbox().click();
+      cy.clickContinueButton();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-value`));
+    });
+
+    it('saves the currency and redirects user to application page when clicking on `Save and return` button', () => {
+      cy.visit(relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-currency`));
+      cy.clickSaveAndReturnButton();
+      cy.url().should('eq', relative(`/gef/application-details/${applications[1].id}`));
+    });
+
+    it('hides back button if visiting page with `change` query', () => {
+      cy.visit(
+        relative(`/gef/application-details/${applications[1].id}/facilities/${applications[1].facilities[1].details._id}/facility-currency?status=change`),
+      );
+      backLink().should('not.exist');
+    });
+  });
+
+  describe('Visiting page as contingent facility', () => {
+    it('displays the correct elements', () => {
+      cy.visit(relative(`/gef/application-details/${applications[2].id}/facilities/${applications[2].facilities[2].details._id}/facility-currency`));
+      backLink();
+      headingCaption();
+      mainHeading().contains('contingent');
+      mainHeading().should('not.contain', 'cash');
+      form();
+      facilityCurrency.hiddenFacilityType().should('not.be.visible');
+      continueButton();
+      saveAndReturnButton();
+    });
+  });
+});
---
+++ e2e-tests/gef/cypress/e2e/facility/facility-end-date-disabled/unissued-facilities/AIN/unissued-facilities-bank-review-date-AIN.spec.js
@@ -0,0 +1,194 @@
+import relative from '../../../../relativeURL';
+import { errorSummary } from '../../../../partials';
+import CONSTANTS from '../../../../../fixtures/constants';
+import {
+  today,
+  threeDaysDay,
+  threeDaysMonth,
+  threeDaysYear,
+  threeMonthsOneDayDay,
+  threeMonthsOneDayYear,
+  threeMonthsOneDayMonth,
+} from '../../../../../../../e2e-fixtures/dateConstants';
+import { MOCK_APPLICATION_AIN } from '../../../../../fixtures/mocks/mock-deals';
+import { BANK1_MAKER1 } from '../../../../../../../e2e-fixtures/portal-users.fixture';
+import { multipleMockGefFacilities } from '../../../../../../../e2e-fixtures/mock-gef-facilities';
+import unissuedFacilityTable from '../../../../pages/unissued-facilities';
+import applicationPreview from '../../../../pages/application-preview';
+import aboutFacilityUnissued from '../../../../pages/unissued-facilities-about-facility';
+import bankReviewDate from '../../../../pages/bank-review-date';
+
+const facilityEndDateEnabled = Number(Cypress.env('GEF_DEAL_VERSION')) >= 1;
+
+const { unissuedCashFacility, issuedCashFacility, unissuedContingentFacility, unissuedCashFacilityWith20MonthsOfCover } = multipleMockGefFacilities({
+  facilityEndDateEnabled,
+});
+
+if (facilityEndDateEnabled) {
+  context('Unissued Facilities AIN - bank review date page', () => {
+    let dealId;
+    let token;
+    let facilityOneId;
+
+    before(() => {
+      cy.apiLogin(BANK1_MAKER1)
+        .then((t) => {
+          token = t;
+        })
+        .then(() => {
+          // creates application and inserts facilities and changes status
+          cy.apiCreateApplication(BANK1_MAKER1, token).then(({ body }) => {
+            dealId = body._id;
+            cy.apiUpdateApplication(dealId, token, MOCK_APPLICATION_AIN).then(() => {
+              cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CASH, token).then((facility) => {
+                facilityOneId = facility.body.details._id;
+                cy.apiUpdateFacility(facility.body.details._id, token, unissuedCashFacility);
+              });
+              cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CASH, token).then((facility) =>
+                cy.apiUpdateFacility(facility.body.details._id, token, issuedCashFacility),
+              );
+              cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CONTINGENT, token).then((facility) =>
+                cy.apiUpdateFacility(facility.body.details._id, token, unissuedContingentFacility),
+              );
+              cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CASH, token).then((facility) =>
+                cy.apiUpdateFacility(facility.body.details._id, token, unissuedCashFacilityWith20MonthsOfCover),
+              );
+              cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CASH, token).then((facility) =>
+                cy.apiUpdateFacility(facility.body.details._id, token, unissuedCashFacilityWith20MonthsOfCover),
+              );
+              cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CASH, token).then((facility) =>
+                cy.apiUpdateFacility(facility.body.details._id, token, unissuedCashFacilityWith20MonthsOfCover),
+              );
+              cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CASH, token).then((facility) =>
+                cy.apiUpdateFacility(facility.body.details._id, token, unissuedCashFacilityWith20MonthsOfCover),
+              );
+              cy.apiSetApplicationStatus(dealId, token, CONSTANTS.DEAL_STATUS.UKEF_ACKNOWLEDGED);
+            });
+          });
+        });
+    });
+
+    beforeEach(() => {
+      cy.saveSession();
+      cy.login(BANK1_MAKER1);
+    });
+
+    describe('Visiting from unissued facilities page', () => {
+      const getUnissuedFacilitiesUrl = () => relative(`/gef/application-details/${dealId}/unissued-facilities`);
+
+      beforeEach(() => {
+        cy.visit(getUnissuedFacilitiesUrl());
+        unissuedFacilityTable.updateIndividualFacilityButton(0).click();
+
+        cy.keyboardInput(aboutFacilityUnissued.issueDateDay(), threeDaysDay);
+        cy.keyboardInput(aboutFacilityUnissued.issueDateMonth(), threeDaysMonth);
+        cy.keyboardInput(aboutFacilityUnissued.issueDateYear(), threeDaysYear);
+
+        aboutFacilityUnissued.shouldCoverStartOnSubmissionNo().click();
+        cy.keyboardInput(aboutFacilityUnissued.coverStartDateDay(), threeDaysDay);
+        cy.keyboardInput(aboutFacilityUnissued.coverStartDateMonth(), threeDaysMonth);
+        cy.keyboardInput(aboutFacilityUnissued.coverStartDateYear(), threeDaysYear);
+
+        cy.keyboardInput(aboutFacilityUnissued.coverEndDateDay(), threeMonthsOneDayDay);
+        cy.keyboardInput(aboutFacilityUnissued.coverEndDateMonth(), threeMonthsOneDayMonth);
+        cy.keyboardInput(aboutFacilityUnissued.coverEndDateYear(), threeMonthsOneDayYear);
+
+        aboutFacilityUnissued.isUsingFacilityEndDateNo().click();
+
+        cy.clickContinueButton();
+      });
+
+      it('should redirect user to the about unissued facility page when clicking back link', () => {
+        const facilityOneBankReviewDateUrl = relative(`/gef/application-details/${dealId}/unissued-facilities/${facilityOneId}/bank-review-date`);
+        cy.url().should('eq', facilityOneBankReviewDateUrl);
+
+        cy.clickBackLink();
+
+        const aboutUnissuedFacilityUrl = relative(`/gef/application-details/${dealId}/unissued-facilities/${facilityOneId}/about`);
+        cy.url().should('eq', aboutUnissuedFacilityUrl);
+      });
+
+      it('should display an error message when the date is entered incorrectly & click continue', () => {
+        cy.keyboardInput(bankReviewDate.bankReviewDateDay(), 'abcd');
+        cy.clickContinueButton();
+
+        errorSummary();
+        bankReviewDate.bankReviewDateError();
+      });
+
+      it('should display an error message when the date is entered incorrectly & click save and return', () => {
+        cy.keyboardInput(bankReviewDate.bankReviewDateDay(), 'abcd');
+        cy.clickSaveAndReturnButton();
+
+        errorSummary();
+        bankReviewDate.bankReviewDateError();
+      });
+
+      it('should redirect user to the unissued facility page when clicking continue', () => {
+        cy.fillInBankReviewDate(today);
+
+        cy.clickContinueButton();
+
+        cy.url().should('eq', getUnissuedFacilitiesUrl());
+      });
+
+      it('should redirect user to the unissued facility page when clicking save and return', () => {
+        cy.fillInBankReviewDate(today);
+
+        cy.clickSaveAndReturnButton();
+
+        cy.url().should('eq', getUnissuedFacilitiesUrl());
+      });
+    });
+
+    describe('Visiting from application preview page', () => {
+      const getApplicationDetailsUrl = () => relative(`/gef/application-details/${dealId}`);
+
+      beforeEach(() => {
+        cy.visit(getApplicationDetailsUrl());
+        applicationPreview.facilitySummaryListTable(6).bankReviewDateAction().click();
+      });
+
+      it('should redirect user to the about unissued facility page when clicking back link', () => {
+        const facilityOneChangeBankReviewDateUrl = relative(`/gef/application-details/${dealId}/unissued-facilities/${facilityOneId}/bank-review-date/change`);
+        cy.url().should('eq', facilityOneChangeBankReviewDateUrl);
+
+        cy.clickBackLink();
+
+        const facilityOneChangeUnissuedFacilityUrl = relative(`/gef/application-details/${dealId}/unissued-facilities/${facilityOneId}/change`);
+        cy.url().should('eq', facilityOneChangeUnissuedFacilityUrl);
+      });
+
+      it('should display an error message when the date is entered incorrectly & click continue', () => {
+        cy.keyboardInput(bankReviewDate.bankReviewDateDay(), 'abcd');
+        cy.clickContinueButton();
+
+        errorSummary();
+        bankReviewDate.bankReviewDateError();
+      });
+
+      it('should display an error message when the date is entered incorrectly & click saveAndReturn', () => {
+        cy.keyboardInput(bankReviewDate.bankReviewDateDay(), 'abcd');
+        cy.clickSaveAndReturnButton();
+
+        errorSummary();
+        bankReviewDate.bankReviewDateError();
+      });
+
+      it('should redirect user to the application details page when clicking continue', () => {
+        cy.fillInBankReviewDate(today);
+        cy.clickContinueButton();
+
+        cy.url().should('eq', getApplicationDetailsUrl());
+      });
+
+      it('should redirect user to the application details page when clicking save and return', () => {
+        cy.fillInBankReviewDate(today);
+
+        cy.clickSaveAndReturnButton();
+
+        cy.url().should('eq', getApplicationDetailsUrl());
+      });
+    });
+  });
+}
