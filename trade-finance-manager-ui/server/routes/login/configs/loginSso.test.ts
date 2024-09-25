85345ba8e8702654559061554fcb2a1cf1fb6c88
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-25 17:03:03 +0100

    feat(dtfs2-6892): add getLogin route tests

---
+++ trade-finance-manager-ui/server/routes/login/configs/loginSso.test.ts
@@ -0,0 +1,1 @@
+describe('routes - login (sso)', () => {});
--- trade-finance-manager-ui/server/routes/login/configs/loginSso.ts
+++ trade-finance-manager-ui/server/routes/login/configs/loginSso.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import * as loginSsoController from '../../../controllers/login/loginSso';
 import { getRouter } from '../../../types/get-router';
 
 export const getLoginSsoRouter: getRouter = () => {
   const loginSsoRouter = express.Router();
+  loginSsoRouter.get('/', loginSsoController.getLogin);
 
   return loginSsoRouter;
 };
