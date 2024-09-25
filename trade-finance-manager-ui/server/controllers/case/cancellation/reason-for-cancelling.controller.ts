68b6ae0ae101d0def486d1b1c5e5db3c6d0e5c01
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-09-25 14:30:15 +0100

    feat(DTFS2-7350): update database on post request

--- trade-finance-manager-ui/server/controllers/case/cancellation/reason-for-cancelling.controller.ts
+++ trade-finance-manager-ui/server/controllers/case/cancellation/reason-for-cancelling.controller.ts
@@ -26,8 +26,8 @@ export const getReasonForCancelling = async (req: GetReasonForCancellingRequest,
       return res.redirect('/not-found');
     }
 
-    if (canSubmissionTypeBeCancelled(deal.dealSnapshot.submissionType)) {
-      return res.redirect(`/case/${_id}/`);
+    if (!canSubmissionTypeBeCancelled(deal.dealSnapshot.submissionType)) {
+      return res.redirect(`/case/${_id}/deal`);
     }
 
     const cancellation = await api.getDealCancellation(_id, userToken);
@@ -52,27 +52,44 @@ export const getReasonForCancelling = async (req: GetReasonForCancellingRequest,
  * @param req - The express request
  * @param res - The express response
  */
-export const postReasonForCancelling = (req: PostReasonForCancellingRequest, res: Response) => {
+export const postReasonForCancelling = async (req: PostReasonForCancellingRequest, res: Response) => {
   const { _id } = req.params;
   const { reason } = req.body;
-  const { user } = asUserSession(req.session);
+  const { user, userToken } = asUserSession(req.session);
 
-  const validationErrors = validateReasonForCancelling(reason);
+  try {
+    const deal = (await api.getDeal(_id, userToken)) as { dealSnapshot: { details: { ukefDealId: string }; submissionType: DealSubmissionType } };
 
-  const formHasErrors = validationErrors.errorSummary.length !== 0;
+    if (!deal) {
+      return res.redirect('/not-found');
+    }
 
-  if (formHasErrors) {
-    const reasonForCancellingViewModel: ReasonForCancellingViewModel = {
-      activePrimaryNavigation: PRIMARY_NAVIGATION_KEYS.ALL_DEALS,
-      user,
-      ukefDealId: '0040613574', // TODO: DTFS2-7350 get values from database
-      dealId: _id,
-      errors: validationErrors,
-      reasonForCancelling: reason,
-    };
+    if (!canSubmissionTypeBeCancelled(deal.dealSnapshot.submissionType)) {
+      return res.redirect(`/case/${_id}/deal`);
+    }
 
-    return res.render('case/cancellation/reason-for-cancelling.njk', reasonForCancellingViewModel);
+    const validationErrors = validateReasonForCancelling(reason);
+
+    const formHasErrors = validationErrors.errorSummary.length !== 0;
+
+    if (formHasErrors) {
+      const reasonForCancellingViewModel: ReasonForCancellingViewModel = {
+        activePrimaryNavigation: PRIMARY_NAVIGATION_KEYS.ALL_DEALS,
+        user,
+        ukefDealId: deal.dealSnapshot.details.ukefDealId,
+        dealId: _id,
+        errors: validationErrors,
+        reasonForCancelling: reason,
+      };
+
+      return res.render('case/cancellation/reason-for-cancelling.njk', reasonForCancellingViewModel);
+    }
+
+    await api.updateDealCancellation(_id, { reason }, userToken);
+
+    return res.redirect(`/case/${_id}/cancellation/bank-request-date`);
+  } catch (error) {
+    console.error('Error updating reason for cancelling', error);
+    return res.render('_partials/problem-with-service.njk');
   }
-
-  return res.redirect(`/case/${_id}/cancellation/bank-request-date`);
 };
