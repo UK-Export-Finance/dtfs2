0e9e03f48e00073f0698299a8402e5caa354aa0d
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-09-27 13:20:38 +0100

    feat(FN-3272): fix failing tests

--- dtfs-central-api/src/services/state-machines/utilisation-report/event-handlers/helpers/calculate-fixed-fee.ts
+++ dtfs-central-api/src/services/state-machines/utilisation-report/event-handlers/helpers/calculate-fixed-fee.ts
@@ -30,7 +30,7 @@ export const calculateFixedFee = ({
   interestPercentage,
   dayCountBasis,
 }: CalculateFixedFeeUtilisationReportParams): number => {
-  const numberOfDaysRemainingInCoverPeriod = getNumberOfDaysInCoverPeriod(coverStartDate, coverEndDate);
+  const numberOfDaysRemainingInCoverPeriod = getNumberOfDaysInCoverPeriod(new Date(coverStartDate), new Date(coverEndDate));
 
   return calculateFixedFeeFromDaysRemaining({ utilisation, numberOfDaysRemainingInCoverPeriod, interestPercentage, dayCountBasis });
 };
