df00b1a5445a7d271254caa74abf4cd2969db13c
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-27 11:35:57 +0100

    feat(dtfs2-6892): add routes

--- trade-finance-manager-ui/server/routes/login/configs/loginSso.ts
+++ trade-finance-manager-ui/server/routes/login/configs/loginSso.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import * as loginSsoController from '../../../controllers/login/loginSso';
+import { LoginController } from '../../../controllers/login/loginSso/login.controller';
 import { getRouter } from '../../../types/get-router';
+import { EntraIdService } from '../../../services/entra-id.service';
+import { EntraIdConfig } from '../../../configs/entra-id.config';
+
+const entraIdConfig = new EntraIdConfig();
+const entraIdService = new EntraIdService(entraIdConfig);
+const loginController = new LoginController(entraIdService);
 
 export const getLoginSsoRouter: getRouter = () => {
   const loginSsoRouter = express.Router();
-  loginSsoRouter.get('/', loginSsoController.getLogin);
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  loginSsoRouter.get('/', (res, req) => loginController.getLogin(res, req));
 
   return loginSsoRouter;
 };
