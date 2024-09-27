4df7c963f5cce50f75636ea6f3fbc359f2f6099d
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-09-27 14:05:34 +0100

    feat(DTFS2-7423): update change from preview tests

--- e2e-tests/gef/cypress/e2e/facility/unissued-facilities/MIN/facility-end-date-disabled/unissued-facility-change-from-preview-MIN.spec.js
+++ e2e-tests/gef/cypress/e2e/facility/unissued-facilities/MIN/facility-end-date-disabled/unissued-facility-change-from-preview-MIN.spec.js
@@ -9,16 +9,14 @@ import dateConstants from '../../../../../../../e2e-fixtures/dateConstants';
 import { MOCK_APPLICATION_MIN } from '../../../../../fixtures/mocks/mock-deals';
 import { BANK1_MAKER1 } from '../../../../../../../e2e-fixtures/portal-users.fixture';
 import { multipleMockGefFacilities } from '../../../../../../../e2e-fixtures/mock-gef-facilities';
-import { mainHeading, continueButton, errorSummary } from '../../../../partials';
+import { continueButton, errorSummary } from '../../../../partials';
 import applicationPreview from '../../../../pages/application-preview';
 import unissuedFacilityTable from '../../../../pages/unissued-facilities';
 import aboutFacilityUnissued from '../../../../pages/unissued-facilities-about-facility';
 import applicationSubmission from '../../../../pages/application-submission';
-import statusBanner from '../../../../pages/application-status-banner';
 
 let dealId;
 let token;
-let facilityOneId;
 
 const { unissuedCashFacility, issuedCashFacility, unissuedContingentFacility, unissuedCashFacilityWith20MonthsOfCover } = multipleMockGefFacilities();
 
@@ -39,7 +37,6 @@ context('Unissued Facilities MIN - change to issued from preview page - feature 
           dealId = body._id;
           cy.apiUpdateApplication(dealId, token, MOCK_APPLICATION_MIN).then(() => {
             cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CASH, token).then((facility) => {
-              facilityOneId = facility.body.details._id;
               cy.apiUpdateFacility(facility.body.details._id, token, unissuedCashFacility);
             });
             cy.apiCreateFacility(dealId, CONSTANTS.FACILITY_TYPE.CASH, token).then((facility) =>
@@ -62,39 +59,6 @@ context('Unissued Facilities MIN - change to issued from preview page - feature 
       cy.saveSession();
       cy.login(BANK1_MAKER1);
       cy.visit(relative(`/gef/application-details/${dealId}`));
-    });
-
-    it('clicking unissued facilities link takes you to unissued facility list page', () => {
-      applicationPreview.unissuedFacilitiesReviewLink().click();
-      unissuedFacilityTable.updateFacilitiesLater().contains('Update facility stage later');
-      statusBanner.applicationBanner().should('exist');
-      cy.clickBackLink();
-      cy.url().should('eq', relative(`/gef/application-details/${dealId}`));
-    });
-
-    it('clicking on update should take you to the update facility page with correct url', () => {
-      applicationPreview.unissuedFacilitiesReviewLink().click();
-      unissuedFacilityTable.updateIndividualFacilityButton(0).click();
-      cy.url().should('eq', relative(`/gef/application-details/${dealId}/unissued-facilities/${facilityOneId}/about`));
-    });
-
-    it('update facility page should have correct titles and text (only name should be prepopulated', () => {
-      applicationPreview.unissuedFacilitiesReviewLink().click();
-      unissuedFacilityTable.updateIndividualFacilityButton(0).click();
-
-      mainHeading().contains("Tell us you've issued this facility");
-      aboutFacilityUnissued.facilityNameLabel().contains('Name for this cash facility');
-      aboutFacilityUnissued.facilityName().should('have.value', unissuedCashFacility.name);
-
-      aboutFacilityUnissued.issueDateDay().should('have.value', '');
-      aboutFacilityUnissued.issueDateMonth().should('have.value', '');
-      aboutFacilityUnissued.issueDateMonth().should('have.value', '');
-      aboutFacilityUnissued.coverStartDateDay().should('have.value', '');
-      aboutFacilityUnissued.coverStartDateMonth().should('have.value', '');
-      aboutFacilityUnissued.coverStartDateYear().should('have.value', '');
-      aboutFacilityUnissued.coverEndDateDay().should('have.value', '');
-      aboutFacilityUnissued.coverEndDateMonth().should('have.value', '');
-      aboutFacilityUnissued.coverEndDateYear().should('have.value', '');
     });
 
     it('should be able to update facility and then go back to application preview page', () => {
