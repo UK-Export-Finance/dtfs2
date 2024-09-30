2e0f67ac710efc01abae9ef0bee201944ab6255c
Alex Bramhill <alex.bramhill@softwire.com>
2024-09-30 09:57:01 +0100

    feat(dtfs2-6892): move DI creation to inside involked login router function

--- trade-finance-manager-ui/server/routes/login/configs/loginSso.ts
+++ trade-finance-manager-ui/server/routes/login/configs/loginSso.ts
@@ -5,12 +5,12 @@ import { EntraIdService } from '../../../services/entra-id.service';
 import { EntraIdConfig } from '../../../configs/entra-id.config';
 import { EntraIdApi } from '../../../apis/entra-id.api';
 
-const entraIdConfig = new EntraIdConfig();
-const entraIdApi = new EntraIdApi({ entraIdConfig });
-const entraIdService = new EntraIdService({ entraIdConfig, entraIdApi });
-const loginController = new LoginController({ entraIdService });
-
 export const getLoginSsoRouter: getRouter = () => {
+  const entraIdConfig = new EntraIdConfig();
+  const entraIdApi = new EntraIdApi({ entraIdConfig });
+  const entraIdService = new EntraIdService({ entraIdConfig, entraIdApi });
+  const loginController = new LoginController({ entraIdService });
+
   const loginSsoRouter = express.Router();
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   loginSsoRouter.get('/', (res, req) => loginController.getLogin(res, req));
