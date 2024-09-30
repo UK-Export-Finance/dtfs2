1bf58276444d80c5aac0f7da4175057eb06b92c3
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-09-30 16:44:49 +0100

    feat(FN-3450-51): added missing documentation

--- trade-finance-manager-ui/server/controllers/utilisation-reports/helpers/is-pdc-read-user.ts
+++ trade-finance-manager-ui/server/controllers/utilisation-reports/helpers/is-pdc-read-user.ts
@@ -1,4 +1,10 @@
 import { TEAM_IDS } from '@ukef/dtfs2-common';
 import { TfmSessionUser } from '../../../types/tfm-session-user';
 
+/**
+ * isPDCReadUser
+ * returns true if user is in PDC_READ team
+ * @param {TfmSessionUser} user
+ * @returns {Boolean}
+ */
 export const isPDCReadUser = (user: TfmSessionUser) => user.teams.includes(TEAM_IDS.PDC_READ);
