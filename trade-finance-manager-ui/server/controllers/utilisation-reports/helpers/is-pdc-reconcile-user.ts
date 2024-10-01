406402dbdb741be80615eab8affe210898bfeeca
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-10-01 14:36:21 +0100

    feat(FN-3450-3451): removed types from is-pdc-reconcile-user

--- trade-finance-manager-ui/server/controllers/utilisation-reports/helpers/is-pdc-reconcile-user.ts
+++ trade-finance-manager-ui/server/controllers/utilisation-reports/helpers/is-pdc-reconcile-user.ts
@@ -3,7 +3,7 @@ import { TfmSessionUser } from '../../../types/tfm-session-user';
 
 /**
  * returns true if user is in PDC_RECONCILE team
- * @param {TfmSessionUser} user
- * @returns {Boolean}
+ * @param user
+ * @returns whether the user is in the PDC_RECONCILE team
  */
 export const isPDCReconcileUser = (user: TfmSessionUser): boolean => user.teams.includes(TEAM_IDS.PDC_RECONCILE);
