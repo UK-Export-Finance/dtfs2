a885f1bfab7051e2cb76e0efa51f4f4940b4a866
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-10-01 13:21:52 +0100

    feat(FN-3272): changed from Number to number

--- dtfs-central-api/src/helpers/calculate-fixed-fee-from-days-remaining.ts
+++ dtfs-central-api/src/helpers/calculate-fixed-fee-from-days-remaining.ts
@@ -11,11 +11,11 @@ const BANK_ADMIN_FEE_ADJUSTMENT = 0.9;
  * calculateFixedFeeFromDaysRemaining
  * Calculates the fixed fee for the given parameters and number of days provided
  * @param param - The parameters to calculate the fixed fee with
- * @param {Number} param.utilisation - The facility utilisation
- * @param {Number} param.numberOfDaysRemainingInCoverPeriod - The number of days remaining in the cover period
- * @param {Number} param.interestPercentage - The facility interest percentage
- * @param {Number} param.dayCountBasis - The facility day count basis
- * @returns {Number} The fixed fee for the current report period
+ * @param {number} param.utilisation - The facility utilisation
+ * @param {number} param.numberOfDaysRemainingInCoverPeriod - The number of days remaining in the cover period
+ * @param {number} param.interestPercentage - The facility interest percentage
+ * @param {number} param.dayCountBasis - The facility day count basis
+ * @returns {number} The fixed fee for the current report period
  */
 export const calculateFixedFeeFromDaysRemaining = ({
   utilisation,
