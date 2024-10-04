98a25a55cb05ec7029b0cc9cb11571b3a93c6dde
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-10-04 11:40:50 +0100

    feat(FN-2311): test currency radio item mapping

---
+++ trade-finance-manager-ui/server/helpers/map-currencies-to-radio-items.test.ts
@@ -0,0 +1,109 @@
+import { CURRENCY } from '@ukef/dtfs2-common';
+import { mapCurrenciesToRadioItems } from './map-currencies-to-radio-items';
+
+describe('map currencies to radio items helper', () => {
+  describe('mapCurrenciesToRadioItems', () => {
+    describe('when called without a checked currency', () => {
+      it('should return an array of RadioItem objects for all currencies', () => {
+        // Act
+        const currencyRadioItems = mapCurrenciesToRadioItems();
+
+        // Assert
+        expect(currencyRadioItems.length).toBe(Object.values(CURRENCY).length);
+        expect(currencyRadioItems).toEqual([
+          {
+            text: CURRENCY.GBP,
+            value: CURRENCY.GBP,
+            checked: false,
+            attributes: {
+              'data-cy': 'currency-GBP',
+            },
+          },
+          {
+            text: CURRENCY.EUR,
+            value: CURRENCY.EUR,
+            checked: false,
+            attributes: {
+              'data-cy': 'currency-EUR',
+            },
+          },
+          {
+            text: CURRENCY.USD,
+            value: CURRENCY.USD,
+            checked: false,
+            attributes: {
+              'data-cy': 'currency-USD',
+            },
+          },
+          {
+            text: CURRENCY.JPY,
+            value: CURRENCY.JPY,
+            checked: false,
+            attributes: {
+              'data-cy': 'currency-JPY',
+            },
+          },
+        ]);
+      });
+    });
+
+    describe('when called with a checked currency', () => {
+      it('should return an array of RadioItem objects with only the specified currency checked', () => {
+        // Arrange
+        const checkedCurrency = CURRENCY.GBP;
+
+        // Act
+        const currencyRadioItems = mapCurrenciesToRadioItems(checkedCurrency);
+
+        // Assert
+        expect(currencyRadioItems).toEqual([
+          {
+            text: CURRENCY.GBP,
+            value: CURRENCY.GBP,
+            checked: true,
+            attributes: {
+              'data-cy': 'currency-GBP',
+            },
+          },
+          {
+            text: CURRENCY.EUR,
+            value: CURRENCY.EUR,
+            checked: false,
+            attributes: {
+              'data-cy': 'currency-EUR',
+            },
+          },
+          {
+            text: CURRENCY.USD,
+            value: CURRENCY.USD,
+            checked: false,
+            attributes: {
+              'data-cy': 'currency-USD',
+            },
+          },
+          {
+            text: CURRENCY.JPY,
+            value: CURRENCY.JPY,
+            checked: false,
+            attributes: {
+              'data-cy': 'currency-JPY',
+            },
+          },
+        ]);
+      });
+    });
+
+    describe('when called with an invalid checked currency', () => {
+      it('should return an array of RadioItem objects with no currency checked', () => {
+        // Arrange
+        const checkedCurrency = 'INVALID_CURRENCY';
+
+        // Act
+        const currencyRadioItems = mapCurrenciesToRadioItems(checkedCurrency);
+
+        // Assert
+        expect(currencyRadioItems.every((currency) => !currency.checked)).toBe(true);
+      });
+    });
+  });
+});
--- trade-finance-manager-ui/server/helpers/map-currencies-to-radio-items.ts
+++ trade-finance-manager-ui/server/helpers/map-currencies-to-radio-items.ts
@@ -1,7 +1,12 @@
 import { CURRENCY, RadioItem } from '@ukef/dtfs2-common';
 
-// TODO FN-2311: Add docs.
-// TODO FN-2311: Add unit tests for this function.
+/**
+ * Maps currencies to radio items for form input.
+ * Uses the @link{CURRENCY} enum to get all available currencies.
+ * @param checkedCurrency - The currency radio item to be marked as checked
+ * (optional)
+ * @returns An array of RadioItem objects representing currency options
+ */
 export const mapCurrenciesToRadioItems = (checkedCurrency?: string): RadioItem[] => {
   const currencies = Object.values(CURRENCY);
 
