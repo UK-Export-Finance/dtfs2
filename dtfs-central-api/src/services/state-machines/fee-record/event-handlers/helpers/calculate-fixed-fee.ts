d99efd4e6799b25e45cd204c250dda2ff3d96370
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2024-10-08 17:13:17 +0100

    fix(FN-3422): add documentation to function

--- dtfs-central-api/src/services/state-machines/fee-record/event-handlers/helpers/calculate-fixed-fee.ts
+++ dtfs-central-api/src/services/state-machines/fee-record/event-handlers/helpers/calculate-fixed-fee.ts
@@ -8,6 +8,13 @@ import { getDateFromMonthAndYear, MonthAndYear, ReportPeriod } from '@ukef/dtfs2
  */
 const BANK_ADMIN_FEE_ADJUSTMENT = 0.9;
 
+/**
+ * Gets the number of days remaining in cover period by calculating difference
+ * between the day after the end of the report period and the cover end date
+ * @param reportPeriodEnd - The report period end
+ * @param coverEndDate - The cover end date of the facility
+ * @returns The number of days remaining in the cover period
+ */
 const getNumberOfDaysRemainingInCoverPeriod = (reportPeriodEnd: MonthAndYear, coverEndDate: Date): number => {
   const endDateOfReportPeriod = startOfDay(endOfMonth(getDateFromMonthAndYear(reportPeriodEnd)));
   const startDateOfNextReportPeriod = addDays(endDateOfReportPeriod, 1);
