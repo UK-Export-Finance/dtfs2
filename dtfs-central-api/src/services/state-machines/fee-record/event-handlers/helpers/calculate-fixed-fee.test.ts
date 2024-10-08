65781c7c0b74ebc4d6c90e5660e8ec025f5bb1f6
Christian McCaffery <cmccaffery@ukexportfinance.gov.uk>
2024-10-08 09:37:58 +0100

    fix(FN-3422): remove edge case covering cover start date being after report period start

--- dtfs-central-api/src/services/state-machines/fee-record/event-handlers/helpers/calculate-fixed-fee.test.ts
+++ dtfs-central-api/src/services/state-machines/fee-record/event-handlers/helpers/calculate-fixed-fee.test.ts
@@ -1,12 +1,9 @@
-import { addDays, addMonths, subMonths } from 'date-fns';
 import { ReportPeriod } from '@ukef/dtfs2-common';
 import { calculateFixedFee } from './calculate-fixed-fee';
 
 console.error = jest.fn();
 
 describe('calculateFixedFeeAdjustment', () => {
-  const TODAY = new Date();
-
   const getReportPeriodForDate = (date: Date): ReportPeriod => ({
     start: { month: date.getMonth() + 1, year: date.getFullYear() },
     end: { month: date.getMonth() + 1, year: date.getFullYear() },
@@ -16,62 +13,30 @@ describe('calculateFixedFeeAdjustment', () => {
     jest.resetAllMocks();
   });
 
-  describe('when the report period ends before the cover start date', () => {
-    it('returns the product of the utilisation, interest percentage (divided by 100), bank admin fee percentage (fixed at 0.9) and number of days remaining in the cover period divided by the day count basis and rounds to 2 decimal places', () => {
-      // Arrange
-      const utilisation = 100000;
-      const interestPercentage = 5;
-      const dayCountBasis = 365;
+  it('returns the product of the utilisation, interest percentage (divided by 100), bank admin fee percentage (fixed at 0.9) and number of days remaining in the cover period divided by the day count basis and rounds to 2 decimal places', () => {
+    // Arrange
+    const utilisation = 100000;
+    const interestPercentage = 5;
+    const dayCountBasis = 365;
 
-      const reportPeriod = getReportPeriodForDate(TODAY);
-      const coverStartDateAfterReportPeriod = addMonths(TODAY, 1);
+    const aDate = new Date(2024, 0, 1);
 
-      // Number of days remaining = 368
-      const coverEndDate = addDays(coverStartDateAfterReportPeriod, 368);
+    const reportPeriod = getReportPeriodForDate(aDate);
 
-      // Act
-      const result = calculateFixedFee({
-        utilisation,
-        interestPercentage,
-        dayCountBasis,
-        reportPeriod,
-        coverStartDate: coverStartDateAfterReportPeriod,
-        coverEndDate,
-      });
+    // Number of days of cover from end of report period to cover end date
+    // is days between 1st Feb 2024 and 1st Sep 2024 = 213
+    const coverEndDate = new Date(2024, 8, 1);
 
-      // Assert
-      expect(result).toBe(4536.99); // 100000 * (5 / 100) * 0.9 * 368 / 365 = 4,536.98630... = 4,536.99 (ROUNDED)
+    // Act
+    const result = calculateFixedFee({
+      utilisation,
+      interestPercentage,
+      dayCountBasis,
+      reportPeriod,
+      coverEndDate,
     });
-  });
 
-  describe('when the report period ends after the cover start date', () => {
-    it('returns the product of the utilisation, interest percentage (divided by 100), bank admin fee percentage (fixed at 0.9) and number of days remaining in the cover period divided by the day count basis and rounds to 2 decimal places', () => {
-      // Arrange
-      const utilisation = 100000;
-      const interestPercentage = 5;
-      const dayCountBasis = 365;
-
-      const aDate = new Date(2024, 0, 1);
-
-      const reportPeriod = getReportPeriodForDate(aDate);
-      const coverStartDateBeforeReportPeriod = subMonths(aDate, 3);
-
-      // Number of days of cover from end of report period to cover end date
-      // is days between 1st Feb 2024 and 1st Sep 2024 = 213
-      const coverEndDate = new Date(2024, 8, 1);
-
-      // Act
-      const result = calculateFixedFee({
-        utilisation,
-        interestPercentage,
-        dayCountBasis,
-        reportPeriod,
-        coverStartDate: coverStartDateBeforeReportPeriod,
-        coverEndDate,
-      });
-
-      // Assert
-      expect(result).toBe(2626.03); // 100000 * (5 / 100) * 0.9 * 213 / 365 = 2,626.0274 = 2,626.03 (ROUNDED)
-    });
+    // Assert
+    expect(result).toBe(2626.03); // 100000 * (5 / 100) * 0.9 * 213 / 365 = 2,626.0274 = 2,626.03 (ROUNDED)
   });
 });
--- dtfs-central-api/src/services/state-machines/fee-record/event-handlers/helpers/calculate-fixed-fee.ts
+++ dtfs-central-api/src/services/state-machines/fee-record/event-handlers/helpers/calculate-fixed-fee.ts
@@ -1,5 +1,5 @@
 import Big from 'big.js';
-import { differenceInDays, isBefore, endOfMonth, addDays, startOfDay } from 'date-fns';
+import { differenceInDays, endOfMonth, addDays, startOfDay } from 'date-fns';
 import { getDateFromMonthAndYear, MonthAndYear, ReportPeriod } from '@ukef/dtfs2-common';
 
 /**
@@ -8,20 +8,16 @@ import { getDateFromMonthAndYear, MonthAndYear, ReportPeriod } from '@ukef/dtfs2
  */
 const BANK_ADMIN_FEE_ADJUSTMENT = 0.9;
 
-const getNumberOfDaysRemainingInCoverPeriod = (reportPeriodEnd: MonthAndYear, coverStartDate: Date, coverEndDate: Date): number => {
+const getNumberOfDaysRemainingInCoverPeriod = (reportPeriodEnd: MonthAndYear, coverEndDate: Date): number => {
   const endDateOfReportPeriod = startOfDay(endOfMonth(getDateFromMonthAndYear(reportPeriodEnd)));
   const startDateOfNextReportPeriod = addDays(endDateOfReportPeriod, 1);
 
-  if (isBefore(startDateOfNextReportPeriod, coverStartDate)) {
-    return differenceInDays(coverEndDate, coverStartDate);
-  }
   return differenceInDays(coverEndDate, startDateOfNextReportPeriod);
 };
 
 export type CalculateFixedFeeParams = {
   utilisation: number;
   reportPeriod: ReportPeriod;
-  coverStartDate: Date;
   coverEndDate: Date;
   interestPercentage: number;
   dayCountBasis: number;
@@ -32,21 +28,13 @@ export type CalculateFixedFeeParams = {
  * @param param - The parameters to calculate the fixed fee with
  * @param param.utilisation - The facility utilisation
  * @param param.reportPeriod - The report period
- * @param param.coverStartDate - The facility cover start date
  * @param param.coverEndDate - The facility cover end date
  * @param param.interestPercentage - The facility interest percentage
  * @param param.dayCountBasis - The facility day count basis
  * @returns The fixed fee for the current report period
  */
-export const calculateFixedFee = ({
-  utilisation,
-  reportPeriod,
-  coverStartDate,
-  coverEndDate,
-  interestPercentage,
-  dayCountBasis,
-}: CalculateFixedFeeParams): number => {
-  const numberOfDaysRemainingInCoverPeriod = getNumberOfDaysRemainingInCoverPeriod(reportPeriod.end, coverStartDate, coverEndDate);
+export const calculateFixedFee = ({ utilisation, reportPeriod, coverEndDate, interestPercentage, dayCountBasis }: CalculateFixedFeeParams): number => {
+  const numberOfDaysRemainingInCoverPeriod = getNumberOfDaysRemainingInCoverPeriod(reportPeriod.end, coverEndDate);
   const interestPercentageAsDecimal = new Big(interestPercentage).div(100);
   return new Big(utilisation)
     .mul(interestPercentageAsDecimal)
