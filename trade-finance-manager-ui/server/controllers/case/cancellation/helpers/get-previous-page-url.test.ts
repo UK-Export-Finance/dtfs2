595cd6b8b263a2e7d18f76077a184e27a7a61067
MarRobSoftwire <108682750+MarRobSoftwire@users.noreply.github.com>
2024-10-09 12:33:16 +0100

    feat(DTFS2-7449): add cancel cancellation POST endpoint (#3643)

---
+++ trade-finance-manager-ui/server/controllers/case/cancellation/helpers/get-previous-page-url.test.ts
@@ -0,0 +1,53 @@
+import { getPreviousPageUrl } from './get-previous-page-url';
+
+const previousPages = ['reason', 'bank-request-date', 'effective-from-date', 'check-details'];
+
+const dealId = 'abcdef123456';
+
+describe('getPreviousPageUrl', () => {
+  describe.each(previousPages)('when the provided url is for the %s page', (page) => {
+    it('returns the correct relative url, when given a relative URL', () => {
+      // Arrange
+      const url = `/case/${dealId}/cancellation/${page}`;
+
+      // Act
+      const response = getPreviousPageUrl(url, dealId);
+
+      // Assert
+      expect(response).toBe(`/case/${dealId}/cancellation/${page}`);
+    });
+
+    it('returns the correct relative url, when given an absolute URL', () => {
+      // Arrange
+      const url = `www.example.com/case/${dealId}/cancellation/${page}`;
+
+      // Act
+      const response = getPreviousPageUrl(url, dealId);
+
+      // Assert
+      expect(response).toBe(`/case/${dealId}/cancellation/${page}`);
+    });
+
+    it('returns the correct relative url, when given a localhost URL', () => {
+      // Arrange
+      const url = `localhost:5003/case/${dealId}/cancellation/${page}`;
+
+      // Act
+      const response = getPreviousPageUrl(url, dealId);
+
+      // Assert
+      expect(response).toBe(`/case/${dealId}/cancellation/${page}`);
+    });
+  });
+
+  it('returns a URL for the deal summary page if the provided URL is not recognised', () => {
+    // Arrange
+    const url = `/case/${dealId}/cancellation/`;
+
+    // Act
+    const response = getPreviousPageUrl(url, dealId);
+
+    // Assert
+    expect(response).toBe(`/case/${dealId}/deal`);
+  });
+});
---
+++ trade-finance-manager-ui/server/controllers/case/cancellation/helpers/get-previous-page-url.ts
@@ -0,0 +1,26 @@
+/**
+ * Validates the if the provided URL is one of
+ *  - reason for cancelling page
+ *  - bank request date page
+ *  - effective from date page
+ *  - check details page
+ *
+ * and returns a new relative URL for that page.
+ * Otherwise, returns the deal summary page URL.
+ * @param url - The URL of the previous page, provided in the post request
+ * @param dealId - The deal id
+ * @returns the previous page URL
+ */
+export const getPreviousPageUrl = (url: string, dealId: string): string => {
+  const previousPages = ['reason', 'bank-request-date', 'effective-from-date', 'check-details'];
+
+  const previousPageUrl = previousPages.reduce((existingValue: string | undefined, currentValue: string) => {
+    if (url.includes(`/case/${dealId}/cancellation/${currentValue}`)) {
+      return `/case/${dealId}/cancellation/${currentValue}`;
+    }
+
+    return existingValue;
+  }, undefined);
+
+  return previousPageUrl ?? `/case/${dealId}/deal`;
+};
