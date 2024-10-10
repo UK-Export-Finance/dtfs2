eb6b48265b99cec4151870c34141b54c24ac43c8
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-10-10 11:53:08 +0100

    feat(FN-2311): move defined selected filters into default params

--- trade-finance-manager-ui/component-tests/utilisation-reports/_macros/payment-details-filters-panel.component-test.ts
+++ trade-finance-manager-ui/component-tests/utilisation-reports/_macros/payment-details-filters-panel.component-test.ts
@@ -10,7 +10,19 @@ describe(component, () => {
       paymentReference: '',
       facilityId: '',
     },
-    selectedFilters: [],
+    selectedFilters: [
+      {
+        heading: {
+          text: 'Field 1',
+        },
+        items: [
+          {
+            href: '/path/to/remove/item',
+            text: 'Value 1',
+          },
+        ],
+      },
+    ],
   };
 
   const filtersContainerSelector = '[data-cy="payment-details--filters-container"]';
@@ -34,30 +46,18 @@ describe(component, () => {
   });
 
   it('should render the clear filters button when no selected filters are provided', () => {
-    const wrapper = render(defaultParams);
+    const params = {
+      ...defaultParams,
+      selectedFilters: [],
+    };
+
+    const wrapper = render(params);
 
     wrapper.expectElement('[data-cy="payment-details--clear-filters-button"]').toExist();
   });
 
   it('should not render the clear filters button when selected filters are provided', () => {
-    const params = {
-      ...defaultParams,
-      selectedFilters: [
-        {
-          heading: {
-            text: 'Field 1',
-          },
-          items: [
-            {
-              href: '/path/to/remove/item',
-              text: 'Value 1',
-            },
-          ],
-        },
-      ],
-    };
-
-    const wrapper = render(params);
+    const wrapper = render(defaultParams);
 
     wrapper.expectElement('[data-cy="payment-details--clear-filters-button"]').notToExist();
   });
@@ -69,24 +69,7 @@ describe(component, () => {
   });
 
   it('should render selected filters when provided', () => {
-    const params = {
-      ...defaultParams,
-      selectedFilters: [
-        {
-          heading: {
-            text: 'Field 1',
-          },
-          items: [
-            {
-              href: '/path/to/remove/item',
-              text: 'Value 1',
-            },
-          ],
-        },
-      ],
-    };
-
-    const wrapper = render(params);
+    const wrapper = render(defaultParams);
 
     wrapper.expectText(`${filtersContainerSelector} .moj-filter__heading-title`).toRead('Selected filters');
     wrapper.expectText(`${filtersContainerSelector} .moj-filter__heading-action`).toRead('Clear filters');
