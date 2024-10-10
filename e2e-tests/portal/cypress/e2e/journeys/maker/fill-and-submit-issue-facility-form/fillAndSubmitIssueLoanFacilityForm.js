4487864738bb2207f34dd9718e85416ad02b3383
Beth Thomas <beth.thomas@softwire.com>
2024-10-10 14:24:40 +0100

    feat(DTFS2-7296): fixes

--- e2e-tests/portal/cypress/e2e/journeys/maker/fill-and-submit-issue-facility-form/fillAndSubmitIssueLoanFacilityForm.js
+++ e2e-tests/portal/cypress/e2e/journeys/maker/fill-and-submit-issue-facility-form/fillAndSubmitIssueLoanFacilityForm.js
@@ -1,15 +1,15 @@
 const pages = require('../../../pages');
 const { oneMonth, today, twoDays } = require('../../../../../../e2e-fixtures/dateConstants');
 
-export const ISSUED_LOAN_DATE_VALUE = today;
-export const COVER_START_DATE_VALUE = twoDays;
-export const COVER_END_DATE_VALUE = oneMonth;
+export const ISSUED_LOAN_DATE_VALUE = today.date;
+export const COVER_START_DATE_VALUE = twoDays.date;
+export const COVER_END_DATE_VALUE = oneMonth.date;
 export const DISBURSEMENT_AMOUNT_VALUE = '50000';
 
 export const fillAndSubmitIssueLoanFacilityFormWithoutRequestedCoverStartDate = () => {
   cy.completeDateFormFields({ idPrefix: 'issuedDate' });
 
-  cy.completeDateFormFields({ idPrefix: 'coverEndDate', date: oneMonth.date });
+  cy.completeDateFormFields({ idPrefix: 'coverEndDate', date: COVER_END_DATE_VALUE });
 
   cy.keyboardInput(pages.loanIssueFacility.disbursementAmount(), DISBURSEMENT_AMOUNT_VALUE);
   cy.keyboardInput(pages.loanIssueFacility.name(), '5678');
@@ -20,9 +20,9 @@ export const fillAndSubmitIssueLoanFacilityFormWithoutRequestedCoverStartDate = 
 export const fillAndSubmitIssueLoanFacilityForm = () => {
   cy.completeDateFormFields({ idPrefix: 'issuedDate' });
 
-  cy.completeDateFormFields({ idPrefix: 'requestedCoverStartDate', date: twoDays.date });
+  cy.completeDateFormFields({ idPrefix: 'requestedCoverStartDate', date: COVER_START_DATE_VALUE });
 
-  cy.completeDateFormFields({ idPrefix: 'coverEndDate', date: oneMonth.date });
+  cy.completeDateFormFields({ idPrefix: 'coverEndDate', date: COVER_END_DATE_VALUE });
 
   cy.keyboardInput(pages.loanIssueFacility.disbursementAmount(), DISBURSEMENT_AMOUNT_VALUE);
   cy.keyboardInput(pages.loanIssueFacility.name(), '5678');
