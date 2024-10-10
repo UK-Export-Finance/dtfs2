8964954a95b7eff5bea7a8a13f26e47c1528fbb5
Alex Bramhill <alex.bramhill@softwire.com>
2024-10-10 18:25:08 +0100

    feat(dtfs2-6892): add entra id schemas

---
+++ trade-finance-manager-ui/server/schemas/entra-id-user.schema.ts
@@ -0,0 +1,17 @@
+/**
+ * Represents user fields in the ID Token claims response from Entra ID.
+ * Note: there are many more fields in the ID Token claims response, but these
+ * are the only ones we care about
+ */
+
+import z from 'zod';
+import { ALL_ENTRA_ID_USER_GROUPS } from '../constants/entra-id-user';
+
+export const EntraIdUserSchema = z.object({
+  oid: z.string(),
+  verified_primary_email: z.array(z.string()),
+  verified_secondary_email: z.array(z.string()),
+  given_name: z.string(),
+  family_name: z.string(),
+  groups: z.array(z.enum(ALL_ENTRA_ID_USER_GROUPS)), // TODO: DTFS2-6892: this is using existing roles, but could be improved
+});
