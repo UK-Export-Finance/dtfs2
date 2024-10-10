55e4fec1c1adc7ffe1d5a3c83816cbcf59036c85
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-10-10 14:42:16 +0100

    feat(FN-2994): remove return for email

--- dtfs-central-api/src/external-api/send-email.ts
+++ dtfs-central-api/src/external-api/send-email.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import axios from 'axios';
 import dotenv from 'dotenv';
 import { HEADERS, AnyObject } from '@ukef/dtfs2-common';
@@ -16,7 +15,7 @@ const { EXTERNAL_API_URL, EXTERNAL_API_KEY } = process.env;
  */
 export const sendEmail = async (templateId: string, sendToEmailAddress: string, emailVariables: AnyObject) => {
   try {
-    const response = await axios({
+    await axios({
       method: 'post',
       url: `${EXTERNAL_API_URL}/email`,
       headers: {
@@ -29,8 +28,6 @@ export const sendEmail = async (templateId: string, sendToEmailAddress: string, 
         emailVariables,
       },
     });
-
-    return response.data;
   } catch (error) {
     console.error('Error sending email to %s %o', sendToEmailAddress, error);
 
--- dtfs-central-api/src/v1/email/index.ts
+++ dtfs-central-api/src/v1/email/index.ts
@@ -1,12 +1,9 @@
 import { AnyObject } from '@ukef/dtfs2-common';
-import { Response } from 'express';
 import externalApi from '../../external-api/api';
 
 export const sendEmail = async (templateId: string, sendToEmailAddress: string, emailVariables: AnyObject) => {
   try {
-    const emailSent = (await externalApi.sendEmail(templateId, sendToEmailAddress, emailVariables)) as Response;
-
-    return emailSent;
+    await externalApi.sendEmail(templateId, sendToEmailAddress, emailVariables);
   } catch (error) {
     console.error('DTFS-Central API - Failed to send email %o', error);
 
