81026973b30c02dbf30c8252ac2c35fe50168b32
Beth Thomas <beth.thomas@softwire.com>
2024-10-11 15:30:08 +0100

    feat(DTFS2-7296): additional improvements

--- e2e-tests/portal/cypress/e2e/journeys/maker-loan/delete-a-loan.spec.js
+++ e2e-tests/portal/cypress/e2e/journeys/maker-loan/delete-a-loan.spec.js
@@ -2,10 +2,11 @@ const pages = require('../../pages');
 const partials = require('../../partials');
 const relative = require('../../relativeURL');
 const MOCK_USERS = require('../../../../../e2e-fixtures');
+const { today } = require('../../../../../e2e-fixtures/dateConstants');
 
 const { ADMIN, BANK1_MAKER1 } = MOCK_USERS;
 
-const now = new Date().valueOf();
+const now = today.unixMilliseconds;
 
 const MOCK_DEAL = {
   bankInternalRefName: 'someDealId',
@@ -27,7 +28,7 @@ const MOCK_DEAL = {
       ukefGuaranteeInMonths: '12',
       name: '',
       guaranteeFeePayableByBank: '18.0000',
-      updatedAt: Date.now(),
+      updatedAt: now,
       value: '1234.00',
       currencySameAsSupplyContractCurrency: 'true',
       interestMarginFee: '20',
@@ -46,7 +47,7 @@ const MOCK_DEAL = {
       ukefGuaranteeInMonths: '12',
       name: '',
       guaranteeFeePayableByBank: '18.0000',
-      updatedAt: Date.now(),
+      updatedAt: now,
       value: '1234.00',
       currencySameAsSupplyContractCurrency: 'true',
       interestMarginFee: '20',
@@ -65,7 +66,7 @@ const MOCK_DEAL = {
       ukefGuaranteeInMonths: '12',
       name: '',
       guaranteeFeePayableByBank: '18.0000',
-      updatedAt: Date.now(),
+      updatedAt: now,
       value: '1234.00',
       currencySameAsSupplyContractCurrency: 'true',
       interestMarginFee: '20',
