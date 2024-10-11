0ab38f7978763ccc63cacb6605fed47aa8d08b41
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2024-10-11 11:11:29 +0100

    feat(FN-3272): store initial utilisation and fee based on supplied and facility values (#3589)

---
+++ dtfs-central-api/src/helpers/calculate-fixed-fee-from-days-remaining.test.ts
@@ -0,0 +1,25 @@
+import { calculateFixedFeeFromDaysRemaining } from './calculate-fixed-fee-from-days-remaining';
+
+describe('calculateFixedFeeFromDaysRemaining', () => {
+  it('should return the product of the utilisation, interest percentage (divided by 100), bank admin fee percentage (fixed at 0.9) and number of days remaining in the cover period divided by the day count basis and rounds to 2 decimal places', () => {
+    // Arrange
+    const utilisation = 100000;
+    const interestPercentage = 5;
+    const dayCountBasis = 365;
+
+    // Number of days remaining = 432
+    const numberOfDaysRemainingInCoverPeriod = 432;
+
+    // Act
+    const result = calculateFixedFeeFromDaysRemaining({
+      utilisation,
+      numberOfDaysRemainingInCoverPeriod,
+      interestPercentage,
+      dayCountBasis,
+    });
+
+    // Assert
+    // utilisation * (interestPercentage / 100) * BANK_ADMIN_FEE_ADJUSTMENT * numberOfDaysRemainingInCoverPeriod / dayCountBasis = 5,326.02739... = 5,326.03 (ROUNDED)
+    expect(result).toEqual(5326.03);
+  });
+});
