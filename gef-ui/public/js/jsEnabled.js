7a23c7123fadbf3cf9a2e660409d6e55e9d1150d
ttbarnes <tony@tonybarnes.me>
2024-10-15 17:03:42 +0100

    chore(DTFS2-7531-7535): bump gov design library

--- gef-ui/public/js/jsEnabled.js
+++ gef-ui/public/js/jsEnabled.js
@@ -1,4 +1,2 @@
-var DTFS_GEF;
-(document.body.className = document.body.className ? ''.concat(document.body.className, ' js-enabled') : 'js-enabled'),
-  ((DTFS_GEF = void 0 === DTFS_GEF ? {} : DTFS_GEF).jsEnabled = {});
+var DTFS_GEF;document.body.className+=" js-enabled"+("noModule"in HTMLScriptElement.prototype?" govuk-frontend-supported":""),(DTFS_GEF=void 0===DTFS_GEF?{}:DTFS_GEF).jsEnabled={};
 //# sourceMappingURL=jsEnabled.js.map
