369a8ebcd143813fe2a64b971724e4e09b9bfe26
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-10-16 12:42:27 +0100

    feat(FN-3504): code improvement to use big

--- dtfs-central-api/src/helpers/calculate-ukef-share-of-utilisation.test.ts
+++ dtfs-central-api/src/helpers/calculate-ukef-share-of-utilisation.test.ts
@@ -9,4 +9,15 @@ describe('calculateUkefShareOfUtilisation', () => {
 
     expect(result).toEqual(5);
   });
+
+  it('should return the ukef share of the utilisation when the result is a decimal', () => {
+    const utilisation = 52;
+    const coverPercentage = 35;
+
+    const result = calculateUkefShareOfUtilisation(utilisation, coverPercentage);
+
+    const expected = utilisation * (coverPercentage / 100);
+
+    expect(result).toEqual(expected);
+  });
 });
--- dtfs-central-api/src/helpers/calculate-ukef-share-of-utilisation.ts
+++ dtfs-central-api/src/helpers/calculate-ukef-share-of-utilisation.ts
@@ -1,7 +1,13 @@
+import Big from 'big.js';
+
 /**
  * calculates the ukef share of the utilisation
  * @param utilisation - provided utilisation value
  * @param coverPercentage - facility cover percentage
  * @returns returns calculated ukef share of utilisation
  */
-export const calculateUkefShareOfUtilisation = (utilisation: number, coverPercentage: number): number => utilisation * (coverPercentage / 100);
+export const calculateUkefShareOfUtilisation = (utilisation: number, coverPercentage: number): number => {
+  const coverPercentageAsDecimal = new Big(coverPercentage).div(100);
+
+  return new Big(utilisation).mul(coverPercentageAsDecimal).round(2).toNumber();
+};
