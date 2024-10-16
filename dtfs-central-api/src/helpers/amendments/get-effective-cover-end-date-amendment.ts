f602a8ae2647fb3a870f4fd6d1c451d75696112f
Ruby Lavender <ruby.lavender@softwire.com>
2024-10-16 10:48:14 +0100

    feat(FN-3519): correct and add more detail to docstrings

--- dtfs-central-api/src/helpers/amendments/get-effective-cover-end-date-amendment.ts
+++ dtfs-central-api/src/helpers/amendments/get-effective-cover-end-date-amendment.ts
@@ -4,10 +4,15 @@ import { filterAndSortCompletedEffectiveAmendments } from './filter-and-sort-com
 
 /**
  * Get the completed amendment cover end date that is effective
- * on the effective at date if there are any
+ * on the effective at date if there are any.
+ *
+ * The effective amendment at a given date is the amendment which:
+ * - Includes a change to cover end date
+ * - Is completed
+ * - Has the latest effective date before the effective at date
  * @param tfmFacility - The tfm facility
  * @param effectiveAtDate - The date to find the effective amendment for
- * @returns The latest completed amendment cover end date
+ * @returns The effective amendment cover end date if there is one
  */
 export const getEffectiveCoverEndDateAmendment = (tfmFacility: TfmFacility, effectiveAtDate: Date): Date | null => {
   const { amendments } = tfmFacility;
--- dtfs-central-api/src/helpers/amendments/get-effective-facility-value-amendment.ts
+++ dtfs-central-api/src/helpers/amendments/get-effective-facility-value-amendment.ts
@@ -3,10 +3,15 @@ import { filterAndSortCompletedEffectiveAmendments } from './filter-and-sort-com
 
 /**
  * Get the completed amendment facility value that is effective
- * on the effective at date if there are any
+ * on the effective at date if there are any.
+ *
+ * The effective amendment at a given date is the amendment which:
+ * - Includes a change to facility value
+ * - Is completed
+ * - Has the latest effective date before the effective at date
  * @param tfmFacility - The tfm facility
  * @param effectiveAtDate - The date to find the effective amendment for
- * @returns The latest completed amendment cover end date
+ * @returns The effective amendment to facility value if there is one
  */
 export const getEffectiveFacilityValueAmendment = (tfmFacility: TfmFacility, effectiveAtDate: Date): number | null => {
   const { amendments } = tfmFacility;
