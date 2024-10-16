d72c828dca90c20ec8328f4b2b722ded75f88675
Beth Thomas <beth.thomas@softwire.com>
2024-10-16 09:40:04 +0100

    feat(DTFS2-7296): review mark ups

--- e2e-tests/e2e-fixtures/dateConstants.js
+++ e2e-tests/e2e-fixtures/dateConstants.js
@@ -11,56 +11,54 @@ export const DD_MMMM_YYYY_FORMAT = 'dd MMMM yyyy';
 export const TIME_HOURS_FORMAT = 'h';
 export const TIME_AM_PM_FORMAT = 'aaa';
 
-const getFormattedValuesFromDate = (date) => {
-  return {
-    date,
-    day: format(date, SHORT_DAY_FORMAT),
-    dayLong: format(date, LONG_DAY_FORMAT),
-    month: format(date, SHORT_MONTH_FORMAT),
-    monthLong: format(date, LONG_MONTH_FORMAT),
-    year: format(date, LONG_YEAR_FORMAT),
-    dd_MMM_yyyy: format(date, DD_MMM_YYYY_FORMAT),
-    d_MMMM_yyyy: format(date, D_MMMM_YYYY_FORMAT),
-    dd_MMMM_yyyy: format(date, DD_MMMM_YYYY_FORMAT),
-    unixSecondsString: getUnixTime(date).toString(),
-    unixMillisecondsString: date.valueOf().toString(),
-    unixMilliseconds: date.valueOf(),
-  };
-};
+const getFormattedValues = (date) => ({
+  date,
+  day: format(date, SHORT_DAY_FORMAT),
+  dayLong: format(date, LONG_DAY_FORMAT),
+  month: format(date, SHORT_MONTH_FORMAT),
+  monthLong: format(date, LONG_MONTH_FORMAT),
+  year: format(date, LONG_YEAR_FORMAT),
+  dd_MMM_yyyy: format(date, DD_MMM_YYYY_FORMAT),
+  d_MMMM_yyyy: format(date, D_MMMM_YYYY_FORMAT),
+  dd_MMMM_yyyy: format(date, DD_MMMM_YYYY_FORMAT),
+  unixSecondsString: getUnixTime(date).toString(),
+  unixMillisecondsString: date.valueOf().toString(),
+  unixMilliseconds: date.valueOf(),
+});
 
 const todayDate = new Date();
 
 // Today
-export const today = getFormattedValuesFromDate(todayDate);
+export const today = getFormattedValues(todayDate);
 
 // Dates in the future
-export const tomorrow = getFormattedValuesFromDate(add(todayDate, { days: 1 }));
-export const twoDays = getFormattedValuesFromDate(add(todayDate, { days: 2 }));
-export const threeDays = getFormattedValuesFromDate(add(todayDate, { days: 3 }));
-export const sevenDays = getFormattedValuesFromDate(add(todayDate, { days: 7 }));
-export const twentyEightDays = getFormattedValuesFromDate(add(todayDate, { days: 28 }));
-export const oneMonth = getFormattedValuesFromDate(add(todayDate, { months: 1 }));
-export const twoMonths = getFormattedValuesFromDate(add(todayDate, { months: 2 }));
-export const threeMonthsMinusThreeDays = getFormattedValuesFromDate(add(todayDate, { months: 3, days: -3 }));
-export const threeMonths = getFormattedValuesFromDate(add(todayDate, { months: 3 }));
-export const threeMonthsOneDay = getFormattedValuesFromDate(add(todayDate, { months: 3, days: 1 }));
-export const oneYear = getFormattedValuesFromDate(add(todayDate, { years: 1 }));
-export const twelveMonthsOneDay = getFormattedValuesFromDate(add(todayDate, { months: 12, days: 1 }));
-export const twoYears = getFormattedValuesFromDate(add(todayDate, { years: 2 }));
-export const threeYears = getFormattedValuesFromDate(add(todayDate, { years: 3 }));
-export const sixYearsOneDay = getFormattedValuesFromDate(add(todayDate, { years: 6, months: 0, days: 1 }));
+export const tomorrow = getFormattedValues(add(todayDate, { days: 1 }));
+export const twoDays = getFormattedValues(add(todayDate, { days: 2 }));
+export const threeDays = getFormattedValues(add(todayDate, { days: 3 }));
+export const sevenDays = getFormattedValues(add(todayDate, { days: 7 }));
+export const twentyEightDays = getFormattedValues(add(todayDate, { days: 28 }));
+export const oneMonth = getFormattedValues(add(todayDate, { months: 1 }));
+export const twoMonths = getFormattedValues(add(todayDate, { months: 2 }));
+export const threeMonthsMinusThreeDays = getFormattedValues(add(todayDate, { months: 3, days: -3 }));
+export const threeMonths = getFormattedValues(add(todayDate, { months: 3 }));
+export const threeMonthsOneDay = getFormattedValues(add(todayDate, { months: 3, days: 1 }));
+export const oneYear = getFormattedValues(add(todayDate, { years: 1 }));
+export const twelveMonthsOneDay = getFormattedValues(add(todayDate, { months: 12, days: 1 }));
+export const twoYears = getFormattedValues(add(todayDate, { years: 2 }));
+export const threeYears = getFormattedValues(add(todayDate, { years: 3 }));
+export const sixYearsOneDay = getFormattedValues(add(todayDate, { years: 6, months: 0, days: 1 }));
 
 // Dates in the past
-export const yesterday = getFormattedValuesFromDate(sub(todayDate, { days: 1 }));
-export const twoDaysAgo = getFormattedValuesFromDate(sub(todayDate, { days: 2 }));
-export const threeDaysAgo = getFormattedValuesFromDate(sub(todayDate, { days: 3 }));
-export const fourDaysAgo = getFormattedValuesFromDate(sub(todayDate, { days: 4 }));
-export const sevenDaysAgo = getFormattedValuesFromDate(sub(todayDate, { days: 7 }));
-export const twentyFiveDaysAgo = getFormattedValuesFromDate(sub(todayDate, { days: 25 }));
-export const thirtyFiveDaysAgo = getFormattedValuesFromDate(sub(todayDate, { days: 35 }));
-export const twelveMonthsOneDayAgo = getFormattedValuesFromDate(sub(todayDate, { months: 12, days: 1 }));
-export const oneYearAgo = getFormattedValuesFromDate(sub(todayDate, { years: 1 }));
-export const twoYearsAgo = getFormattedValuesFromDate(sub(todayDate, { years: 2 }));
+export const yesterday = getFormattedValues(sub(todayDate, { days: 1 }));
+export const twoDaysAgo = getFormattedValues(sub(todayDate, { days: 2 }));
+export const threeDaysAgo = getFormattedValues(sub(todayDate, { days: 3 }));
+export const fourDaysAgo = getFormattedValues(sub(todayDate, { days: 4 }));
+export const sevenDaysAgo = getFormattedValues(sub(todayDate, { days: 7 }));
+export const twentyFiveDaysAgo = getFormattedValues(sub(todayDate, { days: 25 }));
+export const thirtyFiveDaysAgo = getFormattedValues(sub(todayDate, { days: 35 }));
+export const twelveMonthsOneDayAgo = getFormattedValues(sub(todayDate, { months: 12, days: 1 }));
+export const oneYearAgo = getFormattedValues(sub(todayDate, { years: 1 }));
+export const twoYearsAgo = getFormattedValues(sub(todayDate, { years: 2 }));
 
 // Times
 export const todayTimeHours = format(todayDate, TIME_HOURS_FORMAT);
