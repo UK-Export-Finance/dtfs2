7264091a059985b32ce676dbdc5edef5ad9b4f12
Alex Bramhill <alex.bramhill@softwire.com>
2024-10-16 14:54:44 +0100

    feat(dtfs2-6892): update comments

--- dtfs-central-api/src/v1/controllers/tfm/users/tfm-users.controller.js
+++ dtfs-central-api/src/v1/controllers/tfm/users/tfm-users.controller.js
@@ -18,7 +18,7 @@ const createUser = async (user, auditDetails) => {
 exports.createUser = createUser;
 
 /**
- * @deprecated Do not use -- Favour TFM API (removal todo:DTFS2-7160)
+ * @deprecated Do not use (removal todo:DTFS2-6892)
  */
 exports.createTfmUser = async (req, res) => {
   const { user: payload, auditDetails } = req.body;
