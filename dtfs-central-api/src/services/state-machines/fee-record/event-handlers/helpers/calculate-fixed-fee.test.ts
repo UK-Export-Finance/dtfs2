766a9d6ee6e45ccd0c2a900560d2221463ec5939
Ruby Lavender <67907735+RLavender98@users.noreply.github.com>
2024-10-16 12:38:13 +0100

    feat(FN-3519): take into account effective date of amendments (#3685)

--- dtfs-central-api/src/services/state-machines/fee-record/event-handlers/helpers/calculate-fixed-fee.test.ts
+++ dtfs-central-api/src/services/state-machines/fee-record/event-handlers/helpers/calculate-fixed-fee.test.ts
@@ -1,27 +1,44 @@
-import { ReportPeriod } from '@ukef/dtfs2-common';
 import { calculateFixedFee } from './calculate-fixed-fee';
 
 console.error = jest.fn();
 
 describe('calculateFixedFeeAdjustment', () => {
-  const getReportPeriodForDate = (date: Date): ReportPeriod => ({
-    start: { month: date.getMonth() + 1, year: date.getFullYear() },
-    end: { month: date.getMonth() + 1, year: date.getFullYear() },
-  });
-
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  it('should return the correct calculation for the fixed fee', () => {
+  it('should return the correct calculation for the fixed fee for monthly report period', () => {
     // Arrange
     const utilisation = 100000;
     const interestPercentage = 5;
     const dayCountBasis = 365;
 
-    const aDate = new Date(2024, 0, 1);
+    const reportPeriod = { start: { month: 1, year: 2024 }, end: { month: 1, year: 2024 } };
 
-    const reportPeriod = getReportPeriodForDate(aDate);
+    // Number of days of cover from end of report period to cover end date
+    // is days between 1st Feb 2024 and 1st Sep 2024 = 213
+    const coverEndDate = new Date(2024, 8, 1);
+
+    // Act
+    const result = calculateFixedFee({
+      utilisation,
+      reportPeriod,
+      coverEndDate,
+      interestPercentage,
+      dayCountBasis,
+    });
+
+    // Assert
+    expect(result).toEqual(2626.03); // 100000 * (5 / 100) * 0.9 * 213 / 365 = 2,626.0274 = 2,626.03 (ROUNDED)
+  });
+
+  it('should return the correct calculation for the fixed fee for quarterly report period', () => {
+    // Arrange
+    const utilisation = 100000;
+    const interestPercentage = 5;
+    const dayCountBasis = 365;
+
+    const reportPeriod = { start: { month: 11, year: 2023 }, end: { month: 1, year: 2024 } };
 
     // Number of days of cover from end of report period to cover end date
     // is days between 1st Feb 2024 and 1st Sep 2024 = 213
