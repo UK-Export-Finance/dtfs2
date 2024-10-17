f72a6d6f363c2dd89a91454155b42364e169bf6f
Alex Bramhill <alex.bramhill@softwire.com>
2024-10-17 17:50:05 +0100

    feat(dtfs2-6892): add validate sso feature flag middleware to tfm api

---
+++ trade-finance-manager-api/src/v1/middleware/validate-sso-feature-flag.test.ts
@@ -0,0 +1,86 @@
+import { isTfmSsoFeatureFlagEnabled } from '@ukef/dtfs2-common';
+import { HttpStatusCode } from 'axios';
+import { NextFunction, Request, Response } from 'express';
+import httpMocks from 'node-mocks-http';
+import { validateSsoFeatureFlagIsOff, validateSsoFeatureFlagIsOn } from './validate-sso-feature-flag';
+
+jest.mock('@ukef/dtfs2-common', () => ({
+  isTfmSsoFeatureFlagEnabled: jest.fn(),
+}));
+
+describe('validateSsoFeatureFlag', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('validateSsoFeatureFlagIsOn', () => {
+    const makeRequest = validateSsoFeatureFlagIsOn;
+
+    describe('when the SSO feature flag is disabled', () => {
+      beforeEach(() => {
+        mockIsTfmSsoFeatureFlag(false);
+      });
+
+      itReturnsAnErrorResponse({ makeRequest });
+    });
+
+    describe('when the SSO feature flag is enabled', () => {
+      beforeEach(() => {
+        mockIsTfmSsoFeatureFlag(true);
+      });
+
+      itCallsTheNextMiddleware({
+        makeRequest,
+      });
+    });
+  });
+
+  describe('validateSsoFeatureFlagIsOff', () => {
+    const makeRequest = validateSsoFeatureFlagIsOff;
+
+    describe('when the SSO feature flag is disabled', () => {
+      beforeEach(() => {
+        mockIsTfmSsoFeatureFlag(false);
+      });
+
+      itCallsTheNextMiddleware({
+        makeRequest,
+      });
+    });
+
+    describe('when the SSO feature flag is enabled', () => {
+      beforeEach(() => {
+        mockIsTfmSsoFeatureFlag(true);
+      });
+
+      itReturnsAnErrorResponse({ makeRequest });
+    });
+  });
+
+  function getHttpMocks() {
+    return httpMocks.createMocks();
+  }
+
+  function mockIsTfmSsoFeatureFlag(value: boolean) {
+    jest.mocked(isTfmSsoFeatureFlagEnabled).mockReturnValue(value);
+  }
+
+  function itCallsTheNextMiddleware({ makeRequest }: { makeRequest: (req: Request, res: Response, next: NextFunction) => unknown }) {
+    it('calls the next middleware', () => {
+      const { req, res } = getHttpMocks();
+      const next = jest.fn();
+      makeRequest(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+  }
+
+  function itReturnsAnErrorResponse({ makeRequest }: { makeRequest: (req: Request, res: Response, next: NextFunction) => unknown }) {
+    it('returns an error response', () => {
+      const { req, res } = getHttpMocks();
+      const next = jest.fn();
+      makeRequest(req, res, next);
+      expect(res._getStatusCode()).toEqual(HttpStatusCode.BadRequest);
+      expect(res._isEndCalled()).toEqual(true);
+    });
+  }
+});
