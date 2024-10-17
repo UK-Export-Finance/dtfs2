0ea44e31191937972db0d68b661b10d25a3d6618
Ruby Lavender <ruby.lavender@softwire.com>
2024-10-17 12:27:41 +0100

    feat(FN-3400): add docstrings to new functions

--- utils/sql-db-seeder/src/payment-matching-tolerance/payment-matching-tolerance.seed.ts
+++ utils/sql-db-seeder/src/payment-matching-tolerance/payment-matching-tolerance.seed.ts
@@ -1,6 +1,11 @@
 import { DataSource } from 'typeorm';
 import { Currency, CURRENCY, PaymentMatchingToleranceEntity } from '@ukef/dtfs2-common';
 
+/**
+ * Creates an active tolerance entity
+ * @param currency - The currency for the tolerance
+ * @returns the created entity
+ */
 const getActiveTolerance = (currency: Currency) => {
   const tolerance = new PaymentMatchingToleranceEntity();
   tolerance.currency = currency;
@@ -11,6 +16,10 @@ const getActiveTolerance = (currency: Currency) => {
   return tolerance;
 };
 
+/**
+ * Inserts active tolerances for each currency
+ * @param dataSource - the sql db data source
+ */
 export const seedPaymentMatchingTolerances = async (dataSource: DataSource): Promise<void> => {
   const tolerances = [getActiveTolerance(CURRENCY.GBP), getActiveTolerance(CURRENCY.EUR), getActiveTolerance(CURRENCY.USD), getActiveTolerance(CURRENCY.JPY)];
 
