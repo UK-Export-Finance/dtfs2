0e97f28850e063bf95e8760cfe7afbeb185a2461
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-10-18 10:48:18 +0100

    feat(DTFS2-7490): code review

--- trade-finance-manager-api/src/v1/services/deal-cancellation/deal-cancellation.service.ts
+++ trade-finance-manager-api/src/v1/services/deal-cancellation/deal-cancellation.service.ts
@@ -1,11 +1,17 @@
 import { endOfDay, format, isAfter, toDate } from 'date-fns';
-import { TEAMS, TfmDealCancellation } from '@ukef/dtfs2-common';
+import { TEAMS, TfmDealCancellation, TfmFacility } from '@ukef/dtfs2-common';
 import sendTfmEmail from '../send-tfm-email';
 import { CANCEL_DEAL_PAST_DATE, CANCEL_DEAL_FUTURE_DATE } from '../../../constants/email-template-ids';
 import * as api from '../../api';
 import { formatFacilityIds } from './helpers/format-facility-ids';
 
 export class DealCancellationService {
+  /**
+   * Sends email to PIM email address confirming the cancellation has been submitted
+   * @param ukefDealId ukef deal id
+   * @param dealCancellation deal cancellation object
+   * @param facilityIds ukef facility ids
+   */
   private static async sendDealCancellationEmail(ukefDealId: string, dealCancellation: TfmDealCancellation, facilityIds: string[]) {
     const effectiveFromDate = toDate(dealCancellation.effectiveFrom);
     const endOfToday = endOfDay(new Date());
@@ -23,6 +29,20 @@ export class DealCancellationService {
     });
   }
 
+  /**
+   * Maps the facilities objects into their UKEF ids
+   * @param facilities Facilities
+   * @returns UKEF facility Ids
+   */
+  private static getFacilityIds(facilities: TfmFacility[]) {
+    return facilities.map((facility) => facility.facilitySnapshot.ukefFacilityId).filter((id) => id !== null);
+  }
+
+  /**
+   * Submit the deal cancellation
+   * @param dealId the Deal ID
+   * @param dealCancellation the deal cancellation object
+   */
   public static async submitDealCancellation(dealId: string, dealCancellation: TfmDealCancellation) {
     // TODO: DTFS2-7298 - update cancellation in database & return cancelled deal/facility ids
 
@@ -31,7 +51,7 @@ export class DealCancellationService {
     } = await api.findOneDeal(dealId);
     const facilities = await api.findFacilitiesByDealId(dealId);
 
-    const ukefFacilityIds = facilities.map((facility) => facility.facilitySnapshot.ukefFacilityId).filter((id) => id !== null);
+    const ukefFacilityIds = this.getFacilityIds(facilities);
 
     await this.sendDealCancellationEmail(ukefDealId, dealCancellation, ukefFacilityIds);
   }
--- trade-finance-manager-api/src/v1/services/deal-cancellation/helpers/format-facility-ids.ts
+++ trade-finance-manager-api/src/v1/services/deal-cancellation/helpers/format-facility-ids.ts
@@ -1,1 +1,16 @@
+/**
+ * Formats the facility IDs for sending a numbered list in an email
+ *
+ * @param facilityIds an array of the facility UKEF IDs
+ * @returns a formatted numbered list of the facility IDs
+ *
+ * @example
+ * ```
+ * formatFacilityIds(['UkefId1', 'UkefId2'])
+ * /* Returns
+ * * ` 1. Facility ID UkefId1
+ * *  2. Facility Id UkefId2`
+ * /
+ * ```
+ */
 export const formatFacilityIds = (facilityIds: string[]) => facilityIds.map((id, index) => ` ${index + 1}. Facility ID ${id}`).join(`\n`);
