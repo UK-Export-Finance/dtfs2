03487e307bdafb73bff4e0855324b934a3be469f
Beth Thomas <beth.thomas@softwire.com>
2024-10-23 14:30:41 +0100

    feat(DTFS2-7298): test refactor

--- trade-finance-manager-api/src/v1/rest-mappings/mappings/facilities/mapFacilityStage.test.ts
+++ trade-finance-manager-api/src/v1/rest-mappings/mappings/facilities/mapFacilityStage.test.ts
@@ -1,5 +1,8 @@
 import { TFM_FACILITY_STAGE } from '@ukef/dtfs2-common';
 import { mapBssEwcsFacilityStage, mapGefFacilityStage } from './mapFacilityStage';
+import CONSTANTS from '../../../../constants';
+
+const { ISSUED, UNCONDITIONAL, UNISSUED, CONDITIONAL } = CONSTANTS.FACILITIES.FACILITY_STAGE_PORTAL;
 
 describe('mapGefFacilityStage', () => {
   describe('when a tfm facility stage is passed in', () => {
@@ -27,35 +30,35 @@ describe('mapGefFacilityStage', () => {
 describe('mapBssEwcsFacilityStage', () => {
   describe('when a tfm facility stage is passed in', () => {
     it('should return the tfm facility stage', () => {
-      const result = mapBssEwcsFacilityStage(TFM_FACILITY_STAGE.RISK_EXPIRED);
+      const result = mapBssEwcsFacilityStage(ISSUED, TFM_FACILITY_STAGE.RISK_EXPIRED);
       expect(result).toEqual(TFM_FACILITY_STAGE.RISK_EXPIRED);
     });
   });
 
   describe('when facilityStage is `Issued`', () => {
     it('should return facilityStage as `Issued`', () => {
-      const result = mapBssEwcsFacilityStage('Issued');
+      const result = mapBssEwcsFacilityStage(ISSUED);
       expect(result).toEqual(TFM_FACILITY_STAGE.ISSUED);
     });
   });
 
   describe('when facilityStage is `Unconditional`', () => {
     it('should return facilityStage as `Issued`', () => {
-      const result = mapBssEwcsFacilityStage('Unconditional');
+      const result = mapBssEwcsFacilityStage(UNCONDITIONAL);
       expect(result).toEqual(TFM_FACILITY_STAGE.ISSUED);
     });
   });
 
   describe('when facilityStage is `Unissued`', () => {
     it('should return facilityStage as `Commitment`', () => {
-      const result = mapBssEwcsFacilityStage('Unissued');
+      const result = mapBssEwcsFacilityStage(UNISSUED);
       expect(result).toEqual(TFM_FACILITY_STAGE.COMMITMENT);
     });
   });
 
   describe('when facilityStage is `Conditional`', () => {
     it('should return facilityStage as `Commitment`', () => {
-      const result = mapBssEwcsFacilityStage('Conditional');
+      const result = mapBssEwcsFacilityStage(CONDITIONAL);
       expect(result).toEqual(TFM_FACILITY_STAGE.COMMITMENT);
     });
   });
