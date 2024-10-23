7d0fb877383a32b383518c0b4d2226a7c8325b02
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-10-23 15:35:49 +0100

    feat(DTFS2-7490): remove try catch

--- trade-finance-manager-api/src/v1/services/deal-cancellation/deal-cancellation.service.ts
+++ trade-finance-manager-api/src/v1/services/deal-cancellation/deal-cancellation.service.ts
@@ -19,33 +19,28 @@ export class DealCancellationService {
    * This function is exported for unit testing only and should not be used outside of the service
    */
   public static async sendDealCancellationEmail(ukefDealId: string, dealCancellation: TfmDealCancellation, facilityIds: string[]): Promise<void> {
-    try {
-      const effectiveFromDate = toDate(dealCancellation.effectiveFrom);
-      const endOfToday = endOfDay(new Date());
+    const effectiveFromDate = toDate(dealCancellation.effectiveFrom);
+    const endOfToday = endOfDay(new Date());
 
-      const { email: pimEmail } = await api.findOneTeam(TEAMS.PIM.id);
+    const { email: pimEmail } = await api.findOneTeam(TEAMS.PIM.id);
 
-      if (!pimEmail) {
-        throw Error('Failed to send deal cancellation email to PIM');
-      }
-
-      const emailTemplateId = isAfter(effectiveFromDate, endOfToday) ? CANCEL_DEAL_FUTURE_DATE : CANCEL_DEAL_PAST_DATE;
-
-      const formattedEffectiveFromDate = format(dealCancellation.effectiveFrom, D_MMMM_YYYY);
-      const formattedBankRequestDate = format(dealCancellation.bankRequestDate, D_MMMM_YYYY);
-      const cancelReason = dealCancellation.reason || '-';
-
-      await sendTfmEmail(emailTemplateId, pimEmail, {
-        ukefDealId,
-        effectiveFromDate: formattedEffectiveFromDate,
-        bankRequestDate: formattedBankRequestDate,
-        cancelReason,
-        formattedFacilitiesList: formatFacilityIds(facilityIds),
-      });
-    } catch (error) {
-      console.error('An error occurred in DealCancellationService.sendDealCancellationEmail', error);
-      throw error;
+    if (!pimEmail) {
+      throw Error('Failed to send deal cancellation email to PIM');
     }
+
+    const emailTemplateId = isAfter(effectiveFromDate, endOfToday) ? CANCEL_DEAL_FUTURE_DATE : CANCEL_DEAL_PAST_DATE;
+
+    const formattedEffectiveFromDate = format(dealCancellation.effectiveFrom, D_MMMM_YYYY);
+    const formattedBankRequestDate = format(dealCancellation.bankRequestDate, D_MMMM_YYYY);
+    const cancelReason = dealCancellation.reason || '-';
+
+    await sendTfmEmail(emailTemplateId, pimEmail, {
+      ukefDealId,
+      effectiveFromDate: formattedEffectiveFromDate,
+      bankRequestDate: formattedBankRequestDate,
+      cancelReason,
+      formattedFacilitiesList: formatFacilityIds(facilityIds),
+    });
   }
 
   /**
@@ -54,28 +49,23 @@ export class DealCancellationService {
    * @param dealCancellation the deal cancellation object
    */
   public static async submitDealCancellation(dealId: string, dealCancellation: TfmDealCancellation) {
-    try {
-      // TODO: DTFS2-7298 - update cancellation in database & return cancelled deal/facility ids
+    // TODO: DTFS2-7298 - update cancellation in database & return cancelled deal/facility ids
 
-      const { dealSnapshot } = await api.findOneDeal(dealId);
-      const facilities = await api.findFacilitiesByDealId(dealId);
+    const { dealSnapshot } = await api.findOneDeal(dealId);
+    const facilities = await api.findFacilitiesByDealId(dealId);
 
-      const ukefDealId = dealSnapshot.dealType === DEAL_TYPE.BSS_EWCS ? dealSnapshot.details.ukefDealId : dealSnapshot.ukefDealId;
+    const ukefDealId = dealSnapshot.dealType === DEAL_TYPE.BSS_EWCS ? dealSnapshot.details.ukefDealId : dealSnapshot.ukefDealId;
 
-      const ukefFacilityIds = getUkefFacilityIds(facilities);
+    const ukefFacilityIds = getUkefFacilityIds(facilities);
 
-      if (!ukefFacilityIds.length) {
-        throw new Error(`Failed to find facility ids on deal ${dealId} when submitting deal cancellation`);
-      }
-
-      if (ukefFacilityIds.includes(UKEF_ID.PENDING) || ukefFacilityIds.includes(UKEF_ID.TEST)) {
-        throw new Error(`Some UKEF facility ids were invalid when submitting deal ${dealId} for cancellation. No email has been sent`);
-      }
-
-      await this.sendDealCancellationEmail(ukefDealId, dealCancellation, ukefFacilityIds);
-    } catch (error) {
-      console.error('An error occurred in DealCancellationService.submitDealCancellation', error);
-      throw error;
+    if (!ukefFacilityIds.length) {
+      throw new Error(`Failed to find facility ids on deal ${dealId} when submitting deal cancellation`);
     }
+
+    if (ukefFacilityIds.includes(UKEF_ID.PENDING) || ukefFacilityIds.includes(UKEF_ID.TEST)) {
+      throw new Error(`Some UKEF facility ids were invalid when submitting deal ${dealId} for cancellation. No email has been sent`);
+    }
+
+    await this.sendDealCancellationEmail(ukefDealId, dealCancellation, ukefFacilityIds);
   }
 }
