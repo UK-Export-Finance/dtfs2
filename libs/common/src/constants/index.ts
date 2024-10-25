e0ab08f37cc3d2b595821817485b8194743150ad
Beth Thomas <94544807+BethThomas141@users.noreply.github.com>
2024-10-25 17:25:32 +0100

    feat(DTFS2-7298): hide cancel deal button when deal cancelled (#3756)

--- libs/common/src/constants/index.ts
+++ libs/common/src/constants/index.ts
@@ -24,6 +24,5 @@ export * from './url-character-allowances';
 export * from './max-character-count';
 export * from './deal-submission-type';
 export * from './floating-point-rounding-decimal-places';
-export * from './cancellation-status';
 
 export * as PAYLOAD_VERIFICATION from './payload-verification';
--- trade-finance-manager-ui/server/controllers/helpers/deal-cancellation-enabled.helper.ts
+++ trade-finance-manager-ui/server/controllers/helpers/deal-cancellation-enabled.helper.ts
@@ -1,4 +1,11 @@
-import { DEAL_SUBMISSION_TYPE, DealSubmissionType, isTfmDealCancellationFeatureFlagEnabled, TEAM_IDS } from '@ukef/dtfs2-common';
+import {
+  DEAL_SUBMISSION_TYPE,
+  DealSubmissionType,
+  isTfmDealCancellationFeatureFlagEnabled,
+  TEAM_IDS,
+  TFM_DEAL_CANCELLATION_STATUS,
+  TfmDealCancellationStatus,
+} from '@ukef/dtfs2-common';
 import { userIsInTeam } from '../../helpers/user';
 import { TfmSessionUser } from '../../types/tfm-session-user';
 
@@ -19,11 +26,32 @@ export const canSubmissionTypeBeCancelled = (submissionType: DealSubmissionType)
  * @returns true or false depending on the feature flag, submission type and user type
  */
 export const isDealCancellationEnabled = (submissionType: DealSubmissionType, user: TfmSessionUser): boolean => {
-  // TODO: DTFS2-7298: also check that the deal hasn't already been cancelled.
-
   const isUserAllowedToCancelDeal = userIsInTeam(user, [TEAM_IDS.PIM]);
   const isDealCancellationFeatureFlagEnabled = isTfmDealCancellationFeatureFlagEnabled();
   const isAcceptableSubmissionType = canSubmissionTypeBeCancelled(submissionType);
 
   return isUserAllowedToCancelDeal && isDealCancellationFeatureFlagEnabled && isAcceptableSubmissionType;
+};
+
+/**
+ * Checks if a deal can be cancelled; if it has already been cancelled then this returns false
+ * @param cancellationStatus - optional current deal cancellation status
+ * @returns true or false depending on if the deal has already been cancelled
+ */
+export const canDealBeCancelled = (cancellationStatus?: TfmDealCancellationStatus): boolean => {
+  const isCompleted = cancellationStatus === TFM_DEAL_CANCELLATION_STATUS.COMPLETED;
+  const isScheduled = cancellationStatus === TFM_DEAL_CANCELLATION_STATUS.SCHEDULED;
+
+  const canBeCancelled = !isCompleted && !isScheduled;
+
+  return canBeCancelled;
+};
+
+/**
+ * Checks if a deal cancellation is in draft
+ * @param cancellationStatus - optional current deal cancellation status
+ * @returns true or false depending on if the cancellation status is Draft
+ */
+export const isDealCancellationInDraft = (cancellationStatus?: TfmDealCancellationStatus): boolean => {
+  return cancellationStatus === TFM_DEAL_CANCELLATION_STATUS.DRAFT;
 };
