e0b83da57295e521524f99ba66036c453751ab74
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-10-29 12:04:40 +0000

    feat(DTFS2-7578): update eslint

--- portal-api/src/v1/gef/services/clone-facilities.service.ts
+++ portal-api/src/v1/gef/services/clone-facilities.service.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from 'mongodb';
 import { produce } from 'immer';
+import { isBefore, startOfDay } from 'date-fns';
 import { AuditDetails, Facility } from '@ukef/dtfs2-common';
 import { generateAuditDatabaseRecordFromAuditDetails } from '@ukef/dtfs2-common/change-stream';
 import { mongoDbClient } from '../../../drivers/db-client';
@@ -27,35 +28,32 @@ export const cloneFacilities = async (currentDealId: string, newDealId: ObjectId
   }
 
   const clonedFacilities = existingFacilities.map((facility) => {
-    /* eslint-disable no-param-reassign */
-    return produce(facility, (draftFacility) => {
-      delete draftFacility._id;
+    return produce(facility, (draft) => {
+      delete draft._id;
 
-      draftFacility.dealId = new ObjectId(newDealId);
-      draftFacility.createdAt = Date.now();
-      draftFacility.updatedAt = Date.now();
-      draftFacility.ukefFacilityId = null;
-      draftFacility.canResubmitIssuedFacilities = null;
-      draftFacility.issueDate = null;
-      draftFacility.coverDateConfirmed = null;
-      draftFacility.unissuedToIssuedByMaker = {};
-      draftFacility.hasBeenIssuedAndAcknowledged = null;
-      draftFacility.submittedAsIssuedDate = null;
-      draftFacility.auditRecord = generateAuditDatabaseRecordFromAuditDetails(auditDetails);
+      draft.dealId = new ObjectId(newDealId);
+      draft.createdAt = Date.now();
+      draft.updatedAt = Date.now();
+      draft.ukefFacilityId = null;
+      draft.canResubmitIssuedFacilities = null;
+      draft.issueDate = null;
+      draft.coverDateConfirmed = null;
+      draft.unissuedToIssuedByMaker = {};
+      draft.hasBeenIssuedAndAcknowledged = null;
+      draft.submittedAsIssuedDate = null;
+      draft.auditRecord = generateAuditDatabaseRecordFromAuditDetails(auditDetails);
 
       const currentTime = new Date();
       currentTime.setHours(0, 0, 0, 0);
 
-      if (draftFacility.coverStartDate) {
-        const difference = currentTime.valueOf() - new Date(draftFacility.coverStartDate).getTime();
+      if (draft.coverStartDate) {
         // check if the coverStartDate is in the past
-        if (difference > 0) {
+        if (isBefore(new Date(draft.coverStartDate), startOfDay(new Date()))) {
           // if it is, then ask the user to update it
-          draftFacility.coverStartDate = null;
+          draft.coverStartDate = null;
         }
       }
     });
-    /* eslint-enable no-param-reassign */
   }) as Facility[];
   await collection.insertMany(clonedFacilities);
 };
