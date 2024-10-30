4038421bca3d2dcbe6c80a282d0e28af8decc9e1
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-10-30 14:09:21 +0000

    feat(DTFS2-7578): remove comment

--- portal-api/src/v1/gef/services/clone-exporter.service.ts
+++ portal-api/src/v1/gef/services/clone-exporter.service.ts
@@ -9,7 +9,6 @@ import { exporterStatus } from '../controllers/validation/exporter';
 export const cloneExporter = (currentExporter: AnyObject) => {
   const clonedExporter = currentExporter;
 
-  // update the `updatedAt` property and set it to null - default value
   clonedExporter.updatedAt = Date.now();
   clonedExporter.status = exporterStatus(clonedExporter);
 
