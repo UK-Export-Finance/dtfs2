8aee63eda0aae9d801c0f2b4145377d2be363e33
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-10-30 12:33:35 +0000

    feat(DTFS2-7578): add e2e test for cloning v1 deal

---
+++ e2e-tests/gef/cypress/e2e/clone/facility-end-date-enabled/clone-v1-to-v1.spec.js
@@ -0,0 +1,99 @@
+import relative from '../../relativeURL';
+import { BANK1_MAKER1 } from '../../../../../e2e-fixtures/portal-users.fixture';
+import cloneGEFDeal from '../../pages/clone-deal';
+import mandatoryCriteria from '../../pages/mandatory-criteria';
+import nameApplication from '../../pages/name-application';
+import applicationDetails from '../../pages/application-details';
+import { submitButton } from '../../partials';
+import { MOCK_APPLICATION_AIN } from '../../../fixtures/mocks/mock-deals';
+import { anUnissuedCashFacility, anIssuedCashFacility } from '../../../../../e2e-fixtures/mock-gef-facilities';
+import { DEAL_STATUS, FACILITY_TYPE } from '../../../fixtures/constants';
+
+/**
+ * NOTE: These tests check the backwards compatibility with in-flight version 0 deals.
+ * A migration may need to be run on production if this test is updated.
+ */
+context('Clone version 1 deal to version 1', () => {
+  let originalDealId;
+  let clonedDealId;
+  let token;
+
+  const cashFacilityWithFacilityEndDate = {
+    ...anUnissuedCashFacility(),
+    isUsingFacilityEndDate: true,
+    facilityEndDate: new Date(),
+    bankReviewDate: null,
+  };
+  const cashFacilityWithBankReviewDate = {
+    ...anIssuedCashFacility(),
+    isUsingFacilityEndDate: false,
+    bankReviewDate: new Date(),
+    facilityEndDate: null,
+  };
+
+  before(() => {
+    cy.apiLogin(BANK1_MAKER1)
+      .then((t) => {
+        token = t;
+      })
+      .then(() => {
+        // creates application and inserts facilities and changes status
+        cy.apiCreateApplication(BANK1_MAKER1, token).then(({ body }) => {
+          originalDealId = body._id;
+          cy.apiUpdateApplication(originalDealId, token, MOCK_APPLICATION_AIN).then(() => {
+            cy.apiCreateFacility(originalDealId, FACILITY_TYPE.CASH, token).then((facility) => {
+              cy.apiUpdateFacility(facility.body.details._id, token, cashFacilityWithFacilityEndDate);
+            });
+            cy.apiCreateFacility(originalDealId, FACILITY_TYPE.CASH, token).then((facility) => {
+              cy.apiUpdateFacility(facility.body.details._id, token, cashFacilityWithBankReviewDate);
+            });
+            cy.apiSetApplicationStatus(originalDealId, token, DEAL_STATUS.UKEF_ACKNOWLEDGED);
+          });
+        });
+      });
+  });
+
+  beforeEach(() => {
+    cy.saveSession();
+  });
+
+  describe('cloning a version 1 deal', () => {
+    before(() => {
+      cy.login(BANK1_MAKER1);
+
+      cy.visit(relative(`/gef/application-details/${originalDealId}`));
+
+      cloneGEFDeal.cloneGefDealLink().click();
+      mandatoryCriteria.trueRadio().click();
+      cy.clickContinueButton();
+
+      cy.keyboardInput(nameApplication.internalRef(), 'Cloned of v0 deal');
+      cy.clickContinueButton();
+
+      cy.get('table.govuk-table tr').eq(1).find('td').eq(1).find('.govuk-link').click();
+      cy.url().should('contain', relative(`/gef/application-details/`));
+
+      cy.url().then((url) => {
+        clonedDealId = url.split('/').at(-1);
+      });
+    });
+
+    beforeEach(() => {
+      cy.login(BANK1_MAKER1);
+
+      cy.visit(relative(`/gef/application-details/${clonedDealId}`));
+    });
+
+    it('wipes facility end date & bank review date values', () => {
+      applicationDetails.facilitySummaryListTable(0).isUsingFacilityEndDateValue().should('contain', 'Required');
+      applicationDetails.facilitySummaryListTable(1).isUsingFacilityEndDateValue().should('contain', 'Required');
+
+      applicationDetails.facilitySummaryListTable(0).facilityEndDateAction().should('not.exist');
+      applicationDetails.facilitySummaryListTable(0).bankReviewDateAction().should('not.exist');
+      applicationDetails.facilitySummaryListTable(1).facilityEndDateAction().should('not.exist');
+      applicationDetails.facilitySummaryListTable(1).bankReviewDateAction().should('not.exist');
+
+      submitButton().should('not.exist');
+    });
+  });
+});
