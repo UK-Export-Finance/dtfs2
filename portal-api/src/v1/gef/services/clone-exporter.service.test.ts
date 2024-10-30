9cc5a5438244c0a01dc38928fb532808be4a97a7
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-10-30 11:59:57 +0000

    feat(DTFS2-7578): add clone exporter unit test

---
+++ portal-api/src/v1/gef/services/clone-exporter.service.test.ts
@@ -0,0 +1,31 @@
+import { exporterStatus } from '../controllers/validation/exporter';
+import { cloneExporter } from './clone-exporter.service';
+
+describe('cloneExporter', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it('updates the updatedAt and status fields', () => {
+    // Arrange
+    const mockExporter = {
+      updatedAt: 17090930940932,
+      status: 'An exporter status',
+      anotherField: 'anotherField',
+    };
+
+    // Act
+    const response = cloneExporter(mockExporter);
+
+    // Assert
+    expect(response).toEqual({
+      ...mockExporter,
+      updatedAt: Date.now(),
+      status: exporterStatus(mockExporter),
+    });
+  });
+});
