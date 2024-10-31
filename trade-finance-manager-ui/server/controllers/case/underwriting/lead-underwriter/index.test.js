8d3969d8e870699d8d18825fc07cf5f4f706c5ba
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2024-10-31 10:45:25 +0000

    fix(DTFS2-7486): added test coverage

--- trade-finance-manager-ui/server/controllers/case/underwriting/lead-underwriter/index.test.js
+++ trade-finance-manager-ui/server/controllers/case/underwriting/lead-underwriter/index.test.js
@@ -230,6 +230,23 @@ describe('POST underwriting - assign lead underwriter', () => {
     expect(res.render).toHaveBeenCalledWith('_partials/problem-with-service.njk');
   });
 
+  it('should redirect to problem with service page when req.body.assignedTo does not exist', async () => {
+    const req = {
+      params: {
+        _id: dealId,
+      },
+      session,
+      body: {
+        test: '123-test',
+      },
+    };
+
+    await underwriterLeadUnderwriterController.postAssignLeadUnderwriter(req, res);
+
+    expect(console.error).toHaveBeenCalledWith('Error while assigning lead underwriter to deal %s %o', dealId, req?.body);
+    expect(res.render).toHaveBeenCalledWith('_partials/problem-with-service.njk');
+  });
+
   it('should call api.updateLeadUnderwriter and redirect to /lead-underwriter', async () => {
     const req = {
       params: {
