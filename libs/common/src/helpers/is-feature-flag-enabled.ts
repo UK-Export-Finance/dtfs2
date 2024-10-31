ce7e2c29e7bcc5031fddc35dfdf47cfe338cf363
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-10-31 15:27:26 +0000

    feat: additional feature flagging

--- libs/common/src/helpers/is-feature-flag-enabled.ts
+++ libs/common/src/helpers/is-feature-flag-enabled.ts
@@ -14,6 +14,7 @@ const featureFlagsSchema = z.object({
   FF_TFM_FACILITY_END_DATE_ENABLED: featureFlagOptions,
   FF_TFM_DEAL_CANCELLATION_ENABLED: featureFlagOptions,
   FF_TFM_SSO_ENABLED: featureFlagOptions,
+  AUTOMATIC_SF_CUSTOMER_CREATION_ENABLED: featureFlagOptions,
 });
 
 export type FeatureFlag = keyof z.infer<typeof featureFlagsSchema>;
@@ -30,3 +31,5 @@ export const isTfmFacilityEndDateFeatureFlagEnabled = isFeatureFlagEnabled('FF_T
 export const isTfmDealCancellationFeatureFlagEnabled = isFeatureFlagEnabled('FF_TFM_DEAL_CANCELLATION_ENABLED');
 
 export const isTfmSsoFeatureFlagEnabled = isFeatureFlagEnabled('FF_TFM_SSO_ENABLED');
+
+export const isAutomaticSfCustomerCreationFeatureFlagEnabled = isFeatureFlagEnabled('AUTOMATIC_SF_CUSTOMER_CREATION_ENABLED');
