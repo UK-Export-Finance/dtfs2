dee4590c4edfce38fa333da9b1dee0d847d2ece8
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-11-06 14:43:03 +0000

    feat(DTFS2-7306): update jsdoc

--- dtfs-central-api/src/helpers/merge-tfm-values-into-portal-deal.ts
+++ dtfs-central-api/src/helpers/merge-tfm-values-into-portal-deal.ts
@@ -1,5 +1,11 @@
 import { Deal, DEAL_STATUS, TFM_DEAL_STAGE, TfmDeal } from '@ukef/dtfs2-common';
 
+/**
+ * Updates properties on a portal deal with updates from tfm
+ * @param portalDeal the portal deal returned from the database
+ * @param tfmDeal the tfm deal if it exists
+ * @returns portal `deal` object with relevant fields from tfm-deals added in
+ */
 export const mergeTfmValuesIntoPortalDeal = (portalDeal: Deal, tfmDeal: TfmDeal | null): Deal => {
   return {
     ...portalDeal,
