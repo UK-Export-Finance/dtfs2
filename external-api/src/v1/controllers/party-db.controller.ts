ff8a3a34d44d1fd378d0f9e423a78c247947c52c
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-11-07 09:57:18 +0000

    feat: rename endpoint direct -> sf

--- external-api/src/v1/controllers/party-db.controller.ts
+++ external-api/src/v1/controllers/party-db.controller.ts
@@ -22,7 +22,7 @@ export const lookup = async (req: Request, res: Response) => {
   const response: { status: number; data: unknown } = await axios({
     method: 'get',
     url: AUTOMATIC_SF_CUSTOMER_CREATION_ENABLED === 'true'
-      ? `${APIM_MDM_URL}customers/direct?companyRegistrationNumber=${companyReg}`
+      ? `${APIM_MDM_URL}customers/salesforce?companyRegistrationNumber=${companyReg}`
       : `${APIM_MDM_URL}customers?companyReg=${companyReg}`,
     headers,
   }).catch((error: AxiosError) => {
