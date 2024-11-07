edc67353171c4b68207acf58c672cd00fb7ebb1b
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-11-07 18:28:12 +0000

    docs: update docstring

--- gef-ui/server/services/api.js
+++ gef-ui/server/services/api.js
@@ -101,6 +101,16 @@ const updateSupportingInformation = async ({ dealId, application, field, user, u
   }
 };
 
+/**
+ * Updates the application status for a specific deal.
+ * 
+ * Notably, the timeout is so long because in the event that the deal status is being updated to 'Submitted', 
+ * multiple API calls are triggered, and the user has to wait for the completion of these in the UI in the 
+ * current implementation.
+ * 
+ * In the future, these operations should instead be run as part of a fail-safe background process, as discussed 
+ * in the root README. At that point, the timeout can be reduced.
+ */
 const setApplicationStatus = async ({ dealId, status, userToken }) => {
   if (!isValidMongoId(dealId)) {
     console.error('setApplicationStatus: API call failed for dealId %s', dealId);
@@ -114,7 +124,7 @@ const setApplicationStatus = async ({ dealId, status, userToken }) => {
         status,
       },
       { ...config(userToken), timeout: 20000 },
-    ); // Application status has multiple api calls in portal api
+    );
     return data;
   } catch (error) {
     return apiErrorHandler(error);
