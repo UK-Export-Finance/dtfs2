e4e85f514d3c8e90fa53326358c9628337633310
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-11-07 16:52:51 +0000

    feat: test feature flagged getPartyUrn

--- libs/common/src/helpers/is-feature-flag-enabled.test.ts
+++ libs/common/src/helpers/is-feature-flag-enabled.test.ts
@@ -4,7 +4,6 @@ import {
   isTfmFacilityEndDateFeatureFlagEnabled,
   isTfmPaymentReconciliationFeatureFlagEnabled,
   isTfmSsoFeatureFlagEnabled,
-  isAutomaticSfCustomerCreationFeatureFlagEnabled,
 } from './is-feature-flag-enabled';
 import { withBooleanFeatureFlagTests } from './with-boolean-feature-flag.tests';
 
@@ -14,5 +13,4 @@ describe('is-feature-flag-enabled helpers', () => {
   withBooleanFeatureFlagTests({ featureFlagName: 'FF_TFM_FACILITY_END_DATE_ENABLED', getFeatureFlagValue: isTfmFacilityEndDateFeatureFlagEnabled });
   withBooleanFeatureFlagTests({ featureFlagName: 'FF_TFM_DEAL_CANCELLATION_ENABLED', getFeatureFlagValue: isTfmDealCancellationFeatureFlagEnabled });
   withBooleanFeatureFlagTests({ featureFlagName: 'FF_TFM_SSO_ENABLED', getFeatureFlagValue: isTfmSsoFeatureFlagEnabled });
-  withBooleanFeatureFlagTests({ featureFlagName: 'AUTOMATIC_SF_CUSTOMER_CREATION_ENABLED', getFeatureFlagValue: isAutomaticSfCustomerCreationFeatureFlagEnabled });
 });
