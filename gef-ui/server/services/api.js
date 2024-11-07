d9f08771b347a527072085482b96dd94e56c97a2
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-11-07 09:57:04 +0000

    feat: reduce timeout

--- gef-ui/server/services/api.js
+++ gef-ui/server/services/api.js
@@ -113,7 +113,7 @@ const setApplicationStatus = async ({ dealId, status, userToken }) => {
       {
         status,
       },
-      { ...config(userToken), timeout: 30000 },
+      { ...config(userToken), timeout: 20000 },
     ); // Application status has multiple api calls in portal api
     return data;
   } catch (error) {
