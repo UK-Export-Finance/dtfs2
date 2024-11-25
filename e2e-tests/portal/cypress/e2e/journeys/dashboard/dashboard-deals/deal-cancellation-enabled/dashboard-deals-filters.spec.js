a3732e01d872354567a8711c16c42936a9b8c609
Beth Thomas <94544807+BethThomas141@users.noreply.github.com>
2024-11-25 17:19:20 +0000

    feat(DTFS2-7466): add cancellation filter to portal deals (#3943)

---
+++ e2e-tests/portal/cypress/e2e/journeys/dashboard/dashboard-deals/deal-cancellation-enabled/dashboard-deals-filters.spec.js
@@ -0,0 +1,25 @@
+const { DEAL_STATUS } = require('@ukef/dtfs2-common');
+const MOCK_USERS = require('../../../../../../../e2e-fixtures');
+const { dashboardDeals } = require('../../../../pages');
+const { dashboardFilters } = require('../../../../partials');
+
+const { BANK1_MAKER1 } = MOCK_USERS;
+
+const filters = dashboardFilters;
+
+context('Dashboard Deals filters - Deal cancellation feature flag enabled', () => {
+  before(() => {
+    cy.login(BANK1_MAKER1);
+  });
+
+  describe('When deal cancellation is enabled on the dashboard deals page', () => {
+    it('displays the deal cancellation status filter', () => {
+      dashboardDeals.visit();
+      filters.showHideButton().click();
+
+      cy.assertText(dashboardDeals.filters.panel.form.status.cancelled.label(), DEAL_STATUS.CANCELLED);
+      dashboardDeals.filters.panel.form.status.cancelled.checkbox().should('exist');
+      dashboardDeals.filters.panel.form.status.cancelled.checkbox().should('not.be.checked');
+    });
+  });
+});
