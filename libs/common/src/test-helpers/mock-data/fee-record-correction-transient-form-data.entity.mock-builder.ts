0d43de844db2f4c79bb61411bff3c8b19aa91353
Ruby Lavender <ruby.lavender@ukexportfinance.gov.uk>
2024-11-27 17:41:11 +0000

    feat(FN-3578): add transient mock builder

---
+++ libs/common/src/test-helpers/mock-data/fee-record-correction-transient-form-data.entity.mock-builder.ts
@@ -0,0 +1,39 @@
+import { RECORD_CORRECTION_REASON } from '../../constants';
+import { FeeRecordCorrectionTransientFormDataEntity } from '../../sql-db-entities';
+import { RecordCorrectionTransientFormData } from '../../types';
+
+export class FeeRecordCorrectionTransientFormDataEntityMockBuilder {
+  private readonly transientFormData: FeeRecordCorrectionTransientFormDataEntity;
+
+  private constructor() {
+    const data = new FeeRecordCorrectionTransientFormDataEntity();
+
+    data.userId = 'abc123';
+    data.feeRecordId = 123;
+    data.formData = {
+      reasons: [RECORD_CORRECTION_REASON.REPORTED_CURRENCY_INCORRECT],
+      additionalInfo: 'some more information',
+    };
+
+    this.transientFormData = data;
+  }
+
+  public withUserId(userId: string): FeeRecordCorrectionTransientFormDataEntityMockBuilder {
+    this.transientFormData.userId = userId;
+    return this;
+  }
+
+  public withFeeRecordId(feeRecordId: number): FeeRecordCorrectionTransientFormDataEntityMockBuilder {
+    this.transientFormData.feeRecordId = feeRecordId;
+    return this;
+  }
+
+  public withFormData(formData: RecordCorrectionTransientFormData): FeeRecordCorrectionTransientFormDataEntityMockBuilder {
+    this.transientFormData.formData = formData;
+    return this;
+  }
+
+  public build(): FeeRecordCorrectionTransientFormDataEntity {
+    return this.transientFormData;
+  }
+}
