7c3053385a0ca53a29a8a36db5727c78f83caf12
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-11-30 16:12:20 +0000

    feat(DTFS2-7673): add unit test coverage for findOneAmendmentByFacilityIdAndAmendmentId

---
+++ dtfs-central-api/src/repositories/tfm-facilities-repo/tfm-facilities.repo.findOneAmendmentByFacilityIdAndAmendmentId.test.ts
@@ -0,0 +1,131 @@
+import { ObjectId } from 'mongodb';
+import { FacilityAmendment, FacilityNotFoundError, MONGO_DB_COLLECTIONS, TfmFacility } from '@ukef/dtfs2-common';
+import { TfmFacilitiesRepo } from './tfm-facilities.repo';
+import { mongoDbClient } from '../../drivers/db-client';
+import { aTfmFacility } from '../../../test-helpers';
+
+const facilityId = new ObjectId();
+const amendmentId = new ObjectId();
+
+const mockGetCollection = jest.fn();
+const mockFindOne = jest.fn() as jest.Mock<Promise<TfmFacility | null>>;
+
+describe('TfmFacilitiesRepo', () => {
+  describe('findOneAmendmentByFacilityIdAndAmendmentId', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+
+      jest.spyOn(mongoDbClient, 'getCollection').mockImplementation(mockGetCollection);
+      mockGetCollection.mockResolvedValue({
+        findOne: mockFindOne,
+      });
+    });
+
+    it(`calls getCollection with ${MONGO_DB_COLLECTIONS.TFM_FACILITIES}`, async () => {
+      // Arrange
+      mockFindOne.mockResolvedValue(aTfmFacility());
+
+      // Act
+      await TfmFacilitiesRepo.findOneAmendmentByFacilityIdAndAmendmentId(facilityId, amendmentId);
+
+      // Assert
+      expect(mockGetCollection).toHaveBeenCalledTimes(1);
+      expect(mockGetCollection).toHaveBeenCalledWith(MONGO_DB_COLLECTIONS.TFM_FACILITIES);
+    });
+
+    it('throws FacilityNotFoundError if the facility does not exist', async () => {
+      // Act + Assert
+      await expect(() => TfmFacilitiesRepo.findOneAmendmentByFacilityIdAndAmendmentId(facilityId, amendmentId)).rejects.toThrow(FacilityNotFoundError);
+    });
+
+    it('returns the correct amendment with the ukefFacilityId', async () => {
+      // Arrange
+      const amendment = {
+        amendmentId,
+        facilityId,
+      } as FacilityAmendment;
+
+      const facility = aTfmFacility({
+        amendments: [
+          {
+            amendmentId: new ObjectId(),
+            facilityId,
+          } as FacilityAmendment,
+          amendment,
+          {
+            amendmentId: new ObjectId(),
+            facilityId,
+          } as FacilityAmendment,
+        ],
+      });
+
+      mockFindOne.mockResolvedValue(facility);
+
+      // Act
+      const result = await TfmFacilitiesRepo.findOneAmendmentByFacilityIdAndAmendmentId(facilityId, amendmentId);
+
+      // Assert
+      expect(result).toEqual({
+        ...amendment,
+        ukefFacilityId: facility.facilitySnapshot.ukefFacilityId,
+      });
+    });
+
+    it('returns the correct amendment with the ukefFacilityId when the amendmentId is given as a string', async () => {
+      // Arrange
+      const amendment = {
+        amendmentId,
+        facilityId,
+      } as FacilityAmendment;
+
+      const facility = aTfmFacility({
+        amendments: [
+          {
+            amendmentId: new ObjectId(),
+            facilityId,
+          } as FacilityAmendment,
+          amendment,
+          {
+            amendmentId: new ObjectId(),
+            facilityId,
+          } as FacilityAmendment,
+        ],
+      });
+
+      mockFindOne.mockResolvedValue(facility);
+
+      // Act
+      const result = await TfmFacilitiesRepo.findOneAmendmentByFacilityIdAndAmendmentId(facilityId, amendmentId.toString());
+
+      // Assert
+      expect(result).toEqual({
+        ...amendment,
+        ukefFacilityId: facility.facilitySnapshot.ukefFacilityId,
+      });
+    });
+
+    it('returns undefined if the amendmentId does not correspond to an amendment', async () => {
+      // Arrange
+      const facility = aTfmFacility({
+        amendments: [
+          {
+            amendmentId: new ObjectId(),
+            facilityId,
+          } as FacilityAmendment,
+          {
+            amendmentId: new ObjectId(),
+            facilityId,
+          } as FacilityAmendment,
+        ],
+      });
+
+      mockFindOne.mockResolvedValue(facility);
+
+      // Act
+      const result = await TfmFacilitiesRepo.findOneAmendmentByFacilityIdAndAmendmentId(facilityId, amendmentId);
+
+      // Assert
+      expect(result).toEqual(undefined);
+    });
+  });
+});
