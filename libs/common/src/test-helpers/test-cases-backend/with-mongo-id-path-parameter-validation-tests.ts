e2a51dae48f4effa9661e8d7f88b79c0a9f710eb
Matt <173997080+sw-mattw@users.noreply.github.com>
2024-12-02 14:22:33 +0000

    feat(FN-3577): improve id path param validation tests

--- libs/common/src/test-helpers/test-cases-backend/with-mongo-id-path-parameter-validation-tests.ts
+++ libs/common/src/test-helpers/test-cases-backend/with-mongo-id-path-parameter-validation-tests.ts
@@ -5,7 +5,6 @@ import { ErrorResponse } from './types';
 type WithMongoIdPathParameterValidationTestsParams = {
   baseUrl: string;
   makeRequest: (url: string) => Promise<ErrorResponse>;
-  pathParameters?: string[];
 };
 
 const VALID_OBJECT_ID = '5c0a7922c9d89830f4911426';
@@ -20,16 +19,16 @@ const extractParameters = (url: string): string[] =>
  * Test helper to test the mongo id validation middleware being used for a specific route
  * @param params - The test parameters
  * @param params.baseUrl - The base url with format '/v1/path/:pathParamater'
+ * and with all non-mongo id path parameters replaced with values
  * @param params.makeRequest - The function to make the request with
- * @param params.pathParameters - The path parameters to test
  */
-export const withMongoIdPathParameterValidationTests = ({ baseUrl, makeRequest, pathParameters }: WithMongoIdPathParameterValidationTestsParams): void => {
-  const parameters = pathParameters ?? extractParameters(baseUrl);
+export const withMongoIdPathParameterValidationTests = ({ baseUrl, makeRequest }: WithMongoIdPathParameterValidationTestsParams): void => {
+  const parameters = extractParameters(baseUrl);
 
   describe.each(parameters)("when the ':%s' path parameter is not a valid mongo id", (parameter) => {
-    const baseUrlWithTestParamater = baseUrl.replace(`:${parameter}`, 'invalid-id');
+    const baseUrlWithTestParameter = baseUrl.replace(`:${parameter}`, 'invalid-id');
 
-    const testUrl = parameters.reduce((url, validParameter) => url.replace(`:${validParameter}`, VALID_OBJECT_ID), baseUrlWithTestParamater);
+    const testUrl = parameters.reduce((url, validParameter) => url.replace(`:${validParameter}`, VALID_OBJECT_ID), baseUrlWithTestParameter);
 
     it('returns a 400 (Bad request)', async () => {
       // Act
