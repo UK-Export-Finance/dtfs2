88c8d5c72b01556e028cfa51c9840c6b7055ea37
ttbarnes <tony@tonybarnes.me>
2024-12-03 10:19:52 +0000

    feat(DTFS2-7545): rename activity type

--- libs/common/src/types/portal/activity.ts
+++ libs/common/src/types/portal/activity.ts
@@ -6,11 +6,11 @@ export type PortalActivityLabel = ValuesOf<typeof PORTAL_ACTIVITY_LABEL>;
 
 /**
  * NOTE: type and activityType are confusing.
- * - type/ActivityType e.g "DEAL_CANCELLED"
- * - activityType/ActivityLabel e.g "Deal cancelled"
+ * - type/PortalActivityType e.g "DEAL_CANCELLED"
+ * - activityType/PortalActivityLabel e.g "Deal cancelled"
  * Because we have two fields using a "type" naming convention:
  * - The "type" field is clear.
- * - The "activityType" field uses an "ActivityLabel" type to help clarify.
+ * - The "activityType" field uses an "PortalActivityLabel" type to help clarify.
  * Ideally, we would run some data migration to rename one or both of these fields.
  */
 export type PortalActivity = {
