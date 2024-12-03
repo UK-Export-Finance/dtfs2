d5d1bb42b1533a22c35d064eb81646bb04190bcd
Alex Bramhill <alex.bramhill@softwire.com>
2024-12-03 19:28:45 +0000

    feat(dtfs2-6892): add object id schema for tfm sso

--- libs/common/src/schemas/object-id.ts
+++ libs/common/src/schemas/object-id.ts
@@ -1,4 +1,26 @@
 import { ObjectId } from 'mongodb';
 import z from 'zod';
 
-export const OBJECT_ID = z.union([z.instanceof(ObjectId), z.string().refine((id) => ObjectId.isValid(id))]);
+/**
+ * A zod schema that represents a valid ObjectId as an ObjectId object
+ * This schema also transforms any valid string into an ObjectId object
+ */
+export const OBJECT_ID = z.union([
+  z.instanceof(ObjectId),
+  z
+    .string()
+    .refine((id) => ObjectId.isValid(id))
+    .transform((id) => new ObjectId(id)),
+]);
+
+/**
+ * A zod schema that represents a valid ObjectId as a string
+ * This schema also transforms any valid ObjectId object into a string
+ */
+export const OBJECT_ID_STRING = z.union([z.string().refine((id) => ObjectId.isValid(id)), z.instanceof(ObjectId).transform((id) => id.toString())]);
+
+/**
+ * A zod schema that represents a valid ObjectId as an ObjectId object or a string
+ * This schema does not do any transformation
+ */
+export const OBJECT_ID_OR_OBJECT_ID_STRING = z.union([z.instanceof(ObjectId), OBJECT_ID_STRING, z.string().refine((id) => ObjectId.isValid(id))]);
