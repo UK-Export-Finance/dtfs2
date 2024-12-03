b11855407b2b238cb8def65adf52e86fe529c6dc
Beth Thomas <beth.thomas@softwire.com>
2024-12-03 11:46:57 +0000

    feat(DTFS2-7523): add what needs to change page template

---
+++ gef-ui/server/controllers/amendments/what-needs-to-change/what-needs-to-change.ts
@@ -0,0 +1,28 @@
+import { CustomExpressRequest } from '@ukef/dtfs2-common';
+import { Response } from 'express';
+import * as api from '../../../services/api';
+import { WhatNeedsToChangeViewModel } from '../../../types/view-models/amendments/what-needs-to-change-view-model.ts';
+import { asLoggedInUserSession } from '../../../utils/express-session';
+
+type GetWhatNeedsToChangeRequest = CustomExpressRequest<{
+  params: { dealId: string; facilityId: string; amendmentId: string };
+}>;
+
+export const getWhatNeedsToChange = async (req: GetWhatNeedsToChangeRequest, res: Response) => {
+  try {
+    const { dealId, facilityId } = req.params;
+    const { userToken } = asLoggedInUserSession(req.session);
+
+    const deal = await api.getApplication({ dealId, userToken });
+
+    const viewModel: WhatNeedsToChangeViewModel = {
+      exporterName: deal.exporter.companyName,
+      previousPage: `/case/${dealId}/facility/${facilityId}`,
+    };
+
+    return res.render('partials/amendments/what-needs-to-change.njk', viewModel);
+  } catch (error) {
+    console.error(error);
+    return res.render('partials/problem-with-service.njk');
+  }
+};
---
+++ gef-ui/server/routes/facilities/amendments/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import express from 'express';
+import { validateRole, validateToken, validateBank } from '../../../middleware';
+import { MAKER } from '../../../constants/roles';
+import { getWhatNeedsToChange } from '../../../controllers/amendments/what-needs-to-change/what-needs-to-change';
+
+const router = express.Router();
+
+router
+  .route('/application-preview/:dealId/facilities/:facilityId/amendments/:amendmentId/what-needs-to-change')
+  .all([validateToken, validateBank, validateRole({ role: [MAKER] })])
+  .get(getWhatNeedsToChange);
+
+export default router;
