2e412f4e4711c8b40f5c29176871cc72f8c72a0d
Alex Bramhill <alex.bramhill@softwire.com>
2024-12-04 14:33:20 +0000

    feat(dtfs2-6892): update entra user id auth result tests

--- libs/common/src/schemas/tfm/entra-id.schema.entra-id-authentication-result-schema.test.ts
+++ libs/common/src/schemas/tfm/entra-id.schema.entra-id-authentication-result-schema.test.ts
@@ -1,27 +1,35 @@
 import { anEntraIdUser, withSchemaValidationTests } from '../../test-helpers';
+import { EntraIdUser } from '../../types';
 import { EntraIdAuthenticationResult } from '../../types/tfm/entra-id';
 import { ENTRA_ID_AUTHENTICATION_RESULT_SCHEMA } from './entra-id.schema';
 
-describe('ENTRA_ID_AUTHENTICATION_RESULT_SCHEMA', () => {});
+describe('ENTRA_ID_AUTHENTICATION_RESULT_SCHEMA', () => {
+  withSchemaValidationTests({
+    schema: ENTRA_ID_AUTHENTICATION_RESULT_SCHEMA,
+    aValidPayload,
+    testCases: [
+      {
+        parameterPath: 'accessToken',
+        type: 'string',
+      },
+      {
+        parameterPath: 'account.idTokenClaims',
+        type: 'ENTRA_ID_USER_SCHEMA',
+        options: {
+          overrideGetTestObjectWithUpdatedField: (newValue) => {
+            const testObject = aValidPayload();
+            testObject.account.idTokenClaims = newValue as EntraIdUser;
+            return testObject;
+          },
+        },
+      },
+    ],
+  });
 
-withSchemaValidationTests({
-  schema: ENTRA_ID_AUTHENTICATION_RESULT_SCHEMA,
-  aValidPayload,
-  testCases: [
-    {
-      parameterPath: 'accessToken',
-      type: 'string',
-    },
-    {
-      parameterPath: 'account',
-      type: 'ENTRA_ID_USER_SCHEMA',
-    },
-  ],
+  function aValidPayload(): EntraIdAuthenticationResult {
+    return {
+      accessToken: 'an-access-token',
+      account: { idTokenClaims: anEntraIdUser() },
+    };
+  }
 });
-
-function aValidPayload(): EntraIdAuthenticationResult {
-  return {
-    accessToken: 'an-access-token',
-    account: { idTokenClaims: anEntraIdUser() },
-  };
-}
