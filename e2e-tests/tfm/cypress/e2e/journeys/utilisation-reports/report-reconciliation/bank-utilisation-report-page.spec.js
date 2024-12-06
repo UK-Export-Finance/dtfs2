7c9d3bc15acee309e83a01b9c089c6862018c81a
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-12-06 09:49:15 +0000

    feat(FN-3612): test changes

--- e2e-tests/tfm/cypress/e2e/journeys/utilisation-reports/report-reconciliation/bank-utilisation-report-page.spec.js
+++ e2e-tests/tfm/cypress/e2e/journeys/utilisation-reports/report-reconciliation/bank-utilisation-report-page.spec.js
@@ -1,103 +1,48 @@
 import {
-  CURRENCY,
   FeeRecordEntityMockBuilder,
-  REPORT_NOT_RECEIVED,
-  RECONCILIATION_IN_PROGRESS,
   UtilisationReportEntityMockBuilder,
-  toIsoMonthStamp,
-  getPreviousReportPeriodForBankScheduleByMonth,
-  FEE_RECORD_STATUS,
   getFormattedReportPeriodWithLongMonth,
+  FEE_RECORD_STATUS,
+  PENDING_RECONCILIATION,
 } from '@ukef/dtfs2-common';
 import pages from '../../../pages';
 import USERS from '../../../../fixtures/users';
 import { NODE_TASKS } from '../../../../../../e2e-fixtures';
+import { getMatchingTfmFacilitiesForFeeRecords } from '../../../../support/utils/getMatchingTfmFacilitiesForFeeRecords';
 
 const { utilisationReportPage } = pages;
 
 context('Bank utilisation report page', () => {
-  const SUBMISSION_MONTH = toIsoMonthStamp(new Date());
-  const BANK_ID = '961';
-  const REPORT_ID = 1;
-  const FEE_RECORD_ID_ONE = '11';
-  const FACILITY_ID_ONE = '11111111';
+  const bankId = '961';
+  const reportId = 1;
   let bankName;
-  let reportPeriodString;
 
-  const aTfmFacilityFacilitySnapshot = () => ({
-    coverStartDate: new Date().getTime(),
-    coverEndDate: new Date().getTime(),
-    interestPercentage: 5,
-    dayCountBasis: 365,
-    coverPercentage: 80,
-    value: 100000,
-  });
+  const report = UtilisationReportEntityMockBuilder.forStatus(PENDING_RECONCILIATION).withId(reportId).withBankId(bankId).build();
+  const feeRecordAtToDoStatus = FeeRecordEntityMockBuilder.forReport(report).withId(1).withStatus(FEE_RECORD_STATUS.TO_DO).build();
 
-  const TFM_FACILITIES = [
-    {
-      facilitySnapshot: { ...aTfmFacilityFacilitySnapshot(), ukefFacilityId: FACILITY_ID_ONE },
-    },
-  ];
+  const reportPeriodString = getFormattedReportPeriodWithLongMonth(report.reportPeriod);
 
   before(() => {
-    const visibleBanks = [];
+    cy.task(NODE_TASKS.DELETE_ALL_FROM_SQL_DB);
+    cy.task(NODE_TASKS.REINSERT_ZERO_THRESHOLD_PAYMENT_MATCHING_TOLERANCES);
 
-    cy.task(NODE_TASKS.DELETE_ALL_TFM_FACILITIES_FROM_DB);
-    cy.task(NODE_TASKS.INSERT_TFM_FACILITIES_INTO_DB, TFM_FACILITIES);
+    cy.task(NODE_TASKS.INSERT_UTILISATION_REPORTS_INTO_DB, [report]);
+    cy.task(NODE_TASKS.INSERT_FEE_RECORDS_INTO_DB, [feeRecordAtToDoStatus]);
 
     cy.task(NODE_TASKS.GET_ALL_BANKS).then((getAllBanksResult) => {
-      getAllBanksResult
-        .filter((bank) => bank.isVisibleInTfmUtilisationReports)
-        .forEach((visibleBank) => {
-          visibleBanks.push(visibleBank);
-        });
+      const reportBank = getAllBanksResult.filter((bank) => bank.id === report.bankId);
+      bankName = reportBank.bankName;
     });
 
-    cy.task(NODE_TASKS.DELETE_ALL_FROM_SQL_DB);
-
-    cy.wrap(visibleBanks).each((bank) => {
-      const reportPeriod = getPreviousReportPeriodForBankScheduleByMonth(bank.utilisationReportPeriodSchedule, SUBMISSION_MONTH);
-
-      reportPeriodString = getFormattedReportPeriodWithLongMonth(reportPeriod);
-
-      if (bank.id === BANK_ID) {
-        bankName = bank.name;
-
-        const reportToReconcile = UtilisationReportEntityMockBuilder.forStatus(RECONCILIATION_IN_PROGRESS)
-          .withId(REPORT_ID)
-          .withBankId(BANK_ID)
-          .withReportPeriod(reportPeriod)
-          .build();
-        cy.task(NODE_TASKS.INSERT_UTILISATION_REPORTS_INTO_DB, [reportToReconcile]);
-
-        const feeRecordOne = FeeRecordEntityMockBuilder.forReport(reportToReconcile)
-          .withId(FEE_RECORD_ID_ONE)
-          .withFacilityId(FACILITY_ID_ONE)
-          .withExporter('Exporter 1')
-          .withPaymentCurrency(CURRENCY.GBP)
-          .withFeesPaidToUkefForThePeriod(100)
-          .withFeesPaidToUkefForThePeriodCurrency(CURRENCY.JPY)
-          .withPaymentExchangeRate(2)
-          .withStatus(FEE_RECORD_STATUS.MATCH)
-          .build();
-
-        cy.task(NODE_TASKS.INSERT_FEE_RECORDS_INTO_DB, [feeRecordOne]);
-      } else {
-        const mockUtilisationReport = UtilisationReportEntityMockBuilder.forStatus(REPORT_NOT_RECEIVED)
-          .withId(bank.id)
-          .withBankId(bank.id)
-          .withReportPeriod(reportPeriod)
-          .build();
-        cy.task(NODE_TASKS.INSERT_UTILISATION_REPORTS_INTO_DB, [mockUtilisationReport]);
-      }
-    });
+    const matchingTfmFacilities = getMatchingTfmFacilitiesForFeeRecords([feeRecordAtToDoStatus]);
+    cy.task(NODE_TASKS.INSERT_TFM_FACILITIES_INTO_DB, matchingTfmFacilities);
   });
 
   beforeEach(() => {
     pages.landingPage.visit();
     cy.login(USERS.PDC_RECONCILE);
 
-    cy.visit(`utilisation-reports/${REPORT_ID}`);
+    cy.visit(`utilisation-reports/${reportId}`);
   });
 
   describe('when navigating to the page', () => {
