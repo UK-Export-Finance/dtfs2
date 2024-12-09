04eb4701120c30fe8e79dfd365a2462c59ac94b4
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-12-09 11:44:32 +0000

    feat(DTFS2-7674): add unit tests for payload validation

--- libs/common/src/schemas/portal-amendment.ts
+++ libs/common/src/schemas/portal-amendment.ts
@@ -1,6 +1,7 @@
 import z from 'zod';
 import { CURRENCY } from '../constants';
 import { Currency } from '../types';
+import { UNIX_TIMESTAMP_SECONDS_SCHEMA } from './unix-timestamp.schema';
 
 /**
  * Portal Amendment schema to validate an object contains only user provided values on `PortalFacilityAmendment`
@@ -8,19 +9,16 @@ import { Currency } from '../types';
 export const PORTAL_FACILITY_AMENDMENT = z
   .object({
     changeCoverEndDate: z.boolean(),
-    coverEndDate: z.number().int().nullable().optional(),
-    currentCoverEndDate: z.number().int().nullable().optional(),
+    coverEndDate: UNIX_TIMESTAMP_SECONDS_SCHEMA.optional(),
+    currentCoverEndDate: UNIX_TIMESTAMP_SECONDS_SCHEMA.optional(),
     isUsingFacilityEndDate: z.boolean().optional(),
-    facilityEndDate: z.date().optional(),
-    bankReviewDate: z.date().optional(),
+    facilityEndDate: UNIX_TIMESTAMP_SECONDS_SCHEMA.optional(),
+    bankReviewDate: UNIX_TIMESTAMP_SECONDS_SCHEMA.optional(),
     changeFacilityValue: z.boolean().optional(),
-    value: z.number().nullable().optional(),
-    currentValue: z.number().nullable().optional(),
-    currency: z
-      .enum(Object.values(CURRENCY) as [Currency] & Currency[])
-      .nullable()
-      .optional(),
-    ukefExposure: z.number().nullable().optional(),
+    value: z.number().optional(),
+    currentValue: z.number().optional(),
+    currency: z.enum(Object.values(CURRENCY) as [Currency] & Currency[]).optional(),
+    ukefExposure: z.number().optional(),
     coveredPercentage: z.number().optional(),
   })
   .strict();
