2151f8bb769fa25b0c608c91fdbadd9c7b5545b4
Alex Bramhill <Alex.bramhill@softwire.com>
2024-12-09 18:41:08 +0000

    Revert "feat(dtfs2-6892): remove tfm ui changes"
    This reverts commit 466f33ad84b4f8a5856d415a28685c4be1542deb.

--- trade-finance-manager-ui/server/helpers/express-session.ts
+++ trade-finance-manager-ui/server/helpers/express-session.ts
@@ -1,11 +1,12 @@
 import { Request } from 'express';
-import { UserSessionNotDefinedError, UserTokenNotDefinedError } from '@ukef/dtfs2-common';
-import { UserSessionData } from '../types/express-session';
+import { UserPartialLoginDataNotDefinedError, UserSessionNotDefinedError, UserTokenNotDefinedError } from '@ukef/dtfs2-common';
+import { PartiallyLoggedInUserSessionData, UserSessionData } from '../types/express-session';
 
 type Session = Request['session'];
 
-type UserSession = Session & UserSessionData;
+export type UserSession = Session & UserSessionData;
 
+type PartiallyLoggedInUserSession = Session & PartiallyLoggedInUserSessionData;
 /**
  * By default, all session data will be optional
  * (see use of `Partial` {@link https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/express-session/index.d.ts#L17 here})
@@ -29,4 +30,17 @@ export const asUserSession = (session: Session): UserSession => {
   }
 
   return Object.assign(session, { user, userToken });
+};
+
+export const assertPartiallyLoggedInUser: (session: Session) => asserts session is PartiallyLoggedInUserSession = (session: Session) => {
+  const { loginData } = session;
+
+  if (!loginData) {
+    throw new UserPartialLoginDataNotDefinedError();
+  }
+};
+
+export const asPartiallyLoggedInUserSession = (session: Session): PartiallyLoggedInUserSession => {
+  assertPartiallyLoggedInUser(session);
+  return session;
 };
--- trade-finance-manager-ui/server/services/login.service.ts
+++ trade-finance-manager-ui/server/services/login.service.ts
@@ -1,5 +1,12 @@
-import { GetAuthCodeUrlParams, GetAuthCodeUrlResponse } from '@ukef/dtfs2-common';
+import { GetAuthCodeUrlParams, GetAuthCodeUrlResponse, HandleSsoRedirectFormRequest } from '@ukef/dtfs2-common';
+import { PartiallyLoggedInUserSessionData } from '../types/express-session';
 import * as api from '../api';
+
+type HandleSsoRedirectFormAndCreateTokenRequest = {
+  session: PartiallyLoggedInUserSessionData;
+} & HandleSsoRedirectFormRequest;
+
+type HandleSsoRedirectFormAndCreateTokenResponse = { successRedirect?: string };
 
 export class LoginService {
   /**
@@ -8,4 +15,25 @@ export class LoginService {
   public getAuthCodeUrl = async ({ successRedirect }: GetAuthCodeUrlParams): Promise<GetAuthCodeUrlResponse> => {
     return api.getAuthCodeUrl({ successRedirect });
   };
+
+  public handleSsoRedirectFormAndCreateToken = async ({
+    authCodeResponse,
+    originalAuthCodeUrlRequest,
+    // TODO as part of this ticket: uncomment the session parameter
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    session,
+    auditDetails,
+  }: HandleSsoRedirectFormAndCreateTokenRequest): Promise<HandleSsoRedirectFormAndCreateTokenResponse> => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { user, userToken, successRedirect } = await api.handleSsoRedirectForm({ authCodeResponse, originalAuthCodeUrlRequest, auditDetails });
+
+    // TODO: This can be moved into a separate session service
+    // delete session.loginData;
+    // session.user = user;
+    // session.userToken = userToken;
+
+    return { successRedirect };
+  };
+
+  public getSuccessRedirect = () => {};
 }
