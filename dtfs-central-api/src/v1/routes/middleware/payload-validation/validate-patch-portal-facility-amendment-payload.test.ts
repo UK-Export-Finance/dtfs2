7cd0ae7b0d1b8a4821fb8448242ebb80a5722f4e
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-12-10 15:56:08 +0000

    feat(DTFS2-7689): add unit test for patch payload validation

--- dtfs-central-api/src/v1/routes/middleware/payload-validation/validate-patch-portal-facility-amendment-payload.test.ts
+++ dtfs-central-api/src/v1/routes/middleware/payload-validation/validate-patch-portal-facility-amendment-payload.test.ts
@@ -1,204 +1,196 @@
-import { ObjectId } from 'mongodb';
 import { createMocks } from 'node-mocks-http';
 import { HttpStatusCode } from 'axios';
-import { AnyObject, AUDIT_USER_TYPES } from '@ukef/dtfs2-common';
-import { aPortalFacilityAmendmentUserValues } from '@ukef/dtfs2-common/mock-data-backend';
+import { getUnixTime } from 'date-fns';
+import { AUDIT_USER_TYPES, CURRENCY } from '@ukef/dtfs2-common';
 import { generatePortalAuditDetails } from '@ukef/dtfs2-common/change-stream';
 import { aPortalUser } from '../../../../../test-helpers';
-import { validatePutPortalFacilityAmendmentPayload } from './validate-put-portal-facility-amendment-payload';
+import { validatePatchPortalFacilityAmendmentPayload } from './validate-patch-portal-facility-amendment-payload';
 
-const validDealId = new ObjectId().toString();
 const validAuditDetails = generatePortalAuditDetails(aPortalUser()._id);
-const validAmendment = JSON.parse(JSON.stringify(aPortalFacilityAmendmentUserValues())) as AnyObject;
+const validUpdate = {
+  changeCoverEndDate: true,
+  coverEndDate: getUnixTime(new Date()),
+  currentCoverEndDate: getUnixTime(new Date()),
+  isUsingFacilityEndDate: true,
+  facilityEndDate: getUnixTime(new Date()),
+  bankReviewDate: getUnixTime(new Date()),
+  changeFacilityValue: true,
+  value: 1800,
+  currentValue: 1500,
+  currency: CURRENCY.GBP,
+  ukefExposure: 10,
+  coveredPercentage: 23,
+};
 
-describe('validatePutPortalFacilityAmendmentPayload', () => {
+describe('validatePatchPortalFacilityAmendmentPayload', () => {
   const invalidPayloads = [
     {
-      description: 'no amendment is provided',
+      description: 'no update is provided',
       payload: {
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment is not an object',
+      description: 'update is not an object',
       payload: {
-        amendment: 'not an object',
+        update: 'not an object',
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment has additional properties',
+      description: 'update has additional properties',
       payload: {
-        amendment: {
+        update: {
           extra: 'property',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.changeCoverEndDate is not a boolean',
+      description: 'update.changeCoverEndDate is not a boolean',
       payload: {
-        amendment: {
+        update: {
           changeCoverEndDate: 'true',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.coverEndDate is not an integer',
+      description: 'update.coverEndDate is not an integer',
       payload: {
-        amendment: {
+        update: {
           coverEndDate: 'not an integer',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.currentCoverEndDate is not an integer',
+      description: 'update.currentCoverEndDate is not an integer',
       payload: {
-        amendment: {
+        update: {
           currentCoverEndDate: 'not an integer',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.isUsingFacilityEndDate is not a boolean',
+      description: 'update.isUsingFacilityEndDate is not a boolean',
       payload: {
-        amendment: {
+        update: {
           isUsingFacilityEndDate: 'not a boolean',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.facilityEndDate is not a number',
+      description: 'update.facilityEndDate is not a number',
       payload: {
-        amendment: {
+        update: {
           facilityEndDate: 'not a number',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.facilityEndDate is negative',
+      description: 'update.facilityEndDate is negative',
       payload: {
-        amendment: {
+        update: {
           facilityEndDate: -23,
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.bankReviewDate is not a number',
+      description: 'update.bankReviewDate is not a number',
       payload: {
-        amendment: {
+        update: {
           bankReviewDate: 'not a number',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.bankReviewDate is negative',
+      description: 'update.bankReviewDate is negative',
       payload: {
-        amendment: {
+        update: {
           bankReviewDate: -23,
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.changeFacilityValue is not a boolean',
+      description: 'update.changeFacilityValue is not a boolean',
       payload: {
-        amendment: {
+        update: {
           changeFacilityValue: 'not a boolean',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.value is not a number',
+      description: 'update.value is not a number',
       payload: {
-        amendment: {
+        update: {
           value: 'not a number',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.currentValue is not a number',
+      description: 'update.currentValue is not a number',
       payload: {
-        amendment: {
+        update: {
           currentValue: 'not a number',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.currency is not a valid enum value',
+      description: 'update.currency is not a valid enum value',
       payload: {
-        amendment: {
+        update: {
           currency: 'invalid currency',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.ukefExposure is not a number',
+      description: 'update.ukefExposure is not a number',
       payload: {
-        amendment: {
+        update: {
           ukefExposure: 'not a number',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
-      description: 'amendment.coveredPercentage is not a number',
+      description: 'update.coveredPercentage is not a number',
       payload: {
-        amendment: {
+        update: {
           coveredPercentage: 'not a number',
         },
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
     {
       description: 'auditDetails is undefined',
       payload: {
-        amendment: validAmendment,
-        dealId: validDealId,
+        update: validUpdate,
         auditDetails: undefined,
       },
     },
     {
       description: 'auditDetails is an empty object',
       payload: {
-        amendment: validAmendment,
+        update: validUpdate,
         auditDetails: {},
-        dealId: validDealId,
       },
     },
     {
       description: 'auditDetails.userType is undefined',
       payload: {
-        amendment: validAmendment,
-        dealId: validDealId,
+        update: validUpdate,
+
         auditDetails: {
           userType: undefined,
         },
@@ -207,28 +199,12 @@ describe('validatePutPortalFacilityAmendmentPayload', () => {
     {
       description: 'auditDetails.id is invalid and type is portal',
       payload: {
-        amendment: validAmendment,
-        dealId: validDealId,
+        update: validUpdate,
+
         auditDetails: {
           userType: AUDIT_USER_TYPES.PORTAL,
           id: 'invalid',
         },
-      },
-    },
-    {
-      description: 'dealId is not a string',
-      payload: {
-        amendment: validAmendment,
-        dealId: 123456123456123456,
-        auditDetails: validAuditDetails,
-      },
-    },
-    {
-      description: 'dealId is not an object Id',
-      payload: {
-        amendment: validAmendment,
-        dealId: 'invalid deal id',
-        auditDetails: validAuditDetails,
       },
     },
   ];
@@ -239,7 +215,7 @@ describe('validatePutPortalFacilityAmendmentPayload', () => {
     const next = jest.fn();
 
     // Act
-    validatePutPortalFacilityAmendmentPayload(req, res, next);
+    validatePatchPortalFacilityAmendmentPayload(req, res, next);
 
     // Assert
     expect(res._getStatusCode()).toEqual(HttpStatusCode.BadRequest);
@@ -251,18 +227,16 @@ describe('validatePutPortalFacilityAmendmentPayload', () => {
     {
       description: 'the payload is valid',
       payload: {
-        amendment: validAmendment,
+        update: validUpdate,
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
 
     {
-      description: 'the amendment is an empty object',
+      description: 'the update is an empty object',
       payload: {
-        amendment: {},
+        update: {},
         auditDetails: validAuditDetails,
-        dealId: validDealId,
       },
     },
   ];
@@ -273,7 +247,7 @@ describe('validatePutPortalFacilityAmendmentPayload', () => {
     const next = jest.fn();
 
     // Act
-    validatePutPortalFacilityAmendmentPayload(req, res, next);
+    validatePatchPortalFacilityAmendmentPayload(req, res, next);
 
     // Assert
     expect(next).toHaveBeenCalled();
