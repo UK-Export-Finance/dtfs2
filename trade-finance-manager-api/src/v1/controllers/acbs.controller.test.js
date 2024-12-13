e7854dc5a9612e59998f364f75c9c9138a61032b
Abhi Markan <amarkan>
2024-12-13 10:54:27 +0000

    fix(DTFS-7694): added unit test cases

--- trade-finance-manager-api/src/v1/controllers/acbs.controller.test.js
+++ trade-finance-manager-api/src/v1/controllers/acbs.controller.test.js
@@ -1,11 +1,12 @@
 const { DURABLE_FUNCTIONS_LOG } = require('@ukef/dtfs2-common');
-const { issueAcbsFacilities, addToACBSLog } = require('./acbs.controller');
 const api = require('../api');
-const { mongoDbClient: db } = require('../../drivers/db-client');
 const CONSTANTS = require('../../constants');
-const { createACBS } = require('./acbs.controller');
 const MOCK_DEAL_ACBS = require('../__mocks__/mock-deal-acbs');
+
+const { createACBS, issueAcbsFacilities, addToACBSLog, updateIssuedFacilityAcbs, updateAmendedFacilityAcbs } = require('./acbs.controller');
+const { mongoDbClient: db } = require('../../drivers/db-client');
 const { findOneTfmDeal } = require('./deal.controller');
+const { updateFacilityAcbs } = require('./tfm.controller');
 
 const consoleErrorMock = jest.spyOn(console, 'error');
 consoleErrorMock.mockImplementation();
@@ -17,9 +18,9 @@ const insertOneMock = jest.fn().mockResolvedValue(true);
 const getCollectionMock = jest.spyOn(db, 'getCollection');
 getCollectionMock.mockResolvedValue({ insertOne: insertOneMock });
 
-const createACBSAPIMock = jest.fn().mockResolvedValue(123);
+const createAcbsApiMock = jest.fn().mockResolvedValue(123);
 const createACBSMock = jest.spyOn(api, 'createACBS');
-createACBSMock.mockResolvedValue(createACBSAPIMock);
+createACBSMock.mockResolvedValue(createAcbsApiMock);
 
 /**
  * Mock `deal.controller` imperative functions to
@@ -30,7 +31,16 @@ jest.mock('./deal.controller', () => ({
   findOneTfmDeal: jest.fn(),
 }));
 
-const mockACBSTaskLink = {
+/**
+ * Mock `tfm.controller` imperative functions to
+ * expedite unit test execution and keep it in function
+ * scope.
+ */
+jest.mock('./tfm.controller', () => ({
+  updateFacilityAcbs: jest.fn().mockResolvedValue(true),
+}));
+
+const mockAcbsTaskLink = {
   id: '5ce819935e539c343f141ece',
   statusQueryGetUri: 'acbs',
   sendEventPostUri: 'acbs',
@@ -39,8 +49,117 @@ const mockACBSTaskLink = {
   purgeHistoryDeleteUri: 'acbs',
 };
 
+const mockAcbsFacilityIssueTask = {
+  acbsTaskResult: {
+    name: 'acbs-issue-facility',
+    instanceId: 'c2e8705d3813421385da3bff2073121a',
+    runtimeStatus: DURABLE_FUNCTIONS_LOG.STATUS.COMPLETED,
+    customStatus: null,
+    createdTime: '2024-12-11T16:00:27Z',
+    lastUpdatedTime: '2024-12-11T16:00:50Z',
+    input: {
+      facilityIdentifier: '0041231975',
+      amendment: {
+        facility: {
+          _id: '77e10af22d146b1bb8f0fcf8',
+        },
+        deal: {},
+      },
+    },
+    output: {
+      facilityIdentifier: '0041231975',
+      issuedFacilityMaster: {
+        status: 200,
+        updateType: 'issue',
+        submittedToACBS: '2024-09-20T16:00:32+00:00',
+        receivedFromACBS: '2024-09-20T16:00:37+00:00',
+        dataSent: {
+          dealIdentifier: '0041231974',
+          facilityIdentifier: '0041231975',
+          portfolioIdentifier: 'E1',
+          dealBorrowerIdentifier: '001234567',
+          maximumLiability: 100000,
+          productTypeId: '280',
+          capitalConversionFactorCode: '8',
+          currency: 'GBP',
+          guaranteeCommencementDate: '2024-09-20',
+          guaranteeExpiryDate: '2025-09-20',
+          nextQuarterEndDate: '2024-12-31',
+          facilityInitialStatus: 'P',
+          facilityOverallStatus: 'A',
+          delegationType: 'A',
+          interestOrFeeRate: 2.1,
+          facilityStageCode: '07',
+          exposurePeriod: '22',
+          creditRatingCode: '10',
+          guaranteePercentage: 75,
+          premiumFrequencyCode: '2',
+          riskCountryCode: 'GBR',
+          riskStatusCode: '03',
+          effectiveDate: '2024-09-10',
+          forecastPercentage: 100,
+          issueDate: '2024-09-20',
+          description: 'GEF : 24 Months',
+          agentBankIdentifier: '00000000',
+          obligorPartyIdentifier: '00123456',
+          obligorName: 'TEST',
+          obligorIndustryClassification: '1234',
+          probabilityOfDefault: 1.0,
+          productTypeName: 'GEF',
+        },
+        facilityIdentifier: '0041231975',
+      },
+      facilityLoan: {
+        status: 201,
+        dataSent: {
+          postingDate: '2024-09-20',
+          borrowerPartyIdentifier: '00123456',
+          productTypeId: '280',
+          productTypeGroup: 'GM',
+          currency: 'GBP',
+          amount: 10000,
+          issueDate: '2024-09-20',
+          expiryDate: '2025-09-20',
+          spreadRate: 1.23,
+          nextDueDate: '2025-01-01',
+          yearBasis: '1',
+          loanBillingFrequencyType: 'E',
+        },
+        submittedToACBS: '2024-09-20T16:00:37+00:00',
+        receivedFromACBS: '2024-09-20T16:00:44+00:00',
+        bundleIdentifier: '0000412345',
+      },
+      facilityFee: {
+        status: 201,
+        dataSent: {
+          effectiveDate: '2024-09-20',
+          amount: 3348,
+          expirationDate: '2025-09-20',
+          nextDueDate: '2025-09-20',
+          nextAccrueToDate: '2025-09-20',
+          period: '01',
+          currency: 'GBP',
+          lenderTypeCode: '100',
+          incomeClassCode: 'FGT',
+          spreadToInvestorsIndicator: true,
+        },
+        submittedToACBS: '2024-09-20T16:00:44+00:00',
+        receivedFromACBS: '2024-09-20T16:00:50+00:00',
+        facilityIdentifier: '0000412346',
+      },
+    },
+    auditRecord: {
+      lastUpdatedAt: '2024-12-13T08:40:00.194 +00:00',
+      lastUpdatedByPortalUserId: null,
+      lastUpdatedByTfmUserId: null,
+      lastUpdatedByIsSystem: true,
+      noUserLoggedIn: null,
+    },
+  },
+};
+
 const updateACBSFacilityMock = jest.spyOn(api, 'issueACBSfacility');
-updateACBSFacilityMock.mockResolvedValue(mockACBSTaskLink);
+updateACBSFacilityMock.mockResolvedValue(mockAcbsTaskLink);
 
 const invalidDealIds = ['invalid', '', '0000000000', '123', 'ABC', '!"£', [], {}];
 
@@ -96,7 +215,7 @@ describe('addToACBSLog', () => {
       deal: {
         _id: '5ce819935e539c343f141ece',
       },
-      acbsTaskLinks: mockACBSTaskLink,
+      acbsTaskLinks: mockAcbsTaskLink,
     };
 
     const result = await addToACBSLog(payload);
@@ -110,8 +229,8 @@ describe('addToACBSLog', () => {
       facility: {},
       bank: {},
       status: DURABLE_FUNCTIONS_LOG.STATUS.RUNNING,
-      instanceId: mockACBSTaskLink.id,
-      acbsTaskLinks: mockACBSTaskLink,
+      instanceId: mockAcbsTaskLink.id,
+      acbsTaskLinks: mockAcbsTaskLink,
       submittedDate: expect.any(String),
       auditRecord: expect.any(Object),
     });
@@ -193,8 +312,8 @@ describe('issueAcbsFacilities', () => {
       facility: {},
       bank: {},
       status: DURABLE_FUNCTIONS_LOG.STATUS.RUNNING,
-      instanceId: mockACBSTaskLink.id,
-      acbsTaskLinks: mockACBSTaskLink,
+      instanceId: mockAcbsTaskLink.id,
+      acbsTaskLinks: mockAcbsTaskLink,
       submittedDate: expect.any(String),
       auditRecord: expect.any(Object),
     });
@@ -450,5 +569,55 @@ describe('createACBS', () => {
 
     expect(createACBSMock).toHaveBeenCalledTimes(1);
     expect(createACBSMock).toHaveBeenCalledWith(MOCK_DEAL_ACBS, bank);
+  });
+});
+
+describe('updateIssuedFacilityAcbs', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should update the facility acbs object when successfully issued in ACBS', async () => {
+    // Arrange
+    const { facilityIdentifier, issuedFacilityMaster, facilityLoan, facilityFee } = mockAcbsFacilityIssueTask.acbsTaskResult.output;
+
+    // Act
+    await updateIssuedFacilityAcbs(mockAcbsFacilityIssueTask.acbsTaskResult.output);
+
+    // Assert
+    expect(updateFacilityAcbs).toHaveBeenCalledTimes(1);
+    expect(updateFacilityAcbs).toHaveBeenCalledWith(facilityIdentifier, {
+      facilityStage: CONSTANTS.FACILITIES.ACBS_FACILITY_STAGE.ISSUED,
+      issuedFacilityMaster,
+      facilityLoan,
+      facilityFee,
+    });
+  });
+});
+
+describe('updateAmendedFacilityAcbs', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should update the facility acbs object when successfully amended in ACBS', async () => {
+    // Arrange
+    const { instanceId } = mockAcbsFacilityIssueTask.acbsTaskResult;
+    const { _id } = mockAcbsFacilityIssueTask.acbsTaskResult.input.amendment.facility;
+    const { facilityMasterRecord, facilityLoanRecord } = mockAcbsFacilityIssueTask.acbsTaskResult.output;
+
+    const mockAcbsUpdate = {
+      [instanceId]: {
+        facilityMasterRecord,
+        facilityLoanRecord,
+      },
+    };
+
+    // Act
+    await updateAmendedFacilityAcbs(mockAcbsFacilityIssueTask.acbsTaskResult);
+
+    // Assert
+    expect(updateFacilityAcbs).toHaveBeenCalledTimes(1);
+    expect(updateFacilityAcbs).toHaveBeenCalledWith(_id, mockAcbsUpdate);
   });
 });
