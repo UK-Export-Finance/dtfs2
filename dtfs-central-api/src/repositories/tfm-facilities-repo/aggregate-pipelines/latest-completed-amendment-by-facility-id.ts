1915d32dd5c645595d513e0f4602370f0e043db9
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-12-13 11:40:26 +0000

    feat(DTFS2-7674): fix typo

--- dtfs-central-api/src/repositories/tfm-facilities-repo/aggregate-pipelines/latest-completed-amendment-by-facility-id.ts
+++ dtfs-central-api/src/repositories/tfm-facilities-repo/aggregate-pipelines/latest-completed-amendment-by-facility-id.ts
@@ -4,7 +4,7 @@ import { AMENDMENT_STATUS, AMENDMENT_TYPES } from '@ukef/dtfs2-common';
 export const latestCompletedTfmAmendmentByFacilityId = (facilityId: string | ObjectId): Document[] => [
   { $match: { _id: { $eq: new ObjectId(facilityId) } } },
   { $unwind: '$amendments' },
-  { $match: { 'amendments.status': { $eq: AMENDMENT_STATUS.COMPLETED }, 'amendments.type': { $neq: AMENDMENT_TYPES.PORTAL } } },
+  { $match: { 'amendments.status': { $eq: AMENDMENT_STATUS.COMPLETED }, 'amendments.type': { $ne: AMENDMENT_TYPES.PORTAL } } },
   { $sort: { 'amendments.updatedAt': -1, 'amendments.version': -1 } },
   { $project: { _id: false, amendments: true } },
   { $limit: 1 },
