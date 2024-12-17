2ecdbfb3960640e6b672c922923bc9472a59dba0
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2024-12-17 09:46:43 +0000

    feat(DTFS2-7224): revert removing deal cancellation test

--- trade-finance-manager-ui/server/controllers/helpers/deal-cancellation-enabled.helper.test.ts
+++ trade-finance-manager-ui/server/controllers/helpers/deal-cancellation-enabled.helper.test.ts
@@ -15,61 +15,41 @@ const { AIN, MIN, MIA } = DEAL_SUBMISSION_TYPE;
 const { COMPLETED, PENDING, DRAFT } = TFM_DEAL_CANCELLATION_STATUS;
 
 describe('isDealCancellationEnabledForUser', () => {
-  describe('when user is a PIM user', () => {
-    it('should return false if the deal type is MIA', () => {
-      const result = isDealCancellationEnabledForUser(MIA, pimUser);
+  describe('when `FF_TFM_DEAL_CANCELLATION_ENABLED` is set to false', () => {
+    describe('when user is a PIM user', () => {
+      it.each([MIA, MIN, AIN])('should return false if the deal type is %s', (type) => {
+        const result = isDealCancellationEnabledForUser(type, pimUser);
 
-      expect(result).toEqual(false);
+        expect(result).toEqual(false);
+      });
     });
 
-    it('should return true if the deal type is MIN', () => {
-      const result = isDealCancellationEnabledForUser(MIN, pimUser);
+    describe('when user is not a PIM user', () => {
+      it.each([MIA, MIN, AIN])('should return false if the deal type is %s', (type) => {
+        const result = isDealCancellationEnabledForUser(type, nonPimUser);
 
-      expect(result).toEqual(true);
-    });
-
-    it('should return true if the deal type is AIN', () => {
-      const result = isDealCancellationEnabledForUser(AIN, pimUser);
-
-      expect(result).toEqual(true);
-    });
-  });
-
-  describe('when user is not a PIM user', () => {
-    it.each([MIA, MIN, AIN])('should return false if the deal type is %s', (type) => {
-      const result = isDealCancellationEnabledForUser(type, nonPimUser);
-
-      expect(result).toEqual(false);
+        expect(result).toEqual(false);
+      });
     });
   });
 });
 
 describe('isDealCancellationEnabled', () => {
-  describe('when user is a PIM user', () => {
-    it('should return false if the deal type is MIA', () => {
-      const result = isDealCancellationEnabled(MIA);
+  describe('when `FF_TFM_DEAL_CANCELLATION_ENABLED` is set to false', () => {
+    describe('when user is a PIM user', () => {
+      it.each([MIA, MIN, AIN])('should return false if the deal type is %s', (type) => {
+        const result = isDealCancellationEnabled(type);
 
-      expect(result).toEqual(false);
+        expect(result).toEqual(false);
+      });
     });
 
-    it('should return true if the deal type is MIN', () => {
-      const result = isDealCancellationEnabled(MIN);
+    describe('when user is not a PIM user', () => {
+      it.each([MIA, MIN, AIN])('should return false if the deal type is %s', (type) => {
+        const result = isDealCancellationEnabled(type);
 
-      expect(result).toEqual(true);
-    });
-
-    it('should return true if the deal type is AIN', () => {
-      const result = isDealCancellationEnabled(AIN);
-
-      expect(result).toEqual(true);
-    });
-  });
-
-  describe('when user is not a PIM user', () => {
-    it.each([MIA, MIN, AIN])('should return false if the deal type is %s', (type) => {
-      const result = isDealCancellationEnabledForUser(type, nonPimUser);
-
-      expect(result).toEqual(false);
+        expect(result).toEqual(false);
+      });
     });
   });
 });
