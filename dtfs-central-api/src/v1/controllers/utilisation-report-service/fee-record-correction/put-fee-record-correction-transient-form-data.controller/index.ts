59a1c9f1f6fd75c98112e91d1eda384fe7905e3d
Ruby Lavender <ruby.lavender@softwire.com>
2024-12-19 14:53:00 +0000

    feat(FN-3689): add contoller

---
+++ dtfs-central-api/src/v1/controllers/utilisation-report-service/fee-record-correction/put-fee-record-correction-transient-form-data.controller/index.ts
@@ -0,0 +1,72 @@
+import {
+  AnyObject,
+  ApiError,
+  FeeRecordCorrectionTransientFormDataEntity,
+  PortalSessionUser,
+  RecordCorrectionTransientFormData,
+  REQUEST_PLATFORM_TYPE,
+} from '@ukef/dtfs2-common';
+import { HttpStatusCode } from 'axios';
+import { Response } from 'express';
+import { CustomExpressRequest } from '../../../../../types/custom-express-request';
+import { NotFoundError } from '../../../../../errors';
+import { FeeRecordCorrectionRepo } from '../../../../../repositories/fee-record-correction-repo';
+import { FeeRecordCorrectionTransientFormDataRepo } from '../../../../../repositories/fee-record-correction-transient-form-data-repo';
+
+export type PutFeeRecordCorrectionTransientFormDataRequest = CustomExpressRequest<{
+  params: {
+    bankId: string;
+    correctionId: string;
+  };
+  reqBody: AnyObject;
+}>;
+
+/**
+ * Controller for the PUT fee record correction transient form data route.
+ *
+ * Validates the correction exists and belongs to a report for the correct bank.
+ *
+ * Then creates a new fee record correction transient form data entity with the
+ * correction id, user id and form data, and saves it.
+ * @param req - The {@link PutFeeRecordCorrectionTransientFormDataRequest} request object
+ * @param res - The response object
+ */
+export const putFeeRecordCorrectionTransientFormData = async (req: PutFeeRecordCorrectionTransientFormDataRequest, res: Response) => {
+  try {
+    const { correctionId: correctionIdString, bankId } = req.params;
+    const { user, formData } = req.body;
+
+    const correctionId = Number(correctionIdString);
+    const userId = (user as PortalSessionUser)._id.toString();
+
+    const correction = await FeeRecordCorrectionRepo.findByIdAndBankId(correctionId, bankId);
+
+    if (!correction) {
+      throw new NotFoundError(`Failed to find a correction with id '${correctionId}' for bank id '${bankId}'`);
+    }
+
+    // TODO: FN-3688 - validate the form data has the fields expected for the given reasons
+    const validatedFormData = formData as RecordCorrectionTransientFormData;
+
+    const newTransientFormData = FeeRecordCorrectionTransientFormDataEntity.create({
+      userId,
+      correctionId,
+      formData: validatedFormData,
+      requestSource: {
+        platform: REQUEST_PLATFORM_TYPE.PORTAL,
+        userId,
+      },
+    });
+
+    await FeeRecordCorrectionTransientFormDataRepo.save(newTransientFormData);
+
+    return res.sendStatus(HttpStatusCode.Ok);
+  } catch (error) {
+    const errorMessage = `Failed to put fee record correction transient form data`;
+    console.error(errorMessage, error);
+    if (error instanceof ApiError) {
+      return res.status(error.status).send(`${errorMessage}: ${error.message}`);
+    }
+    return res.status(HttpStatusCode.InternalServerError).send(errorMessage);
+  }
+};
