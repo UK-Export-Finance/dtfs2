6ea95cae6d67470fa3f1990c891e39569306516e
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2024-12-31 11:05:17 +0000

    chore(DTFS2-7727): refactor GEF clone deal E2E tests (#4096)
    Co-authored-by: Zain Kassam <zkassam@ukexportfinance.gov.uk>

---
+++ e2e-tests/gef/cypress/e2e/clone/clone-gef-MIN-deal.spec.js
@@ -0,0 +1,80 @@
+import CONSTANTS from '../../fixtures/constants';
+import { anIssuedContingentFacility } from '../../../../e2e-fixtures/mock-gef-facilities';
+import { BANK1_MAKER1 } from '../../../../e2e-fixtures/portal-users.fixture';
+import { MOCK_APPLICATION_MIN } from '../../fixtures/mocks/mock-deals';
+
+context('Clone GEF (MIN) deal', () => {
+  let MINdealId;
+  let token;
+  let facilityOneId;
+
+  const clonedDealName = 'Cloned MIN deal';
+  const facilityName = 'This Contingent facility 1';
+
+  const facilityToInsert = {
+    ...anIssuedContingentFacility(),
+    shouldCoverStartOnSubmission: false,
+    name: facilityName,
+  };
+
+  before(() => {
+    cy.loadData();
+    cy.apiLogin(BANK1_MAKER1)
+      .then((t) => {
+        token = t;
+      })
+      .then(() => {
+        cy.apiCreateApplication(BANK1_MAKER1, token).then(({ body }) => {
+          MINdealId = body._id;
+          cy.apiUpdateApplication(MINdealId, token, MOCK_APPLICATION_MIN).then(() => {
+            cy.apiCreateFacility(MINdealId, CONSTANTS.FACILITY_TYPE.CONTINGENT, token).then((facility) => {
+              facilityOneId = facility.body.details._id;
+              cy.apiUpdateFacility(facilityOneId, token, facilityToInsert);
+            });
+          });
+        });
+      });
+  });
+  describe('Clone MIN deal', () => {
+    let facilityId;
+
+    before(() => {
+      cy.cloneDeal(MINdealId, clonedDealName);
+
+      /**
+       * Fetches all deals
+       * Finds the deal which is cloned by bankInternalRefName matching clonedDealName
+       * Finds the id of the facility which is in progress
+       */
+      cy.apiLogin(BANK1_MAKER1)
+        .then((apiToken) => {
+          token = apiToken;
+        })
+        .then(() => cy.apiFetchAllGefApplications(token))
+        .then(({ body }) => {
+          body.items.forEach((item) => {
+            /**
+             * if the deal has the clonedDealName, then find the facility in progress in this deal
+             * one facility will be in progress as was issued and had a past cover start date
+             */
+            if (item.bankInternalRefName === clonedDealName) {
+              cy.apiFetchAllFacilities(item._id, token).then((res) => {
+                const facility = res.body.items.find((eachFacility) => eachFacility.details.name === facilityName);
+                facilityId = facility.details._id;
+              });
+            }
+          });
+        });
+    });
+
+    beforeEach(() => {
+      cy.saveSession();
+      cy.login(BANK1_MAKER1);
+      cy.get('table.govuk-table tr').eq(1).find('td').eq(1).find('.govuk-link').click();
+    });
+
+    it('should validate the information in the banner and deal', () => {
+      cy.checkClonedDealBannerAndDeal(clonedDealName, facilityId);
+    });
+  });
+});
---
+++ e2e-tests/gef/cypress/support/commands/submit-mock-data-loader-deal-to-checker.js
@@ -0,0 +1,38 @@
+import { DEAL_SUBMISSION_TYPE } from '@ukef/dtfs2-common';
+import { BANK1_MAKER1 } from '../../../../e2e-fixtures/portal-users.fixture';
+import applicationDetails from '../../e2e/pages/application-details';
+import relative from '../relativeURL';
+
+/**
+ * submitMockDataLoaderDealToChecker
+ * completes a deal from the mock data loader and submits it to the checker
+ * completes the automatic or manual cover forms based on provided dealType
+ * submits it to the checker
+ * @param {String} dealId - the deal id
+ * @param {String} dealType - if the deal is AIN or MIA
+ */
+const submitMockDataLoaderDealToChecker = (dealId, dealType = DEAL_SUBMISSION_TYPE.AIN) => {
+  cy.login(BANK1_MAKER1);
+
+  cy.visit(relative(`/gef/application-details/${dealId}`));
+  cy.url().should('eq', relative(`/gef/application-details/${dealId}`));
+
+  applicationDetails.automaticCoverDetailsLink().click();
+
+  if (dealType === DEAL_SUBMISSION_TYPE.AIN) {
+    // Make the deal an AIN
+    cy.automaticEligibilityCriteria();
+    cy.clickSaveAndReturnButton();
+  } else {
+    cy.manualEligibilityCriteria();
+    cy.clickContinueButton();
+    cy.clickContinueButton();
+
+    cy.completeManualInclusionSupportingInfoSections(dealId);
+  }
+
+  cy.clickSubmitButton();
+  cy.clickSubmitButton();
+};
+
+export default submitMockDataLoaderDealToChecker;
