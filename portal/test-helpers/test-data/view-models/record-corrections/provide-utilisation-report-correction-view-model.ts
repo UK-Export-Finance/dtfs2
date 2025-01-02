d8f894d57808e7fc21b8aba0eb2e2371a6cba58a
Matt <173997080+sw-mattw@users.noreply.github.com>
2025-01-02 11:55:36 +0000

    feat(FN-3668): add util report correction VM test helper

---
+++ portal/test-helpers/test-data/view-models/record-corrections/provide-utilisation-report-correction-view-model.ts
@@ -0,0 +1,19 @@
+import { CURRENCY, mapCurrenciesToRadioItems, RECORD_CORRECTION_REASON } from '@ukef/dtfs2-common';
+import { ProvideUtilisationReportCorrectionViewModel } from '../../../../server/types/view-models/record-correction/provide-utilisation-report-correction';
+import { PRIMARY_NAV_KEY } from '../../../../server/constants';
+
+export const aProvideUtilisationReportCorrectionViewModel = (): ProvideUtilisationReportCorrectionViewModel => ({
+  primaryNav: PRIMARY_NAV_KEY.UTILISATION_REPORT_UPLOAD,
+  correctionRequestDetails: {
+    facilityId: '12345678',
+    exporter: 'exporter name',
+    formattedReportedFees: `${CURRENCY.GBP} 77`,
+    reasons: [RECORD_CORRECTION_REASON.OTHER],
+    formattedReasons: 'Other',
+    additionalInfo: 'Some additional info',
+    errorTypeHeader: 'Error type',
+  },
+  paymentCurrencyOptions: mapCurrenciesToRadioItems(),
+  additionalCommentsLabel: 'Additional comments label',
+  additionalCommentsHint: 'Additional comments hint',
+});
