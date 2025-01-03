f39053908211555e1cf7537e1266d3edf1e88f53
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2025-01-03 11:11:29 +0000

    feat(DTFS2-7725): create amendment from make a change button

---
+++ gef-ui/server/controllers/amendments/create-draft/post-create-draft.ts
@@ -0,0 +1,21 @@
+import { CustomExpressRequest } from '@ukef/dtfs2-common';
+import { Response } from 'express';
+import * as api from '../../../services/api';
+import { asLoggedInUserSession } from '../../../utils/express-session';
+import { PORTAL_AMENDMENT_PAGES } from '../../../constants/amendments';
+
+export type PostCreateFacilityAmendmentRequest = CustomExpressRequest<{
+  params: { dealId: string; facilityId: string };
+}>;
+
+export const postCreateDraftFacilityAmendment = async (req: PostCreateFacilityAmendmentRequest, res: Response) => {
+  const { dealId, facilityId } = req.params;
+
+  const { userToken } = asLoggedInUserSession(req.session);
+
+  const { amendmentId } = await api.upsertAmendment({ facilityId, dealId, amendment: {}, userToken });
+
+  return res.redirect(
+    `/gef/application-details/${dealId}/facilities/${facilityId}/amendments/${amendmentId}/${PORTAL_AMENDMENT_PAGES.WHAT_DO_YOU_NEED_TO_CHANGE}`,
+  );
+};
