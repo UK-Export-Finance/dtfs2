eb92dc256269f487174d0e0df00165865f25c640
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2025-01-03 12:21:06 +0000

    feat(DTFS2-7725): add create draft amendment unit test

--- gef-ui/server/controllers/amendments/create-draft/post-create-draft.ts
+++ gef-ui/server/controllers/amendments/create-draft/post-create-draft.ts
@@ -9,13 +9,18 @@ export type PostCreateFacilityAmendmentRequest = CustomExpressRequest<{
 }>;
 
 export const postCreateDraftFacilityAmendment = async (req: PostCreateFacilityAmendmentRequest, res: Response) => {
-  const { dealId, facilityId } = req.params;
+  try {
+    const { dealId, facilityId } = req.params;
 
-  const { userToken } = asLoggedInUserSession(req.session);
+    const { userToken } = asLoggedInUserSession(req.session);
 
-  const { amendmentId } = await api.upsertAmendment({ facilityId, dealId, amendment: {}, userToken });
+    const { amendmentId } = await api.upsertAmendment({ facilityId, dealId, amendment: {}, userToken });
 
-  return res.redirect(
-    `/gef/application-details/${dealId}/facilities/${facilityId}/amendments/${amendmentId}/${PORTAL_AMENDMENT_PAGES.WHAT_DO_YOU_NEED_TO_CHANGE}`,
-  );
+    return res.redirect(
+      `/gef/application-details/${dealId}/facilities/${facilityId}/amendments/${amendmentId}/${PORTAL_AMENDMENT_PAGES.WHAT_DO_YOU_NEED_TO_CHANGE}`,
+    );
+  } catch (error) {
+    console.error('Error upserting draft facility amendment %o', error);
+    return res.render('partials/problem-with-service.njk');
+  }
 };
