a9fe9ec95bbfc04e54ff4f2e52c0c707f02c60fd
Ruby Lavender <ruby.lavender@softwire.com>
2025-01-06 14:13:58 +0000

    feat(FN-3667): remove import of deleted file

--- portal/test-helpers/test-data/index.ts
+++ portal/test-helpers/test-data/index.ts
@@ -1,6 +1,5 @@
 export * from './utilisation-report';
 export * from './report-period';
 export * from './utilisation-report-pending-corrections';
-export * from './portal-session-bank';
 export * from './utilisation-reporting-schedule';
 export * from './pending-corrections-view-model';
