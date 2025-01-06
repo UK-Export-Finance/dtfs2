a6306b8a30d4affa012297388133da6e4a6877b4
MarRobSoftwire <108682750+MarRobSoftwire@users.noreply.github.com>
2025-01-06 16:11:17 +0000

    feat(DTFS2-7725): update amendments routing (#4104)

---
+++ gef-ui/server/controllers/amendments/create-draft/post-create-draft.ts
@@ -0,0 +1,30 @@
+import { CustomExpressRequest } from '@ukef/dtfs2-common';
+import { Response } from 'express';
+import * as api from '../../../services/api';
+import { asLoggedInUserSession } from '../../../utils/express-session';
+import { PORTAL_AMENDMENT_PAGES } from '../../../constants/amendments';
+
+export type PostCreateFacilityAmendmentRequest = CustomExpressRequest<{
+  params: { dealId: string; facilityId: string };
+}>;
+
+/**
+ * Controller to overwrite draft amendment on facility
+ * @param req - the request object
+ * @param res - the response object
+ */
+export const postCreateDraftFacilityAmendment = async (req: PostCreateFacilityAmendmentRequest, res: Response) => {
+  try {
+    const { dealId, facilityId } = req.params;
+    const { userToken } = asLoggedInUserSession(req.session);
+
+    const { amendmentId } = await api.upsertAmendment({ facilityId, dealId, amendment: {}, userToken });
+
+    return res.redirect(
+      `/gef/application-details/${dealId}/facilities/${facilityId}/amendments/${amendmentId}/${PORTAL_AMENDMENT_PAGES.WHAT_DO_YOU_NEED_TO_CHANGE}`,
+    );
+  } catch (error) {
+    console.error('Error upserting draft facility amendment %o', error);
+    return res.render('partials/problem-with-service.njk');
+  }
+};
