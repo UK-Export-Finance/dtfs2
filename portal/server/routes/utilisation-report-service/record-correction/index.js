5929c3e1d6c1ca777a8f43adad96782ab5ab2bbf
Matt <173997080+sw-mattw@users.noreply.github.com>
2025-01-10 10:50:08 +0000

    feat(FN-3669): align route with util report correction naming

--- portal/server/routes/utilisation-report-service/record-correction/index.js
+++ portal/server/routes/utilisation-report-service/record-correction/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { ROLES, validateFeeRecordCorrectionFeatureFlagIsEnabled } = require('@ukef/dtfs2-common');
-const { getProvideUtilisationReportCorrection, getRecordCorrectionInformation } = require('../../../controllers/utilisation-report-service/record-correction');
+const {
+  getProvideUtilisationReportCorrection,
+  getUtilisationReportCorrectionReview,
+} = require('../../../controllers/utilisation-report-service/record-correction');
 const { validateRole, validateToken, validateSqlId } = require('../../middleware');
 
 const router = express.Router();
@@ -16,7 +19,7 @@ router.get(
   '/utilisation-reports/provide-correction/:correctionId/check-the-information',
   [validateFeeRecordCorrectionFeatureFlagIsEnabled, validateToken, validateRole({ role: [ROLES.PAYMENT_REPORT_OFFICER] })],
   validateSqlId('correctionId'),
-  (req, res) => getRecordCorrectionInformation(req, res),
+  (req, res) => getUtilisationReportCorrectionReview(req, res),
 );
 
 module.exports = router;
