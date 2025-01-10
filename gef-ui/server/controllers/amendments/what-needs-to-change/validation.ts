0c245654ce6bdc0267fd3462cb4966dd32207f09
Beth Thomas <beth.thomas@softwire.com>
2025-01-10 14:21:47 +0000

    feat(DTFS2-7523): add post validation files

---
+++ gef-ui/server/controllers/amendments/what-needs-to-change/validation.ts
@@ -0,0 +1,25 @@
+import { ValidationError } from '../../../types/validation-error.ts';
+
+const errRef = 'whatNeedsToChange';
+
+/**
+ * @param changeCoverEndDate The reason for cancelling
+ * @param changeFacilityValue The reason for cancelling
+ * @returns a reason for cancelling errors view model
+ */
+export const validateWhatNeedsToChange = ({
+  changeCoverEndDate,
+  changeFacilityValue,
+}: {
+  changeCoverEndDate: boolean | undefined;
+  changeFacilityValue: boolean | undefined;
+}): ValidationError | null => {
+  if (!changeCoverEndDate && !changeFacilityValue) {
+    return {
+      errRef,
+      errMsg: 'Select if you need to change the facility cover end date, value or both',
+    };
+  }
+
+  return null;
+};
