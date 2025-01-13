7b0c288746fb856aee6ec562e8a34c789a0bb0ec
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2025-01-13 16:40:12 +0000

    feat(DTFS2-7736): use FF_PORTAL_FACILITY_AMENDMENTS_ENABLED in test descriptions

--- gef-ui/api-tests/amendments/cover-end-date.api-test.ts
+++ gef-ui/api-tests/amendments/cover-end-date.api-test.ts
@@ -63,7 +63,7 @@ describe(`GET ${url}`, () => {
     process.env = originalEnv;
   });
 
-  describe('when portal facility amendments feature flag is disabled', () => {
+  describe('when FF_PORTAL_FACILITY_AMENDMENTS_ENABLED is disabled', () => {
     beforeEach(() => {
       process.env.FF_PORTAL_FACILITY_AMENDMENTS_ENABLED = 'false';
     });
@@ -78,7 +78,7 @@ describe(`GET ${url}`, () => {
     });
   });
 
-  describe('when portal facility amendments feature flag is enabled', () => {
+  describe('when FF_PORTAL_FACILITY_AMENDMENTS_ENABLED is enabled', () => {
     beforeEach(() => {
       process.env.FF_PORTAL_FACILITY_AMENDMENTS_ENABLED = 'true';
     });
