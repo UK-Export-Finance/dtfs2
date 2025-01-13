8c66e114971bca2f156f454d0f88306b0ab78e4a
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2025-01-13 12:33:00 +0000

    feat(DTFS2-7737): add post facility end date controller

--- gef-ui/server/routes/facilities/amendments/index.ts
+++ gef-ui/server/routes/facilities/amendments/index.ts
@@ -14,6 +14,7 @@ import { getCancelPortalFacilityAmendment } from '../../../controllers/amendment
 import { getDoYouHaveAFacilityEndDate } from '../../../controllers/amendments/do-you-have-a-facility-end-date/get-do-you-have-a-facility-end-date';
 import { postDoYouHaveAFacilityEndDate } from '../../../controllers/amendments/do-you-have-a-facility-end-date/post-do-you-have-a-facility-end-date';
 import { getFacilityEndDate } from '../../../controllers/amendments/facility-end-date/get-facility-end-date';
+import { postFacilityEndDate } from '../../../controllers/amendments/facility-end-date/post-facility-end-date';
 
 const { WHAT_DO_YOU_NEED_TO_CHANGE, COVER_END_DATE, FACILITY_VALUE, DO_YOU_HAVE_A_FACILITY_END_DATE, FACILITY_END_DATE } = PORTAL_AMENDMENT_PAGES;
 
@@ -55,6 +56,7 @@ router
 router
   .route(`/application-details/:dealId/facilities/:facilityId/amendments/:amendmentId/${FACILITY_END_DATE}`)
   .all([validatePortalFacilityAmendmentsEnabled, validateToken, validateBank, validateRole({ role: [MAKER] })])
-  .get(getFacilityEndDate);
+  .get(getFacilityEndDate)
+  .post(postFacilityEndDate);
 
 export default router;
