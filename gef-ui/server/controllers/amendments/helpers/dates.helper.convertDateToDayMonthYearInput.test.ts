e4f3696a081dd7214988c031e723f7fc74c9f353
Beth Thomas <beth.thomas@softwire.com>
2025-01-14 15:52:15 +0000

    feat(DTFS2-7621): add more date cases

--- gef-ui/server/controllers/amendments/helpers/dates.helper.convertDateToDayMonthYearInput.test.ts
+++ gef-ui/server/controllers/amendments/helpers/dates.helper.convertDateToDayMonthYearInput.test.ts
@@ -18,6 +18,16 @@ describe('convertDateToDayMonthYearInput', () => {
       date: new Date('2026-06-09'),
       expected: { day: '9', month: '6', year: '2026' },
     },
+    {
+      description: '01/01/1970',
+      date: new Date('1970-01-01'),
+      expected: { day: '1', month: '1', year: '1970' },
+    },
+    {
+      description: '9999 year',
+      date: new Date('9999-12-31'),
+      expected: { day: '31', month: '12', year: '9999' },
+    },
   ];
 
   describe.each(testCases)('$description', ({ date, expected }: { date: Date; expected: DayMonthYearInput }) => {
