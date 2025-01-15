798040d80ef9383c9d6ed6285f716fdb3e1b0cfe
Matt <173997080+sw-mattw@users.noreply.github.com>
2025-01-15 11:48:09 +0000

    feat(FN-3688): split out individual field validation into new helper file

---
+++ dtfs-central-api/src/v1/controllers/utilisation-report-service/fee-record-correction/put-fee-record-correction-transient-form-data.controller/helpers/get-record-correction-transient-form-validation-error.ts
@@ -0,0 +1,53 @@
+import { RecordCorrectionReason, MAX_RECORD_CORRECTION_ADDITIONAL_INFO_CHARACTER_COUNT } from '@ukef/dtfs2-common';
+import { UKEF_FACILITY_ID_REGEX } from '../../../../../../constants/regex';
+import { TfmFacilitiesRepo } from '../../../../../../repositories/tfm-facilities-repo';
+
+/**
+ * Validates a facility ID value.
+ * @param facilityIdValue - The facility ID to validate.
+ * @returns A promise that resolves to an error message if validation fails, undefined otherwise.
+ * The error message will indicate if:
+ * - The facility ID is missing or does not match the required format (8-10 digits)
+ * - The facility ID does not correspond to an existing General Export Facility
+ */
+export const getFacilityIdValidationError = async (facilityIdValue?: string): Promise<string | undefined> => {
+  if (!facilityIdValue || !UKEF_FACILITY_ID_REGEX.test(facilityIdValue)) {
+    return 'You must enter a facility ID between 8 and 10 digits using the numbers 0-9 only';
+  }
+
+  const ukefGefFacilityExists = await TfmFacilitiesRepo.ukefGefFacilityExists(facilityIdValue);
+
+  if (!ukefGefFacilityExists) {
+    return 'The facility ID entered has not been recognised, please enter a facility ID for a General Export Facility';
+  }
+
+  return undefined;
+};
+
+/**
+ * Validates the additional comments field for a record correction request.
+ *
+ * If the character count exceeds the maximum allowed character count, the
+ * input field referenced in the error message is matched to the fields label.
+ *
+ * @param correctionReasons - The reasons for the record correction request.
+ * @param additionalComments - The additional comments text to validate.
+ * @returns An error message if validation fails, undefined otherwise.
+ */
+export const getAdditionalCommentsValidationError = (correctionReasons: RecordCorrectionReason[], additionalComments?: string): string | undefined => {
+  if (!additionalComments) {
+    return 'You must enter a comment';
+  }
+
+  if (additionalComments.length < MAX_RECORD_CORRECTION_ADDITIONAL_INFO_CHARACTER_COUNT) {
+    return undefined;
+  }
+
+  const baseCharacterLimitExceededErrorMessage = `You cannot enter more than ${MAX_RECORD_CORRECTION_ADDITIONAL_INFO_CHARACTER_COUNT} characters in the`;
+
+  if (correctionReasons.length === 1) {
+    return `${baseCharacterLimitExceededErrorMessage} record information box`;
+  }
+
+  return `${baseCharacterLimitExceededErrorMessage} additional comments box`;
+};
--- dtfs-central-api/src/v1/controllers/utilisation-report-service/fee-record-correction/put-fee-record-correction-transient-form-data.controller/helpers/validate-record-correction-transient-form-values.ts
+++ dtfs-central-api/src/v1/controllers/utilisation-report-service/fee-record-correction/put-fee-record-correction-transient-form-data.controller/helpers/validate-record-correction-transient-form-values.ts
@@ -1,15 +1,13 @@
 import {
   isCurrencyValid,
   isMonetaryAmountValid,
-  MAX_RECORD_CORRECTION_ADDITIONAL_INFO_CHARACTER_COUNT,
   RECORD_CORRECTION_REASON,
   RecordCorrectionFormValues,
   RecordCorrectionFormValueValidationErrors,
   RecordCorrectionReason,
 } from '@ukef/dtfs2-common';
 import { difference } from 'lodash';
-import { UKEF_FACILITY_ID_REGEX } from '../../../../../../constants/regex';
-import { TfmFacilitiesRepo } from '../../../../../../repositories/tfm-facilities-repo';
+import { getFacilityIdValidationError, getAdditionalCommentsValidationError } from './get-record-correction-transient-form-validation-error';
 
 // TODO: Add unit tests
 /**
@@ -53,56 +51,6 @@ export const validateNoUnexpectedReasonValues = (formValues: RecordCorrectionFor
       throw new Error(`Unexpected form value "${reasonFormValue}" for reason "${reason}" not in the correction request reasons.`);
     }
   });
-};
-
-/**
- * Validates a facility ID value.
- * @param facilityIdValue - The facility ID to validate.
- * @returns A promise that resolves to an error message if validation fails, undefined otherwise.
- * The error message will indicate if:
- * - The facility ID is missing or does not match the required format (8-10 digits)
- * - The facility ID does not correspond to an existing General Export Facility
- */
-export const getFacilityIdValidationError = async (facilityIdValue?: string): Promise<string | undefined> => {
-  if (!facilityIdValue || !UKEF_FACILITY_ID_REGEX.test(facilityIdValue)) {
-    return 'You must enter a facility ID between 8 and 10 digits using the numbers 0-9 only';
-  }
-
-  const ukefGefFacilityExists = await TfmFacilitiesRepo.ukefGefFacilityExists(facilityIdValue);
-
-  if (!ukefGefFacilityExists) {
-    return 'The facility ID entered has not been recognised, please enter a facility ID for a General Export Facility';
-  }
-
-  return undefined;
-};
-
-/**
- * Validates the additional comments field for a record correction request.
- *
- * If the character count exceeds the maximum allowed character count, the
- * input field referenced in the error message is matched to the fields label.
- *
- * @param correctionReasons - The reasons for the record correction request.
- * @param additionalComments - The additional comments text to validate.
- * @returns An error message if validation fails, undefined otherwise.
- */
-export const getAdditionalCommentsValidationError = (correctionReasons: RecordCorrectionReason[], additionalComments?: string): string | undefined => {
-  if (!additionalComments) {
-    return 'You must enter a comment';
-  }
-
-  if (additionalComments.length < MAX_RECORD_CORRECTION_ADDITIONAL_INFO_CHARACTER_COUNT) {
-    return undefined;
-  }
-
-  const baseCharacterLimitExceededErrorMessage = `You cannot enter more than ${MAX_RECORD_CORRECTION_ADDITIONAL_INFO_CHARACTER_COUNT} characters in the`;
-
-  if (correctionReasons.length === 1) {
-    return `${baseCharacterLimitExceededErrorMessage} record information box`;
-  }
-
-  return `${baseCharacterLimitExceededErrorMessage} additional comments box`;
 };
 
 type ValidationMap = Record<
