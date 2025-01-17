a8c0dff25ebc61e934c9a7dc30eec7ef4711590b
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2025-01-17 11:38:05 +0000

    feat(DTFS2-7768): add documentation on what each status means

--- libs/common/src/constants/amendments.ts
+++ libs/common/src/constants/amendments.ts
@@ -1,10 +1,23 @@
 export const TFM_AMENDMENT_STATUS = {
+  /**
+   * Amendment is created but no values have been provided
+   */
   NOT_STARTED: 'Not started',
+  /**
+   * Amendment is in progress. This includes PIM creating the amendment and (if `requireUkefApproval` is true) the underwriting process
+   */
   IN_PROGRESS: 'In progress',
+  /**
+   * Amendment has been completed. It may have been accepted or rejected
+   */
   COMPLETED: 'Completed',
 } as const;
 
 export const PORTAL_AMENDMENT_STATUS = {
+  /**
+   * Amendment is in progress by the maker but not submitted to the checker
+   * This is not displayed in the UI
+   */
   DRAFT: 'Draft',
 } as const;
 
