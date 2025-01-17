2f44a890adb9d3f04481ac691bc3fb1efc893791
Abhi Markan <amarkan>
2025-01-17 14:05:42 +0000

    fix(DTFS2-7771): added api tests

--- portal/api-tests/static-routes.api-test.js
+++ portal/api-tests/static-routes.api-test.js
@@ -17,16 +17,47 @@ const { get } = require('./create-api').createApi(app);
 
 const allRoles = Object.values(ROLES);
 
-describe('static routes', () => {
-  describe('GET /.well-known/security.txt', () => {
+describe('/.well-known/security.txt', () => {
+  describe('GET', () => {
     withRoleValidationApiTests({
       makeRequestWithHeaders: (headers) => get('/.well-known/security.txt', {}, headers),
       whitelistedRoles: allRoles,
       successCode: 200,
     });
-  });
 
-  describe('GET /thanks.txt', () => {
+    it('should have all the file properties', async () => {
+      // Act
+      const response = await get('/.well-known/security.txt');
+
+      // Assert
+      expect(response.text).toContain('Contact: https://hackerone.com/7af14fd9-fe4e-4f39-bea1-8f8a364061b8/embedded_submissions/new');
+      expect(response.text).toContain('Contact: https://www.gov.uk/contact/govuk');
+      expect(response.text).toContain('Contact: https://get-a-guarantee-for-export-finance.service.gov.uk/feedback');
+      expect(response.text).toContain('Expires: 2026-01-17T00:00:00.000Z');
+      expect(response.text).toContain('Acknowledgments: https://get-a-guarantee-for-export-finance.service.gov.uk/thanks.txt');
+      expect(response.text).toContain('Preferred-Languages: en');
+      expect(response.text).toContain('Canonical: https://get-a-guarantee-for-export-finance.service.gov.uk/.well-known/security.txt');
+      expect(response.text).toContain('Policy: https://www.gov.uk/guidance/report-a-vulnerability-on-a-ukef-system');
+      expect(response.text).toContain('Hiring: https://www.civilservicejobs.service.gov.uk/');
+    });
+
+    it('should ensure file is not expired', async () => {
+      // Act
+      const response = await get('/.well-known/security.txt');
+      // Below should extract expiry date in YYYY-MM-DD format
+      const expires = response.text.split('Expires: ')[1].split('T00:00:00.000Z')[0];
+      // Convert to EPOCH
+      const today = new Date().valueOf();
+      const expiry = new Date(expires).valueOf();
+
+      // Assert
+      expect(today).toBeLessThan(expiry);
+    });
+  });
+});
+
+describe('/thanks.txt', () => {
+  describe('GET', () => {
     withRoleValidationApiTests({
       makeRequestWithHeaders: (headers) => get('/thanks.txt', {}, headers),
       whitelistedRoles: allRoles,
