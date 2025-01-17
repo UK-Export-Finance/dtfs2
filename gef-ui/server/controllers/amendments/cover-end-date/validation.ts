8770c0bbcbfb0f3326cf9e807d2c3d407c5652fb
rkontogianni <rkontogianni@ukexportfinance.gov.uk>
2025-01-17 14:52:22 +0000

    feat(DTFS2-7706): improve doc

--- gef-ui/server/controllers/amendments/cover-end-date/validation.ts
+++ gef-ui/server/controllers/amendments/cover-end-date/validation.ts
@@ -4,9 +4,9 @@ import { ErrorsOrValue } from '../../../types/errors-or-value';
 import { mapValidationError } from '../../../utils/map-validation-error';
 
 /**
- * @param dayMonthYear coverEndDate in DayMonthYearInput format
- * @param coverStartDate
- * @returns ErrorsOrValue<UnixTimestampSeconds>
+ * @param {DayMonthYearInput} [params.dayMonthYear] - The cover end date as a DayMonthYearInput object
+ * @param {Date} [params.dayMonthYear] - The cover start date as Date
+ * @returns ErrorsOrValue<UnixTimestampSeconds> the value or errors depending on the validation result
  */
 export const validateAndParseCoverEndDate = (dayMonthYear: DayMonthYearInput, coverStartDate: Date): ErrorsOrValue<UnixTimestampSeconds> => {
   const errRef = 'coverEndDate';
