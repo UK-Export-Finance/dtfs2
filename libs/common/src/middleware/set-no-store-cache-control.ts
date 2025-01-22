8d27ee7308dac9c2c576713ac9ec5f3fde13aa61
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2025-01-22 10:20:24 +0000

    feat(FN-3611): fix linting error

--- libs/common/src/middleware/set-no-store-cache-control.ts
+++ libs/common/src/middleware/set-no-store-cache-control.ts
@@ -4,11 +4,11 @@ import { Request, Response, NextFunction } from 'express';
  * middleware to set Cache-Control to no-store
  * stops cached version of a page being rendered when going back to a page (using the browser back button)
  * ensures the get controller for the page is called (if cached, get controller for a page is not called)
- * @param req
+ * @param _req
  * @param res
  * @param next
  */
-export const setNoStoreCacheControl = (req: Request, res: Response, next: NextFunction) => {
+export const setNoStoreCacheControl = (_req: Request, res: Response, next: NextFunction) => {
   res.set('Cache-Control', 'no-store');
   next();
 };
