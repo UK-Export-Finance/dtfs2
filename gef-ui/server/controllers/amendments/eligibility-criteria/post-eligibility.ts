d4670f223b7c8d6e045bc670facf77f00dcca368
Beth Thomas <beth.thomas@softwire.com>
2025-01-23 09:33:49 +0000

    feat(DTFS2-7498): add eligibility post controller

---
+++ gef-ui/server/controllers/amendments/eligibility-criteria/post-eligibility.ts
@@ -0,0 +1,77 @@
+import { CustomExpressRequest } from '@ukef/dtfs2-common';
+import { Response } from 'express';
+import * as api from '../../../services/api';
+import { asLoggedInUserSession } from '../../../utils/express-session';
+import { EligibilityReqBody, parseEligibilityResponse } from '../helpers/eligibility.helper.ts';
+import { EligibilityViewModel } from '../../../types/view-models/amendments/eligibility-view-model.ts';
+import { getNextPage } from '../helpers/navigation.helper.ts';
+import { PORTAL_AMENDMENT_PAGES } from '../../../constants/amendments.ts';
+import { validationErrorHandler } from '../../../utils/helpers';
+import { validateEligibilityResponse } from './validation.ts';
+
+export type PostEligibilityRequest = CustomExpressRequest<{
+  params: { dealId: string; facilityId: string; amendmentId: string };
+  reqBody: EligibilityReqBody & { previousPage: string };
+}>;
+
+/**
+ * Controller to post eligibility criteria responses
+ * @param req - The express request
+ * @param res - The express response
+ */
+export const postEligibility = async (req: PostEligibilityRequest, res: Response) => {
+  try {
+    const { dealId, facilityId, amendmentId } = req.params;
+    const { previousPage } = req.body;
+    const { userToken } = asLoggedInUserSession(req.session);
+
+    const deal = await api.getApplication({ dealId, userToken });
+    const { details: facility } = await api.getFacility({ facilityId, userToken });
+
+    if (!deal || !facility) {
+      console.error('Deal %s or Facility %s was not found', dealId, facilityId);
+      return res.redirect('/not-found');
+    }
+
+    const amendment = await api.getAmendment({ facilityId, amendmentId, userToken });
+    if (!amendment) {
+      console.error('Amendment %s was not found on facility %s', amendmentId, facilityId);
+      return res.redirect('/not-found');
+    }
+
+    const {
+      eligibilityCriteria: { version, criteria },
+    } = amendment;
+
+    if (!criteria) {
+      console.error('Eligibility criteria was not found on amendment %s on facility %s', amendmentId, facilityId);
+      return res.redirect('/not-found');
+    }
+
+    const parsedResponse = parseEligibilityResponse(req.body, criteria);
+
+    const errorsOrValidCriteria = validateEligibilityResponse(parsedResponse);
+
+    if ('errors' in errorsOrValidCriteria) {
+      const viewModel: EligibilityViewModel = {
+        exporterName: deal.exporter.companyName,
+        facilityType: facility.type,
+        cancelUrl: `/gef/application-details/${dealId}/facilities/${facilityId}/amendments/${amendmentId}/cancel`,
+        previousPage,
+        criteria: parsedResponse,
+        errors: validationErrorHandler(errorsOrValidCriteria.errors),
+      };
+
+      return res.render('partials/amendments/eligibility.njk', viewModel);
+    }
+
+    const update = { eligibilityCriteria: { version, criteria: errorsOrValidCriteria.value } };
+
+    const updatedAmendment = await api.updateAmendment({ facilityId, amendmentId, update, userToken });
+
+    return res.redirect(getNextPage(PORTAL_AMENDMENT_PAGES.ELIGIBILITY, updatedAmendment));
+  } catch (error) {
+    console.error('Error posting amendments eligibility page %o', error);
+    return res.render('partials/problem-with-service.njk');
+  }
+};
---
+++ gef-ui/server/controllers/amendments/helpers/eligibility.helper.ts
@@ -0,0 +1,50 @@
+import { AmendmentsEligibilityCriterionWithAnswer } from '@ukef/dtfs2-common';
+
+export type EligibilityReqBody = {
+  [key: string]: string;
+};
+
+/**
+ * Parses the result of the nunjucks radio boxes ('true' or 'false') as a boolean
+ * @param value - the string to parse as a boolean
+ * @returns the value as a boolean if it was 'true' or 'false', otherwise null
+ */
+export const parseStringAsBoolean = (value: string): boolean | null => {
+  if (value === 'true') {
+    return true;
+  }
+
+  if (value === 'false') {
+    return false;
+  }
+
+  return null;
+};
+
+/**
+ * The nunjucks file returns the eligibility criteria responses as an object of the form e.g. { '1': 'true', '2': 'false', '3': 'true' }.
+ * This function returns the eligibility criteria responses in an array of objects more suitable for database storage: [{ id: 1, text: 'criteria 1 text', answer: true }, { id: 2, .... }, ... ]
+ * @param responseBody - the response received from the nunjucks file after user submission
+ * @param currentCriteriaResponses - the eligibility criteria existing on the amendment, these may have previous answers attached to them or all have null for the `answer` field
+ * @returns eligibility criteria responses in a format to validate and save to the amendment
+ */
+export const parseEligibilityResponse = (
+  responseBody: EligibilityReqBody,
+  currentCriteriaResponses: AmendmentsEligibilityCriterionWithAnswer[],
+): AmendmentsEligibilityCriterionWithAnswer[] => {
+  const criteriaWithNewResponses: AmendmentsEligibilityCriterionWithAnswer[] = [];
+
+  currentCriteriaResponses.forEach((item: AmendmentsEligibilityCriterionWithAnswer) => {
+    const { id, text, textList } = item;
+
+    const answer = responseBody[id.toString()] ? parseStringAsBoolean(responseBody[id.toString()]) : null;
+
+    if (textList?.length) {
+      criteriaWithNewResponses.push({ id, text, textList, answer });
+    } else {
+      criteriaWithNewResponses.push({ id, text, answer });
+    }
+  });
+
+  return criteriaWithNewResponses;
+};
