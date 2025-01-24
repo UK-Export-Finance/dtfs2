411475940a0cd36232f0c616e5fd465cc980b491
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2025-01-24 10:22:06 +0000

    feat: add new feature flag to test.yml e2e tests and qat

--- libs/common/src/helpers/is-feature-flag-enabled.test.ts
+++ libs/common/src/helpers/is-feature-flag-enabled.test.ts
@@ -14,8 +14,5 @@ describe('is-feature-flag-enabled helpers', () => {
   withBooleanFeatureFlagTests({ featureFlagName: 'FF_TFM_DEAL_CANCELLATION_ENABLED', getFeatureFlagValue: isTfmDealCancellationFeatureFlagEnabled });
   withBooleanFeatureFlagTests({ featureFlagName: 'FF_PORTAL_FACILITY_AMENDMENTS_ENABLED', getFeatureFlagValue: isPortalFacilityAmendmentsFeatureFlagEnabled });
   withBooleanFeatureFlagTests({ featureFlagName: 'FF_TFM_SSO_ENABLED', getFeatureFlagValue: isTfmSsoFeatureFlagEnabled });
-  withBooleanFeatureFlagTests({
-    featureFlagName: 'FF_SALESFORCE_CUSTOMER_CREATION_ENABLED',
-    getFeatureFlagValue: isSalesforceCustomerCreationEnabled,
-  });
+  withBooleanFeatureFlagTests({ featureFlagName: 'FF_SALESFORCE_CUSTOMER_CREATION_ENABLED', getFeatureFlagValue: isSalesforceCustomerCreationEnabled });
 });
