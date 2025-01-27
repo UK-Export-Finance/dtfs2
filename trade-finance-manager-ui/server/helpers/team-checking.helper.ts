ad717a414b62e58641a84dde12971974b6dfeab7
Alex Bramhill <alex.bramhill@softwire.com>
2025-01-27 14:39:57 +0000

    feat(dtfs2-7693): add jsdocs

--- trade-finance-manager-ui/server/helpers/team-checking.helper.ts
+++ trade-finance-manager-ui/server/helpers/team-checking.helper.ts
@@ -1,5 +1,8 @@
 import { ALL_TEAM_IDS, TeamId } from '@ukef/dtfs2-common';
 
+/**
+ * Type containing the team ID, URL for use in the team checking view model
+ */
 export type TeamToCheck = {
   teams: TeamId[];
   url: string;
@@ -7,9 +10,8 @@ export type TeamToCheck = {
 };
 
 /**
- * Generates an array of team combinations for validation purposes.
+ * Generates an array of team combinations for rendering the team checking view model.
  * Each combination consists of a single team ID, its corresponding URL, and description.
+ * @link TeamCheckingViewModel
  */
-export const getTeamCombinations = (): TeamToCheck[] =>
-  // We add these to the start of the array for easier navigation
-  ALL_TEAM_IDS.map((teamId) => ({ teams: [teamId], url: teamId, description: teamId }));
+export const getTeamCombinations = (): TeamToCheck[] => ALL_TEAM_IDS.map((teamId) => ({ teams: [teamId], url: teamId, description: teamId }));
