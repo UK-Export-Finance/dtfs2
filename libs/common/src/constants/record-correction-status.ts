a6c63d71d2cdca01bf434798ed3135559189863e
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2025-01-28 15:18:01 +0000

    feat(FN-3796): code markups

--- libs/common/src/constants/record-correction-status.ts
+++ libs/common/src/constants/record-correction-status.ts
@@ -1,13 +1,9 @@
-export type RecordCorrectionDisplayStatus = 'Record correction sent' | 'Record correction received';
-
-export type RecordCorrectionStatus = 'RECORD_CORRECTION_SENT' | 'RECORD_CORRECTION_RECEIVED';
-
-export const RECORD_CORRECTION_DISPLAY_STATUS: { SENT: RecordCorrectionDisplayStatus; RECEIVED: RecordCorrectionDisplayStatus } = {
+export const RECORD_CORRECTION_DISPLAY_STATUS = {
   SENT: 'Record correction sent',
   RECEIVED: 'Record correction received',
-};
+} as const;
 
-export const RECORD_CORRECTION_STATUS: { SENT: RecordCorrectionStatus; RECEIVED: RecordCorrectionStatus } = {
+export const RECORD_CORRECTION_STATUS = {
   SENT: 'RECORD_CORRECTION_SENT',
   RECEIVED: 'RECORD_CORRECTION_RECEIVED',
-};
+} as const;
