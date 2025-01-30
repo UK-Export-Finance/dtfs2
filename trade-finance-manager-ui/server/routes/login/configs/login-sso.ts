395bf8a0cfef8752dcfe0a151deee4bd8f9c1927
Alex Bramhill <alex.bramhill@softwire.com>
2025-01-30 18:21:01 +0000

    feat(dtfs2-6892): update tests

--- trade-finance-manager-ui/server/routes/login/configs/login-sso.ts
+++ trade-finance-manager-ui/server/routes/login/configs/login-sso.ts
@@ -1,17 +1,12 @@
 import express from 'express';
 import { LoginController } from '../../../controllers/login/login-sso/login.controller';
 import { GetRouter } from '../../../types/get-router';
-import { LoginService } from '../../../services/login.service';
-import { UserSessionService } from '../../../services/user-session.service';
 
 export const getLoginSsoRouter: GetRouter = () => {
-  const loginService = new LoginService();
-  const userSessionService = new UserSessionService();
-  const loginController = new LoginController({ loginService, userSessionService });
   const loginSsoRouter = express.Router();
 
   loginSsoRouter.get('/', (req, res, next) => {
-    loginController.getLogin(req, res).catch(next);
+    LoginController.getLogin(req, res).catch(next);
   });
 
   loginSsoRouter.get('/logout', loginController.getLogout);
--- trade-finance-manager-ui/server/services/user-session.service.ts
+++ trade-finance-manager-ui/server/services/user-session.service.ts
@@ -4,29 +4,37 @@ import { TfmSessionUser } from '@ukef/dtfs2-common';
 import { Session, SessionData } from 'express-session';
 
 export class UserSessionService {
-  private static deleteExistingPartiallyLoggedInSession(session: Session & Partial<SessionData>) {
+  private static deleteExistingPartiallyLoggedInSession = (session: Session & Partial<SessionData>) => {
     delete session.loginData;
-  }
+  };
 
-  private static deleteExistingLoggedInSession(session: Session & Partial<SessionData>) {
+  private static deleteExistingLoggedInSession = (session: Session & Partial<SessionData>) => {
     delete session.user;
     delete session.userToken;
-  }
+  };
 
-  public static createPartiallyLoggedInSession({
+  public static createPartiallyLoggedInSession = ({
     session,
     authCodeUrlRequest,
   }: {
     session: Session & Partial<SessionData>;
     authCodeUrlRequest: AuthorizationUrlRequest;
-  }) {
+  }) => {
     UserSessionService.deleteExistingLoggedInSession(session);
     session.loginData = { authCodeUrlRequest };
-  }
+  };
 
-  public static createLoggedInSession({ session, user, userToken }: { session: Session & Partial<SessionData>; user: TfmSessionUser; userToken: string }) {
+  public static createLoggedInSession = ({
+    session,
+    user,
+    userToken,
+  }: {
+    session: Session & Partial<SessionData>;
+    user: TfmSessionUser;
+    userToken: string;
+  }) => {
     UserSessionService.deleteExistingPartiallyLoggedInSession(session);
     session.user = user;
     session.userToken = userToken;
-  }
+  };
 }
