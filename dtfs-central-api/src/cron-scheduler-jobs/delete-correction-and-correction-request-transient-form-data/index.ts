adff34668d50e8f4061df3f26b4b639a276aeebe
Matt <173997080+sw-mattw@users.noreply.github.com>
2025-01-31 14:01:27 +0000

    feat(FN-3737): clear old record correction transient form data (portal)

---
+++ dtfs-central-api/src/cron-scheduler-jobs/delete-correction-and-correction-request-transient-form-data/index.ts
@@ -0,0 +1,30 @@
+import { asString, CronSchedulerJob } from '@ukef/dtfs2-common';
+import { FeeRecordCorrectionRequestTransientFormDataRepo } from '../../repositories/fee-record-correction-request-transient-form-data-repo';
+import { FeeRecordCorrectionTransientFormDataRepo } from '../../repositories/fee-record-correction-transient-form-data-repo';
+
+const { RECORD_CORRECTION_TRANSIENT_FORM_DATA_DELETE_SCHEDULE } = process.env;
+
+/**
+ * Deletes record correction and correction request transient form data more than 1 day old.
+ */
+export const deleteCorrectionAndCorrectionRequestTransientFormData = async (): Promise<void> => {
+  console.info('Deleting old transient record corrections and correction requests - deleteCorrectionRequestTransientFormData CRON job');
+
+  try {
+    await FeeRecordCorrectionTransientFormDataRepo.deleteByLastUpdatedOlderThanOneDayAgo();
+  } catch (error) {
+    console.error('Error deleting old transient record correction form data - deleteCorrectionRequestTransientFormData CRON job: %o', error);
+  }
+
+  try {
+    await FeeRecordCorrectionRequestTransientFormDataRepo.deleteByLastUpdatedOlderThanOneDayAgo();
+  } catch (error) {
+    console.error('Error deleting old transient record correction request form data - deleteCorrectionRequestTransientFormData CRON job: %o', error);
+  }
+};
+
+export const deleteCorrectionAndCorrectionRequestTransientFormDataJob: CronSchedulerJob = {
+  cronExpression: asString(RECORD_CORRECTION_TRANSIENT_FORM_DATA_DELETE_SCHEDULE, 'RECORD_CORRECTION_TRANSIENT_FORM_DATA_DELETE_SCHEDULE'),
+  description: 'Deletes record correction and record correction request transient form data older than 1 day',
+  task: deleteCorrectionAndCorrectionRequestTransientFormData,
+};
