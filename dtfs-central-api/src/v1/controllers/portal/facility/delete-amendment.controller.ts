c416b1bb1703924ec02692991e83575bc50ba8be
rkontogianni <rkontogianni@ukexportfinance.gov.uk>
2025-02-04 15:29:10 +0000

    feat(DTFS2-7730): delete amendment endpoint dtfs central api (#4179)
    Co-authored-by: Abhi Markan <32125108+abhi-markan@users.noreply.github.com>

---
+++ dtfs-central-api/src/v1/controllers/portal/facility/delete-amendment.controller.ts
@@ -0,0 +1,47 @@
+import { ApiError, AUDIT_USER_TYPES, CustomExpressRequest } from '@ukef/dtfs2-common';
+import { HttpStatusCode } from 'axios';
+import { ObjectId } from 'mongodb';
+import { Response } from 'express';
+import { validateAuditDetailsAndUserType } from '@ukef/dtfs2-common/change-stream';
+import { TfmFacilitiesRepo } from '../../../../repositories/tfm-facilities-repo';
+import { DeletePortalFacilityAmendmentPayload } from '../../../routes/middleware/payload-validation/validate-delete-portal-facility-amendment-payload';
+
+type DeletePortalAmendmentRequestParams = { facilityId: string; amendmentId: string };
+export type DeletePortalAmendmentRequest = CustomExpressRequest<{ params: DeletePortalAmendmentRequestParams; reqBody: DeletePortalFacilityAmendmentPayload }>;
+
+/**
+ * delete portal facility amendment
+ * @param req - request
+ * @param res - response
+ */
+export const deletePortalAmendment = async (req: DeletePortalAmendmentRequest, res: Response) => {
+  const { facilityId, amendmentId } = req.params;
+  const { auditDetails } = req.body;
+  const facilityMongoId = new ObjectId(facilityId);
+  const amendmentMongoId = new ObjectId(amendmentId);
+
+  try {
+    validateAuditDetailsAndUserType(auditDetails, AUDIT_USER_TYPES.PORTAL);
+
+    await TfmFacilitiesRepo.deletePortalFacilityAmendment({
+      facilityId: facilityMongoId,
+      amendmentId: amendmentMongoId,
+      auditDetails,
+    });
+
+    return res.sendStatus(HttpStatusCode.NoContent);
+  } catch (error) {
+    if (error instanceof ApiError) {
+      const { status, message, code } = error;
+      console.error('Error deleting amendment: %s', message);
+      return res.status(status).send({ status, message, code });
+    }
+
+    console.error('Error deleting amendment on facilityId %s: %o', facilityId, error);
+
+    return res.status(HttpStatusCode.InternalServerError).send({
+      status: HttpStatusCode.InternalServerError,
+      message: 'Unknown error occurred when deleting portal amendment',
+    });
+  }
+};
