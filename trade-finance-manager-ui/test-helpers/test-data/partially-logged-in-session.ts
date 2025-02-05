ac184ab7904ae1245d9914b27b1877be109d74b2
Alex Bramhill <alex.bramhill@softwire.com>
2025-02-05 17:54:45 +0000

    feat(dtfs2-6892): review comments

--- trade-finance-manager-ui/test-helpers/test-data/partially-logged-in-session.ts
+++ trade-finance-manager-ui/test-helpers/test-data/partially-logged-in-session.ts
@@ -1,6 +1,9 @@
 import { anAuthorisationUrlRequest } from '@ukef/dtfs2-common';
 import { PartiallyLoggedInUserSessionData } from '../../server/types/express-session';
 
+/**
+ * Returns a new instance of a partially logged in user session for use in tests
+ */
 export const aPartiallyLoggedInUserSession = (): PartiallyLoggedInUserSessionData => ({
   loginData: {
     authCodeUrlRequest: anAuthorisationUrlRequest(),
