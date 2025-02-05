7fe84e6b3229c2a298a7f22d8f03900a36858480
Mark Roberts <mroberts@ukexportfinance.gov.uk>
2025-02-05 11:02:53 +0000

    feat(DTFS2-7795): add get CYA endpoint

---
+++ gef-ui/server/controllers/amendments/check-your-answers/get-check-your-answers.ts
@@ -0,0 +1,56 @@
+import { CustomExpressRequest } from '@ukef/dtfs2-common';
+import { Response } from 'express';
+import * as api from '../../../services/api';
+import { asLoggedInUserSession } from '../../../utils/express-session';
+import { userCanAmendFacility } from '../../../utils/facility-amendments.helper';
+import { getAmendmentsUrl, getPreviousPage } from '../helpers/navigation.helper';
+import { PORTAL_AMENDMENT_PAGES } from '../../../constants/amendments';
+
+export type GetCheckYourAnswersRequest = CustomExpressRequest<{
+  params: { dealId: string; facilityId: string; amendmentId: string };
+}>;
+
+/**
+ * Controller to get the `Check your answers` page
+ * @param req - the request object
+ * @param res - the response object
+ */
+export const getCheckYourAnswers = async (req: GetCheckYourAnswersRequest, res: Response) => {
+  try {
+    const { dealId, facilityId, amendmentId } = req.params;
+    const { userToken, user } = asLoggedInUserSession(req.session);
+
+    const deal = await api.getApplication({ dealId, userToken });
+    const { details: facility } = await api.getFacility({ facilityId, userToken });
+
+    if (!deal || !facility) {
+      console.error('Deal %s or Facility %s was not found', dealId, facilityId);
+      return res.redirect('/not-found');
+    }
+
+    if (!userCanAmendFacility(facility, deal, user.roles)) {
+      console.error('User cannot amend facility %s on deal %s', facilityId, dealId);
+      return res.redirect(`/gef/application-details/${dealId}`);
+    }
+
+    const amendment = await api.getAmendment({ facilityId, amendmentId, userToken });
+
+    if (!amendment) {
+      console.error('Amendment %s was not found on facility %s', amendmentId, facilityId);
+      return res.redirect('/not-found');
+    }
+
+    const viewModel = {
+      exporterName: deal.exporter.companyName,
+      facilityType: facility.type,
+      cancelUrl: getAmendmentsUrl({ dealId, facilityId, amendmentId, page: PORTAL_AMENDMENT_PAGES.CANCEL }),
+      previousPage: getPreviousPage(PORTAL_AMENDMENT_PAGES.CHECK_YOUR_ANSWERS, amendment),
+      amendment,
+    };
+
+    return res.render('partials/amendments/check-your-answers.njk', viewModel);
+  } catch (error) {
+    console.error('Error getting amendments check your answers page %o', error);
+    return res.render('partials/problem-with-service.njk');
+  }
+};
