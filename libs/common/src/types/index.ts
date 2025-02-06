c068bbfd3bd7ae21911bdc42b2d9ac223b71932b
Ruby Lavender <ruby.lavender@softwire.com>
2025-02-06 14:13:53 +0000

    feat(FN-3813): remove component tests for adjustment columns

--- libs/common/src/types/index.ts
+++ libs/common/src/types/index.ts
@@ -15,7 +15,6 @@ export * from './audit-user-types';
 export * from './selected-fee-records-details';
 export * from './express-custom-request';
 export * from './keying-sheet-row-status';
-export * from './keying-sheet-adjustment';
 export * from './portal-session-data';
 export * from './amendment-status';
 export * from './api-error-response-body';
--- trade-finance-manager-ui/component-tests/utilisation-reports/_macros/keying-sheet-table-row.component-test.ts
+++ trade-finance-manager-ui/component-tests/utilisation-reports/_macros/keying-sheet-table-row.component-test.ts
@@ -21,23 +21,12 @@ describe(component, () => {
         formattedDateReceived: 'Some date',
       },
     ],
-    fixedFeeAdjustment: {
-      amount: '100',
-      change: 'INCREASE',
-    },
-    principalBalanceAdjustment: {
-      amount: '100',
-      change: 'INCREASE',
-    },
     checkboxId: 'feeRecordId-1-status-TO_DO',
     isChecked: false,
   });
 
   const getWrapper = ({ keyingSheetRow, userCanEdit }: { keyingSheetRow?: KeyingSheetTableRow; userCanEdit?: boolean } = {}) =>
     render({ keyingSheetRow: keyingSheetRow ?? aKeyingSheetTableRow(), userCanEdit: userCanEdit ?? true });
-
-  const getPrincipalBalanceAdjustmentSelector = (change: 'increase' | 'decrease') => `td[data-cy="principal-balance-adjustment--${change}"]`;
-  const getFixedFeeAdjustmentSelector = (change: 'increase' | 'decrease') => `td[data-cy="fixed-fee-adjustment--${change}"]`;
 
   it('renders the keying sheet status, facility id, exporter, base currency and fee payment in the table row when keying sheet row has fee payments', () => {
     const keyingSheetRow: KeyingSheetTableRow = {
@@ -108,121 +97,6 @@ describe(component, () => {
     wrapper.expectElement('tr td:contains("Some date")').toExist();
   });
 
-  it('renders the keying sheet adjustment increase and decrease columns', () => {
-    const wrapper = getWrapper();
-
-    wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('increase')}`).toExist();
-    wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('decrease')}`).toExist();
-    wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('increase')}`).toExist();
-    wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('decrease')}`).toExist();
-  });
-
-  it("renders the '-' in all the increase and decrease adjustment cells when the adjustment change is 'NONE'", () => {
-    const change = 'NONE';
-    const keyingSheetRow: KeyingSheetTableRow = {
-      ...aKeyingSheetTableRow(),
-      fixedFeeAdjustment: { change, amount: '111.11' },
-      principalBalanceAdjustment: { change, amount: '222.22' },
-    };
-    const wrapper = getWrapper({ keyingSheetRow });
-
-    wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('increase')}:contains("-")`).toExist();
-    wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('decrease')}:contains("-")`).toExist();
-    wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('increase')}:contains("-")`).toExist();
-    wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('decrease')}:contains("-")`).toExist();
-
-    wrapper.expectElement('tr td:contains("111.11")').notToExist();
-    wrapper.expectElement('tr td:contains("222.22")').notToExist();
-  });
-
-  describe('when the keying sheet adjustment value change field is set to INCREASE', () => {
-    const change = 'INCREASE';
-
-    it('renders the fixed fee adjustment amount in the increase column with the numeric cell class and does not use the text align right class', () => {
-      const keyingSheetRow: KeyingSheetTableRow = {
-        ...aKeyingSheetTableRow(),
-        fixedFeeAdjustment: { change, amount: '111.11' },
-      };
-      const wrapper = getWrapper({ keyingSheetRow });
-
-      wrapper.expectElement(`${getFixedFeeAdjustmentSelector('increase')}:contains("111.11")`).toExist();
-      wrapper.expectElement(`${getFixedFeeAdjustmentSelector('increase')}:contains("111.11")`).hasClass('govuk-table__cell--numeric');
-      wrapper.expectElement(`${getFixedFeeAdjustmentSelector('increase')}:contains("111.11")`).doesNotHaveClass('govuk-!-text-align-right');
-    });
-
-    it('renders the principal balance adjustment in the increase column with the numeric cell class and does not use the text align right class', () => {
-      const keyingSheetRow: KeyingSheetTableRow = {
-        ...aKeyingSheetTableRow(),
-        principalBalanceAdjustment: { change, amount: '333.33' },
-      };
-      const wrapper = getWrapper({ keyingSheetRow });
-
-      wrapper.expectElement(`${getPrincipalBalanceAdjustmentSelector('increase')}:contains("333.33")`).toExist();
-      wrapper.expectElement(`${getPrincipalBalanceAdjustmentSelector('increase')}:contains("333.33")`).hasClass('govuk-table__cell--numeric');
-      wrapper.expectElement(`${getPrincipalBalanceAdjustmentSelector('increase')}:contains("333.33")`).doesNotHaveClass('govuk-!-text-align-right');
-    });
-
-    it("sets all the decrease columns to the '-' character with the text align right class and does not use the numeric cell class", () => {
-      const keyingSheetRow: KeyingSheetTableRow = {
-        ...aKeyingSheetTableRow(),
-        fixedFeeAdjustment: { change, amount: '111.11' },
-        principalBalanceAdjustment: { change, amount: '333.33' },
-      };
-      const wrapper = getWrapper({ keyingSheetRow });
-
-      wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('decrease')}:contains("-")`).toExist();
-      wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('decrease')}:contains("-")`).hasClass('govuk-!-text-align-right');
-      wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('decrease')}:contains("-")`).doesNotHaveClass('govuk-table__cell--numeric');
-      wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('decrease')}:contains("-")`).toExist();
-      wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('decrease')}:contains("-")`).hasClass('govuk-!-text-align-right');
-      wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('decrease')}:contains("-")`).doesNotHaveClass('govuk-table__cell--numeric');
-    });
-  });
-
-  describe('when the keying sheet adjustment value change field is set to DECREASE', () => {
-    const change = 'DECREASE';
-
-    it('renders the fixed fee adjustment amount in the decrease column with the numeric cell class and does not use the text align right class', () => {
-      const keyingSheetRow: KeyingSheetTableRow = {
-        ...aKeyingSheetTableRow(),
-        fixedFeeAdjustment: { change, amount: '111.11' },
-      };
-      const wrapper = getWrapper({ keyingSheetRow });
-
-      wrapper.expectElement(`${getFixedFeeAdjustmentSelector('decrease')}:contains("111.11")`).toExist();
-      wrapper.expectElement(`${getFixedFeeAdjustmentSelector('decrease')}:contains("111.11")`).hasClass('govuk-table__cell--numeric');
-      wrapper.expectElement(`${getFixedFeeAdjustmentSelector('decrease')}:contains("111.11")`).doesNotHaveClass('govuk-!-text-align-right');
-    });
-
-    it('renders the principal balance adjustment in the decrease column with the numeric cell class and does not use the text align right class', () => {
-      const keyingSheetRow: KeyingSheetTableRow = {
-        ...aKeyingSheetTableRow(),
-        principalBalanceAdjustment: { change, amount: '333.33' },
-      };
-      const wrapper = getWrapper({ keyingSheetRow });
-
-      wrapper.expectElement(`${getPrincipalBalanceAdjustmentSelector('decrease')}:contains("333.33")`).toExist();
-      wrapper.expectElement(`${getPrincipalBalanceAdjustmentSelector('decrease')}:contains("333.33")`).hasClass('govuk-table__cell--numeric');
-      wrapper.expectElement(`${getPrincipalBalanceAdjustmentSelector('decrease')}:contains("333.33")`).doesNotHaveClass('govuk-!-text-align-right');
-    });
-
-    it("sets all the increase columns to the '-' character with the text align right class and does not use the numeric cell class", () => {
-      const keyingSheetRow: KeyingSheetTableRow = {
-        ...aKeyingSheetTableRow(),
-        fixedFeeAdjustment: { change, amount: '111.11' },
-        principalBalanceAdjustment: { change, amount: '333.33' },
-      };
-      const wrapper = getWrapper({ keyingSheetRow });
-
-      wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('increase')}:contains("-")`).toExist();
-      wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('increase')}:contains("-")`).hasClass('govuk-!-text-align-right');
-      wrapper.expectElement(`tr ${getFixedFeeAdjustmentSelector('increase')}:contains("-")`).doesNotHaveClass('govuk-table__cell--numeric');
-      wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('increase')}:contains("-")`).toExist();
-      wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('increase')}:contains("-")`).hasClass('govuk-!-text-align-right');
-      wrapper.expectElement(`tr ${getPrincipalBalanceAdjustmentSelector('increase')}:contains("-")`).doesNotHaveClass('govuk-table__cell--numeric');
-    });
-  });
-
   describe('when userCanEdit is set to true', () => {
     const userCanEdit = true;
 
@@ -276,7 +150,7 @@ describe(component, () => {
       wrapper.expectElement('tr').toHaveCount(3);
     });
 
-    it('renders the none-fee payment keying sheet data and checkbox only in the first row', () => {
+    it('renders the non-fee payment keying sheet data and checkbox only in the first row', () => {
       const keyingSheetRow: KeyingSheetTableRow = {
         ...aKeyingSheetTableRow(),
         facilityId: '12345678',
@@ -285,8 +159,6 @@ describe(component, () => {
         displayStatus: 'To do',
         baseCurrency: 'EUR',
         checkboxId: 'feeRecordId-123-status-TO_DO',
-        fixedFeeAdjustment: { change: 'NONE', amount: '0' },
-        principalBalanceAdjustment: { change: 'NONE', amount: '0' },
         feePayments: [aFeePayment(), aFeePayment(), aFeePayment()],
       };
       const wrapper = getWrapper({ keyingSheetRow });
