1ef4091953b69b8ecf8a9ab58ca0fec854d87f7a
Alex Bramhill <alex.bramhill@softwire.com>
2025-02-06 11:49:36 +0000

    feat(dtfs2-6892): review comments

--- libs/common/src/schemas/tfm/tfm-session-user.schema.ts
+++ libs/common/src/schemas/tfm/tfm-session-user.schema.ts
@@ -10,4 +10,4 @@ export const TFM_SESSION_USER_SCHEMA = TFM_USER_SCHEMA.pick({
   lastName: true,
   status: true,
   lastLogin: true,
-}).extend({ _id: OBJECT_ID_STRING }); // _id is extended as a string as apposed to objectId due to existing implimentation of TFM session user
+}).extend({ _id: OBJECT_ID_STRING }); // _id is extended as a string as opposed to objectId due to existing implementation of TFM session user
