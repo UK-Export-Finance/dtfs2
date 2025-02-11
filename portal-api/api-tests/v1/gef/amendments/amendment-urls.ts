692176c7a163db78e4068cce572ffa1ccb25a25b
MarRobSoftwire <108682750+MarRobSoftwire@users.noreply.github.com>
2025-02-11 10:29:11 +0000

    feat(DTFS2-7794): add update amendment status endpoint to portal api (#4210)

--- portal-api/api-tests/v1/gef/amendments/amendment-urls.ts
+++ portal-api/api-tests/v1/gef/amendments/amendment-urls.ts
@@ -5,3 +5,6 @@ export const patchAmendmentUrl = ({ facilityId, amendmentId }: { facilityId: str
   `/v1/gef/facilities/${facilityId}/amendments/${amendmentId}`;
 
 export const putAmendmentUrl = ({ facilityId }: { facilityId: string }) => `/v1/gef/facilities/${facilityId}/amendments`;
+
+export const patchAmendmentStatusUrl = ({ facilityId, amendmentId }: { facilityId: string; amendmentId: string }) =>
+  `/v1/gef/facilities/${facilityId}/amendments/${amendmentId}/status`;
