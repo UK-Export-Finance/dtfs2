a15dbe3f1fd8898e117b2bcfea50d4dc444d4a88
Beth Thomas <beth.thomas@softwire.com>
2025-02-12 11:34:12 +0000

    feat(DTFS2-7494): add ui logic to show or hide make a change button

--- gef-ui/server/controllers/application-details/index.js
+++ gef-ui/server/controllers/application-details/index.js
@@ -1,5 +1,5 @@
 const startCase = require('lodash/startCase');
-const { DEAL_TYPE, timeZoneConfig, DEAL_STATUS } = require('@ukef/dtfs2-common');
+const { DEAL_TYPE, timeZoneConfig, DEAL_STATUS, PORTAL_AMENDMENT_UNDERWAY_STATUSES } = require('@ukef/dtfs2-common');
 const api = require('../../services/api');
 const { canUpdateUnissuedFacilitiesCheck } = require('./canUpdateUnissuedFacilitiesCheck');
 const {
@@ -264,7 +264,10 @@ const applicationDetails = async (req, res, next) => {
       params.link += '/unissued-facilities';
     }
 
-    params.canIssuedFacilitiesBeAmended = canUserAmendIssuedFacilities(application.submissionType, application.status, userRoles);
+    const amendmentsUnderwayOnDeal = await api.getAmendmentsOnDeal({ dealId, statuses: PORTAL_AMENDMENT_UNDERWAY_STATUSES, userToken });
+
+    params.canIssuedFacilitiesBeAmended =
+      canUserAmendIssuedFacilities(application.submissionType, application.status, userRoles) && !amendmentsUnderwayOnDeal.length;
 
     return res.render(`partials/${partial}.njk`, params);
   } catch (error) {
