41a5dc7d6ea7e6b11c884cb0812f1956d96b15dc
Alex Bramhill <alex.bramhill@softwire.com>
2025-02-13 12:00:32 +0000

    Merge branch 'feat/DTFS2-6892/add-schema-tests-for-commonised-tfm-user' into feat/DTFS2-6892/add-token-issuance

--- trade-finance-manager-api/src/v1/__mocks__/builders/user.service.mock.builder.ts
+++ trade-finance-manager-api/src/v1/__mocks__/builders/user.service.mock.builder.ts
@@ -1,21 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { BaseMockBuilder } from '@ukef/dtfs2-common';
 import { aTfmUser } from '@ukef/dtfs2-common/mock-data-backend';
-import {
-  UpsertTfmUserFromEntraIdUserParams,
-  UpsertTfmUserFromEntraIdUserResponse,
-  saveUserLoginInformationParams,
-  UserService,
-} from '../../services/user.service';
+import { UpsertTfmUserFromEntraIdUserResponse, UserService } from '../../services/user.service';
 
 export class UserServiceMockBuilder extends BaseMockBuilder<UserService> {
   constructor() {
     super({
       defaultInstance: {
-        upsertTfmUserFromEntraIdUser({ entraIdUser, auditDetails }: UpsertTfmUserFromEntraIdUserParams): Promise<UpsertTfmUserFromEntraIdUserResponse> {
+        upsertTfmUserFromEntraIdUser(): Promise<UpsertTfmUserFromEntraIdUserResponse> {
           return Promise.resolve(aTfmUser());
         },
-        saveUserLoginInformation({ userId, sessionIdentifier, auditDetails }: saveUserLoginInformationParams): Promise<void> {
+        saveUserLoginInformation(): Promise<void> {
           return Promise.resolve();
         },
       },
