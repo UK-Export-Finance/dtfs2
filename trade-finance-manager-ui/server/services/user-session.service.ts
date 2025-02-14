45f8b7a26adcbfb15aa3263bd125b04619683a99
Alex Bramhill <alex.bramhill@softwire.com>
2025-02-14 16:07:17 +0000

    feat(dtfs2-6892): review comments

--- trade-finance-manager-ui/server/services/user-session.service.ts
+++ trade-finance-manager-ui/server/services/user-session.service.ts
@@ -4,15 +4,28 @@ import { TfmSessionUser } from '@ukef/dtfs2-common';
 import { Session, SessionData } from 'express-session';
 
 export class UserSessionService {
+  /**
+   * Deletes the partially logged in session data from a session object.
+   * @param session The session object to remove partially logged in data from.
+   */
   private static deleteExistingPartiallyLoggedInSession = (session: Session & Partial<SessionData>) => {
     delete session.loginData;
   };
 
+  /**
+   * Deletes the existing logged in session data from a session object.
+   * @param session The session object to remove existing logged in data from.
+   */
   private static deleteExistingLoggedInSession = (session: Session & Partial<SessionData>) => {
     delete session.user;
     delete session.userToken;
   };
 
+  /**
+   * Creates a partially logged-in session.
+   * @param session The session object to initialize.
+   * @param authCodeUrlRequest The authorization code URL request.
+   */
   public static createPartiallyLoggedInSession = ({
     session,
     authCodeUrlRequest,
@@ -24,6 +37,12 @@ export class UserSessionService {
     session.loginData = { authCodeUrlRequest };
   };
 
+  /**
+   * creates a logged in session.
+   * @param session The existing session object.
+   * @param user The user object.
+   * @param userToken The user token.
+   */
   public static createLoggedInSession = ({
     session,
     user,
