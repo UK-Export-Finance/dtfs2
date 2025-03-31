97f3e8fb9b9d3beb12ac23efceca0dbc3584ee7f
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2025-03-31 18:22:04 +0100

    feat(DTFS2-7721): added documentation

--- e2e-tests/portal/cypress/support/utils/assertPrintDialogue.js
+++ e2e-tests/portal/cypress/support/utils/assertPrintDialogue.js
@@ -1,3 +1,8 @@
+/**
+ * function to assert print dialogue is rendered
+ * when print button is clicked
+ * @param {Function} printButton - cypress selector for print button to press
+ */
 export const assertPrintDialogue = (printButton) => {
   cy.window().then((win) => {
     cy.stub(win, 'print').as('printDialog');
