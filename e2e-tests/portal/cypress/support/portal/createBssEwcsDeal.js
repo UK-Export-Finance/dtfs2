ea919d8499eb60bddf3c9d0e024bfa321bd91ec8
Vamshidhar Reddy <vreddy@ukexportfinance.gov.uk>
2025-04-02 10:10:04 +0100

    chore(DTFS2-7431): seperated functions from createbssewcs function

--- e2e-tests/portal/cypress/support/portal/createBssEwcsDeal.js
+++ e2e-tests/portal/cypress/support/portal/createBssEwcsDeal.js
@@ -1,75 +1,6 @@
-const { contract, contractAboutSupplier, contractAboutBuyer, contractAboutFinancial } = require('../../e2e/pages');
-const { submissionDetails } = require('../../fixtures/deal');
 const MOCK_USERS = require('../../../../e2e-fixtures');
 
 const { BANK1_MAKER1 } = MOCK_USERS;
-
-/**
- * Fills in the supplier details form in the portal.
- *
- * @param {string} exporterCompanyName - The name of the exporter company. If provided, the form will be filled with this name and related details. If not provided, default submission details will be used.
- */
-const fillSupplierDetails = (exporterCompanyName) => {
-  contract.aboutSupplierDetailsLink().click();
-
-  if (exporterCompanyName) {
-    contractAboutSupplier.supplierType().select('Exporter');
-    cy.keyboardInput(contractAboutSupplier.supplierName(), exporterCompanyName);
-    contractAboutSupplier.supplierAddress().country().select('United Kingdom');
-    cy.keyboardInput(contractAboutSupplier.supplierAddress().line1(), 'Test');
-    cy.keyboardInput(contractAboutSupplier.supplierPostCode(), 'AB1 2CD');
-    contractAboutSupplier.supplierCorrespondenceAddressSame().click();
-    contractAboutSupplier.industrySector().select('Accommodation and food service activities');
-    contractAboutSupplier.industryClass().select('Event catering activities');
-  } else {
-    contractAboutSupplier.supplierType().select(submissionDetails['supplier-type']);
-    cy.keyboardInput(contractAboutSupplier.supplierCompaniesHouseRegistrationNumber(), '12345678');
-    contractAboutSupplier.supplierSearchCompaniesHouse().click();
-    contractAboutSupplier.supplierAddress().country().select('United Kingdom');
-    contractAboutSupplier.supplierCorrespondenceAddressSame().click();
-  }
-  contractAboutSupplier.smeTypeSmall().click();
-  cy.keyboardInput(contractAboutSupplier.supplyContractDescription(), 'Supply Contract Description');
-  contractAboutSupplier.notLegallyDistinct().click();
-  contractAboutSupplier.nextPage().click();
-};
-
-/**
- * Fills in the buyer details form in the portal.
- *
- * This function inputs the buyer's name, address, and destination of goods and services,
- * then navigates to the next page.
- *
- * The following fields are filled:
- * - Buyer Name
- * - Buyer Address (Country, Line 1, Line 2, Line 3, Town, Postcode)
- * - Destination of Goods and Services
- *
- * The function uses Cypress commands to interact with the form elements.
- */
-const fillBuyerDetails = () => {
-  cy.keyboardInput(contractAboutBuyer.buyerName(), 'Buyer Name');
-  contractAboutBuyer.buyerAddress().country().select('United Kingdom');
-  cy.keyboardInput(contractAboutBuyer.buyerAddress().line1(), 'Line 1');
-  cy.keyboardInput(contractAboutBuyer.buyerAddress().line2(), 'Line 2');
-  cy.keyboardInput(contractAboutBuyer.buyerAddress().line3(), 'Line 3');
-  cy.keyboardInput(contractAboutBuyer.buyerAddress().town(), 'Town');
-  cy.keyboardInput(contractAboutBuyer.buyerAddress().postcode(), 'AB1 2CD');
-  contractAboutBuyer.destinationOfGoodsAndServices().select('United Kingdom');
-  contractAboutBuyer.nextPage().click();
-};
-
-/**
- * Fills in the financial details for a contract.
- *
- * This function sets the supply contract value to 12000, selects the currency as GBP,
- * and then saves the details and navigates back.
- */
-const fillFinancialDetails = () => {
-  cy.keyboardInput(contractAboutFinancial.supplyContractValue(), '12000');
-  contractAboutFinancial.supplyContractCurrency().select('GBP');
-  contractAboutFinancial.saveAndGoBack().click();
-};
 
 /**
  * Fills out the deal details form with the provided information.
@@ -80,9 +11,9 @@ const fillFinancialDetails = () => {
  * @param {string} params.exporterCompanyName - The name of the exporter company.
  */
 const fillOutDealDetails = ({ dealSubmissionType, facilityStage, exporterCompanyName }) => {
-  fillSupplierDetails(exporterCompanyName);
-  fillBuyerDetails();
-  fillFinancialDetails();
+  cy.completeAboutSupplierSection({ exporterCompanyName });
+  cy.completeAboutBuyerSection();
+  cy.completeAboutFinancialSection();
   cy.completeEligibilityCriteria(dealSubmissionType);
   cy.clickSaveGoBackButton();
 
@@ -115,4 +46,4 @@ const completeBssEwcsDealFields = ({ dealSubmissionType, facilityStage, exporter
   fillOutDealDetails({ dealSubmissionType, facilityStage, exporterCompanyName });
 };
 
-export { createBssEwcsDeal, completeBssEwcsDealFields, fillSupplierDetails, fillBuyerDetails, fillFinancialDetails };
+export { createBssEwcsDeal, completeBssEwcsDealFields };
