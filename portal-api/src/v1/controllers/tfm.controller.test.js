955e247d3b94798cae214290e229ac7ca45b6cd1
Abhi Markan <amarkan>
2025-04-24 09:52:38 +0100

    feat(DTFS2-7939): moved to controller and added test cases

--- portal-api/src/v1/controllers/tfm.controller.test.js
+++ portal-api/src/v1/controllers/tfm.controller.test.js
@@ -1,7 +1,8 @@
 import { HttpStatusCode } from 'axios';
 import { ROLES, ALL_TEAM_IDS } from '@ukef/dtfs2-common';
 import { tfmTeam } from './tfm.controller';
-import { mockReq, mockRes } from '../../../api-tests/v1/mocks';
+import { mockRes } from '../../../api-tests/v1/mocks';
+import * as api from '../api';
 
 const mockResponse = mockRes();
 const invalidTeamIds = [
@@ -19,27 +20,164 @@ const invalidTeamIds = [
   ROLES.READ_ONLY,
 ];
 
+jest.mock('../api');
 console.error = jest.fn();
 
 describe('tfmTeam', () => {
-  it.each(invalidTeamIds)('should throw an error when an invalid %s TFM id is provided', async (teamId) => {
-    // Arrange
-    const expectedResponse = {
-      status: HttpStatusCode.BadRequest,
-      data: `Invalid TFM team ID provide ${teamId}`,
-    };
-    const mockRequest = {
-      ...mockReq,
-      params: {
-        teamId,
-      },
-    };
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-    // Act
-    const response = await tfmTeam(mockRequest, mockResponse);
+  describe('Invalid TFM team ID', () => {
+    it.each(invalidTeamIds)(`Should return ${HttpStatusCode.BadRequest} when an invalid TFM team ID is provided`, async (teamId) => {
+      // Arrange
+      const mockRequest = {
+        params: {
+          teamId,
+        },
+      };
 
-    // Assert
-    expect(console.error).toHaveBeenCalledWith('Invalid TFM team ID %s provided', teamId);
-    expect(response).toStrictEqual(expectedResponse);
+      // Act
+      await tfmTeam(mockRequest, mockResponse);
+
+      // Assert
+      expect(console.error).toHaveBeenCalledWith('Invalid TFM team ID %s provided', teamId);
+      expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCode.BadRequest);
+      expect(mockResponse.send).toHaveBeenCalledWith(`Invalid TFM team ID provided ${teamId}`);
+    });
+  });
+
+  describe('Invalid response received', () => {
+    it('should throw an error when an empty object response is received from the API call', async () => {
+      // Arrange
+      const mockRequest = {
+        params: {
+          teamId: ALL_TEAM_IDS[0],
+        },
+      };
+      const error = new Error('Invalid response received');
+
+      api.getTfmTeam.mockResolvedValueOnce({});
+
+      // Act
+      await tfmTeam(mockRequest, mockResponse);
+
+      // Assert
+      expect(console.error).toHaveBeenCalledWith('Unable to get the TFM team with ID %s %o', mockRequest.params?.teamId, error);
+      expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCode.InternalServerError);
+      expect(mockResponse.send).toHaveBeenCalledWith('Unable to get the TFM team');
+    });
+
+    it('should throw an error when an undefined response is received from the API call', async () => {
+      // Arrange
+      const mockRequest = {
+        params: {
+          teamId: ALL_TEAM_IDS[0],
+        },
+      };
+      const error = new Error('Invalid response received');
+
+      api.getTfmTeam.mockResolvedValueOnce(undefined);
+
+      // Act
+      await tfmTeam(mockRequest, mockResponse);
+
+      // Assert
+      expect(console.error).toHaveBeenCalledWith('Unable to get the TFM team with ID %s %o', mockRequest.params?.teamId, error);
+      expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCode.InternalServerError);
+      expect(mockResponse.send).toHaveBeenCalledWith('Unable to get the TFM team');
+    });
+
+    it('should throw an error when a null response is received from the API call', async () => {
+      // Arrange
+      const mockRequest = {
+        params: {
+          teamId: ALL_TEAM_IDS[0],
+        },
+      };
+      const error = new Error('Invalid response received');
+
+      api.getTfmTeam.mockResolvedValueOnce(null);
+
+      // Act
+      await tfmTeam(mockRequest, mockResponse);
+
+      // Assert
+      expect(console.error).toHaveBeenCalledWith('Unable to get the TFM team with ID %s %o', mockRequest.params?.teamId, error);
+      expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCode.InternalServerError);
+      expect(mockResponse.send).toHaveBeenCalledWith('Unable to get the TFM team');
+    });
+
+    it('should throw an error when an empty data response object is received from the API call', async () => {
+      // Arrange
+      const mockRequest = {
+        params: {
+          teamId: ALL_TEAM_IDS[0],
+        },
+      };
+      const error = new Error('Invalid response received');
+
+      api.getTfmTeam.mockResolvedValueOnce({
+        data: {},
+      });
+
+      // Act
+      await tfmTeam(mockRequest, mockResponse);
+
+      // Assert
+      expect(console.error).toHaveBeenCalledWith('Unable to get the TFM team with ID %s %o', mockRequest.params?.teamId, error);
+    });
+  });
+
+  describe('Valid response received', () => {
+    it('should return the team data with a successful response', async () => {
+      // Arrange
+      const mockRequest = {
+        params: {
+          teamId: ALL_TEAM_IDS[0],
+        },
+      };
+      const team = {
+        id: ALL_TEAM_IDS[0],
+        name: ALL_TEAM_IDS[0],
+        email: 'checker2@ukexportfinance.gov.uk',
+      };
+
+      api.getTfmTeam.mockResolvedValueOnce({
+        data: {
+          team,
+        },
+      });
+
+      // Act
+      await tfmTeam(mockRequest, mockResponse);
+
+      // Assert
+      expect(console.error).not.toHaveBeenCalled();
+      expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCode.Ok);
+      expect(mockResponse.send).toHaveBeenCalledWith(team);
+    });
+  });
+
+  describe('An exception has occurred', () => {
+    it(`should thrown an ${HttpStatusCode.InternalServerError} if an exception has occurred`, async () => {
+      // Arrange
+      const mockRequest = {
+        params: {
+          teamId: ALL_TEAM_IDS[0],
+        },
+      };
+      const error = new Error('API error');
+
+      api.getTfmTeam.mockRejectedValue(error);
+
+      // Act
+      await tfmTeam(mockRequest, mockResponse);
+
+      // Assert
+      expect(console.error).toHaveBeenCalledWith('Unable to get the TFM team with ID %s %o', mockRequest.params?.teamId, error);
+      expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCode.InternalServerError);
+      expect(mockResponse.send).toHaveBeenCalledWith('Unable to get the TFM team');
+    });
   });
 });
