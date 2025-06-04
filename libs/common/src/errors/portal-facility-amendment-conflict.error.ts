76179e2793493d08dd03a474cb834497ef1113e0
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2025-06-04 11:17:48 +0100

    feat(DTFS2-7758): fix failing tests

--- libs/common/src/errors/portal-facility-amendment-conflict.error.ts
+++ libs/common/src/errors/portal-facility-amendment-conflict.error.ts
@@ -2,10 +2,10 @@ import { HttpStatusCode } from 'axios';
 import { ApiError } from './api.error';
 
 export class PortalFacilityAmendmentConflictError extends ApiError {
-  constructor(dealId?: string) {
-    const message = dealId
-      ? `There is already a portal facility amendment under way on deal ${dealId}`
-      : 'There is already a portal facility amendment under way on the deal';
+  constructor(facilityId?: string) {
+    const message = facilityId
+      ? `There is already a portal facility amendment under way on the facility ${facilityId}`
+      : 'There is already a portal facility amendment under way on the facility';
     super({ message, status: HttpStatusCode.Conflict });
 
     this.name = 'PortalFacilityAmendmentConflictError';
