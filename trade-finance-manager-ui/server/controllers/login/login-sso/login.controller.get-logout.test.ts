11070659683f48d36257e9ecb7da3517e7f2b845
Abhi Markan <amarkan>
2025-06-17 16:04:34 +0100

    feat(DTFS2-7130): fixed logout access in the absence of user session

--- trade-finance-manager-ui/server/controllers/login/login-sso/login.controller.get-logout.test.ts
+++ trade-finance-manager-ui/server/controllers/login/login-sso/login.controller.get-logout.test.ts
@@ -2,43 +2,89 @@ import httpMocks from 'node-mocks-http';
 import { Request, Response } from 'express';
 import { LoginController } from './login.controller';
 
-describe('controllers - login (sso)', () => {
-  let consoleInfoMock: jest.Mock;
+const { getLogout } = LoginController;
 
+describe('login.controller - logout', () => {
   describe('getLogout', () => {
     let res: httpMocks.MockResponse<Response>;
     let req: httpMocks.MockRequest<Request>;
 
     beforeAll(() => {
-      consoleInfoMock = jest.fn();
-      console.info = consoleInfoMock;
+      console.info = jest.fn();
     });
 
     beforeEach(() => {
-      ({ req, res } = httpMocks.createMocks({
-        session: { destroy: jest.fn((callback: () => void) => callback()) },
-      }));
+      jest.resetAllMocks();
     });
 
-    afterAll(() => {
-      consoleInfoMock.mockRestore();
+    describe('User is not logged-in', () => {
+      beforeEach(() => {
+        ({ req, res } = httpMocks.createMocks({
+          session: {
+            destroy: jest.fn(),
+          },
+        }));
+      });
+
+      it('should log user re-direction when user is not logged-in', () => {
+        // Act
+        getLogout(req, res);
+
+        // Assert
+        expect(console.info).toHaveBeenCalledWith('Re-directing user to TFM SSO login');
+        expect(console.info).toHaveBeenCalledTimes(1);
+      });
+
+      it('should redirect the user to /', () => {
+        // Act
+        getLogout(req, res);
+
+        // Assert
+        expect(res._getRedirectUrl()).toEqual('/');
+      });
     });
 
-    it('should render the user logged out page', () => {
-      // Act
-      LoginController.getLogout(req, res);
+    describe('User is logged-in', () => {
+      beforeEach(() => {
+        ({ req, res } = httpMocks.createMocks({
+          session: {
+            user: {
+              _id: '68517461b924dcd38b853260',
+            },
+            destroy: jest.fn((callback: () => void) => callback()),
+          },
+        }));
+      });
 
-      // Assert
-      expect(res._getRenderView()).toEqual('user-logged-out.njk');
-    });
+      afterAll(() => {
+        jest.restoreAllMocks();
+      });
 
-    it('should destroy the session', () => {
-      // Act
-      LoginController.getLogout(req, res);
+      it('should log user log-out action with user id', () => {
+        // Act
+        getLogout(req, res);
 
-      // Assert
-      // eslint-disable-next-line @typescript-eslint/unbound-method
-      expect(req.session.destroy).toHaveBeenCalledTimes(1);
+        // Assert
+        expect(console.info).toHaveBeenCalledWith('Logging out TFM user %s', req.session.user?._id);
+        expect(console.info).toHaveBeenCalledTimes(1);
+      });
+
+      it('should render the user logged out page', () => {
+        // Act
+        getLogout(req, res);
+
+        // Assert
+        expect(res._getRenderView()).toEqual('user-logged-out.njk');
+      });
+
+      it('should destroy the session', () => {
+        // Act
+        getLogout(req, res);
+
+        // Assert
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        expect(req.session.destroy).toHaveBeenCalledTimes(1);
+      });
     });
   });
 });
