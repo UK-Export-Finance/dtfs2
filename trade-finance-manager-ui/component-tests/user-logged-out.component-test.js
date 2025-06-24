f37f8c36ec284493c108fefed77b9063ac8c5ead
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2025-06-24 09:37:28 +0100

    feat(DTFS2-7130): non-delegated banks deal migration to TFM (#4346)
    Co-authored-by: Abhi Markan <amarkan>
    Co-authored-by: Abhi Markan <AMarkan@Mac.broadband>

--- trade-finance-manager-ui/component-tests/user-logged-out.component-test.js
+++ trade-finance-manager-ui/component-tests/user-logged-out.component-test.js
@@ -12,10 +12,14 @@ describe(page, () => {
   });
 
   it('should render user logged out heading', () => {
-    wrapper.expectText('[data-cy="you-have-been-logged-out-heading"]').toRead('You have been logged out');
+    wrapper.expectText('[data-cy="you-have-been-logged-out-heading"]').toRead('You have been logged out of Trade Finance Manager');
   });
 
-  it('should render user logged out message', () => {
-    wrapper.expectText('[data-cy="you-have-been-logged-out-message"]').toRead('You have been logged out of Trade Finance Manager.');
+  it('should render the primary navigation button', () => {
+    wrapper.expectElement('[data-cy="govuk-sso-login-button"]').toExist();
+  });
+
+  it('should validate primary navigation button link and text', () => {
+    wrapper.expectLink('[data-cy="govuk-sso-login-button"]').toLinkTo('/', 'Return to sign in');
   });
 });
