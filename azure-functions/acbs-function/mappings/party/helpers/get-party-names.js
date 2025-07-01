443c82dd8f7a5c7f0f0b9fc1df06bdb07f0d61d1
Abhi Markan <amarkan>
2025-07-01 20:02:26 +0100

    feat(DTFS2-8002): added dobule quotes removal from party names

--- azure-functions/acbs-function/mappings/party/helpers/get-party-names.js
+++ azure-functions/acbs-function/mappings/party/helpers/get-party-names.js
@@ -1,12 +1,14 @@
+const { stripDoubleQuotes } = require('../../../helpers/string');
+
 /* ACBS needs party names split into 3 fields:
   name1: chars 1-35
   name2: chars 36-70
   name3: chars 71-105
 */
 const getPartyNames = (partyName) => ({
-  name1: partyName ? partyName.substring(0, 35) : partyName,
-  name2: partyName ? partyName.substring(35, 70) : partyName,
-  name3: partyName ? partyName.substring(70, 105) : partyName,
+  name1: partyName ? stripDoubleQuotes(partyName.substring(0, 35)) : partyName,
+  name2: partyName ? stripDoubleQuotes(partyName.substring(35, 70)) : partyName,
+  name3: partyName ? stripDoubleQuotes(partyName.substring(70, 105)) : partyName,
 });
 
 module.exports = getPartyNames;
