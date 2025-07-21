115032e40abff27f6d5055e58b64171683f06f26
Abhi Markan <amarkan>
2025-07-21 09:48:13 +0100

    feat(DTFS2-8033): fixed component test cases

--- portal/component-tests/pageRenderer.js
+++ portal/component-tests/pageRenderer.js
@@ -1,11 +1,11 @@
-/* eslint-disable import/no-import-module-exports */
-import cheerio from 'cheerio';
+const cheerio = require('cheerio');
 
-import assertions from './assertions';
-import configureNunjucks from '../server/nunjucks-configuration';
+const assertions = require('./assertions');
+const configureNunjucks = require('../server/nunjucks-configuration');
+
+const nunjucks = configureNunjucks({});
 
 const pageRenderer = (pageLocation) => (params) => {
-  const nunjucks = configureNunjucks({});
   const html = nunjucks.render(pageLocation, params);
   const wrapper = cheerio.load(html);
   return assertions(wrapper, html, params);
