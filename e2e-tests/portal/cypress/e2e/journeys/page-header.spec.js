c6d0421fdbf3f8bf2f670e8e3ea057fbef00f24f
Abhi Markan <amarkan>
2025-08-01 16:36:12 +0100

    fix(DTFS2-8062): feature flag test cases for fee record correction portal links

--- e2e-tests/portal/cypress/e2e/journeys/page-header.spec.js
+++ e2e-tests/portal/cypress/e2e/journeys/page-header.spec.js
@@ -1,16 +1,17 @@
-const page = require('./pages/header');
-const MOCK_USERS = require('../../../e2e-fixtures/portal-users.fixture');
-const relative = require('./relativeURL');
+const page = require('../pages/header');
+const relative = require('../relativeURL');
+const { dashboardDeals } = require('../pages');
+const MOCK_USERS = require('../../../../e2e-fixtures');
 
-const { BANK1_MAKER1, ADMIN, BANK1_PAYMENT_REPORT_OFFICER1 } = MOCK_USERS;
+const { BANK1_MAKER1, BANK1_CHECKER1, ADMIN, BANK1_PAYMENT_REPORT_OFFICER1, READ_ONLY } = MOCK_USERS;
 
-beforeEach(() => {
-  cy.saveSession();
+context('portal GovUK header', () => {
+  beforeEach(() => {
+    cy.saveSession();
 
-  cy.visit(relative('/gef/mandatory-criteria'));
-});
+    dashboardDeals.visit();
+  });
 
-context('Check GEF GOVUK header displays correctly', () => {
   describe('signed-out user header', () => {
     it('should ensure the header component exist', () => {
       page.header().should('exist');
@@ -79,8 +80,8 @@ context('Check GEF GOVUK header displays correctly', () => {
     before(() => {
       cy.login(BANK1_MAKER1);
 
-      cy.visit(relative('/gef/mandatory-criteria'));
-      cy.url().should('eq', relative('/gef/mandatory-criteria'));
+      dashboardDeals.visit();
+      cy.url().should('eq', relative('/dashboard/deals/0'));
     });
 
     it('should ensure the header component exist', () => {
@@ -142,12 +143,79 @@ context('Check GEF GOVUK header displays correctly', () => {
     });
   });
 
+  describe('signed-in checker user header', () => {
+    before(() => {
+      cy.login(BANK1_CHECKER1);
+
+      dashboardDeals.visit();
+      cy.url().should('eq', relative('/dashboard/deals/0'));
+    });
+
+    it('should ensure the header component exist', () => {
+      page.header().should('exist');
+    });
+
+    it('should displays the correct header component with the crown logo', () => {
+      page.header().contains('GOV.UK');
+
+      page
+        .header()
+        .invoke('attr', 'href')
+        .then((href) => {
+          expect(href).to.equal('https://www.gov.uk/');
+        });
+
+      page.crown().should('exist');
+    });
+
+    it('should ensure the service navigation component exist', () => {
+      page.navigation().should('exist');
+    });
+
+    it('should displays the correct service navigation component with the service name', () => {
+      page.navigationLink().contains('Get a guarantee for export finance');
+
+      page
+        .navigationLink()
+        .invoke('attr', 'href')
+        .then((href) => {
+          expect(href).to.equal('https://get-a-guarantee-for-export-finance.service.gov.uk/');
+        });
+    });
+
+    it('should display all the checker service navigation links', () => {
+      page.dashboard().should('exist');
+      page.reports().should('exist');
+      page.profile().should('exist');
+      page.logout().should('exist');
+    });
+
+    it('should ensure the beta banner component exist', () => {
+      page.betaBanner().should('exist');
+    });
+
+    it('should display the beta banner component correctly', () => {
+      page.betaBanner().contains('This is a new service – your feedback will help us to improve it.');
+
+      cy.assertText(page.betaBannerTag(), 'Beta');
+
+      page.betaBannerHref().contains('feedback');
+
+      page
+        .betaBannerHref()
+        .invoke('attr', 'href')
+        .then((href) => {
+          expect(href).to.equal('/feedback');
+        });
+    });
+  });
+
   describe('signed-in admin user header', () => {
     before(() => {
       cy.login(ADMIN);
 
-      cy.visit(relative('/gef/mandatory-criteria'));
-      cy.url().should('eq', relative('/gef/mandatory-criteria'));
+      dashboardDeals.visit();
+      cy.url().should('eq', relative('/dashboard/deals/0'));
     });
 
     it('should ensure the header component exist', () => {
@@ -186,6 +254,72 @@ context('Check GEF GOVUK header displays correctly', () => {
       page.dashboard().should('exist');
       page.reports().should('exist');
       page.users().should('exist');
+      page.profile().should('exist');
+      page.logout().should('exist');
+    });
+
+    it('should ensure the beta banner component exist', () => {
+      page.betaBanner().should('exist');
+    });
+
+    it('should display the beta banner component correctly', () => {
+      page.betaBanner().contains('This is a new service – your feedback will help us to improve it.');
+
+      cy.assertText(page.betaBannerTag(), 'Beta');
+
+      page.betaBannerHref().contains('feedback');
+
+      page
+        .betaBannerHref()
+        .invoke('attr', 'href')
+        .then((href) => {
+          expect(href).to.equal('/feedback');
+        });
+    });
+  });
+
+  describe('signed-in read only user header', () => {
+    before(() => {
+      cy.login(READ_ONLY);
+
+      dashboardDeals.visit();
+      cy.url().should('eq', relative('/dashboard/deals/0'));
+    });
+
+    it('should ensure the header component exist', () => {
+      page.header().should('exist');
+    });
+
+    it('should displays the correct header component with the crown logo', () => {
+      page.header().contains('GOV.UK');
+
+      page
+        .header()
+        .invoke('attr', 'href')
+        .then((href) => {
+          expect(href).to.equal('https://www.gov.uk/');
+        });
+
+      page.crown().should('exist');
+    });
+
+    it('should ensure the service navigation component exist', () => {
+      page.navigation().should('exist');
+    });
+
+    it('should displays the correct service navigation component with the service name', () => {
+      page.navigationLink().contains('Get a guarantee for export finance');
+
+      page
+        .navigationLink()
+        .invoke('attr', 'href')
+        .then((href) => {
+          expect(href).to.equal('https://get-a-guarantee-for-export-finance.service.gov.uk/');
+        });
+    });
+
+    it('should display all the read only service navigation links', () => {
+      page.dashboard().should('exist');
       page.profile().should('exist');
       page.logout().should('exist');
     });
