23222d4a5ad52c79aa2f1b5a1d48834542d23faf
rkontogianni <rkontogianni@ukexportfinance.gov.uk>
2025-08-08 12:38:36 +0100

    fix(DTFS2-7880): address pr comments

--- libs/common/src/helpers/amendment-future-effectiveDate.ts
+++ libs/common/src/helpers/amendment-future-effectiveDate.ts
@@ -3,7 +3,7 @@ import { UnixTimestampSeconds } from '../types/date';
 import { now } from './date';
 /**
  * Returns a boolean indicating whether the given amendment effective date is in the future.
- *
- * @returns {Boolean} If the effective date is in the future, returns true; otherwise, false.
+ * @param date - The effective date of an amendment
+ * @returns If the effective date is in the future, returns true; otherwise, false.
  */
 export const isFutureEffectiveDate = (date: UnixTimestampSeconds): boolean => new Date(fromUnixTime(date ?? 0)) > now();
