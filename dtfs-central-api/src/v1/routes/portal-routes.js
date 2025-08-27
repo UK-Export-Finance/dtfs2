e62f547696198e0dc6fc7618775f3b6d7d73679c
rkontogianni <rkontogianni@ukexportfinance.gov.uk>
2025-08-27 12:50:10 +0100

    chore(DTFS2-7071-7072): improve  endpoints (#4397)

--- dtfs-central-api/src/v1/routes/portal-routes.js
+++ dtfs-central-api/src/v1/routes/portal-routes.js
@@ -88,14 +88,14 @@ portalRouter.use((req, res, next) => {
  *               auditDetails:
  *                 type: object
  *                 $ref: '#/definitions/PortalAuditDetails'
- *           example:
- *             bankInternalRefName: 'a1'
- *             additionalRefName: 'test'
- *             bank: { id: '9' }
- *             maker: { _id: '123abc' }
- *             details:
- *               ukefDealId: '20010739'
- *               submissionCount: 1
+ *         example:
+ *           bankInternalRefName: 'a1'
+ *           additionalRefName: 'test'
+ *           bank: { id: '9' }
+ *           maker: { _id: '123abc' }
+ *           details:
+ *           ukefDealId: '20010739'
+ *           submissionCount: 1
  *     responses:
  *       200:
  *         description: OK
@@ -150,14 +150,10 @@ portalRouter.route('/deals').post(createDealController.createDealPost);
  *                   properties:
  *                     _id:
  *                       example: 123456abc
+ *       400:
+ *         description: Invalid Deal Id
  *       404:
  *         description: Not found
- */
-portalRouter.route('/deals/:id').get(getDealController.findOneDealGet);
-
-/**
- * @openapi
- * /portal/deals/:id:
  *   put:
  *     summary: Update a Portal BSS deal
  *     tags: [Portal - BSS]
@@ -201,15 +197,11 @@ portalRouter.route('/deals/:id').get(getDealController.findOneDealGet);
  *                       example: true
  *       404:
  *         description: Not found
- */
-portalRouter.route('/deals/:id').put(updateDealController.updateDealPut);
-
-/**
- * @openapi
- * /portal/deals/:id:
+ *       500:
+ *         description: Internal server error
  *   delete:
  *     summary: Delete a Portal BSS deal
- *     tags: [TFM]
+ *     tags: [Portal - BSS]
  *     description: Delete a Portal BSS deal by ID
  *     parameters:
  *       - in: path
@@ -226,8 +218,14 @@ portalRouter.route('/deals/:id').put(updateDealController.updateDealPut);
  *             example:
  *               acknowledged: true
  *               deletedCount: 1
+ *       400:
+ *        description: Invalid Deal Id
+ *       404:
+ *         description: Deal Not found
+ *       500:
+ *         description: Internal server error
  */
-portalRouter.route('/deals/:id').delete(deleteDealController.deleteDeal);
+portalRouter.route('/deals/:id').get(getDealController.findOneDealGet).put(updateDealController.updateDealPut).delete(deleteDealController.deleteDeal);
 
 /**
  * @openapi
@@ -267,8 +265,12 @@ portalRouter.route('/deals/:id').delete(deleteDealController.deleteDeal);
  *                           example: Submitted
  *                         status:
  *                           example: Acknowledged
+ *       400:
+ *         description: Invalid Deal Id
  *       404:
  *         description: Not found
+ *       500:
+ *         description: Internal server error
  */
 portalRouter.route('/deals/:id/status').put(updateDealStatusController.updateDealStatusPut);
 
@@ -316,8 +318,12 @@ portalRouter.route('/deals/:id/status').put(updateDealStatusController.updateDea
  *           application/json:
  *             schema:
  *               $ref: '#/definitions/DealBSS'
+ *       400:
+ *         description: Invalid Deal Id
  *       404:
  *         description: Deal not found
+ *       500:
+ *         description: Internal server error
  */
 portalRouter.route('/deals/:id/comment').post(addDealCommentController.addDealCommentPost);
 
@@ -384,9 +390,56 @@ portalRouter.route('/facilities').get(getFacilitiesController.findAllGet);
  *                   dealId:
  *                     order: '2'
  *                     text: 'Enter the Associated deal id'
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal server error
  */
 portalRouter.route('/facilities').post(createFacilityController.createFacilityPost);
 
+/**
+ * @openapi
+ * /portal/multiple-facilities:
+ *   post:
+ *     summary: Create BSS/EWCS facilities in Portal facilities collection (e2e tests only)
+ *     tags: [Portal - BSS]
+ *     description: Create BSS/EWCS facilities in Portal facilities collection
+ *     requestBody:
+ *       description: Fields required to create facilities.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               facilities:
+ *                 type: array
+ *               dealId:
+ *                 type: string
+ *               user:
+ *                 type: object
+ *               auditDetails:
+ *                 type: object
+ *                 $ref: '#/definitions/PortalAuditDetails'
+ *           example:
+ *             facilities: [{ aNewField: true }]
+ *             dealId: '123abc'
+ *             user: { _id: '123456abc' }
+ *             auditDetails: { userType: 'portal', id: 'abcdef123456abcdef123456' }
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             example:
+ *               _id: '123456abc'
+ *       400:
+ *         description: Invalid Deal Id
+ *       404:
+ *         description: Deal not found
+ *       500:
+ *         description: Internal server error
+ */
 portalRouter.route('/multiple-facilities').post(createMultipleFacilitiesController.createMultipleFacilitiesPost);
 
 /**
@@ -413,6 +466,8 @@ portalRouter.route('/multiple-facilities').post(createMultipleFacilitiesControll
  *           application/json:
  *             schema:
  *                $ref: '#/definitions/PortalAmendment'
+ *       204:
+ *         description: No content
  *       404:
  *         description: Not found
  *       500:
@@ -443,12 +498,8 @@ portalRouter.route('/facilities/amendments').get(getAllFacilityAmendmentControll
  *               $ref: '#/definitions/FacilityBSS'
  *       404:
  *         description: Not found
- */
-portalRouter.route('/facilities/:id').get(getFacilityController.findOneFacilityGet);
-
-/**
- * @openapi
- * /portal/facilities/:id:
+ *       500:
+ *         description: Internal server error
  *   put:
  *     summary: Update a Portal BSS/EWCS facility
  *     tags: [Portal - BSS]
@@ -488,15 +539,11 @@ portalRouter.route('/facilities/:id').get(getFacilityController.findOneFacilityG
  *                       example: true
  *       404:
  *         description: Not found
- */
-portalRouter.route('/facilities/:id').put(updateFacilityController.updateFacilityPut);
-
-/**
- * @openapi
- * /portal/facilities/:id:
+ *       500:
+ *        description: Internal server error
  *   delete:
  *     summary: Delete a Portal BSS/EWCS facility
- *     tags: [TFM]
+ *     tags: [Portal - BSS]
  *     description: Delete a Portal BSS/EWCS facility by ID. Also updates the facilities array in the associated deal.
  *     parameters:
  *       - in: path
@@ -515,8 +562,14 @@ portalRouter.route('/facilities/:id').put(updateFacilityController.updateFacilit
  *               deletedCount: 1
  *       404:
  *         description: Not found
+ *       500:
+ *         description: Internal server error
  */
-portalRouter.route('/facilities/:id').delete(deleteFacilityController.deleteFacility);
+portalRouter
+  .route('/facilities/:id')
+  .get(getFacilityController.findOneFacilityGet)
+  .put(updateFacilityController.updateFacilityPut)
+  .delete(deleteFacilityController.deleteFacility);
 
 /**
  * @openapi
@@ -558,6 +611,8 @@ portalRouter.route('/facilities/:id').delete(deleteFacilityController.deleteFaci
  *                       example: Draft
  *       404:
  *         description: Not found
+ *       500:
+ *         description: Internal server error
  */
 portalRouter.route('/facilities/:id/status').put(updateFacilityStatusController.updateFacilityStatusPut);
 
@@ -652,6 +707,8 @@ portalRouter
  *                $ref: '#/definitions/PortalAmendment'
  *       404:
  *         description: Not found
+ *       500:
+ *         description: Internal server error
  *   patch:
  *     summary: Update a Portal GEF facility amendment
  *     tags: [Portal - Amendments]
@@ -692,6 +749,8 @@ portalRouter
  *               $ref: '#/definitions/PortalAmendment'
  *       404:
  *         description: Not found
+ *       500:
+ *         description: Internal server error
  *   delete:
  *     summary: Delete a Portal GEF facility amendment
  *     tags: [Portal - Amendments]
@@ -757,6 +816,12 @@ portalRouter
  *               newStatus:
  *                  type: string
  *                  enum: ["Ready for checker's approval"]
+ *               makersEmail:
+ *                 type: string
+ *               checkersEmail:
+ *                 type: string
+ *               emailVariables:
+ *                type: object
  *     responses:
  *       200:
  *         description: OK
@@ -769,6 +834,8 @@ portalRouter
  *         description: Not found
  *       409:
  *         description: Conflict - amendment cannot currently be updated to the given status
+ *       500:
+ *         description: Internal server error
  */
 portalRouter
   .route('/facilities/:facilityId/amendments/:amendmentId/status')
@@ -815,6 +882,12 @@ portalRouter
  *                  enum: ["Acknowledged"]
  *               referenceNumber:
  *                  type: string
+ *               makersEmail:
+ *                 type: string
+ *               checkersEmail:
+ *                 type: string
+ *               emailVariables:
+ *                type: object
  *     responses:
  *       200:
  *         description: OK
@@ -827,6 +900,8 @@ portalRouter
  *         description: Not found
  *       409:
  *         description: Conflict - amendment cannot currently be updated
+ *       500:
+ *         description: Internal server error
  */
 portalRouter
   .route('/facilities/:facilityId/amendments/:amendmentId/submit-amendment')
@@ -880,6 +955,8 @@ portalRouter
  *         description: Not found
  *       409:
  *         description: Conflict
+ *       500:
+ *         description: Internal server error
  */
 portalRouter
   .route('/facilities/:facilityId/amendments')
@@ -891,7 +968,7 @@ portalRouter
  * /deals/:dealId/all-types-amendments:
  *   get:
  *     summary: Get all type facility amendments on a given deal
- *     tags: [Portal - Amendments]
+ *     tags: [Portal - Amendments, TFM - Amendments]
  *     description: Get all type facility amendments on a given deal
  *     parameters:
  *       - in: path
@@ -958,6 +1035,8 @@ portalRouter
  *                $ref: '#/definitions/PortalAmendment'
  *       404:
  *         description: Not found
+ *       500:
+ *         description: Internal server error
  */
 portalRouter
   .route('/deals/:dealId/amendments')
@@ -971,6 +1050,18 @@ portalRouter
  *     summary: Create a GEF deal in Portal deals collection
  *     tags: [Portal - GEF]
  *     description: Create a deal in Portal deals collection
+ *     requestBody:
+ *       description: Fields required to create a deal.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               deal:
+ *                 type: object
+ *           example:
+ *             deal: { _id: '123456abc' }
  *     responses:
  *       200:
  *         description: OK
@@ -978,6 +1069,8 @@ portalRouter
  *           application/json:
  *             example:
  *               _id: '123456abc'
+ *       400:
+ *         description: Invalid deal type
  */
 portalRouter.route('/gef/deals').post(createGefDealController.createDealPost);
 
@@ -1004,12 +1097,6 @@ portalRouter.route('/gef/deals').post(createGefDealController.createDealPost);
  *               $ref: '#/definitions/DealGEF'
  *       404:
  *         description: Not found
- */
-portalRouter.route('/gef/deals/:id').get(getGefDealController.findOneDealGet);
-
-/**
- * @openapi
- * /portal/gef/deals/:id:
  *   put:
  *     summary: Update a Portal GEF deal
  *     tags: [Portal - GEF]
@@ -1027,9 +1114,15 @@ portalRouter.route('/gef/deals/:id').get(getGefDealController.findOneDealGet);
  *         application/json:
  *           schema:
  *             type: object
+ *            properties:
+ *              dealUpdate:
+ *               type: object
+ *             auditDetails:
+ *               type: object
+ *               $ref: '#/definitions/PortalAuditDetails'
  *             example:
- *               user: { _id: '123456abc' }
  *               dealUpdate: { aNewField: true }
+ *               auditDetails: { userType: 'portal', id: 'abcdef123456abcdef123456' }
  *     responses:
  *       200:
  *         description: OK
@@ -1044,8 +1137,10 @@ portalRouter.route('/gef/deals/:id').get(getGefDealController.findOneDealGet);
  *                       example: true
  *       404:
  *         description: Not found
+ *       500:
+ *         description: Internal server error
  */
-portalRouter.route('/gef/deals/:id').put(updateGefDealController.updateDealPut);
+portalRouter.route('/gef/deals/:id').get(getGefDealController.findOneDealGet).put(updateGefDealController.updateDealPut);
 
 /**
  * @openapi
@@ -1061,6 +1156,16 @@ portalRouter.route('/gef/deals/:id').put(updateGefDealController.updateDealPut);
  *           type: string
  *         required: true
  *         description: Deal ID to find deal and facilities and portalActivities to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *            properties:
+ *             auditDetails:
+ *               type: object
+ *               $ref: '#/definitions/PortalAuditDetails'
  *     responses:
  *       200:
  *         description: OK
@@ -1068,8 +1173,10 @@ portalRouter.route('/gef/deals/:id').put(updateGefDealController.updateDealPut);
  *           application/json:
  *             schema:
  *               $ref: '#/definitions/DealGEF'
- *       404:
- *         description: Not found
+ *       400:
+ *         description: Invalid Deal Id
+ *       500:
+ *         description: Internal server error
  */
 portalRouter.route('/gef/deals/activity/:id').put(gefActivityController.generateMINActivities);
 
@@ -1093,6 +1200,12 @@ portalRouter.route('/gef/deals/activity/:id').put(gefActivityController.generate
  *         application/json:
  *           schema:
  *             type: object
+ *            properties:
+ *             status:
+ *              type: string
+ *             auditDetails:
+ *               type: object
+ *               $ref: '#/definitions/PortalAuditDetails'
  *             example:
  *               status: UKEF_ACKNOWLEDGED
  *     responses:
@@ -1109,8 +1222,10 @@ portalRouter.route('/gef/deals/activity/:id').put(gefActivityController.generate
  *                       example: Submitted
  *                     status:
  *                       example: Acknowledged
- *       404:
- *         description: Not found
+ *       400:
+ *         description: Invalid Deal Id
+ *       500:
+ *         description: Internal server error
  */
 portalRouter.route('/gef/deals/:id/status').put(putGefDealStatusController.updateDealStatusPut);
 
@@ -1156,6 +1271,8 @@ portalRouter.route('/gef/deals/:id/status').put(putGefDealStatusController.updat
  *           application/json:
  *             schema:
  *               $ref: '#/definitions/DealGEF'
+ *       400:
+ *         description: Invalid Deal Id
  *       404:
  *         description: Deal not found
  */
@@ -1182,6 +1299,8 @@ portalRouter.route('/gef/deals/:id/comment').post(addCommentToGefDeal.addUnderwr
  *           application/json:
  *             schema:
  *               $ref: '#/definitions/FacilitiesGEF'
+ *       400:
+ *         description: Invalid Deal Id
  */
 portalRouter.route('/gef/deals/:id/facilities').get(getGefFacilitiesController.findAllGet);
 
@@ -1199,6 +1318,8 @@ portalRouter.route('/gef/deals/:id/facilities').get(getGefFacilitiesController.f
  *           application/json:
  *             example:
  *               _id: '123456abc'
+ *       400:
+ *         description: Invalid facility payload
  *       404:
  *         description: Deal not found
  */
@@ -1256,6 +1377,8 @@ portalRouter.route('/gef/facilities').get(getGefFacilitiesController.findAllFaci
  *                       example: true
  *       404:
  *         description: Not found
+ *       500:
+ *         description: Internal server error
  */
 portalRouter.route('/gef/facilities/:id').put(updateGefFacilityController.updateFacilityPut);
 
@@ -1271,6 +1394,8 @@ portalRouter.route('/gef/facilities/:id').put(updateGefFacilityController.update
  *         description: OK
  *       210:
  *         description: FAILURE
+ *       500:
+ *         description: Internal server error
  *
  */
 portalRouter.route('/durable-functions').delete(durableFunctionsController.deleteAllDurableFunctions);
@@ -1291,7 +1416,38 @@ portalRouter.route('/durable-functions').delete(durableFunctionsController.delet
  */
 portalRouter.route('/cron-jobs').delete(cronJobsController.deleteAllEstoreLogs);
 
+/**
+ * @openapi
+ * /gef/mandatory-criteria/latest:
+ *   get:
+ *     summary: Get latest gef mandatory criteria
+ *     tags: [Portal - GEF]
+ *     description: Get latest gef mandatory criteria
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Invalid Version
+ *       404:
+ *         description: No mandatory criteria found
+ */
 portalRouter.route('/gef/mandatory-criteria/latest').get(mandatoryCriteria.getLatestGefMandatoryCriteria);
+
+/**
+ * @openapi
+ * /gef/mandatory-criteria/version/:version:
+ *   get:
+ *     summary: Get gef mandatory criteria by version
+ *     tags: [Portal - GEF]
+ *     description: Get gef mandatory criteria by version
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Invalid Version
+ *       404:
+ *         description: No mandatory criteria found
+ */
 portalRouter.route('/gef/mandatory-criteria/version/:version').get(mandatoryCriteria.getGefMandatoryCriteriaByVersion);
 
 module.exports = portalRouter;
