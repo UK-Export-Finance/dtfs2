941c0fe644a2905d81d78b1d6156301a818c2d67
rkontogianni <rkontogianni@ukexportfinance.gov.uk>
2025-09-03 12:41:52 +0100

    fix(DTFS2-6707): fix failing test

--- portal-api/src/cron-scheduler-jobs/send-report-submission-period-start-emails-job/index.test.ts
+++ portal-api/src/cron-scheduler-jobs/send-report-submission-period-start-emails-job/index.test.ts
@@ -30,12 +30,12 @@ describe('sendReportSubmissionPeriodStartEmailsJob', () => {
 
     jest.useFakeTimers().setSystemTime(new Date('2023-11-01'));
 
-    const validTestbank1Email = 'valid-testbank1-email@example.com';
+    const validTestbank1Email = 'valid-testbank1-email@ukexportfinance.gov.uk';
     const validtestBank1 = produce(aBank(), (draftBank) => {
       draftBank.paymentOfficerTeam.emails = [validTestbank1Email];
     });
 
-    const validTestbank2Email1 = 'valid-testbank2-email@example.com';
+    const validTestbank2Email1 = 'valid-testbank2-email@ukexportfinance.gov.uk';
     const quarterlyReportingSchedule = [
       { startMonth: 11, endMonth: 1 },
       { startMonth: 2, endMonth: 4 },
