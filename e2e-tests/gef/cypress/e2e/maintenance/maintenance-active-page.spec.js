6a037e0faf40c83df4ab71cd7b32c78af5ebe335
Abhi Markan <amarkan>
2025-09-05 14:59:31 +0100

    feat(DTFS2-7939): added spec.js files

---
+++ e2e-tests/gef/cypress/e2e/maintenance/maintenance-active-page.spec.js
@@ -0,0 +1,29 @@
+const page = require('../pages/maintenance');
+
+context('Maintenance', () => {
+  const contactUsEmailAddress = Cypress.env('CONTACT_US_EMAIL_ADDRESS');
+
+  beforeEach(() => {
+    cy.saveSession();
+
+    cy.visit('/', {
+      failOnStatusCode: false,
+    });
+  });
+
+  describe('service unavailable page', () => {
+    it('should ensure service unavailable heading exist ', () => {
+      page.heading().should('exist');
+    });
+
+    it('should ensure service unavailable message exist ', () => {
+      page.message().should('exist');
+      page.message().contains('You will be able to use the service from 11:59pm on Friday 31 December 9999.');
+    });
+
+    it('should ensure service unavailable contact exist ', () => {
+      page.contact().should('exist');
+      page.contact().contains(`If you have any urgent enquiries, please contact the helpdesk at ${contactUsEmailAddress}`);
+    });
+  });
+});
