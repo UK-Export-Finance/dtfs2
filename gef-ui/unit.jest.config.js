a638394c4081c198d49675a0c9a98a3ff3e2f18d
Abhi Markan <amarkan>
2025-09-10 16:42:37 +0100

    feat(DTFS2-6423): fixed gef-ui unit test

--- gef-ui/unit.jest.config.js
+++ gef-ui/unit.jest.config.js
@@ -5,5 +5,4 @@ module.exports = {
   collectCoverageFrom: ['server/controllers/**/*.{js,ts}', 'server/routes/**/*.{js,ts}', 'server/helpers/**/*.{js,ts}', 'scripts/**/*.{js,ts}'],
   coverageReporters: ['text', 'text-summary'],
   testMatch: ['**/*.test.{js,ts}', '**/*.component-test.{js,ts}'],
-  testEnvironment: 'jsdom',
 };
