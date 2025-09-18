dd4452197679376d6b26d92f94bf455f953980f4
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2025-09-18 13:29:02 +0100

    feat(DTFS2-8115): add swagger endpoints to UI microservices (#4436)
    Co-authored-by: Abhi Markan <amarkan>

--- libs/common/src/constants/swagger.ts
+++ libs/common/src/constants/swagger.ts
@@ -1,19 +1,30 @@
 /**
- * Swagger-related endpoint constants.
+ * Constants used for Swagger API documentation configuration.
  *
  * @remarks
- * This object contains endpoints for accessing the Swagger UI and downloading the Swagger JSON specification.
+ * This object contains endpoints for accessing the Swagger UI and downloading the API definition in JSON format,
+ * as well as file path patterns for locating Swagger definition files in JavaScript and TypeScript.
  *
- * @property ENDPOINTS - Contains endpoint paths for Swagger.
+ * @property ENDPOINTS - Endpoints related to Swagger documentation.
  * @property ENDPOINTS.UI - Path to the Swagger UI.
- * @property ENDPOINTS.DOWNLOAD - Contains endpoints for downloading Swagger specs.
- * @property ENDPOINTS.DOWNLOAD.JSON - Path to download the Swagger JSON specification.
+ * @property ENDPOINTS.DOWNLOAD - Endpoints for downloading Swagger definitions.
+ * @property ENDPOINTS.DOWNLOAD.JSON - Path to download the Swagger definition as a JSON file.
+ * @property DEFINITIONS - File path patterns for Swagger definition files.
+ * @property DEFINITIONS.PATHS - Paths for locating Swagger definition files.
+ * @property DEFINITIONS.PATHS.JS - Glob pattern for JavaScript Swagger definition files.
+ * @property DEFINITIONS.PATHS.TS - Glob pattern for TypeScript Swagger definition files.
  */
 export const SWAGGER = {
   ENDPOINTS: {
     UI: 'api-docs',
     DOWNLOAD: {
       JSON: '/api.json',
+    },
+  },
+  DEFINITIONS: {
+    PATHS: {
+      JS: '/**/swagger-definitions/**/*.js',
+      TS: '/**/swagger-definitions/**/*.ts',
     },
   },
 };
