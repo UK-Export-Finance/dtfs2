679fd14922d64b4ea63b446e010fde7341998b8f
Abhi Markan <amarkan>
2025-09-23 20:49:56 +0100

    feat(DTFS2-8122): updated paths

--- portal-api/server/v1/validation/fields/covered-percentage.js
+++ portal-api/server/v1/validation/fields/covered-percentage.js
@@ -1,4 +1,4 @@
-const { decimalsCount } = require('@ukef/dtfs2-common');
+const { decimalsCount } = require('@ukef/dtfs2-common/frontend');
 const { hasValue } = require('../../../utils/string');
 const { isNumeric } = require('../../../utils/number');
 const { orderNumber } = require('../../../utils/error-list-order-number');
