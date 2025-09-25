2ba07be1059aea3e6b3570efa9f28f77e9d644cf
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2025-09-25 11:09:49 +0100

    chore(DTFS2-7112): fix mock data loader

--- utils/mock-data-loader/portal-users/index.js
+++ utils/mock-data-loader/portal-users/index.js
@@ -1,9 +1,12 @@
 import { ROLES } from '@ukef/dtfs2-common';
+import { BANK1_MAKER1 } from '@ukef/dtfs2-common/test-helpers';
 import MOCK_BANKS from '../banks';
 
 const UKEF_TEST_BANK_1 = MOCK_BANKS.find((bank) => bank.name === 'Bank 1');
 const UKEF_TEST_BANK_2 = MOCK_BANKS.find((bank) => bank.name === 'Bank 2');
 const UKEF_GEF_ONLY_BANK = MOCK_BANKS.find((bank) => bank.name === 'GEF Only Bank');
+
+export { BANK1_MAKER1 };
 
 export const BANK1_MAKER2 = {
   username: 'maker2@ukexportfinance.gov.uk',
