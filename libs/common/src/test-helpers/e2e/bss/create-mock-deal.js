21f1b263a6b9f9b067f43ab4a07fc41a80f83d21
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2025-09-25 14:19:51 +0100

    chore(DTFS2-7112): lint disable

--- libs/common/src/test-helpers/e2e/bss/create-mock-deal.js
+++ libs/common/src/test-helpers/e2e/bss/create-mock-deal.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /* eslint-disable */
 const { MOCK_DEAL_AIN } = require('./mock-AIN-deal');
 
--- libs/common/src/test-helpers/e2e/index.ts
+++ libs/common/src/test-helpers/e2e/index.ts
@@ -1,4 +1,4 @@
+// @ts-nocheck disabled as used in e2e tests
 export * from './date-constants';
 export * from './bss/mock-AIN-deal';
-// @ts-ignore disabled for tests
 export * from './bss/create-mock-deal';
