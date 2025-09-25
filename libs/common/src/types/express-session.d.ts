72ce4df996edccfe073fc58307e6d451502b2fdc
Abhi Markan <amarkan>
2025-09-25 08:28:11 +0100

    feat(DTFS2-8122): express session optional properties augmentation

--- libs/common/src/types/express-session.d.ts
+++ libs/common/src/types/express-session.d.ts
@@ -14,7 +14,23 @@ declare module 'express-session' {
   }
 }
 
-// TODO: Add types to below properties
+// TODO[DTFS2-8133] Add strict property types
+/**
+ * Extends the Express `Request` interface to include a custom `session` object.
+ *
+ * @property csrf - Optional CSRF token for request validation.
+ * @property user - Optional user information stored in the session.
+ * @property userToken - Optional user token for authentication.
+ * @property loginStatus - Optional login status information.
+ * @property removeFeesFromPaymentErrorKey - Optional error key for removing fees from payment.
+ * @property editPaymentFormValues - Optional form values for editing payments.
+ * @property loginData - Optional login data.
+ * @property recordCorrectionRequestEmails - Optional list of emails for record correction requests.
+ * @property generateKeyingDataErrorKey - Optional error key for generating keying data.
+ * @property initiateRecordCorrectionRequestErrorKey - Optional error key for initiating record correction requests.
+ * @property checkedCheckboxIds - Optional IDs of checked checkboxes.
+ * @property addPaymentErrorKey - Optional error key for adding payments.
+ */
 declare module 'express' {
   interface Request {
     session: Session & {
