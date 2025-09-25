d2868146d12219709eea5bcebd635beb312a5492
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2025-09-25 13:42:42 +0100

    chore(DTFS2-7112): lint fix

--- libs/common/src/test-helpers/types/e2e.ts
+++ libs/common/src/test-helpers/types/e2e.ts
@@ -2,7 +2,7 @@ import { AnyObject } from '../../types';
 
 export type DealOverrides = {
   details?: { submissionDate?: string; [key: string]: unknown };
-  mockFacilities?: AnyObject[];
+  mockFacilities?: { [key: string]: unknown }[];
   bank?: AnyObject;
   submissionDetails?: AnyObject;
   [key: string]: unknown;
