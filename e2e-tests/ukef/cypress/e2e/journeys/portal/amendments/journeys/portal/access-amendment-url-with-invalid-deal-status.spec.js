36e6c7a08e1eaf5e471298d5930d3890e4f33f4a
Abhi Markan <amarkan>
2025-10-01 15:17:43 +0100

    feat(DTFS2-8146): allowd 404 for visit url

--- e2e-tests/ukef/cypress/e2e/journeys/portal/amendments/journeys/portal/access-amendment-url-with-invalid-deal-status.spec.js
+++ e2e-tests/ukef/cypress/e2e/journeys/portal/amendments/journeys/portal/access-amendment-url-with-invalid-deal-status.spec.js
@@ -67,7 +67,7 @@ context('Amendments - access amendment url when deal status is "Cancelled"', () 
   });
 
   it('should navigate /not-found when going to the amendment details url', () => {
-    cy.visit(relative(amendmentDetailsUrl));
+    cy.visit(relative(amendmentDetailsUrl), { failOnStatusCode: false });
 
     cy.url().should('eq', relative('/not-found'));
   });
