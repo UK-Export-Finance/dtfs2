c8b6da8dc2100d6a3bf8676008509a1ce9cfc252
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2025-10-11 13:50:01 +0100

    chore(DTFS2-8158): revert change to email file

--- external-api/server/v1/controllers/email.controller.ts
+++ external-api/server/v1/controllers/email.controller.ts
@@ -1,21 +1,33 @@
-/* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
 import * as dotenv from 'dotenv';
-import axios, { HttpStatusCode } from 'axios';
+import axios, { AxiosError, HttpStatusCode } from 'axios';
 import { Request, Response } from 'express';
 import { HEADERS, isGovNotifyMocked } from '@ukef/dtfs2-common';
+import { MDM } from '../../constants';
+import { getNowAsEpoch } from '../../helpers/date';
 
 dotenv.config();
 
 const referenceProxyUrl = process.env.EXTERNAL_API_URL;
 
+const { APIM_MDM_VALUE, APIM_MDM_KEY, APIM_MDM_URL } = process.env;
+
+const headers = {
+  [HEADERS.CONTENT_TYPE.KEY]: HEADERS.CONTENT_TYPE.VALUES.JSON,
+  [String(APIM_MDM_KEY)]: APIM_MDM_VALUE,
+  [String(MDM.GOV_UK_NOTIFY_KEY_HEADER_NAME)]: process.env.GOV_NOTIFY_API_KEY ?? '',
+};
+
 export const emailNotification = async (req: Request, res: Response) => {
-  const { templateId }: { templateId: string; sendToEmailAddress: string; emailVariables: Record<string, string> } = req.body;
+  const { templateId, sendToEmailAddress, emailVariables }: { templateId: string; sendToEmailAddress: string; emailVariables: Record<string, string> } =
+    req.body;
 
   // Add a unique reference to an email
+  const reference = `${templateId}-${getNowAsEpoch()}`;
+
+  const personalisation = emailVariables;
 
   console.info('Calling APIM MDM GovNotify API template id %s', templateId);
 
@@ -30,30 +42,29 @@ export const emailNotification = async (req: Request, res: Response) => {
     return res.status(HttpStatusCode.Ok).send({});
   }
 
-  // const response: { status: number | undefined; data: unknown } = await axios({
-  //   method: 'post',
-  //   url: `${APIM_MDM_URL}emails`,
-  //   headers,
-  //   data: {
-  //     templateId,
-  //     sendToEmailAddress,
-  //     reference,
-  //     personalisation,
-  //   },
-  // }).catch((error: AxiosError) => {
-  //   console.error('Error calling APIM MDM GovNotify API %o', error);
-  //   return { status: error?.response?.status || HttpStatusCode.UnprocessableEntity, data: 'Failed to call MDM GovNotify API' };
-  // });
+  const response: { status: number | undefined; data: unknown } = await axios({
+    method: 'post',
+    url: `${APIM_MDM_URL}emails`,
+    headers,
+    data: {
+      templateId,
+      sendToEmailAddress,
+      reference,
+      personalisation,
+    },
+  }).catch((error: AxiosError) => {
+    console.error('Error calling APIM MDM GovNotify API %o', error);
+    return { status: error?.response?.status || HttpStatusCode.UnprocessableEntity, data: 'Failed to call MDM GovNotify API' };
+  });
 
-  // if (!response) {
-  //   console.error('Empty APIM MDM GovNotify API response');
-  //   return res.status(HttpStatusCode.UnprocessableEntity).send({});
-  // }
+  if (!response) {
+    console.error('Empty APIM MDM GovNotify API response');
+    return res.status(HttpStatusCode.UnprocessableEntity).send({});
+  }
 
-  // const { status, data } = response;
+  const { status, data } = response;
 
-  // return res.status(status || HttpStatusCode.UnprocessableEntity).send(data);
-  return res.status(HttpStatusCode.Ok).send({});
+  return res.status(status || HttpStatusCode.UnprocessableEntity).send(data);
 };
 
 export const sendEmail = async (templateId: string, sendToEmailAddress: string, emailVariables: object) => {
