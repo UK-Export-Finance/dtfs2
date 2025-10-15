fefee07ac522424eada6bc21e56468668bb31f6f
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2025-10-15 14:42:00 +0100

    feat(DTFS2-8151): enabled hot reloading for gef-ui (#4490)
    Co-authored-by: Abhi Markan <amarkan>

---
+++ start.sh
@@ -0,0 +1,18 @@
+# This script starts a Node.js microservice with environment-specific behavior.
+# - If NODE_ENV is not "production":
+#     - Installs `nodemon` globally for automatic restarts on file changes.
+#     - Runs the microservice in development mode using "npm run start:dev" with the specified MICROSERVICE.
+# - If NODE_ENV is "production":
+#     - Runs the microservice in production mode using "npm run start" with the specified MICROSERVICE.
+# The script uses "set -euo pipefail" for safer error handling.
+
+
+#!/bin/sh
+set -euo pipefail
+
+if [ "$NODE_ENV" != "production" ]; then
+    npm i nodemon
+    exec npm run start:dev -w "$MICROSERVICE"
+else
+    exec npm run start -w "$MICROSERVICE"
+fi
