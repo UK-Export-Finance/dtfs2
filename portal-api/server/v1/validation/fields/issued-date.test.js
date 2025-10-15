18ed1b4424ed47fa149f5c567df8978c94652e6a
rkontogianni <rkontogianni@ukexportfinance.gov.uk>
2025-10-15 06:19:50 +0100

    chore(DTFS2-7029): validate issued date correctly (#4482)

---
+++ portal-api/server/v1/validation/fields/issued-date.test.js
@@ -0,0 +1,65 @@
+const { sub, format, add } = require('date-fns');
+const validateIssuedDate = require('./issued-date');
+const { getLongFormattedDate } = require('../../helpers/date');
+
+const today = new Date();
+const twoDaysAgo = sub(today, { days: 2 });
+const tomorrow = add(today, { days: 1 });
+
+const deal = {
+  details: {
+    submissionDate: today.valueOf(),
+  },
+};
+const errorList = [];
+
+describe('validateIssuedDate', () => {
+  it('should return missing error if no date and no day values', () => {
+    const facility = {
+      issuedDate: null,
+      'issuedDate-day': null,
+      'issuedDate-month': null,
+      'issuedDate-year': null,
+    };
+
+    const result = validateIssuedDate(facility, errorList, deal);
+
+    expect(result.issuedDate.text).toEqual('Enter the Issued Date');
+  });
+  it('should return error if date is before dealSubmissionDate', () => {
+    const facility = {
+      issuedDate: twoDaysAgo.valueOf(),
+      'issuedDate-day': format(twoDaysAgo, 'dd'),
+      'issuedDate-month': format(twoDaysAgo, 'MM'),
+      'issuedDate-year': format(twoDaysAgo, 'yyyy'),
+    };
+
+    const result = validateIssuedDate(facility, errorList, deal);
+
+    expect(result.issuedDate.text).toEqual(`Issued Date must be on or after ${getLongFormattedDate(deal.details.submissionDate)}`);
+  });
+  it('should return error if date is in the future', () => {
+    const facility = {
+      issuedDate: tomorrow.valueOf(),
+      'issuedDate-day': format(tomorrow, 'dd'),
+      'issuedDate-month': format(tomorrow, 'MM'),
+      'issuedDate-year': format(tomorrow, 'yyyy'),
+    };
+
+    const result = validateIssuedDate(facility, errorList, deal);
+
+    expect(result.issuedDate.text).toEqual('Issued Date must be today or in the past');
+  });
+  it('should return missing error if day values exist but full date is missing', () => {
+    const facility = {
+      issuedDate: null,
+      'issuedDate-day': format(tomorrow, 'dd'),
+      'issuedDate-month': format(tomorrow, 'MM'),
+      'issuedDate-year': format(tomorrow, 'yyyy'),
+    };
+
+    const result = validateIssuedDate(facility, errorList, deal);
+
+    expect(result.issuedDate.text).toEqual('Enter the Issued Date');
+  });
+});
