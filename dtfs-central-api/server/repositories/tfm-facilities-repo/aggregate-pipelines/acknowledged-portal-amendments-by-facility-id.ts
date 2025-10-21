848fee980a2476ba86fb23b5687f635351d0917c
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2025-10-21 12:02:25 +0100

    chore(DTFS2-8159): use referenceNumber instead of updatedAt

--- dtfs-central-api/server/repositories/tfm-facilities-repo/aggregate-pipelines/acknowledged-portal-amendments-by-facility-id.ts
+++ dtfs-central-api/server/repositories/tfm-facilities-repo/aggregate-pipelines/acknowledged-portal-amendments-by-facility-id.ts
@@ -10,6 +10,6 @@ export const acknowledgedPortalAmendmentsByFacilityId = (facilityId: string | Ob
   { $match: { _id: { $eq: new ObjectId(facilityId) } } },
   { $unwind: '$amendments' },
   { $match: { 'amendments.status': { $eq: PORTAL_AMENDMENT_STATUS.ACKNOWLEDGED }, 'amendments.type': { $eq: AMENDMENT_TYPES.PORTAL } } },
-  { $sort: { 'amendments.updatedAt': -1, 'amendments.version': -1 } },
+  { $sort: { 'amendments.referenceNumber': -1, 'amendments.version': -1 } },
   { $project: { _id: false, amendments: true } },
 ];
