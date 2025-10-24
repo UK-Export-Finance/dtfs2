adbfb033447ec6199097d3bb5888797473e53c6d
Abhi Markan <amarkan>
2025-10-24 13:05:32 +0100

    fix(DTFS2-8157): removed tfm-api

--- trade-finance-manager-api/server/utils/crypto.util.js
+++ trade-finance-manager-api/server/utils/crypto.util.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto');
 const dotenv = require('dotenv');
 const jsonwebtoken = require('jsonwebtoken');
-const { salt: generateSalt } = require('@ukef/dtfs2-common');
 
 dotenv.config();
 
@@ -42,7 +41,7 @@ function genPasswordResetToken(user) {
  */
 function issueJWT(user) {
   const { _id } = user;
-  const sessionIdentifier = generateSalt().toString('hex');
+  const sessionIdentifier = crypto.randomBytes(32).toString('hex');
 
   const expiresIn = '1d';
 
