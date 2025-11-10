3fd09ee6b5cd5dd01504d34b44357b649bfe5112
rkontogianni <rkontogianni@ukexportfinance.gov.uk>
2025-11-10 12:49:07 +0000

    feat(DTFS2-8193): resolve pr comments

--- portal-ui/component-tests/login/_macros/access-code-attempts.component-test.js
+++ portal-ui/component-tests/login/_macros/access-code-attempts.component-test.js
@@ -14,20 +14,20 @@ describe(component, () => {
   it('should render all access code info sign in button and access code link', () => {
     const wrapper = render(params);
 
-    wrapper.expectElement('[data-cy="access-code-expired-security-info"]').hasClass('govuk-body');
+    wrapper.expectElement('[data-cy="access-code-spam-or-junk"]').hasClass('govuk-body');
     wrapper
-      .expectElement('[data-cy="access-code-expired-security-info"]')
+      .expectElement('[data-cy="access-code-spam-or-junk"]')
       .toContain('Please check your spam or junk folders and be aware emails may sometimes take a few minutes to arrive.');
 
-    wrapper.expectElement('[data-cy="access-code-expired-support-info"]').hasClass('govuk-body');
-    wrapper.expectElement('[data-cy="access-code-expired-support-info"]').toContain('If you are still having problems signing in, contact us for support.');
+    wrapper.expectElement('[data-cy="access-code-support-info"]').hasClass('govuk-body');
+    wrapper.expectElement('[data-cy="access-code-support-info"]').toContain('If you are still having problems signing in, contact us for support.');
 
-    wrapper.expectElement('[data-cy="access-code-expired-attempts-info"]').hasClass('govuk-body');
-    wrapper.expectElement('[data-cy="access-code-expired-attempts-info"]').toContain('You have 3 attempts remaining.');
+    wrapper.expectElement('[data-cy="access-code-attempts-info"]').hasClass('govuk-body');
+    wrapper.expectElement('[data-cy="access-code-attempts-info"]').toContain('You have 3 attempts remaining.');
 
-    wrapper.expectElement('[data-cy="access-code-expired-suspend-info"]').hasClass('govuk-body');
+    wrapper.expectElement('[data-cy="access-code-suspend-info"]').hasClass('govuk-body');
     wrapper
-      .expectElement('[data-cy="access-code-expired-suspend-info"]')
+      .expectElement('[data-cy="access-code-suspend-info"]')
       .toContain('If you request too many access codes your account will be suspended for security purposes and you will be prompted to contact us.');
 
     wrapper.expectElement('[data-cy="submit-button"]').toContain('Sign in');
@@ -39,7 +39,7 @@ describe(component, () => {
   it('should not render support info or request code link when flags are false', () => {
     const wrapper = render({ ...params, isSupportInfo: false, isAccessCodeLink: false });
 
-    wrapper.expectElement('[data-cy="access-code-expired-support-info"]').notToExist();
+    wrapper.expectElement('[data-cy="access-code-support-info"]').notToExist();
     wrapper.expectElement('[data-cy="request-code-link"]').notToExist();
   });
 });
