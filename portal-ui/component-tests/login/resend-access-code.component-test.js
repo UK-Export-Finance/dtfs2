865c9e664ed9fb0e50488508fe0e64d41d3de314
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2025-11-10 22:36:26 +0000

    feat(8194): add resend access code page and associated tests

---
+++ portal-ui/component-tests/login/resend-access-code.component-test.js
@@ -0,0 +1,64 @@
+const pageRenderer = require('../pageRenderer');
+const { withContactUsEmailAddressTests } = require('../test-helpers/with-contact-us-email-address.component-tests');
+
+const page = 'login/resend-access-code.njk';
+const render = pageRenderer(page);
+
+describe(page, () => {
+  const email = 'user@example.com';
+  const attemptsRemaining = 2;
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = render({ email, attemptsRemaining });
+  });
+
+  withContactUsEmailAddressTests({ page });
+
+  it('should render the heading', () => {
+    wrapper.expectText('[data-cy="access-code-email-sent-heading"]').toRead("We've sent you another access code");
+  });
+
+  it('should render the description paragraph with email', () => {
+    wrapper.expectText('[data-cy="access-code-email-sent-description"]').toMatch(new RegExp(email));
+  });
+
+  it('should render the access code input', () => {
+    const input = wrapper.expectInput('[data-cy="access-code-input"]');
+    expect(input).toBeDefined();
+  });
+
+  it('should render the label for the access code input', () => {
+    wrapper.expectText('label[for="accessCode"]').toRead('Enter access code:');
+  });
+
+  it('should render the expiry info paragraph', () => {
+    wrapper
+      .expectText('[data-cy="access-code-email-sent-expiry-info"]')
+      .toRead('This code will expire after 30 minutes. Any previous access codes we have sent will no longer be valid.');
+  });
+
+  it('should render the spam or junk info paragraph', () => {
+    wrapper
+      .expectText('[data-cy="access-code-spam-or-junk"]')
+      .toRead('Please check your spam or junk folders and be aware emails may sometimes take a few minutes to arrive.');
+  });
+
+  it('should render the support info paragraph', () => {
+    wrapper.expectText('[data-cy="access-code-support-info"]').toRead('If you are still having problems signing in, contact us for support.');
+  });
+
+  it('should render the attempts remaining paragraph', () => {
+    wrapper.expectText('[data-cy="access-code-attempts-info"]').toRead(`You have ${attemptsRemaining} attempts remaining.`);
+  });
+
+  it('should render the suspend info paragraph', () => {
+    wrapper
+      .expectText('[data-cy="access-code-suspend-info"]')
+      .toRead('If you request too many access codes your account will be suspended for security purposes and you will be prompted to contact us.');
+  });
+
+  it('should render the sign in button', () => {
+    wrapper.expectText('[data-cy="submit-button"]').toRead('Sign in');
+  });
+});
