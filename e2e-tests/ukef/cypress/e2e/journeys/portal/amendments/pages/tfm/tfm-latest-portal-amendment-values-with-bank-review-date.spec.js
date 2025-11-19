9b3d2adf06624af0bc36c27ba5e06f2dbc6dae31
rkontogianni <rkontogianni@ukeportfinance.gov.uk>
2025-11-19 13:31:15 +0000

    chore(DTFS2-8209): fix bank review Date not update on tfm

---
+++ e2e-tests/ukef/cypress/e2e/journeys/portal/amendments/pages/tfm/tfm-latest-portal-amendment-values-with-bank-review-date.spec.js
@@ -0,0 +1,83 @@
+import { format, differenceInMonths } from 'date-fns';
+import { D_MMMM_YYYY_FORMAT, twoYears, tomorrow } from '@ukef/dtfs2-common/test-helpers';
+import MOCK_USERS from '../../../../../../../../e2e-fixtures/portal-users.fixture';
+import { MOCK_APPLICATION_AIN_DRAFT } from '../../../../../../../../e2e-fixtures/gef/mocks/mock-deals';
+import { anIssuedCashFacility } from '../../../../../../../../e2e-fixtures/mock-gef-facilities';
+import { PIM_USER_1, TFM_URL } from '../../../../../../../../e2e-fixtures';
+import facilityPage from '../../../../../../../../tfm/cypress/e2e/pages/facilityPage';
+
+const { BANK1_MAKER1 } = MOCK_USERS;
+
+const mockFacility = anIssuedCashFacility({ facilityEndDateEnabled: true });
+
+const CHANGED_FACILITY_VALUE = '20000';
+
+context('Amendments - TFM - Latest values for portal amendment - Bank Review Date', () => {
+  let dealId;
+  let facilityId;
+  let applicationDetailsUrl;
+  let tfmDealPage;
+  let tfmFacilityPage;
+
+  before(() => {
+    cy.insertOneGefDeal(MOCK_APPLICATION_AIN_DRAFT, BANK1_MAKER1).then((insertedDeal) => {
+      dealId = insertedDeal._id;
+      applicationDetailsUrl = `/gef/application-details/${dealId}`;
+
+      cy.updateGefDeal(dealId, MOCK_APPLICATION_AIN_DRAFT, BANK1_MAKER1);
+
+      cy.createGefFacilities(dealId, [mockFacility], BANK1_MAKER1).then((createdFacility) => {
+        facilityId = createdFacility.details._id;
+
+        tfmDealPage = `${TFM_URL}/case/${dealId}/deal`;
+        tfmFacilityPage = `${TFM_URL}/case/${dealId}/facility/${facilityId}`;
+
+        cy.makerLoginSubmitGefDealForReview(insertedDeal);
+        cy.checkerLoginSubmitGefDealToUkef(insertedDeal);
+
+        cy.clearSessionCookies();
+
+        cy.loginAndSubmitPortalAmendmentRequestToUkef({
+          facilityValueExists: true,
+          coverEndDateExists: true,
+          changedCoverEndDate: tomorrow.date,
+          changedBankReviewDate: tomorrow.date,
+          changedFacilityValue: CHANGED_FACILITY_VALUE,
+          applicationDetailsUrl,
+          facilityId,
+          dealId,
+        });
+
+        cy.visit(TFM_URL);
+
+        cy.tfmLogin(PIM_USER_1);
+        cy.visit(tfmDealPage);
+      });
+    });
+  });
+
+  after(() => {
+    cy.clearCookies();
+    cy.clearSessionCookies();
+  });
+
+  beforeEach(() => {
+    cy.clearSessionCookies();
+    cy.login(BANK1_MAKER1);
+    cy.visit(TFM_URL);
+
+    cy.tfmLogin(PIM_USER_1);
+    cy.visit(tfmFacilityPage);
+  });
+
+  it('should display the latest amended values on facility summary list', () => {
+    const tenor = differenceInMonths(new Date(twoYears.date), new Date()) + 1; // +1 to include the current month
+    const coverEndDateFormatted = format(new Date(tomorrow.date), D_MMMM_YYYY_FORMAT);
+    const bankReviewDateFormatted = format(new Date(tomorrow.date), D_MMMM_YYYY_FORMAT);
+
+    facilityPage.facilityCoverEndDate().contains(coverEndDateFormatted);
+    facilityPage.facilityTenor().contains(`${tenor} months`);
+    facilityPage.facilityBankReviewDate().contains(bankReviewDateFormatted);
+    facilityPage.facilityValueGbp().contains(CHANGED_FACILITY_VALUE);
+  });
+});
