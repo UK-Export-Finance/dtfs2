4c9284c83b32734f8ea0af9985804c7c2fdfec66
rkontogianni <rkontogianni@ukeportfinance.gov.uk>
2026-01-08 17:52:25 +0000

    fix(DTFS2-8218): add reference number e2e tests on portal

--- e2e-tests/gef/cypress/e2e/pages/application-amendments.js
+++ e2e-tests/gef/cypress/e2e/pages/application-amendments.js
@@ -3,6 +3,21 @@ const applicationAmendments = {
   tabHeading: () => cy.get('[data-cy="tab-heading"]'),
   summaryList: () => cy.get('[data-cy="submitted-amendment-summary-list"]'),
   amendmentDetailsLink: () => cy.get('[data-cy="amendment-details-link"]'),
+  amendmentDetails: {
+    row: (index) => {
+      cy.get(`[data-cy="amendment--details-${index}"]`).as('row');
+      return {
+        facilityId: () => cy.get('@row').get(`[data-cy="amendment--details-${index}-facility-id"]`),
+        facilityType: () => cy.get('@row').get(`[data-cy="amendment--details-${index}-facility-type"]`),
+        status: () => cy.get('@row').get(`[data-cy="amendment--details-${index}-status"]`),
+        newCoverEndDate: () => cy.get('@row').get(`[data-cy="amendment--details-${index}-new-cover-end-date"]`),
+        newFacilityEndDate: () => cy.get('@row').get(`[data-cy="amendment--details-${index}-new-facility-end-date"]`),
+        newFacilityValue: () => cy.get('@row').get(`[data-cy="amendment--details-${index}-new-facility-value"]`),
+        effectiveFrom: () => cy.get('@row').get(`[data-cy="amendment--details-${index}-effective-from"]`),
+        createdBy: () => cy.get('@row').get(`[data-cy="amendment--details-${index}-created-by"]`),
+      };
+    },
+  },
 };
 
 module.exports = applicationAmendments;
--- e2e-tests/ukef/cypress/e2e/journeys/portal/amendments/pages/portal/application-details/application-details-amendments-tab.spec.js
+++ e2e-tests/ukef/cypress/e2e/journeys/portal/amendments/pages/portal/application-details/application-details-amendments-tab.spec.js
@@ -1,3 +1,5 @@
+import { PORTAL_AMENDMENT_STATUS, CURRENCY, getFormattedMonetaryValue } from '@ukef/dtfs2-common';
+import { today } from '@ukef/dtfs2-common/test-helpers';
 import relative from '../../../../../../relativeURL';
 import MOCK_USERS from '../../../../../../../../../e2e-fixtures/portal-users.fixture';
 import { MOCK_APPLICATION_AIN_DRAFT } from '../../../../../../../../../e2e-fixtures/gef/mocks/mock-deals';
@@ -5,11 +7,13 @@ import { anIssuedCashFacility, anIssuedContingentFacility } from '../../../../..
 import { applicationAmendments } from '../../../../../../../../../gef/cypress/e2e/pages';
 
 const { BANK1_MAKER1 } = MOCK_USERS;
+const CHANGED_FACILITY_VALUE = '20000';
 
 context('Application details - Amendments Tab', () => {
   let dealId;
   let issuedCashFacilityId;
   let issuedContingentFacilityId;
+  let ukefFacilityId;
   const issuedCashFacility = anIssuedCashFacility({ facilityEndDateEnabled: true });
   const issuedContingentFacility = anIssuedContingentFacility({ facilityEndDateEnabled: true });
 
@@ -21,6 +25,7 @@ context('Application details - Amendments Tab', () => {
 
       cy.createGefFacilities(dealId, [issuedCashFacility], BANK1_MAKER1).then((createdCashFacility) => {
         issuedCashFacilityId = createdCashFacility.details._id;
+        ukefFacilityId = createdCashFacility.details.ukefFacilityId;
 
         cy.createGefFacilities(dealId, [issuedContingentFacility], BANK1_MAKER1).then((createdContingentFacility) => {
           issuedContingentFacilityId = createdContingentFacility.details._id;
@@ -65,6 +70,38 @@ context('Application details - Amendments Tab', () => {
       applicationAmendments.tabHeading().should('exist');
       applicationAmendments.tabHeading().should('have.text', 'Amendments');
       applicationAmendments.summaryList().should('have.length', 2);
+    });
+
+    it('should display the first row with the 2nd newest amendment', () => {
+      cy.assertText(applicationAmendments.amendmentDetails.row(2).facilityId(), ukefFacilityId);
+      cy.assertText(applicationAmendments.amendmentDetails.row(2).facilityType(), 'Cash Facility');
+      cy.assertText(applicationAmendments.amendmentDetails.row(2).status(), PORTAL_AMENDMENT_STATUS.ACKNOWLEDGED);
+
+      cy.assertText(applicationAmendments.amendmentDetails.row(2).newCoverEndDate(), today.dd_MMMM_yyyy);
+      cy.assertText(applicationAmendments.amendmentDetails.row(2).newFacilityEndDate(), today.dd_MMMM_yyyy);
+      cy.assertText(
+        applicationAmendments.amendmentDetails.row(2).newFacilityValue(),
+        `${CURRENCY.GBP} ${getFormattedMonetaryValue(CHANGED_FACILITY_VALUE, 2)}`,
+      );
+
+      cy.assertText(applicationAmendments.amendmentDetails.row(2).effectiveFrom(), today.dd_MMMM_yyyy);
+      cy.assertText(applicationAmendments.amendmentDetails.row(2).createdBy(), 'First Last');
+    });
+
+    it('should display the second row with the 3rd newest amendment', () => {
+      cy.assertText(applicationAmendments.amendmentDetails.row(1).facilityId(), ukefFacilityId);
+      cy.assertText(applicationAmendments.amendmentDetails.row(1).facilityType(), 'Cash Facility');
+      cy.assertText(applicationAmendments.amendmentDetails.row(1).status(), PORTAL_AMENDMENT_STATUS.ACKNOWLEDGED);
+
+      cy.assertText(applicationAmendments.amendmentDetails.row(1).newCoverEndDate(), today.dd_MMMM_yyyy);
+      cy.assertText(applicationAmendments.amendmentDetails.row(1).newFacilityEndDate(), today.dd_MMMM_yyyy);
+      cy.assertText(
+        applicationAmendments.amendmentDetails.row(1).newFacilityValue(),
+        `${CURRENCY.GBP} ${getFormattedMonetaryValue(CHANGED_FACILITY_VALUE, 2)}`,
+      );
+
+      cy.assertText(applicationAmendments.amendmentDetails.row(1).effectiveFrom(), today.dd_MMMM_yyyy);
+      cy.assertText(applicationAmendments.amendmentDetails.row(1).createdBy(), 'First Last');
     });
   });
 });
