7a3b9b464f3e8ed798f56d7fa5464d176b88dce9
rkontogianni <rkontogianni@ukeportfinance.gov.uk>
2026-02-06 17:01:12 +0000

    feat(DTFS2-8199): address pr comments

--- portal-ui/server/controllers/login/get-check-your-email-access-code.ts
+++ portal-ui/server/controllers/login/get-check-your-email-access-code.ts
@@ -7,7 +7,7 @@ type ViewModel = {
   email?: string;
 };
 
-type GetCheckYourEmailAccessCodePageRequestSession = { numberOfSignInOtpAttemptsRemaining?: number; userEmail?: string };
+type GetCheckYourEmailAccessCodePageRequestSession = { numberOfSignInOtpAttemptsRemaining: number; userEmail?: string };
 export type GetCheckYourEmailAccessCodePageRequest = CustomExpressRequest<Record<string, never>> & {
   session: GetCheckYourEmailAccessCodePageRequestSession;
 };
@@ -22,22 +22,17 @@ export const getCheckYourEmailAccessCodePage = (req: GetCheckYourEmailAccessCode
     session: { numberOfSignInOtpAttemptsRemaining: attemptsLeft, userEmail },
   } = req;
 
-  // we check if typeof attemptsLeft is undefined rather than falsy as it can be 0 when the user has used all their attempts
-  if (typeof attemptsLeft === 'undefined') {
-    console.error('No remaining OTP attempts found in session when rendering check your email access code page');
-    return res.render('partials/problem-with-service.njk');
-  }
-
   const viewModel: ViewModel = {
     attemptsLeft,
     requestNewCodeUrl: '/login/request-new-access-code',
     email: userEmail,
   };
 
-  try {
+  if (attemptsLeft >= 0) {
     return res.render('login/check-your-email-access-code.njk', viewModel);
-  } catch (error) {
-    console.error('Error getting check your email access code page %o', error);
-    return res.render('partials/problem-with-service.njk');
   }
+
+  console.error('Error getting check your email access code page');
+
+  return res.render('partials/problem-with-service.njk');
 };
