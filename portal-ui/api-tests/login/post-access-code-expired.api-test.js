d9b804813fe6efcad0c3e1171a2451e7ddc92a1e
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-02-24 15:18:44 +0000

    feat(DTFS2-8222): comment out api test until 2fa is avaiable

--- portal-ui/api-tests/login/post-access-code-expired.api-test.js
+++ portal-ui/api-tests/login/post-access-code-expired.api-test.js
@@ -1,65 +1,67 @@
-jest.mock('@ukef/dtfs2-common', () => ({
-  ...jest.requireActual('@ukef/dtfs2-common'),
-  verify: jest.fn((req, res, next) => next()),
-}));
+// TODO DTFS2-8222: - Enable these tests when 2FA login flow is enabled in `portal-ui\server\routes\login\index.js', this test is currently commented as the route is not active, tested locally by temporarily enabling the route and running the tests -- all passed.
 
-jest.mock('../../server/api', () => ({
-  login: jest.fn(),
-  loginWithSignInOtp: jest.fn(),
-  validateToken: jest.fn(() => false),
-  validatePartialAuthToken: jest.fn(),
-  sendSignInOTP: jest.fn(),
-}));
+// jest.mock('@ukef/dtfs2-common', () => ({
+//   ...jest.requireActual('@ukef/dtfs2-common'),
+//   verify: jest.fn((req, res, next) => next()),
+// }));
 
-const { when, resetAllWhenMocks } = require('jest-when');
-const { createApi } = require('@ukef/dtfs2-common/api-test');
-const { HttpStatusCode } = require('axios');
-const api = require('../../server/api');
-const app = require('../../server/createApp');
-const extractSessionCookie = require('../helpers/extractSessionCookie');
-const { withPartial2faAuthValidationApiTests } = require('../common-tests/partial-2fa-auth-validation-api-tests');
+// jest.mock('../../server/api', () => ({
+//   login: jest.fn(),
+//   loginWithSignInOtp: jest.fn(),
+//   validateToken: jest.fn(() => false),
+//   validatePartialAuthToken: jest.fn(),
+//   sendSignInOTP: jest.fn(),
+// }));
 
-const { post } = createApi(app);
+// const { when, resetAllWhenMocks } = require('jest-when');
+// const { createApi } = require('@ukef/dtfs2-common/api-test');
+// const { HttpStatusCode } = require('axios');
+// const api = require('../../server/api');
+// const app = require('../../server/createApp');
+// const extractSessionCookie = require('../helpers/extractSessionCookie');
+// const { withPartial2faAuthValidationApiTests } = require('../common-tests/partial-2fa-auth-validation-api-tests');
 
-const partialAuthToken = 'partial auth token';
-const email = 'email@example.com';
-const password = 'a password';
+// const { post } = createApi(app);
 
-describe('POST /login/check-your-email-access-code', () => {
-  withPartial2faAuthValidationApiTests({
-    makeRequestWithHeaders: (headers) => post({ sixDigitAccessCode: '123456' }, headers).to('/login/check-your-email-access-code'),
-    validateResponseWasSuccessful: (response) => {
-      expect(response.status).toEqual(HttpStatusCode.Found);
-    },
-  });
+// const partialAuthToken = 'partial auth token';
+// const email = 'email@example.com';
+// const password = 'a password';
 
-  describe('when the user has a valid partial auth token and an expired access code', () => {
-    let sessionCookie;
+// describe('POST /login/check-your-email-access-code', () => {
+//   withPartial2faAuthValidationApiTests({
+//     makeRequestWithHeaders: (headers) => post({ sixDigitAccessCode: '123456' }, headers).to('/login/check-your-email-access-code'),
+//     validateResponseWasSuccessful: (response) => {
+//       expect(response.status).toEqual(HttpStatusCode.Found);
+//     },
+//   });
 
-    beforeEach(async () => {
-      // Arrange
-      resetAllWhenMocks();
-      jest.clearAllMocks();
-      // Use a custom login mock that includes userId, which is required by the controller
-      api.login.mockResolvedValue({
-        token: partialAuthToken,
-        user: { email, userId: 'mock-user-id' },
-        loginStatus: 'Valid username and password',
-      });
-      api.sendSignInOTP.mockResolvedValue({ data: { numberOfSignInOtpAttemptsRemaining: 2 } });
-      api.loginWithSignInOtp.mockResolvedValue({ isExpired: true });
-      when(api.validatePartialAuthToken).calledWith(partialAuthToken).mockResolvedValue(undefined);
+//   describe('when the user has a valid partial auth token and an expired access code', () => {
+//     let sessionCookie;
 
-      sessionCookie = await post({ email, password }).to('/login').then(extractSessionCookie);
-    });
+//     beforeEach(async () => {
+//       // Arrange
+//       resetAllWhenMocks();
+//       jest.clearAllMocks();
+//       // Use a custom login mock that includes userId, which is required by the controller
+//       api.login.mockResolvedValue({
+//         token: partialAuthToken,
+//         user: { email, userId: 'mock-user-id' },
+//         loginStatus: 'Valid username and password',
+//       });
+//       api.sendSignInOTP.mockResolvedValue({ data: { numberOfSignInOtpAttemptsRemaining: 2 } });
+//       api.loginWithSignInOtp.mockResolvedValue({ isExpired: true });
+//       when(api.validatePartialAuthToken).calledWith(partialAuthToken).mockResolvedValue(undefined);
 
-    it('should redirect to /login/access-code-expired', async () => {
-      // Act
-      const response = await post({ sixDigitAccessCode: '000000' }, { Cookie: sessionCookie }).to('/login/check-your-email-access-code');
+//       sessionCookie = await post({ email, password }).to('/login').then(extractSessionCookie);
+//     });
 
-      // Assert
-      expect(response.status).toEqual(HttpStatusCode.Found);
-      expect(response.headers.location).toEqual('/login/access-code-expired');
-    });
-  });
-});
+//     it('should redirect to /login/access-code-expired', async () => {
+//       // Act
+//       const response = await post({ sixDigitAccessCode: '000000' }, { Cookie: sessionCookie }).to('/login/check-your-email-access-code');
+
+//       // Assert
+//       expect(response.status).toEqual(HttpStatusCode.Found);
+//       expect(response.headers.location).toEqual('/login/access-code-expired');
+//     });
+//   });
+// });
