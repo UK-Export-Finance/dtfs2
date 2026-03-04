154f639ee627939db09548fe9c41a01f0f345eee
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-03-04 10:53:40 +0000

    feat(DTFS2-8291): add error helpers export via index

--- dtfs-central-api/server/errors/index.ts
+++ dtfs-central-api/server/errors/index.ts
@@ -1,5 +1,7 @@
 export { ApiError } from './api.error';
+export { EmailSendError } from './email-send-error';
 export { InvalidEnvironmentVariableError } from './invalid-environment-variable.error';
+export { MissingUserFieldsError } from './missing-user-fields-error';
 export { InvalidPayloadError } from './invalid-payload';
 export { InvalidStateMachineTransitionError } from './invalid-state-machine-transition.error';
 export { NotImplementedError } from './not-implemented.error';
