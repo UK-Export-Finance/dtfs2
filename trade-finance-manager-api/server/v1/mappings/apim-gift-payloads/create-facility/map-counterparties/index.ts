5a3dc05d0b87b939484108b21375136e3d933298
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-03-09 18:32:27 +0000

    feat(DTFS2-8318): remove empty file

--- trade-finance-manager-api/server/v1/mappings/apim-gift-payloads/create-facility/map-counterparties/index.ts
+++
@@ -1,5 +0,0 @@
-// This will not always be immediately available.
-
-// If this field exists during deal submission - send facility to GIFT.
-
-// If the field does NOT exist - human entry required in TFM UI (parties-buyer-area.njk, postPartyDetails UI controller)
