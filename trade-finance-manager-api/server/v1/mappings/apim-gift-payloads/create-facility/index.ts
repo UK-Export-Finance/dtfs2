aed6d70cf1035af4a03d83e0039d278ee07c7b00
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-03-12 17:01:59 +0000

    feat(DTFS2-8316): markups

--- trade-finance-manager-api/server/v1/mappings/apim-gift-payloads/create-facility/index.ts
+++ trade-finance-manager-api/server/v1/mappings/apim-gift-payloads/create-facility/index.ts
@@ -32,7 +32,7 @@ export const createFacility = async ({ deal, facility }: FacilityCreationParams)
 
   const facilityCategoryCode = String(facilitySnapshot.type);
   const facilityName = facilitySnapshot.name;
-  const facilityAmount = Number(tfm.ukefExposure); // TODO: is this right?
+  const facilityAmount = Number(tfm.ukefExposure); // TODO: DTFS2-8306 is this correct?
   const productTypeCode = PRODUCT_TYPES.BSS; // TODO: DTFS2-8307
 
   const dealId = getTfmUkefDealId(deal);
