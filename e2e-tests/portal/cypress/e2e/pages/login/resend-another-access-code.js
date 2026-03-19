cabd8d18a94ab174dc07f339007726e3c0225e5a
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-03-19 09:28:34 +0000

    feat(DTFS2-8265): fix resend another ac test

--- e2e-tests/portal/cypress/e2e/pages/login/resend-another-access-code.js
+++ e2e-tests/portal/cypress/e2e/pages/login/resend-another-access-code.js
@@ -9,7 +9,6 @@ const resendAnotherAccessCode = {
   attemptsInfo: () => cy.get('[data-cy="access-code-attempts-info"]'),
   suspendInfo: () => cy.get('[data-cy="access-code-suspend-info"]'),
   submitButton: () => cy.get('[data-cy="submit-button"]'),
-  requestNewSignInLink: () => cy.get('[data-cy="request-new-sign-in-link"]'),
   obscuredEmail: () => cy.get('[data-cy="obscured-sign-in-link-target-email-address"]'),
   csrfToken: () => cy.get('[data-cy=csrf-input]').then((csrfInput) => csrfInput.attr('value')),
   errorSummary: () => cy.get('[data-cy="error-summary"]'),
