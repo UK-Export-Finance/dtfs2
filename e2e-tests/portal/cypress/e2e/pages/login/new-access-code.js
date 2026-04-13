ee3998d4d0e6be71f407425d879b37721545387a
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-04-13 12:58:30 +0100

    feat(DTFS2-8265): address pr comments

--- e2e-tests/portal/cypress/e2e/pages/login/new-access-code.js
+++ e2e-tests/portal/cypress/e2e/pages/login/new-access-code.js
@@ -7,7 +7,6 @@ const newAccessCode = {
   spamOrJunk: () => cy.get('[data-cy="access-code-spam-or-junk"]'),
   attemptsInfo: () => cy.get('[data-cy="access-code-attempts-info"]'),
   suspendInfo: () => cy.get('[data-cy="access-code-suspend-info"]'),
-  submitButton: () => cy.get('[data-cy="submit-button"]'),
   requestCodeLink: () => cy.get('[data-cy="request-code-link"]'),
   errorSummary: () => cy.get('[data-cy="error-summary"]'),
   inlineError: () => cy.get('[data-cy="six-digit-access-code-inline-error"]'),
