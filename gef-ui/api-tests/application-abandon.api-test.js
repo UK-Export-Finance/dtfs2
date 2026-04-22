15d302c675934ee1be11850de7fdb4c6a190999a
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-04-22 22:11:56 +0100

    chore(DTFS2-6697): update tests to use mocks

--- gef-ui/api-tests/application-abandon.api-test.js
+++ gef-ui/api-tests/application-abandon.api-test.js
@@ -2,18 +2,30 @@ const { createApi } = require('@ukef/dtfs2-common/api-test');
 const { MAKER } = require('../server/constants/roles');
 const { withRoleValidationApiTests } = require('./common-tests/role-validation-api-tests');
 const app = require('../server/createApp');
+const api = require('../server/services/api');
+const { MOCK_BASIC_DEAL } = require('../server/utils/mocks/mock-applications');
 
 const dealId = '123';
 
 const { get, post } = createApi(app);
 
 describe('application abandon routes', () => {
+  beforeEach(() => {
+    api.getApplication.mockResolvedValue(MOCK_BASIC_DEAL);
+    api.getFacilities.mockResolvedValue(MOCK_BASIC_DEAL.facilities);
+    api.getUserDetails.mockResolvedValue({});
+    api.setApplicationStatus.mockResolvedValue({});
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('GET /application-details/:dealId/abandon', () => {
     withRoleValidationApiTests({
       makeRequestWithHeaders: (headers) => get(`/application-details/${dealId}/abandon`, {}, headers),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 
@@ -21,8 +33,7 @@ describe('application abandon routes', () => {
     withRoleValidationApiTests({
       makeRequestWithHeaders: (headers) => post({}, headers).to(`/application-details/${dealId}/abandon`),
       whitelistedRoles: [MAKER],
-      successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
+      successCode: 302,
     });
   });
 });
