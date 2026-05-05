199458b8246d66b0bcde74245313ca93a6186c2f
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-05-05 21:07:11 +0100

    feat(DTFS2-8427): show suspended account on re-try login

--- portal-ui/server/controllers/login/post-login.test.js
+++ portal-ui/server/controllers/login/post-login.test.js
@@ -267,7 +267,7 @@ describe('postLogin', () => {
           });
         });
 
-        it(`should render login/temporarily-suspended-access-code.njk when a ${HttpStatusCode.Forbidden} error is thrown`, async () => {
+        it(`should redirect to /login/temporarily-suspended-access-code when a ${HttpStatusCode.Forbidden} error is thrown`, async () => {
           const forbiddenError = { response: { status: HttpStatusCode.Forbidden } };
           jest.mocked(api.login).mockRejectedValue(forbiddenError);
 
