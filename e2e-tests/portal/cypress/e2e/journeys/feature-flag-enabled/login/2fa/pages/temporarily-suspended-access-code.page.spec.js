1c38ad04d0db57704ae1491056cf25882418b587
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-05-11 16:54:36 +0100

    chore(DTFS2-8427): updated test to use assert text

--- e2e-tests/portal/cypress/e2e/journeys/feature-flag-enabled/login/2fa/pages/temporarily-suspended-access-code.page.spec.js
+++ e2e-tests/portal/cypress/e2e/journeys/feature-flag-enabled/login/2fa/pages/temporarily-suspended-access-code.page.spec.js
@@ -19,9 +19,9 @@ context('2FA Page - Temporarily suspended account', () => {
     cy.clearCookies();
     cy.clearLocalStorage();
 
-    cy.request({ url: '/login/temporarily-suspended-access-code', followRedirect: false }).then((resp) => {
-      expect(resp.status).to.equal(200);
-    });
+    temporarilySuspendedAccessCode.visit();
+
+    cy.assertText(temporarilySuspendedAccessCode.heading(), 'This account has been temporarily suspended');
   });
 
   it('should render temporarily suspended page with heading and message', () => {
