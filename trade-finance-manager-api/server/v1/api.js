e09b604ba796383229ce9cf343ab0a3bcdb6a09c
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-05-12 11:09:57 +0100

    chore(DTFS2-8416): logging improvement

--- trade-finance-manager-api/server/v1/api.js
+++ trade-finance-manager-api/server/v1/api.js
@@ -1938,6 +1938,8 @@ const getRecordCorrectionLogDetailsById = async (correctionId) => {
  */
 const createGiftFacility = async (facilityData) => {
   try {
+    console.info('Calling external API "Create GIFT facility" endpoint with data: %o', facilityData);
+
     const response = await axios({
       method: 'post',
       url: `${EXTERNAL_API_URL}/gift/facility`,
@@ -1946,7 +1948,7 @@ const createGiftFacility = async (facilityData) => {
     });
     return response.data;
   } catch (error) {
-    console.error('Unable to send GIFT facility %o', error);
+    console.error('Unable to send GIFT facility to external API %o', error);
 
     return false;
   }
