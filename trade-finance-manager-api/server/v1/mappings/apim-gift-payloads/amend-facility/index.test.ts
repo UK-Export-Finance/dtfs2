cdb3ef10a1616b48a24fcf03cb81668789423b50
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-05-21 14:17:11 +0100

    feat(DTFS2-8431-8431): update apim gift payload - amend facility

--- trade-finance-manager-api/server/v1/mappings/apim-gift-payloads/amend-facility/index.test.ts
+++ trade-finance-manager-api/server/v1/mappings/apim-gift-payloads/amend-facility/index.test.ts
@@ -1,11 +1,16 @@
+import { APIM_GIFT_INTEGRATION } from '../constants';
 import { amendFacility } from '.';
+
+const {
+  AMENDMENT_TYPE: { DECREASE_AMOUNT, INCREASE_AMOUNT, REPLACE_EXPIRY_DATE },
+} = APIM_GIFT_INTEGRATION;
 
 describe('amendFacility', () => {
   it('should be an object with amendment methods', () => {
     // Assert
-    expect(typeof amendFacility.decreaseAmount).toEqual('function');
-    expect(typeof amendFacility.increaseAmount).toEqual('function');
-    expect(typeof amendFacility.replaceExpiryDate).toEqual('function');
+    expect(typeof amendFacility[DECREASE_AMOUNT]).toEqual('function');
+    expect(typeof amendFacility[INCREASE_AMOUNT]).toEqual('function');
+    expect(typeof amendFacility[REPLACE_EXPIRY_DATE]).toEqual('function');
   });
 
   it('should have the correct amount of amendment methods', () => {
--- trade-finance-manager-api/server/v1/mappings/apim-gift-payloads/amend-facility/index.ts
+++ trade-finance-manager-api/server/v1/mappings/apim-gift-payloads/amend-facility/index.ts
@@ -1,9 +1,14 @@
+import { APIM_GIFT_INTEGRATION } from '../constants';
 import { decreaseAmount } from './decrease-amount';
 import { increaseAmount } from './increase-amount';
 import { replaceExpiryDate } from './replace-expiry-date';
 
+const {
+  AMENDMENT_TYPE: { DECREASE_AMOUNT, INCREASE_AMOUNT, REPLACE_EXPIRY_DATE },
+} = APIM_GIFT_INTEGRATION;
+
 export const amendFacility = {
-  decreaseAmount,
-  increaseAmount,
-  replaceExpiryDate,
+  [DECREASE_AMOUNT]: decreaseAmount,
+  [INCREASE_AMOUNT]: increaseAmount,
+  [REPLACE_EXPIRY_DATE]: replaceExpiryDate,
 };
