b7e67d750af803de5e87533da7ea2a866d7b0b09
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-05-21 15:18:22 +0100

    chore(DTFS2-8408): gef-ui happy path api tests

--- gef-ui/api-tests/companies-house.api-test.js
+++ gef-ui/api-tests/companies-house.api-test.js
@@ -2,18 +2,32 @@ const { createApi } = require('@ukef/dtfs2-common/api-test');
 const { MAKER } = require('../server/constants/roles');
 const { withRoleValidationApiTests } = require('./common-tests/role-validation-api-tests');
 const app = require('../server/createApp');
+const api = require('../server/services/api');
+const { MOCK_BASIC_DEAL } = require('../server/utils/mocks/mock-applications');
 
 const { get, post } = createApi(app);
 
 const dealId = '123';
 
 describe('companies house routes', () => {
+  beforeEach(() => {
+    api.getApplication.mockResolvedValue(MOCK_BASIC_DEAL);
+    api.getCompanyByRegistrationNumber.mockResolvedValue({
+      company: null,
+      errRef: 'regNumber',
+      errMsg: 'Enter a Companies House registration number',
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('GET /application-details/:dealId/companies-house', () => {
     withRoleValidationApiTests({
       makeRequestWithHeaders: (headers) => get(`/application-details/${dealId}/companies-house`, {}, headers),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 
@@ -22,7 +36,6 @@ describe('companies house routes', () => {
       makeRequestWithHeaders: (headers) => post({}, headers).to(`/application-details/${dealId}/companies-house`),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 });
