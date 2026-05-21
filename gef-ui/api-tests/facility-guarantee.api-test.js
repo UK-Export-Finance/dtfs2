4e4e229230c2ab73766f1f2a8f6e5960da0a43aa
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-05-21 15:57:16 +0100

    chore(DTFS2-8450): gef-ui happy path tests part-3

--- gef-ui/api-tests/facility-guarantee.api-test.js
+++ gef-ui/api-tests/facility-guarantee.api-test.js
@@ -2,6 +2,9 @@ const { createApi } = require('@ukef/dtfs2-common/api-test');
 const { MAKER } = require('../server/constants/roles');
 const { withRoleValidationApiTests } = require('./common-tests/role-validation-api-tests');
 const app = require('../server/createApp');
+const api = require('../server/services/api');
+const { MOCK_BASIC_DEAL } = require('../server/utils/mocks/mock-applications');
+const { MOCK_ISSUED_FACILITY } = require('../server/utils/mocks/mock-facilities');
 
 const { get, post } = createApi(app);
 
@@ -9,12 +12,20 @@ const dealId = '123';
 const facilityId = '111';
 
 describe('facility guarantee routes', () => {
+  beforeEach(() => {
+    api.getApplication.mockResolvedValue(MOCK_BASIC_DEAL);
+    api.getFacility.mockResolvedValue(MOCK_ISSUED_FACILITY);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('GET /application-details/:dealId/facilities/:facilityId/facility-guarantee', () => {
     withRoleValidationApiTests({
       makeRequestWithHeaders: (headers) => get(`/application-details/${dealId}/facilities/${facilityId}/facility-guarantee`, {}, headers),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 
@@ -23,7 +34,6 @@ describe('facility guarantee routes', () => {
       makeRequestWithHeaders: (headers) => post({}, headers).to(`/application-details/${dealId}/facilities/${facilityId}/facility-guarantee`),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 });
