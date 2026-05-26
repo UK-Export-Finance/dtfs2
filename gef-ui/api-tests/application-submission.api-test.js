2e380a70113408a6586611c14510a955d90c9a95
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-05-26 12:33:12 +0100

    chore(DTFS2-8453): refactor part-4 gef-ui happy path api tests

--- gef-ui/api-tests/application-submission.api-test.js
+++ gef-ui/api-tests/application-submission.api-test.js
@@ -2,18 +2,33 @@ const { createApi } = require('@ukef/dtfs2-common/api-test');
 const { MAKER } = require('../server/constants/roles');
 const { withRoleValidationApiTests } = require('./common-tests/role-validation-api-tests');
 const app = require('../server/createApp');
+const api = require('../server/services/api');
+const { MOCK_BASIC_DEAL } = require('../server/utils/mocks/mock-applications');
+
+const cloneMock = (value) => JSON.parse(JSON.stringify(value));
 
 const dealId = '123';
 
 const { get, post } = createApi(app);
 
 describe('application submission routes', () => {
+  beforeEach(() => {
+    api.getApplication.mockResolvedValue(cloneMock(MOCK_BASIC_DEAL));
+    api.getFacilities.mockResolvedValue({ status: 'Completed', items: [] });
+    api.getUserDetails.mockResolvedValue({ _id: '619bae3467cc7c002069fc21', firstname: 'Checker', surname: 'One' });
+    api.updateApplication.mockResolvedValue({});
+    api.setApplicationStatus.mockResolvedValue({});
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('GET /application-details/:dealId/submit', () => {
     withRoleValidationApiTests({
       makeRequestWithHeaders: (headers) => get(`/application-details/${dealId}/submit`, {}, headers),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 
@@ -22,7 +37,6 @@ describe('application submission routes', () => {
       makeRequestWithHeaders: (headers) => post({}, headers).to(`/application-details/${dealId}/submit`),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 });
