491195e69f9e80f48b8d061eac7d8cd6d80aded6
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-05-27 10:03:11 +0100

    chore(DTFS2-8454): part-5 gef-ui happy path api tests

--- gef-ui/api-tests/confirm-cover-start-date.api-test.js
+++ gef-ui/api-tests/confirm-cover-start-date.api-test.js
@@ -1,7 +1,16 @@
+jest.mock('@ukef/dtfs2-common', () => ({
+  ...jest.requireActual('@ukef/dtfs2-common'),
+  verify: jest.fn((req, res, next) => next()),
+}));
+
 const { createApi } = require('@ukef/dtfs2-common/api-test');
 const { MAKER } = require('../server/constants/roles');
 const { withRoleValidationApiTests } = require('./common-tests/role-validation-api-tests');
 const app = require('../server/createApp');
+const api = require('../server/services/api');
+const { MOCK_BASIC_DEAL } = require('../server/utils/mocks/mock-applications');
+
+const cloneMock = (value) => JSON.parse(JSON.stringify(value));
 
 const { get, post } = createApi(app);
 
@@ -9,12 +18,25 @@ const dealId = '123';
 const facilityId = '111';
 
 describe('confirm cover start date routes', () => {
+  beforeEach(() => {
+    api.getApplication.mockResolvedValue(cloneMock(MOCK_BASIC_DEAL));
+    api.getFacilities.mockResolvedValue({ status: 'Completed', items: [] });
+    api.getUserDetails.mockResolvedValue({ _id: '619bae3467cc7c002069fc21', firstname: 'Checker', surname: 'One' });
+    api.getPortalAmendmentsOnDeal.mockResolvedValue([]);
+    api.getFacility.mockResolvedValue({
+      details: { _id: facilityId, dealId, name: 'Facility one', coverEndDate: '2030-01-01T00:00:00.000Z', coverStartDate: null },
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('GET /application-details/:dealId/:facilityId/confirm-cover-start-date/', () => {
     withRoleValidationApiTests({
       makeRequestWithHeaders: (headers) => get(`/application-details/${dealId}/${facilityId}/confirm-cover-start-date/`, {}, headers),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 
@@ -23,7 +45,6 @@ describe('confirm cover start date routes', () => {
       makeRequestWithHeaders: (headers) => post({}, headers).to(`/application-details/${dealId}/${facilityId}/confirm-cover-start-date/`),
       whitelistedRoles: [MAKER],
       successCode: 200,
-      disableHappyPath: true, // TODO DTFS2-6697: remove and test happy path.
     });
   });
 });
