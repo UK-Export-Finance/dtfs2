94fc20237d750f463a85c95df446838d50092033
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-06-05 15:33:13 +0100

    chore(DTFS2-8431): update getIdFromNumberGenerator to always start with 00

--- e2e-tests/tfm/cypress/support/external-api/api.js
+++ e2e-tests/tfm/cypress/support/external-api/api.js
@@ -1,7 +1,10 @@
-// mock out number generator call and use 7 digit ID instead
-// reduced in size from 10 as premium schedule cannot handle large numbers
-let ukefId = 1e7;
+/**
+ * mock out number generator call and use zero-prefixed 8 digit ID instead
+ * reduced in size from 10 as premium schedule cannot handle large numbers
+ */
+let ukefId = 0;
+
 module.exports.getIdFromNumberGenerator = () => {
   ukefId += 1;
-  return String(ukefId);
+  return String(ukefId).padStart(8, '0');
 };
