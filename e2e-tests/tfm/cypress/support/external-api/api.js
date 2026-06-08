af20071c7a979e64aaec4eb7e7471b1cbfca561d
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-06-08 16:06:50 +0100

    feat(DTFS2-8431): markups

--- e2e-tests/tfm/cypress/support/external-api/api.js
+++ e2e-tests/tfm/cypress/support/external-api/api.js
@@ -1,10 +1,10 @@
 /**
- * mock out number generator call and use zero-prefixed 8 digit ID instead
+ * mock out number generator call and use zero-prefixed 10 digit UKEF ID format ("00" + 8 digits)
  * reduced in size from 10 as premium schedule cannot handle large numbers
  */
 let ukefId = 0;
 
 module.exports.getIdFromNumberGenerator = () => {
   ukefId += 1;
-  return String(ukefId).padStart(8, '0');
+  return String(ukefId).padStart(10, '0');
 };
