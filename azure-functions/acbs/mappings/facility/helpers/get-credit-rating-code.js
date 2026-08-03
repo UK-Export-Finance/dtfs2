a39b235d0847288676b30c1b76535ce8a3221f5c
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2026-08-03 09:38:09 +0100

    chore(DTFS2-8365): review markups

--- azure-functions/acbs/mappings/facility/helpers/get-credit-rating-code.js
+++ azure-functions/acbs/mappings/facility/helpers/get-credit-rating-code.js
@@ -5,10 +5,10 @@ const { EXPORTER_CREDIT_RATING, CREDIT_RATING } = CONSTANTS.DEAL;
 /**
  * Returns ACBS credit rating code based on deal's exporter credit rating.
  * `AIN` = B+(14)
- * `MIN` = TFM value
- * Handles both the old and new style of credit rating values
+ * `MIN` = TFM value if BB- or B+, otherwise Not known (98)
+ * Handles both the old and new style of stored credit rating values
  * @param {object} deal Deal object
- * @returns {string} ACBS credit rating code, defaults to `B+(14)`
+ * @returns {string} ACBS credit rating code`
  */
 const getCreditRatingCode = (deal) => {
   // `AIN` = B+(14)
@@ -19,19 +19,13 @@ const getCreditRatingCode = (deal) => {
   // `MIN` = TFM value
   if (deal.tfm) {
     switch (deal.tfm.exporterCreditRating) {
-      // BB- (13) for Good (BB-)
+      // BB- (13) for Good (BB-) or BB-
       case EXPORTER_CREDIT_RATING.BB_MINUS:
-        return CONSTANTS.FACILITY.CREDIT_RATING.BB_MINUS;
-
-      // BB- (13) for BB-
       case CREDIT_RATING.BB_MINUS:
         return CONSTANTS.FACILITY.CREDIT_RATING.BB_MINUS;
 
-      // B+ (14) for Acceptable (B+)
+      // B+ (14) for Acceptable (B+) or B+
       case EXPORTER_CREDIT_RATING.B_PLUS:
-        return CONSTANTS.FACILITY.CREDIT_RATING.B_PLUS;
-
-      // B+ (14) for B+
       case CREDIT_RATING.B_PLUS:
         return CONSTANTS.FACILITY.CREDIT_RATING.B_PLUS;
 
