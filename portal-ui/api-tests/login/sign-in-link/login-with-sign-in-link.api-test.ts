8e7936b0bf5eb596b8aa1312eeb863bcb72669d5
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-08-03 21:20:25 +0100

    chore(DTFS2-8543): convert sign in link api tests to typescript

--- portal-ui/api-tests/login/sign-in-link/login-with-sign-in-link.api-test.ts
+++ portal-ui/api-tests/login/sign-in-link/login-with-sign-in-link.api-test.ts
@@ -1,24 +1,27 @@
+import { when } from 'jest-when';
+import { HttpStatusCode } from 'axios';
+import { PORTAL_LOGIN_STATUS } from '@ukef/dtfs2-common';
+import { createApi } from '@ukef/dtfs2-common/api-test';
+import app from '../../../server/createApp';
+import * as api from '../../../server/api';
+import { HTTP_ERROR_CAUSES } from '../../../server/constants';
+
 jest.mock('@ukef/dtfs2-common', () => ({
-  ...jest.requireActual('@ukef/dtfs2-common'),
-  verify: jest.fn((req, res, next) => next()),
+  ...jest.requireActual<typeof import('@ukef/dtfs2-common')>('@ukef/dtfs2-common'),
+  verify: jest.fn((_req: unknown, _res: unknown, next: () => void): void => {
+    next();
+  }),
 }));
-jest.mock('../../server/api', () => ({
+
+jest.mock('../../../server/api', () => ({
   login: jest.fn(),
   sendSignInLink: jest.fn(),
   loginWithSignInLink: jest.fn(),
   validateToken: () => true,
 }));
 
-const { when } = require('jest-when');
-const { PORTAL_LOGIN_STATUS } = require('@ukef/dtfs2-common');
-const { createApi } = require('@ukef/dtfs2-common/api-test');
-const app = require('../../server/createApp');
-const api = require('../../server/api');
-const { HTTP_ERROR_CAUSES } = require('../../server/constants');
-
-const { get } = createApi(app);
-
 describe('GET /login/sign-in-link?t={signInToken}&u={userId}', () => {
+  const { get } = createApi(app);
   const validSignInToken = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef';
   const validUserId = '65626dc0bda51f77a78b86ae';
   const userToken = 'a token';
@@ -28,114 +31,114 @@ describe('GET /login/sign-in-link?t={signInToken}&u={userId}', () => {
     email: userEmail,
   };
 
-  const getSignInLinkLoginPage = (query) => get('/login/sign-in-link', query);
+  const getSignInLinkLoginPage = (query: { u?: string; t?: string }) => get('/login/sign-in-link', query);
 
-  it('returns a 200 response if the login API request succeeds', async () => {
+  it(`should return a ${HttpStatusCode.Ok} response if the login API request succeeds`, async () => {
     when(api.loginWithSignInLink)
       .calledWith({ signInToken: validSignInToken, userId: validUserId })
       .mockResolvedValueOnce({ loginStatus, token: userToken, user });
 
     const { status } = await getSignInLinkLoginPage({ u: validUserId, t: validSignInToken });
 
-    expect(status).toEqual(200);
+    expect(status).toEqual(HttpStatusCode.Ok);
   });
 
-  it('redirects to /login/sign-in-link-expired if the login API request fails with a token expired 403', async () => {
+  it(`should redirect to /login/sign-in-link-expired if the login API request fails with a token expired ${HttpStatusCode.Forbidden}`, async () => {
     when(api.loginWithSignInLink)
       .calledWith({ signInToken: validSignInToken, userId: validUserId })
       .mockRejectedValueOnce({
-        response: { status: 403, data: { errors: [{ cause: HTTP_ERROR_CAUSES.TOKEN_EXPIRED }] } },
+        response: { status: HttpStatusCode.Forbidden, data: { errors: [{ cause: HTTP_ERROR_CAUSES.TOKEN_EXPIRED }] } },
       });
 
     const { status, headers } = await getSignInLinkLoginPage({ u: validUserId, t: validSignInToken });
 
-    expect(status).toEqual(302);
+    expect(status).toEqual(HttpStatusCode.Found);
     expect(headers.location).toEqual('/login/sign-in-link-expired');
   });
 
-  it('redirects to /login/sign-in-link-expired if the login API request fails with a user blocked 403', async () => {
+  it(`should render the temporarily suspended page if the login API request fails with a user blocked ${HttpStatusCode.Forbidden}`, async () => {
     when(api.loginWithSignInLink)
       .calledWith({ signInToken: validSignInToken, userId: validUserId })
       .mockRejectedValueOnce({
-        response: { status: 403, data: { errors: [{ cause: HTTP_ERROR_CAUSES.USER_BLOCKED }] } },
+        response: { status: HttpStatusCode.Forbidden, data: { errors: [{ cause: HTTP_ERROR_CAUSES.USER_BLOCKED }] } },
       });
 
     const { status, text } = await getSignInLinkLoginPage({ u: validUserId, t: validSignInToken });
 
-    expect(status).toEqual(403);
+    expect(status).toEqual(HttpStatusCode.Forbidden);
     expect(text).toContain('This account has been temporarily suspended');
   });
 
-  it('redirects to /login if the login API request fails with a 401', async () => {
+  it(`should redirect to /login if the login API request fails with a ${HttpStatusCode.Unauthorized}`, async () => {
     when(api.loginWithSignInLink)
       .calledWith({ signInToken: validSignInToken, userId: validUserId })
-      .mockRejectedValueOnce({ response: { status: 401 } });
+      .mockRejectedValueOnce({ response: { status: HttpStatusCode.Unauthorized } });
 
     const { status, headers } = await getSignInLinkLoginPage({ u: validUserId, t: validSignInToken });
 
-    expect(status).toEqual(302);
+    expect(status).toEqual(HttpStatusCode.Found);
     expect(headers.location).toEqual('/login');
   });
 
-  it('redirects to /login if the login API request fails with a 404', async () => {
+  it(`should redirect to /login if the login API request fails with a ${HttpStatusCode.NotFound}`, async () => {
     when(api.loginWithSignInLink)
       .calledWith({ signInToken: validSignInToken, userId: validUserId })
-      .mockRejectedValueOnce({ response: { status: 404 } });
+      .mockRejectedValueOnce({ response: { status: HttpStatusCode.NotFound } });
 
     const { status, headers } = await getSignInLinkLoginPage({ u: validUserId, t: validSignInToken });
 
-    expect(status).toEqual(302);
+    expect(status).toEqual(HttpStatusCode.Found);
     expect(headers.location).toEqual('/login');
   });
 
-  it('returns a 500 response if the login API request has an unexpected error', async () => {
+  it(`should return a ${HttpStatusCode.InternalServerError} response if the login API request has an unexpected error`, async () => {
     when(api.loginWithSignInLink).calledWith({ signInToken: validSignInToken, userId: validUserId }).mockRejectedValueOnce(new Error());
 
     const { status, text } = await getSignInLinkLoginPage({ u: validUserId, t: validSignInToken });
 
-    expect(status).toEqual(500);
+    expect(status).toEqual(HttpStatusCode.InternalServerError);
     expect(text).toContain('Problem with the service');
   });
 
-  it('returns a 400 response if the u query string is not a valid ObjectId', async () => {
+  it(`should return a ${HttpStatusCode.BadRequest} response if the u query string is not a valid ObjectId`, async () => {
     const { status, text } = await getSignInLinkLoginPage({ u: '123', t: validSignInToken });
 
-    expect(status).toEqual(400);
+    expect(status).toEqual(HttpStatusCode.BadRequest);
     expect(text).toContain('Problem with the service');
   });
 
-  it('returns a 400 response if the u query string is not provided', async () => {
+  it(`should return a ${HttpStatusCode.BadRequest} response if the u query string is not provided`, async () => {
     const { status, text } = await getSignInLinkLoginPage({ t: validSignInToken });
 
-    expect(status).toEqual(400);
+    expect(status).toEqual(HttpStatusCode.BadRequest);
     expect(text).toContain('Problem with the service');
   });
 
-  it('returns a 400 response if the u query string is empty', async () => {
+  it(`should return a ${HttpStatusCode.BadRequest} response if the u query string is empty`, async () => {
     const { status, text } = await getSignInLinkLoginPage({ t: validSignInToken, u: '' });
 
-    expect(status).toEqual(400);
+    expect(status).toEqual(HttpStatusCode.BadRequest);
     expect(text).toContain('Problem with the service');
   });
 
-  it('returns a 400 response if the t query string is not a string of hex characters', async () => {
+  it(`should return a ${HttpStatusCode.BadRequest} response if the t query string is not a string of hex characters`, async () => {
     const { status, text } = await getSignInLinkLoginPage({ u: validUserId, t: 'not-a-hex-string' });
 
-    expect(status).toEqual(400);
+    expect(status).toEqual(HttpStatusCode.BadRequest);
     expect(text).toContain('Problem with the service');
   });
 
-  it('returns a 400 response if the t query string is not provided', async () => {
+  it(`should return a ${HttpStatusCode.BadRequest} response if the t query string is not provided`, async () => {
     const { status, text } = await getSignInLinkLoginPage({ u: validUserId });
 
-    expect(status).toEqual(400);
+    expect(status).toEqual(HttpStatusCode.BadRequest);
     expect(text).toContain('Problem with the service');
   });
 
-  it('returns a 400 response if the t query string is empty', async () => {
+  it(`should return a ${HttpStatusCode.BadRequest} response if the t query string is empty`, async () => {
     const { status, text } = await getSignInLinkLoginPage({ u: validUserId, t: '' });
 
-    expect(status).toEqual(400);
+    expect(status).toEqual(HttpStatusCode.BadRequest);
     expect(text).toContain('Problem with the service');
   });
 });
