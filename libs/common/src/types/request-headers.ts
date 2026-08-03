12f01634dd16fdc2bedec78970c8f19472b25baa
sungharsh-ukef <Sungharsh.Jarathi@ukexportfinance.gov.uk>
2026-08-03 21:52:02 +0100

    chore(DTFS2-8543): update request-headers ts type

--- libs/common/src/types/request-headers.ts
+++ libs/common/src/types/request-headers.ts
@@ -3,9 +3,12 @@
  *
  * This type models the subset of headers tests need to pass to the test
  * HTTP client (for example a `Cookie` header containing session information).
+ * Any additional header is allowed via the index signature so this type stays
+ * compatible with Node's `IncomingHttpHeaders` shape.
  *
- * @property Cookie - A single `Cookie` string or an array of cookie strings.
+ * @property Cookie - Optional `Cookie` header (string or array of cookie strings).
  */
 export type RequestHeaders = {
-  Cookie: string | string[];
+  Cookie?: string | string[];
+  [key: string]: string | string[] | undefined;
 };
