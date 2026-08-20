48d6820cf3e8ddf6d34c3ad6d873cc3c98fa16b9
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2026-08-20 18:19:19 +0100

    chore(DTFS2-8570): documentation

--- libs/common/src/helpers/errors/log-user-auth-error.ts
+++ libs/common/src/helpers/errors/log-user-auth-error.ts
@@ -7,6 +7,12 @@ export type AuthErrorObject = {
   };
 };
 
+/**
+ * Logs user authentication errors to the console with a custom message.
+ * If certain properties of the error object are missing, it will log default values instead.
+ * @param error The error object containing details about the authentication error.
+ * @param message A custom message to provide context for the error.
+ */
 export const logUserAuthError = (error: AuthErrorObject, message: string) => {
   const status = error?.response?.status;
   const errorMessage = error?.message ?? 'Unknown error';
