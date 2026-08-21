6624ac6016606c9873360b222a526d8a5b617d3c
zainUKEF <zain.kassam@ukexportfinance.gov.uk>
2026-08-21 10:19:53 +0100

    chore(deps): install chokidar

--- trade-finance-manager-ui/jest.common.config.js
+++ trade-finance-manager-ui/jest.common.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
+  // Dependencies are hoisted to the repository root by npm workspaces. Jest
+  // resolves presets relative to this package's rootDir, so use absolute
+  // package paths to support the hoisted layout.
+  preset: require.resolve('ts-jest/jest-preset.js'),
   moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'babel-jest',
+    '^.+\\.(css|less|scss)$': require.resolve('babel-jest'),
   },
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': require.resolve('babel-jest'),
+    '^.+\\.ts$': require.resolve('ts-jest'),
   },
 };
